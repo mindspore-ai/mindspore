5e9cfaf6effad2cf6121e63ca434a31bd00f7d4d
chang zherui <760161589@qq.com>
2020-04-10 19:20:37 +0800

    syn-code1

--- mindspore/ccsrc/utils/callbacks.h
+++ mindspore/ccsrc/utils/callbacks.h
@@ -40,7 +40,7 @@ const int kCallbackOk = 0;
 const int kCallbackFalied = 1;
 
 bool GetParameterShape(const FuncGraphPtr& anf_graph, const std::string& param_name,
-                       const std::shared_ptr<std::vector<int>>& shape)
+                       const std::shared_ptr<std::vector<int>>& shape);
 uint32_t SummarySaveCallback(uint32_t, const std::map<std::string, TensorPtr>&);
 
 }  // namespace callbacks
