4f8fa79f33f1db91a1d68db0b8c3cf5cf456ac26
buxue <yiren19920727@163.com>
2020-04-20 20:52:05 +0800

    fix attribute mapping when docking open source operators

--- mindspore/ops/_op_impl/tbe/depthwise_conv2d_backprop_filter.py
+++ mindspore/ops/_op_impl/tbe/depthwise_conv2d_backprop_filter.py
@@ -26,7 +26,7 @@ depthwise_conv2d_backprop_filter_op_info = TBERegOp("DepthwiseConv2dNativeBackpr
     .attr("filter_size", "required", "listInt", "all") \
     .attr("stride", "required", "listInt", "all") \
     .attr("dilation", "required", "listInt", "all") \
-    .attr("pads", "required", "str", "all") \
+    .attr("pads", "required", "listInt", "all") \
     .attr("data_format", "required", "str", "all") \
     .input(0, "input", False, "required", "all") \
     .input(1, "out_backprop", False, "required", "all") \
