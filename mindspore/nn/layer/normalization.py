d51cbb6e33b27524d9dfa62bc1705656f4d8ab8b
zhaojichen <zhaojichen1@huawei.com>
2020-04-26 22:25:48 -0400

    fix doc problems

--- mindspore/nn/layer/normalization.py
+++ mindspore/nn/layer/normalization.py
@@ -324,7 +324,7 @@ class GlobalBatchNorm(_BatchNorm):
 
     Args:
         num_features (int): `C` from an expected input of size (N, C, H, W).
-        device_num_each_group (int): The number of device in each group.
+        device_num_each_group (int): The number of devices in each group.
         eps (float): A value added to the denominator for numerical stability. Default: 1e-5.
         momentum (float): A floating hyperparameter of the momentum for the
             running_mean and running_var computation. Default: 0.9.
@@ -350,7 +350,7 @@ class GlobalBatchNorm(_BatchNorm):
         Tensor, the normalized, scaled, offset tensor, of shape :math:`(N, C_{out}, H_{out}, W_{out})`.
 
     Examples:
-        >>> global_bn_op = nn.GlobalBatchNorm(num_features=3, group=4)
+        >>> global_bn_op = nn.GlobalBatchNorm(num_features=3, device_num_each_group=4)
         >>> input = Tensor(np.random.randint(0, 255, [1, 3, 224, 224]), mindspore.float32)
         >>> global_bn_op(input)
     """
