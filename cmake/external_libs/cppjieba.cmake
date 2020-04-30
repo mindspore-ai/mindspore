5edadf1542effa052f461b430daf41da8b2aad1c
xulei2020 <xulei83@huawei.com>
2020-04-30 11:02:47 +0800

    add jieba c++ code

--- cmake/external_libs/cppjieba.cmake
+++ cmake/external_libs/cppjieba.cmake
@@ -3,7 +3,7 @@ set(cppjieba_CFLAGS "-D_FORTIFY_SOURCE=2 -O2")
 mindspore_add_pkg(cppjieba
         VER 5.0.3
         HEAD_ONLY ./
-        URL https://codeload.github.com/yanyiwu/cppjieba/tar.gz/v5.0.3
+        URL https://github.com/yanyiwu/cppjieba/archive/v5.0.3.tar.gz
         MD5 b8b3f7a73032c9ce9daafa4f67196c8c
         PATCHES ${CMAKE_SOURCE_DIR}/third_party/patch/cppjieba/cppjieba.patch001)
 include_directories(${cppjieba_INC}include)
