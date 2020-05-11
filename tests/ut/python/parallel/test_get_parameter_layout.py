05fde3d23d34f7f9cb078fdd5e525a8aed40d48b
yangzhenzhang <285824651@qq.com>
2020-05-11 14:07:35 +0800

    add slice shape for param info

--- tests/ut/python/parallel/test_get_parameter_layout.py
+++ tests/ut/python/parallel/test_get_parameter_layout.py
@@ -48,8 +48,8 @@ def test_get_parameter_layout():
     net.set_auto_parallel()
     exe = me._executor
     exe.compile(net, x, auto_parallel_mode=True)
-    x_layout = ([2, 4], [1, -1])  # device_arrangement = [2, 4], tensor_map = [1, -1]
-    weight_layout = ([2, 4], [0, -1])  # device_arrangement = [2, 4], tensor_map = [0, -1]
+    x_layout = [[2, 4], [1, -1], [16, 32]]  # device_arrangement = [2, 4], tensor_map = [1, -1]
+    weight_layout = [[2, 4], [0, -1], [16, 32]]  # device_arrangement = [2, 4], tensor_map = [0, -1]
     expect_dict = {'x': x_layout, 'w1': weight_layout}
     # to be resovled: static local variable count_p is used in step_parallel.cc, it needs to be reset between each ut 
     assert (net.parameter_layout_dict == expect_dict)
