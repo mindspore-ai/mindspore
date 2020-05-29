df04230e13878abdace99ece37403b83412d0988
lizhenyu <lizhenyu13@huawei.com>
2020-05-29 20:53:50 +0800

    fix get nullptr when use graph manager

--- mindspore/ccsrc/session/session_basic.cc
+++ mindspore/ccsrc/session/session_basic.cc
@@ -590,6 +590,7 @@ KernelGraphPtr SessionBasic::ConstructKernelGraph(const AnfNodePtrList &lst, con
   graph->set_output(ConstructOutput(outputs, graph));
   MS_EXCEPTION_IF_NULL(context_);
   FuncGraphManagerPtr manager = MakeManager({graph});
+  context_->AddManager(manager);
   if (manager) {
     manager->AddFuncGraph(graph);
     graph->set_manager(manager);
