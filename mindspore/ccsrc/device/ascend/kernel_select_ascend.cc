ba48964f2a7e8581cc84f395ee3a4a4c4ecf4b13
WilliamLian <lianliguang@huawei.com>
2020-05-30 15:42:49 +0800

    add reduce mean kernel filter function

--- mindspore/ccsrc/device/ascend/kernel_select_ascend.cc
+++ mindspore/ccsrc/device/ascend/kernel_select_ascend.cc
@@ -178,7 +178,7 @@ void SetTensorDeviceInfo(const kernel::KernelBuildInfo &selected_kernel_info, co
     if (AnfAlgo::GetOutputDeviceDataType(real_input_node, 0) == kTypeUnknown || is_ref) {
       std::vector<std::string> output_format = {selected_kernel_info.GetInputFormat(input_index)};
       builder->SetOutputsFormat(output_format);
-      std::vector<TypeId> output_type = {AnfAlgo::GetOutputInferDataType(real_input_node, 0)};
+      std::vector<TypeId> output_type = {selected_kernel_info.GetInputDeviceType(input_index)};
       builder->SetOutputsDeviceType(output_type);
       AnfAlgo::SetSelectKernelBuildInfo(builder->Build(), real_input_node.get());
     }
