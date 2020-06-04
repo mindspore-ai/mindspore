b33a29a7a557559f9afdbab31e414a86981a9a26
wukesong <wukesong1@huawei.com>
2020-06-04 20:55:31 +0800

    dataset mode True default

--- example/alexnet_cifar10/train.py
+++ example/alexnet_cifar10/train.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     parser.add_argument('--data_path', type=str, default="./", help='path where the dataset is saved')
     parser.add_argument('--ckpt_path', type=str, default="./ckpt", help='if is test, must provide\
                         path where the trained ckpt file')
-    parser.add_argument('--dataset_sink_mode', type=bool, default=False, help='dataset_sink_mode is False or True')
+    parser.add_argument('--dataset_sink_mode', type=bool, default=True, help='dataset_sink_mode is False or True')
     args = parser.parse_args()
 
     context.set_context(mode=context.GRAPH_MODE, device_target=args.device_target)
