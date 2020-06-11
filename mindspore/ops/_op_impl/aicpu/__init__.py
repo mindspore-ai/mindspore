10555b77026e92ee7e75d13e4436230ba279ce23
xutianchun <xutianchun@huawei.com>
2020-06-11 15:36:36 +0800

    add CropAndResize op

--- mindspore/ops/_op_impl/aicpu/__init__.py
+++ mindspore/ops/_op_impl/aicpu/__init__.py
@@ -29,3 +29,4 @@ from .rnnt_loss import _rnnt_loss_aicpu
 from .random_categorical import _random_categorical_aicpu
 from .reverse_sequence import _reverse_sequence_aicpu
 from .pack import _pack_aicpu
+from .crop_and_resize import _crop_and_resize_aicpu
