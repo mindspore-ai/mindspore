5a6a55c5e716c6404c1c7c1d54137c636bb285f1
jinyaohui <jinyaohui@huawei.com>
2020-06-22 22:06:09 +0800

    delete ENABLE_GE

--- mindspore/ccsrc/pipeline/pipeline.cc
+++ mindspore/ccsrc/pipeline/pipeline.cc
@@ -245,9 +245,7 @@ py::dict ExecutorPy::GetAllreduceFusion(const std::string &phase) {
 }
 
 void ExecutorPy::DelNetRes(const std::string &id) {
-#ifdef ENABLE_GE
   FinalizeBackend();
-#endif
   if (executor_ != nullptr) {
     bool flag = false;
     auto tmp_info = info_;
