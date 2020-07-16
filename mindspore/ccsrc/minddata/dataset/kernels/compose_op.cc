c41fbd96b707d6f65f05b77ed7bf9563e133e5c4
Zirui Wu <zirui.wu@huawei.com>
2020-07-16 09:47:05 -0400

    fix minor comments
    follow up

--- mindspore/ccsrc/minddata/dataset/kernels/compose_op.cc
+++ mindspore/ccsrc/minddata/dataset/kernels/compose_op.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 Huawei Technologies Co., Ltd
+ * Copyright 2020 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@
 #include <vector>
 
 #include "minddata/dataset/core/tensor.h"
-#include "minddata/dataset/kernels/py_func_op.h"
 #include "minddata/dataset/kernels/tensor_op.h"
 #include "minddata/dataset/util/status.h"
 
