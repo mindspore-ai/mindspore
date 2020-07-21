09b437285b2765cae11d77a7fdf3cca74be3fa96
kingfo <wangqiuliang@huawei.com>
2020-07-21 22:15:26 +0800

    support mix precision in pynative

--- tests/st/ops/cpu/test_lstm_op.py
+++ tests/st/ops/cpu/test_lstm_op.py
@@ -23,7 +23,7 @@ from mindspore.ops import composite as C
 from mindspore.common.tensor import Tensor
 from mindspore.common.parameter import ParameterTuple, Parameter
 
-context.set_context(device_target='CPU')
+context.set_context(mode=context.GRAPH_MODE, device_target='CPU')
 
 
 class LstmNet(nn.Cell):
