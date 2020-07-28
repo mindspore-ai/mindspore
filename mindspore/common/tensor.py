fix batchnorm under mix precision in pynative mode

mindspore/common/tensor.py
@@ -278,7 +278,7 @@ class SparseTensor:
     Returns:
         SparseTensor, composed of `indices`, `values`, `dense_shape`.
 
-    Examples:     
+    Examples:
         >>> class Net(nn.Cell):
         >>>     def __init__(self, dense_shape):
         >>>         super(Net, self).__init__()