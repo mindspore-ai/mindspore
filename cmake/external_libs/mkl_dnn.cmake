13df76c984203dff42c61e0d13aa61df961b5035
kswang <wangkaisheng2@huawei.com>
2020-08-04 21:41:07 +0800

    upgrade dnnl to v1.5

--- cmake/external_libs/mkl_dnn.cmake
+++ cmake/external_libs/mkl_dnn.cmake
@@ -2,18 +2,18 @@ set(onednn_CXXFLAGS "-D_FORTIFY_SOURCE=2 -O2")
 set(onednn_CFLAGS "-D_FORTIFY_SOURCE=2 -O2")
 if (CMAKE_SYSTEM_NAME MATCHES "Windows")
     mindspore_add_pkg(onednn
-        VER 1.1.1
+        VER 1.5
         LIBS dnnl mkldnn
         HEAD_ONLY ./include
         RELEASE on
-        URL https://github.com/oneapi-src/oneDNN/releases/download/v1.1.1/dnnl_win_1.1.1_cpu_vcomp.zip
-        MD5 ecaab9ed549643067699c80e5cea1c23)
+        URL https://github.com/oneapi-src/oneDNN/releases/download/v1.5/dnnl_win_1.5.0_cpu_vcomp.zip
+        MD5 17757c84f49edd42d34ae8c9288110a1)
 else()
     mindspore_add_pkg(onednn
-        VER 1.1.2
+        VER 1.5
         LIBS dnnl mkldnn
-        URL https://github.com/oneapi-src/oneDNN/archive/v1.1.2.tar.gz
-        MD5 ab40d52230f3ad1d7a6f06ce0f6bc17a
+        URL https://github.com/oneapi-src/oneDNN/archive/v1.5.tar.gz
+        MD5 5d97e0e8f4c0b37da5f524533b7a644b
         CMAKE_OPTION -DDNNL_ARCH_OPT_FLAGS='' -DDNNL_CPU_RUNTIME='SEQ' -DDNNL_BUILD_EXAMPLES=OFF -DDNNL_BUILD_TESTS=OFF)
 endif()
 
