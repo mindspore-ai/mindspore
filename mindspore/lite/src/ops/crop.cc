e872470c70bd45c7ce099eafe1c6868fe8c950fa
yeyunpeng <yeyunpeng1@huawei.com>
2020-08-11 10:16:10 +0800

    fix crop and DeconvDepthwiseConv2D bugs

--- mindspore/lite/src/ops/crop.cc
+++ mindspore/lite/src/ops/crop.cc
@@ -32,7 +32,8 @@ int Crop::InferShape(std::vector<tensor::Tensor *> inputs, std::vector<tensor::T
     return RET_PARAM_INVALID;
   }
   outputs[0]->set_shape(inputs[1]->shape());
-  outputs[0]->SetFormat(inputs[1]->GetFormat());
+  outputs[0]->SetFormat(inputs[0]->GetFormat());
+  outputs[0]->set_data_type(inputs[0]->data_type());
 
   return RET_OK;
 }
