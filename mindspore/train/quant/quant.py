7bc5b71b44b89d1d1080c2138060d4fa492eed2e
chengxianbin <chengxianbin@huawei.com>
2020-08-13 23:22:49 +0800

    modify quant DenseBnAct code

--- mindspore/train/quant/quant.py
+++ mindspore/train/quant/quant.py
@@ -274,7 +274,7 @@ class ConvertToQuantNetwork:
         subcell.dense = dense_inner
         if subcell.has_act and subcell.activation is not None:
             subcell.activation = self._convert_activation(subcell.activation)
-        elif subcell.after_fake:
+        else:
             subcell.has_act = True
             subcell.activation = _AddFakeQuantAfterSubCell(F.identity,
                                                            num_bits=self.act_bits,
