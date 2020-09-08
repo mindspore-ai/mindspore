e4a0b0db34d712e47b65c5214ec1e8045ed08ff4
caojian05 <caojian5@huawei.com>
2020-09-08 23:35:56 +0800

    googlenet support imagenet dataset on Ascend

--- model_zoo/googlenet/eval.py
+++ model_zoo/googlenet/eval.py
@@ -16,30 +16,64 @@
 ##############test googlenet example on cifar10#################
 python eval.py
 """
+import argparse
+
 import mindspore.nn as nn
 from mindspore import context
 from mindspore.nn.optim.momentum import Momentum
 from mindspore.train.model import Model
 from mindspore.train.serialization import load_checkpoint, load_param_into_net
 
-from src.config import cifar_cfg as cfg
-from src.dataset import create_dataset
+from src.config import cifar_cfg, imagenet_cfg
+from src.dataset import create_dataset_cifar10, create_dataset_imagenet
+
 from src.googlenet import GoogleNet
 
 
+parser = argparse.ArgumentParser(description='googlenet')
+parser.add_argument('--dataset_name', type=str, default='cifar10', choices=['imagenet', 'cifar10'],
+                    help='dataset name.')
+parser.add_argument('--checkpoint_path', type=str, default=None, help='Checkpoint file path')
+args_opt = parser.parse_args()
+
 if __name__ == '__main__':
+
+    if args_opt.dataset_name == 'cifar10':
+        cfg = cifar_cfg
+        dataset = create_dataset_cifar10(cfg.data_path, 1, False)
+        loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction='mean', is_grad=False)
+        net = GoogleNet(num_classes=cfg.num_classes)
+        opt = Momentum(filter(lambda x: x.requires_grad, net.get_parameters()), 0.01, cfg.momentum,
+                       weight_decay=cfg.weight_decay)
+        model = Model(net, loss_fn=loss, optimizer=opt, metrics={'acc'})
+
+    elif args_opt.dataset_name == "imagenet":
+        cfg = imagenet_cfg
+        dataset = create_dataset_imagenet(cfg.val_data_path, 1, False)
+        if not cfg.use_label_smooth:
+            cfg.label_smooth_factor = 0.0
+        loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction="mean",
+                                                smooth_factor=cfg.label_smooth_factor, num_classes=cfg.num_classes)
+        net = GoogleNet(num_classes=cfg.num_classes)
+        model = Model(net, loss_fn=loss, metrics={'top_1_accuracy', 'top_5_accuracy'})
+
+    else:
+        raise ValueError("dataset is not support.")
+
+    device_target = cfg.device_target
     context.set_context(mode=context.GRAPH_MODE, device_target=cfg.device_target)
-    context.set_context(device_id=cfg.device_id)
+    if device_target == "Ascend":
+        context.set_context(device_id=cfg.device_id)
 
-    net = GoogleNet(num_classes=cfg.num_classes)
-    opt = Momentum(filter(lambda x: x.requires_grad, net.get_parameters()), 0.01, cfg.momentum,
-                   weight_decay=cfg.weight_decay)
-    loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction='mean', is_grad=False)
-    model = Model(net, loss_fn=loss, optimizer=opt, metrics={'acc'})
+    if args_opt.checkpoint_path is not None:
+        param_dict = load_checkpoint(args_opt.checkpoint_path)
+        print("load checkpoint from [{}].".format(args_opt.checkpoint_path))
+    else:
+        param_dict = load_checkpoint(cfg.checkpoint_path)
+        print("load checkpoint from [{}].".format(cfg.checkpoint_path))
 
-    param_dict = load_checkpoint(cfg.checkpoint_path)
     load_param_into_net(net, param_dict)
     net.set_train(False)
-    dataset = create_dataset(cfg.data_path, 1, False)
+
     acc = model.eval(dataset)
     print("accuracy: ", acc)
--- model_zoo/googlenet/train.py
+++ model_zoo/googlenet/train.py
@@ -25,21 +25,23 @@ import numpy as np
 import mindspore.nn as nn
 from mindspore import Tensor
 from mindspore import context
-from mindspore.communication.management import init
+from mindspore.communication.management import init, get_rank
 from mindspore.nn.optim.momentum import Momentum
 from mindspore.train.callback import ModelCheckpoint, CheckpointConfig, LossMonitor, TimeMonitor
-from mindspore.train.model import Model, ParallelMode
+from mindspore.train.loss_scale_manager import DynamicLossScaleManager, FixedLossScaleManager
+from mindspore.train.model import Model
+from mindspore import ParallelMode
 from mindspore.train.serialization import load_checkpoint, load_param_into_net
 
-from src.config import cifar_cfg as cfg
-from src.dataset import create_dataset
+from src.config import cifar_cfg, imagenet_cfg
+from src.dataset import create_dataset_cifar10, create_dataset_imagenet
 from src.googlenet import GoogleNet
 
 random.seed(1)
 np.random.seed(1)
 
 
-def lr_steps(global_step, lr_max=None, total_epochs=None, steps_per_epoch=None):
+def lr_steps_cifar10(global_step, lr_max=None, total_epochs=None, steps_per_epoch=None):
     """Set learning rate."""
     lr_each_step = []
     total_steps = steps_per_epoch * total_epochs
@@ -60,25 +62,79 @@ def lr_steps(global_step, lr_max=None, total_epochs=None, steps_per_epoch=None):
     return learning_rate
 
 
+def lr_steps_imagenet(_cfg, steps_per_epoch):
+    """lr step for imagenet"""
+    from src.lr_scheduler.warmup_step_lr import warmup_step_lr
+    from src.lr_scheduler.warmup_cosine_annealing_lr import warmup_cosine_annealing_lr
+    if _cfg.lr_scheduler == 'exponential':
+        _lr = warmup_step_lr(_cfg.lr_init,
+                             _cfg.lr_epochs,
+                             steps_per_epoch,
+                             _cfg.warmup_epochs,
+                             _cfg.epoch_size,
+                             gamma=_cfg.lr_gamma,
+                            )
+    elif _cfg.lr_scheduler == 'cosine_annealing':
+        _lr = warmup_cosine_annealing_lr(_cfg.lr_init,
+                                         steps_per_epoch,
+                                         _cfg.warmup_epochs,
+                                         _cfg.epoch_size,
+                                         _cfg.T_max,
+                                         _cfg.eta_min)
+    else:
+        raise NotImplementedError(_cfg.lr_scheduler)
+
+    return _lr
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Cifar10 classification')
+    parser = argparse.ArgumentParser(description='Classification')
+    parser.add_argument('--dataset_name', type=str, default='cifar10', choices=['imagenet', 'cifar10'],
+                        help='dataset name.')
     parser.add_argument('--device_id', type=int, default=None, help='device id of GPU or Ascend. (Default: None)')
     args_opt = parser.parse_args()
 
-    context.set_context(mode=context.GRAPH_MODE, device_target=cfg.device_target)
-    if args_opt.device_id is not None:
-        context.set_context(device_id=args_opt.device_id)
+    if args_opt.dataset_name == "cifar10":
+        cfg = cifar_cfg
+    elif args_opt.dataset_name == "imagenet":
+        cfg = imagenet_cfg
     else:
-        context.set_context(device_id=cfg.device_id)
+        raise ValueError("Unsupport dataset.")
 
+    # set context
+    device_target = cfg.device_target
+
+    context.set_context(mode=context.GRAPH_MODE, device_target=cfg.device_target)
     device_num = int(os.environ.get("DEVICE_NUM", 1))
-    if device_num > 1:
-        context.reset_auto_parallel_context()
-        context.set_auto_parallel_context(device_num=device_num, parallel_mode=ParallelMode.DATA_PARALLEL,
-                                          mirror_mean=True)
+
+    if device_target == "Ascend":
+        if args_opt.device_id is not None:
+            context.set_context(device_id=args_opt.device_id)
+        else:
+            context.set_context(device_id=cfg.device_id)
+
+        if device_num > 1:
+            context.reset_auto_parallel_context()
+            context.set_auto_parallel_context(device_num=device_num, parallel_mode=ParallelMode.DATA_PARALLEL,
+                                              mirror_mean=True)
+            init()
+    elif device_target == "GPU":
         init()
 
-    dataset = create_dataset(cfg.data_path, cfg.epoch_size)
+        if device_num > 1:
+            context.reset_auto_parallel_context()
+            context.set_auto_parallel_context(device_num=device_num, parallel_mode=ParallelMode.DATA_PARALLEL,
+                                              mirror_mean=True)
+    else:
+        raise ValueError("Unsupported platform.")
+
+    if args_opt.dataset_name == "cifar10":
+        dataset = create_dataset_cifar10(cfg.data_path, cfg.epoch_size)
+    elif args_opt.dataset_name == "imagenet":
+        dataset = create_dataset_imagenet(cfg.data_path, cfg.epoch_size)
+    else:
+        raise ValueError("Unsupport dataset.")
+
     batch_num = dataset.get_dataset_size()
 
     net = GoogleNet(num_classes=cfg.num_classes)
@@ -86,16 +142,75 @@ if __name__ == '__main__':
     if cfg.pre_trained:
         param_dict = load_checkpoint(cfg.checkpoint_path)
         load_param_into_net(net, param_dict)
-    lr = lr_steps(0, lr_max=cfg.lr_init, total_epochs=cfg.epoch_size, steps_per_epoch=batch_num)
-    opt = Momentum(filter(lambda x: x.requires_grad, net.get_parameters()), Tensor(lr), cfg.momentum,
-                   weight_decay=cfg.weight_decay)
-    loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction='mean', is_grad=False)
-    model = Model(net, loss_fn=loss, optimizer=opt, metrics={'acc'},
-                  amp_level="O2", keep_batchnorm_fp32=False, loss_scale_manager=None)
+
+    loss_scale_manager = None
+    if args_opt.dataset_name == 'cifar10':
+        lr = lr_steps_cifar10(0, lr_max=cfg.lr_init, total_epochs=cfg.epoch_size, steps_per_epoch=batch_num)
+        opt = Momentum(filter(lambda x: x.requires_grad, net.get_parameters()),
+                       learning_rate=Tensor(lr),
+                       momentum=cfg.momentum,
+                       weight_decay=cfg.weight_decay)
+        loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction='mean', is_grad=False)
+
+    elif args_opt.dataset_name == 'imagenet':
+        lr = lr_steps_imagenet(cfg, batch_num)
+
+
+        def get_param_groups(network):
+            """ get param groups """
+            decay_params = []
+            no_decay_params = []
+            for x in network.trainable_params():
+                parameter_name = x.name
+                if parameter_name.endswith('.bias'):
+                    # all bias not using weight decay
+                    # print('no decay:{}'.format(parameter_name))
+                    no_decay_params.append(x)
+                elif parameter_name.endswith('.gamma'):
+                    # bn weight bias not using weight decay, be carefully for now x not include BN
+                    # print('no decay:{}'.format(parameter_name))
+                    no_decay_params.append(x)
+                elif parameter_name.endswith('.beta'):
+                    # bn weight bias not using weight decay, be carefully for now x not include BN
+                    # print('no decay:{}'.format(parameter_name))
+                    no_decay_params.append(x)
+                else:
+                    decay_params.append(x)
+
+            return [{'params': no_decay_params, 'weight_decay': 0.0}, {'params': decay_params}]
+
+
+        if cfg.is_dynamic_loss_scale:
+            cfg.loss_scale = 1
+
+        opt = Momentum(params=get_param_groups(net),
+                       learning_rate=Tensor(lr),
+                       momentum=cfg.momentum,
+                       weight_decay=cfg.weight_decay,
+                       loss_scale=cfg.loss_scale)
+        if not cfg.use_label_smooth:
+            cfg.label_smooth_factor = 0.0
+        loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction="mean",
+                                                smooth_factor=cfg.label_smooth_factor, num_classes=cfg.num_classes)
+
+        if cfg.is_dynamic_loss_scale == 1:
+            loss_scale_manager = DynamicLossScaleManager(init_loss_scale=65536, scale_factor=2, scale_window=2000)
+        else:
+            loss_scale_manager = FixedLossScaleManager(cfg.loss_scale, drop_overflow_update=False)
+
+    if device_target == "Ascend":
+        model = Model(net, loss_fn=loss, optimizer=opt, metrics={'acc'},
+                      amp_level="O2", keep_batchnorm_fp32=False, loss_scale_manager=loss_scale_manager)
+        ckpt_save_dir = "./"
+    else:  # GPU
+        model = Model(net, loss_fn=loss, optimizer=opt, metrics={'acc'},
+                      amp_level="O2", keep_batchnorm_fp32=True, loss_scale_manager=loss_scale_manager)
+        ckpt_save_dir = "./ckpt_" + str(get_rank()) + "/"
 
     config_ck = CheckpointConfig(save_checkpoint_steps=batch_num * 5, keep_checkpoint_max=cfg.keep_checkpoint_max)
     time_cb = TimeMonitor(data_size=batch_num)
-    ckpoint_cb = ModelCheckpoint(prefix="train_googlenet_cifar10", directory="./", config=config_ck)
+    ckpoint_cb = ModelCheckpoint(prefix="train_googlenet_" + args_opt.dataset_name, directory=ckpt_save_dir,
+                                 config=config_ck)
     loss_cb = LossMonitor()
     model.train(cfg.epoch_size, dataset, callbacks=[time_cb, ckpoint_cb, loss_cb])
     print("train success")
