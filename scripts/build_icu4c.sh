4c49d557d28533375d58c53049fe56f8e9c47e62
qianlong <qianlong3@huawei.com>
2020-09-16 10:23:36 +0800

    fix icu4c compile

--- scripts/build_icu4c.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo '{
-  "strategy": "additive",
-  "featureFilters": {
-    "normalization": "include"
-  }
-}' > filter.json
-CFLAGS="-fstack-protector -Wl,-z,now -D_FORTIFY_SOURCE=2 -O2" ./icu4c/source/runConfigureICU Linux --enable-rpath --disable-tests --disable-samples --disable-icuio --disable-extras ICU_DATA_FILTER_FILE=filter.json "$@"
