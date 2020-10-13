4b98754dee8520359c50fdc321f8db9d4f369020
zhaozhenlong <zhaozhenlong1@huawei.com>
2020-10-13 19:05:33 +0800

    custom op normalize

---
+++ mindspore/lite/src/runtime/kernel/arm/string/normalize.h
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2020 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MINDSPORE_LITE_SRC_RUNTIME_KERNEL_ARM_STRING_NORMALIZE_H_
+#define MINDSPORE_LITE_SRC_RUNTIME_KERNEL_ARM_STRING_NORMALIZE_H_
+
+#include <vector>
+#include <string>
+#include "src/lite_kernel.h"
+#include "include/context.h"
+#include "src/common/string_util.h"
+
+namespace mindspore::kernel {
+class NormalizeCPUKernel : public LiteKernel {
+ public:
+  NormalizeCPUKernel(OpParameter *parameter, const std::vector<lite::Tensor *> &inputs,
+                     const std::vector<lite::Tensor *> &outputs, const lite::InnerContext *ctx,
+                     const mindspore::lite::PrimitiveC *primitive)
+      : LiteKernel(parameter, inputs, outputs, ctx, primitive) {}
+  ~NormalizeCPUKernel() = default;
+
+  int Init() override;
+  int ReSize() override;
+  int Run() override;
+
+ private:
+  std::string Trim(const std::string &str, const std::string &whitespace = " \t\n\v\f\r");
+  std::string GlobalReplace(const std::string &str, const std::string &reg, const std::string &replace);
+  std::string Normalize(const std::string &str);
+  std::vector<char *> normalized_strs;
+  void FreeBuffer();
+};
+}  // namespace mindspore::kernel
+
+#endif  // MINDSPORE_LITE_SRC_RUNTIME_KERNEL_ARM_STRING_NORMALIZE_H_
---
+++ mindspore/lite/test/ut/src/runtime/kernel/arm/string/normalize.cc
@@ -0,0 +1,87 @@
+/**
+ * Copyright 2020 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+#include "src/runtime/kernel/arm/fp32/skip_gram.h"
+#include "src/runtime/kernel/arm/string/normalize.h"
+#include "mindspore/lite/src/kernel_registry.h"
+#include "nnacl/fp32/skip_gram.h"
+#include "src/common/file_utils.h"
+#include "common/common_test.h"
+#include "src/common/log_adapter.h"
+#include "src/common/string_util.h"
+
+namespace mindspore {
+using mindspore::lite::StringPack;
+using mindspore::lite::Tensor;
+
+class TestNormalize : public mindspore::CommonTest {
+ public:
+  TestNormalize() {}
+  void NormalizeTestInit();
+
+ public:
+  Tensor input_tensor_;
+  Tensor output_tensor_;
+  std::vector<Tensor *> inputs_{&input_tensor_};
+  std::vector<Tensor *> outputs_{&output_tensor_};
+  OpParameter parameter_ = {};
+  lite::InnerContext ctx_ = lite::InnerContext();
+  kernel::KernelKey desc_ = {kernel::KERNEL_ARCH::kCPU, kNumberTypeFloat32, schema::PrimitiveType_CustomNormalize};
+  kernel::KernelCreator creator_ = nullptr;
+  kernel::LiteKernel *kernel_ = nullptr;
+};
+
+void TestNormalize::NormalizeTestInit() {
+  input_tensor_.set_data_type(kObjectTypeString);
+  input_tensor_.SetFormat(schema::Format_NHWC);
+
+  std::vector<StringPack> str_pack;
+  const char sentence1[] = "  I don't know what happened\n";
+  str_pack.push_back({static_cast<int>(strlen(sentence1) + 1), sentence1});
+  const char sentence2[] = "She's not here when Alex arrived!!!";
+  str_pack.push_back({static_cast<int>(strlen(sentence2) + 1), sentence2});
+  mindspore::lite::WriteStringsToTensor(&input_tensor_, str_pack);
+
+  output_tensor_.set_data_type(kObjectTypeString);
+  output_tensor_.SetFormat(schema::Format_NHWC);
+}
+
+TEST_F(TestNormalize, TestSentence) {
+  NormalizeTestInit();
+  ASSERT_EQ(lite::RET_OK, ctx_.Init());
+  creator_ = lite::KernelRegistry::GetInstance()->GetCreator(desc_);
+  ASSERT_NE(creator_, nullptr);
+  kernel_ = creator_(inputs_, outputs_, &parameter_, &ctx_, desc_, nullptr);
+  ASSERT_NE(kernel_, nullptr);
+  auto ret = kernel_->Init();
+  MS_ASSERT(ret == 0);
+  ret = kernel_->Run();
+  MS_ASSERT(ret == 0);
+
+  std::vector<StringPack> output = mindspore::lite::ParseTensorBuffer(outputs_[0]);
+  for (int i = 0; i < output.size(); i++) {
+    for (int j = 0; j < output[i].len; j++) {
+      printf("%c", output[i].data[j]);
+    }
+    printf("\n");
+  }
+
+  input_tensor_.SetData(nullptr);
+  output_tensor_.SetData(nullptr);
+}
+
+}  // namespace mindspore
