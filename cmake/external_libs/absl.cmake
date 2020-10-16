e7fefb48170e05a96d662548a275f65710eebdd1
lilei <lilei120@huawei.com>
2020-10-16 17:10:48 +0800

    modify_cmake

--- cmake/external_libs/absl.cmake
+++ cmake/external_libs/absl.cmake
@@ -1,8 +1,16 @@
+if (ENABLE_GITEE)
+    set(REQ_URL "https://gitee.com/mirrors/abseil-cpp/repository/archive/20200225.2.tar.gz")
+    set(MD5 "7e84ac40ee4541f645f5b9c90c9c98e6")
+else()
+    set(REQ_URL "https://github.com/abseil/abseil-cpp/archive/20200225.2.tar.gz")
+    set(MD5 "73f2b6e72f1599a9139170c29482ddc4")
+endif ()
+
 mindspore_add_pkg(absl
         VER 20200225.2
         LIBS absl_strings absl_throw_delegate absl_raw_logging_internal absl_int128 absl_bad_optional_access
-        URL https://github.com/abseil/abseil-cpp/archive/20200225.2.tar.gz
-        MD5 73f2b6e72f1599a9139170c29482ddc4
+        URL ${REQ_URL}
+        MD5 ${MD5}
         CMAKE_OPTION -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=TRUE)
 
 include_directories(${absl_INC})
--- cmake/external_libs/zlib.cmake
+++ cmake/external_libs/zlib.cmake
@@ -1,8 +1,16 @@
+if (ENABLE_GITEE)
+    set(REQ_URL "https://gitee.com/mirrors/zlib/repository/archive/v1.2.11.tar.gz")
+    set(MD5 "be6d144068d8835e86a81b3f36b66a42")
+else()
+    set(REQ_URL "https://github.com/madler/zlib/archive/v1.2.11.tar.gz")
+    set(MD5 "0095d2d2d1f3442ce1318336637b695f")
+endif ()
+
 mindspore_add_pkg(zlib
         VER 1.2.11
         LIBS z
-        URL https://github.com/madler/zlib/archive/v1.2.11.tar.gz
-        MD5 0095d2d2d1f3442ce1318336637b695f
+        URL ${REQ_URL}
+        MD5 ${MD5}
         CMAKE_OPTION -DCMAKE_BUILD_TYPE:STRING=Release)
 
 include_directories(${zlib_INC})
