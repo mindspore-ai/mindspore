9c1a0c796bf082550bcb252eb015604a466a356a
xulei2020 <xulei83@huawei.com>
2020-10-20 17:20:25 +0800

    add gitee repo for third_party download

--- cmake/external_libs/libtiff.cmake
+++ cmake/external_libs/libtiff.cmake
@@ -15,11 +15,19 @@ endif()
 
 set(tiff_LDFLAGS "-Wl,-z,relro,-z,now,-z,noexecstack")
 
+if (ENABLE_GITEE)
+    set(REQ_URL "https://gitee.com/mirrors/libtiff/repository/archive/v4.1.0.tar.gz")
+    set(MD5 "76b951159c8bdf47dba4803659c232d1")
+else()
+    set(REQ_URL "https://gitlab.com/libtiff/libtiff/-/archive/v4.1.0/libtiff-v4.1.0.tar.gz")
+    set(MD5 "21de8d35c1b21ac82663fa9f56d3350d")
+endif ()
+
 mindspore_add_pkg(tiff
         VER 4.1.0
         LIBS tiff
-        URL https://gitlab.com/libtiff/libtiff/-/archive/v4.1.0/libtiff-v4.1.0.tar.gz
-        MD5 21de8d35c1b21ac82663fa9f56d3350d
+        URL ${REQ_URL}
+        MD5 ${MD5}
         CMAKE_OPTION -DCMAKE_BUILD_TYPE=Release -Djbig=OFF -Dlzma=OFF -Djpeg12=OFF -Dzstd=OFF -Dpixarlog=OFF
         -Dold-jpeg=OFF -Dwebp=OFF -DBUILD_SHARED_LIBS=OFF)
 message("tiff include = ${tiff_INC}")
