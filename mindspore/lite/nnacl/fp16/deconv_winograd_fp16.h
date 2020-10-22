d8b928b7f8dd911f1a4a7faa976a8aa7a7f2df78
ling <lingqiaomin.huawei.com>
2020-10-22 20:20:27 +0800

    [MSLITE] deconv winograd fp16 neon

--- mindspore/lite/nnacl/fp16/deconv_winograd_fp16.h
+++ mindspore/lite/nnacl/fp16/deconv_winograd_fp16.h
@@ -32,6 +32,15 @@ void DeconvWgFp16(float16_t *nhwc_input_, float16_t *tile_in, float16_t *tile_ou
 void DeconvWgPostFp16(float16_t *tile_out, float16_t *nc4hw4_output, ConvParameter *conv_param,
                       DeConvParam *deconv_param, int calculate_count, int tile_index);
 
+void TiledC4MatmulFp16(float16_t *dst, const float16_t *src, const float16_t *weight, size_t ic4, size_t cal_num,
+                       size_t oc4);
+
+void WinogradTransLeftFp16(const float16_t *S, const float16_t *B, float16_t *M, size_t w, size_t h, size_t k,
+                           size_t length);
+
+void WinogradTransRightFp16(const float16_t *S, const float16_t *B, float16_t *M, size_t w, size_t h, size_t k,
+                            size_t length);
+
 #ifdef __cplusplus
 }
 #endif
