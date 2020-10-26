b8a72e94408d3aa993f1098908931be41e2b3a14
yangruoqi713 <yangruoqi@huawei.com>
2020-10-26 16:05:21 +0800

    [MSLITE][Develop] optimize arm cpu int8 op deconv dw, add post assembly

--- mindspore/lite/nnacl/int8/common_func_int8.h
+++ mindspore/lite/nnacl/int8/common_func_int8.h
@@ -47,6 +47,9 @@ void ConvDwInt8Center(int8_t *dst, const int8_t *src, const int16_t *weight, con
                       size_t in_sh_step, size_t in_sw_step, size_t in_kh_step, size_t in_kw_step, int8_t *in_zp,
                       int32_t *out_zp, int32_t *out_multiplier, int32_t *left_shift, int32_t *right_shift,
                       int32_t *acc_min, int32_t *acc_max);
+void DeconvDwInt8Post(int8_t *dst, int32_t *output_buffer, const int32_t *bias, int block_channel, int pixel_nums,
+                      int out_multiplier, int left_shift, int right_shift, int32_t out_zp, int32_t acc_min,
+                      int32_t acc_max);
 #endif
 
 #ifdef ENABLE_ARM32
@@ -54,6 +57,9 @@ void IndirectGemmInt8_2x4(int8_t *output, const int8_t *input, const int8_t *wei
                           size_t ic4, size_t oc, size_t offset, const int32_t *input_sum, size_t act_min,
                           size_t act_max, size_t out_zp, int32_t *out_multiplier, int32_t *shift_before,
                           int32_t *shift_after, size_t asymmetric, size_t per_channel, size_t per_channel_offset);
+void ConvDw3x3Int8BorderPixel(int8_t *dst, const int8_t *src, const int16_t *weight, const int32_t *bias, int height,
+                              int width, int in_kh_step, int in_kw_step, int channel, int8_t in_zp, int32_t out_zp,
+                              int out_multiplier, int left_shift, int right_shift, int32_t acc_min, int32_t acc_max);
 #endif
 
 #ifdef ENABLE_ARM64
