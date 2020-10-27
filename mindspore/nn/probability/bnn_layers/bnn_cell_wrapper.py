11aa3f6f5f36b87074a50648bb62d11cb50c9acb
bingyaweng <wengbingya@huawei.com>
2020-10-27 20:06:39 +0800

    modify the method for calculating kl loss

--- mindspore/nn/probability/bnn_layers/bnn_cell_wrapper.py
+++ mindspore/nn/probability/bnn_layers/bnn_cell_wrapper.py
@@ -15,29 +15,12 @@
 """Generate WithLossCell suitable for BNN."""
 from .conv_variational import _ConvVariational
 from .dense_variational import _DenseVariational
-from ..transforms.bnn_loss.generate_kl_loss import gain_bnn_with_loss
+from ...cell import Cell
 
 __all__ = ['WithBNNLossCell']
 
 
-class ClassWrap:
-    """Decorator of WithBNNLossCell"""
-    def __init__(self, cls):
-        self._cls = cls
-        self.bnn_loss_file = None
-        self.__doc__ = cls.__doc__
-        self.__name__ = cls.__name__
-        self.__bases__ = cls.__bases__
-
-    def __call__(self, backbone, loss_fn, dnn_factor, bnn_factor):
-        obj = self._cls(backbone, loss_fn, dnn_factor, bnn_factor)
-        bnn_with_loss = obj()
-        self.bnn_loss_file = obj.bnn_loss_file
-        return bnn_with_loss
-
-
-@ClassWrap
-class WithBNNLossCell:
+class WithBNNLossCell(Cell):
     r"""
     Generate a suitable WithLossCell for BNN to wrap the bayesian network with loss function.
 
@@ -68,6 +51,7 @@ class WithBNNLossCell:
     """
 
     def __init__(self, backbone, loss_fn, dnn_factor=1, bnn_factor=1):
+        super(WithBNNLossCell, self).__init__(auto_prefix=False)
         if isinstance(dnn_factor, bool) or not isinstance(dnn_factor, (int, float)):
             raise TypeError('The type of `dnn_factor` should be `int` or `float`')
         if dnn_factor < 0:
@@ -78,28 +62,36 @@ class WithBNNLossCell:
         if bnn_factor < 0:
             raise ValueError('The value of `bnn_factor` should >= 0')
 
-        self.backbone = backbone
-        self.loss_fn = loss_fn
+        self._backbone = backbone
+        self._loss_fn = loss_fn
         self.dnn_factor = dnn_factor
         self.bnn_factor = bnn_factor
-        self.bnn_loss_file = None
+        self.kl_loss = []
+        self._add_kl_loss(self._backbone)
 
-    def _generate_loss_cell(self):
-        """Generate WithBNNLossCell by ast."""
-        layer_count = self._kl_loss_count(self.backbone)
-        bnn_with_loss, self.bnn_loss_file = gain_bnn_with_loss(layer_count, self.backbone, self.loss_fn,
-                                                               self.dnn_factor, self.bnn_factor)
-        return bnn_with_loss
+    def construct(self, x, label):
+        y_pred = self._backbone(x)
+        backbone_loss = self._loss_fn(y_pred, label)
+        kl_loss = 0
+        for i in range(len(self.kl_loss)):
+            kl_loss += self.kl_loss[i]()
+        loss = backbone_loss * self.dnn_factor + kl_loss * self.bnn_factor
+        return loss
 
-    def _kl_loss_count(self, net):
-        """ Calculate the number of Bayesian layers."""
-        count = 0
+    def _add_kl_loss(self, net):
+        """Collect kl loss of each Bayesian layer."""
         for (_, layer) in net.name_cells().items():
             if isinstance(layer, (_DenseVariational, _ConvVariational)):
-                count += 1
+                self.kl_loss.append(layer.compute_kl_loss)
             else:
-                count += self._kl_loss_count(layer)
-        return count
+                self._add_kl_loss(layer)
 
-    def __call__(self):
-        return self._generate_loss_cell()
+    @property
+    def backbone_network(self):
+        """
+        Returns the backbone network.
+
+        Returns:
+            Cell, the backbone network.
+        """
+        return self._backbone
