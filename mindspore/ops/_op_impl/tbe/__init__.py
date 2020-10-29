2a284a0575e948b4ed9b4890dac6469042a0f9ba
hwjiaorui <jiaorui3@huawei.com>
2020-10-29 22:08:46 +0800

    register gatherv2

--- mindspore/ops/_op_impl/tbe/__init__.py
+++ mindspore/ops/_op_impl/tbe/__init__.py
@@ -296,6 +296,7 @@ from .fused_mul_add_n_l2loss import _fused_mul_add_n_l2loss_tbe
 from .fused_mul_apply_momentum_extern import _fused_mul_apply_momentum_extern_tbe
 from .lamb_next_right import _lamb_next_right_tbe
 from .sparse_gather_v2 import _sparse_gather_v2_tbe
+from .sparse_gather_v2_ds import _sparse_gather_v2_ds_tbe
 from .data_format_dim_map import _data_format_dim_map_tbe
 from .histogram_fixed_width import _histogram_fixed_width_tbe
 from .tensor_scatter_update import _tensor_scatter_update_tbe
