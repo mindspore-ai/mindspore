ceaca1708c104cfeb0dce618f6de9f9ff61ee3d4
yuzhenhua <yuzhenhua.yuzhenhua@huawei.com>
2020-12-01 11:09:57 +0800

    add export file for resnet50_quant and densenet121, fix centerface and transformer bug

---
+++ model_zoo/official/cv/densenet121/export.py
@@ -0,0 +1,57 @@
+# Copyright 2020 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+import argparse
+import numpy as np
+
+from mindspore.common import dtype as mstype
+from mindspore import context, Tensor
+from mindspore.train.serialization import export, load_checkpoint, load_param_into_net
+
+from src.network import DenseNet121
+from src.config import config
+
+parser = argparse.ArgumentParser(description="densenet121 export")
+parser.add_argument("--device_id", type=int, default=0, help="Device id")
+parser.add_argument("--batch_size", type=int, default=32, help="batch size")
+parser.add_argument("--ckpt_file", type=str, required=True, help="Checkpoint file path.")
+parser.add_argument("--file_name", type=str, default="densenet121", help="output file name.")
+parser.add_argument("--file_format", type=str, choices=["AIR", "ONNX", "MINDIR"], default="AIR", help="file format")
+args = parser.parse_args()
+
+context.set_context(mode=context.GRAPH_MODE, device_target="Ascend", device_id=args.device_id)
+
+if __name__ == "__main__":
+    network = DenseNet121(config.num_classes)
+
+    param_dict = load_checkpoint(args.ckpt_file)
+
+    param_dict_new = {}
+    for key, value in param_dict.items():
+        if key.startswith("moments."):
+            continue
+        elif key.startswith("network."):
+            param_dict_new[key[8:]] = value
+        else:
+            param_dict_new[key] = value
+
+    load_param_into_net(network, param_dict_new)
+
+    network.add_flags_recursive(fp16=True)
+    network.set_train(False)
+
+    shape = [int(args.batch_size), 3] + [int(config.image_size.split(",")[0]), int(config.image_size.split(",")[1])]
+    input_data = Tensor(np.zeros(shape), mstype.float32)
+
+    export(network, input_data, file_name=args.file_name, file_format=args.file_format)
