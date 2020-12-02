73a7c31d418e556fac7b881fc8b2e8247ed361b5
peixu_ren <peixu.ren1@huawei.com>
2020-12-02 10:35:33 -0500

    Delete redundant phrase in LGamma doc

--- mindspore/nn/layer/math.py
+++ mindspore/nn/layer/math.py
@@ -236,9 +236,6 @@ class LGamma(Cell):
     Outputs:
         Tensor, has the same shape and dtype as the `x`.
 
-    Supported Platforms:
-        ``Ascend``
-
     Examples:
         >>> input_x = Tensor(np.array([2, 3, 4]).astype(np.float32))
         >>> op = nn.LGamma()
