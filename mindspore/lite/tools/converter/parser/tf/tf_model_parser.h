f00ecc3820bb80caab8fcc2a74cddbb83db0e7bc
zhengjun10 <zhengjun10@huawei.com>
2020-12-04 12:22:26 +0800

    add tf subgraph parser and rm unused code

--- mindspore/lite/tools/converter/parser/tf/tf_model_parser.h
+++ mindspore/lite/tools/converter/parser/tf/tf_model_parser.h
@@ -17,17 +17,17 @@
 #ifndef MINDSPORE_LITE_TOOLS_CONVERTER_PARSER_TF_MODEL_PARSER_H
 #define MINDSPORE_LITE_TOOLS_CONVERTER_PARSER_TF_MODEL_PARSER_H
 
-#include <string>
-#include <vector>
-#include <memory>
 #include <map>
+#include <memory>
+#include <string>
 #include <unordered_map>
+#include <vector>
+#include "proto/graph.pb.h"
+#include "proto/node_def.pb.h"
+#include "schema/inner/model_generated.h"
 #include "securec/include/securec.h"
 #include "tools/common/tensor_util.h"
 #include "tools/converter/model_parser.h"
-#include "schema/inner/model_generated.h"
-#include "proto/node_def.pb.h"
-#include "proto/graph.pb.h"
 
 namespace mindspore {
 namespace lite {
@@ -43,24 +43,39 @@ class TFModelParser : public ModelParser {
                                 const QuantType &quantType = QuantType_QUANT_NONE) override;
 
  private:
-  AnfNodePtr GetAnfNode(const std::string &name);
-  std::string GetOriginInputName(const tensorflow::NodeDef &node);
   STATUS ConvertConstTensor(const tensorflow::AttrValue &attr_value, const TypeId &type, const ParameterPtr &parameter,
                             std::vector<int64_t> *shape_vector);
-  STATUS ConvertParameter(const tensorflow::NodeDef &node, const ParameterPtr &parameter);
-  STATUS ConvertGraphInputsAndConsts();
+  STATUS ConvertParameter(const tensorflow::NodeDef &node, const ParameterPtr &parameter,
+                          std::unordered_map<std::string, AnfNodePtr> *anf_node_map);
+  STATUS ConvertGraphInputsAndConsts(const std::map<std::string, const tensorflow::NodeDef *> &tf_graph_nodes,
+                                     const FuncGraphPtr &anf_graph,
+                                     std::unordered_map<std::string, AnfNodePtr> *anf_node_map);
   STATUS ConvertInputNodes(const tensorflow::NodeDef &node_def, const std::vector<std::string> &input_names,
+                           const std::map<std::string, const tensorflow::NodeDef *> &tf_node_map,
+                           const std::unordered_map<std::string, AnfNodePtr> &anf_node_map,
                            std::vector<AnfNodePtr> *inputs);
-  STATUS ConvertOutputTensor(const tensorflow::NodeDef &op, const CNodePtr &anf_node, int output_size);
-  STATUS ConvertOps();
-  STATUS ConvertGraphOutputs();
+  STATUS ConvertOutputTensor(const tensorflow::NodeDef &op, const CNodePtr &anf_node,
+                             std::unordered_map<std::string, AnfNodePtr> *anf_node_map, const FuncGraphPtr &anf_graph,
+                             int output_size);
+  STATUS ConvertOps(const tensorflow::NodeDef &node_def,
+                    const std::map<std::string, const tensorflow::NodeDef *> &tf_node_map,
+                    const FuncGraphPtr &func_graph_ptr, std::unordered_map<std::string, AnfNodePtr> *anf_node_map);
+  STATUS ConvertRootGraphOutputs();
 
-  FuncGraphPtr funcGraphPtr;
-  std::unique_ptr<tensorflow::GraphDef> tf_graph_def;
-  std::map<std::string, const tensorflow::NodeDef *> tf_node_map;
-  std::unordered_map<std::string, AnfNodePtr> anf_node_map;
-  std::vector<std::string> graph_input_names;
-  std::vector<std::string> graph_output_names;
+  STATUS ConvertSubgraph();
+
+  STATUS WhileNodePostProcess(const std::map<CNodePtr, FuncGraphPtr> &while_cond_map,
+                              const std::map<CNodePtr, FuncGraphPtr> &while_body_map);
+
+  STATUS MakeAnfGraphOutputs(std::vector<AnfNodePtr> *output_nodes, const FuncGraphPtr &anf_graph);
+
+  FuncGraphPtr anf_root_graph_;
+  std::unique_ptr<tensorflow::GraphDef> tf_root_graph_;                     // tf root graph def
+  std::map<std::string, const tensorflow::NodeDef *> tf_root_graph_nodes_;  // tf root graph node map
+  std::unordered_map<std::string, AnfNodePtr> anf_root_node_map_;
+  std::vector<std::string> graph_input_names_;
+  std::vector<std::string> graph_output_names_;
+  std::map<std::string, AnfNodePtr> function_while_map_;  // tf function name->while_node_name
 };
 }  // namespace lite
 }  // namespace mindspore
