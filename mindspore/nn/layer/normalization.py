0f75370377a02d5b559b8ec448215a52b8226f50
wangshuide2020 <7511764+wangshuide2020@user.noreply.gitee.com>
2020-12-31 10:38:45 +0800

    update the documentation of MaxPoolWithArgmax, BroadcastTo, GlobalBatchNorm and DistributedGradReducer operators.

--- mindspore/nn/layer/normalization.py
+++ mindspore/nn/layer/normalization.py
@@ -575,7 +575,7 @@ class GlobalBatchNorm(_BatchNorm):
         >>>
         >>> device_id = int(os.environ["DEVICE_ID"])
         >>> context.set_context(mode=context.GRAPH_MODE, device_target="Ascend", save_graphs=True,
-        >>>                     device_id=int(device_id))
+        ...                     device_id=int(device_id))
         >>> init()
         >>> context.reset_auto_parallel_context()
         >>> context.set_auto_parallel_context(parallel_mode=ParallelMode.DATA_PARALLEL)
