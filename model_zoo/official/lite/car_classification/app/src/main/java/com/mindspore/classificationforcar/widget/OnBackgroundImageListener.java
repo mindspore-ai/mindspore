cbb5e2f4cec4b556f6158a293ed5bb343dff3ef7
hukang hwx963878 <hukang22@huawei.com>
2021-01-07 16:29:48 +0800

    car classification init

---
+++ model_zoo/official/lite/car_classification/app/src/main/java/com/mindspore/classificationforcar/widget/OnBackgroundImageListener.java
@@ -0,0 +1,9 @@
+package com.mindspore.classificationforcar.widget;
+
+import android.view.View;
+
+public interface OnBackgroundImageListener {
+    void onBackImageSelected(int position);
+
+//    void onImageAdd(View view);
+}
