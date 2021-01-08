60ac42adcae24c8b37ab4153546b946362d91689
zhangyi <zhangyi267@huawei.com>
2021-01-08 12:02:30 +0800

    fix api comments.

--- mindspore/nn/layer/conv.py
+++ mindspore/nn/layer/conv.py
@@ -191,10 +191,10 @@ class Conv2d(_Conv):
 
     Inputs:
         - **input** (Tensor) - Tensor of shape :math:`(N, C_{in}, H_{in}, W_{in})` \
-          or `(N, H_{in}, W_{in}, C_{in})`.
+          or :math:`(N, H_{in}, W_{in}, C_{in})`.
 
     Outputs:
-        Tensor of shape :math:`(N, C_{out}, H_{out}, W_{out})` or `(N, H_{out}, W_{out}, C_{out})`.
+        Tensor of shape :math:`(N, C_{out}, H_{out}, W_{out})` or :math:`(N, H_{out}, W_{out}, C_{out})`.
 
     Supported Platforms:
         ``Ascend`` ``GPU`` ``CPU``
