9d9d2e1ff41f4240d3df83e3688f7de847682f8b
yangruoqi713 <yangruoqi@huawei.com>
2021-01-18 14:54:54 +0800

    [MSLITE][DEVELOP] add cpu fp32 op: crop_and_resize

--- mindspore/lite/nnacl/resize_parameter.h
+++ mindspore/lite/nnacl/resize_parameter.h
@@ -26,4 +26,11 @@ typedef struct ResizeParameter {
   int coordinate_transform_mode_;
   bool preserve_aspect_ratio_;
 } ResizeParameter;
+
+typedef struct CropAndResizeParameter {
+  // primitive parameter
+  OpParameter op_parameter_;
+  int method_;
+  float extrapolation_value_;
+} CropAndResizeParameter;
 #endif  // MINDSPORE_LITE_NNACL_RESIZE_PARAMETER_H_
--- mindspore/lite/src/runtime/kernel/arm/fp32/crop_and_resize_fp32.h
+++ mindspore/lite/src/runtime/kernel/arm/fp32/crop_and_resize_fp32.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Huawei Technologies Co., Ltd
+ * Copyright 2021 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,34 +13,40 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef MINDSPORE_LITE_SRC_RUNTIME_KERNEL_ARM_FP32_RESIZE_H_
-#define MINDSPORE_LITE_SRC_RUNTIME_KERNEL_ARM_FP32_RESIZE_H_
+#ifndef MINDSPORE_LITE_SRC_RUNTIME_KERNEL_ARM_FP32_CROP_AND_RESIZE_H_
+#define MINDSPORE_LITE_SRC_RUNTIME_KERNEL_ARM_FP32_CROP_AND_RESIZE_H_
 
 #include <vector>
-#include <algorithm>
 #include "include/errorcode.h"
-#include "nnacl/fp32/resize_fp32.h"
+#include "nnacl/resize_parameter.h"
 #include "src/lite_kernel.h"
-#include "src/runtime/kernel/arm/base/resize_base.h"
 
 namespace mindspore::kernel {
-class ResizeCPUKernel : public ResizeBaseCPUKernel {
+class CropAndResizeCPUKernel : public LiteKernel {
  public:
-  ResizeCPUKernel(OpParameter *parameter, const std::vector<lite::Tensor *> &inputs,
-                  const std::vector<lite::Tensor *> &outputs, const lite::InnerContext *ctx,
-                  const mindspore::lite::PrimitiveC *primitive)
-      : ResizeBaseCPUKernel(parameter, inputs, outputs, ctx, primitive) {}
+  CropAndResizeCPUKernel(OpParameter *parameter, const std::vector<lite::Tensor *> &inputs,
+                         const std::vector<lite::Tensor *> &outputs, const lite::InnerContext *ctx,
+                         const mindspore::lite::PrimitiveC *primitive)
+      : LiteKernel(parameter, inputs, outputs, ctx, primitive) {
+    param_ = reinterpret_cast<CropAndResizeParameter *>(op_parameter_);
+  }
 
-  ~ResizeCPUKernel() { FreeTmpBuffer(); }
+  ~CropAndResizeCPUKernel() = default;
 
   int Init() override;
   int ReSize() override;
   int Run() override;
-  virtual int RunImpl(int task_id);
+  int RunImpl(int task_id);
+
+ protected:
   int MallocTmpBuffer();
   void FreeTmpBuffer();
 
- protected:
+  CropAndResizeParameter *param_;
+  int batch_;
+  int new_height_;
+  int new_width_;
+  bool is_crop_ = true;
   int *y_tops_ = nullptr;
   int *y_bottoms_ = nullptr;
   int *x_lefts_ = nullptr;
@@ -48,8 +54,7 @@ class ResizeCPUKernel : public ResizeBaseCPUKernel {
   float *y_bottom_weights_ = nullptr;
   float *x_left_weights_ = nullptr;
   float *line_buffer_ = nullptr;
-  CalculateOriginalCoordinate calculate_ = nullptr;
 };
 }  // namespace mindspore::kernel
 
-#endif  // MINDSPORE_LITE_SRC_RUNTIME_KERNEL_ARM_FP32_RESIZE_H_
+#endif  // MINDSPORE_LITE_SRC_RUNTIME_KERNEL_ARM_FP32_CROP_AND_RESIZE_H_
