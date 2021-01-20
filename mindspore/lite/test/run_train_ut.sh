0b49683fa9d3e11f0f1d714593d5655c96fceb7a
zhanghaibo5 <zhanghaibo5@huawei.com>
2021-01-20 16:44:14 +0800

    remove useless script of mindspore lite

--- mindspore/lite/test/run_train_ut.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ./ut/src/runtime/kernel/arm || exit 1
-../../../../../../build/test/lite-test --gtest_filter=NetworkTest.efficient_net
-# ../../../../../../build/test/lite-test --gtest_filter=NetworkTest.tuning_layer
-# ../../../../../../build/test/lite-test --gtest_filter=NetworkTest.lenetnet
