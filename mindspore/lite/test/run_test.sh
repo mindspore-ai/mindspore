ad2757946855353020ff1f5ac7a01ff28c961926
zhanghaibo5 <zhanghaibo5@huawei.com>
2021-01-20 14:25:08 +0800

    update run scription of ut

--- mindspore/lite/test/run_test.sh
+++ mindspore/lite/test/run_test.sh
@@ -3,33 +3,43 @@
 set -e
 CUR_DIR=$(cd "$(dirname $0)"; pwd)
 BUILD_DIR=${CUR_DIR}/../build
+
+# prepare run directory for ut
 mkdir -pv ${CUR_DIR}/do_test
+
+# prepare data for ut
 cd ${CUR_DIR}/do_test
 cp ${BUILD_DIR}/test/lite-test ./
 cp -r ${CUR_DIR}/ut/src/runtime/kernel/arm/test_data/* ./
 cp -r ${CUR_DIR}/ut/tools/converter/parser/tflite/test_data/* ./
-## prepare data for dataset
+# prepare data for dataset
 TEST_DATA_DIR=${CUR_DIR}/../../../tests/ut/data/dataset/
 cp -fr $TEST_DATA_DIR/testPK ./data
 
+# test cases of MindData
 ./lite-test --gtest_filter="*MindDataTestTensorDE*"
 ./lite-test --gtest_filter="*MindDataTestEager*"
 
-./lite-test --gtest_filter="TestTfliteParser*"
+# test cases of Converter
+## ./lite-test --gtest_filter="TestTfliteParser*"
 
-./lite-test --gtest_filter="*TestHebing*"
+# test cases with benchmark
+## ./lite-test --gtest_filter="*TestHebing*"
 
+# test cases of framework
+./lite-test --gtest_filter="ControlFlowTest.TestMergeWhileModel"
+
+# test cases of FP32 OP
 ./lite-test --gtest_filter=TestFcFp32*
 ./lite-test --gtest_filter=TestConv1x1Fp32*
 ./lite-test --gtest_filter=TestStrassenFp32*
-./lite-test --gtest_filter=TestDeConvolutionFp32*
+## ./lite-test --gtest_filter=TestDeConvolutionFp32*
 
-./lite-test --gtest_filter=TestPadInt8.*
-./lite-test --gtest_filter=TestDeconvInt8.*
+# test cases of INT8 OP
+## ./lite-test --gtest_filter=TestPadInt8.*
+## ./lite-test --gtest_filter=TestDeconvInt8.*
 
-./lite-test --gtest_filter="TestTfliteParser*"
-
-# for GPU OpenCL
+# test cases of GPU OpenCL
 ./lite-test --gtest_filter="TestConvolutionOpenCL.simple_test*"
 ./lite-test --gtest_filter="TestArithmeticSelfOpenCLCI.ArithmeticSelfRound*"
 ./lite-test --gtest_filter="TestConcatOpenCLCI.ConcatFp32_2inputforCI*"
@@ -48,3 +58,9 @@ cp -fr $TEST_DATA_DIR/testPK ./data
 ./lite-test --gtest_filter="TestFullConnectionOpenCL*"
 ./lite-test --gtest_filter="TestResizeOpenCL*"
 ./lite-test --gtest_filter="TestSwishOpenCLCI.Fp32CI"
+
+# test cases of train
+./lite-test --gtest_filter="NetworkTest.efficient_net"
+## ./lite-test --gtest_filter="NetworkTest.tuning_layer"
+## ./lite-test --gtest_filter="NetworkTest.lenetnet"
+
