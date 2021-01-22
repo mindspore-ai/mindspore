1446d4c0e2dc59c045561288c21bd08821aefb55
lihongkang <[lihongkang1@huawei.com]>
2021-01-22 16:50:48 +0800

    fix bugs

--- mindspore/nn/layer/lstm.py
+++ mindspore/nn/layer/lstm.py
@@ -93,7 +93,8 @@ class LSTM(Cell):
         bidirectional (bool): Specifies whether it is a bidirectional LSTM. Default: False.
 
     Inputs:
-        - **input** (Tensor) - Tensor of shape (seq_len, batch_size, `input_size`).
+        - **input** (Tensor) - Tensor of shape (seq_len, batch_size, `input_size`) or
+          (batch_size, seq_len, `input_size`).
         - **hx** (tuple) - A tuple of two Tensors (h_0, c_0) both of data type mindspore.float32 or
           mindspore.float16 and shape (num_directions * `num_layers`, batch_size, `hidden_size`).
           Data type of `hx` must be the same as `input`.
