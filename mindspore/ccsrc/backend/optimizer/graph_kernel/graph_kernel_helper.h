5a4ae71257d78da8f9a1aa7c3d81205f1eb534d8
tronzhang <6517937+tronzhang@user.noreply.gitee.com>
2021-02-09 15:21:37 +0800

    exclude special node when expand or basic fusion

--- mindspore/ccsrc/backend/optimizer/graph_kernel/graph_kernel_helper.h
+++ mindspore/ccsrc/backend/optimizer/graph_kernel/graph_kernel_helper.h
@@ -91,6 +91,7 @@ std::vector<int64_t> GetReduceAxis(const AnfNodePtr &node);
 
 CNodePtr CreateCNode(const std::vector<AnfNodePtr> &inputs, const FuncGraphPtr &func_graph, const DataInfo &out_info);
 void MakeCNodeSafeForAttr(const AnfNodePtr &node);
+bool IsKeepBasicNode(const AnfNodePtr &node);
 
 template <typename T>
 ValueNodePtr CreateScalarTensorValueNode(const DataInfo &info, T value, size_t data_length) {
