afa3de46b20465e53d04979d2ebd457459919cca
yangruoqi713 <yangruoqi@huawei.com>
2021-03-16 21:24:58 +0800

    [MSLITE][Develop] optimize cpu fp32 op: lstm

--- mindspore/lite/nnacl/fp32/lstm_fp32.h
+++ mindspore/lite/nnacl/fp32/lstm_fp32.h
@@ -23,6 +23,8 @@ extern "C" {
 #endif
 void PackLstmWeight(float *dst, const float *src, int batch, int deep, int col, int col_align);
 
+void PackLstmBias(float *dst, const float *src, int batch, int col, int col_align, bool is_bidirectional);
+
 void PackLstmInput(const float *src, float *dst, int row, int deep);
 
 void LstmMatMul(float *c, const float *a, const float *b, const float *bias, int row, int deep, int col, bool is_vec);
@@ -31,8 +33,8 @@ void ElementMulAcc(const float *input0, const float *input1, float *output, int 
 
 int ElementOptMulAcc(const float *input0, const float input1, float *output, const int element_size);
 
-void Lstm(float *output, const float *input, const float *weight_i, const float *weight_h, const float *bias,
-          float *hidden_state, float *cell_state, float *gate_buffer, float *state_buffer[2], float *matmul_buffer[2],
+void Lstm(float *output, const float *input, const float *weight_i, const float *weight_h, const float *input_bias,
+          const float *state_bias, float *hidden_state, float *cell_state, float *state_buffer[2], float *buffer[4],
           const LstmParameter *lstm_param);
 #ifdef __cplusplus
 }
