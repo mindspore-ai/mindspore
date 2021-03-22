b93259568ce4508f73dc584e035b72fab5fcc847
zhujingxuan <zhujingxuan@huawei.com>
2021-03-22 16:42:56 +0800

    add script to full process

--- mindspore/lite/micro/example/mnist_x86/mnist_codegen.sh
+++ mindspore/lite/micro/example/mnist_x86/mnist_codegen.sh
@@ -34,20 +34,32 @@ MINDSPORE_FILE_NAME="mindspore-lite-${VERSION_STR}-inference-linux-x64"
 MINDSPORE_FILE="${MINDSPORE_FILE_NAME}.tar.gz"
 MINDSPORE_LITE_DOWNLOAD_URL="https://ms-release.obs.cn-north-4.myhuaweicloud.com/${VERSION_STR}/MindSpore/lite/release/linux/${MINDSPORE_FILE}"
 
+MNIST_NAME=mnist
+MNIST_FILE=${MNIST_NAME}.ms
+MNIST_DOWNLOAD_URL=https://download.mindspore.cn/model_zoo/official/lite/mnist_lite/${MNIST_FILE}
+
 mkdir -p build
 
 if [ ! -e ${BASEPATH}/build/${MINDSPORE_FILE} ]; then
   wget -c -O ${BASEPATH}/build/${MINDSPORE_FILE} --no-check-certificate ${MINDSPORE_LITE_DOWNLOAD_URL}
 fi
 
+if [ ! -e ${BASEPATH}/build/${MNIST_FILE} ]; then
+  wget -c -O ${BASEPATH}/build/${MNIST_FILE} --no-check-certificate ${MNIST_DOWNLOAD_URL}
+fi
+
 tar xzvf ${BASEPATH}/build/${MINDSPORE_FILE} -C ${BASEPATH}/build/ || exit 1
 rm ${BASEPATH}/build/${MINDSPORE_FILE} || exit 1
 PKG_PATH=${BASEPATH}/build/${MINDSPORE_FILE_NAME}
-# build benchmark
+
+# 1. codegen
+${BASEPATH}/build/${MINDSPORE_FILE_NAME}/tools/codegen/codegen --codePath=${BASEPATH}/build --modelPath=${BASEPATH}/build/${MNIST_FILE}
+
+# 2. build benchmark
 mkdir -p ${BASEPATH}/build/benchmark && cd ${BASEPATH}/build/benchmark || exit 1
-cmake -DPKG_PATH=${PKG_PATH} ${BASEPATH}
+cmake -DPKG_PATH=${PKG_PATH} ${BASEPATH}/build/${MNIST_NAME}
 make
 
-echo "net file: ${BASEPATH}/src/mnist.bin"
 # 3. run benchmark
+echo "net file: ${BASEPATH}/src/mnist.bin"
 ./benchmark ${INPUT_BIN} ${BASEPATH}/src/net.bin
