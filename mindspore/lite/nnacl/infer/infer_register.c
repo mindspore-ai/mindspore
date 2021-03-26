b9cc44ef16d421d09dcae0d5ea7cef2df593efc8
xuanyue <xuanyue@huawei.com>
2021-03-26 14:21:03 +0800

    add destructor for infer

--- mindspore/lite/nnacl/infer/infer_register.c
+++ mindspore/lite/nnacl/infer/infer_register.c
@@ -15,13 +15,24 @@
  */
 #include "nnacl/infer/infer_register.h"
 
-InferShape *g_infer_func;
+InferShape *g_infer_func = NULL;
 
 __attribute__((constructor(101))) void InitInferFuncBuf() {
+  if (g_infer_func != NULL) {
+    return;
+  }
   g_infer_func = malloc(PrimType_MAX * sizeof(InferShape));
   if (g_infer_func != NULL) {
     memset(g_infer_func, 0, PrimType_MAX * sizeof(InferShape));
   }
+}
+
+__attribute__((destructor)) void FreeInferFuncBuf() {
+  if (g_infer_func == NULL) {
+    return;
+  }
+  free(g_infer_func);
+  g_infer_func = NULL;
 }
 
 InferShape GetInferFunc(int prim_type) {
