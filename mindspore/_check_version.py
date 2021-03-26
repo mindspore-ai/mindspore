e3b2b1e50a3314dd03f163083326531b827ea899
shenwei41 <shenwei41@huawei.com>
2021-03-26 16:04:15 +0800

    update graphegine commit id to master

--- mindspore/_check_version.py
+++ mindspore/_check_version.py
@@ -174,7 +174,7 @@ class AscendEnvChecker(EnvChecker):
     """ascend environment check"""
 
     def __init__(self):
-        self.version = ["1.77.22.1.220"]
+        self.version = ["1.77.22.2.220"]
         atlas_nnae_version = "/usr/local/Ascend/nnae/latest/fwkacllib/version.info"
         atlas_toolkit_version = "/usr/local/Ascend/ascend-toolkit/latest/fwkacllib/version.info"
         hisi_fwk_version = "/usr/local/Ascend/fwkacllib/version.info"
