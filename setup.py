0d1d8e68f2168937182ffac3be4155097e49b2b0
luopengting <luopengting@huawei.com>
2021-03-26 17:45:44 +0800

    change the version from 1.2.0 to 1.2.0-rc1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 from setuptools.command.egg_info import egg_info
 from setuptools.command.build_py import build_py
 
-version = '1.2.0'
+version = '1.2.0-rc1'
 
 backend_policy = os.getenv('BACKEND_POLICY')
 device_target = os.getenv('BACKEND_TARGET')
