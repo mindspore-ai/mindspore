8c47ce18033c0783cb907d033747037fd59d7fad
buxue <zhangbuxue@huawei.com>
2021-03-31 09:58:24 +0800

    show accurate error info and error lines when use unsupported builtin function

--- tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py
+++ tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py
@@ -12,41 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-""" test use undefined var"""
-import numpy as np
+""" test use undefined name or unsupported builtin function"""
 import pytest
 
 import mindspore.nn as nn
 from mindspore import Tensor
 from mindspore import context
+from mindspore import dtype as mstype
 
-context.set_context(mode=context.GRAPH_MODE)
+context.set_context(mode=context.GRAPH_MODE, save_graphs=True)
 
 
-def test_use_undefined_var():
+def test_use_undefined_name():
     class Net(nn.Cell):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.value = [11, 22, 33, 44]
-
         def construct(self, x):
             ret = x + a
             return ret
 
     net = Net()
     with pytest.raises(NameError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "The name 'a' is not defined" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(33)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(29)" in \
+           str(err.value)
     assert "ret = x + a" in str(err.value)
 
 
-def test_insert_undefined_var():
+def test_insert_undefined_name():
     class Net(nn.Cell):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.value = [11, 22, 33, 44]
-
         def construct(self, x):
             b
             ret = x + x
@@ -54,17 +47,14 @@ def test_insert_undefined_var():
 
     net = Net()
     with pytest.raises(NameError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "The name 'b' is not defined" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(51)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(44)" in \
+           str(err.value)
 
 
-def test_insert_undefined_var_compute():
+def test_insert_undefined_name_compute():
     class Net(nn.Cell):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.value = [11, 22, 33, 44]
-
         def construct(self, x):
             c + x
             ret = x + x
@@ -72,18 +62,15 @@ def test_insert_undefined_var_compute():
 
     net = Net()
     with pytest.raises(NameError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "The name 'c' is not defined" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(69)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(59)" in \
+           str(err.value)
     assert "c + x" in str(err.value)
 
 
-def test_insert_undefined_var_in_if():
+def test_insert_undefined_name_in_if():
     class Net(nn.Cell):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.value = [11, 22, 33, 44]
-
         def construct(self, x):
             if x > 0:
                 i
@@ -92,17 +79,14 @@ def test_insert_undefined_var_in_if():
 
     net = Net()
     with pytest.raises(NameError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "The name 'i' is not defined" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(89)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(76)" in \
+           str(err.value)
 
 
-def test_insert_undefined_var_in_while_inner_if():
+def test_insert_undefined_name_in_while_inner_if():
     class Net(nn.Cell):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.value = [11, 22, 33, 44]
-
         def construct(self, x):
             while x > 0:
                 if x > 1:
@@ -113,17 +97,14 @@ def test_insert_undefined_var_in_while_inner_if():
 
     net = Net()
     with pytest.raises(NameError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "The name 'j' is not defined" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(109)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(93)" in \
+           str(err.value)
 
 
-def test_insert_undefined_var_compute__in_while_inner_if():
+def test_insert_undefined_name_compute__in_while_inner_if():
     class Net(nn.Cell):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.value = [11, 22, 33, 44]
-
         def construct(self, x):
             while x > 0:
                 if x > 1:
@@ -134,13 +115,14 @@ def test_insert_undefined_var_compute__in_while_inner_if():
 
     net = Net()
     with pytest.raises(NameError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "The name 'p' is not defined" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(130)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(111)" in \
+           str(err.value)
     assert "p + x" in str(err.value)
 
 
-def test_insert_undefined_var_compute__in_for_inner_if():
+def test_insert_undefined_name_compute__in_if_in_for():
     class Net(nn.Cell):
         def __init__(self):
             super(Net, self).__init__()
@@ -156,13 +138,14 @@ def test_insert_undefined_var_compute__in_for_inner_if():
 
     net = Net()
     with pytest.raises(NameError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "The name 'w' is not defined" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(152)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(134)" in \
+           str(err.value)
     assert "w" in str(err.value)
 
 
-def test_use_undefined_var_for_inner_if():
+def test_use_undefined_name_for_inner_if():
     class Net(nn.Cell):
         def __init__(self):
             super(Net, self).__init__()
@@ -177,13 +160,14 @@ def test_use_undefined_var_for_inner_if():
 
     net = Net()
     with pytest.raises(NameError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "The name 'y' is not defined" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(174)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(157)" in \
+           str(err.value)
     assert "y" in str(err.value)
 
 
-def test_use_undefined_var_in_for():
+def test_use_undefined_name_in_for():
     class Net(nn.Cell):
         def __init__(self):
             super(Net, self).__init__()
@@ -196,13 +180,14 @@ def test_use_undefined_var_in_for():
 
     net = Net()
     with pytest.raises(NameError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "The name 'd' is not defined" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(194)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(178)" in \
+           str(err.value)
     assert "x = x + d + i" in str(err.value)
 
 
-def test_insert_undefined_var_in_for():
+def test_insert_undefined_name_in_for():
     class Net(nn.Cell):
         def __init__(self):
             super(Net, self).__init__()
@@ -216,13 +201,14 @@ def test_insert_undefined_var_in_for():
 
     net = Net()
     with pytest.raises(NameError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "The name 'e' is not defined" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(213)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(198)" in \
+           str(err.value)
     assert "e" in str(err.value)
 
 
-def test_insert_undefined_var_compute_in_for():
+def test_insert_undefined_name_compute_in_for():
     class Net(nn.Cell):
         def __init__(self):
             super(Net, self).__init__()
@@ -236,17 +222,15 @@ def test_insert_undefined_var_compute_in_for():
 
     net = Net()
     with pytest.raises(NameError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "The name 'f' is not defined" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(233)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(219)" in \
+           str(err.value)
     assert "f + i" in str(err.value)
 
 
-def test_use_undefined_var_in_while():
+def test_use_undefined_name_in_while():
     class Net(nn.Cell):
-        def __init__(self):
-            super(Net, self).__init__()
-
         def construct(self, x):
             while x < 0:
                 x = x - g
@@ -254,18 +238,15 @@ def test_use_undefined_var_in_while():
 
     net = Net()
     with pytest.raises(NameError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "The name 'g' is not defined" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(252)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(236)" in \
+           str(err.value)
     assert "x = x - g" in str(err.value)
 
 
-def test_insert_undefined_var_in_while():
+def test_insert_undefined_name_in_while():
     class Net(nn.Cell):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.value = [11, 22, 33, 44]
-
         def construct(self, x):
             while x < 0:
                 h
@@ -274,18 +255,15 @@ def test_insert_undefined_var_in_while():
 
     net = Net()
     with pytest.raises(NameError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "The name 'h' is not defined" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(271)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(252)" in \
+           str(err.value)
     assert "h" in str(err.value)
 
 
-def test_insert_undefined_var_compute_while():
+def test_insert_undefined_name_compute_while():
     class Net(nn.Cell):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.value = [11, 22, 33, 44]
-
         def construct(self, x):
             while x < 0:
                 x + i
@@ -294,9 +272,10 @@ def test_insert_undefined_var_compute_while():
 
     net = Net()
     with pytest.raises(NameError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "The name 'i' is not defined" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(291)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(269)" in \
+           str(err.value)
     assert "x + i" in str(err.value)
 
 
@@ -314,9 +293,10 @@ def test_call_none_in_if():
 
     net = Net()
     with pytest.raises(RuntimeError) as err:
-        net(Tensor(np.arange(4)))
+        net(Tensor([1, 2, 3], mstype.float32))
     assert "Not AbstractFunction: AbstractNone(Value: None)" in str(err.value)
-    assert "tests/ut/python/pipeline/parse/test_use_undefined_var.py(312)" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(291)" in \
+           str(err.value)
     assert "ret = self.func(x)" in str(err.value)
 
 
@@ -332,7 +312,7 @@ def test_insert_defined_var():
             return ret
 
     net = Net()
-    net(Tensor(np.arange(4)))
+    net(Tensor([1, 2, 3], mstype.float32))
 
 
 def test_insert_defined_var_compute():
@@ -347,4 +327,48 @@ def test_insert_defined_var_compute():
             return ret
 
     net = Net()
-    net(Tensor(np.arange(4)))
+    net(Tensor([1, 2, 3], mstype.float32))
+
+
+def test_call_unsupported_builtin_function_in_while():
+    class Net(nn.Cell):
+        def __init__(self):
+            super(Net, self).__init__()
+
+        def construct(self, x, y):
+            ret = 0
+            number = 5
+            while number > 0:
+                ret = divmod(x, y)
+                number -= 1
+            return ret
+
+    net = Net()
+    with pytest.raises(NameError) as err:
+        net(Tensor([1, 2, 3]), Tensor([4, 5, 6]))
+    assert "The builtin function 'divmod' is not supported in graph mode" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(342)" in str(
+        err.value)
+    assert "ret = divmod(x, y)" in str(err.value)
+
+
+def test_call_unsupported_builtin_function_in_if_in_for():
+    class Net(nn.Cell):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.value = [11, 22, 33, 44]
+
+        def construct(self, x):
+            for i in self.value:
+                if x > 1:
+                    x = divmod(x, i)
+            ret = x + x
+            return ret
+
+    net = Net()
+    with pytest.raises(NameError) as err:
+        net(Tensor([1, 2, 3], mstype.float32))
+    assert "The builtin function 'divmod' is not supported in graph mode" in str(err.value)
+    assert "tests/ut/python/pipeline/parse/test_use_undefined_name_or_unsupported_builtin_function.py(364)" in \
+           str(err.value)
+    assert "x = divmod(x, i)" in str(err.value)
