386ccb83e0cc5f87c8bc182e41f2ab20ca718514
wilfChen <chenweifeng720@huawei.com>
2021-04-12 10:25:40 +0800

    add relu pass check

--- mindspore/ccsrc/backend/optimizer/gpu/add_relu_v2_fusion.cc
+++ mindspore/ccsrc/backend/optimizer/gpu/add_relu_v2_fusion.cc
@@ -72,6 +72,12 @@ const AnfNodePtr AddReluV2Fusion::Process(const FuncGraphPtr &graph, const AnfNo
     return nullptr;
   }
 
+  std::vector<size_t> shape1 = AnfAlgo::GetPrevNodeOutputInferShape(tensor_add, 0);
+  std::vector<size_t> shape2 = AnfAlgo::GetPrevNodeOutputInferShape(tensor_add, 1);
+  if (shape1 != shape2) {
+    return nullptr;
+  }
+
   auto prim = std::make_shared<Primitive>(kFusedAddReluV2Name);
   MS_EXCEPTION_IF_NULL(prim);
   std::vector<AnfNodePtr> inputs = {NewValueNode(prim), x1, x2};
