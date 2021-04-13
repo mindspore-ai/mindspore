70aad9820d4151df8b1ec77a80545e7e357d70e6
lizhenyu <lizhenyu13@huawei.com>
2021-04-13 21:32:10 +0800

    fix CodeDex warning

--- mindspore/ccsrc/backend/kernel_compiler/cpu/ps/embedding_look_up_ps_kernel.cc
+++ mindspore/ccsrc/backend/kernel_compiler/cpu/ps/embedding_look_up_ps_kernel.cc
@@ -74,12 +74,13 @@ bool EmbeddingLookUpPSKernel::Execute(const std::vector<AddressPtr> &inputs, con
 
 void EmbeddingLookUpPSKernel::UpdateEmbeddings(float *embedding_table, const size_t *lookup_ids,
                                                const float *update_vals, size_t ids_size) {
-  size_t copy_lens = outer_dim_size_ * sizeof(float);
+  size_t copy_len = outer_dim_size_ * sizeof(float);
+  size_t dest_len = copy_len;
   for (size_t i = 0; i < ids_size; ++i) {
     int index = lookup_ids[i] - offset_;
     if (index >= 0 && index < SizeToInt(first_dim_size_)) {
       auto ret =
-        memcpy_s(embedding_table + index * outer_dim_size_, copy_lens, update_vals + i * outer_dim_size_, copy_lens);
+        memcpy_s(embedding_table + index * outer_dim_size_, dest_len, update_vals + i * outer_dim_size_, copy_len);
       if (ret != EOK) {
         MS_LOG(EXCEPTION) << "LookUpTable task memcpy failed.";
       }
