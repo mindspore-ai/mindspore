433581f5b04115b251d8d5da217ee4cef9379563
lilei <lilei120@huawei.com>
2021-04-22 15:03:39 +0800

    modify model_zoo textcnn net for clould

--- model_zoo/official/nlp/textcnn/scripts/run_eval.sh
+++ model_zoo/official/nlp/textcnn/scripts/run_eval.sh
@@ -13,14 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
+BASE_PATH=$(cd ./ "`dirname $0`" || exit; pwd)
 dataset_type='MR'
+CONFIG_FILE="${BASE_PATH}/mr_config.yaml"
 if [ $# == 2 ]
 then
-    if [ $2 != "MR" ] && [ $2 != "SUBJ" ] && [ $2 != "SST2" ]
-    then
+    if [ $2 == "MR" ]; then
+        CONFIG_FILE="${BASE_PATH}/mr_config.yaml"
+    elif [ $2 == "SUBJ" ]; then
+        CONFIG_FILE="${BASE_PATH}/subj_config.yaml"
+    elif [ $2 == "SST2" ]; then
+        CONFIG_FILE="${BASE_PATH}/sst2_config.yaml"
+    else
         echo "error: the selected dataset is not in supported set{MR, SUBJ, SST2}"
     exit 1
     fi
     dataset_type=$2
 fi
-python eval.py --checkpoint_path="$1" --dataset=$dataset_type > eval.log 2>&1 &
+python eval.py --checkpoint_file_path="$1" --dataset=$dataset_type --config_path=$CONFIG_FILE > eval.log 2>&1 &
--- model_zoo/official/nlp/textcnn/train.py
+++ model_zoo/official/nlp/textcnn/train.py
@@ -16,7 +16,7 @@
 #################train textcnn example on movie review########################
 python train.py
 """
-import argparse
+import os
 import math
 
 import mindspore.nn as nn
@@ -26,62 +26,62 @@ from mindspore.train.callback import ModelCheckpoint, CheckpointConfig, LossMoni
 from mindspore.train.model import Model
 from mindspore.train.serialization import load_checkpoint, load_param_into_net
 
-from src.config import cfg_mr, cfg_subj, cfg_sst2
+from utils.moxing_adapter import moxing_wrapper
+from utils.device_adapter import get_device_id, get_rank_id
+from utils.config import config
 from src.textcnn import TextCNN
 from src.textcnn import SoftmaxCrossEntropyExpand
 from src.dataset import MovieReview, SST2, Subjectivity
 
-parser = argparse.ArgumentParser(description='TextCNN')
-parser.add_argument('--device_target', type=str, default="Ascend", choices=['Ascend', 'GPU', 'CPU'],
-                    help='device where the code will be implemented (default: Ascend)')
-parser.add_argument('--device_id', type=int, default=5, help='device id of GPU or Ascend.')
-parser.add_argument('--dataset', type=str, default="MR", choices=['MR', 'SUBJ', 'SST2'])
-args_opt = parser.parse_args()
+def modelarts_pre_process():
+    config.checkpoint_path = os.path.join(config.output_path, str(get_rank_id()), config.checkpoint_path)
 
-if __name__ == '__main__':
-    rank = 0
+@moxing_wrapper(pre_process=modelarts_pre_process)
+def train_net():
+    '''train net'''
     # set context
-    context.set_context(mode=context.GRAPH_MODE, device_target=args_opt.device_target)
-    context.set_context(device_id=args_opt.device_id)
-    if args_opt.dataset == 'MR':
-        cfg = cfg_mr
-        instance = MovieReview(root_dir=cfg.data_path, maxlen=cfg.word_len, split=0.9)
-    elif args_opt.dataset == 'SUBJ':
-        cfg = cfg_subj
-        instance = Subjectivity(root_dir=cfg.data_path, maxlen=cfg.word_len, split=0.9)
-    elif args_opt.dataset == 'SST2':
-        cfg = cfg_sst2
-        instance = SST2(root_dir=cfg.data_path, maxlen=cfg.word_len, split=0.9)
+    context.set_context(mode=context.GRAPH_MODE, device_target=config.device_target)
+    context.set_context(device_id=get_device_id())
+    if config.dataset == 'MR':
+        instance = MovieReview(root_dir=config.data_path, maxlen=config.word_len, split=0.9)
+    elif config.dataset == 'SUBJ':
+        instance = Subjectivity(root_dir=config.data_path, maxlen=config.word_len, split=0.9)
+    elif config.dataset == 'SST2':
+        instance = SST2(root_dir=config.data_path, maxlen=config.word_len, split=0.9)
 
-    dataset = instance.create_train_dataset(batch_size=cfg.batch_size, epoch_size=cfg.epoch_size)
+    dataset = instance.create_train_dataset(batch_size=config.batch_size, epoch_size=config.epoch_size)
     batch_num = dataset.get_dataset_size()
 
-    base_lr = cfg.base_lr
+    base_lr = float(config.base_lr)
     learning_rate = []
-    warm_up = [base_lr / math.floor(cfg.epoch_size / 5) * (i + 1) for _ in range(batch_num) for i in
-               range(math.floor(cfg.epoch_size / 5))]
-    shrink = [base_lr / (16 * (i + 1)) for _ in range(batch_num) for i in range(math.floor(cfg.epoch_size * 3 / 5))]
+    warm_up = [base_lr / math.floor(config.epoch_size / 5) * (i + 1) for _ in range(batch_num) for i in
+               range(math.floor(config.epoch_size / 5))]
+    shrink = [base_lr / (16 * (i + 1)) for _ in range(batch_num) for i in range(math.floor(config.epoch_size * 3 / 5))]
     normal_run = [base_lr for _ in range(batch_num) for i in
-                  range(cfg.epoch_size - math.floor(cfg.epoch_size / 5) - math.floor(cfg.epoch_size * 2 / 5))]
+                  range(config.epoch_size - math.floor(config.epoch_size / 5) - math.floor(config.epoch_size * 2 / 5))]
     learning_rate = learning_rate + warm_up + normal_run + shrink
 
-    net = TextCNN(vocab_len=instance.get_dict_len(), word_len=cfg.word_len,
-                  num_classes=cfg.num_classes, vec_length=cfg.vec_length)
+    net = TextCNN(vocab_len=instance.get_dict_len(), word_len=config.word_len,
+                  num_classes=config.num_classes, vec_length=config.vec_length)
     # Continue training if set pre_trained to be True
-    if cfg.pre_trained:
-        param_dict = load_checkpoint(cfg.checkpoint_path)
+    if config.pre_trained:
+        param_dict = load_checkpoint(config.checkpoint_path)
         load_param_into_net(net, param_dict)
 
-    opt = nn.Adam(filter(lambda x: x.requires_grad, net.get_parameters()), learning_rate=learning_rate, weight_decay=cfg.weight_decay)
+    opt = nn.Adam(filter(lambda x: x.requires_grad, net.get_parameters()), \
+                                    learning_rate=learning_rate, weight_decay=float(config.weight_decay))
     loss = SoftmaxCrossEntropyExpand(sparse=True)
 
     model = Model(net, loss_fn=loss, optimizer=opt, metrics={'acc': Accuracy()})
 
-    config_ck = CheckpointConfig(save_checkpoint_steps=int(cfg.epoch_size*batch_num/2),
-                                 keep_checkpoint_max=cfg.keep_checkpoint_max)
+    config_ck = CheckpointConfig(save_checkpoint_steps=int(config.epoch_size*batch_num/2),
+                                 keep_checkpoint_max=config.keep_checkpoint_max)
     time_cb = TimeMonitor(data_size=batch_num)
-    ckpt_save_dir = "./ckpt_" + str(rank) + "/"
+    ckpt_save_dir = os.path.join(config.output_path, config.checkpoint_path)
     ckpoint_cb = ModelCheckpoint(prefix="train_textcnn", directory=ckpt_save_dir, config=config_ck)
     loss_cb = LossMonitor()
-    model.train(cfg.epoch_size, dataset, callbacks=[time_cb, ckpoint_cb, loss_cb])
+    model.train(config.epoch_size, dataset, callbacks=[time_cb, ckpoint_cb, loss_cb])
     print("train success")
+
+if __name__ == '__main__':
+    train_net()
