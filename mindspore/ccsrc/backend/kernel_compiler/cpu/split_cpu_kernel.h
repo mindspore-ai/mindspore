77c83d18cf004748b2c9349406ab2940b43c9bba
shaoxiangdong <shaoxiangdong@huawei.com>
2021-04-30 17:04:05 +0800

    modify split cpu kernel

--- mindspore/ccsrc/backend/kernel_compiler/cpu/split_cpu_kernel.h
+++ mindspore/ccsrc/backend/kernel_compiler/cpu/split_cpu_kernel.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Huawei Technologies Co., Ltd
+ * Copyright 2020-2021 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@
 #include <thread>
 #include "backend/kernel_compiler/cpu/cpu_kernel.h"
 #include "backend/kernel_compiler/cpu/cpu_kernel_factory.h"
+#include "backend/kernel_compiler/cpu/nnacl/base/split_base.h"
 
 namespace mindspore {
 namespace kernel {
@@ -42,7 +43,8 @@ class SplitCPUKernel : public CPUKernel {
  private:
   void CheckParam(const CNodePtr &kernel_node);
   void Reshape();
-  void LaunchSplit(const T *input, T **output, size_t size);
+  void LaunchSplit(T *input, T **output, size_t size);
+  void FreeTmpBuff();
   int64_t axis_;
   int64_t output_num_;
   int64_t axis_step_;
@@ -53,7 +55,11 @@ class SplitCPUKernel : public CPUKernel {
 
   std::vector<std::vector<size_t>> output_shape_list_;
   std::vector<size_t> input_shape_;
+  std::vector<int> input_shape_int_;
   TypeId dtype_{kTypeUnknown};
+
+ protected:
+  SplitParameter *param_ = nullptr;
 };
 
 MS_REG_CPU_KERNEL_T(
