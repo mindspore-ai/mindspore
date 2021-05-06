248be56be75e94701bf227d4e92f4c62f9c519fa
TinaMengtingZhang <tina.mengting.zhang@huawei.com>
2021-05-06 11:18:46 -0400

    change C style struct definition

--- mindspore/ccsrc/debug/debugger/offline_debug/mi_pybind_register.cc
+++ mindspore/ccsrc/debug/debugger/offline_debug/mi_pybind_register.cc
@@ -29,38 +29,38 @@ PYBIND11_MODULE(_mindspore_offline_debug, m) {
     .def("ReadTensors", &DbgServices::ReadTensors)
     .def("GetVersion", &DbgServices::GetVersion);
 
-  py::class_<parameter>(m, "parameter")
+  py::class_<parameter_t>(m, "parameter")
     .def(py::init<std::string, bool, double, bool, double>())
-    .def("get_name", &parameter::get_name)
-    .def("get_disabled", &parameter::get_disabled)
-    .def("get_value", &parameter::get_value)
-    .def("get_hit", &parameter::get_hit)
-    .def("get_actual_value", &parameter::get_actual_value);
+    .def("get_name", &parameter_t::get_name)
+    .def("get_disabled", &parameter_t::get_disabled)
+    .def("get_value", &parameter_t::get_value)
+    .def("get_hit", &parameter_t::get_hit)
+    .def("get_actual_value", &parameter_t::get_actual_value);
 
-  py::class_<watchpoint_hit>(m, "watchpoint_hit")
+  py::class_<watchpoint_hit_t>(m, "watchpoint_hit")
     .def(py::init<std::string, uint32_t, int, uint32_t, std::vector<parameter_t>, int32_t, uint32_t, uint32_t>())
-    .def("get_name", &watchpoint_hit::get_name)
-    .def("get_slot", &watchpoint_hit::get_slot)
-    .def("get_condition", &watchpoint_hit::get_condition)
-    .def("get_watchpoint_id", &watchpoint_hit::get_watchpoint_id)
-    .def("get_parameters", &watchpoint_hit::get_parameters)
-    .def("get_error_code", &watchpoint_hit::get_error_code)
-    .def("get_device_id", &watchpoint_hit::get_device_id)
-    .def("get_root_graph_id", &watchpoint_hit::get_root_graph_id);
+    .def("get_name", &watchpoint_hit_t::get_name)
+    .def("get_slot", &watchpoint_hit_t::get_slot)
+    .def("get_condition", &watchpoint_hit_t::get_condition)
+    .def("get_watchpoint_id", &watchpoint_hit_t::get_watchpoint_id)
+    .def("get_parameters", &watchpoint_hit_t::get_parameters)
+    .def("get_error_code", &watchpoint_hit_t::get_error_code)
+    .def("get_device_id", &watchpoint_hit_t::get_device_id)
+    .def("get_root_graph_id", &watchpoint_hit_t::get_root_graph_id);
 
-  py::class_<tensor_info>(m, "tensor_info")
+  py::class_<tensor_info_t>(m, "tensor_info")
     .def(py::init<std::string, uint32_t, uint32_t, uint32_t, uint32_t, bool>())
-    .def("get_node_name", &tensor_info::get_node_name)
-    .def("get_slot", &tensor_info::get_slot)
-    .def("get_iteration", &tensor_info::get_iteration)
-    .def("get_device_id", &tensor_info::get_device_id)
-    .def("get_root_graph_id", &tensor_info::get_root_graph_id)
-    .def("get_is_parameter", &tensor_info::get_is_parameter);
+    .def("get_node_name", &tensor_info_t::get_node_name)
+    .def("get_slot", &tensor_info_t::get_slot)
+    .def("get_iteration", &tensor_info_t::get_iteration)
+    .def("get_device_id", &tensor_info_t::get_device_id)
+    .def("get_root_graph_id", &tensor_info_t::get_root_graph_id)
+    .def("get_is_parameter", &tensor_info_t::get_is_parameter);
 
-  py::class_<tensor_data>(m, "tensor_data")
+  py::class_<tensor_data_t>(m, "tensor_data")
     .def(py::init<char *, uint64_t, int, std::vector<int64_t>>())
-    .def("get_data_ptr", &tensor_data::get_data_ptr)
-    .def("get_data_size", &tensor_data::get_data_size)
-    .def("get_dtype", &tensor_data::get_dtype)
-    .def("get_shape", &tensor_data::get_shape);
+    .def("get_data_ptr", &tensor_data_t::get_data_ptr)
+    .def("get_data_size", &tensor_data_t::get_data_size)
+    .def("get_dtype", &tensor_data_t::get_dtype)
+    .def("get_shape", &tensor_data_t::get_shape);
 }
