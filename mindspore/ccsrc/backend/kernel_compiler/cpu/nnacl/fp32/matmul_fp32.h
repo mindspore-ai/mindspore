8983f9029311c8fa0a853afc0a19c114657a2418
zhaozhenlong <zhaozhenlong1@huawei.com>
2021-05-14 17:44:55 +0800

    loop split

--- mindspore/ccsrc/backend/kernel_compiler/cpu/nnacl/fp32/matmul_fp32.h
+++ mindspore/ccsrc/backend/kernel_compiler/cpu/nnacl/fp32/matmul_fp32.h
@@ -57,6 +57,12 @@ void MatmulFloatNeon64(const float *a, const float *b, float *c, const float *bi
                        int col, size_t stride, size_t writeNhwc, size_t WriteWino);
 void MatmulFloatNeon64Opt(const float *a, const float *b, float *c, const float *bias, int act_type, int depth, int row,
                           int col, size_t stride, size_t write_mode);
+void MatmulFloatNeon64OptRow8(const float *a, const float *b, float *c, const float *bias, int act_type, int depth,
+                              int row, int col, size_t stride, size_t write_mode);
+void MatmulFloatNeon64OptRow4(const float *a, const float *b, float *c, const float *bias, int act_type, int depth,
+                              int row, int col, size_t stride, size_t write_mode);
+void MatmulFloatNeon64OptRow12(const float *a, const float *b, float *c, const float *bias, int act_type, int depth,
+                               int row, int col, size_t stride, size_t write_mode);
 #elif ENABLE_ARM32
 void MatmulFloatNeon32(const float *a, const float *b, float *c, const float *bias, int act_type, int depth, int row,
                        int col, int stride, size_t writeNhwc, size_t WriteWino);
@@ -75,6 +81,8 @@ void MatmulFloatAvxOpt(const float *a, const float *b, float *c, const float *bi
                        size_t row, size_t col, size_t stride, size_t write_mode);
 #endif
 #endif
+void MatMul12x8(const float *a, const float *b, float *dst, const float *bias, ActType act_type, int deep, int row,
+                int col, int stride, int out_type);
 
 #ifdef __cplusplus
 }
