b5ec25cc49f090775c359e6a3d6f77a54bdc15d5
buxue <zhangbuxue@huawei.com>
2021-05-18 17:51:44 +0800

    develop int32 AddN op for cpu

--- mindspore/ccsrc/backend/kernel_compiler/cpu/mkldnn/addn_cpu_kernel.cc
+++ mindspore/ccsrc/backend/kernel_compiler/cpu/mkldnn/addn_cpu_kernel.cc
@@ -17,14 +17,25 @@
 #include "backend/kernel_compiler/cpu/mkldnn/addn_cpu_kernel.h"
 #include "backend/kernel_compiler/cpu/mkldnn/mkl_kernel_engine.h"
 #include "runtime/device/cpu/cpu_device_address.h"
+#include "backend/kernel_compiler/cpu/nnacl/fp32/add_fp32.h"
+#include "backend/kernel_compiler/cpu/nnacl/errorcode.h"
 #include "utils/ms_utils.h"
+#include "common/thread_pool.h"
 
 namespace mindspore {
 namespace kernel {
+void AddInt(const int *in_0, const int *in_1, int *out, int start, int end) {
+  int ret = ElementAddInt(in_0 + start, in_1 + start, out + start, end - start);
+  if (ret != NNACL_OK) {
+    MS_LOG(EXCEPTION) << "Add failed.";
+  }
+}
+
 void AddNCPUKernel::InitKernel(const CNodePtr &kernel_node) {
   MS_EXCEPTION_IF_NULL(kernel_node);
-  input_num_ = AnfAlgo::GetInputTensorNum(kernel_node);
   CheckParam(kernel_node);
+  input_num_ = AnfAlgo::GetInputTensorNum(kernel_node);
+  dtype_ = AnfAlgo::GetPrevNodeOutputInferDataType(kernel_node, 0);
   std::vector<size_t> src0_shape = AnfAlgo::GetInputDeviceShape(kernel_node, 0);
   std::vector<size_t> src1_shape = AnfAlgo::GetInputDeviceShape(kernel_node, 1);
   std::vector<size_t> dst_shape = AnfAlgo::GetOutputDeviceShape(kernel_node, 0);
@@ -42,15 +53,31 @@ void AddNCPUKernel::InitKernel(const CNodePtr &kernel_node) {
 bool AddNCPUKernel::Launch(const std::vector<kernel::AddressPtr> &inputs,
                            const std::vector<kernel::AddressPtr> & /*workspace*/,
                            const std::vector<kernel::AddressPtr> &outputs) {
-  SetArgumentHandle(DNNL_ARG_SRC_0, inputs[0]->addr);
-  SetArgumentHandle(DNNL_ARG_SRC_1, inputs[1]->addr);
-  SetArgumentHandle(DNNL_ARG_DST, outputs[0]->addr);
-  ExecutePrimitive();
-  for (size_t index = 2; index < input_num_; ++index) {
-    SetArgumentHandle(DNNL_ARG_SRC_0, outputs[0]->addr);
-    SetArgumentHandle(DNNL_ARG_SRC_1, inputs[index]->addr);
+  if (dtype_ == kNumberTypeFloat32) {
+    SetArgumentHandle(DNNL_ARG_SRC_0, inputs[0]->addr);
+    SetArgumentHandle(DNNL_ARG_SRC_1, inputs[1]->addr);
     SetArgumentHandle(DNNL_ARG_DST, outputs[0]->addr);
     ExecutePrimitive();
+    for (size_t index = 2; index < input_num_; ++index) {
+      SetArgumentHandle(DNNL_ARG_SRC_0, outputs[0]->addr);
+      SetArgumentHandle(DNNL_ARG_SRC_1, inputs[index]->addr);
+      SetArgumentHandle(DNNL_ARG_DST, outputs[0]->addr);
+      ExecutePrimitive();
+    }
+  } else if (dtype_ == kNumberTypeInt32) {
+    size_t elements_num = outputs[0]->size / sizeof(int);
+    const auto input_0 = reinterpret_cast<int *>(inputs[0]->addr);
+    const auto input_1 = reinterpret_cast<int *>(inputs[1]->addr);
+    auto output = reinterpret_cast<int *>(outputs[0]->addr);
+    auto task_0 = std::bind(AddInt, input_0, input_1, output, std::placeholders::_1, std::placeholders::_2);
+    CPUKernelUtils::ParallelFor(task_0, elements_num);
+    for (size_t index = 2; index < input_num_; ++index) {
+      const auto input = reinterpret_cast<int *>(inputs[index]->addr);
+      auto task = std::bind(AddInt, input, output, output, std::placeholders::_1, std::placeholders::_2);
+      CPUKernelUtils::ParallelFor(task, elements_num);
+    }
+  } else {
+    MS_LOG(EXCEPTION) << "AddN only support float32 and int32, but got " << TypeIdToType(dtype_)->ToString();
   }
   return true;
 }
