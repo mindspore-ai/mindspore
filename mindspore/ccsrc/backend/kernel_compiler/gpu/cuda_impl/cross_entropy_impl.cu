e63a47db8ca50c35d120accc87378fb72aacdd60
ZPaC <zhoupeichen@huawei.com>
2021-05-20 18:50:16 +0800

    Optimize cse

--- mindspore/ccsrc/backend/kernel_compiler/gpu/cuda_impl/cross_entropy_impl.cu
+++ mindspore/ccsrc/backend/kernel_compiler/gpu/cuda_impl/cross_entropy_impl.cu
@@ -18,33 +18,32 @@
 #include <stdint.h>
 #include <cuda_runtime.h>
 #include "cross_entropy_impl.cuh"
+#include "backend/kernel_compiler/gpu/cuda_impl/util.cuh"
 
 template <typename T, typename S>
 __global__ void CrossEntropyWithSparseKernel(const T *logits, const S *labels, const size_t batch_size,
                                              const size_t class_num, T *loss) {
-  double total_loss = 0.0;
+  *loss = 0;
   T epsilon = 1e-6;
-  for (size_t i = 0; i < batch_size; ++i) {
-    T logit = logits[i * class_num + labels[i]];
+  for (size_t index = blockIdx.x * blockDim.x + threadIdx.x; index < batch_size; index += blockDim.x * gridDim.x) {
+    T logit = logits[index * class_num + labels[index]];
     if (logit <= 0) {
       logit = epsilon;
     }
-    total_loss += -logf(logit);
+    MsAtomicAdd(loss, -logf(logit) / batch_size);
   }
-  total_loss /= batch_size;
-  loss[0] = static_cast<T>(total_loss);
   return;
 }
 
 template <typename T, typename S>
 __global__ void CrossEntropyGradWithSparseKernel(const T *logits, const S *labels, const size_t batch_size,
                                                  const size_t class_num, T *grad) {
-  for (size_t i = 0; i < batch_size; i++) {
-    for (size_t j = blockIdx.x * blockDim.x + threadIdx.x; j < class_num; j += blockDim.x * gridDim.x) {
-      if (labels[i] == j) {
-        grad[i * class_num + j] = (logits[i * class_num + j] - 1) / batch_size;
+  for (size_t i = 0; i < class_num; i++) {
+    for (size_t j = blockIdx.x * blockDim.x + threadIdx.x; j < batch_size; j += blockDim.x * gridDim.x) {
+      if (labels[j] == i) {
+        grad[j * class_num + i] = (logits[j * class_num + i] - 1) / batch_size;
       } else {
-        grad[i * class_num + j] = logits[i * class_num + j] / batch_size;
+        grad[j * class_num + i] = logits[j * class_num + i] / batch_size;
       }
     }
   }
@@ -54,9 +53,7 @@ __global__ void CrossEntropyGradWithSparseKernel(const T *logits, const S *label
 template <typename T, typename S>
 __global__ void CrossEntropyKernel(const T *logits, const S *labels, const size_t batch_size, const size_t class_num,
                                    T epsilon, T *losses, T *dlogits) {
-  for (size_t index = blockIdx.x * blockDim.x + threadIdx.x;
-       index < batch_size;
-       index += blockDim.x * gridDim.x) {
+  for (size_t index = blockIdx.x * blockDim.x + threadIdx.x; index < batch_size; index += blockDim.x * gridDim.x) {
     losses[index] = 0;
     const int start = index * class_num;
     const int end = (index + 1) * class_num;
@@ -70,15 +67,16 @@ __global__ void CrossEntropyKernel(const T *logits, const S *labels, const size_
 template <typename T, typename S>
 void CrossEntropyWithSparse(const T *logits, const S *labels, const size_t batch_size, const size_t class_num, T *loss,
                             cudaStream_t cuda_stream) {
-  CrossEntropyWithSparseKernel<<<1, 1, 0, cuda_stream>>>(logits, labels, batch_size, class_num, loss);
+  CrossEntropyWithSparseKernel<<<GET_BLOCKS(batch_size), GET_THREADS, 0, cuda_stream>>>(logits, labels, batch_size,
+                                                                                        class_num, loss);
   return;
 }
 
 template <typename T, typename S>
 void CrossEntropyGradWithSparse(const T *logits, const S *labels, const size_t batch_size, const size_t class_num,
                                 T *grad, cudaStream_t cuda_stream) {
-  CrossEntropyGradWithSparseKernel<<<GET_BLOCKS(class_num), GET_THREADS, 0, cuda_stream>>>(logits, labels, batch_size,
-                                                                                           class_num, grad);
+  CrossEntropyGradWithSparseKernel<<<GET_BLOCKS(batch_size), GET_THREADS, 0, cuda_stream>>>(logits, labels, batch_size,
+                                                                                            class_num, grad);
   return;
 }
 
