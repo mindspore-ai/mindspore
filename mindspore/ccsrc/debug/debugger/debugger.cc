2ca5e2f9282d91a50d4b0c8e9d099de8e22a0867
maning202007 <maning36@huawei.com>
2021-05-25 17:07:36 +0800

    fix codecheck for debugger of r1.2

--- mindspore/ccsrc/debug/debugger/debugger.cc
+++ mindspore/ccsrc/debug/debugger/debugger.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Huawei Technologies Co., Ltd
+ * Copyright 2020-2021 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  */
 
 #include <dirent.h>
-#include <stdio.h>
+#include <cstdio>
 #include <fstream>
 #include <tuple>
 #include <vector>
@@ -47,7 +47,7 @@ using debugger::WatchCondition_Parameter;
 using debugger::WatchNode;
 using debugger::WatchpointHit;
 
-#define CHUNK_SIZE 1024 * 1024 * 3
+constexpr int CHUNK_SIZE = 1024 * 1024 * 3;
 
 namespace mindspore {
 
@@ -131,7 +131,7 @@ void Debugger::EnableDebugger() {
 
   if (debugger_enabled_) {
     // configure grpc host
-    const std::string env_host_str = std::getenv("MS_DEBUGGER_HOST");
+    std::string env_host_str = common::GetEnv("MS_DEBUGGER_HOST");
     std::string host;
     if (!env_host_str.empty()) {
       if (CheckIp(env_host_str)) {
@@ -147,7 +147,7 @@ void Debugger::EnableDebugger() {
       host = "localhost";
     }
     // configure grpc port
-    const std::string env_port_str = std::getenv("MS_DEBUGGER_PORT");
+    std::string env_port_str = common::GetEnv("MS_DEBUGGER_PORT");
     std::string port;
     if (!env_port_str.empty()) {
       if (CheckPort(env_port_str)) {
@@ -160,7 +160,7 @@ void Debugger::EnableDebugger() {
       }
     } else {
       port = "50051";
-      if (!CheckPort(port.c_str())) {
+      if (!CheckPort(port)) {
         MS_EXCEPTION(ValueError) << "Default MS_DEBUGGER_PORT is not valid. Custom port ranging from 1 to 65535";
       }
       MS_LOG(INFO) << "Environment variable MS_DEBUGGER_PORT doesn't exist. Using default debugger port: 50051";
@@ -1020,7 +1020,7 @@ uint64_t BytestoInt64(const std::vector<char> &buffer) {
   return ret;
 }
 
-#define BUF_SIZ 256
+constexpr int BUF_SIZ = 256;
 std::vector<std::string> Debugger::CheckOpOverflow() {
   std::vector<double> bin_list;
   std::vector<std::string> op_names;
--- mindspore/ccsrc/debug/debugger/tensor_summary.cc
+++ mindspore/ccsrc/debug/debugger/tensor_summary.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Huawei Technologies Co., Ltd
+ * Copyright 2020-2021 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#include <math.h>
+#include <cmath>
 #include <algorithm>
 #include <limits>
 #include <memory>
