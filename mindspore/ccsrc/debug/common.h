5f082ee8904c635011f36428ad61715fc2187dc5
louei5 <liujiabin5@huawei.com>
2021-05-27 18:40:23 +0800

    fix pclint & codecheck warnings

--- mindspore/ccsrc/debug/common.h
+++ mindspore/ccsrc/debug/common.h
@@ -32,17 +32,16 @@ class Common {
   static std::optional<std::string> GetRealPath(const std::string &input_path);
   static std::optional<std::string> GetConfigFile(const std::string &env);
   static std::optional<std::string> GetEnvConfigFile();
-  static bool IsStrLengthValid(const std::string &str, const int &length_limit, const std::string &error_message = "");
-  static bool IsPathValid(const std::string &path, const int &length_limit, const std::string &error_message = "");
-  static bool IsFilenameValid(const std::string &filename, const int &length_limit,
-                              const std::string &error_message = "");
+  static bool IsStrLengthValid(const std::string &str, size_t length_limit, const std::string &error_message = "");
+  static bool IsPathValid(const std::string &path, size_t length_limit, const std::string &error_message = "");
+  static bool IsFilenameValid(const std::string &filename, size_t length_limit, const std::string &error_message = "");
   static bool CreateNotExistDirs(const std::string &path);
 
   static std::string AddId(const std::string &filename, const std::string &suffix);
   static bool OpenFile(const std::string filename, std::ofstream &ofs);
 
  private:
-  static bool IsEveryFilenameValid(const std::string &path, const int &length_limit, const std::string &error_message);
+  static bool IsEveryFilenameValid(const std::string &path, size_t length_limit, const std::string &error_message);
 };
 }  // namespace mindspore
 #endif  // MINDSPORE_CCSRC_DEBUG_COMMON_H_
