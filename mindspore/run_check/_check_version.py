0af87e888ea6544ed54054106cb4996a09ed1acc
dingpeifei <dingpeifei1@huawei.com>
2021-06-17 19:54:54 +0800

    upgrade_ascend_0617

--- mindspore/run_check/_check_version.py
+++ mindspore/run_check/_check_version.py
@@ -206,7 +206,7 @@ class AscendEnvChecker(EnvChecker):
     """ascend environment check"""
 
     def __init__(self):
-        self.version = ["1.78.T21.0.B210"]
+        self.version = ["1.78.23.0.230"]
         atlas_nnae_version = "/usr/local/Ascend/nnae/latest/fwkacllib/version.info"
         atlas_toolkit_version = "/usr/local/Ascend/ascend-toolkit/latest/fwkacllib/version.info"
         hisi_fwk_version = "/usr/local/Ascend/fwkacllib/version.info"
