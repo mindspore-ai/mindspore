e927c193c56424890965c3813b8e237154a76e9d
zhangzhewei <zhangzhewei1@huawei.com>
2021-06-17 16:00:20 +0800

    fancy index cpu unsorted_segment_sum change

--- mindspore/ccsrc/backend/kernel_compiler/cpu/nnacl/base/unsorted_segment_sum_base.h
+++ mindspore/ccsrc/backend/kernel_compiler/cpu/nnacl/base/unsorted_segment_sum_base.h
@@ -17,15 +17,21 @@
 #ifndef MINDSPORE_NNACL_UNSORTED_SEGMENT_SUM_BASE_H_
 #define MINDSPORE_NNACL_UNSORTED_SEGMENT_SUM_BASE_H_
 
+#include "nnacl/op_base.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-#define UnsortedSegmentSum(type, input, unit_num, input_dim1, indices, output, output_dim0, output_dim1) \
-  UnsortedSegmentSum_##type(input, unit_num, input_dim1, indices, output, output_dim0, output_dim1)
-int UnsortedSegmentSum_int(const int *input, int unit_num, int input_dim1, const int *indices, int *output,
-                           int output_dim0, int output_dim1);
-int UnsortedSegmentSum_float(const float *input, int unit_num, int input_dim1, const int *indices, float *output,
-                             int output_dim0, int output_dim1);
+#define UnsortedSegmentSum(type, type1, input, unit_num, input_dim1, indices, output, output_dim0, output_dim1) \
+  UnsortedSegmentSum_##type##_##type1(input, unit_num, input_dim1, indices, output, output_dim0, output_dim1)
+int UnsortedSegmentSum_int_int(const int *input, int unit_num, int input_dim1, const int *indices, int *output,
+                               int output_dim0, int output_dim1);
+int UnsortedSegmentSum_float_int(const float *input, int unit_num, int input_dim1, const int *indices, float *output,
+                                 int output_dim0, int output_dim1);
+int UnsortedSegmentSum_int_int64_t(const int *input, int unit_num, int input_dim1, const int64_t *indices, int *output,
+                                   int output_dim0, int output_dim1);
+int UnsortedSegmentSum_float_int64_t(const float *input, int unit_num, int input_dim1, const int64_t *indices,
+                                     float *output, int output_dim0, int output_dim1);
 #ifdef __cplusplus
 }
 #endif
