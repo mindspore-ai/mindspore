1dc600295b43b1798420ec80a7fd3acf43804b39
simson <wudengsong@huawei.com>
2021-07-02 16:55:34 +0800

    infer for layernormgrad in c++

---
+++ mindspore/core/ops/LayerNormBetaGammaBackprop.cc
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2021 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "LayerNormBetaGammaBackprop.h"
+#include <memory>
+#include <set>
+#include "ops/op_utils.h"
+#include "utils/check_convert_utils.h"
+#include "utils/tensor_construct_utils.h"
+#include "abstract/primitive_infer_map.h"
+
+namespace mindspore {
+namespace ops {
+namespace {
+abstract::TupleShapePtr LayerNormBetaGammaBackpropInferShape(const PrimitivePtr &primitive,
+                                                             const std::vector<AbstractBasePtr> &input_args) {
+  MS_EXCEPTION_IF_NULL(primitive);
+  auto beta_shape_ptr = CheckAndConvertUtils::GetTensorInputShape(primitive->name(), input_args, 2);
+  ValuePtr gamma_value_ptr = primitive->GetAttr("shape_gamma");
+  MS_EXCEPTION_IF_NULL(gamma_value_ptr);
+  auto gamma_shape = GetValue<ShapeVector>(gamma_value_ptr);
+  auto gamma_shape_ptr =
+    std::make_shared<abstract::Shape>(gamma_shape, beta_shape_ptr->min_shape(), beta_shape_ptr->max_shape());
+  return std::make_shared<abstract::TupleShape>(std::vector<abstract::BaseShapePtr>{gamma_shape_ptr, beta_shape_ptr});
+}
+
+TypePtr LayerNormBetaGammaBackpropInferType(const PrimitivePtr &prim, const std::vector<AbstractBasePtr> &input_args) {
+  MS_EXCEPTION_IF_NULL(prim);
+  auto prim_name = prim->name();
+  // check
+  std::set<TypePtr> valid_types = {kFloat16, kFloat32};
+  std::map<std::string, TypePtr> types;
+  const int64_t beta_index = 1;
+  const int64_t gamma_index = 2;
+  types.emplace("beta", input_args[beta_index]->BuildType());
+  types.emplace("gamma", input_args[gamma_index]->BuildType());
+  return CheckAndConvertUtils::CheckTensorTypeSame(types, valid_types, prim_name);
+}
+}  // namespace
+
+AbstractBasePtr LayerNormBetaGammaBackpropInfer(const abstract::AnalysisEnginePtr &, const PrimitivePtr &primitive,
+                                                const std::vector<AbstractBasePtr> &input_args) {
+  MS_EXCEPTION_IF_NULL(primitive);
+  const int64_t input_num = 4;
+  CheckAndConvertUtils::CheckInteger("LayerNormBetaGammaBackprop infer", SizeToLong(input_args.size()), kGreaterEqual,
+                                     input_num, primitive->name());
+  return abstract::MakeAbstract(LayerNormBetaGammaBackpropInferShape(primitive, input_args),
+                                LayerNormBetaGammaBackpropInferType(primitive, input_args));
+}
+REGISTER_PRIMITIVE_EVAL_IMPL(LayerNormBetaGammaBackprop, prim::kPrimLayerNormBetaGammaBackprop,
+                             LayerNormBetaGammaBackpropInfer, nullptr, true);
+}  // namespace ops
+}  // namespace mindspore
---
+++ mindspore/core/ops/LayerNormXBackprop.cc
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2021 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "LayerNormXBackprop.h"
+#include <memory>
+#include <set>
+#include "ops/op_utils.h"
+#include "utils/check_convert_utils.h"
+#include "utils/tensor_construct_utils.h"
+#include "abstract/primitive_infer_map.h"
+
+namespace mindspore {
+namespace ops {
+namespace {
+abstract::ShapePtr LayerNormXBackpropInferShape(const PrimitivePtr &primitive,
+                                                const std::vector<AbstractBasePtr> &input_args) {
+  MS_EXCEPTION_IF_NULL(primitive);
+  auto x_shape = CheckAndConvertUtils::GetTensorInputShape(primitive->name(), input_args, 1);
+  return x_shape;
+}
+
+TypePtr LayerNormXBackpropInferType(const PrimitivePtr &prim, const std::vector<AbstractBasePtr> &input_args) {
+  MS_EXCEPTION_IF_NULL(prim);
+  auto prim_name = prim->name();
+  // check
+  std::set<TypePtr> valid_types = {kFloat16, kFloat32};
+  const int64_t x_index = 1;
+  auto x_type = input_args[x_index]->BuildType();
+  (void)CheckAndConvertUtils::CheckTensorTypeValid("x", x_type, valid_types, prim_name);
+  return x_type;
+}
+}  // namespace
+
+AbstractBasePtr LayerNormXBackpropInfer(const abstract::AnalysisEnginePtr &, const PrimitivePtr &primitive,
+                                        const std::vector<AbstractBasePtr> &input_args) {
+  MS_EXCEPTION_IF_NULL(primitive);
+  const int64_t input_num = 5;
+  CheckAndConvertUtils::CheckInteger("LayerNormXBackprop infer", SizeToLong(input_args.size()), kGreaterEqual,
+                                     input_num, primitive->name());
+  return abstract::MakeAbstract(LayerNormXBackpropInferShape(primitive, input_args),
+                                LayerNormXBackpropInferType(primitive, input_args));
+}
+REGISTER_PRIMITIVE_EVAL_IMPL(LayerNormXBackprop, prim::kPrimLayerNormXBackprop, LayerNormXBackpropInfer, nullptr, true);
+}  // namespace ops
+}  // namespace mindspore
