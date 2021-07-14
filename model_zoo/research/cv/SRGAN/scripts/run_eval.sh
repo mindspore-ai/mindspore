3cf3405cbe1484167a5ebf5a184fc4d98d440fe8
l_emon <715485601@qq.com>
2021-07-14 16:21:19 +0800

    bug fix

--- model_zoo/research/cv/SRGAN/scripts/run_eval.sh
+++ model_zoo/research/cv/SRGAN/scripts/run_eval.sh
@@ -12,18 +12,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-if [ $# != 3 ]
+if [ $# != 4 ]
 then
-    echo "Usage: sh run_eval.sh [CKPT] [EVALLRPATH] [EVALGTPATH]"
+    echo "Usage: sh run_eval.sh [CKPT] [EVALLRPATH] [EVALGTPATH] [DEVICE_ID]"
     exit 1
 fi
 
 export CKPT=$1
 export EVALLRPATH=$2
 export EVALGTPATH=$3
+export DEVICE_ID=$4
+
+rm -rf ./eval
+mkdir ./eval
+cp -r ../src ./eval
+cp -r ../*.py ./eval
+cd ./eval || exit
 
 env > env.log
-if [ $# == 3 ]
-then
-python ../eval.py --generator_path=$CKPT --test_LR_path=$EVALLRPATH --test_GT_path=$EVALGTPATH &> log &
-fi
+python ./eval.py --generator_path=$CKPT --test_LR_path=$EVALLRPATH --device_id $DEVICE_ID\
+                 --test_GT_path=$EVALGTPATH &> log &
