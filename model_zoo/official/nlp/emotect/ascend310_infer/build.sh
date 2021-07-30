0075c1b02ebc275fbf565c934e828aa44c861ad1
lvyufeng <lvyufeng@cqu.edu.cn>
2021-07-30 14:38:33 +0800

    add emotect 310 infer code

--- model_zoo/official/nlp/emotect/ascend310_infer/build.sh
+++ model_zoo/official/nlp/emotect/ascend310_infer/build.sh
@@ -13,21 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-mkdir -p ms_log
-CUR_DIR=`pwd`
-DATA_PATH=${CUR_DIR}/data
-SAVE_PATH=${CUR_DIR}/save_models
-export GLOG_log_dir=${CUR_DIR}/ms_log
-export GLOG_logtostderr=0
-python ${CUR_DIR}/run_ernie_classifier.py  \
-    --device_target="Ascend" \
-    --do_train="false" \
-    --do_eval="true" \
-    --device_id=0 \
-    --num_class=3 \
-    --train_data_shuffle="true" \
-    --eval_data_shuffle="false" \
-    --eval_batch_size=32 \
-    --load_finetune_checkpoint_path="${SAVE_PATH}/classifier-3_301.ckpt" \
-    --eval_data_file_path="${DATA_PATH}/test.mindrecord" \
-    --schema_file_path="" > ${GLOG_log_dir}/eval_classifier_log.txt 2>&1 &
+if [ -d out ]; then
+    rm -rf out
+fi
+
+mkdir out
+cd out || exit
+
+if [ -f "Makefile" ]; then
+  make clean
+fi
+
+cmake .. \
+    -DMINDSPORE_PATH="`pip3.7 show mindspore-ascend | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+make
