b09d411dc4775f3bb393fccce7031fefe287681b
zhoufeng <zhoufeng54@huawei.com>
2021-08-04 21:41:37 +0800

    fix same node is used by two comm op
    Signed-off-by: zhoufeng <zhoufeng54@huawei.com>

--- mindspore/ccsrc/backend/optimizer/ascend/enhancer/insert_tensor_move_for_hccl_op.h
+++ mindspore/ccsrc/backend/optimizer/ascend/enhancer/insert_tensor_move_for_hccl_op.h
@@ -32,7 +32,7 @@ class InsertTensorMoveForHcclOp : public PatternProcessPass {
 
  private:
   void InsertTensorMove(const FuncGraphPtr &graph, const CNodePtr &hccl_node) const;
-  bool NeedInsertTensorMove(const FuncGraphPtr &graph, const AnfNodePtr &input, const CNodePtr &cur_node) const;
+  bool NeedInsertTensorMove(const FuncGraphPtr &graph, const CNodePtr &cur_node, size_t input_idx) const;
   KernelQueryPtr kernel_query_;
 };
 }  // namespace opt
