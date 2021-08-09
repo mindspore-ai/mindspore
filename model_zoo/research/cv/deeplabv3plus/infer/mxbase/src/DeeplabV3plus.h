16052400ea59b12c3f484f1bde8d3f48004da5e6
Boyka_Occam <zhangguizong123@sina.com>
2021-08-09 13:11:38 +0000

    update model_zoo/research/cv/deeplabv3plus/infer/mxbase/src/DeeplabV3plus.h.

--- model_zoo/research/cv/deeplabv3plus/infer/mxbase/src/DeeplabV3plus.h
+++ model_zoo/research/cv/deeplabv3plus/infer/mxbase/src/DeeplabV3plus.h
@@ -36,26 +36,26 @@ struct InitParam {
 };
 
 class DeeplabV3plus {
-public:
-    APP_ERROR Init(const InitParam &initParam);
-    APP_ERROR DeInit();
-    APP_ERROR ReadImage(const std::string &imgPath, cv::Mat &imageMat);
-    APP_ERROR ResizeImage(const cv::Mat &srcImageMat, cv::Mat &dstImageMat, MxBase::ResizedImageInfo &resizedImageInfo);
-    APP_ERROR Normalize(const cv::Mat &srcImageMat, cv::Mat &dstImageMat);
-    APP_ERROR Padding(const cv::Mat &srcImageMat, cv::Mat &dstImageMat);
-    APP_ERROR CVMatToTensorBase(const cv::Mat &imageMat, MxBase::TensorBase &tensorBase);
-    APP_ERROR Inference(const std::vector<MxBase::TensorBase> &inputs, std::vector<MxBase::TensorBase> &outputs);
-    APP_ERROR PostProcess(const std::vector<MxBase::TensorBase> &inputs,
-        std::vector<MxBase::SemanticSegInfo> &segInfo, const std::vector<MxBase::ResizedImageInfo> &resizedInfo);
-    APP_ERROR Process(const std::string &imgPath);
-    APP_ERROR SaveResultToImage(const MxBase::SemanticSegInfo &segInfo, const std::string &filePath);
+ public:
+     APP_ERROR Init(const InitParam &initParam);
+     APP_ERROR DeInit();
+     APP_ERROR ReadImage(const std::string &imgPath, cv::Mat &imageMat);
+     APP_ERROR ResizeImage(const cv::Mat &srcImageMat, cv::Mat &dstImageMat, MxBase::ResizedImageInfo &resizedImageInfo);
+     APP_ERROR Normalize(const cv::Mat &srcImageMat, cv::Mat &dstImageMat);
+     APP_ERROR Padding(const cv::Mat &srcImageMat, cv::Mat &dstImageMat);
+     APP_ERROR CVMatToTensorBase(const cv::Mat &imageMat, MxBase::TensorBase &tensorBase);
+     APP_ERROR Inference(const std::vector<MxBase::TensorBase> &inputs, std::vector<MxBase::TensorBase> &outputs);
+     APP_ERROR PostProcess(const std::vector<MxBase::TensorBase> &inputs,
+         std::vector<MxBase::SemanticSegInfo> &segInfo, const std::vector<MxBase::ResizedImageInfo> &resizedInfo);
+     APP_ERROR Process(const std::string &imgPath);
+     APP_ERROR SaveResultToImage(const MxBase::SemanticSegInfo &segInfo, const std::string &filePath);
 
-private:
-    std::shared_ptr<MxBase::DvppWrapper> dvppWrapper_;
-    std::shared_ptr<MxBase::ModelInferenceProcessor> model_;
-    std::shared_ptr<MxBase::Deeplabv3Post> post_;
-    MxBase::ModelDesc modelDesc_;
-    uint32_t deviceId_ = 0;
+ private:
+     std::shared_ptr<MxBase::DvppWrapper> dvppWrapper_;
+     std::shared_ptr<MxBase::ModelInferenceProcessor> model_;
+     std::shared_ptr<MxBase::Deeplabv3Post> post_;
+     MxBase::ModelDesc modelDesc_;
+     uint32_t deviceId_ = 0;
 };
 
 #endif
