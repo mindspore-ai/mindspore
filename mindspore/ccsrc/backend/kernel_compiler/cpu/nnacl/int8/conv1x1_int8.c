27d04c9a494fc1dd018914c8730d66dd17bea69c
zhaozhenlong <zhaozhenlong1@huawei.com>
2021-08-10 10:09:37 +0800

    fix memleak and declare const pointer

--- mindspore/ccsrc/backend/kernel_compiler/cpu/nnacl/int8/conv1x1_int8.c
+++ mindspore/ccsrc/backend/kernel_compiler/cpu/nnacl/int8/conv1x1_int8.c
@@ -18,7 +18,8 @@
 
 void Conv1x1Int8Opt(const int8_t *packed_input, const int8_t *packed_weight, int8_t *dst, const int32_t *input_sum,
                     const int32_t *bias, int row, int col, int deep4, int32_t *left_shift, int32_t *right_shift,
-                    int32_t *multiplier, ConvParameter *conv_param, MATMUL_OPT_DP_FUNC matmul_func, int *filter_zp) {
+                    int32_t *multiplier, ConvParameter *conv_param, MATMUL_OPT_DP_FUNC matmul_func,
+                    const int *filter_zp) {
   int is_per_oc = (int)conv_param->conv_quant_arg_.filter_arg_num_ != 1;
   matmul_func(packed_input, packed_weight, dst, row, col, deep4, conv_param->output_channel_, input_sum, bias,
               left_shift, right_shift, multiplier, conv_param->conv_quant_arg_.output_quant_args_[0].zp_,
@@ -29,7 +30,7 @@ void Conv1x1Int8Opt(const int8_t *packed_input, const int8_t *packed_weight, int
 
 void Conv1x1Int8(const int8_t *packed_input, const int8_t *packed_weight, int8_t *dst, const int32_t *input_sum,
                  const int32_t *bias, int row, int col, int deep16, int32_t *left_shift, int32_t *right_shift,
-                 int32_t *multiplier, ConvParameter *conv_param, int32_t *filter_zp) {
+                 int32_t *multiplier, ConvParameter *conv_param, const int32_t *filter_zp) {
   int is_per_oc = (int)conv_param->conv_quant_arg_.filter_arg_num_ != 1;
   MatmulInt8Opt(packed_input, packed_weight, dst, row, col, deep16, input_sum, bias,
                 conv_param->conv_quant_arg_.out_act_min_[0], conv_param->conv_quant_arg_.out_act_max_[0],
