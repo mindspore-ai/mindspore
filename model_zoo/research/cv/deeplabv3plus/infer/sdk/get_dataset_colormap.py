a3260ac7cdd540252fe531b6964349b0adf11fc3
Boyka_Occam <zhangguizong123@sina.com>
2021-08-10 01:08:13 +0000

    update model_zoo/research/cv/deeplabv3plus/infer/sdk/get_dataset_colormap.py.

--- model_zoo/research/cv/deeplabv3plus/infer/sdk/get_dataset_colormap.py
+++ model_zoo/research/cv/deeplabv3plus/infer/sdk/get_dataset_colormap.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
+"""generate the colormap"""
 from six.moves import range
 import numpy as np
 
