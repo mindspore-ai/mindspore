49ab087f154d773ddc9f8ff1c870b2207ffc8d14
Boyka_Occam <zhangguizong123@sina.com>
2021-08-10 01:10:11 +0000

    update model_zoo/research/cv/deeplabv3plus/infer/sdk/main_deeplabv3plus_mindspore.py.

--- model_zoo/research/cv/deeplabv3plus/infer/sdk/main_deeplabv3plus_mindspore.py
+++ model_zoo/research/cv/deeplabv3plus/infer/sdk/main_deeplabv3plus_mindspore.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-
+"""the main sdk infer file"""
 import argparse
 import base64
 import json
