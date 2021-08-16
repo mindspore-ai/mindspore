f9f59fcd408d014bd7e1a0563edb582234a6fb04
ling <lingqiaomin@huawei.com>
2021-08-16 10:45:10 +0800

    [MSLITE] npu innerContext comment bug

--- mindspore/ccsrc/backend/kernel_compiler/cpu/nnacl/base/split_with_over_lap_base.h
+++ mindspore/ccsrc/backend/kernel_compiler/cpu/nnacl/base/split_with_over_lap_base.h
@@ -23,7 +23,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-int DoSplitWithOverlapParallel(char *in_data, char **out_data, int slice_idx, SplitWithOverlapParameter *param,
+int DoSplitWithOverlapParallel(char *in_data, char **out_data, int slice_idx, const SplitWithOverlapParameter *param,
                                const int *start_indices, const int *end_indices);
 #ifdef __cplusplus
 }
