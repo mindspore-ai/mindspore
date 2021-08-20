291e5cbe5e7ca7688b0746810c1fd29c58dfbfec
lzk <liuzhongkai2@huawei.com>
2021-08-20 00:28:53 -0700

    NC4HW4 instance

--- mindspore/ccsrc/backend/kernel_compiler/cpu/nnacl/fp32/instance_norm_fp32.c
+++ mindspore/ccsrc/backend/kernel_compiler/cpu/nnacl/fp32/instance_norm_fp32.c
@@ -23,7 +23,7 @@ int InstanceNorm(const float *src_data, float *dst_data, const float *gamma_data
                  const InstanceNormParameter *param, size_t task_id) {
   NNACL_CHECK_NULL_RETURN_ERR(src_data);
   NNACL_CHECK_NULL_RETURN_ERR(dst_data);
-  NNACL_CHECK_NULL_RETURN_ERR(param->op_parameter_.thread_num_)
+  NNACL_CHECK_ZERO_RETURN_ERR(param->op_parameter_.thread_num_)
   int channel_step = UP_DIV(param->channel_, param->op_parameter_.thread_num_);
   int channel_begin = (int)(task_id)*channel_step;
   int channel_end = MSMIN(channel_begin + channel_step, param->channel_);
@@ -116,14 +116,13 @@ int InstanceNormNC4HW4(const float *src_data, float *dst_data, const float *gamm
                        const InstanceNormParameter *param, size_t task_id) {
   NNACL_CHECK_NULL_RETURN_ERR(src_data);
   NNACL_CHECK_NULL_RETURN_ERR(dst_data);
-  NNACL_CHECK_NULL_RETURN_ERR(param->op_parameter_.thread_num_);
+  NNACL_CHECK_ZERO_RETURN_ERR(param->op_parameter_.thread_num_);
   int channel = param->channel_;
   int hw_plane = param->inner_size_;
   int channel_step = UP_DIV(UP_DIV(channel, C4NUM), param->op_parameter_.thread_num_) * C4NUM;
   int channel_begin = (int)(task_id)*channel_step;
   int channel_end = MSMIN(channel_begin + channel_step, channel);
 #if defined(ENABLE_SSE) || defined(ENABLE_ARM)
-  int c4_down = channel_end / C4NUM * C4NUM;
   MS_FLOAT32X4 hw_planev = MS_MOVQ_F32((float)(hw_plane));
 #endif
   for (int b = 0; b < param->batch_; b++) {
@@ -131,7 +130,135 @@ int InstanceNormNC4HW4(const float *src_data, float *dst_data, const float *gamm
     float *dst_b = dst_data + b * channel * hw_plane;
     int c = channel_begin;
 #if defined(ENABLE_ARM) || defined(ENABLE_SSE)
-    for (; c < c4_down; c += C4NUM) {
+    for (; c <= channel_end - C16NUM; c += C16NUM) {
+      const float *src = src_b + c * hw_plane;
+      const float *src1 = src_b + (c + C4NUM) * hw_plane;
+      const float *src2 = src_b + (c + C8NUM) * hw_plane;
+      const float *src3 = src_b + (c + C12NUM) * hw_plane;
+      float *dst = dst_b + c;
+      MS_FLOAT32X4 mean = MS_MOVQ_F32(0.0f);
+      MS_FLOAT32X4 mean1 = MS_MOVQ_F32(0.0f);
+      MS_FLOAT32X4 mean2 = MS_MOVQ_F32(0.0f);
+      MS_FLOAT32X4 mean3 = MS_MOVQ_F32(0.0f);
+      MS_FLOAT32X4 square_mean = MS_MOVQ_F32(0.0f);
+      MS_FLOAT32X4 square_mean1 = MS_MOVQ_F32(0.0f);
+      MS_FLOAT32X4 square_mean2 = MS_MOVQ_F32(0.0f);
+      MS_FLOAT32X4 square_mean3 = MS_MOVQ_F32(0.0f);
+      for (int index = 0; index < hw_plane; ++index) {
+        MS_FLOAT32X4 srcv = MS_LDQ_F32(src + index * C4NUM);
+        MS_FLOAT32X4 srcv1 = MS_LDQ_F32(src1 + index * C4NUM);
+        MS_FLOAT32X4 srcv2 = MS_LDQ_F32(src2 + index * C4NUM);
+        MS_FLOAT32X4 srcv3 = MS_LDQ_F32(src3 + index * C4NUM);
+        MS_FLOAT32X4 squarev = MS_MULQ_F32(srcv, srcv);
+        MS_FLOAT32X4 squarev1 = MS_MULQ_F32(srcv1, srcv1);
+        MS_FLOAT32X4 squarev2 = MS_MULQ_F32(srcv2, srcv2);
+        MS_FLOAT32X4 squarev3 = MS_MULQ_F32(srcv3, srcv3);
+        mean = MS_ADDQ_F32(mean, srcv);
+        mean1 = MS_ADDQ_F32(mean1, srcv1);
+        mean2 = MS_ADDQ_F32(mean2, srcv2);
+        mean3 = MS_ADDQ_F32(mean3, srcv3);
+        square_mean = MS_ADDQ_F32(square_mean, squarev);
+        square_mean1 = MS_ADDQ_F32(square_mean1, squarev1);
+        square_mean2 = MS_ADDQ_F32(square_mean2, squarev2);
+        square_mean3 = MS_ADDQ_F32(square_mean3, squarev3);
+      }
+      mean = MS_DIVQ_F32(mean, hw_planev);
+      mean1 = MS_DIVQ_F32(mean1, hw_planev);
+      mean2 = MS_DIVQ_F32(mean2, hw_planev);
+      mean3 = MS_DIVQ_F32(mean3, hw_planev);
+      square_mean = MS_DIVQ_F32(square_mean, hw_planev);
+      square_mean1 = MS_DIVQ_F32(square_mean1, hw_planev);
+      square_mean2 = MS_DIVQ_F32(square_mean2, hw_planev);
+      square_mean3 = MS_DIVQ_F32(square_mean3, hw_planev);
+      MS_FLOAT32X4 deno = MS_ADDQ_F32(MS_SUBQ_F32(square_mean, MS_MULQ_F32(mean, mean)), MS_MOVQ_F32(param->epsilon_));
+      MS_FLOAT32X4 deno1 =
+        MS_ADDQ_F32(MS_SUBQ_F32(square_mean1, MS_MULQ_F32(mean1, mean1)), MS_MOVQ_F32(param->epsilon_));
+      MS_FLOAT32X4 deno2 =
+        MS_ADDQ_F32(MS_SUBQ_F32(square_mean2, MS_MULQ_F32(mean2, mean2)), MS_MOVQ_F32(param->epsilon_));
+      MS_FLOAT32X4 deno3 =
+        MS_ADDQ_F32(MS_SUBQ_F32(square_mean3, MS_MULQ_F32(mean3, mean3)), MS_MOVQ_F32(param->epsilon_));
+      deno = MS_DIVQ_F32(MS_MOVQ_F32(1.0f), MS_SQRTFX4_F32(deno));
+      deno1 = MS_DIVQ_F32(MS_MOVQ_F32(1.0f), MS_SQRTFX4_F32(deno1));
+      deno2 = MS_DIVQ_F32(MS_MOVQ_F32(1.0f), MS_SQRTFX4_F32(deno2));
+      deno3 = MS_DIVQ_F32(MS_MOVQ_F32(1.0f), MS_SQRTFX4_F32(deno3));
+
+      MS_FLOAT32X4 gammav = MS_MULQ_F32(MS_LDQ_F32(gamma_data + c), deno);             // deno * gamma_data[c]
+      MS_FLOAT32X4 gammav1 = MS_MULQ_F32(MS_LDQ_F32(gamma_data + c + C4NUM), deno1);   // deno * gamma_data[c]
+      MS_FLOAT32X4 gammav2 = MS_MULQ_F32(MS_LDQ_F32(gamma_data + c + C8NUM), deno2);   // deno * gamma_data[c]
+      MS_FLOAT32X4 gammav3 = MS_MULQ_F32(MS_LDQ_F32(gamma_data + c + C12NUM), deno3);  // deno * gamma_data[c]
+      MS_FLOAT32X4 betav = MS_LDQ_F32(beta_data + c);
+      MS_FLOAT32X4 betav1 = MS_LDQ_F32(beta_data + c + C4NUM);
+      MS_FLOAT32X4 betav2 = MS_LDQ_F32(beta_data + c + C8NUM);
+      MS_FLOAT32X4 betav3 = MS_LDQ_F32(beta_data + c + C12NUM);
+      for (int index = 0; index < hw_plane; ++index) {
+        MS_FLOAT32X4 srcv = MS_LDQ_F32(src + index * C4NUM);
+        MS_FLOAT32X4 srcv1 = MS_LDQ_F32(src1 + index * C4NUM);
+        MS_FLOAT32X4 srcv2 = MS_LDQ_F32(src2 + index * C4NUM);
+        MS_FLOAT32X4 srcv3 = MS_LDQ_F32(src3 + index * C4NUM);
+        MS_FLOAT32X4 outv = MS_SUBQ_F32(srcv, mean);
+        MS_FLOAT32X4 outv1 = MS_SUBQ_F32(srcv1, mean1);
+        MS_FLOAT32X4 outv2 = MS_SUBQ_F32(srcv2, mean2);
+        MS_FLOAT32X4 outv3 = MS_SUBQ_F32(srcv3, mean3);
+        outv = MS_MULQ_F32(outv, gammav);
+        outv1 = MS_MULQ_F32(outv1, gammav1);
+        outv2 = MS_MULQ_F32(outv2, gammav2);
+        outv3 = MS_MULQ_F32(outv3, gammav3);
+        outv = MS_ADDQ_F32(outv, betav);
+        outv1 = MS_ADDQ_F32(outv1, betav1);
+        outv2 = MS_ADDQ_F32(outv2, betav2);
+        outv3 = MS_ADDQ_F32(outv3, betav3);
+        MS_STQ_F32(dst + index * channel, outv);
+        MS_STQ_F32(dst + index * channel + C4NUM, outv1);
+        MS_STQ_F32(dst + index * channel + C8NUM, outv2);
+        MS_STQ_F32(dst + index * channel + C12NUM, outv3);
+      }
+    }
+    for (; c <= channel_end - C8NUM; c += C8NUM) {
+      const float *src = src_b + c * hw_plane;
+      const float *src1 = src_b + (c + C4NUM) * hw_plane;
+      float *dst = dst_b + c;
+      MS_FLOAT32X4 mean = MS_MOVQ_F32(0.0f);
+      MS_FLOAT32X4 mean1 = MS_MOVQ_F32(0.0f);
+      MS_FLOAT32X4 square_mean = MS_MOVQ_F32(0.0f);
+      MS_FLOAT32X4 square_mean1 = MS_MOVQ_F32(0.0f);
+      for (int index = 0; index < hw_plane; ++index) {
+        MS_FLOAT32X4 srcv = MS_LDQ_F32(src + index * C4NUM);
+        MS_FLOAT32X4 srcv1 = MS_LDQ_F32(src1 + index * C4NUM);
+        MS_FLOAT32X4 squarev = MS_MULQ_F32(srcv, srcv);
+        MS_FLOAT32X4 squarev1 = MS_MULQ_F32(srcv1, srcv1);
+        mean = MS_ADDQ_F32(mean, srcv);
+        mean1 = MS_ADDQ_F32(mean1, srcv1);
+        square_mean = MS_ADDQ_F32(square_mean, squarev);
+        square_mean1 = MS_ADDQ_F32(square_mean1, squarev1);
+      }
+      mean = MS_DIVQ_F32(mean, hw_planev);
+      mean1 = MS_DIVQ_F32(mean1, hw_planev);
+      square_mean = MS_DIVQ_F32(square_mean, hw_planev);
+      square_mean1 = MS_DIVQ_F32(square_mean1, hw_planev);
+      MS_FLOAT32X4 deno = MS_ADDQ_F32(MS_SUBQ_F32(square_mean, MS_MULQ_F32(mean, mean)), MS_MOVQ_F32(param->epsilon_));
+      MS_FLOAT32X4 deno1 =
+        MS_ADDQ_F32(MS_SUBQ_F32(square_mean1, MS_MULQ_F32(mean1, mean1)), MS_MOVQ_F32(param->epsilon_));
+      deno = MS_DIVQ_F32(MS_MOVQ_F32(1.0f), MS_SQRTFX4_F32(deno));
+      deno1 = MS_DIVQ_F32(MS_MOVQ_F32(1.0f), MS_SQRTFX4_F32(deno1));
+
+      MS_FLOAT32X4 gammav = MS_MULQ_F32(MS_LDQ_F32(gamma_data + c), deno);            // deno * gamma_data[c]
+      MS_FLOAT32X4 gammav1 = MS_MULQ_F32(MS_LDQ_F32(gamma_data + c + C4NUM), deno1);  // deno * gamma_data[c]
+      MS_FLOAT32X4 betav = MS_LDQ_F32(beta_data + c);
+      MS_FLOAT32X4 betav1 = MS_LDQ_F32(beta_data + c + C4NUM);
+      for (int index = 0; index < hw_plane; ++index) {
+        MS_FLOAT32X4 srcv = MS_LDQ_F32(src + index * C4NUM);
+        MS_FLOAT32X4 srcv1 = MS_LDQ_F32(src1 + index * C4NUM);
+        MS_FLOAT32X4 outv = MS_SUBQ_F32(srcv, mean);
+        MS_FLOAT32X4 outv1 = MS_SUBQ_F32(srcv1, mean1);
+        outv = MS_MULQ_F32(outv, gammav);
+        outv1 = MS_MULQ_F32(outv1, gammav1);
+        outv = MS_ADDQ_F32(outv, betav);
+        outv1 = MS_ADDQ_F32(outv1, betav1);
+        MS_STQ_F32(dst + index * channel, outv);
+        MS_STQ_F32(dst + index * channel + C4NUM, outv1);
+      }
+    }
+    for (; c <= channel_end - C4NUM; c += C4NUM) {
       const float *src = src_b + c * hw_plane;
       float *dst = dst_b + c;
       MS_FLOAT32X4 mean = MS_MOVQ_F32(0.0f);
