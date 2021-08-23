b80171012f3d6cc1bf68e46f65ac5be428512d51
Boyka_Occam <zhangguizong123@sina.com>
2021-08-23 14:17:37 +0800

    删除冗余代码

--- model_zoo/official/cv/yolov4_tiny/eval.py
+++ model_zoo/official/cv/yolov4_tiny/eval.py
@@ -27,66 +27,11 @@ from src.yolo_dataset import create_yolo_dataset
 from src.eval_utils import apply_eval
 
 from model_utils.config import config
-from model_utils.moxing_adapter import moxing_wrapper
-from model_utils.device_adapter import get_device_id, get_device_num
 
 config.data_root = os.path.join(config.data_dir, 'val2017')
 config.ann_val_file = os.path.join(config.data_dir, 'annotations/instances_val2017.json')
 
-def modelarts_pre_process():
-    '''modelarts pre process function.'''
-    def unzip(zip_file, save_dir):
-        import zipfile
-        s_time = time.time()
-        if not os.path.exists(os.path.join(save_dir, config.modelarts_dataset_unzip_name)):
-            zip_isexist = zipfile.is_zipfile(zip_file)
-            if zip_isexist:
-                fz = zipfile.ZipFile(zip_file, 'r')
-                data_num = len(fz.namelist())
-                print("Extract Start...")
-                print("unzip file num: {}".format(data_num))
-                data_print = int(data_num / 100) if data_num > 100 else 1
-                i = 0
-                for file in fz.namelist():
-                    if i % data_print == 0:
-                        print("unzip percent: {}%".format(int(i * 100 / data_num)), flush=True)
-                    i += 1
-                    fz.extract(file, save_dir)
-                print("cost time: {}min:{}s.".format(int((time.time() - s_time) / 60),
-                                                     int(int(time.time() - s_time) % 60)))
-                print("Extract Done.")
-            else:
-                print("This is not zip.")
-        else:
-            print("Zip has been extracted.")
 
-    if config.need_modelarts_dataset_unzip:
-        zip_file_1 = os.path.join(config.data_path, config.modelarts_dataset_unzip_name + ".zip")
-        save_dir_1 = os.path.join(config.data_path)
-
-        sync_lock = "/tmp/unzip_sync.lock"
-
-        # Each server contains 8 devices as most.
-        if get_device_id() % min(get_device_num(), 8) == 0 and not os.path.exists(sync_lock):
-            print("Zip file path: ", zip_file_1)
-            print("Unzip file save dir: ", save_dir_1)
-            unzip(zip_file_1, save_dir_1)
-            print("===Finish extract data synchronization===")
-            try:
-                os.mknod(sync_lock)
-            except IOError:
-                pass
-
-        while True:
-            if os.path.exists(sync_lock):
-                break
-            time.sleep(1)
-
-        print("Device: {}, Finish sync unzip data from {} to {}.".format(get_device_id(), zip_file_1, save_dir_1))
-
-    config.log_path = os.path.join(config.output_path, config.log_path)
-
-@moxing_wrapper(pre_process=modelarts_pre_process)
 def run_eval():
     start_time = time.time()
     device_id = int(os.getenv('DEVICE_ID')) if os.getenv('DEVICE_ID') else 0
