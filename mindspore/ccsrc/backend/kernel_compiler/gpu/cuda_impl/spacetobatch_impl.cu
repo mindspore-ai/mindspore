1639ce42afb917fa168b0a22e70e313ce31a8bab
zong_shuai <zongshuai1@huawei.com>
2021-08-24 17:24:28 +0800

    spacetobatch

--- mindspore/ccsrc/backend/kernel_compiler/gpu/cuda_impl/spacetobatch_impl.cu
+++ mindspore/ccsrc/backend/kernel_compiler/gpu/cuda_impl/spacetobatch_impl.cu
@@ -52,8 +52,8 @@ __global__ void SpaceToBatch(const size_t size, const T *input, const size_t in,
     idx_on = (((idx_ih + pad_up) % block_num) * block_num + ((idx_iw + pad_lft) % block_num)) * in + idx_in;
     input_pos = idx_on * oc;
     input_pos = (input_pos + idx_ic) * oh;
-    input_pos = (input_pos + ((idx_oh + pad_up) - (idx_on / (on * block_num))) / block_num) * ow;
-    input_pos = (input_pos + ((idx_ow + pad_lft) - ((idx_on / on) % block_num)) / block_num);
+    input_pos = (input_pos + ((idx_ih + pad_up) - (idx_on / (on * block_num))) / block_num) * ow;
+    input_pos = (input_pos + ((idx_iw + pad_lft) - ((idx_on / on) % block_num)) / block_num);
     output[input_pos] = input[pos];
   }
   return;
