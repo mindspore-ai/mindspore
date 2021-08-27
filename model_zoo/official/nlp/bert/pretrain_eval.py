4c302c0c5bbc2bc26270a6b2d440fae075c9c455
sl_wang <wangshuangling1@huawei.com>
2021-08-27 18:06:35 +0800

    bert script for thor generalization

--- model_zoo/official/nlp/bert/pretrain_eval.py
+++ model_zoo/official/nlp/bert/pretrain_eval.py
@@ -150,10 +150,11 @@ def MLM_eval():
     res = net.eval(dataset, dataset_sink_mode=False)
     print("==============================================================")
     for _, v in res.items():
-        print("Accuracy is: ")
-        print(v)
+        print("Accuracy is: ", v)
     print("==============================================================")
 
 
 if __name__ == "__main__":
+    DEVICE_ID = 0
+    os.environ['DEVICE_ID'] = str(DEVICE_ID)
     MLM_eval()
