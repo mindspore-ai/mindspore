2f662a1ef256ba1cb3d07a2ab503342cad60fb1c
Dai Surong <daisurong@mail.nankai.edu.cn>
2021-08-30 20:22:55 +0800

    const test 1-th

--- model_zoo/official/cv/dpn/infer/mxbase/src/DPN.h
+++ model_zoo/official/cv/dpn/infer/mxbase/src/DPN.h
@@ -11,11 +11,12 @@
 
 #ifndef MXBASE_DPN_H
 #define MXBASE_DPN_H
+#include <memory>
+#include <string>
+#include <vector>
 #include <opencv2/opencv.hpp>
-
 #include "MxBase/DvppWrapper/DvppWrapper.h"
 #include "MxBase/ModelInfer/ModelInferenceProcessor.h"
-// #include "ClassPostProcessors/DPN92PostProcess.h"
 #include "ClassPostProcessors/Resnet50PostProcess.h"
 #include "MxBase/Tensor/TensorContext/TensorContext.h"
 
@@ -30,31 +31,33 @@ struct InitParam {
 };
 
 class DPN {
-    public:
-        APP_ERROR Init(const InitParam &initParam);
-        APP_ERROR DeInit();
-        APP_ERROR ReadImage(const std::string &imgPath, cv::Mat &imageMat);
-        APP_ERROR ResizeImage(const cv::Mat &srcImageMat, cv::Mat &dstImageMat);
-        APP_ERROR CenterCropImage(const cv::Mat &img, cv::Mat &crop_im);
-        APP_ERROR CVMatToTensorBase(const cv::Mat &imageMat, MxBase::TensorBase &tensorBase);
-        APP_ERROR Inference(const std::vector<MxBase::TensorBase> &inputs, std::vector<MxBase::TensorBase> &outputs);
-        APP_ERROR PostProcess(const std::vector<MxBase::TensorBase> &inputs,
-                              std::vector<std::vector<MxBase::ClassInfo>> &clsInfos);
-        APP_ERROR Process(const std::vector<std::string> &batchImgPaths);
-        // get infer time
-        double GetInferCostMilliSec() const {return inferCostTimeMilliSec;}
+ public:
+  APP_ERROR Init(const InitParam &initParam);
+  APP_ERROR DeInit();
+  APP_ERROR ReadImage(const std::string &imgPath, cv::Mat &imageMat);
+  APP_ERROR ResizeImage(const cv::Mat &srcImageMat, cv::Mat &dstImageMat);
+  APP_ERROR CenterCropImage(const cv::Mat &img, cv::Mat &crop_im);
+  APP_ERROR CVMatToTensorBase(const cv::Mat &imageMat, MxBase::TensorBase &tensorBase);
+  APP_ERROR Inference(const std::vector<MxBase::TensorBase> &inputs, std::vector<MxBase::TensorBase> &outputs);
+  APP_ERROR PostProcess(const std::vector<MxBase::TensorBase> &inputs,
+                          std::vector<std::vector<MxBase::ClassInfo>> &clsInfos);
+  APP_ERROR Process(const std::vector<std::string> &batchImgPaths);
+  // get infer time
+  double GetInferCostMilliSec() const {return inferCostTimeMilliSec;}
 
-    private:
-        APP_ERROR SaveResult(const std::vector<std::string> &batchImgPaths, const std::vector<std::vector<MxBase::ClassInfo>> &batchClsInfos);
-    private:
-        std::shared_ptr<MxBase::DvppWrapper> dvppWrapper_;
-        std::shared_ptr<MxBase::ModelInferenceProcessor> model_;
-        std::shared_ptr<MxBase::Resnet50PostProcess> post_;
-        MxBase::ModelDesc modelDesc_;
-        uint32_t deviceId_ = 0;
-        // infer time
-        double inferCostTimeMilliSec = 0.0;
+ private:
+  APP_ERROR SaveResult(const std::vector<std::string> &batchImgPaths,
+                                const std::vector<std::vector<MxBase::ClassInfo>> &batchClsInfos);
+
+ private:
+  std::shared_ptr<MxBase::DvppWrapper> dvppWrapper_;
+  std::shared_ptr<MxBase::ModelInferenceProcessor> model_;
+  std::shared_ptr<MxBase::Resnet50PostProcess> post_;
+  MxBase::ModelDesc modelDesc_;
+  uint32_t deviceId_ = 0;
+  // infer time
+  double inferCostTimeMilliSec = 0.0;
 };
 
-
 #endif
+
