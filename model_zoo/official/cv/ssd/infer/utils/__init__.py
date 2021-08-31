43151aee65e64966d1b68d07383c3867e2a0f23f
Atlas_hrp <haoripei@huawei.com>
2021-08-31 15:59:16 +0800

    [模型训练] 支持r1.3分支resnet50代码修改 [修改人] haoripei [审核人] chenshushu

--- model_zoo/official/cv/ssd/infer/utils/__init__.py
+++ model_zoo/official/cv/ssd/infer/utils/__init__.py
@@ -1,6 +1,6 @@
 # coding: UTF-8
 
-# Copyright (C) 2020.Huawei Technologies Co., Ltd. All rights reserved.
+# Copyright (C) 2021.Huawei Technologies Co., Ltd. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
