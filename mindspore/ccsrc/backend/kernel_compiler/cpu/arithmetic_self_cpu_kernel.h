a1b9e33a93d79857d15fcdb30cbe9440900142a0
fan-jibin <fanjibin@huawei.com>
2021-09-06 17:03:52 +0800

    fix exp precision

--- mindspore/ccsrc/backend/kernel_compiler/cpu/arithmetic_self_cpu_kernel.h
+++ mindspore/ccsrc/backend/kernel_compiler/cpu/arithmetic_self_cpu_kernel.h
@@ -22,7 +22,6 @@
 
 const float MAX_NEG_SERIAL_SIZE = 5000;
 const float MAX_SQUARE_SERIAL_SIZE = 5000;
-const float MAX_EXP_SERIAL_SIZE = 15000;
 
 namespace mindspore {
 namespace kernel {
@@ -60,8 +59,6 @@ class IdentityCPUKernel : public ArithmeticSelfCPUKernel {
 MS_REG_CPU_KERNEL(Square, KernelAttr().AddInputAttr(kNumberTypeInt32).AddOutputAttr(kNumberTypeInt32),
                   ArithmeticSelfCPUKernel);
 MS_REG_CPU_KERNEL(Square, KernelAttr().AddInputAttr(kNumberTypeFloat32).AddOutputAttr(kNumberTypeFloat32),
-                  ArithmeticSelfCPUKernel);
-MS_REG_CPU_KERNEL(Exp, KernelAttr().AddInputAttr(kNumberTypeFloat32).AddOutputAttr(kNumberTypeFloat32),
                   ArithmeticSelfCPUKernel);
 MS_REG_CPU_KERNEL(Neg, KernelAttr().AddInputAttr(kNumberTypeFloat32).AddOutputAttr(kNumberTypeFloat32),
                   ArithmeticSelfCPUKernel);
