d2d442a21fe32cf960d1e44282392bbceb52171e
ms_yan <yanpanhui@huawei.com>
2021-09-15 09:29:23 +0800

    modify description of cache

--- mindspore/dataset/__init__.py
+++ mindspore/dataset/__init__.py
@@ -19,8 +19,9 @@ their own datasets with this module.
 
 Besides, this module provides APIs to sample data while loading.
 
-Please notice that cache is not supported on Windows platform yet. Please do not use it
-while loading and processing data on Windows.
+We can enable cache in most of the dataset with its key arguments 'cache'. Please notice that cache is not supported
+on Windows platform yet. Do not use it while loading and processing data on Windows. More introductions and limitations
+can refer `Single-Node Tensor Cache <https://www.mindspore.cn/docs/programming_guide/en/master/cache.html>`_.
 """
 
 from .core import config
