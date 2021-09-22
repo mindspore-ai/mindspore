df5c80f94d0923f1c54b451ac27f0b7c3b329cec
yao_yf <yaoyifan1@huawei.com>
2021-09-22 09:16:03 +0800

    cpu realdiv support fp16

--- mindspore/ops/_op_impl/cpu/real_div.py
+++ mindspore/ops/_op_impl/cpu/real_div.py
@@ -21,6 +21,7 @@ real_div_op_info = CpuRegOp("RealDiv") \
     .output(0, "output", "required") \
     .dtype_format(DataType.I32_Default, DataType.I32_Default, DataType.I32_Default) \
     .dtype_format(DataType.F32_Default, DataType.F32_Default, DataType.F32_Default) \
+    .dtype_format(DataType.F16_Default, DataType.F16_Default, DataType.F16_Default) \
     .dtype_format(DataType.I64_Default, DataType.I64_Default, DataType.I64_Default) \
     .get_op_info()
 
