81aab2e5e09f5c9e1b1b4e4a7f0391da6ca88cb3
xuanyue <xuanyue@huawei.com>
2021-09-23 19:33:18 +0800

    do not open flatbuffer and protobuf

--- mindspore/lite/tools/providers/NNIE/Hi3516D/compile_nnie.sh
+++ mindspore/lite/tools/providers/NNIE/Hi3516D/compile_nnie.sh
@@ -39,8 +39,6 @@ function Run_Build_x86() {
 
     cd ${open_source_ms_path}/output/ || exit 1
     # remove unused static library
-    rm ./mindspore-lite-${version}-linux-x64/tools/converter/lib/libprotobuf.a
-    rm ./mindspore-lite-${version}-linux-x64/tools/converter/lib/libflatbuffers.a
     echo "cp new nnie so to release pkg success"
     rm ${open_source_ms_path}/output/mindspore-lite-${version}-linux-x64.tar.gz
     tar -zcf ./mindspore-lite-${version}-linux-x64.tar.gz ./mindspore-lite-${version}-linux-x64/
