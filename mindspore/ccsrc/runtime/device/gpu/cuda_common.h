f2f53ba51c9a583da209e9849f7f22c562c64b16
VectorSL <shiliang10@huawei.com>
2021-10-08 10:50:02 +0800

    add warning for cuda capability is lower than 5.3

--- mindspore/ccsrc/runtime/device/gpu/cuda_common.h
+++ mindspore/ccsrc/runtime/device/gpu/cuda_common.h
@@ -31,6 +31,7 @@ class CudaCommon {
  public:
   inline int threads_num() const { return threads_per_block_; }
   inline int major_sm() const { return major_sm_; }
+  inline float cuda_cap() const { return static_cast<float>(major_sm_ * 10 + minor_sm_) / 10.0; }
   inline int blocks_num(const int total_threads) const {
     return std::min(((total_threads - 1) / threads_per_block_) + 1, max_blocks_);
   }
@@ -51,6 +52,7 @@ class CudaCommon {
     threads_per_block_ = prop.maxThreadsPerBlock;
     max_blocks_ = prop.multiProcessorCount;
     major_sm_ = prop.major;
+    minor_sm_ = prop.minor;
     max_share_memory_ = prop.sharedMemPerBlock;
   }
   ~CudaCommon() = default;
@@ -60,15 +62,18 @@ class CudaCommon {
   int max_blocks_;
   int threads_per_block_;
   int major_sm_;
+  int minor_sm_;
   size_t max_share_memory_;
   bool check_sm_{true};
 };
 #define GET_BLOCKS(total_threads) mindspore::device::gpu::CudaCommon::GetInstance().blocks_num(total_threads)
 #define GET_THREADS mindspore::device::gpu::CudaCommon::GetInstance().threads_num()
 #define GET_MAJOR_SM mindspore::device::gpu::CudaCommon::GetInstance().major_sm()
+#define GET_CUDA_CAP mindspore::device::gpu::CudaCommon::GetInstance().cuda_cap()
 #define SHARED_MEM_PER_BLOCK mindspore::device::gpu::CudaCommon::GetInstance().share_memory_size()
 #define MINIUM_SM 6
 #define RECOMMEND_SM 7
+#define SUPPORTED_CAP 5.3
 }  // namespace gpu
 }  // namespace device
 }  // namespace mindspore
