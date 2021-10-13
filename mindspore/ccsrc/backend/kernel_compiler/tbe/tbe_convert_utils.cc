5fcace9a01088737815961df338ab95e47f53a0a
hwjiaorui <jiaorui3@huawei.com>
2021-10-13 15:32:26 +0800

    Revert "clean code"
    This reverts commit 54237f077927477809fa86f3d94da22dfd9423a3.

--- mindspore/ccsrc/backend/kernel_compiler/tbe/tbe_convert_utils.cc
+++ mindspore/ccsrc/backend/kernel_compiler/tbe/tbe_convert_utils.cc
@@ -22,11 +22,53 @@
 
 #include "backend/session/anf_runtime_algorithm.h"
 #include "utils/ms_utils.h"
-#include "backend/kernel_compiler/common_utils.h"
 
 namespace mindspore {
 namespace kernel {
 namespace tbe {
+const std::unordered_map<std::string, TypeId> type_str_id_maps = {
+  {"float", TypeId::kNumberTypeFloat32},
+  {"float16", TypeId::kNumberTypeFloat16},
+  {"float32", TypeId::kNumberTypeFloat32},
+  {"float64", TypeId::kNumberTypeFloat64},
+  {"int", TypeId::kNumberTypeInt},
+  {"int8", TypeId::kNumberTypeInt8},
+  {"int16", TypeId::kNumberTypeInt16},
+  {"int32", TypeId::kNumberTypeInt32},
+  {"int64", TypeId::kNumberTypeInt64},
+  {"uint", TypeId::kNumberTypeUInt},
+  {"uint8", TypeId::kNumberTypeUInt8},
+  {"uint16", TypeId::kNumberTypeUInt16},
+  {"uint32", TypeId::kNumberTypeUInt32},
+  {"uint64", TypeId::kNumberTypeUInt64},
+  {"bool", TypeId::kNumberTypeBool},
+  {"int4", TypeId::kNumberTypeInt4},
+  {"complex64", TypeId::kNumberTypeComplex64},
+  {"complex128", TypeId::kNumberTypeComplex128},
+  {"", TypeId::kMetaTypeNone},
+};
+
+const std::map<TypeId, std::string> type_id_str_maps = {
+  {TypeId::kNumberTypeFloat32, "float32"},
+  {TypeId::kNumberTypeFloat16, "float16"},
+  {TypeId::kNumberTypeFloat, "float32"},
+  {TypeId::kNumberTypeFloat64, "float64"},
+  {TypeId::kNumberTypeInt, "int"},
+  {TypeId::kNumberTypeInt8, "int8"},
+  {TypeId::kNumberTypeInt16, "int16"},
+  {TypeId::kNumberTypeInt32, "int32"},
+  {TypeId::kNumberTypeInt64, "int64"},
+  {TypeId::kNumberTypeUInt, "uint"},
+  {TypeId::kNumberTypeUInt8, "uint8"},
+  {TypeId::kNumberTypeUInt16, "uint16"},
+  {TypeId::kNumberTypeUInt32, "uint32"},
+  {TypeId::kNumberTypeUInt64, "uint64"},
+  {TypeId::kNumberTypeBool, "int8"},
+  {TypeId::kNumberTypeInt4, "int4"},
+  {TypeId::kNumberTypeComplex64, "complex64"},
+  {TypeId::kNumberTypeComplex128, "complex128"},
+  {TypeId::kMetaTypeNone, ""},
+};
 
 const std::unordered_map<std::string, size_t> type_nbyte_maps = {
   {"float16", sizeof(float) / 2}, {"float32", sizeof(float)},       {"float64", sizeof(float) * 2},
@@ -37,16 +79,16 @@ const std::unordered_map<std::string, size_t> type_nbyte_maps = {
 };
 
 TypeId DtypeToTypeId(const std::string &dtypes) {
-  auto iter = type_id_maps.find(dtypes);
-  if (iter == type_id_maps.end()) {
+  auto iter = type_str_id_maps.find(dtypes);
+  if (iter == type_str_id_maps.end()) {
     MS_LOG(EXCEPTION) << "Illegal input device dtype: " << dtypes;
   }
   return iter->second;
 }
 
 std::string TypeIdToString(TypeId type_id) {
-  auto iter = type_id_str_map.find(type_id);
-  if (iter == type_id_str_map.end()) {
+  auto iter = type_id_str_maps.find(type_id);
+  if (iter == type_id_str_maps.end()) {
     MS_LOG(EXCEPTION) << "Illegal input dtype: " << TypeIdLabel(type_id);
   }
   return iter->second;
