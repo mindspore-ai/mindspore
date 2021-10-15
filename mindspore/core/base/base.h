e213f979ea537fbcf406331168396bd7d49f831a
yujianfeng <yujianfeng5@huawei.com>
2021-10-15 09:32:56 +0800

    Add comments for the core api

--- mindspore/core/base/base.h
+++ mindspore/core/base/base.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 Huawei Technologies Co., Ltd
+ * Copyright 2019-2021 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,10 +37,26 @@ struct is_shared_ptr : public std::false_type {};
 template <typename T>
 struct is_shared_ptr<std::shared_ptr<T>> : public std::true_type {};
 
+/// \brief Base is a base class of many derived classes, which provides basic interfaces such as hash, and so on.
 class MS_CORE_API Base : public std::enable_shared_from_this<Base> {
  public:
+  /// \brief The constructor of Base.
+  ///
+  /// \return The instance of Base.
   constexpr Base() = default;
+
+  /// \brief The copy constructor of Base.
+  ///
+  /// \param[in] other Define another instance of Base.
+  ///
+  /// \return The instance of Base.
   Base(const Base &other) : std::enable_shared_from_this<Base>(other) {}
+
+  /// \brief The operator overloading for "==".
+  ///
+  /// \param[in] rhs Define the right operand of "==".
+  ///
+  /// \return The comparison result.
   virtual bool operator==(const Base &rhs) {
     if (this == &rhs) {
       return true;
@@ -48,22 +64,64 @@ class MS_CORE_API Base : public std::enable_shared_from_this<Base> {
     return false;
   }
 
+  /// \brief The copy assignment operator of Base.
+  ///
+  /// \param[in] Define another instance of Base.
+  ///
+  /// \return The instance of Base.
   virtual Base &operator=(const Base &) { return *this; }
+
+  /// \brief The destructor of Base.
   virtual ~Base() = default;
+
+  /// \brief Get the hash value of this object.
+  ///
+  /// \return The hash value.
   virtual std::size_t hash() const { return tid(); }
+
+  /// \brief Get the string representation of this object.
+  ///
+  /// \return The string representation.
   virtual std::string ToString() const { return type_name(); }
+
+  /// \brief Export the string representation to the standard output stream.
   virtual void dump() const { std::cout << ToString() << std::endl; }
 
+  /// \brief Get the text representation of this object.
+  ///
+  /// \return The text representation.
   virtual std::string DumpText() const { return ToString(); }
 
+  /// \brief Judge whether a type id is the same as the type id of this object.
+  ///
+  /// \param[in] tid Define a type id.
+  ///
+  /// \return The result of the judgment.
   virtual const bool IsFromTypeId(uint32_t tid) const;
+
+  /// \brief Get the type name of this object.
+  ///
+  /// \return The type name.
   virtual std::string type_name() const { return "Base"; }
+
+  /// \brief Get a type id which is a hash value from a type name.
+  ///
+  /// \param[in] type_key Define a string for the type name.
+  ///
+  /// \return The type id.
   static uint32_t GetTypeId(const char *const type_key);
+
+  /// \brief Get the type id of this object.
+  ///
+  /// \return The type id.
   virtual uint32_t tid() const {
     static const uint32_t tid = GetTypeId(typeid(Base).name());
     return tid;
   }
 
+  /// \brief Judge whether this object is an instance of a given class which is derived from Base.
+  ///
+  /// \return The result of the judgment.
   template <typename T,
             typename std::enable_if<!is_shared_ptr<T>::value && std::is_base_of<Base, T>::value, T>::type * = nullptr>
   inline bool isa() const {
@@ -71,6 +129,9 @@ class MS_CORE_API Base : public std::enable_shared_from_this<Base> {
     return this->IsFromTypeId(tid);
   }
 
+  /// \brief Cast a shared_ptr of this object to a given class.
+  ///
+  /// \return If success, a shared_ptr of the given class will be returned. Otherwise a nullptr will be returned.
   template <typename T, typename U = typename std::enable_if<is_shared_ptr<T>::value, typename T::element_type>::type>
   inline T cast() {
     if (isa<U>()) {
@@ -81,6 +142,9 @@ class MS_CORE_API Base : public std::enable_shared_from_this<Base> {
   }
 
  protected:
+  /// \brief Get the shared_ptr of Base.
+  ///
+  /// \return The shared_ptr of Base.
   template <typename Derived>
   std::shared_ptr<Derived> shared_from_base() {
     return std::static_pointer_cast<Derived>(shared_from_this());
@@ -145,11 +209,19 @@ class AnalysisContext;
 using AnalysisContextPtr = std::shared_ptr<AnalysisContext>;
 }  // namespace abstract
 
+/// \brief TypeIdManager is a manager for saving and reading the type id.
 struct MS_EXPORT TypeIdManager {
   std::mutex mutex;
   std::atomic<uint32_t> type_counter{0};
   std::unordered_map<std::string, uint32_t> map;
+  /// \brief Get the singleton pointer of TypeIdManager.
+  ///
+  /// \return The singleton pointer of TypeIdManager.
   static TypeIdManager *Get();
+
+  /// \brief The constructor of TypeIdManager.
+  ///
+  /// \return The instance of TypeIdManager.
   TypeIdManager() : mutex(), type_counter(0), map() {}
 };
 }  // namespace mindspore
