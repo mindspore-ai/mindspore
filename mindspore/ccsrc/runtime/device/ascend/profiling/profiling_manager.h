3a7f1198cbcdc3918454a8d40b7b8edbb5fc0971
yanghaitao1 <yanghaitao1@huawei.com>
2021-10-15 04:04:45 -0400

    do not start profiling again if profiling has start

--- mindspore/ccsrc/runtime/device/ascend/profiling/profiling_manager.h
+++ mindspore/ccsrc/runtime/device/ascend/profiling/profiling_manager.h
@@ -82,6 +82,7 @@ class ProfilingManager {
   uint32_t device_id_;
   MsprofCallback prof_cb_;
   bool hccl_enabled_bef_profiling_enabled_;
+  bool has_start;
 };
 
 Status ProfCommandHandle(ProfCommandHandleType type);
