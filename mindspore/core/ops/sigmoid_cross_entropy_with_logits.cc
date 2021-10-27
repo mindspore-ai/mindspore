cb30040949e90008ce156ce98c810a4cef35f31d
bsx <2980708493@qq.com>
2021-10-27 16:46:53 +0800

    [feat][assistant][I48OBM] Modify SigmoidCrossEntropyWithLogits to be a dynamic shape operator

--- mindspore/core/ops/sigmoid_cross_entropy_with_logits.cc
+++ mindspore/core/ops/sigmoid_cross_entropy_with_logits.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Huawei Technologies Co., Ltd
+ * Copyright 2021 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,40 +15,58 @@
  */
 
 #include "ops/sigmoid_cross_entropy_with_logits.h"
-#include <string>
-#include <algorithm>
-#include <memory>
-#include <set>
-#include <vector>
 #include "ops/op_utils.h"
 #include "utils/check_convert_utils.h"
-#include "abstract/primitive_infer_map.h"
 
 namespace mindspore {
 namespace ops {
-AbstractBasePtr SigmoidCrossEntropyWithLogitsInfer(const abstract::AnalysisEnginePtr &, const PrimitivePtr &primitive,
-                                                   const std::vector<AbstractBasePtr> &input_args) {
+namespace {
+abstract::ShapePtr SigmoidCrossEntropyWithLogitsInferShape(const PrimitivePtr &primitive,
+                                                           const std::vector<AbstractBasePtr> &input_args) {
   MS_EXCEPTION_IF_NULL(primitive);
-  auto prim_name = primitive->name();
-  const int64_t input_num = 2;
-  (void)CheckAndConvertUtils::CheckInteger("sigmoid_cross_extropy_with_logits_infer", SizeToLong(input_args.size()),
-                                           kEqual, input_num, prim_name);
+  auto op_name = primitive->name();
+  MS_LOG(INFO) << "Do infer shape for op " << op_name;
+  const int64_t kInputNum = 2;
+  (void)CheckAndConvertUtils::CheckInputArgs(input_args, kGreaterEqual, kInputNum, op_name);
+  auto logits_shape = input_args[0]->BuildShape();
+  auto label_shape = input_args[1]->BuildShape();
+  auto logits_shape_ptr = logits_shape->cast<abstract::ShapePtr>();
+  auto label_shape_ptr = label_shape->cast<abstract::ShapePtr>();
+  // logits and label must have the same shape when is not dynamic
+  if (!logits_shape_ptr->IsDynamic() && !label_shape_ptr->IsDynamic()) {
+    if (*logits_shape != *label_shape) {
+      MS_EXCEPTION(ValueError) << op_name << " evaluator arg label shape " << label_shape->ToString()
+                               << " are not consistent with logits shape " << logits_shape->ToString();
+    }
+  }
+  auto logits_element = logits_shape->cast<abstract::ShapePtr>();
+  MS_EXCEPTION_IF_NULL(logits_element);
+  return logits_element;
+}
 
-  // Infer shape
-  auto x_shape = CheckAndConvertUtils::ConvertShapePtrToShapeMap(input_args[0]->BuildShape())[kShape];
-  auto y_shape = CheckAndConvertUtils::ConvertShapePtrToShapeMap(input_args[1]->BuildShape())[kShape];
-  CheckAndConvertUtils::Check("x_shape", x_shape, kEqual, "y_shape", y_shape, prim_name, TypeError);
-
-  // Infer type
+TypePtr SigmoidCrossEntropyWithLogitsInferType(const PrimitivePtr &primitive,
+                                               const std::vector<AbstractBasePtr> &input_args) {
+  MS_EXCEPTION_IF_NULL(primitive);
+  const int64_t kInputNum = 2;
+  (void)CheckAndConvertUtils::CheckInputArgs(input_args, kGreaterEqual, kInputNum, primitive->name());
+  auto logits_type = input_args[kInputIndex0]->BuildType();
+  auto label_type = input_args[kInputIndex1]->BuildType();
   const std::set<TypePtr> valid_types = {kBool,   kInt,    kInt8,   kInt16, kInt32,   kInt64,   kUInt,    kUInt8,
                                          kUInt16, kUInt32, kUInt64, kFloat, kFloat16, kFloat32, kFloat64, kComplex64};
   std::map<std::string, TypePtr> args;
-  args.emplace("x_type", input_args[0]->BuildType());
-  args.emplace("y_type", input_args[1]->BuildType());
-  auto x_type = CheckAndConvertUtils::CheckTensorTypeSame(args, valid_types, prim_name);
-
-  return std::make_shared<abstract::AbstractTensor>(x_type, x_shape);
+  (void)args.insert({"logits_type", logits_type});
+  (void)args.insert({"label_type", label_type});
+  (void)CheckAndConvertUtils::CheckTensorTypeSame(args, valid_types, primitive->name());
+  return logits_type;
 }
-REGISTER_PRIMITIVE_C(kNameSigmoidCrossEntropyWithLogits, SigmoidCrossEntropyWithLogits);
+}  // namespace
+AbstractBasePtr SigmoidCrossEntropyWithLogitsInfer(const abstract::AnalysisEnginePtr &, const PrimitivePtr &primitive,
+                                                   const std::vector<AbstractBasePtr> &input_args) {
+  auto infer_type = SigmoidCrossEntropyWithLogitsInferType(primitive, input_args);
+  auto infer_shape = SigmoidCrossEntropyWithLogitsInferShape(primitive, input_args);
+  return abstract::MakeAbstract(infer_shape, infer_type);
+}
+REGISTER_PRIMITIVE_EVAL_IMPL(SigmoidCrossEntropyWithLogits, prim::kPrimSigmoidCrossEntropyWithLogits,
+                             SigmoidCrossEntropyWithLogitsInfer, nullptr, true);
 }  // namespace ops
 }  // namespace mindspore
---
+++ mindspore/ops/_op_impl/tbe/sigmoid_cross_entropy_with_logits_ds.py
@@ -0,0 +1,42 @@
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed unde:q!r the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""SigmoidCrossEntropyWithLogits op"""
+from mindspore.ops.op_info_register import op_info_register, TBERegOp, DataType
+
+sigmoid_cross_entropy_with_logits_ds_op_info = TBERegOp("SigmoidCrossEntropyWithLogits") \
+    .fusion_type("OPAQUE") \
+    .async_flag(False) \
+    .binfile_name("sigmoid_cross_entropy_with_logits.so") \
+    .compute_cost(10) \
+    .kernel_name("sigmoid_cross_entropy_with_logits") \
+    .partial_flag(True) \
+    .dynamic_shape(True) \
+    .input(0, "predict", False, "required", "all") \
+    .input(1, "target", False, "required", "all") \
+    .output(0, "loss", False, "required", "all") \
+    .dtype_format(DataType.F16_Default, DataType.F16_Default, DataType.F16_Default) \
+    .dtype_format(DataType.F16_5HD, DataType.F16_5HD, DataType.F16_5HD) \
+    .dtype_format(DataType.F32_Default, DataType.F32_Default, DataType.F32_Default) \
+    .dtype_format(DataType.F32_5HD, DataType.F32_5HD, DataType.F32_5HD) \
+    .dtype_format(DataType.F16_NDC1HWC0, DataType.F16_NDC1HWC0, DataType.F16_NDC1HWC0) \
+    .dtype_format(DataType.F32_NDC1HWC0, DataType.F32_NDC1HWC0, DataType.F32_NDC1HWC0) \
+    .get_op_info()
+
+
+@op_info_register(sigmoid_cross_entropy_with_logits_ds_op_info)
+def _sigmoid_cross_entropy_with_logits_ds_tbe():
+    """SigmoidCrossEntropyWithLogits TBE register"""
+    return
