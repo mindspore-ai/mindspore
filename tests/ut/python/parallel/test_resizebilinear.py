c42081619e55e781d4573decf1412bb92269bf5b
yangzhenzhang <yangzhenzhang@huawei.com>
2021-10-27 14:39:26 +0800

    add parallel op for resizenearestneighbor

--- tests/ut/python/parallel/test_resizebilinear.py
+++ tests/ut/python/parallel/test_resizebilinear.py
@@ -43,17 +43,20 @@ class Net2(Cell):
     '''
     create the test Net
     '''
-    def __init__(self, conv2d_weight, out_channel, kernel_size, pad_mode, stride,
+    def __init__(self, conv2d_weight, mul_weight, out_channel, kernel_size, pad_mode, stride,
                  strategy1=None, strategy2=None):
         super(Net2, self).__init__()
         self.conv2d = P.Conv2D(out_channel=out_channel, kernel_size=kernel_size,
                                pad_mode=pad_mode, stride=stride).shard(strategy1)
         self.conv2d_weight = Parameter(conv2d_weight, "w1")
         self.resize_neighbor = P.ResizeNearestNeighbor((16, 16)).shard(strategy2)
+        self.mul = P.Mul()
+        self.mul_weight = Parameter(mul_weight, "w2")
 
     def construct(self, x):
         out = self.conv2d(x, self.conv2d_weight)
         out = self.resize_neighbor(out)
+        out = self.mul(out, self.mul_weight)
         return out
 
 class Net3(Cell):
@@ -76,6 +79,7 @@ class Net3(Cell):
 
 _x = Tensor(np.ones([32, 16, 8, 8]), dtype=ms.float32)
 _w1 = Tensor(np.ones([8, 16, 2, 2]), dtype=ms.float32)
+_w2 = Tensor(np.ones([32, 8, 16, 16]), dtype=ms.float32)
 
 
 def compile_net(net, inputs=_x):
@@ -130,21 +134,21 @@ def test_neighbor_data_parallel():
     context.set_auto_parallel_context(parallel_mode="semi_auto_parallel", device_num=8, global_rank=0)
     strategy1 = ((8, 1, 1, 1), (1, 1, 1, 1))
     strategy2 = ((8, 1, 1, 1),)
-    net = Net2(_w1, out_channel=8, kernel_size=2, pad_mode="same", stride=1,
+    net = Net2(_w1, _w2, out_channel=8, kernel_size=2, pad_mode="same", stride=1,
                strategy1=strategy1, strategy2=strategy2)
     compile_net(net)
 
 
 def test_neighbor_model_parallel1():
-    context.set_auto_parallel_context(parallel_mode="semi_auto_parallel", device_num=8, global_rank=0)
+    context.set_auto_parallel_context(parallel_mode="semi_auto_parallel", device_num=16, global_rank=0)
     strategy1 = ((2, 2, 1, 1), (2, 2, 1, 1))
-    strategy2 = ((4, 2, 1, 1),)
-    net = Net2(_w1, out_channel=8, kernel_size=2, pad_mode="same", stride=1,
+    strategy2 = ((2, 2, 2, 2),)
+    net = Net2(_w1, _w2, out_channel=8, kernel_size=2, pad_mode="same", stride=1,
                strategy1=strategy1, strategy2=strategy2)
     compile_net(net)
 
 
 def test_neighbor_auto_parallel():
     context.set_auto_parallel_context(parallel_mode="auto_parallel", device_num=8, global_rank=0)
-    net = Net2(_w1, out_channel=8, kernel_size=2, pad_mode="same", stride=1)
+    net = Net2(_w1, _w2, out_channel=8, kernel_size=2, pad_mode="same", stride=1)
     compile_net(net)
