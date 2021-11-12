0b131c07f066897b2cb5d77d05ac869f6c41d04f
emmmmtang <tangcong9@huawei.com>
2021-11-12 18:46:25 +0800

    update md5 value of openssl

--- cmake/external_libs/openssl.cmake
+++ cmake/external_libs/openssl.cmake
@@ -1,6 +1,6 @@
 if(ENABLE_GITEE)
   set(REQ_URL "https://gitee.com/mirrors/openssl/repository/archive/OpenSSL_1_1_1k.tar.gz")
-  set(MD5 "5e96e1713bb1f93358f68cf1a85d4512")
+  set(MD5 "bdd51a68ad74618dd2519da8e0bcc759")
 else()
   set(REQ_URL "https://github.com/openssl/openssl/archive/refs/tags/OpenSSL_1_1_1k.tar.gz")
   set(MD5 "bdd51a68ad74618dd2519da8e0bcc759")
