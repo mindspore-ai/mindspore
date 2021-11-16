431fe7052741de3d20e9ab2f134d56227bfd54cd
dingpeifei <dingpeifei1@huawei.com>
2021-11-16 16:40:43 +0800

    md5 open source r1.3 1116

--- cmake/external_libs/libevent.cmake
+++ cmake/external_libs/libevent.cmake
@@ -5,7 +5,7 @@ endif()
 
 if(ENABLE_GITEE)
     set(REQ_URL "https://gitee.com/mirrors/libevent/repository/archive/release-2.1.12-stable.tar.gz")
-    set(MD5 "c9036513dd9e5b4fa1c81ade23b7ead2")
+    set(MD5 "0d5a27436bf7ff8253420c8cf09f47ca")
 else()
     set(REQ_URL
       "https://github.com/libevent/libevent/releases/download/release-2.1.12-stable/libevent-2.1.12-stable.tar.gz")
