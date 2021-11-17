a16d6971540f2480741852afe214b3046fecfaa7
ZPaC <zhoupeichen@huawei.com>
2021-11-17 10:06:28 +0800

    Fix master nccl and ompi md5

--- cmake/external_libs/nccl.cmake
+++ cmake/external_libs/nccl.cmake
@@ -1,6 +1,6 @@
 if(ENABLE_GITEE)
     set(REQ_URL "https://gitee.com/mirrors/nccl/repository/archive/v2.7.6-1.tar.gz")
-    set(MD5 "0f2797ab8c876840d09d1277bdead339")
+    set(MD5 "6884364c2b1cf229f0bdaf94efcb5782")
 else()
     set(REQ_URL "https://github.com/NVIDIA/nccl/archive/v2.7.6-1.tar.gz")
     set(MD5 "073b19899f374c5ba07d2db02dc38f9f")
--- cmake/external_libs/ompi.cmake
+++ cmake/external_libs/ompi.cmake
@@ -1,6 +1,6 @@
 if(ENABLE_GITEE)
     set(REQ_URL "https://gitee.com/mirrors/ompi/repository/archive/v4.0.3.tar.gz")
-    set(MD5 "77865fe49f85c6294416007c5633a448")
+    set(MD5 "70f764c26ab6cd99487d58be0cd8c409")
 else()
     set(REQ_URL "https://github.com/open-mpi/ompi/archive/v4.0.3.tar.gz")
     set(MD5 "86cb724e8fe71741ad3be4e7927928a2")
