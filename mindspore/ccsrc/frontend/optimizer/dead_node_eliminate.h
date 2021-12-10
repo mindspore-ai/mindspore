5e654594c2cf02a4e4986b1924a66a66207a1955
chenfei <chenfei52@huawei.com>
2021-12-10 11:23:52 +0800

    dead node eliminate
    move dead node elim to front of after_cconv
    
    clean code
    
    add special handle for switch_layer and Fix ExistClosure
    
    split define of value getter
    
    visit from root graph output
    
    add renormalize after dead node elim
    
    Revert "add renormalize after dead node elim"
    
    This reverts commit cb402e7000bd9fbacbefc38177171aafca03dcc1.
    
    add eliminate to after cconv
    
    fix bug of monad no abstract
    
    code review fix
    
    add change flag of analyzer

---
+++ mindspore/ccsrc/frontend/optimizer/dead_node_eliminate.h
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2021 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MINDSPORE_CCSRC_FRONTEND_OPTIMIZER_DEAD_NODE_ELIMINATE_H
+#define MINDSPORE_CCSRC_FRONTEND_OPTIMIZER_DEAD_NODE_ELIMINATE_H
+#include "ir/anf.h"
+#include "ir/manager.h"
+#include "frontend/optimizer/optimizer.h"
+namespace mindspore::opt {
+bool EliminateDeadNode(const FuncGraphPtr &func_graph);
+class EliminateDeadNodePass {
+ public:
+  EliminateDeadNodePass() = default;
+  ~EliminateDeadNodePass() = default;
+  bool operator()(const FuncGraphPtr &func_graph, const OptimizerPtr &optimizer) {
+    bool enable_closure = common::GetEnv("MS_DEV_ENABLE_CLOSURE") == "1";
+    MS_LOG(INFO) << "Closure enable:" << enable_closure;
+    if (!enable_closure) {
+      return false;
+    }
+
+    auto change = EliminateDeadNode(func_graph);
+    if (change) {
+      // Enable renormalize.
+      optimizer->set_is_untyped_generated();
+    }
+    return change;
+  }
+};
+}  // namespace mindspore::opt
+#endif  // MINDSPORE_CCSRC_FRONTEND_OPTIMIZER_DEAD_NODE_ELIMINATE_H
