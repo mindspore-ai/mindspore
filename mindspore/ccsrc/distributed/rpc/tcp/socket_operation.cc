2dc1d5fe085a9d3bab8c28a00939d7a67db257fe
cristoval <chengang82@huawei.com>
2021-12-12 17:55:52 +0800

    Add socket operation for RPC

---
+++ mindspore/ccsrc/distributed/rpc/tcp/socket_operation.cc
@@ -0,0 +1,318 @@
+/**
+ * Copyright 2021 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "distributed/rpc/tcp/socket_operation.h"
+
+#include <arpa/inet.h>
+#include <securec.h>
+#include <netinet/tcp.h>
+#include <unistd.h>
+#include <csignal>
+#include <system_error>
+
+#include "actor/log.h"
+#include "distributed/rpc/tcp/constants.h"
+
+namespace mindspore {
+namespace distributed {
+namespace rpc {
+int SocketOperation::SetSocketKeepAlive(int fd, int keepalive, int keepidle, int keepinterval, int keepcount) {
+  int option_val = 0;
+  int ret = 0;
+
+  option_val = keepalive;
+  ret = setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &option_val, sizeof(option_val));
+  if (ret < 0) {
+    MS_LOG(ERROR) << "Failed to call setsockopt SO_KEEPALIVE, fd: " << fd << ", errno:" << errno;
+    return -1;
+  }
+
+  // Send first probe after `interval' seconds.
+  option_val = keepidle;
+  ret = setsockopt(fd, IPPROTO_TCP, TCP_KEEPIDLE, &option_val, sizeof(option_val));
+  if (ret < 0) {
+    MS_LOG(ERROR) << "Failed to call setsockopt TCP_KEEPIDLE, fd: " << fd << ", errno:" << errno;
+    return -1;
+  }
+
+  // Send next probes after the specified interval.
+  option_val = keepinterval;
+  ret = setsockopt(fd, IPPROTO_TCP, TCP_KEEPINTVL, &option_val, sizeof(option_val));
+  if (ret < 0) {
+    MS_LOG(ERROR) << "Failed to call setsockopt TCP_KEEPINTVL, fd: " << fd << ", errno:" << errno;
+    return -1;
+  }
+
+  /* Consider the socket in error state after three we send three ACK
+   * probes without getting a reply. */
+  option_val = keepcount;
+  ret = setsockopt(fd, IPPROTO_TCP, TCP_KEEPCNT, &option_val, sizeof(option_val));
+  if (ret < 0) {
+    MS_LOG(ERROR) << "Failed to call setsockopt TCP_KEEPCNT, fd: " << fd << ", errno:" << errno;
+    return -1;
+  }
+  return 0;
+}
+
+int SocketOperation::SetSocketOptions(int sock_fd) {
+  int option_val = 1;
+  int ret = 0;
+
+  ret = setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &option_val, sizeof(option_val));
+  if (ret) {
+    MS_LOG(ERROR) << "Failed to call setsockopt SO_REUSEADDR, fd: " << sock_fd << ", errno:" << errno;
+    return -1;
+  }
+
+  ret = setsockopt(sock_fd, IPPROTO_TCP, TCP_NODELAY, &option_val, sizeof(option_val));
+  if (ret) {
+    MS_LOG(ERROR) << "Failed to call setsockopt TCP_NODELAY, fd: " << sock_fd << ", errno:" << errno;
+    return -1;
+  }
+
+  ret = SetSocketKeepAlive(sock_fd, SOCKET_KEEPALIVE, SOCKET_KEEPIDLE, SOCKET_KEEPINTERVAL, SOCKET_KEEPCOUNT);
+  if (ret) {
+    MS_LOG(WARNING) << "Failed to call setsockopt keep alive, fd: " << sock_fd;
+  }
+  return 0;
+}
+
+int SocketOperation::CreateServerSocket(sa_family_t family) {
+  int ret = 0;
+  int fd = 0;
+
+  // Create server socket
+  fd = ::socket(family, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0);
+  if (fd < 0) {
+    MS_LOG(WARNING) << "Failed to create socket: " << errno;
+    return -1;
+  }
+
+  ret = SetSocketOptions(fd);
+  if (ret < 0) {
+    close(fd);
+    return -1;
+  }
+  return fd;
+}
+
+std::string SocketOperation::GetIP(const std::string &url) {
+  size_t index1 = url.find("[");
+  if (index1 == std::string::npos) {
+    index1 = url.find(URL_PROTOCOL_IP_SEPARATOR);
+    if (index1 == std::string::npos) {
+      index1 = 0;
+    } else {
+      index1 = index1 + sizeof(URL_PROTOCOL_IP_SEPARATOR) - 1;
+    }
+  } else {
+    index1 = index1 + 1;
+  }
+
+  size_t index2 = url.find("]");
+  if (index2 == std::string::npos) {
+    index2 = url.rfind(URL_IP_PORT_SEPARATOR);
+    if (index2 == std::string::npos) {
+      MS_LOG(INFO) << "Couldn't find the character: " << URL_IP_PORT_SEPARATOR << ", url: " << url.c_str();
+      return "";
+    }
+  }
+
+  if (index1 > index2) {
+    MS_LOG(INFO) << "Parse ip failed, url: " << url.c_str();
+    return "";
+  }
+
+  std::string ip = url.substr(index1, index2 - index1);
+  SocketAddress addr;
+
+  int result = inet_pton(AF_INET, ip.c_str(), &addr.saIn.sin_addr);
+  if (result <= 0) {
+    result = inet_pton(AF_INET6, ip.c_str(), &addr.saIn6.sin6_addr);
+    if (result <= 0) {
+      MS_LOG(INFO) << "Parse ip failed, result: " << result << ", url:" << url.c_str();
+      return "";
+    }
+  }
+  return ip;
+}
+
+bool SocketOperation::GetSockAddr(const std::string &url, SocketAddress *addr) {
+  std::string ip;
+  uint16_t port = 0;
+
+  size_t len = sizeof(*addr);
+  (void)memset_s(addr, len, 0, len);
+
+  size_t index1 = url.find(URL_PROTOCOL_IP_SEPARATOR);
+  if (index1 == std::string::npos) {
+    index1 = 0;
+  } else {
+    index1 = index1 + sizeof(URL_PROTOCOL_IP_SEPARATOR) - 1;
+  }
+
+  size_t index2 = url.rfind(':');
+  if (index2 == std::string::npos) {
+    MS_LOG(ERROR) << "Couldn't find the character colon.";
+    return false;
+  }
+
+  ip = url.substr(index1, index2 - index1);
+  if (ip.empty()) {
+    MS_LOG(ERROR) << "Couldn't find ip in url: " << url.c_str();
+    return false;
+  }
+
+  try {
+    port = (uint16_t)std::stoul(url.substr(index2 + sizeof(URL_IP_PORT_SEPARATOR) - 1));
+  } catch (const std::system_error &e) {
+    MS_LOG(ERROR) << "Couldn't find port in url: " << url.c_str();
+    return false;
+  }
+
+  int result = inet_pton(AF_INET, ip.c_str(), &addr->saIn.sin_addr);
+  if (result > 0) {
+    addr->saIn.sin_family = AF_INET;
+    addr->saIn.sin_port = htons(port);
+    return true;
+  }
+
+  result = inet_pton(AF_INET6, ip.c_str(), &(addr->saIn6.sin6_addr));
+  if (result > 0) {
+    addr->saIn6.sin6_family = AF_INET6;
+    addr->saIn6.sin6_port = htons(port);
+    return true;
+  }
+
+  MS_LOG(ERROR) << "Parse ip failed, result: " << result << ", url: " << url.c_str();
+  return false;
+}
+
+uint16_t SocketOperation::GetPort(int fd) {
+  uint16_t port = 0;
+  int retval = 0;
+  union SocketAddress isa;
+  socklen_t isaLen = sizeof(struct sockaddr_storage);
+
+  retval = getsockname(fd, &isa.sa, &isaLen);
+  if (retval) {
+    MS_LOG(INFO) << "Failed to call getsockname, fd: " << fd << ", ret: " << retval << ", errno: " << errno;
+    return port;
+  }
+
+  if (isa.sa.sa_family == AF_INET) {
+    port = ntohs(isa.saIn.sin_port);
+  } else if (isa.sa.sa_family == AF_INET6) {
+    port = ntohs(isa.saIn6.sin6_port);
+  } else {
+    MS_LOG(INFO) << "Unknown fd: " << fd << ", family: " << isa.sa.sa_family;
+  }
+  return port;
+}
+
+std::string SocketOperation::GetPeer(int sock_fd) {
+  std::string peer;
+  int retval = 0;
+  union SocketAddress isa;
+  socklen_t isaLen = sizeof(struct sockaddr_storage);
+
+  retval = getpeername(sock_fd, &isa.sa, &isaLen);
+  if (retval < 0) {
+    MS_LOG(INFO) << "Failed to call getpeername, fd: " << sock_fd << ", ret: " << retval << ", errno: " << errno;
+    return peer;
+  }
+
+  char ipdotdec[IP_LEN_MAX];
+  if (isa.sa.sa_family == AF_INET) {
+    inet_ntop(AF_INET, reinterpret_cast<void *>(&isa.saIn.sin_addr), ipdotdec, IP_LEN_MAX);
+    peer = std::string(ipdotdec) + ":" + std::to_string(ntohs(isa.saIn.sin_port));
+  } else if (isa.sa.sa_family == AF_INET6) {
+    inet_ntop(AF_INET6, reinterpret_cast<void *>(&isa.saIn6.sin6_addr), ipdotdec, IP_LEN_MAX);
+    peer = std::string(ipdotdec) + ":" + std::to_string(ntohs(isa.saIn6.sin6_port));
+  } else {
+    MS_LOG(INFO) << "Unknown fd: " << sock_fd << ", family: " << isa.sa.sa_family;
+  }
+  return peer;
+}
+
+int SocketOperation::Connect(int sock_fd, const struct sockaddr *sa, socklen_t saLen, uint16_t *boundPort) {
+  int retval = 0;
+
+  retval = connect(sock_fd, sa, saLen);
+  if (retval != 0) {
+    if (errno == EINPROGRESS) {
+      /* set iomux for write event */
+    } else {
+      MS_LOG(ERROR) << "Failed to call connect, fd: " << sock_fd << ", ret: " << retval << ", errno: " << errno;
+      return retval;
+    }
+  }
+
+  // to get local port
+  *boundPort = GetPort(sock_fd);
+  if (*boundPort == 0) {
+    return RPC_ERROR;
+  }
+  return RPC_OK;
+}
+
+int SocketOperation::Listen(const std::string &url) {
+  int listenFd = 0;
+  SocketAddress addr;
+
+  if (!GetSockAddr(url, &addr)) {
+    return -1;
+  }
+
+  // create server socket
+  listenFd = CreateServerSocket(addr.sa.sa_family);
+  if (listenFd < 0) {
+    MS_LOG(ERROR) << "Failed to create socket, url: " << url.c_str();
+    return -1;
+  }
+
+  // bind
+  if (::bind(listenFd, (struct sockaddr *)&addr, sizeof(SocketAddress))) {
+    MS_LOG(ERROR) << "Failed to call bind, url: " << url.c_str();
+    close(listenFd);
+    return -1;
+  }
+
+  // listen
+  if (::listen(listenFd, SOCKET_LISTEN_BACKLOG)) {
+    MS_LOG(ERROR) << "Failed to call listen, fd: " << listenFd << ", errno: " << errno << ", url: " << url.c_str();
+    close(listenFd);
+    return -1;
+  }
+  return listenFd;
+}
+
+int SocketOperation::Accept(int sock_fd) {
+  SocketAddress storage;
+  socklen_t length = sizeof(storage);
+
+  // accept connection
+  auto acceptFd = ::accept4(sock_fd, (struct sockaddr *)&storage, &length, SOCK_NONBLOCK | SOCK_CLOEXEC);
+  if (acceptFd < 0) {
+    MS_LOG(ERROR) << "Failed to call accept, errno: " << errno << ", server: " << sock_fd;
+    return acceptFd;
+  }
+  SetSocketOptions(acceptFd);
+  return acceptFd;
+}
+}  // namespace rpc
+}  // namespace distributed
+}  // namespace mindspore
---
+++ mindspore/ccsrc/distributed/rpc/tcp/socket_operation.h
@@ -0,0 +1,87 @@
+/**
+ * Copyright 2021 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MINDSPORE_CCSRC_DISTRIBUTED_RPC_TCP_SOCKET_OPERATION_H_
+#define MINDSPORE_CCSRC_DISTRIBUTED_RPC_TCP_SOCKET_OPERATION_H_
+
+#include <netinet/in.h>
+#include <string>
+
+namespace mindspore {
+namespace distributed {
+namespace rpc {
+class Connection;
+
+union SocketAddress {
+  struct sockaddr sa;
+  struct sockaddr_in saIn;
+  struct sockaddr_in6 saIn6;
+  struct sockaddr_storage saStorage;
+};
+
+class SocketOperation {
+ public:
+  SocketOperation() = default;
+  virtual ~SocketOperation() {}
+
+  static std::string GetIP(const std::string &url);
+  static uint16_t GetPort(int sock_fd);
+
+  // Get the address(ip:port) of the other end of the connection.
+  static std::string GetPeer(int sock_fd);
+
+  // Get socket address of the url.
+  static bool GetSockAddr(const std::string &url, SocketAddress *addr);
+
+  // Create a server socket.
+  static int CreateServerSocket(sa_family_t family);
+
+  // Set socket options.
+  static int SetSocketOptions(int sock_fd);
+  static int SetSocketKeepAlive(int fd, int keepalive, int keepidle, int keepinterval, int keepcount);
+
+  // Connect to the Socket sock_fd.
+  static int Connect(int sock_fd, const struct sockaddr *sa, socklen_t saLen, uint16_t *boundPort);
+
+  // Close the given connection.
+  virtual void Close(Connection *connection) = 0;
+
+  // Start and listen on the socket represented by the given url.
+  static int Listen(const std::string &url);
+
+  // Accept connection on the server socket.
+  static int Accept(int sock_fd);
+
+  // Call recv with flag MSG_PEEK which means do not delete data in buffer after reading.
+  virtual int ReceivePeek(Connection *connection, char *recvBuf, uint32_t recvLen) = 0;
+
+  // Try to receive messages up to totalRecvLen (for message header).
+  virtual int Receive(Connection *connection, char *recvBuf, uint32_t totalRecvLen, uint32_t *recvLen) = 0;
+
+  // Receive message (for message body).
+  virtual int ReceiveMessage(Connection *connection, struct msghdr *recvMsg, uint32_t recvLen) = 0;
+
+  virtual int SendMessage(Connection *connection, struct msghdr *sendMsg, uint32_t *sendLen) = 0;
+
+  // Handle connect and connected events.
+  virtual void NewConnEventHandler(int fd, uint32_t events, void *context) = 0;
+  virtual void ConnEstablishedEventHandler(int fd, uint32_t events, void *context) = 0;
+};
+}  // namespace rpc
+}  // namespace distributed
+}  // namespace mindspore
+
+#endif
