fix arange dtype error and st precision

mindspore/nn/layer/rnns.py
@@ -32,8 +32,8 @@ __all__ = ['LSTM', 'GRU', 'RNN']
 
 
 @constexpr
-def arange(start, stop, step):
-    return Tensor(np.arange(start, stop, step), mstype.int32)
+def arange(start, stop, step, dtype):
+    return Tensor(np.arange(start, stop, step), dtype)
 
 
 @constexpr
@@ -73,7 +73,7 @@ def _check_tuple_length(param_name, input_data, length, cls_name):
 
 def sequence_mask(lengths, maxlen):
     """generate mask matrix by seq_length"""
-    range_vector = arange(0, maxlen, 1)
+    range_vector = arange(0, maxlen, 1, lengths.dtype)
     result = range_vector < lengths.view(lengths.shape + (1,))
     return result.astype(mstype.int32)
 
@@ -84,7 +84,7 @@ def select_by_mask(inputs, mask):
 
 def get_hidden(output, seq_length):
     """get hidden state by seq_length"""
-    batch_index = arange(0, seq_length.shape[0], 1)
+    batch_index = arange(0, seq_length.shape[0], 1, seq_length.dtype)
     indices = P.Concat(1)((seq_length.view(-1, 1) - 1, batch_index.view(-1, 1)))
     return P.GatherNd()(output, indices)
 

tests/st/ops/ascend/test_lstm_op.py
@@ -127,9 +127,9 @@ def test_sit_lstm_forward_input_3_32_32_is_32_hs_16():
     out_pynative, (hy_pynative, cy_pynative) = net_pynative(input_ms, h0, c0)
     context.set_context(mode=context.GRAPH_MODE)
 
-    assert np.allclose(out.asnumpy(), out_pynative.asnumpy(), 0.0001, 0.0001)
-    assert np.allclose(hy.asnumpy(), hy_pynative.asnumpy(), 0.0001, 0.0001)
-    assert np.allclose(cy.asnumpy(), cy_pynative.asnumpy(), 0.0001, 0.0001)
+    assert np.allclose(out.asnumpy(), out_pynative.asnumpy(), 0.002, 0.002)
+    assert np.allclose(hy.asnumpy(), hy_pynative.asnumpy(), 0.002, 0.002)
+    assert np.allclose(cy.asnumpy(), cy_pynative.asnumpy(), 0.002, 0.002)
 
 @pytest.mark.level0
 @pytest.mark.platform_arm_ascend_training