75cef0f2a1e64d7df2438742656ee6b07eeb9688
ZPaC <zhoupeichen@huawei.com>
2021-12-20 09:53:41 +0800

    Fix ps context default value

--- mindspore/python/mindspore/context.py
+++ mindspore/python/mindspore/context.py
@@ -967,9 +967,9 @@ def set_ps_context(**kwargs):
                           Default: False.
         config_file_path (string): Configuration file path used by recovery. Default: ''.
         scheduler_manage_port (int): scheduler manage port used to scale out/in. Default: 11202.
-        enable_ssl (bool): Set PS SSL mode enabled or disabled. Default: true.
-        client_password (str): Password to decrypt the secret key stored in the client certificate.
-        server_password (str): Password to decrypt the secret key stored in the server certificate.
+        enable_ssl (bool): Set PS SSL mode enabled or disabled. Default: False.
+        client_password (str): Password to decrypt the secret key stored in the client certificate. Default: ''.
+        server_password (str): Password to decrypt the secret key stored in the server certificate. Default: ''.
 
     Raises:
         ValueError: If input key is not the attribute in parameter server training mode context.
@@ -1058,9 +1058,9 @@ def set_fl_context(**kwargs):
             be applied to protect clients' model from stealing in cross-silo scenario. Default: 'NOT_ENCRYPT'.
         config_file_path (string): Configuration file path used by recovery. Default: ''.
         scheduler_manage_port (int): scheduler manage port used to scale out/in. Default: 11202.
-        enable_ssl (bool): Set PS SSL mode enabled or disabled. Default: true.
-        client_password (str): Password to decrypt the secret key stored in the client certificate.
-        server_password (str): Password to decrypt the secret key stored in the server certificate.
+        enable_ssl (bool): Set PS SSL mode enabled or disabled. Default: False.
+        client_password (str): Password to decrypt the secret key stored in the client certificate. Default: ''.
+        server_password (str): Password to decrypt the secret key stored in the server certificate. Default: ''.
 
     Raises:
         ValueError: If input key is not the attribute in federated learning mode context.
