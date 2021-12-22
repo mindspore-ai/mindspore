0f099d4a578a7b334d1dba2718114ca8056ed6a5
z00512249 <zhuzhongrui1@huawei.com>
2021-12-22 16:10:30 +0800

    upgrade eigen from 3.3.7 to 3.3.9

--- cmake/external_libs/eigen.cmake
+++ cmake/external_libs/eigen.cmake
@@ -1,20 +1,17 @@
 set(Eigen3_CXXFLAGS "-D_FORTIFY_SOURCE=2 -O2")
 set(Eigen3_CFLAGS "-D_FORTIFY_SOURCE=2 -O2")
 
-if(ENABLE_GITEE)
-    set(REQ_URL "https://gitee.com/mirrors/eigen-git-mirrorsource/repository/archive/3.3.7.tar.gz")
-    set(MD5 "4caffd68ea975d2a10a477e3db57dd18")
-else()
-    set(REQ_URL "https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz")
-    set(MD5 "9e30f67e8531477de4117506fe44669b")
-endif()
+
+set(REQ_URL "https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz")
+set(MD5 "609286804b0f79be622ccf7f9ff2b660")
+
 
 mindspore_add_pkg(Eigen3
-        VER 3.3.7
+        VER 3.3.9
         URL ${REQ_URL}
         MD5 ${MD5}
         CMAKE_OPTION -DBUILD_TESTING=OFF)
-find_package(Eigen3 3.3.7 REQUIRED ${MS_FIND_NO_DEFAULT_PATH})
+find_package(Eigen3 3.3.9 REQUIRED ${MS_FIND_NO_DEFAULT_PATH})
 include_directories(${Eigen3_INC})
 include_directories(${EIGEN3_INCLUDE_DIR})
 set_property(TARGET Eigen3::Eigen PROPERTY IMPORTED_GLOBAL TRUE)
