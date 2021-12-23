27d300677ec4a5b0681e3ee24b956e8bd939cf78
liqiliang <liqiliang1@huawei.com>
2021-12-23 15:06:04 +0800

    Modify the imported interfaces in scipy _init_().

--- mindspore/python/mindspore/scipy/__init__.py
+++ mindspore/python/mindspore/scipy/__init__.py
@@ -14,14 +14,4 @@
 # ============================================================================
 """Scipy-like interfaces in mindspore."""
 
-from . import optimize, sparse, linalg
-from .optimize import minimize, line_search
-from .sparse import cg, gmres
-from .linalg import block_diag, solve_triangular, inv, cho_factor, cholesky, cho_solve, lu, lu_factor, lu_solve
-
-__all__ = []
-__all__.extend(optimize.__all__)
-__all__.extend(sparse.__all__)
-__all__.extend(linalg.__all__)
-
-__all__.sort()
+from . import linalg, optimize, sparse
