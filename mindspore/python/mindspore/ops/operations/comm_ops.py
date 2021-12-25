20b983b49617e330322b8154996891034417bd33
zhoufeng <zhoufeng54@huawei.com>
2021-12-25 10:52:55 +0800

    fix alltoall doc of attrs
    Signed-off-by: zhoufeng <zhoufeng54@huawei.com>

--- mindspore/python/mindspore/ops/operations/comm_ops.py
+++ mindspore/python/mindspore/ops/operations/comm_ops.py
@@ -660,7 +660,7 @@ class NeighborExchange(Primitive):
         recv_shapes (tuple(list(int))): Data shape which received from recv_rank_ids.
         send_shapes (tuple(list(int))): Data shape which send to the send_rank_ids.
         recv_type (type): Data type which received from recv_rank_ids
-        group (str):
+        group (str): The communication group to work on. Default: "GlobalComm.WORLD_COMM_GROUP".
 
     Example:
         >>> # This example should be run with 2 devices. Refer to the tutorial > Distributed Training on mindspore.cn
