a2f0253998dcbf4da94588395c042d911979ca51
zheng_pengfei <18865382565@163.com>
2021-12-29 10:32:54 +0800

    [feat] [assistant] [I48OAR] greater with dynamic infer shape

--- mindspore/core/ops/greater.h
+++ mindspore/core/ops/greater.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Huawei Technologies Co., Ltd
+ * Copyright 2020-2021 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +39,9 @@ class MS_CORE_API Greater : public PrimitiveC {
   /// \brief Init. Refer to the parameters of Python API @ref mindspore.ops.Greater for the inputs.
   void Init() {}
 };
+AbstractBasePtr GreaterInfer(const abstract::AnalysisEnginePtr &, const PrimitivePtr &primitive,
+                             const std::vector<AbstractBasePtr> &input_args);
+using kPrimGreaterPtr = std::shared_ptr<Greater>;
 }  // namespace ops
 }  // namespace mindspore
 
