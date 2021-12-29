7b1b133d1fd02236f0a4d10081900771f74071c4
Bowen Ma <iambowen.m@gmail.com>
2021-12-29 16:50:38 +0800

    update ubuntu + gpu with pip/conda install script.

--- scripts/install/ubuntu-gpu-conda.sh
+++ scripts/install/ubuntu-gpu-conda.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 set -ex
 
+MINDSPORE_VERSION=${MINDSPORE_VERSION:-1.5.0}
 PYTHON_VERSION=${PYTHON_VERSION:-3.7.5}
 MINDSPORE_VERSION=${MINDSPORE_VERSION:-1.5.0}
 CUDA_VERSION=${CUDA_VERSION:-8.0.5.39-1+cuda11.1}
 DISTRIBUTED=${DISTRIBUTED:-false}
 CUDA_INSTALL_PATH=${CUDA_INSTALL_PATH:-cuda-11}
-ARCH=$(uname -m)
-
-if [[ "${PYTHON_VERSION}" == "3.7.5" ]]; then
-VERSION="${MINDSPORE_VERSION}-cp37-cp37m"
-else
-VERSION="${MINDSPORE_VERSION}-cp39-cp39m"
-fi
+CUDATOOLKIT_VERSION=${CUDATOOLKIT_VERSION:-11.1}
+CUDNN_VERSION=${CUDNN_VERSION:-8.0.5}
 
 #use huaweicloud mirror in China
 sudo sed -i "s@http://.*archive.ubuntu.com@http://repo.huaweicloud.com@g" /etc/apt/sources.list
@@ -20,25 +16,44 @@ sudo sed -i "s@http://.*security.ubuntu.com@http://repo.huaweicloud.com@g" /etc/
 sudo apt-get update
 
 # install python 3.7 and make it default
-sudo apt-get install gcc-7 libgmp-dev curl python3.7  openssl ubuntu-drivers-common openssl -y
+sudo apt-get install gcc-7 libgmp-dev curl python3.7  -y
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 100
 
 cd /tmp
-curl -O https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
+curl -O https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-py37_4.10.3-Linux-x86_64.sh
+bash Miniconda3-py37_4.10.3-Linux-x86_64.sh
 
-# add pip mirror
-
-mkdir ~/.pip
-
-cat > ~/.pip/pip.conf <<END
-[global]
-index-url = https://repo.huaweicloud.com/repository/pypi/simple
-trusted-host = repo.huaweicloud.com
-timeout = 120
+# add conda to PATH
+ echo -e 'export PATH=~/miniconda3/bin/:$PATH' >> ~/.bash_profile
+ echo -e '. ~/miniconda3/etc/profile.d/conda.sh' >> ~/.bash_profile
+ source ~/.bash_profile
+conda init bash
+# setting up conda mirror
+cat >~/.condarc  <<END
+channels:
+  - defaults
+show_channel_urls: true
+default_channels:
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/msys2
+custom_channels:
+  conda-forge: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  msys2: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  bioconda: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  menpo: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  pytorch: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  pytorch-lts: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  simpleitk: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
 END
 
+#initialize conda env and install mindspore-cpu
+
+conda create -n py37  python=3.7.5 -y
+conda activate py37
+
 # install gmp 6.1.2, downloading gmp is slow
+echo "install gmp start"
 
 sudo apt-get install m4 -y 
 cd /tmp
@@ -50,8 +65,9 @@ make
 sudo make install
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/gmp-6.1.2/lib' >> ~/.bash_profile
 
-# install cuda with linux.run
-# another option is to use https://developer.download.nvidia.com/compute/cuda/11.1.0/local_installers/cuda_11.1.0_455.23.05_linux.run
+echo "install gmp success"
+# install cuda with apt-get
+echo "install cuda start"
 
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
 sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
@@ -96,6 +112,5 @@ if [[ "$DISTRIBUTED" == "false" ]]; then
     source ~/.bash_profile
 fi
 
-# install mindspore gpu
-
-pip install https://ms-release.obs.cn-north-4.myhuaweicloud.com/${MINDSPORE_VERSION}/MindSpore/gpu/${CUDA_INSTALL_PATH}/mindspore-${VERSION}-linux_${ARCH}.whl --trusted-host ms-release.obs.cn-north-4.myhuaweicloud.com -i https://pypi.tuna.tsinghua.edu.cn/simple
+# install mindspore-gpu with conda
+conda install mindspore-gpu=${MINDSPORE_VERSION} cudatoolkit=${CUDATOOLKIT_VERSION} cudnn=${CUDNN_VERSION} -c mindspore -c conda-forge
