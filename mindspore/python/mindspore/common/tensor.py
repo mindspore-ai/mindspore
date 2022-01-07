6982baa4a021ccd5589c4ffb95e04442a2834699
liuyang_655 <liuyang655@huawei.com>
2022-01-06 22:05:11 -0500

    modify api note

--- mindspore/python/mindspore/common/tensor.py
+++ mindspore/python/mindspore/common/tensor.py
@@ -1591,7 +1591,7 @@ class Tensor(Tensor_):
             Tensor, if Tensor is 2-D, return a 1-D Tensor containing the diagonal.
 
         Raises:
-            ValueError: if the input tensor has less than two dimensions.
+            ValueError: If the input tensor has less than two dimensions.
 
         Supported Platforms:
             ``Ascend`` ``GPU`` ``CPU``
@@ -1727,7 +1727,7 @@ class Tensor(Tensor_):
             Tensor, the indexed result.
 
         Raises:
-            ValueError: if `axis` is out of range, or `mode` has values other than ('raise', 'wrap', 'clip')
+            ValueError: If `axis` is out of range, or `mode` has values other than ('raise', 'wrap', 'clip')
 
         Supported Platforms:
             ``Ascend`` ``GPU`` ``CPU``
@@ -1798,7 +1798,7 @@ class Tensor(Tensor_):
             ``Ascend`` ``GPU`` ``CPU``
 
         Raises:
-            ValueError: if the input tensor and any of the `choices` cannot be broadcast.
+            ValueError: If the input tensor and any of the `choices` cannot be broadcast.
 
         Examples:
             >>> import numpy as np
@@ -1866,7 +1866,7 @@ class Tensor(Tensor_):
             Tensor, array of insertion points with the same shape as `v`.
 
         Raises:
-            ValueError: if argument for `side` or `sorter` is invalid.
+            ValueError: If argument for `side` or `sorter` is invalid.
 
         Supported Platforms:
             ``Ascend`` ``GPU`` ``CPU``
