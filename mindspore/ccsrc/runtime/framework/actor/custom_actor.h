1b55594149a85e788b753a3e6e15a4eb0a6f43d7
zhoufeng <zhoufeng54@huawei.com>
2022-01-10 11:14:10 +0800

    delete redundant include at custom actor
    Signed-off-by: zhoufeng <zhoufeng54@huawei.com>

--- mindspore/ccsrc/runtime/framework/actor/custom_actor.h
+++ mindspore/ccsrc/runtime/framework/actor/custom_actor.h
@@ -17,36 +17,22 @@
 #ifndef MINDSPORE_CCSRC_RUNTIME_FRAMEWORK_ACTOR_CUSTOM_ACTOR_H_
 #define MINDSPORE_CCSRC_RUNTIME_FRAMEWORK_ACTOR_CUSTOM_ACTOR_H_
 
-#include <vector>
 #include <string>
 #include <memory>
-#include <utility>
-#include "utils/hash_map.h"
-#include "runtime/framework/actor/actor_common.h"
-#include "runtime/framework/actor/debug_aware_actor.h"
+#include "runtime/framework/actor/abstract_actor.h"
 #include "runtime/hardware/device_context.h"
-#include "runtime/framework/device_tensor_store.h"
-#include "backend/kernel_compiler/kernel.h"
 #include "ir/anf.h"
-#include "ir/tensor.h"
-#include "utils/anf_utils.h"
 
 namespace mindspore {
 namespace runtime {
-using mindspore::device::DeviceContext;
-using mindspore::device::KernelInfo;
-using mindspore::kernel::Address;
-using mindspore::kernel::KernelLaunchInfo;
-using mindspore::tensor::TensorPtr;
-
 class CustomActor : public AbstractActor {
  public:
-  CustomActor(const std::string &name, const AnfNodePtr &kernel, const DeviceContext *device_context,
+  CustomActor(const std::string &name, const AnfNodePtr &kernel, const device::DeviceContext *device_context,
               const AID *recorder_aid)
       : AbstractActor(name, KernelTransformType::kCustomActor, recorder_aid), kernel_(kernel) {
     device_contexts_.push_back(device_context);
   }
-  CustomActor(const std::string &name, const AnfNodePtr &kernel, const DeviceContext *device_context,
+  CustomActor(const std::string &name, const AnfNodePtr &kernel, const device::DeviceContext *device_context,
               const AID *recorder_aid, GraphExecutionStrategy strategy)
       : AbstractActor(name, KernelTransformType::kCustomActor, recorder_aid), kernel_(kernel), strategy_(strategy) {
     device_contexts_.push_back(device_context);
@@ -73,5 +59,4 @@ class CustomActor : public AbstractActor {
 using CustomActorPtr = std::shared_ptr<CustomActor>;
 }  // namespace runtime
 }  // namespace mindspore
-
 #endif  // MINDSPORE_CCSRC_RUNTIME_FRAMEWORK_ACTOR_CUSTOM_ACTOR_H_
