8c093f462a6595ae08d33e533a722c5a7052bf39
lianghao23 <lianghao23@huawei.com>
2022-01-12 23:01:32 +0800

    Fix the order of infer shape and type for xlogy

--- mindspore/core/ops/xlogy.cc
+++ mindspore/core/ops/xlogy.cc
@@ -29,31 +29,16 @@ namespace mindspore {
 namespace ops {
 namespace {
 abstract::ShapePtr XlogyInferShape(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) {
-  MS_EXCEPTION_IF_NULL(primitive);
   auto prim_name = primitive->name();
-  const int64_t input_num = 2;
-  (void)CheckAndConvertUtils::CheckInteger("input number", SizeToLong(input_args.size()), kEqual, input_num, prim_name);
-  for (const auto &item : input_args) {
-    MS_EXCEPTION_IF_NULL(item);
-  }
   return BroadCastInferShape(prim_name, input_args);
 }
 TypePtr XlogyInferType(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) {
-  MS_EXCEPTION_IF_NULL(primitive);
   auto prim_name = primitive->name();
-  const int64_t input_num = 2;
-  (void)CheckAndConvertUtils::CheckInteger("input number", SizeToLong(input_args.size()), kEqual, input_num, prim_name);
-  MS_EXCEPTION_IF_NULL(input_args[0]);
-  MS_EXCEPTION_IF_NULL(input_args[1]);
   auto x = CheckAndConvertUtils::CheckArgs<abstract::AbstractTensor>(prim_name, input_args, 0);
   auto y = CheckAndConvertUtils::CheckArgs<abstract::AbstractTensor>(prim_name, input_args, 1);
   (void)abstract::CheckDtypeSame(prim_name, x, y);
   auto input_type = input_args[0]->BuildType();
   MS_EXCEPTION_IF_NULL(input_type);
-  if (!input_type->isa<TensorType>()) {
-    MS_EXCEPTION(TypeError) << "The " << prim_name << "'s"
-                            << " input must be tensor type but got " << input_type->ToString();
-  }
   const std::set<TypePtr> valid_types = {kFloat32, kFloat16};
   (void)CheckAndConvertUtils::CheckTensorTypeValid("x", input_type, valid_types, prim_name);
   (void)CheckAndConvertUtils::CheckTensorTypeValid("y", input_type, valid_types, prim_name);
@@ -62,8 +47,11 @@ TypePtr XlogyInferType(const PrimitivePtr &primitive, const std::vector<Abstract
 }  // namespace
 AbstractBasePtr XlogyInfer(const abstract::AnalysisEnginePtr &, const PrimitivePtr &primitive,
                            const std::vector<AbstractBasePtr> &input_args) {
-  auto shape = XlogyInferShape(primitive, input_args);
+  MS_EXCEPTION_IF_NULL(primitive);
+  const int64_t input_num = 2;
+  CheckAndConvertUtils::CheckInputArgs(input_args, kEqual, input_num, primitive->name());
   auto type = XlogyInferType(primitive, input_args);
+  auto shape = XlogyInferShape(primitive, input_args);
   return abstract::MakeAbstract(shape, type);
 }
 REGISTER_PRIMITIVE_EVAL_IMPL(Xlogy, prim::kPrimXlogy, XlogyInfer, nullptr, true);
