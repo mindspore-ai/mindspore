4e5d29243b7890298ceefecbd94642a408f6a5bb
tacyi139 <tacyi@139.com>
2022-01-13 10:29:02 +0800

    optimize error message

--- mindspore/core/ops/select.cc
+++ mindspore/core/ops/select.cc
@@ -62,7 +62,7 @@ abstract::BaseShapePtr SelectInferShape(const PrimitivePtr &primitive, const std
     MS_LOG(ERROR) << " cond shape :" << input_args[kCondIndex]->BuildShape()->ToString();
     MS_LOG(ERROR) << " x shape :" << input_args[kXIndex]->BuildShape()->ToString();
     MS_LOG(ERROR) << " y shape :" << input_args[kYIndex]->BuildShape()->ToString();
-    MS_EXCEPTION(ValueError) << "The x_shape is not same as y_shape and cond_shape";
+    MS_EXCEPTION(ValueError) << "The shape of cond, x and y should be equal";
   }
   return input_args[1]->BuildShape();
 }
