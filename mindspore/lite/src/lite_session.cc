7619a249677ced678c5a6b835ee75f6e0df2bdfc
Liu_Xuu <liuxu72@huawei.com>
2022-01-18 16:08:14 +0800

    [MSLITE] fix mem leak and diverse wide deep networks compatibility in tensorrt delegate 0210_04

--- mindspore/lite/src/lite_session.cc
+++ mindspore/lite/src/lite_session.cc
@@ -927,6 +927,8 @@ int LiteSession::DelegateInit() {
 int LiteSession::Init(InnerContext *context) {
   bool expected = false;
   if (!is_running_.compare_exchange_strong(expected, true)) {
+    delete context;
+    context = nullptr;
     MS_LOG(ERROR) << "Not support multi-threading";
     return RET_ERROR;
   }
@@ -1767,6 +1769,7 @@ int lite::LiteSession::LoadModelAndCompileByPath(const std::string &model_path, 
   (reinterpret_cast<lite::LiteModel *>(model))->set_keep_model_buf(true);
   auto ret = CompileGraph(model);
   if (ret != lite::RET_OK) {
+    delete model;
     MS_LOG(ERROR) << "Compile model failed";
     return RET_ERROR;
   }
@@ -1792,6 +1795,7 @@ int lite::LiteSession::LoadModelAndCompileByPath(const std::string &model_path, 
   (reinterpret_cast<lite::LiteModel *>(model))->set_keep_model_buf(true);
   auto ret = CompileGraph(model);
   if (ret != lite::RET_OK) {
+    delete model;
     MS_LOG(ERROR) << "Compile model failed";
     return RET_ERROR;
   }
