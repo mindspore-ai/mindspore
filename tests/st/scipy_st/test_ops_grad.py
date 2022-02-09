1e5d22a7fae2b5c1db8989981f2677861de68476
z00512249 <zhuzhongrui1@huawei.com>
2022-02-09 16:31:35 +0800

    add gpu cholesky_grad st case

--- tests/st/scipy_st/test_ops_grad.py
+++ tests/st/scipy_st/test_ops_grad.py
@@ -24,6 +24,7 @@ from tests.st.scipy_st.utils import create_random_rank_matrix, create_sym_pos_ma
 
 @pytest.mark.level0
 @pytest.mark.platform_x86_cpu
+@pytest.mark.platform_x86_gpu_training
 @pytest.mark.env_onecard
 @pytest.mark.parametrize('shape', [(8, 8)])
 @pytest.mark.parametrize('data_type', [(onp.float32, 1e-2, 1e-3), (onp.float64, 1e-4, 1e-7)])
@@ -41,7 +42,7 @@ def test_cholesky_grad(shape, data_type):
         def __init__(self):
             super(CholeskyNet, self).__init__()
             self.mean = ops.ReduceMean()
-            # args clean not supports grad right now, just default to clean.
+            # Input arg clean not supports grad right now, just default clean to True.
             self.cholesky = Cholesky(clean=True)
 
         def construct(self, a):
