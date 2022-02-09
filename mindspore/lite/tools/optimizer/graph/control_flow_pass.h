57eb0933a92fd8a9a5c3db3de6639a806f7ab2d9
wang_shaocong <wangshaocong1@huawei.com>
2022-02-09 16:01:09 +0800

    [MSLITE] Codex clean.

--- mindspore/lite/tools/optimizer/graph/control_flow_pass.h
+++ mindspore/lite/tools/optimizer/graph/control_flow_pass.h
@@ -64,14 +64,14 @@ class ControlFlowPass : public Pass {
   int CreateIfPartialNodeExternalInputs(const CNodePtr &if_cnode, const FuncGraphPtr &partial_fg,
                                         std::vector<AnfNodePtr> *then_partial_cnode_inputs);
   int CreateIfPartialNode(const FuncGraphPtr &fg, const size_t &index,
-                          std::vector<AnfNodePtr> *fg_inputs_only_used_by_after_partial, const CNodePtr *if_cnode,
-                          const FuncGraphPtr *after_fg, CNodePtr *then_partial_cnode);
+                          std::vector<AnfNodePtr> *fg_inputs_only_used_by_after_partial, const CNodePtr &if_cnode,
+                          const FuncGraphPtr &after_fg, CNodePtr *then_partial_cnode);
   int CreateIfThenPartialNode(const FuncGraphPtr &main_fg,
-                              std::vector<AnfNodePtr> *fg_inputs_only_used_by_after_partial, CNodePtr *if_cnode,
-                              FuncGraphPtr *after_fg, CNodePtr *then_partial_cnode);
+                              std::vector<AnfNodePtr> *fg_inputs_only_used_by_after_partial, const CNodePtr &if_cnode,
+                              const FuncGraphPtr &after_fg, CNodePtr *then_partial_cnode);
   int CreateIfElsePartialNode(const FuncGraphPtr &main_fg,
-                              std::vector<AnfNodePtr> *fg_inputs_only_used_by_after_partial, const CNodePtr *if_cnode,
-                              const FuncGraphPtr *after_fg, CNodePtr *else_partial_cnode);
+                              std::vector<AnfNodePtr> *fg_inputs_only_used_by_after_partial, const CNodePtr &if_cnode,
+                              const FuncGraphPtr &after_fg, CNodePtr *else_partial_cnode);
   int ProcessIfOp(const FuncGraphPtr &fg, const std::set<AnfNodePtr> &visited_nodes,
                   const std::vector<AnfNodePtr> &remain_nodes, const AnfNodePtr &if_node);
 
