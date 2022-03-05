92abbd7af1b96d14119fb8e4e82db690d70b2454
yanghaoran <yanghaoran2@huawei.com>
2022-03-05 16:20:24 +0800

    takedown test_dynamic_ops.py::test_dynamic_layernorm due to probalistic failure

--- tests/st/ops/ascend/test_dynamic_ops.py
+++ tests/st/ops/ascend/test_dynamic_ops.py
@@ -116,7 +116,7 @@ class AddNet(nn.Cell):
         return ops.add(x, y)
 
 
-@pytest.mark.level0
+@pytest.mark.level1
 @pytest.mark.platform_arm_ascend_training
 @pytest.mark.platform_x86_ascend_training
 @pytest.mark.env_onecard
