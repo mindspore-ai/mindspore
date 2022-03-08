287e8f246b16d25e006c9e0fccba2eeb8163af8b
wilfChen <chenweifeng720@huawei.com>
2022-03-08 16:43:59 +0800

    priority replay buffer primitive

--- mindspore/python/mindspore/ops/_op_impl/cpu/__init__.py
+++ mindspore/python/mindspore/ops/_op_impl/cpu/__init__.py
@@ -74,3 +74,5 @@ from .pyfunc import _pyfunc_cpu
 from .buffer_append import _buffer_append_cpu
 from .buffer_get import _buffer_get_cpu
 from .buffer_sample import _buffer_sample_cpu
+from .priority_replay_buffer import _prb_push_op_cpu
+from .priority_replay_buffer import _prb_sample_op_cpu
