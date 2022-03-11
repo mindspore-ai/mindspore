278ed67a06219121fcc382bdb97b8668c82a8eb3
韩泽禹 <657155628@qq.com>
2022-03-11 12:03:15 +0800

    [feat][assistant][I48O6M]Add Tan operator

--- mindspore/core/ops/tan.cc
+++ mindspore/core/ops/tan.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020-2021 Huawei Technologies Co., Ltd
+ * Copyright 2020-2022 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,6 @@ namespace mindspore {
 namespace ops {
 namespace {
 abstract::ShapePtr TanInferShape(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) {
-  MS_EXCEPTION_IF_NULL(primitive);
   auto prim_name = primitive->name();
   (void)CheckAndConvertUtils::CheckArgs<abstract::AbstractTensor>(prim_name, input_args, 0);
   auto x = input_args[0]->BuildShape();
@@ -39,11 +38,11 @@ abstract::ShapePtr TanInferShape(const PrimitivePtr &primitive, const std::vecto
 }
 
 TypePtr TanInferType(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) {
-  MS_EXCEPTION_IF_NULL(primitive);
-  auto x_dtype = input_args[0]->BuildType();
-  const std::set<TypePtr> valid_types = {kFloat16, kFloat32, kInt32};
-  (void)CheckAndConvertUtils::CheckTensorTypeValid("x", x_dtype, valid_types, primitive->name());
-  return x_dtype;
+  auto prim_name = primitive->name();
+  const std::set<TypePtr> valid_types = {kInt32, kFloat16, kFloat32, kFloat64, kComplex64, kComplex128};
+  auto x_type = input_args[kInputIndex0]->BuildType();
+  (void)CheckAndConvertUtils::CheckTensorTypeValid("x", x_type, valid_types, prim_name);
+  return input_args[kInputIndex0]->BuildType();
 }
 }  // namespace
 
@@ -51,10 +50,11 @@ MIND_API_OPERATOR_IMPL(Tan, BaseOperator);
 AbstractBasePtr TanInfer(const abstract::AnalysisEnginePtr &, const PrimitivePtr &primitive,
                          const std::vector<AbstractBasePtr> &input_args) {
   MS_EXCEPTION_IF_NULL(primitive);
-  MS_EXCEPTION_IF_NULL(input_args[0]);
-  const int64_t kInputNum = 1;
-  CheckAndConvertUtils::CheckInputArgs(input_args, kGreaterEqual, kInputNum, primitive->name());
-  return abstract::MakeAbstract(TanInferShape(primitive, input_args), TanInferType(primitive, input_args));
+  const size_t input_num = 1;
+  (void)CheckAndConvertUtils::CheckInputArgs(input_args, kEqual, input_num, primitive->name());
+  auto types = TanInferType(primitive, input_args);
+  auto shapes = TanInferShape(primitive, input_args);
+  return abstract::MakeAbstract(shapes, types);
 }
 REGISTER_PRIMITIVE_EVAL_IMPL(Tan, prim::kPrimTan, TanInfer, nullptr, true);
 }  // namespace ops
