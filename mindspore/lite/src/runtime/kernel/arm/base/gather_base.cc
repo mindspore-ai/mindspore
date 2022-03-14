8a113a1c0a852c87f29430e1861d8c5f99cf1428
xuanyue <xuanyue@huawei.com>
2022-03-14 15:25:10 +0800

    unified cutting optimization for gather

--- mindspore/lite/src/runtime/kernel/arm/base/gather_base.cc
+++ mindspore/lite/src/runtime/kernel/arm/base/gather_base.cc
@@ -15,13 +15,15 @@
  */
 
 #include "src/runtime/kernel/arm/base/gather_base.h"
-#include <limits>
-#include "nnacl/base/gather_base.h"
+#include <algorithm>
 
 using mindspore::lite::RET_ERROR;
 using mindspore::lite::RET_OK;
 
 namespace mindspore::kernel {
+#ifdef SERVER_INFERENCE
+constexpr int kMinCostPerThread = 16384;
+#endif
 int GatherRun(const void *cdata, int task_id, float, float) {
   auto gather_kernel = reinterpret_cast<const GatherBaseCPUKernel *>(cdata);
   auto error_code = gather_kernel->DoGather(task_id);
@@ -43,39 +45,57 @@ int GatherBaseCPUKernel::Prepare() {
   return ReSize();
 }
 
-int GatherBaseCPUKernel::ReSize() { return ChooseThreadCuttingstrategy(); }
+int GatherBaseCPUKernel::ReSize() {
+  auto status = InitDynamicStatus();
+  if (status != RET_OK) {
+    MS_LOG(ERROR) << "Gather init status failed when resizing." << name_;
+    return status;
+  }
+  return ChooseThreadCuttingstrategy();
+}
 
 int GatherBaseCPUKernel::DoGather(int task_id) const {
   auto *int8_in = reinterpret_cast<int8_t *>(in_tensors_[FIRST_INPUT]->data());
   CHECK_NULL_RETURN(int8_in);
-  auto in_shape = in_tensors_[FIRST_INPUT]->shape();
-  MS_CHECK_LT(axis_, static_cast<int>(in_shape.size()), RET_ERROR);
-  const int64_t limit = in_shape.at(axis_);
   auto *int8_out = reinterpret_cast<int8_t *>(out_tensors_[FIRST_INPUT]->data());
   CHECK_NULL_RETURN(int8_out);
-  int data_size = static_cast<int>(lite::DataTypeSize(in_tensors_[FIRST_INPUT]->data_type()));
-  auto index_num = in_tensors_[SECOND_INPUT]->ElementsNum();
-  int64_t byte_inner_size = inner_size_ * data_size;
-  int64_t byte_out_stride = index_num * byte_inner_size;
-  int64_t all_count = split_by_index ? index_num : outer_size_;
-  int64_t count = (task_id < static_cast<int>(split_points_.size()) - 1)
-                    ? split_points_[task_id + 1] - split_points_[task_id]
-                    : all_count - split_points_[task_id];
-
-  int ret = RET_OK;
-  if (split_by_index) {
-    int *indices_data = indices_data_ + split_points_[task_id];
-    int8_out += split_points_[task_id] * byte_inner_size;
-    ret = Gather(int8_in, outer_size_, byte_inner_size, limit, indices_data, count, int8_out, byte_out_stride);
-  } else {
-    int8_in += split_points_[task_id] * limit * byte_inner_size;
-    int8_out += split_points_[task_id] * byte_out_stride;
-    ret = Gather(int8_in, count, byte_inner_size, limit, indices_data_, index_num, int8_out, byte_out_stride);
+  auto begin_batch = block_boundary_infos_[task_id].begin_batch;
+  auto begin_index = block_boundary_infos_[task_id].begin_index;
+  auto end_batch = block_boundary_infos_[task_id].end_batch;
+  auto end_index = block_boundary_infos_[task_id].end_index;
+  int64_t byte_in_stride = limit_ * byte_inner_size_;
+  int8_in += begin_batch * byte_in_stride;
+  int8_out += begin_batch * indices_size_ * byte_inner_size_ + begin_index * byte_inner_size_;
+  auto HandleCopy = [this, int8_out, int8_in, byte_in_stride](int64_t begin, int64_t end) mutable {
+    for (; begin < end; ++begin) {
+      int index = indices_data_[begin];
+      index = (index < 0 ? index + limit_ : index);
+      if (index < 0 || index >= limit_) {
+        memset(int8_out, 0, byte_inner_size_);
+      } else {
+        memcpy(int8_out, int8_in + index * byte_inner_size_, byte_inner_size_);
+      }
+      int8_out += byte_inner_size_;
+    }
+    int8_in += byte_in_stride;
+  };
+  if (begin_batch == end_batch) {
+    HandleCopy(begin_index, end_index);
+    return RET_OK;
   }
-  return ret;
+  HandleCopy(begin_index, indices_size_);
+  ++begin_batch;
+  for (; begin_batch < end_batch; ++begin_batch) {
+    HandleCopy(0, indices_size_);
+  }
+  HandleCopy(0, end_index);
+  return RET_OK;
 }
 
 int GatherBaseCPUKernel::Run() {
+  if (outer_size_ == 0 || indices_size_ == 0 || byte_inner_size_ == 0) {
+    return RET_OK;
+  }
   bool isIndicesInt32 = in_tensors_[SECOND_INPUT]->data_type() == kNumberTypeInt32;
   int ret = AssignIndicesData(isIndicesInt32);
   if (ret != RET_OK) {
@@ -83,7 +103,7 @@ int GatherBaseCPUKernel::Run() {
     return ret;
   }
 
-  ret = ParallelLaunch(this->ms_context_, GatherRun, this, thread_count_);
+  ret = ParallelLaunch(this->ms_context_, GatherRun, this, block_boundary_infos_.size());
   if (ret != RET_OK) {
     MS_LOG(ERROR) << "Gather function error error_code[" << ret << "]";
   }
@@ -94,39 +114,68 @@ int GatherBaseCPUKernel::Run() {
   return ret;
 }
 
-int GatherBaseCPUKernel::ChooseThreadCuttingstrategy() {
+int GatherBaseCPUKernel::InitDynamicStatus() {
   auto in_shape = in_tensors_[FIRST_INPUT]->shape();
   int in_rank = static_cast<int>(in_shape.size());
   MS_CHECK_TRUE_MSG(axis_ < in_rank, RET_ERROR, "gather's inputs are invalid.");
+  limit_ = in_shape[axis_];
   outer_size_ = 1;
   for (int i = 0; i < axis_; ++i) {
     outer_size_ *= in_shape.at(i);
   }
-  inner_size_ = 1;
+  byte_inner_size_ = static_cast<int64_t>(lite::DataTypeSize(out_tensors_.front()->data_type()));
   for (int i = axis_ + 1; i < in_rank; ++i) {
-    inner_size_ *= in_shape.at(i);
+    byte_inner_size_ *= in_shape.at(i);
   }
-  int64_t all_count = outer_size_;
-  auto index_num = in_tensors_[SECOND_INPUT]->ElementsNum();
-  if (outer_size_ >= index_num) {
-    split_by_index = false;
-  } else {
-    all_count = index_num;
-    split_by_index = true;
+  indices_size_ = in_tensors_[SECOND_INPUT]->ElementsNum();
+  return RET_OK;
+}
+
+int GatherBaseCPUKernel::ChooseThreadCuttingstrategy() {
+  block_boundary_infos_.clear();
+  if (outer_size_ == 0 || indices_size_ == 0 || byte_inner_size_ == 0) {
+    return RET_OK;
   }
-  int64_t count_step = MSMAX(all_count / op_parameter_->thread_num_, 1);
-  int64_t count_remaining = MSMAX(all_count - count_step * op_parameter_->thread_num_, 0);
-  split_points_.clear();
-  int64_t split_point = 0;
-  while (split_point < all_count) {
-    split_points_.push_back(split_point);
-    split_point += count_step;
-    if (count_remaining > 0) {
-      split_point += 1;
-      --count_remaining;
+  int64_t block_size = 1;
+#ifdef SERVER_INFERENCE
+  auto all_bytes = static_cast<int64_t>(out_tensors_.front()->Size());
+  if (all_bytes <= static_cast<int64_t>(kMinCostPerThread)) {
+    block_boundary_infos_.emplace_back(BlockBoundaryInfo{0, 0, outer_size_, 0});
+    return RET_OK;
+  }
+  auto total_block = outer_size_ * indices_size_;
+  int64_t min_block_per_unit = 1;
+  if (byte_inner_size_ < kMinCostPerThread) {
+    min_block_per_unit = UP_DIV(kMinCostPerThread, byte_inner_size_);
+  }
+  block_size = std::max(min_block_per_unit, total_block / op_parameter_->thread_num_);
+  int thread_num = MSMIN(UP_DIV(total_block, block_size), op_parameter_->thread_num_);
+#else
+  auto total_block = outer_size_ * indices_size_;
+  int thread_num = op_parameter_->thread_num_;
+#endif
+  if (thread_num < 1) {
+    thread_num = 1;
+  }
+  block_size = total_block / thread_num;
+  auto remain_block = total_block - block_size * thread_num;
+  int64_t start = 0;
+  while (start < total_block) {
+    BlockBoundaryInfo block_boundary_info{};
+    block_boundary_info.begin_batch = start / indices_size_;
+    block_boundary_info.begin_index = start % indices_size_;
+    start += block_size;
+    if (remain_block > 0) {
+      ++start;
+      --remain_block;
     }
+    if (start >= total_block) {
+      start = total_block;
+    }
+    block_boundary_info.end_batch = start / indices_size_;
+    block_boundary_info.end_index = start % indices_size_;
+    block_boundary_infos_.push_back(block_boundary_info);
   }
-  thread_count_ = static_cast<int>(split_points_.size());
   return RET_OK;
 }
 }  // namespace mindspore::kernel
