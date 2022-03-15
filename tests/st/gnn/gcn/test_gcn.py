f727bf4ad454bf627130fa629b61d433837bd294
yanghaoran <yanghaoran2@huawei.com>
2022-03-15 11:53:07 +0800

    takedown test_gcn due to probalistic failure

--- tests/st/gnn/gcn/test_gcn.py
+++ tests/st/gnn/gcn/test_gcn.py
@@ -31,7 +31,7 @@ TEST_NODE_NUM = 1000
 SEED = 20
 
 
-@pytest.mark.level0
+@pytest.mark.level1
 @pytest.mark.platform_arm_ascend_training
 @pytest.mark.platform_x86_ascend_training
 @pytest.mark.env_onecard
