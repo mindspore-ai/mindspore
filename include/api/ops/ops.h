bab6c1739907c225765c71f33f03e830b8c75dff
sunsuodong <sunsuodong@huawei.com>
2022-03-17 19:27:59 -0700

    code review

--- include/api/ops/ops.h
+++ include/api/ops/ops.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Huawei Technologies Co., Ltd
+ * Copyright 2020-2022 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ struct MS_API Conv2D : public OpCell<Conv2D> {
 
   Output operator()(const Input &, const Input &) const;
 
-  int out_channel;
+  int out_channel = 0;
   std::vector<int> kernel_size;
   int mode = 1;
   std::string pad_mode = "valid";
