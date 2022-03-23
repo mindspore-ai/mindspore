b8aab5cdf5610977c5d7dd83d330a98937335114
nizzan <nizzan.kimhi@huawei.com>
2022-03-23 11:51:05 +0200

    is_training BN mode

--- mindspore/lite/src/runtime/kernel/arm/fp32_grad/bn_grad.cc
+++ mindspore/lite/src/runtime/kernel/arm/fp32_grad/bn_grad.cc
@@ -124,7 +124,7 @@ int BNGradCPUKernel::Execute(int task_id) {
         }
       }
       if (thread_num == 1) {
-        backwardAll(x, yt, save_mean, save_var, scale, total, channels, dbias, dscale, dx);
+        backwardAll(x, yt, save_mean, save_var, scale, total, channels, dbias, dscale, dx, IsTrain());
       }
       break;
     }
@@ -134,7 +134,7 @@ int BNGradCPUKernel::Execute(int task_id) {
     }
     case 2: {
       backwardP2(x + task_id * stride * channels, yt + task_id * stride * channels, save_mean, save_var, dscale, dbias,
-                 scale, count, total, channels, dx + task_id * stride * channels);
+                 scale, count, total, channels, dx + task_id * stride * channels, IsTrain());
       break;
     }
   }
