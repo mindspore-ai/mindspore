9199ba8303a3e1071915273a4c8fba4921cba80e
Henry Shi <shixinyu1@huawei.com>
2022-03-23 22:38:44 +0800

    dynamic shape feed mode bug fix&api

--- mindspore/python/mindspore/_extends/parallel_compile/tbe_compiler/tbe_helper.py
+++ mindspore/python/mindspore/_extends/parallel_compile/tbe_compiler/tbe_helper.py
@@ -146,14 +146,13 @@ def get_single_io_arg(info):
         check_arg_info(info)
         del info['valid']
         del info['name']
+        if 'range' in info:
+            for i in range(len(info['range'])):
+                if info['range'][i][1] == -1:
+                    info['range'][i][1] = None
         res = info
     else:
         res = None
-    if 'range' in info:
-        for i in range(len(info['range'])):
-            if info['range'][i][1] == -1:
-                info['range'][i][1] = None
-        res = info
     return res
 
 
