28c06dbd76e059944ad557ce8d9ac838aabeecc2
xulei <xulei201@huawei.com>
2022-03-25 14:58:15 +0800

    fix mac compile

--- mindspore/core/ops/log1p.cc
+++ mindspore/core/ops/log1p.cc
@@ -51,7 +51,7 @@ MIND_API_BASE_IMPL(Log1p, PrimitiveC, BaseOperator);
 AbstractBasePtr Log1pInfer(const abstract::AnalysisEnginePtr &, const PrimitivePtr &primitive,
                            const std::vector<AbstractBasePtr> &input_args) {
   MS_EXCEPTION_IF_NULL(primitive);
-  for (const auto item : input_args) {
+  for (const auto &item : input_args) {
     MS_EXCEPTION_IF_NULL(item);
   }
   return abstract::MakeAbstract(Log1pInferShape(primitive, input_args), Log1pInferType(primitive, input_args));
