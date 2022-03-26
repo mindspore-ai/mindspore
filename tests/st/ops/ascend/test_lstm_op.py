8ed5d91c8a10191690bd7b331a77218b712650e1
Henry <shixinyu1@huawei.com>
2022-03-26 11:29:18 +0800

    CI

--- tests/st/ops/ascend/test_lstm_op.py
+++ tests/st/ops/ascend/test_lstm_op.py
@@ -87,7 +87,8 @@ class LSTMWeightBias():
         b_hh_list = ParameterTuple(b_hh_list)
         return w_ih_list, w_hh_list, b_ih_list, b_hh_list
 
-@pytest.mark.level1
+
+@pytest.mark.level0
 @pytest.mark.platform_arm_ascend_training
 @pytest.mark.platform_x86_ascend_training
 @pytest.mark.env_onecard
@@ -131,7 +132,8 @@ def test_sit_lstm_forward_input_3_32_32_is_32_hs_16():
     assert np.allclose(hy.asnumpy(), hy_pynative.asnumpy(), 0.002, 0.002)
     assert np.allclose(cy.asnumpy(), cy_pynative.asnumpy(), 0.002, 0.002)
 
-@pytest.mark.level1
+
+@pytest.mark.level0
 @pytest.mark.platform_arm_ascend_training
 @pytest.mark.platform_x86_ascend_training
 @pytest.mark.env_onecard
