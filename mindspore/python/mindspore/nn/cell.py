8b32805954f678d3bbd3c562a1afcb7d21fa19f4
Henry <shixinyu1@huawei.com>
2022-03-26 17:36:20 +0800

    dynamic shape feed mode example fix

--- mindspore/python/mindspore/nn/cell.py
+++ mindspore/python/mindspore/nn/cell.py
@@ -878,10 +878,22 @@ class Cell(Cell_):
             inputs (tuple): Inputs of the Cell object.
 
         Examples:
-            >>> n = Net()
-            >>> input_dyn = Tensor(shape = [3, None], dtype=type)
+            >>> import numpy as np
+            >>> import mindspore
+            >>> from mindspore import nn, Tensor, context
+            >>>
+            >>> context.set_context(mode=context.GRAPH_MODE, device_target="Ascend")
+            >>> class reluNet(nn.Cell):
+            >>>     def __init__(self):
+            >>>         super(reluNet, self).__init__()
+            >>>         self.relu = nn.ReLU()
+            >>>     def construct(self, x):
+            >>>         return self.relu(x)
+            >>>
+            >>> net = reluNet()
+            >>> input_dyn = Tensor(shape=[3, None], dtype=mindspore.float32)
             >>> net.set_inputs(input_dyn)
-            >>> input1 = Tensor(np.random.random([3, 10]), dtype=type)
+            >>> input1 = Tensor(np.random.random([3, 10]), dtype=mindspore.float32)
             >>> output = net(input1)
 
         NOTE:
