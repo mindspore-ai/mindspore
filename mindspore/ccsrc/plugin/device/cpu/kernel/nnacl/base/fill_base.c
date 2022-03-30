9e0d2526c9ec3af8109731f596205312029174db
greatpanc <changpan_yin@163.com>
2022-03-30 19:15:05 +0800

    avx512 cast fill opt

--- mindspore/ccsrc/plugin/device/cpu/kernel/nnacl/base/fill_base.c
+++ mindspore/ccsrc/plugin/device/cpu/kernel/nnacl/base/fill_base.c
@@ -16,22 +16,44 @@
 
 #include "nnacl/base/fill_base.h"
 
+// 32 bits, block_size : (512/256/128/32), block_num : (16/8/4/1)
+#define SimdFillFp32CoreCalc(block_size, block_num, output, size, data, index)                  \
+  for (int block_max_size = size - block_num + 1; index < block_max_size; index += block_num) { \
+    MS_ST_F32(block_size, output + index, MS_MOVN_F32(block_size, data));                       \
+  }
+
 int FillFp32(float *output, int size, float data) {
   if (output == NULL) {
     return NNACL_NULL_PTR;
   }
-  for (int i = 0; i < size; ++i) {
-    output[i] = data;
+
+  int index = 0;
+
+  MS_SIMD_RUN_NO_SCALAR(SimdFillFp32CoreCalc, output, size, data, index);
+
+  for (; index < size; ++index) {
+    output[index] = data;
   }
   return NNACL_OK;
 }
+
+// 32 bits, block_size : (512/256/128/32), block_num : (16/8/4/1)
+#define SimdFillInt32CoreCalc(block_size, block_num, output, size, data, index)                 \
+  for (int block_max_size = size - block_num + 1; index < block_max_size; index += block_num) { \
+    MS_ST_EPI32(block_size, output + index, MS_MOVN_EPI32(block_size, data));                   \
+  }
 
 int FillInt32(int *output, int size, int data) {
   if (output == NULL) {
     return NNACL_NULL_PTR;
   }
-  for (int i = 0; i < size; ++i) {
-    output[i] = data;
+
+  int index = 0;
+
+  MS_SIMD_RUN_NO_SCALAR(SimdFillInt32CoreCalc, output, size, data, index);
+
+  for (; index < size; ++index) {
+    output[index] = data;
   }
   return NNACL_OK;
 }
