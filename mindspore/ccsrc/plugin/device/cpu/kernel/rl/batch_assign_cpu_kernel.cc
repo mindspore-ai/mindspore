28a398a98f37ac2b4d017ea4e29f0978414985e6
VectorSL <shiliang10@huawei.com>
2022-04-07 15:13:17 +0800

    rl add cpu kernel: batch assigin

---
+++ mindspore/ccsrc/plugin/device/cpu/kernel/rl/batch_assign_cpu_kernel.cc
@@ -0,0 +1,78 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "plugin/device/cpu/kernel/rl/batch_assign_cpu_kernel.h"
+#include <memory>
+#include "kernel/common_utils.h"
+namespace mindspore {
+namespace kernel {
+constexpr size_t kHalf = 2;
+std::shared_mutex BatchAssignCpuBaseMod::rw_mutex_;
+
+BatchAssignCpuKernelMod::BatchAssignCpuKernelMod() : elements_num_(0), lock_(false) {}
+
+void BatchAssignCpuKernelMod::InitKernel(const CNodePtr &kernel_node) {
+  MS_EXCEPTION_IF_NULL(kernel_node);
+  kernel_name_ = common::AnfAlgo::GetCNodeName(kernel_node);
+  lock_ = common::AnfAlgo::GetNodeAttr<bool>(kernel_node, "lock");
+  size_t input_num = common::AnfAlgo::GetInputNum(kernel_node);
+  elements_num_ = input_num / kHalf;
+  // Compute the size for each input. There has two input lists.
+  // Each list has the same elements number, shape series， type series.
+  for (size_t i = 0; i < elements_num_; i++) {
+    auto type = AnfAlgo::GetInputDeviceDataType(kernel_node, i);
+    size_t element_size = GetTypeByte(TypeIdToType(type));
+    auto shape = AnfAlgo::GetInputDeviceShape(kernel_node, i);
+    for (auto x : shape) {
+      element_size *= LongToSize(x);
+    }
+    input_size_list_.push_back(element_size);
+  }
+  // Set input size for another input list.
+  for (size_t i = 0; i < elements_num_; i++) {
+    input_size_list_.push_back(input_size_list_[i]);
+  }
+  // Set an output for placeholder.
+  output_size_list_.push_back(sizeof(float));
+}
+
+bool BatchAssignCpuKernelMod::Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &,
+                                     const std::vector<AddressPtr> &) {
+  if (lock_) {
+    std::unique_lock<std::shared_mutex> lock(rw_mutex_);
+  } else {
+    std::shared_lock<std::shared_mutex> lock(rw_mutex_);
+  }
+  // Usually, we will get two inputs list, the first half are the weights to be updated, and the last half
+  // are the sources. So we just copy the source to overwrite the dst.
+  auto task = [this, &inputs](size_t start, size_t end) {
+    for (size_t i = start; i < end; i++) {
+      auto local_addr = GetDeviceAddress<unsigned char>(inputs, i);
+      auto source_addr = GetDeviceAddress<unsigned char>(inputs, i + elements_num_);
+      MS_EXCEPTION_IF_NULL(local_addr);
+      MS_EXCEPTION_IF_NULL(source_addr);
+      auto ret = memcpy_s(local_addr, input_size_list_[i], source_addr, input_size_list_[i + elements_num_]);
+      if (ret != EOK) {
+        MS_LOG(EXCEPTION) << kernel_name_ << " memcpy failed, errorno(" << ret << ")";
+      }
+    }
+  };
+  ParallelLaunchAutoSearch(task, elements_num_, this, &parallel_search_info_);
+  return true;
+}
+
+MS_KERNEL_FACTORY_REG(NativeCpuKernelMod, BatchAssign, BatchAssignCpuKernelMod);
+}  // namespace kernel
+}  // namespace mindspore
---
+++ mindspore/ccsrc/plugin/device/cpu/kernel/rl/batch_assign_cpu_kernel.h
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_RL_BATCH_ASSIGN_CPU_KERNEL_H_
+#define MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_RL_BATCH_ASSIGN_CPU_KERNEL_H_
+
+#include <plugin/device/cpu/kernel/rl/batch_assign_cpu_base.h>
+#include <string>
+#include <vector>
+#include "plugin/factory/ms_factory.h"
+
+namespace mindspore {
+namespace kernel {
+class BatchAssignCpuKernelMod : public BatchAssignCpuBaseMod {
+ public:
+  BatchAssignCpuKernelMod();
+  ~BatchAssignCpuKernelMod() = default;
+
+  bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
+              const std::vector<AddressPtr> &outputs) override;
+  void InitKernel(const CNodePtr &kernel_node) override;
+
+ protected:
+  std::vector<KernelAttr> GetOpSupport() override {
+    static std::vector<KernelAttr> support_list = {
+      KernelAttr().AddAllSameAttr(true).AddInputAttr(kNumberTypeBool).AddOutputAttr(kNumberTypeInt64),
+      KernelAttr().AddAllSameAttr(true).AddInputAttr(kNumberTypeFloat16).AddOutputAttr(kNumberTypeInt64),
+      KernelAttr().AddAllSameAttr(true).AddInputAttr(kNumberTypeFloat32).AddOutputAttr(kNumberTypeInt64),
+      KernelAttr().AddAllSameAttr(true).AddInputAttr(kNumberTypeFloat64).AddOutputAttr(kNumberTypeInt64),
+      KernelAttr().AddAllSameAttr(true).AddInputAttr(kNumberTypeInt32).AddOutputAttr(kNumberTypeInt64),
+      KernelAttr().AddAllSameAttr(true).AddInputAttr(kNumberTypeInt16).AddOutputAttr(kNumberTypeInt64),
+      KernelAttr().AddAllSameAttr(true).AddInputAttr(kNumberTypeInt64).AddOutputAttr(kNumberTypeInt64),
+      KernelAttr().AddAllSameAttr(true).AddInputAttr(kNumberTypeInt8).AddOutputAttr(kNumberTypeInt64),
+      KernelAttr().AddAllSameAttr(true).AddInputAttr(kNumberTypeUInt32).AddOutputAttr(kNumberTypeInt64),
+      KernelAttr().AddAllSameAttr(true).AddInputAttr(kNumberTypeUInt16).AddOutputAttr(kNumberTypeInt64),
+      KernelAttr().AddAllSameAttr(true).AddInputAttr(kNumberTypeUInt64).AddOutputAttr(kNumberTypeInt64),
+      KernelAttr().AddAllSameAttr(true).AddInputAttr(kNumberTypeUInt8).AddOutputAttr(kNumberTypeInt64)};
+    return support_list;
+  }
+
+ private:
+  size_t elements_num_;
+  bool lock_;
+};
+}  // namespace kernel
+}  // namespace mindspore
+
+#endif  // MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_RL_BATCH_ASSIGN_CPU_KERNEL_H_
