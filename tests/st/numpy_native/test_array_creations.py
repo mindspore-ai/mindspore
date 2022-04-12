a0402e2c9ed3b3da97acfd2390e46eb1f889cf19
yanghaoran <yanghaoran2@huawei.com>
2022-04-12 19:54:15 +0800

    attempt to restore windows st failed, revert testcases that were taken down

--- tests/st/numpy_native/test_array_creations.py
+++ tests/st/numpy_native/test_array_creations.py
@@ -595,7 +595,7 @@ def onp_trace(arr):
     return onp.trace(arr, offset=4, axis1=1, axis2=2)
 
 
-@pytest.mark.level1
+@pytest.mark.level0
 @pytest.mark.platform_arm_ascend_training
 @pytest.mark.platform_x86_ascend_training
 @pytest.mark.platform_x86_gpu_training
