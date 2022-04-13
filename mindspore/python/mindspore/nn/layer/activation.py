7b7c7494c050ef8ddd170803887debbd5bb00056
huodagu <yuhan24@h-partners.com>
2022-04-13 14:23:44 +0800

    modify pagedisplay

--- mindspore/python/mindspore/nn/layer/activation.py
+++ mindspore/python/mindspore/nn/layer/activation.py
@@ -270,7 +270,7 @@ class ReLU(Cell):
 
     Inputs:
         - **x** (Tensor) - The input of ReLU is a Tensor of any dimension. The data type is `number <https://www.mind
-        spore.cn/docs/en/master/api_python/mindspore.html#mindspore.dtype>`_ .
+          spore.cn/docs/en/master/api_python/mindspore.html#mindspore.dtype>`_ .
 
     Outputs:
         Tensor, with the same type and shape as the `x`.
