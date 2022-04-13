7df88afe44d358b4badf353729d80bd5d1526263
TinaMengtingZhang <tina.mengting.zhang@huawei.com>
2022-04-13 16:59:09 -0400

    Develop SquareSumAll cpu op

---
+++ mindspore/ccsrc/plugin/device/cpu/kernel/square_sum_all_cpu_kernel.cc
@@ -0,0 +1,93 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "plugin/device/cpu/kernel/square_sum_all_cpu_kernel.h"
+#include <functional>
+#include "plugin/device/cpu/hal/device/cpu_device_address.h"
+
+namespace mindspore {
+namespace kernel {
+namespace {
+constexpr size_t kSquareSumAllInputsNum = 2;
+constexpr size_t kSquareSumAllOutputsNum = 2;
+constexpr float kPowerSquareExp = 2.0;
+
+template <typename T>
+void SquareSum(const T *in0, const T *in1, T *out0, T *out1, size_t start, size_t end) {
+  for (size_t index = start; index < end; index++) {
+    // as the size of both two input tensors are known to be identical, we can compute sum of two tensors in one for
+    // loop.
+    size_t split = end / kSquareSumAllInputsNum;
+    if (index < split) {
+      auto ret = pow(in0[index], T(kPowerSquareExp));
+      out0[0] = (index == 0) ? ret : out0[0] + ret;
+    } else {
+      auto ret = pow(in1[index - split], T(kPowerSquareExp));
+      out1[0] = (index == split) ? ret : out1[0] + ret;
+    }
+  }
+}
+}  // namespace
+
+void SquareSumAllCpuKernelMod::InitKernel(const CNodePtr &kernel_node) {
+  MS_EXCEPTION_IF_NULL(kernel_node);
+  kernel_name_ = common::AnfAlgo::GetCNodeName(kernel_node);
+  auto input_shape = AnfAlgo::GetInputDeviceShape(kernel_node, 0);
+  input_size_ = std::accumulate(input_shape.begin(), input_shape.end(), 1, std::multiplies<size_t>());
+  dtype_ = AnfAlgo::GetInputDeviceDataType(kernel_node, 0);
+}
+
+bool SquareSumAllCpuKernelMod::Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
+                                      const std::vector<AddressPtr> &outputs) {
+  bool ret = true;
+  CHECK_KERNEL_INPUTS_NUM(inputs.size(), kSquareSumAllInputsNum, kernel_name_);
+  CHECK_KERNEL_OUTPUTS_NUM(outputs.size(), kSquareSumAllOutputsNum, kernel_name_);
+  if (dtype_ == kNumberTypeFloat16) {
+    ret = LaunchKernel<float16>(inputs, outputs);
+  } else if (dtype_ == kNumberTypeFloat32) {
+    ret = LaunchKernel<float>(inputs, outputs);
+  } else {
+    MS_EXCEPTION(TypeError) << "Unsupported input data type for operator [" << kernel_name_
+                            << "]: " << TypeIdToType(dtype_)->ToString();
+  }
+  return ret;
+}
+
+template <typename T>
+bool SquareSumAllCpuKernelMod::LaunchKernel(const std::vector<kernel::AddressPtr> &inputs,
+                                            const std::vector<kernel::AddressPtr> &outputs) {
+  const T *input_0_addr = reinterpret_cast<T *>(inputs[0]->addr);
+  const T *input_1_addr = reinterpret_cast<T *>(inputs[1]->addr);
+  T *output_0_addr = reinterpret_cast<T *>(outputs[0]->addr);
+  T *output_1_addr = reinterpret_cast<T *>(outputs[1]->addr);
+  auto task = std::bind(SquareSum<T>, input_0_addr, input_1_addr, output_0_addr, output_1_addr, std::placeholders::_1,
+                        std::placeholders::_2);
+  ParallelLaunchAutoSearch(task, input_size_ * kSquareSumAllInputsNum, this, &parallel_search_info_);
+  return true;
+}
+
+std::vector<KernelAttr> SquareSumAllCpuKernelMod::GetOpSupport() {
+  static std::vector<KernelAttr> support_list = {
+    KernelAttr().AddAllSameAttr(true).AddInputAttr(kNumberTypeFloat16).AddOutputAttr(kNumberTypeFloat16),
+
+    KernelAttr().AddAllSameAttr(true).AddInputAttr(kNumberTypeFloat32).AddOutputAttr(kNumberTypeFloat32)};
+
+  return support_list;
+}
+
+MS_KERNEL_FACTORY_REG(NativeCpuKernelMod, SquareSumAll, SquareSumAllCpuKernelMod);
+}  // namespace kernel
+}  // namespace mindspore
---
+++ tests/st/ops/cpu/test_square_sum_all.py
@@ -0,0 +1,112 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+import numpy as np
+import pytest
+import mindspore.context as context
+from mindspore import Tensor
+from mindspore.nn import Cell
+import mindspore.ops.operations as P
+
+
+class Net(Cell):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.squaresumall = P.SquareSumAll()
+
+    def construct(self, x0, x1):
+        return self.squaresumall(x0, x1)
+
+
+def run_net(datatype, input_tensors, output_tensors):
+    inp0 = Tensor(np.array(input_tensors[0]).astype(datatype))
+    inp1 = Tensor(np.array(input_tensors[1]).astype(datatype))
+    net = Net()
+    [output0, output1] = net(inp0, inp1)
+    expect0 = Tensor(output_tensors[0])
+    expect1 = Tensor(output_tensors[1])
+    assert output0 == expect0
+    assert output1 == expect1
+    assert output0.dtype == inp0.dtype
+    assert output1.dtype == inp1.dtype
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('dtype', [np.float16, np.float32])
+@pytest.mark.parametrize('input_tensors, output_tensors', [
+    ([[1, 2, 4], [0, 1, -1]], [21, 2]),
+    ([[[1, 2], [3, 4]], [[0, 0], [1, 0]]], [30, 1])
+])
+def test_cpu(dtype, input_tensors, output_tensors):
+    """
+    Feature: SquareSumAll cpu op.
+    Description: test data type is float16 and float32.
+    Expectation: success.
+    """
+    context.set_context(mode=context.GRAPH_MODE, device_target="CPU")
+    run_net(dtype, input_tensors, output_tensors)
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_cpu_exception_dtype_diff():
+    """
+    Feature: SquareSumAll cpu op.
+    Description: Test data type of two input tensors is different.
+    Expectation: Throw TypeError exception.
+    """
+    context.set_context(mode=context.GRAPH_MODE, device_target="CPU")
+    with pytest.raises(TypeError):
+        inp0 = Tensor(np.array([1, 2, 4]).astype(np.float16))
+        inp1 = Tensor(np.array([0, 1, -1]).astype(np.float32))
+        net = Net()
+        _ = net(inp0, inp1)
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_cpu_exception_dtype_not_support():
+    """
+    Feature: SquareSumAll cpu op.
+    Description: Test unsupported data type.
+    Expectation: Throw TypeError exception.
+    """
+    context.set_context(mode=context.GRAPH_MODE, device_target="CPU")
+    with pytest.raises(TypeError):
+        inp0 = Tensor(np.array([1, 2, 4]).astype(np.float64))
+        inp1 = Tensor(np.array([0, 1, -1]).astype(np.float64))
+        net = Net()
+        _ = net(inp0, inp1)
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_cpu_exception_shape_diff():
+    """
+    Feature: SquareSumAll cpu op.
+    Description: Test shape of two input tensors is different.
+    Expectation: Throw ValueError exception.
+    """
+    context.set_context(mode=context.GRAPH_MODE, device_target="CPU")
+    with pytest.raises(ValueError):
+        inp0 = Tensor(np.array([1, 2, 4]).astype(np.float32))
+        inp1 = Tensor(np.array([0, 1]).astype(np.float32))
+        net = Net()
+        _ = net(inp0, inp1)
