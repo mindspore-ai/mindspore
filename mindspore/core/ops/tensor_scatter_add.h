cb754fa39a8fe951488581d1d3843e96baef4ed7
liangzelang <liangzelang1@huawei.com>
2022-04-24 09:45:44 +0800

    Add c++ primitive and infer func for TensorScatterAdd/Sub/Max/Min

---
+++ mindspore/core/ops/tensor_scatter_add.h
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MINDSPORE_CORE_OPS_TENSOR_SCATTER_ADD_H_
+#define MINDSPORE_CORE_OPS_TENSOR_SCATTER_ADD_H_
+#include <memory>
+#include <vector>
+
+#include "ops/base_operator.h"
+#include "mindapi/base/types.h"
+
+namespace mindspore {
+namespace ops {
+constexpr auto kNameTensorScatterAdd = "TensorScatterAdd";
+/// \brief Creates a new tensor by adding the values from the positions in input_x indicated by indices, with values
+/// from updates. Refer to Python API @ref mindspore.ops.TensorScatterAdd for more details.
+class MIND_API TensorScatterAdd : public BaseOperator {
+ public:
+  MIND_API_BASE_MEMBER(TensorScatterAdd);
+  /// \brief Constructor.
+  TensorScatterAdd() : BaseOperator(kNameTensorScatterAdd) { InitIOName({"input_x", "indices", "updates"}, {"y"}); }
+};
+
+using kPrimTensorScatterAddPtr = std::shared_ptr<TensorScatterAdd>;
+}  // namespace ops
+}  // namespace mindspore
+
+#endif  // MINDSPORE_CORE_OPS_TENSOR_SCATTER_ADD_H_
---
+++ mindspore/core/ops/tensor_scatter_max.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MINDSPORE_CORE_OPS_TENSOR_SCATTER_MAX_H_
+#define MINDSPORE_CORE_OPS_TENSOR_SCATTER_MAX_H_
+#include <memory>
+#include <vector>
+
+#include "ops/base_operator.h"
+#include "mindapi/base/types.h"
+
+namespace mindspore {
+namespace ops {
+constexpr auto kNameTensorScatterMax = "TensorScatterMax";
+/// \brief By comparing the value at the position indicated by the index in input_x with the value in the update, the
+/// value at the index will eventually be equal to the largest one to create a new tensor. Refer to Python API @ref
+/// mindspore.ops.TensorScatterMax for more details.
+class MIND_API TensorScatterMax : public BaseOperator {
+ public:
+  MIND_API_BASE_MEMBER(TensorScatterMax);
+  /// \brief Constructor.
+  TensorScatterMax() : BaseOperator(kNameTensorScatterMax) { InitIOName({"input_x", "indices", "updates"}, {"y"}); }
+};
+
+using kPrimTensorScatterMaxPtr = std::shared_ptr<TensorScatterMax>;
+}  // namespace ops
+}  // namespace mindspore
+
+#endif  // MINDSPORE_CORE_OPS_TENSOR_SCATTER_MAX_H_
