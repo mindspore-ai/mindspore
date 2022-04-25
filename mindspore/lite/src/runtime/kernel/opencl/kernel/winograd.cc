63af3f4059eaf20aef524d5318bc1732345d4158
greatpanc <changpan_yin@163.com>
2022-04-25 20:07:35 +0800

    gpu quant fp16bit bugfix

--- mindspore/lite/src/runtime/kernel/opencl/kernel/winograd.cc
+++ mindspore/lite/src/runtime/kernel/opencl/kernel/winograd.cc
@@ -27,20 +27,24 @@ namespace mindspore::kernel {
 namespace {
 void Align(const std::vector<int> &global, const std::vector<int> &local, cl::NDRange *global_range,
            cl::NDRange *local_range) {
-  *local_range = cl::NDRange(local[0], local[1], local[2]);
-  *global_range =
-    cl::NDRange(UP_ROUND(global[0], local[0]), UP_ROUND(global[1], local[1]), UP_ROUND(global[2], local[2]));
+  *local_range = cl::NDRange(local[CLIDX_X], local[CLIDX_Y], local[CLIDX_Z]);
+  *global_range = cl::NDRange(UP_ROUND(global[CLIDX_X], local[CLIDX_X]), UP_ROUND(global[CLIDX_Y], local[CLIDX_Y]),
+                              UP_ROUND(global[CLIDX_Z], local[CLIDX_Z]));
 }
 
 constexpr float Gt[] = {1.0000000000, 1.0000000000, 1.0000000000,  1.0000000000, 1.0000000000,  0.0000000000,
                         0.0000000000, 0.7071067691, -0.7071067691, 1.4142135382, -1.4142135382, 0.0000000000,
                         0.0000000000, 0.4999999702, 0.4999999702,  1.9999998808, 1.9999998808,  1.0000000000};
 
-#ifndef ENABLE_ARM64
 constexpr float G[] = {1.0000000000, 0.0000000000,  0.0000000000, 1.0000000000, 0.7071067691, 0.4999999702,
                        1.0000000000, -0.7071067691, 0.4999999702, 1.0000000000, 1.4142135382, 1.9999998808,
                        1.0000000000, -1.4142135382, 1.9999998808, 0.0000000000, 0.0000000000, 1.0000000000};
-std::vector<float> GenerateWinogradFilter(void *src, TypeId dtype, size_t CO, size_t CI) {
+const int F43_IH = 3;
+const int F43_IW = 3;
+const int F43_OH = 6;
+const int F43_OW = 6;
+
+std::vector<float> GenerateWinogradF43Filter(void *src, TypeId dtype, size_t CO, size_t CI) {
 #ifdef ENABLE_FP16
   auto src_fp32 = reinterpret_cast<float *>(src);
   auto src_fp16 = reinterpret_cast<float16_t *>(src);
@@ -56,32 +60,31 @@ std::vector<float> GenerateWinogradFilter(void *src, TypeId dtype, size_t CO, si
   access_func = [=](int idx) { return src_fp32[idx]; };
 #endif
   // OHWI -> O66I
-  std::vector<float> dst(CO * 6 * 6 * CI);
+  std::vector<float> dst(CO * F43_OH * F43_OW * CI);
   if (src == nullptr) {
     return dst;
   }
   for (size_t co = 0; co < CO; ++co) {
     for (size_t ci = 0; ci < CI; ++ci) {
-      float in_vals[9];
-      for (int kh = 0; kh < 3; ++kh) {
-        for (int kw = 0; kw < 3; ++kw) {
-          const int f_index = ((co * 3 + kh) * 3 + kw) * CI + ci;
-          in_vals[kh * 3 + kw] = access_func(f_index);
+      float in_vals[F43_IH * F43_IW];
+      for (int kh = 0; kh < F43_IH; ++kh) {
+        for (int kw = 0; kw < F43_IW; ++kw) {
+          const int f_index = ((co * F43_IH + kh) * F43_IW + kw) * CI + ci;
+          in_vals[kh * F43_IW + kw] = access_func(f_index);
         }
       }
-      auto temp_vals = MatrixMultiply(G, in_vals, 6, 3, 3);
-      auto out_vals = MatrixMultiply(temp_vals.data(), Gt, 6, 3, 6);
-      for (int kh = 0; kh < 6; ++kh) {
-        for (int kw = 0; kw < 6; ++kw) {
-          const int f_index = ((co * 6 + kh) * 6 + kw) * CI + ci;
-          dst[f_index] = out_vals[kh * 6 + kw];
+      auto temp_vals = MatrixMultiply(G, in_vals, F43_OH, F43_IH, F43_IW);
+      auto out_vals = MatrixMultiply(temp_vals.data(), Gt, F43_OH, F43_IH, F43_OW);
+      for (int kh = 0; kh < F43_OH; ++kh) {
+        for (int kw = 0; kw < F43_OW; ++kw) {
+          const int f_index = ((co * F43_OH + kh) * F43_OW + kw) * CI + ci;
+          dst[f_index] = out_vals[kh * F43_OW + kw];
         }
       }
     }
   }
   return dst;
 }
-#endif
 }  // namespace
 
 int WinogradOpenCLKernel::BuildKernel() {
@@ -117,7 +120,7 @@ int WinogradOpenCLKernel::InitFilter() {
   size_t size = 0;
   int Ogroup = 2;
   if (filter_type_ == MemType::IMG) {
-    size_t width = 6 * 6 * UP_ROUND(CI_, CI_TILE);
+    size_t width = F43_OH * F43_OW * UP_ROUND(CI_, CI_TILE);
     size_t height = CO_SLICES_;
     size_t dtype = use_fp16_ ? CL_HALF_FLOAT : CL_FLOAT;
     size = width * height * CO_TILE * sizeof_FLT_;
@@ -127,7 +130,7 @@ int WinogradOpenCLKernel::InitFilter() {
       return RET_ERROR;
     }
   } else {
-    size = UP_DIV(CO_SLICES_, Ogroup) * 6 * 6 * CI_SLICES_ * Ogroup * CI_TILE * CO_TILE * sizeof_FLT_;
+    size = UP_DIV(CO_SLICES_, Ogroup) * F43_OH * F43_OW * CI_SLICES_ * Ogroup * CI_TILE * CO_TILE * sizeof_FLT_;
     packed_filter_ = allocator->Malloc(size, MemType::BUF);
     if (packed_filter_ == nullptr) {
       MS_LOG(ERROR) << "Malloc failed.";
@@ -139,32 +142,17 @@ int WinogradOpenCLKernel::InitFilter() {
   auto filter_tensor = in_tensors_.at(1);
   void *src_filter_data = stored_filter_ == nullptr ? filter_tensor->data() : stored_filter_;
   MS_ASSERT(src_filter_data);
-#ifndef ENABLE_ARM64
-  auto winograd_filter = GenerateWinogradFilter(src_filter_data, filter_tensor->data_type(), CO_, CI_);
+
+  auto winograd_filter = GenerateWinogradF43Filter(src_filter_data, filter_tensor->data_type(), CO_, CI_);
   void *src_data = winograd_filter.data();
-#else
-  auto winograd_filter = std::make_unique<float[]>(CO_ * 6 * 6 * CI_);
-  if (winograd_filter == nullptr) {
-    MS_LOG(ERROR) << "new winograd_filter failed.";
-    return RET_ERROR;
-  }
-  int trans_ret =
-    WinogradWeightTransform(reinterpret_cast<const float *>(src_filter_data),
-                            reinterpret_cast<float *>(winograd_filter.get()), nullptr, Gt, 1, 6, 3, CI_, CO_, false);
-  if (trans_ret != NNACL_OK) {
-    MS_LOG(ERROR) << "WinogradWeightTransform failed.";
-    return RET_ERROR;
-  }
-  void *src_data = winograd_filter.get();
-#endif
 
   auto src_dtype = kNumberTypeFloat32;
   auto dst_dtype = use_fp16_ ? kNumberTypeFloat16 : kNumberTypeFloat32;
   std::vector<char> tmp(size, 0);
   if (filter_type_ == MemType::IMG) {
-    ConvertFilter(src_data, tmp.data(), src_dtype, dst_dtype, OHWI, OHWIOgroupI4O4, CO_, 6, 6, CI_);
+    ConvertFilter(src_data, tmp.data(), src_dtype, dst_dtype, OHWI, OHWIOgroupI4O4, CO_, F43_OH, F43_OW, CI_);
   } else {
-    ConvertFilter(src_data, tmp.data(), src_dtype, dst_dtype, OHWI, OHWIOgroupI4O4, CO_, 6, 6, CI_, Ogroup);
+    ConvertFilter(src_data, tmp.data(), src_dtype, dst_dtype, OHWI, OHWIOgroupI4O4, CO_, F43_OH, F43_OW, CI_, Ogroup);
   }
 
   // unmap
@@ -190,7 +178,7 @@ int WinogradOpenCLKernel::AllocateMemory() {
   size_t img_dtype = use_fp16_ ? CL_HALF_FLOAT : CL_FLOAT;
 
   size_t width = TILE_HW_;
-  size_t height = CI_SLICES_ * 36;
+  size_t height = CI_SLICES_ * F43_OH * F43_OW;
   winograd_mem0_ = allocator->Malloc({width, height, img_dtype});
   if (winograd_mem0_ == nullptr) {
     MS_LOG(ERROR) << "Malloc failed.";
@@ -198,7 +186,7 @@ int WinogradOpenCLKernel::AllocateMemory() {
   }
 
   width = TILE_HW_;
-  height = CO_SLICES_ * 36;
+  height = CO_SLICES_ * F43_OH * F43_OW;
   winograd_mem1_ = allocator->Malloc({width, height, img_dtype});
   if (winograd_mem1_ == nullptr) {
     MS_LOG(ERROR) << "Malloc failed.";
@@ -264,9 +252,9 @@ int WinogradOpenCLKernel::SetConstArgs() {
     return RET_ERROR;
   }
 
-  arg_cn = 2;
+  arg_cn = CLARGSINDEX2;
   cl_int4 output_shape = {batch_size_, OH_, OW_, CO_SLICES_};
-  if (ocl_runtime_->SetKernelArg(kernel_36to4x4_, 0, winograd_mem1_) != CL_SUCCESS) {
+  if (ocl_runtime_->SetKernelArg(kernel_36to4x4_, CLARGSINDEX0, winograd_mem1_) != CL_SUCCESS) {
     MS_LOG(ERROR) << "SetKernelArg failed.";
     return RET_ERROR;
   }
@@ -294,9 +282,10 @@ int WinogradOpenCLKernel::SetConstArgs() {
 }
 
 int WinogradOpenCLKernel::SetGlobalLocal() {
-  Align({TILE_HW_, 6, CI_SLICES_}, {8, 6, 4}, &global_4x4to36_, &local_4x4to36_);
-  Align({UP_DIV(TILE_HW_, 2), 36, UP_DIV(CO_SLICES_, 2)}, {8, 3, 8}, &global_range_, &local_range_);
-  Align({TILE_HW_, 4, CO_SLICES_}, {4, 4, 8}, &global_36to4x4_, &local_36to4x4_);
+  Align({TILE_HW_, F43_OH, CI_SLICES_}, {8, F43_OW, 4}, &global_4x4to36_, &local_4x4to36_);  // local {8, 6, 4}
+  Align({UP_DIV(TILE_HW_, C2NUM), F43_OH * F43_OW, UP_DIV(CO_SLICES_, C2NUM)}, {8, 3, 8},    // local {8, 3, 8}
+        &global_range_, &local_range_);
+  Align({TILE_HW_, 4, CO_SLICES_}, {4, 4, 8}, &global_36to4x4_, &local_36to4x4_);  // local {4, 4, 8}
 
   return RET_OK;
 }
@@ -304,7 +293,7 @@ int WinogradOpenCLKernel::SetGlobalLocal() {
 int WinogradOpenCLKernel::Run() {
   MS_LOG(DEBUG) << this->name() << " winograd Running!";
   MS_LOG(DEBUG) << "winograd kernel0 Running!";
-  if (ocl_runtime_->SetKernelArg(kernel_4x4to36_, 0, in_tensors_.front()->data()) != CL_SUCCESS) {
+  if (ocl_runtime_->SetKernelArg(kernel_4x4to36_, CLARGSINDEX0, in_tensors_.front()->data()) != CL_SUCCESS) {
     MS_LOG(ERROR) << "SetKernelArg failed.";
     return RET_ERROR;
   }
@@ -320,7 +309,7 @@ int WinogradOpenCLKernel::Run() {
   }
 
   MS_LOG(DEBUG) << "winograd kernel2 Running!";
-  if (ocl_runtime_->SetKernelArg(kernel_36to4x4_, 1, out_tensors_.front()->data()) != CL_SUCCESS) {
+  if (ocl_runtime_->SetKernelArg(kernel_36to4x4_, CLARGSINDEX1, out_tensors_.front()->data()) != CL_SUCCESS) {
     MS_LOG(ERROR) << "SetKernelArg failed.";
     return RET_ERROR;
   }
