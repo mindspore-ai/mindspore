fix apply power sign bug and add st

mindspore/python/mindspore/ops/operations/nn_ops.py
@@ -6103,7 +6103,7 @@ class ApplyPowerSign(Primitive):
                       is not supported.
 
     Supported Platforms:
-        ``Ascend``
+        ``Ascend`` ``CPU``
 
     Examples:
         >>> class Net(nn.Cell):