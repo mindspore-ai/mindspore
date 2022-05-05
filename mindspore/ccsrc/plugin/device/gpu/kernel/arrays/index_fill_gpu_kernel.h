15eb164049a39ad09fb248c90c74093a824739d4
hezhenhao1 <hezhenhao1@huawei.com>
2022-05-05 09:25:09 +0800

    Add IndexFill primitive and adapt GPU implementation to new interface.

--- mindspore/ccsrc/plugin/device/gpu/kernel/arrays/index_fill_gpu_kernel.h
+++ mindspore/ccsrc/plugin/device/gpu/kernel/arrays/index_fill_gpu_kernel.h
@@ -16,10 +16,11 @@
 #ifndef MINDSPORE_CCSRC_PLUGIN_DEVICE_GPU_KERNEL_ARRAYS_INDEX_FILL_GPU_KERNEL_H_
 #define MINDSPORE_CCSRC_PLUGIN_DEVICE_GPU_KERNEL_ARRAYS_INDEX_FILL_GPU_KERNEL_H_
 
-#include <cstdint>
 #include <limits>
 #include <vector>
 #include <functional>
+#include <map>
+#include <utility>
 #include "plugin/device/gpu/kernel/cuda_impl/cuda_ops/cuda_common.h"
 #include "plugin/device/gpu/kernel/cuda_impl/cuda_ops/index_fill_impl.cuh"
 #include "plugin/device/gpu/kernel/cuda_impl/cuda_class/cuda_class_common.h"
@@ -28,154 +29,42 @@
 
 namespace mindspore {
 namespace kernel {
-constexpr size_t kIndexFillInputsNum = 4;
-constexpr size_t kIndexFillOutputsNum = 1;
-using kIndexType = int32_t;
-template <typename T>
-class IndexFillGpuKernelMod : public DeprecatedNativeGpuKernelMod {
+class IndexFillGpuKernelMod : public NativeGpuKernelMod {
  public:
-  IndexFillGpuKernelMod() { ResetResource(); }
-  ~IndexFillGpuKernelMod() = default;
-
-  bool Init(const CNodePtr &kernel_node) override {
-    kernel_name_ = common::AnfAlgo::GetCNodeName(kernel_node);
-    kernel_node_ = kernel_node;
-    // Check the number of inputs and outputs.
-    auto inputs_num = common::AnfAlgo::GetInputTensorNum(kernel_node);
-    if (inputs_num != kIndexFillInputsNum) {
-      MS_LOG(EXCEPTION) << "For '" << kernel_name_ << "', the number of inputs must be " << kIndexFillInputsNum
-                        << ", but got " << inputs_num;
-    }
-    auto outputs_num = common::AnfAlgo::GetOutputTensorNum(kernel_node);
-    if (outputs_num != kIndexFillOutputsNum) {
-      MS_LOG(EXCEPTION) << "For '" << kernel_name_ << "', the number of outputs must be " << kIndexFillOutputsNum
-                        << ", but got " << outputs_num;
-    }
-    // 'dim' and 'value' must be tensor with empty shape.
-    auto dim_shape = AnfAlgo::GetInputDeviceShape(kernel_node, kIndex1);
-    if (!(dim_shape.empty() || (dim_shape.size() == 1 && dim_shape.front() == 1))) {
-      MS_LOG(EXCEPTION) << "For '" << kernel_name_ << "', the shape of 'dim' must be empty or (1,), but got "
-                        << dim_shape;
-    }
-    auto value_shape = AnfAlgo::GetInputDeviceShape(kernel_node, kIndex3);
-    if (!(value_shape.empty() || (value_shape.size() == 1 && value_shape.front() == 1))) {
-      MS_LOG(EXCEPTION) << "For '" << kernel_name_ << "', the shape of 'value' must be empty or (1,), but got "
-                        << value_shape;
-    }
-    // 'index' must be vector/scalar.
-    auto index_shape = AnfAlgo::GetInputDeviceShape(kernel_node, kIndex2);
-    if (index_shape.size() > 1) {
-      MS_LOG(EXCEPTION) << "For '" << kernel_name_ << "', the shape of 'index' can not be greater than 1D, but got "
-                        << index_shape;
-    }
-    // The shape of 'x' and 'y' is equal.
-    auto x_shape = AnfAlgo::GetInputDeviceShape(kernel_node, kIndex0);
-    auto y_shape = AnfAlgo::GetInputDeviceShape(kernel_node, kIndex0);
-    bool is_equal = x_shape.size() == y_shape.size();
-    for (size_t i = 0; is_equal && i < x_shape.size(); ++i) {
-      is_equal &= (x_shape[i] == y_shape[i]);
-    }
-    if (!is_equal) {
-      MS_LOG(EXCEPTION) << "For '" << kernel_name_ << "', the shape of 'x' and 'y' must be equal, but got "
-                        << "shape of 'x' is " << CONVERT_VECTOR_TO_STRING(x_shape) << ", shape of 'output' is "
-                        << y_shape;
-    }
-
-    is_null_input_ = CHECK_SHAPE_NULL(x_shape_, kernel_name_, "x");
-    if (is_null_input_) {
-      InitSizeLists();
-      return true;
-    }
-    x_size_ = std::accumulate(x_shape.begin(), x_shape.end(), 1, std::multiplies<size_t>());
-    index_size_ = std::accumulate(index_shape.begin(), index_shape.end(), 1, std::multiplies<size_t>());
-    x_shape_ = x_shape;
-    InitSizeLists();
-    return true;
-  }
+  IndexFillGpuKernelMod() = default;
+  ~IndexFillGpuKernelMod() override = default;
 
   bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
               const std::vector<AddressPtr> &outputs, void *stream_ptr) override {
-    if (is_null_input_) {
-      return true;
-    }
-    auto x_ptr = GetDeviceAddress<T>(inputs, kIndex0);
-    auto dim_ptr = GetDeviceAddress<kIndexType>(inputs, kIndex1);
-    auto index_ptr = GetDeviceAddress<kIndexType>(inputs, kIndex2);
-    auto value_ptr = GetDeviceAddress<T>(inputs, kIndex3);
-    auto y_ptr = GetDeviceAddress<T>(outputs, kIndex0);
-    auto out_bound_ptr = GetDeviceAddress<bool>(workspace, kIndex0);
-    auto cuda_stream = reinterpret_cast<cudaStream_t>(stream_ptr);
-
-    // Check and Initialize 'dim'.
-    kIndexType dim, rank = SizeToInt(x_shape_.size());
-    CHECK_CUDA_RET_WITH_EXCEPT(kernel_node_, cudaMemcpy(&dim, dim_ptr, sizeof(kIndexType), cudaMemcpyDeviceToHost),
-                               "cudaMemcpy input 'dim' to host failed");
-    if (dim < -rank || dim >= rank) {
-      MS_LOG(EXCEPTION) << "For '" << kernel_name_ << "', the 'dim' must be in the range [-" << rank << "," << rank
-                        << "), but got " << dim;
-    }
-    // Prepare index_num, dim_size, outer_size, inner_size
-    dim = dim >= 0 ? dim : dim + rank;
-    int dim_size = 1;
-    size_t outer_size = 1, inner_size = 1;
-    for (int i = 0; i < rank; i++) {
-      if (i < dim) {
-        outer_size *= x_shape_.at(IntToSize(i));
-      } else if (i > dim) {
-        inner_size *= x_shape_.at(IntToSize(i));
-      } else {
-        dim_size = static_cast<kIndexType>(x_shape_.at(IntToSize(i)));
-      }
-    }
-    size_t index_num = index_size_ * (outer_size * inner_size);
-    // Copy from 'x' into 'y'.
-    CHECK_CUDA_RET_WITH_EXCEPT(
-      kernel_node_, cudaMemcpyAsync(y_ptr, x_ptr, x_size_ * sizeof(T), cudaMemcpyDeviceToDevice, cuda_stream),
-      "cudaMemcpyAsync output 'y' from 'x' failed.");
-    // Initialize out_bound_ptr.
-    bool out_bound = false;
-    CHECK_CUDA_RET_WITH_EXCEPT(
-      kernel_node_, cudaMemcpyAsync(out_bound_ptr, &out_bound, sizeof(bool), cudaMemcpyHostToDevice, cuda_stream),
-      "cudaMemcpyAsync out_bound variable failed.");
-
-    IndexFill(y_ptr, index_ptr, index_num, outer_size, dim_size, inner_size, value_ptr, out_bound_ptr, cuda_stream);
-
-    CHECK_CUDA_RET_WITH_EXCEPT(
-      kernel_node_, cudaMemcpyAsync(&out_bound, out_bound_ptr, sizeof(bool), cudaMemcpyDeviceToHost, cuda_stream),
-      "cudaMemcpyAsync out_bound_ variable failed.");
-    CHECK_CUDA_RET_WITH_EXCEPT(kernel_node_, cudaStreamSynchronize(cuda_stream),
-                               "IndexFill cudaStreamSynchronized failed");
-    if (out_bound) {
-      MS_LOG(EXCEPTION) << "For '" << kernel_name_ << "', the input 'index' is out of bound.";
-    }
-    return true;
+    return kernel_func_(this, inputs, workspace, outputs, stream_ptr);
   }
 
-  void ResetResource() noexcept override {
-    is_null_input_ = false;
-    x_size_ = 0;
-    index_size_ = 0;
-    x_shape_.clear();
-    input_size_list_.clear();
-    output_size_list_.clear();
-    workspace_size_list_.clear();
-  }
+  bool Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+            const std::vector<KernelTensorPtr> &outputs) override;
+
+  int Resize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+             const std::vector<KernelTensorPtr> &outputs, const std::map<uint32_t, tensor::TensorPtr> &) override;
 
  protected:
-  void InitSizeLists() override {
-    input_size_list_.push_back(x_size_ * sizeof(T));
-    input_size_list_.push_back(sizeof(kIndexType));
-    input_size_list_.push_back(index_size_ * sizeof(kIndexType));
-    input_size_list_.push_back(sizeof(T));
-    workspace_size_list_.push_back(sizeof(bool));  // Place out_bound.
-    output_size_list_.push_back(x_size_ * sizeof(T));
-  }
+  std::vector<KernelAttr> GetOpSupport() override;
 
  private:
-  size_t x_size_;
-  size_t index_size_;
-  std::vector<size_t> x_shape_;
-  bool is_null_input_;
+  void ResetResource() noexcept;
+
+  template <typename DataType, typename IndexType>
+  bool LaunchKernel(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
+                    const std::vector<AddressPtr> &outputs, void *stream_ptr);
+
+  using IndexFillLaunchFunc =
+    std::function<bool(IndexFillGpuKernelMod *, const std::vector<AddressPtr> &, const std::vector<AddressPtr> &,
+                       const std::vector<AddressPtr> &, void *)>;
+  static std::vector<std::pair<KernelAttr, IndexFillLaunchFunc>> func_list_;
+  IndexFillLaunchFunc kernel_func_;
+  int64_t x_num_;
+  int64_t index_num_;
+  size_t data_size_;  // That is, sizeof(DataType).
+  size_t dim_size_;   // That is, sizeof(DimType)
+  std::vector<int64_t> x_shape_{};
 };
 }  // namespace kernel
 }  // namespace mindspore
--- mindspore/core/ops/core_ops.h
+++ mindspore/core/ops/core_ops.h
@@ -141,6 +141,7 @@ constexpr auto kUpperBound = "UpperBound";
 constexpr auto kCummax = "Cummax";
 constexpr auto kTril = "Tril";
 constexpr auto kTriu = "Triu";
+constexpr auto kIndexFill = "IndexFill";
 
 // NN
 constexpr auto kFractionalMaxPool3DWithFixedKsize = "FractionalMaxPool3DWithFixedKsize";
@@ -781,6 +782,7 @@ GVAR_DEF(PrimitivePtr, kPrimMatrixInverse, std::make_shared<Primitive>(kMatrixIn
 GVAR_DEF(PrimitivePtr, kPrimMatrixDeterminant, std::make_shared<Primitive>(kMatrixDeterminant));
 GVAR_DEF(PrimitivePtr, kPrimLogMatrixDeterminant, std::make_shared<Primitive>(kLogMatrixDeterminant));
 GVAR_DEF(PrimitivePtr, kPrimIndexAdd, std::make_shared<Primitive>("IndexAdd"));
+GVAR_DEF(PrimitivePtr, kPrimIndexFill, std::make_shared<Primitive>(kIndexFill));
 GVAR_DEF(PrimitivePtr, kPrimIdentityMath, std::make_shared<Primitive>("Identity", kSideEffectPropagate));
 GVAR_DEF(PrimitivePtr, kPrimInvGrad, std::make_shared<Primitive>("InvGrad"));
 GVAR_DEF(PrimitivePtr, kPrimErfinv, std::make_shared<Primitive>("Erfinv"));
