d3dfded173773c36cce642db9b4cf147eb535d39
Margaret_wangrui <wangrui178@huawei.com>
2022-05-10 14:10:55 +0800

    [Fallback][Control_flow] Add some fallback control flow testcases.

---
+++ tests/st/fallback/control_flow/test_fallback_121_if_after_while_in_while.py
@@ -0,0 +1,100 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+""" test graph fallback control flow."""
+import pytest
+import numpy as np
+from mindspore import Tensor, ms_function, context
+
+context.set_context(mode=context.GRAPH_MODE)
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_ascend_training
+@pytest.mark.env_onecard
+def test_if_after_while_in_while_tensor():
+    """
+    Feature: JIT Fallback
+    Description: Test fallback with control flow.
+    Expectation: No exception.
+    """
+    @ms_function
+    def control_flow_if_after_while_in_while():
+        x = Tensor(1)
+        y = Tensor(2)
+        z = Tensor(0)
+        while z < 3:
+            z = z + y
+            while y > x:
+                y -= x
+        if x + y >= z and x > y:
+            y = y * x - z
+        return y
+    res = control_flow_if_after_while_in_while()
+    assert res == 1
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_ascend_training
+@pytest.mark.env_onecard
+def test_if_after_while_in_while_tensor_2():
+    """
+    Feature: JIT Fallback
+    Description: Test fallback with control flow.
+    Expectation: No exception.
+    """
+    @ms_function
+    def control_flow_if_after_while_in_while():
+        x = Tensor(1)
+        y = Tensor(2)
+        z = Tensor(3)
+        while z <= 3:
+            z = z + y
+            while y >= x:
+                y -= x
+            x = y - z
+        if x + y >= z and not y:
+            y = y * x - z
+        return y
+    res = control_flow_if_after_while_in_while()
+    assert res == 0
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_ascend_training
+@pytest.mark.env_onecard
+def test_if_after_while_in_while_numpy():
+    """
+    Feature: JIT Fallback
+    Description: Test fallback with control flow.
+    Expectation: No exception.
+    """
+    @ms_function
+    def control_flow_if_after_while_in_while():
+        x = np.array([1, 2, 3, 4])
+        y = Tensor(5)
+        while y > Tensor(x[3]):
+            while y > Tensor(x[0]):
+                y -= Tensor(x[1])
+        if y != 0:
+            y = y + Tensor(sum(x))
+        return y
+    res = control_flow_if_after_while_in_while()
+    assert res == 11
