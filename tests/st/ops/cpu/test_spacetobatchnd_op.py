c05037c0e768ebbdbc3ee829818f2d51e1ed442e
looop5 <xuhui78@huawei.com>
2022-05-18 15:03:52 +0800

    move vmap testcases to st/ops directory

--- tests/st/ops/cpu/test_spacetobatchnd_op.py
+++ tests/st/ops/cpu/test_spacetobatchnd_op.py
@@ -22,6 +22,7 @@ from mindspore import Tensor
 from mindspore.common.api import ms_function
 from mindspore.common.initializer import initializer
 from mindspore.common.parameter import Parameter
+from mindspore.ops.functional import vmap
 
 
 class SpaceToBatchNDNet(nn.Cell):
@@ -199,3 +200,56 @@ def test_space_to_batch_nd_dynamic():
     context.set_context(mode=context.GRAPH_MODE, device_target="CPU")
     output = dyn_net(input_x, input_y)
     assert (output.asnumpy() == expect).all()
+
+
+def vmap_case():
+    class Net(nn.Cell):
+        def __init__(self, block_size, paddings):
+            super(Net, self).__init__()
+            self.space_to_batch_nd = ops.SpaceToBatchND(block_size, paddings)
+
+        def construct(self, a):
+            return self.space_to_batch_nd(a)
+
+    class WrapNet(nn.Cell):
+        def __init__(self, net, in_axes, out_axes):
+            super(WrapNet, self).__init__()
+            self.net = net
+            self.in_axes = in_axes
+            self.out_axes = out_axes
+
+        def construct(self, input_x):
+            return vmap(self.net, self.in_axes, self.out_axes)(input_x)
+
+    block_size = [2, 2]
+    paddings = [[0, 0], [0, 0]]
+    input_shape = (2, 3, 1, 4, 4)
+    data_np = np.arange(np.prod(input_shape)).reshape(input_shape).astype(np.float32)
+    net = Net(block_size, paddings)
+
+    # test input axis and output axis are the same
+    v_net_1 = WrapNet(Net(block_size, paddings), (0,), 0)
+    output_v = v_net_1(Tensor(data_np)).asnumpy()
+
+    for i in range(input_shape[0]):
+        assert np.allclose(output_v[i, :, :, :, :], net(Tensor(data_np[i, :, :, :, :])).asnumpy())
+
+    # test input axis and output axis are different
+    v_net_2 = WrapNet(Net(block_size, paddings), (0,), 1)
+    output_v = v_net_2(Tensor(data_np)).asnumpy()
+
+    for i in range(input_shape[0]):
+        assert np.allclose(output_v[:, i, :, :, :], net(Tensor(data_np[i, :, :, :, :])).asnumpy())
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_space_to_batch_nd_vmap_cpu():
+    """
+    Feature: test SpactToBatchND vmap on CPU.
+    Description: inputs with batch.
+    Expectation: the result match with expect
+    """
+    context.set_context(mode=context.GRAPH_MODE, device_target="CPU")
+    vmap_case()
