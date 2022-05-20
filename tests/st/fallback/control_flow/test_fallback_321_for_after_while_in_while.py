8137dfa0a9e6e282f946f826fa9ab56a94bf9bc2
huanghui <huanghui44@huawei.com>
2022-05-20 16:02:08 +0800

    add st for fallback constant control flow with 320 321 322

---
+++ tests/st/fallback/control_flow/test_fallback_321_for_after_while_in_while.py
@@ -0,0 +1,143 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+""" test graph fallback control flow."""
+import pytest
+import numpy as np
+from mindspore import Tensor, ms_function, context
+
+context.set_context(mode=context.GRAPH_MODE)
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_ascend_training
+@pytest.mark.env_onecard
+def test_for_after_while_in_while_1():
+    """
+    Feature: JIT Fallback
+    Description: Test fallback with control flow.
+    Expectation: No exception.
+    """
+
+    @ms_function
+    def func3211():
+        x = Tensor([0])
+        y = Tensor([0])
+
+        while x < Tensor([3]):
+            x = x + Tensor([1])
+            while x > Tensor([4]):
+                x = x + 1
+        for _ in range(3):
+            y = y - 1
+        return x + y
+
+    res = func3211()
+    assert res == 0
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_ascend_training
+@pytest.mark.env_onecard
+def test_for_after_while_in_while_2():
+    """
+    Feature: JIT Fallback
+    Description: Test fallback with control flow.
+    Expectation: No exception.
+    """
+
+    @ms_function
+    def func3212():
+        x = Tensor([2])
+        y = Tensor([2])
+        while y < Tensor([5]) and x > Tensor([1]):
+            y = y + Tensor([3])
+            while y == Tensor([5]):
+                y = y * Tensor([sum((1, 1))])
+
+        for i in range(3):
+            z = Tensor([i])
+            x = x + z
+
+        return x, y
+
+    x, y = func3212()
+    assert x == 5
+    assert y == 10
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_ascend_training
+@pytest.mark.env_onecard
+def test_for_after_while_in_while_3():
+    """
+    Feature: JIT Fallback
+    Description: Test fallback with control flow.
+    Expectation: No exception.
+    """
+
+    @ms_function
+    def func3213():
+        x = np.array([0])
+        y = np.array([5, 6, 7])
+        while x < 2:
+            y = y - np.array([2, 3, 4])
+            while x < 3:
+                x = x + 1
+            x = x - 1
+
+        z = Tensor(y)
+        out = 1
+        for i in z:
+            out = out * i
+        return out
+
+    res = func3213()
+    assert res == 27
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_ascend_training
+@pytest.mark.env_onecard
+def test_for_after_while_in_while_4():
+    """
+    Feature: JIT Fallback
+    Description: Test fallback with control flow.
+    Expectation: No exception.
+    """
+
+    @ms_function
+    def func3214():
+        x = Tensor([1])
+        y = Tensor([2])
+        while max(x, y) == Tensor([2]):
+            while min(x, y) == Tensor([1]):
+                x = x - min(x, y)
+                y = y + max(x, y)
+
+        z = (Tensor(1), Tensor(2), Tensor(3))
+        for i in zip(z):
+            x = x * i
+        return x + y
+
+    res = func3214()
+    assert res == 4
