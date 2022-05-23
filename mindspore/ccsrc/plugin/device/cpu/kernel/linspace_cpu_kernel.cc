1e4ad0b841c741f27595b3f75ff9ee0381035136
zhujingxuan <zhujingxuan@huawei.com>
2022-05-23 19:57:49 +0800

    Update LinSpace CPU kernel to new API && add batch support

--- mindspore/ccsrc/plugin/device/cpu/kernel/linspace_cpu_kernel.cc
+++ mindspore/ccsrc/plugin/device/cpu/kernel/linspace_cpu_kernel.cc
@@ -17,6 +17,7 @@
 #include "plugin/device/cpu/kernel/linspace_cpu_kernel.h"
 #include <algorithm>
 #include <utility>
+#include <map>
 #include "plugin/device/cpu/hal/device/cpu_device_address.h"
 #include "kernel/common_utils.h"
 
@@ -25,84 +26,86 @@ namespace kernel {
 namespace {
 constexpr auto kInputNum = 3;
 constexpr auto kOutputNum = 1;
+using KernelRunFunc = LinSpaceCpuKernelMod::KernelRunFunc;
 }  // namespace
+bool LinSpaceCpuKernelMod::Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+                                const std::vector<KernelTensorPtr> &outputs) {
+  kernel_name_ = base_operator->name();
 
-void LinSpaceCpuKernelMod::InitKernel(const CNodePtr &kernel_node) {
-  auto kernel_name = common::AnfAlgo::GetCNodeName(kernel_node);
-  size_t input_num = common::AnfAlgo::GetInputTensorNum(kernel_node);
-  if (input_num != kInputNum) {
-    MS_LOG(EXCEPTION) << "For '" << kernel_name << "', the number of inputs must be 3, but got " << input_num;
+  if (!MatchKernelFunc(base_operator, inputs, outputs)) {
+    return false;
   }
-  size_t output_num = common::AnfAlgo::GetOutputTensorNum(kernel_node);
-  if (output_num != kOutputNum) {
-    MS_LOG(EXCEPTION) << "For '" << kernel_name << "', the number of outputs must be 1, but got " << output_num;
-  }
-
-  auto start = common::AnfAlgo::GetPrevNodeOutputInferShape(kernel_node, 0);
-  auto end = common::AnfAlgo::GetPrevNodeOutputInferShape(kernel_node, 1);
-  auto value_count = common::AnfAlgo::GetOutputInferShape(kernel_node, 0);
-
-  // error checking input data
-  if ((start.size() != 0) || (end.size() != 0)) {
-    MS_LOG(EXCEPTION) << "For '" << kernel_name << "', both start and end must be 0-D Tensors, but got dimension "
-                      << "of start: " << start.size() << " and dimension of end: " << end.size();
-  }
-
-  if (value_count.size() != 1) {
-    MS_LOG(EXCEPTION) << "For '" << kernel_name << "', the dimension of output must be 1, but got "
-                      << value_count.size();
-  }
-  value_count_ = value_count[0];
-
-  auto kernel_attr = GetKernelAttrFromNode(kernel_node);
-  auto [is_match, index] = MatchKernelAttr(kernel_attr, GetOpSupport());
-  if (!is_match) {
-    MS_LOG(EXCEPTION) << "LinSpace does not support this kernel data type: " << kernel_attr;
-  }
-  kernel_func_ = func_list_[index].second;
-}
-
-template <typename T>
-bool LinSpaceCpuKernelMod::LaunchKernel(const std::vector<kernel::AddressPtr> &inputs,
-                                        const std::vector<kernel::AddressPtr> &outputs) {
-  auto value_start = *reinterpret_cast<T *>(inputs[0]->addr);
-  auto value_stop = *reinterpret_cast<T *>(inputs[1]->addr);
-  auto output = reinterpret_cast<T *>(outputs[0]->addr);
-
-  T add_value = ((value_stop - value_start) / (value_count_ - 1));
-
-  auto task = [output, value_start, add_value](size_t start, size_t end) {
-    for (size_t i = 0; i < end; i++) {
-      output[i] = value_start + add_value * i;
-    }
-  };
-
-  ParallelLaunchAutoSearch(task, value_count_, this, &parallel_search_info_);
 
   return true;
 }
 
-std::vector<std::pair<KernelAttr, LinSpaceCpuKernelMod::LinSpaceFunc>> LinSpaceCpuKernelMod::func_list_ = {
-  {KernelAttr()
-     .AddInputAttr(kNumberTypeFloat32)
-     .AddInputAttr(kNumberTypeFloat32)
-     .AddInputAttr(kNumberTypeInt64)
-     .AddOutputAttr(kNumberTypeFloat32),
-   &LinSpaceCpuKernelMod::LaunchKernel<float>},
-  {KernelAttr()
-     .AddInputAttr(kNumberTypeFloat64)
-     .AddInputAttr(kNumberTypeFloat64)
-     .AddInputAttr(kNumberTypeInt64)
-     .AddOutputAttr(kNumberTypeFloat64),
-   &LinSpaceCpuKernelMod::LaunchKernel<double>}};
+int LinSpaceCpuKernelMod::Resize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+                                 const std::vector<KernelTensorPtr> &outputs,
+                                 const std::map<uint32_t, tensor::TensorPtr> &) {
+  if (auto ret = KernelMod::Resize(base_operator, inputs, outputs); ret != KRET_OK) {
+    return ret;
+  }
 
-std::vector<KernelAttr> LinSpaceCpuKernelMod::GetOpSupport() {
-  std::vector<KernelAttr> support_list;
-  (void)std::transform(func_list_.begin(), func_list_.end(), std::back_inserter(support_list),
-                       [](const std::pair<KernelAttr, LinSpaceFunc> &pair) { return pair.first; });
-  return support_list;
+  const auto input_shape = inputs.at(kIndex0)->GetShapeVector();
+  batch_num_ = std::accumulate(input_shape.begin(), input_shape.end(), 1, std::multiplies{});
+  batch_num_ = (batch_num_ == 0) ? 1 : batch_num_;
+
+  const auto dtype_size = abstract::TypeIdSize(inputs.at(kIndex0)->GetDtype());
+
+  // Deal with workspace_size_list_
+  workspace_size_list_.clear();
+  workspace_size_list_ = {batch_num_ * dtype_size};
+
+  return KRET_OK;
 }
 
+template <typename T>
+bool LinSpaceCpuKernelMod::LaunchKernel(const std::vector<kernel::AddressPtr> &inputs,
+                                        const std::vector<kernel::AddressPtr> &workspace,
+                                        const std::vector<kernel::AddressPtr> &outputs) {
+  auto starts = reinterpret_cast<T *>(inputs[kIndex0]->addr);
+  auto stops = reinterpret_cast<T *>(inputs[kIndex1]->addr);
+  const int64_t num = *reinterpret_cast<int64_t *>(inputs[kIndex2]->addr);
+
+  auto add_values = reinterpret_cast<T *>(workspace[kIndex0]->addr);
+
+  auto output = reinterpret_cast<T *>(outputs[kIndex0]->addr);
+
+  for (int64_t i = 0; i < batch_num_; ++i) {
+    add_values[i] = ((stops[i] - starts[i]) / (num - 1));
+  }
+
+  // Run parallel both on batch and also the calculated axis
+  auto task = [output, num, starts, add_values](size_t start, size_t end) {
+    for (size_t index = start; index < end; index++) {
+      const size_t batch = index / num;
+      const size_t i = index % num;
+      output[index] = starts[batch] + add_values[batch] * i;
+    }
+  };
+
+  ParallelLaunchAutoSearch(task, batch_num_ * num, this, &parallel_search_info_);
+
+  return true;
+}
+
+const std::vector<std::pair<KernelAttr, KernelRunFunc>> &LinSpaceCpuKernelMod::GetFuncList() const {
+  static const std::vector<std::pair<KernelAttr, KernelRunFunc>> func_list = {
+    {KernelAttr()
+       .AddInputAttr(kNumberTypeFloat32)
+       .AddInputAttr(kNumberTypeFloat32)
+       .AddInputAttr(kNumberTypeInt64)
+       .AddOutputAttr(kNumberTypeFloat32),
+     &LinSpaceCpuKernelMod::LaunchKernel<float>},
+    {KernelAttr()
+       .AddInputAttr(kNumberTypeFloat64)
+       .AddInputAttr(kNumberTypeFloat64)
+       .AddInputAttr(kNumberTypeInt64)
+       .AddOutputAttr(kNumberTypeFloat64),
+     &LinSpaceCpuKernelMod::LaunchKernel<double>},
+  };
+  return func_list;
+}
 MS_KERNEL_FACTORY_REG(NativeCpuKernelMod, LinSpace, LinSpaceCpuKernelMod);
 }  // namespace kernel
 }  // namespace mindspore
