0c407f1fb26a5af820a0b64e97d119125b3ce8b5
limingqi107 <limingqi@huawei.com>
2022-05-27 10:33:06 +0800

    data arrow add the member is_batch_arrow

--- mindspore/core/mindrt/include/actor/op_actor.h
+++ mindspore/core/mindrt/include/actor/op_actor.h
@@ -33,10 +33,15 @@ namespace mindspore {
 // OpActor data route.
 struct DataArrow {
   DataArrow(int from_output_index, const AID &to_op_id, int to_input_index)
-      : from_output_index_(from_output_index), to_op_id_(to_op_id), to_input_index_(to_input_index) {}
+      : from_output_index_(from_output_index),
+        to_op_id_(to_op_id),
+        to_input_index_(to_input_index),
+        is_batch_arrow_{false} {}
   int from_output_index_;
   AID to_op_id_;
   int to_input_index_;
+  // Indicates whether to send data routes in batches to increase the sending performance.
+  bool is_batch_arrow_;
 };
 using DataArrowPtr = std::shared_ptr<DataArrow>;
 
