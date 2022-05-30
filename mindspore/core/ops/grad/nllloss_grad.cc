dadcbe124f81ef767e41c5ae19a14e3e21ec5bfc
tronzhang <zhangzhaochuang@huawei.com>
2022-05-30 15:30:25 +0800

    add dynamic and vmap support for nlllossgrad

--- mindspore/core/ops/grad/nllloss_grad.cc
+++ mindspore/core/ops/grad/nllloss_grad.cc
@@ -17,10 +17,51 @@
 #include "ops/grad/nllloss_grad.h"
 
 #include "ops/op_utils.h"
+#include "utils/check_convert_utils.h"
 #include "mindapi/src/helper.h"
 
 namespace mindspore {
 namespace ops {
+namespace {
+void CheckValueIn(const std::string &arg_name, size_t value, const std::vector<size_t> &in_values,
+                  const std::string &prim_name) {
+  if (!std::any_of(in_values.begin(), in_values.end(), [value](size_t iv) { return iv == value; })) {
+    std::ostringstream buffer;
+    if (prim_name.empty()) {
+      buffer << "The";
+    } else {
+      buffer << "For '" << prim_name << "', the";
+    }
+
+    buffer << "'" << arg_name << "'";
+    buffer << " must in [";
+    for (auto item : in_values) {
+      buffer << item << ",";
+    }
+    buffer << "], but got '" << value << "' with type 'int'.";
+    MS_EXCEPTION(ValueError) << buffer.str();
+  }
+}
+
+void CheckNLLLossGradShapeValid(const std::string &prim_name, const ShapeVector &x_shape, const ShapeVector &t_shape,
+                                const ShapeVector &w_shape) {
+  if (x_shape.empty() || t_shape.empty() || w_shape.empty()) {
+    return;
+  }
+
+  CheckValueIn("x rank", x_shape.size(), {1, 2}, prim_name);
+  CheckAndConvertUtils::CheckInteger("target rank", t_shape.size(), kEqual, 1, prim_name);
+  CheckAndConvertUtils::CheckInteger("weight rank", w_shape.size(), kEqual, 1, prim_name);
+  if (x_shape.size() == 1) {
+    CheckAndConvertUtils::Check("target_shape", 1, kEqual, t_shape[0], prim_name);
+    CheckAndConvertUtils::Check("weight_shape", x_shape[0], kEqual, w_shape[0], prim_name);
+  } else {
+    CheckAndConvertUtils::Check("target_shape", x_shape[0], kEqual, t_shape[0], prim_name);
+    CheckAndConvertUtils::Check("weight_shape", x_shape[1], kEqual, w_shape[0], prim_name);
+  }
+}
+}  // namespace
+
 void NLLLossGrad::Init(const Reduction &reduction) { set_reduction(reduction); }
 
 void NLLLossGrad::set_reduction(const Reduction &reduction) {
@@ -33,7 +74,73 @@ Reduction NLLLossGrad::get_reduction() const {
   return Reduction(GetValue<int64_t>(value_ptr));
 }
 
+abstract::ShapePtr NLLLossGradInferShape(const PrimitivePtr &primitive,
+                                         const std::vector<AbstractBasePtr> &input_args) {
+  auto prim_name = primitive->name();
+
+  // Check valid.
+  const size_t x_idx = 0;
+  const size_t t_idx = 2;
+  const size_t w_idx = 3;
+  (void)CheckAndConvertUtils::CheckArgs<abstract::AbstractTensor>(prim_name, input_args, x_idx);
+  auto x = input_args[x_idx]->BuildShape();
+  MS_EXCEPTION_IF_NULL(x);
+  (void)CheckAndConvertUtils::CheckArgs<abstract::AbstractTensor>(prim_name, input_args, t_idx);
+  auto t = input_args[t_idx]->BuildShape();
+  MS_EXCEPTION_IF_NULL(t);
+  (void)CheckAndConvertUtils::CheckArgs<abstract::AbstractTensor>(prim_name, input_args, w_idx);
+  auto w = input_args[w_idx]->BuildShape();
+  MS_EXCEPTION_IF_NULL(w);
+
+  auto x_shape = x->cast<abstract::ShapePtr>();
+  MS_EXCEPTION_IF_NULL(x_shape);
+
+  if (x->IsDynamic() || t->IsDynamic() || w->IsDynamic()) {
+    return x_shape;
+  }
+
+  auto t_shape = t->cast<abstract::ShapePtr>();
+  MS_EXCEPTION_IF_NULL(t_shape);
+  auto w_shape = w->cast<abstract::ShapePtr>();
+  MS_EXCEPTION_IF_NULL(w_shape);
+
+  CheckNLLLossGradShapeValid(prim_name, x_shape->shape(), t_shape->shape(), w_shape->shape());
+
+  return x_shape;
+}
+
+TypePtr NLLLossGradInferType(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) {
+  MS_EXCEPTION_IF_NULL(primitive);
+  auto prim_name = primitive->name();
+  // check
+  std::set<TypePtr> valid_types = {kFloat16, kFloat32};
+  auto x_dtype = input_args[kInputIndex0]->BuildType();
+  auto y_grad_dtype = input_args[kInputIndex1]->BuildType();
+  auto t_dtype = input_args[kInputIndex2]->BuildType();
+  auto w_dtype = input_args[kInputIndex3]->BuildType();
+  auto tw_dtype = input_args[kInputIndex4]->BuildType();
+  (void)CheckAndConvertUtils::CheckTensorTypeValid("x_dtype", x_dtype, valid_types, prim_name);
+  (void)CheckAndConvertUtils::CheckTensorTypeValid("y_grad_dtype", y_grad_dtype, valid_types, prim_name);
+  (void)CheckAndConvertUtils::CheckTensorTypeValid("t_dtype", t_dtype, {kInt32}, prim_name);
+  (void)CheckAndConvertUtils::CheckTensorTypeValid("w_dtype", w_dtype, valid_types, prim_name);
+  (void)CheckAndConvertUtils::CheckTensorTypeValid("tw_dtype", tw_dtype, valid_types, prim_name);
+  CheckAndConvertUtils::Check("w_dtype", std::vector<TypeId>{tw_dtype->type_id()}, kEqual,
+                              std::vector<TypeId>{w_dtype->type_id()}, prim_name);
+  return x_dtype;
+}
+
+AbstractBasePtr NLLLossGradInfer(const abstract::AnalysisEnginePtr &, const PrimitivePtr &primitive,
+                                 const std::vector<AbstractBasePtr> &input_args) {
+  MS_EXCEPTION_IF_NULL(primitive);
+  auto prim_name = primitive->name();
+  const size_t input_num = 5;
+  (void)CheckAndConvertUtils::CheckInputArgs(input_args, kEqual, input_num, prim_name);
+  auto types = NLLLossGradInferType(primitive, input_args);
+  auto shapes = NLLLossGradInferShape(primitive, input_args);
+  return abstract::MakeAbstract(shapes, types);
+}
+
 MIND_API_OPERATOR_IMPL(NLLLossGrad, BaseOperator);
-REGISTER_PRIMITIVE_C(kNameNLLLossGrad, NLLLossGrad);
+REGISTER_PRIMITIVE_EVAL_IMPL(NLLLossGrad, std::make_shared<Primitive>("NLLLossGrad"), NLLLossGradInfer, nullptr, true);
 }  // namespace ops
 }  // namespace mindspore
---
+++ mindspore/python/mindspore/ops/_vmap/vmap_grad_nn_ops.py
@@ -0,0 +1,98 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""nn_ops vmap impl."""
+
+from functools import reduce
+import mindspore.numpy as mnp
+from mindspore.ops.operations import _grad_ops as G
+from mindspore.ops import functional as F
+from mindspore.ops import constexpr
+from ..primitive import Primitive
+from .._vmap.vmap_base import vmap_rules_getters, vmap_general_preprocess, _raise_value_error
+
+
+@vmap_rules_getters.register(G.NLLLossGrad)
+def get_nll_loss_grad_vmap_rule(prim, axis_size):
+    r"""
+    VmapRule for NLLLossGrad operations.
+
+    Limited by current kernel capability:
+    1. Only support one dim batch for x, loss_grad and target.
+    2. And weight only support shape as (C,), while total_weight should be a scalar.
+    """
+
+    @constexpr
+    def _get_reshape_shape(shape, keep_dim=0):
+        new_batch_size = reduce(lambda x, y: x * y, shape if keep_dim == 0 else shape[:-keep_dim])
+        return (new_batch_size,) if keep_dim == 0 else (new_batch_size, *shape[-keep_dim:])
+
+    if isinstance(prim, str):
+        prim = Primitive(prim)
+        reduction_type = "none"
+    else:
+        reduction_type = prim.reduction
+
+    def vmap_rule(x_bdim, loss_grad_bdim, target_bdim, weight_bdim, total_weight_bdim):
+        is_all_none, result = vmap_general_preprocess(
+            prim, x_bdim, loss_grad_bdim, target_bdim, weight_bdim, total_weight_bdim)
+        if is_all_none:
+            return result
+
+        x, x_dim = x_bdim
+        loss_grad, lg_dim = loss_grad_bdim
+        target, target_dim = target_bdim
+        weight, w_dim = weight_bdim
+        total_weight, tw_dim = total_weight_bdim
+
+        x_shape = F.shape(x)
+        loss_grad_shape = F.shape(loss_grad)
+        target_shape = F.shape(target)
+        base_x_len = len(x_shape) - (1 if x_dim is not None else 0)
+
+        if w_dim is not None or tw_dim is not None:
+            _raise_value_error("The source axis of weight and total_weight in `NLLLossGrad` must be None for now, "
+                               "but got {} and {}.".format(w_dim, tw_dim))
+        if lg_dim is not None and (base_x_len != 2 or reduction_type != "none"):
+            _raise_value_error("The source axis of loss_grad in `NLLLossGrad` can be not None "
+                               "just when x is 2d and reduction type is none, "
+                               "but x is {}d and reduction type is {}.".format(base_x_len, reduction_type))
+
+        # If stacked, move vmap_dim to first dim and reshape to right shape.
+        if x_dim is not None:
+            if x_dim != 0:
+                mnp.moveaxis(x, x_dim, 0)
+                x_shape = F.shape(x)
+            if base_x_len == 2:
+                x = F.reshape(x, _get_reshape_shape(x_shape, 1))
+
+        if lg_dim is not None:
+            if lg_dim != 0:
+                mnp.moveaxis(loss_grad, lg_dim, 0)
+                loss_grad_shape = F.shape(loss_grad)
+            loss_grad = F.reshape(loss_grad, _get_reshape_shape(loss_grad_shape))
+
+        if target_dim is not None:
+            if target_dim != 0:
+                mnp.moveaxis(target, target_dim, 0)
+                target_shape = F.shape(target)
+            target = F.reshape(target, _get_reshape_shape(target_shape))
+
+        out = prim(x, loss_grad, target, weight, total_weight)
+        output = F.reshape(out, x_shape)
+        out_dim = 0
+        return (output, out_dim)
+
+    return vmap_rule
