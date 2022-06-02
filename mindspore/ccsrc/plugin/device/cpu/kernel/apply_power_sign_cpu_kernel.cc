678ca86804559ff369781b002678dc266516ad55
chujinjin <chujinjin52@huawei.com>
2022-06-02 10:58:57 +0800

    add vmap for applypowersign

--- mindspore/ccsrc/plugin/device/cpu/kernel/apply_power_sign_cpu_kernel.cc
+++ mindspore/ccsrc/plugin/device/cpu/kernel/apply_power_sign_cpu_kernel.cc
@@ -15,6 +15,10 @@
  */
 
 #include "plugin/device/cpu/kernel/apply_power_sign_cpu_kernel.h"
+#include <algorithm>
+#include <map>
+#include <functional>
+#include "kernel/common_utils.h"
 #include "plugin/device/cpu/kernel/nnacl/errorcode.h"
 #include "plugin/device/cpu/kernel/nnacl/fp32/adam_fp32.h"
 #include "plugin/device/cpu/hal/device/cpu_device_address.h"
@@ -56,28 +60,34 @@ void ApplyPowerSignCpuKernelMod::LaunchPowerSign(const std::vector<kernel::Addre
   T *beta = reinterpret_cast<T *>(inputs[kIndexBeta]->addr);
   T *gradient = reinterpret_cast<T *>(inputs[kIndexGrad]->addr);
 
-  // multithreading
-  size_t lens = inputs[kIndexVar]->size > 0 ? static_cast<size_t>(inputs[kIndexVar]->size / sizeof(T)) : 1;
-  auto task = [this, &var, &m, &gradient, &lr, &beta, &logbase, &sign_decay](size_t start, size_t end) {
-    T one = static_cast<T>(1.0);
-    for (size_t i = start; i < end; i++) {
-      m[i] = gradient[i] * (one - beta[0]) + m[i] * beta[0];
-      T sign_value = static_cast<T>(Sgn(gradient[i]) * Sgn(m[i]));
-      T update = exp(logbase[0] * sign_decay[0] * sign_value) * gradient[i];
-      var[i] = var[i] - lr[0] * update;
-    }
-  };
-  ParallelLaunchAutoSearch(task, lens, this, &parallel_search_info_);
+  for (int64_t b = 0; b < batch_size_; b++) {
+    // multithreading
+    auto task = [this, &var, &m, &gradient, &lr, &beta, &logbase, &sign_decay](size_t start, size_t end) {
+      T one = static_cast<T>(1.0);
+      for (size_t i = start; i < end; i++) {
+        m[i] = gradient[i] * (one - beta[0]) + m[i] * beta[0];
+        T sign_value = static_cast<T>(Sgn(gradient[i]) * Sgn(m[i]));
+        T update = exp(logbase[0] * sign_decay[0] * sign_value) * gradient[i];
+        var[i] = var[i] - lr[0] * update;
+      }
+    };
+    ParallelLaunchAutoSearch(task, input_elements_, this, &parallel_search_info_);
+    var = var + input_elements_;
+    m = m + input_elements_;
+    gradient = gradient + input_elements_;
+    lr++;
+    beta++;
+    logbase++;
+    sign_decay++;
+  }
 }
 
-void ApplyPowerSignCpuKernelMod::InitKernel(const CNodePtr &kernel_node) {
-  MS_EXCEPTION_IF_NULL(kernel_node);
-  kernel_name_ = common::AnfAlgo::GetCNodeName(kernel_node);
-  dtype_ = AnfAlgo::GetInputDeviceDataType(kernel_node, 0);
-  size_t input_num = common::AnfAlgo::GetInputTensorNum(kernel_node);
-  CHECK_KERNEL_INPUTS_NUM(input_num, kPowerSignInputsNum, kernel_name_);
-  size_t output_num = common::AnfAlgo::GetOutputTensorNum(kernel_node);
-  CHECK_KERNEL_OUTPUTS_NUM(output_num, kPowerSignOutputsNum, kernel_name_);
+bool ApplyPowerSignCpuKernelMod::Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+                                      const std::vector<KernelTensorPtr> &outputs) {
+  kernel_name_ = base_operator->name();
+  dtype_ = inputs[0]->GetDtype();
+  batch_rank_ = base_operator->get_batch_rank();
+  return true;
 }
 
 bool ApplyPowerSignCpuKernelMod::Launch(const std::vector<kernel::AddressPtr> &inputs,
@@ -95,6 +105,70 @@ bool ApplyPowerSignCpuKernelMod::Launch(const std::vector<kernel::AddressPtr> &i
                       << TypeIdToType(dtype_)->ToString();
   }
   return true;
+}
+
+int ApplyPowerSignCpuKernelMod::Resize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+                                       const std::vector<KernelTensorPtr> &outputs,
+                                       const std::map<uint32_t, tensor::TensorPtr> &inputsOnHost) {
+  int ret = KernelMod::Resize(base_operator, inputs, outputs, inputsOnHost);
+  if (ret != 0) {
+    return ret;
+  }
+
+  if (input_size_list_.size() != kPowerSignInputsNum) {
+    MS_LOG(ERROR) << "For '" << kernel_name_ << "' input size must be equal 7.";
+    return KRET_RESIZE_FAILED;
+  }
+
+  std::vector<int64_t> var_shape = inputs[kIndexVar]->GetShapeVector();
+  std::vector<int64_t> m_shape = inputs[kIndexM]->GetShapeVector();
+  std::vector<int64_t> lr_shape = inputs[kIndexLr]->GetShapeVector();
+  std::vector<int64_t> grad_shape = inputs[kIndexGrad]->GetShapeVector();
+
+  if (var_shape.empty()) {
+    MS_LOG(ERROR) << "For '" << kernel_name_
+                  << "', the dimension of 'var' must be at least 1-D, but got scalar or None.";
+    return KRET_RESIZE_FAILED;
+  }
+
+  if (!IsSameShape(var_shape, m_shape)) {
+    MS_LOG(ERROR) << "For '" << kernel_name_
+                  << "', the shape of 'accum' must be the same as the shape of 'var', "
+                     "but got the shape of 'accum': "
+                  << Vector2Str(m_shape) << " and the shape of 'var': " << Vector2Str(var_shape);
+    return KRET_RESIZE_FAILED;
+  }
+
+  if (!IsSameShape(var_shape, grad_shape)) {
+    MS_LOG(ERROR) << "For '" << kernel_name_
+                  << "', the shape of 'grad' must be the same as the shape of 'var', "
+                     "but got the shape of 'grad': "
+                  << Vector2Str(grad_shape) << " and the shape of 'var': " << Vector2Str(var_shape);
+    return KRET_RESIZE_FAILED;
+  }
+
+  if (batch_rank_ < 0 || lr_shape.size() != static_cast<size_t>(batch_rank_)) {
+    MS_LOG(ERROR) << "For '" << kernel_name_
+                  << "', the shape size of 'lr' must be equal to 'batch_rank', "
+                     "but got the shape of 'lr': "
+                  << Vector2Str(lr_shape) << " and 'batch_rank': " << batch_rank_;
+    return KRET_RESIZE_FAILED;
+  }
+
+  if (!lr_shape.empty()) {
+    batch_size_ = std::accumulate(lr_shape.begin(), lr_shape.end(), 1, std::multiplies<int64_t>());
+  }
+
+  if (batch_size_ <= 0) {
+    MS_LOG(ERROR) << "For '" << kernel_name_
+                  << "', batch_size_ must be greater than 0, but got batch_size: " << batch_size_;
+    return KRET_RESIZE_FAILED;
+  }
+
+  input_elements_ = std::accumulate(var_shape.begin(), var_shape.end(), 1, std::multiplies<int64_t>());
+  input_elements_ = input_elements_ / batch_size_;
+
+  return ret;
 }
 
 std::vector<KernelAttr> ApplyPowerSignCpuKernelMod::GetOpSupport() {
