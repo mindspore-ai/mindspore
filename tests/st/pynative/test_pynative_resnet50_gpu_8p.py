24658f4888aabafb33a9cd276c8a983a37121014
yanghaoran <yanghaoran2@huawei.com>
2022-06-22 11:42:41 +0800

    takedown pynative_resnet50_gpu_8p_performance to ensure gate stability

--- tests/st/pynative/test_pynative_resnet50_gpu_8p.py
+++ tests/st/pynative/test_pynative_resnet50_gpu_8p.py
@@ -15,7 +15,7 @@
 import os
 import pytest
 
-@pytest.mark.level0
+@pytest.mark.level1
 @pytest.mark.platform_x86_gpu_training
 @pytest.mark.env_single
 def test_pynative_resnet50_gpu_8p_performance():
