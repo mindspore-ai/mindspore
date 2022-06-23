4fb671392d855836a756fbc6bb29a7572f7eae35
VectorSL <shiliang10@huawei.com>
2022-06-23 14:37:18 +0800

    sparsematirxadd support batch

--- mindspore/core/ops/sparse_matrix_add.cc
+++ mindspore/core/ops/sparse_matrix_add.cc
@@ -31,6 +31,21 @@ using mindspore::abstract::AbstractTensor;
 using mindspore::abstract::AbstractTuple;
 namespace {
 constexpr auto kDenseShape = "dense_shape";
+constexpr size_t kSMAInputsNum = 12;
+constexpr size_t kADenseShapeIdx = 0;
+constexpr size_t kABatchPtrIdx = 1;
+constexpr size_t kAIndptrIdx = 2;
+constexpr size_t kAIndicesIdx = 3;
+constexpr size_t kAValuesIdx = 4;
+constexpr size_t kBDenseShapeIdx = 5;
+constexpr size_t kBBatchPtrIdx = 6;
+constexpr size_t kBIndptrIdx = 7;
+constexpr size_t kBIndicesIdx = 8;
+constexpr size_t kBValuesIdx = 9;
+constexpr size_t kAlphaIndex = 10;
+constexpr size_t kBetaIndex = 11;
+constexpr size_t kDefaultRank = 2;
+constexpr size_t kBatchedRank = 3;
 
 inline void CheckSparseShape(const size_t sparse_shape_size, const size_t expected_dim, const std::string &arg_name) {
   if (sparse_shape_size != expected_dim) {
@@ -70,38 +85,40 @@ AbstractBasePtr SparseMatrixAddInfer(const abstract::AnalysisEnginePtr &, const 
   MS_EXCEPTION_IF_NULL(primitive);
   const std::string op_name = primitive->name();
   // Addition of two CSR tensors. C = Alpha * A + Beta * B.
-  // Eight input (CSR_A(three tensors), CSR_B(three tensors), Alpha, Beta)
-  constexpr size_t kSMAInputsNum = 8;
-  constexpr size_t kSMAShapeSize = 2;
-  constexpr size_t kAIndptrIdx = 0;
-  constexpr size_t kAIndicesIdx = 1;
-  constexpr size_t kAValuesIdx = 2;
-  constexpr size_t kBIndptrIdx = 3;
-  constexpr size_t kBIndicesIdx = 4;
-  constexpr size_t kBValuesIdx = 5;
-  constexpr size_t kAlphaIndex = 6;
-  constexpr size_t kBetaIndex = 7;
+  // Eight input (CSR_A(five tensors with dense_shape, batch, indptr, index, value), CSR_B(five tensors), Alpha, Beta)
   mindspore::abstract::CheckArgsSize(op_name, input_args, kSMAInputsNum);
+  auto a_dense_shape = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kADenseShapeIdx);
+  auto a_batch = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kABatchPtrIdx);
   auto a_indptr = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kAIndptrIdx);
   auto a_indices = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kAIndicesIdx);
   auto a_values = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kAValuesIdx);
+  auto b_dense_shape = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kBDenseShapeIdx);
+  auto b_batch = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kBBatchPtrIdx);
   auto b_indptr = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kBIndptrIdx);
   auto b_indices = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kBIndicesIdx);
   auto b_values = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kBValuesIdx);
   auto alpha = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kAlphaIndex);
   auto beta = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kBetaIndex);
+  MS_EXCEPTION_IF_NULL(a_dense_shape);
+  MS_EXCEPTION_IF_NULL(a_batch);
   MS_EXCEPTION_IF_NULL(a_indptr);
   MS_EXCEPTION_IF_NULL(a_indices);
   MS_EXCEPTION_IF_NULL(a_values);
+  MS_EXCEPTION_IF_NULL(b_dense_shape);
+  MS_EXCEPTION_IF_NULL(b_batch);
   MS_EXCEPTION_IF_NULL(b_indptr);
   MS_EXCEPTION_IF_NULL(b_indices);
   MS_EXCEPTION_IF_NULL(b_values);
   MS_EXCEPTION_IF_NULL(alpha);
   MS_EXCEPTION_IF_NULL(beta);
-  if (!primitive->HasAttr(kDenseShape)) {
-    MS_LOG(EXCEPTION) << "Primitive don't have dense_shape attr!";
+  // dense_shape is input[0]
+  std::vector<int64_t> a_dense_shape_shape =
+    CheckAndConvertUtils::ConvertShapePtrToShapeMap(input_args[kADenseShapeIdx]->BuildShape())[kShape];
+  const int64_t rank_a = a_dense_shape_shape[0];
+  if (rank_a != kBatchedRank && rank_a != kDefaultRank) {
+    MS_EXCEPTION(ValueError) << "For " << op_name << ", the input dense_shape should have rank 2 or 3, "
+                             << "but got " << rank_a << ".";
   }
-  auto dense_shape = GetValue<std::vector<int64_t>>(primitive->GetAttr(kDenseShape));
   // 1-D indptr
   CheckSparseShape(a_indptr->shape()->shape().size(), 1, "A indptr");
   CheckSparseShape(b_indptr->shape()->shape().size(), 1, "B indptr");
@@ -131,28 +148,38 @@ AbstractBasePtr SparseMatrixAddInfer(const abstract::AnalysisEnginePtr &, const 
   // Indices must be int16, int32 or int64.
   CheckSparseIndicesDtype(a_indices->element()->BuildType(), op_name);
   CheckSparseIndicesDtype(b_indices->element()->BuildType(), op_name);
-
-  if (dense_shape.size() != kSMAShapeSize) {
-    MS_EXCEPTION(ValueError) << "Currently only supports 2-dimensional csr tensor, but got shape length = "
-                             << dense_shape.size() << ".";
+  abstract::ShapePtr out_batch_shape;
+  abstract::ShapePtr out_indptr_shape;
+  if (input_args[kADenseShapeIdx]->isa<abstract::AbstractTensor>() &&
+      !input_args[kADenseShapeIdx]->BuildValue()->isa<AnyValue>() &&
+      !input_args[kADenseShapeIdx]->BuildValue()->isa<None>()) {
+    ShapeVector out_batch_ptr_shape;
+    ShapeVector out_index_ptr_shape;
+    auto dense_shape = input_args[kADenseShapeIdx]->cast<abstract::AbstractTensorPtr>();
+    auto dense_shape_ptr = dense_shape->BuildValue();
+    auto dense_shape_ptr_tensor = CheckAndConvertUtils::CheckTensorIntValue("a_dense_shape", dense_shape_ptr, op_name);
+    auto row = dense_shape_ptr_tensor[(rank_a == kBatchedRank) ? 1 : 0];
+    auto batch_size = (rank_a == kBatchedRank) ? dense_shape_ptr_tensor[0] : 1;
+    out_batch_ptr_shape.emplace_back(batch_size + 1);
+    out_batch_shape = std::make_shared<abstract::Shape>(out_batch_ptr_shape);
+    out_index_ptr_shape.emplace_back((row + 1) * batch_size);
+    out_indptr_shape = std::make_shared<abstract::Shape>(out_index_ptr_shape);
+  } else {
+    ShapeVector out_shape{-2};
+    ShapeVector min_shape{1};
+    ShapeVector max_shape{1};
+    out_batch_shape = std::make_shared<abstract::Shape>(out_shape, min_shape, max_shape);
+    out_indptr_shape = std::make_shared<abstract::Shape>(out_shape, min_shape, max_shape);
   }
-  if (dense_shape[0] + 1 != a_indptr->shape()->shape()[0]) {
-    MS_EXCEPTION(ValueError) << "Indptr must have length (dense_shape[0] + 1), but got "
-                             << a_indptr->shape()->shape()[0];
-  }
-  // Make output a csr(indptr, indices, values)
+  // Make output a csr(dense_shape, batch, indptr, indices, values)
   // Indices maybe dynamic, max_shape = min(a_idx + b_idx, m * n); min_shape = max(a_idx, b_idx)
-  int64_t max_indices_shape_ = std::min(a_indices_shape[0] + b_indices_shape[0], dense_shape[0] * dense_shape[1]);
-  int64_t min_indices_shape_ = std::max(a_indices_shape[0], b_indices_shape[0]);
   ShapeVector out_shape{-1};
-  ShapeVector min_shape{min_indices_shape_};
-  ShapeVector max_shape{max_indices_shape_};
-  auto out_indices = std::make_shared<AbstractTensor>(
-    a_indices->element()->BuildType(), std::make_shared<mindspore::abstract::Shape>(out_shape, min_shape, max_shape));
-  auto out_values = std::make_shared<AbstractTensor>(
-    a_type, std::make_shared<mindspore::abstract::Shape>(out_shape, min_shape, max_shape));
-  auto out_indptr = std::make_shared<AbstractTensor>(a_indptr->element()->BuildType(), a_indptr->shape()->shape());
-  AbstractBasePtrList ret = {out_indptr, out_indices, out_values};
+  auto out_indices = std::make_shared<AbstractTensor>(a_indices->element()->BuildType(),
+                                                      std::make_shared<mindspore::abstract::Shape>(out_shape));
+  auto out_values = std::make_shared<AbstractTensor>(a_type, std::make_shared<mindspore::abstract::Shape>(out_shape));
+  auto out_batch = std::make_shared<AbstractTensor>(a_batch->element()->BuildType(), out_batch_shape);
+  auto out_indptr = std::make_shared<AbstractTensor>(a_indptr->element()->BuildType(), out_indptr_shape);
+  AbstractBasePtrList ret = {a_dense_shape, out_batch, out_indptr, out_indices, out_values};
   return std::make_shared<AbstractTuple>(ret);
 }
 MIND_API_OPERATOR_IMPL(SparseMatrixAdd, BaseOperator);
