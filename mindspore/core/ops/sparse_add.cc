641c6a2a902df654540f7f7ce457e9c5d97b9cdd
YijieChen <chenyijie6@huawei.com>
2022-06-23 17:07:23 +0800

    Modify input of sparse add

--- mindspore/core/ops/sparse_add.cc
+++ mindspore/core/ops/sparse_add.cc
@@ -30,10 +30,6 @@ namespace ops {
 using mindspore::abstract::AbstractTensor;
 using mindspore::abstract::AbstractTuple;
 namespace {
-constexpr auto kADenseShape = "a_shape";
-constexpr auto kBDenseShape = "b_shape";
-constexpr auto kThresh = "thresh";
-
 inline void CheckSparseAddShape(const size_t sparse_shape_size, const size_t expected_dim,
                                 const std::string &arg_name) {
   if (sparse_shape_size != expected_dim) {
@@ -45,74 +41,43 @@ inline void CheckSparseAddShape(const size_t sparse_shape_size, const size_t exp
 
 inline void CheckSparseAddIndicesDtype(const mindspore::TypePtr dtype, const std::string &arg_name) {
   if (!(dtype->equal(mindspore::kInt32))) {
-    MS_EXCEPTION(mindspore::TypeError) << "The dtype of " << arg_name << " must be Int16 or Int32 or Int64, but got "
-                                       << dtype->ToString() << ".";
+    MS_EXCEPTION(mindspore::TypeError) << "The dtype of " << arg_name << " Int32 but got " << dtype->ToString() << ".";
   }
 }
 }  // namespace
 
-void SparseAdd::set_a_dense_shape(const std::vector<int64_t> &shape) {
-  (void)this->AddAttr(kADenseShape, api::MakeValue(shape));
-}
-void SparseAdd::set_b_dense_shape(const std::vector<int64_t> &shape) {
-  (void)this->AddAttr(kBDenseShape, api::MakeValue(shape));
-}
-void SparseAdd::set_thresh(const float &thresh) { (void)this->AddAttr(kThresh, api::MakeValue(thresh)); }
-std::vector<int64_t> SparseAdd::get_a_dense_shape() const {
-  auto value_ptr = GetAttr(kADenseShape);
-  MS_EXCEPTION_IF_NULL(value_ptr);
-  return GetValue<std::vector<int64_t>>(value_ptr);
-}
-std::vector<int64_t> SparseAdd::get_b_dense_shape() const {
-  auto value_ptr = GetAttr(kBDenseShape);
-  MS_EXCEPTION_IF_NULL(value_ptr);
-  return GetValue<std::vector<int64_t>>(value_ptr);
-}
-float SparseAdd::get_thresh() const {
-  auto value_ptr = GetAttr(kThresh);
-  MS_EXCEPTION_IF_NULL(value_ptr);
-  return GetValue<float>(value_ptr);
-}
-
-void SparseAdd::Init(const std::vector<int64_t> &a_shape, const std::vector<int64_t> &b_shape, const float &thresh) {
-  auto op_name = this->name();
-  if (a_shape.size() != b_shape.size()) {
-    MS_LOG(EXCEPTION) << "For " << op_name
-                      << " the rank of two dense shape should be the same, but got the rank of a_shape is "
-                      << a_shape.size() << ", and b_shape is " << b_shape.size();
-  }
-  if (a_shape != b_shape) {
-    MS_LOG(EXCEPTION) << "For " << op_name << " two dense shape should be the same, but got a_shape is " << a_shape
-                      << ", and b_shape is " << b_shape;
-  }
-  this->set_a_dense_shape(a_shape);
-  this->set_b_dense_shape(b_shape);
-  this->set_thresh(thresh);
-}
-
 AbstractBasePtr SparseAddInfer(const abstract::AnalysisEnginePtr &, const PrimitivePtr &primitive,
                                const std::vector<abstract::AbstractBasePtr> &input_args) {
   MS_EXCEPTION_IF_NULL(primitive);
-  auto a_shape = GetValue<std::vector<int64_t>>(primitive->GetAttr(kADenseShape));
   const std::string op_name = primitive->name();
 
   constexpr size_t kAIndicesIdx = 0;
   constexpr size_t kAValuesIdx = 1;
-  constexpr size_t kBIndicesIdx = 2;
-  constexpr size_t kBValuesIdx = 3;
-  constexpr size_t kNumOfInputs = 4;
+  constexpr size_t kAShapeIdx = 2;
+  constexpr size_t kBIndicesIdx = 3;
+  constexpr size_t kBValuesIdx = 4;
+  constexpr size_t kBShapeIdx = 5;
+  constexpr size_t kThreshIdx = 6;
+
+  constexpr size_t kNumOfInputs = 7;
   constexpr size_t kIndicesShape = 2;
 
   mindspore::abstract::CheckArgsSize(op_name, input_args, kNumOfInputs);
   auto a_indices = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kAIndicesIdx);
   auto a_values = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kAValuesIdx);
+  auto a_shape = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kAShapeIdx);
   auto b_indices = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kBIndicesIdx);
   auto b_values = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kBValuesIdx);
+  auto b_shape = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kBShapeIdx);
+  auto thresh = mindspore::abstract::CheckArg<AbstractTensor>(op_name, input_args, kThreshIdx);
 
   MS_EXCEPTION_IF_NULL(a_indices);
   MS_EXCEPTION_IF_NULL(a_values);
+  MS_EXCEPTION_IF_NULL(a_shape);
   MS_EXCEPTION_IF_NULL(b_indices);
   MS_EXCEPTION_IF_NULL(b_values);
+  MS_EXCEPTION_IF_NULL(b_shape);
+  MS_EXCEPTION_IF_NULL(thresh);
 
   // 2-D indices
   auto a_indices_shape = a_indices->shape()->shape();
@@ -124,6 +89,12 @@ AbstractBasePtr SparseAddInfer(const abstract::AnalysisEnginePtr &, const Primit
   auto b_values_shape = b_values->shape()->shape();
   CheckSparseAddShape(a_values_shape.size(), 1, "a_values");
   CheckSparseAddShape(b_values_shape.size(), 1, "b_values");
+
+  auto a_shape_shape = a_shape->shape()->shape();
+  auto b_shape_shape = b_shape->shape()->shape();
+  CheckSparseAddShape(a_shape_shape.size(), 1, "a_dense_shape");
+  CheckSparseAddShape(b_shape_shape.size(), 1, "b_dense_shape");
+  auto a_shape_type = a_shape->element()->BuildType();
 
   auto a_type = a_values->element()->BuildType();
   auto b_type = b_values->element()->BuildType();
@@ -137,7 +108,7 @@ AbstractBasePtr SparseAddInfer(const abstract::AnalysisEnginePtr &, const Primit
   CheckSparseAddIndicesDtype(a_indices->element()->BuildType(), op_name);
   CheckSparseAddIndicesDtype(b_indices->element()->BuildType(), op_name);
 
-  int64_t max_indices_shape_ = std::min(a_indices_shape[0] + b_indices_shape[0], a_shape[0] * a_shape[1]);
+  int64_t max_indices_shape_ = a_indices_shape[0] + b_indices_shape[0];
   int64_t min_indices_shape_ = std::max(a_indices_shape[0], b_indices_shape[0]);
   ShapeVector out_indices_shape{-1, 2};
   ShapeVector out_value_shape{-1};
@@ -151,8 +122,10 @@ AbstractBasePtr SparseAddInfer(const abstract::AnalysisEnginePtr &, const Primit
     std::make_shared<mindspore::abstract::Shape>(out_indices_shape, min_indices_shape, max_indices_shape));
   auto out_values = std::make_shared<AbstractTensor>(
     a_type, std::make_shared<mindspore::abstract::Shape>(out_value_shape, min_value_shape, max_value_shape));
+  auto out_shape =
+    std::make_shared<AbstractTensor>(a_shape_type, std::make_shared<mindspore::abstract::Shape>(a_shape_shape));
 
-  AbstractBasePtrList ret = {out_indices, out_values};
+  AbstractBasePtrList ret = {out_indices, out_values, out_shape};
   return std::make_shared<AbstractTuple>(ret);
 }
 MIND_API_OPERATOR_IMPL(SparseAdd, BaseOperator);
