6226a3fc23e8e4b5f92711e0461f7c05555932bb
CHEN YU <chenyu289@huawei.com>
2022-06-23 14:50:12 +0800

    make sigmoid and normalize support FP16

--- mindspore/lite/src/extendrt/delegate/tensorrt/tensorrt_utils.cc
+++ mindspore/lite/src/extendrt/delegate/tensorrt/tensorrt_utils.cc
@@ -220,9 +220,9 @@ std::experimental::optional<ActivationParams> TryConvertActivationType(schema::A
     {schema::ActivationType_RELU6, ActivationParams{nvinfer1::ActivationType::kCLIP, true, 0, true, 6}},
     {schema::ActivationType_RELU1, ActivationParams{nvinfer1::ActivationType::kCLIP, true, 0, true, 1}},
     {schema::ActivationType_HARD_TANH, ActivationParams{nvinfer1::ActivationType::kCLIP, true, -1, true, 1}},
+    {schema::ActivationType_SWISH, ActivationParams{nvinfer1::ActivationType::kSIGMOID, false, 0, false, 0}},
     // using plugin
-    {schema::ActivationType_GELU, ActivationParams{nvinfer1::ActivationType::kTHRESHOLDED_RELU, false, 0, false, 0}},
-    {schema::ActivationType_SWISH, ActivationParams{nvinfer1::ActivationType::kTHRESHOLDED_RELU, false, 0, false, 0}}};
+    {schema::ActivationType_GELU, ActivationParams{nvinfer1::ActivationType::kTHRESHOLDED_RELU, false, 0, false, 0}}};
   return action_map.find(activation_type) != action_map.end()
            ? std::experimental::optional<ActivationParams>(action_map[activation_type])
            : std::experimental::nullopt;
