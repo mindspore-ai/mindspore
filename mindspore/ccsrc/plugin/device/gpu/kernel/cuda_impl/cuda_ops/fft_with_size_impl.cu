0133fdb3d5abeab62cd9032acc1762f990423003
Yufei Wang <yufeiwang.work@outlook.com>
2022-06-29 17:46:37 +0800

    [feat] add new gpu backend of operator FFTWithSize(fft,ifft,rfft,irfft)

---
+++ mindspore/ccsrc/plugin/device/gpu/kernel/cuda_impl/cuda_ops/fft_with_size_impl.cu
@@ -0,0 +1,164 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "plugin/device/gpu/kernel/cuda_impl/cuda_ops/fft_with_size_impl.cuh"
+#include "include/cuda_runtime.h"
+
+// cublas exec scale
+#ifndef CUBLAS_EXEC_SCALE
+#define CUBLAS_EXEC_SCALE(cublas_exec, real, cu_complex)                                           \
+  do {                                                                                             \
+    if (scale_factor != 1.0) {                                                                     \
+      auto alpha = static_cast<real>(scale_factor);                                                \
+      auto out = static_cast<cu_complex *>(y_ptr);                                                 \
+      CUBLAS_CALL(cublas_exec(scale_plan, y_elements, &alpha, out, 1));                            \
+    }                                                                                              \
+  } while (0)
+#endif  // CUBLAS_EXEC_SCALE
+
+void CalculateFFT(cufftComplex *x_ptr, cufftComplex *y_ptr,
+                  const double &scale_factor, const int &y_elements,
+                  cufftHandle cufft_plan, cublasHandle_t scale_plan,
+                  const uint32_t &device_id, cudaStream_t cuda_stream) {
+  CUFFT_CALL(cufftSetStream(cufft_plan, cuda_stream));
+  CUBLAS_CALL(cublasSetStream_v2(scale_plan, cuda_stream));
+  CUFFT_CALL(cufftExecC2C(cufft_plan, x_ptr, y_ptr, CUFFT_FORWARD));
+  CUBLAS_EXEC_SCALE(cublasCsscal_v2, float, cuComplex);
+}
+
+void CalculateFFT(cufftDoubleComplex *x_ptr, cufftDoubleComplex *y_ptr,
+                  const double &scale_factor, const int &y_elements,
+                  cufftHandle cufft_plan, cublasHandle_t scale_plan,
+                  const uint32_t &device_id, cudaStream_t cuda_stream) {
+  CUFFT_CALL(cufftSetStream(cufft_plan, cuda_stream));
+  CUBLAS_CALL(cublasSetStream_v2(scale_plan, cuda_stream));
+  CUFFT_CALL(cufftExecZ2Z(cufft_plan, x_ptr, y_ptr, CUFFT_FORWARD));
+  CUBLAS_EXEC_SCALE(cublasZdscal_v2, double, cuDoubleComplex);
+}
+
+void CalculateIFFT(cufftComplex *x_ptr, cufftComplex *y_ptr,
+                   const double &scale_factor, const int &y_elements,
+                   cufftHandle cufft_plan, cublasHandle_t scale_plan,
+                   const uint32_t &device_id, cudaStream_t cuda_stream) {
+  CUFFT_CALL(cufftSetStream(cufft_plan, cuda_stream));
+  CUBLAS_CALL(cublasSetStream_v2(scale_plan, cuda_stream));
+  CUFFT_CALL(cufftExecC2C(cufft_plan, x_ptr, y_ptr, CUFFT_INVERSE));
+  CUBLAS_EXEC_SCALE(cublasCsscal_v2, float, cuComplex);
+}
+
+void CalculateIFFT(cufftDoubleComplex *x_ptr, cufftDoubleComplex *y_ptr,
+                   const double &scale_factor, const int &y_elements,
+                   cufftHandle cufft_plan, cublasHandle_t scale_plan,
+                   const uint32_t &device_id, cudaStream_t cuda_stream) {
+  CUFFT_CALL(cufftSetStream(cufft_plan, cuda_stream));
+  CUBLAS_CALL(cublasSetStream_v2(scale_plan, cuda_stream));
+  CUFFT_CALL(cufftExecZ2Z(cufft_plan, x_ptr, y_ptr, CUFFT_INVERSE));
+  CUBLAS_EXEC_SCALE(cublasZdscal_v2, double, cuDoubleComplex);
+}
+
+__global__ void Float2FloatComplex(const float *input_addr, cufftComplex *output_addr, const int len) {
+  for (size_t pos = blockIdx.x * blockDim.x + threadIdx.x; pos < len; pos += blockDim.x * gridDim.x) {
+    output_addr[pos] = make_cuFloatComplex(input_addr[pos], 0.);
+  }
+}
+
+__global__ void Double2DoubleComplex(const double *input_addr, cufftDoubleComplex *output_addr, const int len) {
+  for (size_t pos = blockIdx.x * blockDim.x + threadIdx.x; pos < len; pos += blockDim.x * gridDim.x) {
+    output_addr[pos] = make_cuDoubleComplex(input_addr[pos], 0.);
+  }
+}
+
+__global__ void FloatComplex2Float(const cufftComplex *input_addr, float *output_addr, const int len) {
+  for (size_t pos = blockIdx.x * blockDim.x + threadIdx.x; pos < len; pos += blockDim.x * gridDim.x) {
+    output_addr[pos] = cuCrealf(input_addr[pos]);
+  }
+}
+
+__global__ void DoubleComplex2Double(const cufftDoubleComplex *input_addr, double *output_addr, const int len) {
+  for (size_t pos = blockIdx.x * blockDim.x + threadIdx.x; pos < len; pos += blockDim.x * gridDim.x) {
+    output_addr[pos] = cuCreal(input_addr[pos]);
+  }
+}
+
+void CalculateRFFT(float *x_ptr, cufftComplex *w_ptr, cufftComplex *y_ptr,
+                   const bool &is_onesided, const double &scale_factor, const int &x_elements, const int &y_elements,
+                   cufftHandle cufft_plan, cublasHandle_t scale_plan,
+                   const uint32_t &device_id, cudaStream_t cuda_stream) {
+  CUFFT_CALL(cufftSetStream(cufft_plan, cuda_stream));
+  CUBLAS_CALL(cublasSetStream_v2(scale_plan, cuda_stream));
+  if (is_onesided) {  // onesided use native cufft r2c
+    CUFFT_CALL(cufftExecR2C(cufft_plan, x_ptr, y_ptr));
+  } else {  // full freq use [casting + c2c], cast real input buffer to complex workspace buffer
+    Float2FloatComplex<<<CUDA_BLOCKS(device_id, x_elements), CUDA_THREADS(device_id), 0, cuda_stream>>>(
+      x_ptr, w_ptr, x_elements);
+    CUFFT_CALL(cufftExecC2C(cufft_plan, w_ptr, y_ptr, CUFFT_FORWARD));
+  }
+  CUBLAS_EXEC_SCALE(cublasCsscal_v2, float, cuComplex);
+}
+
+void CalculateRFFT(double *x_ptr, cufftDoubleComplex *w_ptr, cufftDoubleComplex *y_ptr,
+                   const bool &is_onesided, const double &scale_factor, const int &x_elements, const int &y_elements,
+                   cufftHandle cufft_plan, cublasHandle_t scale_plan,
+                   const uint32_t &device_id, cudaStream_t cuda_stream) {
+  CUFFT_CALL(cufftSetStream(cufft_plan, cuda_stream));
+  CUBLAS_CALL(cublasSetStream_v2(scale_plan, cuda_stream));
+  if (is_onesided) {  // onesided use native cufft r2c
+    CUFFT_CALL(cufftExecD2Z(cufft_plan, x_ptr, y_ptr));
+  } else {  // full freq use [casting + c2c], cast real input buffer to complex workspace buffer
+    Double2DoubleComplex<<<CUDA_BLOCKS(device_id, x_elements), CUDA_THREADS(device_id), 0, cuda_stream>>>(
+      x_ptr, w_ptr, x_elements);
+    CUFFT_CALL(cufftExecZ2Z(cufft_plan, w_ptr, y_ptr, CUFFT_FORWARD));
+  }
+  CUBLAS_EXEC_SCALE(cublasZdscal_v2, double, cuDoubleComplex);
+}
+
+void CalculateIRFFT(cufftComplex *x_ptr, cufftComplex *w_ptr, float *y_ptr,
+                    const bool &is_onesided, const double &scale_factor, const int &x_elements, const int &y_elements,
+                    cufftHandle cufft_plan, cublasHandle_t scale_plan,
+                    const uint32_t &device_id, cudaStream_t cuda_stream) {
+  CUFFT_CALL(cufftSetStream(cufft_plan, cuda_stream));
+  CUBLAS_CALL(cublasSetStream_v2(scale_plan, cuda_stream));
+  if (is_onesided) {  // onesided use native cufft c2r
+    // complex-to-real need to copy input buffer to tmp buffer to avoid cufft overwriting.
+    CUDA_RT_CALL(cudaMemcpyAsync(w_ptr, x_ptr, x_elements * sizeof(cufftComplex), cudaMemcpyDeviceToDevice));
+    CUFFT_CALL(cufftExecC2R(cufft_plan, w_ptr, y_ptr));
+  } else {  // full freq use [c2c + casting]
+    // dump c2c result to workspace buffer, then cast complex workspace buffer to real output buffer.
+    CUFFT_CALL(cufftExecC2C(cufft_plan, x_ptr, w_ptr, CUFFT_INVERSE));
+    FloatComplex2Float<<<CUDA_BLOCKS(device_id, y_elements), CUDA_THREADS(device_id), 0, cuda_stream>>>(
+      w_ptr, y_ptr, y_elements);
+  }
+  CUBLAS_EXEC_SCALE(cublasSscal_v2, float, float);
+}
+
+void CalculateIRFFT(cufftDoubleComplex *x_ptr, cufftDoubleComplex *w_ptr, double *y_ptr,
+                    const bool &is_onesided, const double &scale_factor, const int &x_elements, const int &y_elements,
+                    cufftHandle cufft_plan, cublasHandle_t scale_plan,
+                    const uint32_t &device_id, cudaStream_t cuda_stream) {
+  CUFFT_CALL(cufftSetStream(cufft_plan, cuda_stream));
+  CUBLAS_CALL(cublasSetStream_v2(scale_plan, cuda_stream));
+  if (is_onesided) {  // onesided use native cufft r2c
+    // complex-to-real need to copy input buffer to tmp buffer to avoid cufft overwriting.
+    CUDA_RT_CALL(cudaMemcpyAsync(w_ptr, x_ptr, x_elements * sizeof(cufftDoubleComplex), cudaMemcpyDeviceToDevice));
+    CUFFT_CALL(cufftExecZ2D(cufft_plan, w_ptr, y_ptr));
+  } else {  // full freq use [c2c + casting]
+    // dump c2c result to workspace buffer, then cast complex workspace buffer to real output buffer.
+    CUFFT_CALL(cufftExecZ2Z(cufft_plan, x_ptr, w_ptr, CUFFT_INVERSE));
+    DoubleComplex2Double<<<CUDA_BLOCKS(device_id, y_elements), CUDA_THREADS(device_id), 0, cuda_stream>>>(
+      w_ptr, y_ptr, y_elements);
+  }
+  CUBLAS_EXEC_SCALE(cublasDscal_v2, double, double);
+}
---
+++ mindspore/ccsrc/plugin/device/gpu/kernel/math/fft_with_size_gpu_kernel.h
@@ -0,0 +1,119 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MINDSPORE_CCSRC_PLUGIN_DEVICE_GPU_KERNEL_MATH_FFT_WITH_SIZE_GPU_KERNEL_H_
+#define MINDSPORE_CCSRC_PLUGIN_DEVICE_GPU_KERNEL_MATH_FFT_WITH_SIZE_GPU_KERNEL_H_
+
+#include <cufft.h>
+#include <cublas_v2.h>
+#include <cuda_runtime_api.h>
+#include <map>
+#include <vector>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <functional>
+#include "mindspore/core/ops/fft_with_size.h"
+#include "plugin/device/gpu/kernel/gpu_kernel.h"
+#include "plugin/device/gpu/kernel/gpu_kernel_factory.h"
+#include "plugin/device/gpu/kernel/cuda_impl/cuda_ops/fft_with_size_impl.cuh"
+
+namespace mindspore {
+namespace kernel {
+enum class FFTVariety { unknown = 0, fft = 1, ifft = 2, rfft = 3, irfft = 4 };
+
+class FFTWithSizeGpuKernelMod : public NativeGpuKernelMod {
+ public:
+  FFTWithSizeGpuKernelMod() = default;
+  ~FFTWithSizeGpuKernelMod() override { ResetResource(); };
+
+  bool Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+            const std::vector<KernelTensorPtr> &outputs) override;
+
+  int Resize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+             const std::vector<KernelTensorPtr> &outputs,
+             const std::map<uint32_t, tensor::TensorPtr> &inputsOnHost) override {
+    return resize_func_(this, base_operator, inputs, outputs, inputsOnHost);
+  }
+
+  bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
+              const std::vector<AddressPtr> &outputs, void *stream_ptr) override {
+    return launch_func_(this, inputs, workspace, outputs, stream_ptr);
+  }
+
+ protected:
+  std::vector<KernelAttr> GetOpSupport() override;
+
+ private:
+  void ResetResource() noexcept;
+  bool MakeCufftPlan(const std::vector<KernelTensorPtr> &inputs, const std::vector<KernelTensorPtr> &outputs);
+  int ResizeBase(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+                 const std::vector<KernelTensorPtr> &outputs, const std::map<uint32_t, tensor::TensorPtr> &);
+  int ResizeFFT(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+                const std::vector<KernelTensorPtr> &outputs, const std::map<uint32_t, tensor::TensorPtr> &);
+  int ResizeIFFT(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+                 const std::vector<KernelTensorPtr> &outputs, const std::map<uint32_t, tensor::TensorPtr> &);
+  int ResizeRFFT(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+                 const std::vector<KernelTensorPtr> &outputs, const std::map<uint32_t, tensor::TensorPtr> &);
+  int ResizeIRFFT(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+                  const std::vector<KernelTensorPtr> &outputs, const std::map<uint32_t, tensor::TensorPtr> &);
+  using FFTWithSizeResizeFunc =
+    std::function<int(FFTWithSizeGpuKernelMod *, const BaseOperatorPtr &, const std::vector<KernelTensorPtr> &,
+                      const std::vector<KernelTensorPtr> &, const std::map<uint32_t, tensor::TensorPtr> &)>;
+  FFTWithSizeResizeFunc resize_func_{};
+
+  template <typename T>
+  bool LaunchFFT(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
+                 const std::vector<AddressPtr> &outputs, void *stream_ptr);
+  template <typename T>
+  bool LaunchIFFT(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
+                  const std::vector<AddressPtr> &outputs, void *stream_ptr);
+  template <typename S, typename T>
+  bool LaunchRFFT(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
+                  const std::vector<AddressPtr> &outputs, void *stream_ptr);
+  template <typename S, typename T>
+  bool LaunchIRFFT(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
+                   const std::vector<AddressPtr> &outputs, void *stream_ptr);
+  using FFTWithSizeLaunchFunc =
+    std::function<bool(FFTWithSizeGpuKernelMod *, const std::vector<AddressPtr> &, const std::vector<AddressPtr> &,
+                       const std::vector<AddressPtr> &, void *)>;
+  FFTWithSizeLaunchFunc launch_func_{};
+
+ private:
+  int64_t rank_{1};
+  bool is_inverse_{false};  // 0: forward, 1: inverse
+  bool is_real_{false};
+  std::string norm_type_{"backward"};  // forward, backward, ortho
+  // is_onesided controls whether frequency is halved when signal is real, which means is_real_ is true.
+  // The default value is true. cufft does not support full freq with real signal. We use cast as a temporary solution.
+  bool is_onesided_{true};
+
+  cufftHandle cufft_plan_{0};
+  cufftType cufft_type_{CUFFT_C2C};
+  cublasHandle_t scale_plan_{nullptr};
+  double scale_factor_{1.0};
+
+  FFTVariety fft_variety_{FFTVariety::unknown};
+  size_t data_type_bytes_{1};
+  std::vector<int64_t> x_shape_;
+  std::vector<int64_t> y_shape_;
+  int x_elements_{0};
+  int y_elements_{0};
+};
+}  // namespace kernel
+}  // namespace mindspore
+
+#endif  // MINDSPORE_CCSRC_PLUGIN_DEVICE_GPU_KERNEL_MATH_FFT_WITH_SIZE_GPU_KERNEL_H_
