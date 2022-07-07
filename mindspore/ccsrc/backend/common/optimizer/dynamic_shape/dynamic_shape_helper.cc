66883e56f5f477b9517576430937578158ce2f0c
tronzhang <zhangzhaochuang@huawei.com>
2022-07-07 14:52:32 +0800

    fix error log and refactor function args

--- mindspore/ccsrc/backend/common/optimizer/dynamic_shape/dynamic_shape_helper.cc
+++ mindspore/ccsrc/backend/common/optimizer/dynamic_shape/dynamic_shape_helper.cc
@@ -19,6 +19,7 @@
 #include <memory>
 #include <stack>
 #include <set>
+#include <string>
 #include <vector>
 #include <map>
 #include <utility>
@@ -95,12 +96,15 @@ bool InferShapeForDefiniteOutputNode(const CNodePtr &cnode) {
   return true;
 }
 
-tensor::TensorPtr GetDependValueTensor(size_t i, const AnfNodePtr &real_input, size_t real_input_index,
-                                       bool skip_nop_node, void *args) {
+tensor::TensorPtr GetDependValueTensor(const AnfNodePtr &node, size_t i,
+                                       const std::pair<AnfNodePtr, size_t> &input_node_with_index, bool skip_nop_node,
+                                       void *args) {
+  auto real_input = input_node_with_index.first;
+  MS_EXCEPTION_IF_NULL(real_input);
+  auto real_input_index = input_node_with_index.second;
   auto shapes = trans::GetRuntimePaddingShape(real_input, real_input_index);
   auto host_type = common::AnfAlgo::GetOutputInferDataType(real_input, real_input_index);
   auto out_tensor = std::make_shared<tensor::Tensor>(host_type, shapes);
-  MS_EXCEPTION_IF_NULL(out_tensor);
 
   auto output_addr = AnfAlgo::GetMutableOutputAddr(real_input, real_input_index, skip_nop_node);
   if (output_addr != nullptr && output_addr->GetPtr() != nullptr) {
@@ -111,17 +115,32 @@ tensor::TensorPtr GetDependValueTensor(size_t i, const AnfNodePtr &real_input, s
   } else {
     // If real_input is parameter and is control flow's output, the device address stored in AnfNode is useless.
     if (args == nullptr) {
-      MS_LOG(EXCEPTION) << "Address is nullptr, something error";
+      MS_LOG(EXCEPTION) << "Address is nullptr, and no valid address args is passed!";
     }
     auto input_device_address = reinterpret_cast<std::vector<device::DeviceAddress *> *>(args);
     if (i >= input_device_address->size() || input_device_address->at(i) == nullptr) {
-      MS_LOG(EXCEPTION) << "i is nullptr, something error";
+      MS_EXCEPTION_IF_NULL(node);
+      MS_LOG(EXCEPTION) << "There is no valid address for " << i << " input of " << node->fullname_with_scope();
     }
 
     out_tensor->data_sync_directly(input_device_address->at(i));
   }
 
   return out_tensor;
+}
+
+std::string GetResultShapeStr(const AbstractBasePtr &eval_result) {
+  MS_EXCEPTION_IF_NULL(eval_result);
+  auto result_shape = eval_result->BuildShape();
+  MS_EXCEPTION_IF_NULL(result_shape);
+  return result_shape->ToString();
+}
+
+std::string GetResultTypeStr(const AbstractBasePtr &eval_result) {
+  MS_EXCEPTION_IF_NULL(eval_result);
+  auto result_type = eval_result->BuildType();
+  MS_EXCEPTION_IF_NULL(result_type);
+  return result_type->ToString();
 }
 
 void InferShape(const CNodePtr &cnode, std::map<uint32_t, tensor::TensorPtr> *depend_tensor_map, void *args) {
@@ -154,7 +173,7 @@ void InferShape(const CNodePtr &cnode, std::map<uint32_t, tensor::TensorPtr> *de
       InferShapeForNopNode(real_input);
     }
     if (depend_list.find(i) != depend_list.end()) {
-      auto out_tensor = GetDependValueTensor(i, real_input, real_input_index, skip_nop_node, args);
+      auto out_tensor = GetDependValueTensor(cnode, i, input_node_with_index, skip_nop_node, args);
       auto ret2 = depend_tensor_map->try_emplace(i, out_tensor);
       if (!ret2.second) {
         MS_LOG(EXCEPTION) << "Insert map failed.";
@@ -176,6 +195,9 @@ void InferShape(const CNodePtr &cnode, std::map<uint32_t, tensor::TensorPtr> *de
     common::AnfAlgo::AddArgList(&args_spec_list, real_input, real_input_index);
   }
   auto eval_result = opt::CppInferShape(primitive, args_spec_list);
+
+  MS_LOG(DEBUG) << "Infer result of " << cnode->fullname_with_scope() << " is: shapes["
+                << GetResultShapeStr(eval_result) << "], types[" << GetResultTypeStr(eval_result) << "].";
   cnode->set_abstract(eval_result);
 }
 }  // namespace
