c58e745ffadbb7df19d6fd1bebd459b31221d06f
fan-jibin <fanjibin@huawei.com>
2022-07-07 18:01:45 +0800

    optimize maxpool3dwithargmax

--- mindspore/ccsrc/plugin/device/gpu/kernel/cuda_impl/cuda_ops/maxpool3d_with_argmax_impl.cuh
+++ mindspore/ccsrc/plugin/device/gpu/kernel/cuda_impl/cuda_ops/maxpool3d_with_argmax_impl.cuh
@@ -19,13 +19,12 @@
 #include "plugin/device/gpu/kernel/cuda_impl/cuda_ops/cuda_common.h"
 
 template <typename T, typename S>
-CUDA_LIB_EXPORT void CalMaxPool3DWithArgmax(const T *input, const int64_t n, const int64_t c, const int64_t d,
-                                            const int64_t h, const int64_t w, const int64_t ksize_d,
-                                            const int64_t ksize_h, const int64_t ksize_w, const int64_t stride_d,
-                                            const int64_t stride_h, const int64_t stride_w, const int64_t pad_front,
-                                            const int64_t pad_top, const int64_t pad_left, const int64_t dilation_d,
-                                            const int64_t dilation_h, const int64_t dilation_w, const int64_t out_d,
-                                            const int64_t out_h, const int64_t out_w, T *output, S *index,
+CUDA_LIB_EXPORT void CalMaxPool3DWithArgmax(const T *input, const int n, const int c, const int d, const int h,
+                                            const int w, const int ksize_d, const int ksize_h, const int ksize_w,
+                                            const int stride_d, const int stride_h, const int stride_w,
+                                            const int pad_front, const int pad_top, const int pad_left,
+                                            const int dilation_d, const int dilation_h, const int dilation_w,
+                                            const int out_d, const int out_h, const int out_w, T *output, S *index,
                                             const uint32_t device_id, cudaStream_t cuda_stream);
 
 #endif  // MINDSPORE_CCSRC_PLUGIN_DEVICE_GPU_KERNEL_CUDA_IMPL_CUDA_OPS_MAXPOOL3D_WITH_ARGMAX_IMPL_CUH_
