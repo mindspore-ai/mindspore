3eb390772962c2f74f20330644ff3e5dafb583fc
lilinjie <lilinjie11@huawei.com>
2022-07-12 17:19:24 +0800

    remove duplicated definitions

--- mindspore/python/mindspore/ops/function/math_func.py
+++ mindspore/python/mindspore/ops/function/math_func.py
@@ -982,7 +982,7 @@ def logical_not(x):
     return logical_not_(x)
 
 
-def logical_or(x):
+def logical_or(x, y):
     """
     Computes the "logical OR" of two tensors element-wise.
 
@@ -1021,10 +1021,10 @@ def logical_or(x):
         >>> print(output)
         [ True  True  True]
     """
-    return logical_or_(x)
+    return logical_or_(x, y)
 
 
-def logical_and(x):
+def logical_and(x, y):
     r"""
     Computes the "logical AND" of two tensors element-wise.
 
@@ -1063,7 +1063,7 @@ def logical_and(x):
         >>> print(output)
         [ True False False]
     """
-    return logical_and_(x)
+    return logical_and_(x, y)
 
 
 def sin(x):
