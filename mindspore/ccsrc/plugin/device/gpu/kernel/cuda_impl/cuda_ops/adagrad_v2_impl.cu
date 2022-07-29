a32ba54ee34cd64970b5531d102af70b3838a314
kyr <284831332@qq.com>
2022-07-29 18:13:07 +0800

    new

---
+++ mindspore/ccsrc/plugin/device/gpu/kernel/cuda_impl/cuda_ops/adagrad_v2_impl.cu
@@ -0,0 +1,242 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "plugin/device/gpu/kernel/cuda_impl/cuda_ops/adagrad_v2_impl.cuh"
+#include "include/cuda_fp16.h"
+
+template <typename T>
+__device__ __forceinline__ T SqrtFunc(T input) {
+  return sqrt(input);
+}
+
+template <>
+__device__ __forceinline__ half SqrtFunc(half input) {
+  return hsqrt(input);
+}
+
+template <typename T, typename S>
+__global__ void ApplyAdagradV2Kernel(const size_t size, const float epsilon, T *variable, T *accumulation,
+                                     const S *learning_rate, const T *gradient) {
+  T grad = static_cast<T>(0);
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    grad = gradient[i];
+    accumulation[i] += grad * grad;
+    variable[i] -= learning_rate[0] * grad / (SqrtFunc(accumulation[i] + epsilon));
+  }
+}
+
+template <>
+__global__ void ApplyAdagradV2Kernel(const size_t size, const float epsilon, half *variable, half *accumulation,
+                                     const half *learning_rate, const half *gradient) {
+  half grad = static_cast<half>(0);
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    grad = gradient[i];
+    accumulation[i] += grad * grad;
+    variable[i] -= learning_rate[0] * grad / (SqrtFunc(accumulation[i] + __float2half(epsilon)));
+  }
+}
+
+template <>
+__global__ void ApplyAdagradV2Kernel(const size_t size, const float epsilon, half *variable, half *accumulation,
+                                     const float *learning_rate, const half *gradient) {
+  half grad = static_cast<half>(0);
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    grad = gradient[i];
+    accumulation[i] += grad * grad;
+    variable[i] -= __float2half(learning_rate[0]) * grad / (SqrtFunc(accumulation[i] + __float2half(epsilon)));
+  }
+}
+
+template <>
+__global__ void ApplyAdagradV2Kernel(const size_t size, const float epsilon, half *variable, half *accumulation,
+                                     const double *learning_rate, const half *gradient) {
+  half grad = static_cast<half>(0);
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    grad = gradient[i];
+    accumulation[i] += grad * grad;
+    variable[i] -= __float2half(learning_rate[0]) * grad / (SqrtFunc(accumulation[i] + __float2half(epsilon)));
+  }
+}
+
+template <>
+__global__ void ApplyAdagradV2Kernel(const size_t size, const float epsilon, double *variable, double *accumulation,
+                                     const half *learning_rate, const double *gradient) {
+  double grad = static_cast<double>(0);
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    grad = gradient[i];
+    accumulation[i] += grad * grad;
+    variable[i] -= __half2float(learning_rate[0]) * grad / (SqrtFunc(accumulation[i] + epsilon));
+  }
+}
+
+template <>
+__global__ void ApplyAdagradV2Kernel(const size_t size, const float epsilon, double *variable, double *accumulation,
+                                     const float *learning_rate, const double *gradient) {
+  double grad = static_cast<double>(0);
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    grad = gradient[i];
+    accumulation[i] += grad * grad;
+    variable[i] -= learning_rate[0] * grad / (SqrtFunc(accumulation[i] + epsilon));
+  }
+}
+
+template <>
+__global__ void ApplyAdagradV2Kernel(const size_t size, const float epsilon, float *variable, float *accumulation,
+                                     const half *learning_rate, const float *gradient) {
+  float grad = static_cast<float>(0);
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    grad = gradient[i];
+    accumulation[i] += grad * grad;
+    variable[i] -= __half2float(learning_rate[0]) * grad / (SqrtFunc(accumulation[i] + epsilon));
+  }
+}
+
+template <>
+__global__ void ApplyAdagradV2Kernel(const size_t size, const float epsilon, float *variable, float *accumulation,
+                                     const double *learning_rate, const float *gradient) {
+  float grad = static_cast<float>(0);
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    grad = gradient[i];
+    accumulation[i] += grad * grad;
+    variable[i] -= learning_rate[0] * grad / (SqrtFunc(accumulation[i] + epsilon));
+  }
+}
+
+template <typename T, typename S>
+__global__ void ApplyAdagradV2Kernel_(const size_t size, const float epsilon, T *variable, T *accumulation,
+                                      const S *learning_rate, const T *gradient) {
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    variable[i] -= learning_rate[0] * gradient[i] / (SqrtFunc(accumulation[i] + epsilon));
+  }
+}
+
+template <>
+__global__ void ApplyAdagradV2Kernel_(const size_t size, const float epsilon, half *variable, half *accumulation,
+                                      const half *learning_rate, const half *gradient) {
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    variable[i] -= learning_rate[0] * gradient[i] / (SqrtFunc(accumulation[i] + __float2half(epsilon)));
+  }
+}
+
+template <>
+__global__ void ApplyAdagradV2Kernel_(const size_t size, const float epsilon, half *variable, half *accumulation,
+                                      const float *learning_rate, const half *gradient) {
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    variable[i] -= __float2half(learning_rate[0]) * gradient[i] / (SqrtFunc(accumulation[i] + __float2half(epsilon)));
+  }
+}
+
+template <>
+__global__ void ApplyAdagradV2Kernel_(const size_t size, const float epsilon, half *variable, half *accumulation,
+                                      const double *learning_rate, const half *gradient) {
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    variable[i] -= __float2half(learning_rate[0]) * gradient[i] / (SqrtFunc(accumulation[i] + __float2half(epsilon)));
+  }
+}
+
+template <>
+__global__ void ApplyAdagradV2Kernel_(const size_t size, const float epsilon, double *variable, double *accumulation,
+                                      const half *learning_rate, const double *gradient) {
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    variable[i] -= __half2float(learning_rate[0]) * gradient[i] / (SqrtFunc(accumulation[i] + epsilon));
+  }
+}
+
+template <>
+__global__ void ApplyAdagradV2Kernel_(const size_t size, const float epsilon, double *variable, double *accumulation,
+                                      const float *learning_rate, const double *gradient) {
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    variable[i] -= learning_rate[0] * gradient[i] / (SqrtFunc(accumulation[i] + epsilon));
+  }
+}
+
+template <>
+__global__ void ApplyAdagradV2Kernel_(const size_t size, const float epsilon, float *variable, float *accumulation,
+                                      const half *learning_rate, const float *gradient) {
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    variable[i] -= __half2float(learning_rate[0]) * gradient[i] / (SqrtFunc(accumulation[i] + epsilon));
+  }
+}
+
+template <>
+__global__ void ApplyAdagradV2Kernel_(const size_t size, const float epsilon, float *variable, float *accumulation,
+                                      const double *learning_rate, const float *gradient) {
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < size; i += gridDim.x * blockDim.x) {
+    variable[i] -= learning_rate[0] * gradient[i] / (SqrtFunc(accumulation[i] + epsilon));
+  }
+}
+
+template <typename T, typename S>
+void ApplyAdagradV2(const size_t size, const float epsilon, const bool update_slots, T *variable, T *accumulation,
+                    const S *learning_rate, const T *gradient, const uint32_t &device_id, cudaStream_t cuda_stream) {
+  if (update_slots) {
+    ApplyAdagradV2Kernel<<<CUDA_BLOCKS(device_id, size), CUDA_THREADS(device_id), 0, cuda_stream>>>(
+      size, epsilon, variable, accumulation, learning_rate, gradient);
+  } else {
+    ApplyAdagradV2Kernel_<<<CUDA_BLOCKS(device_id, size), CUDA_THREADS(device_id), 0, cuda_stream>>>(
+      size, epsilon, variable, accumulation, learning_rate, gradient);
+  }
+}
+
+template CUDA_LIB_EXPORT void ApplyAdagradV2<double, double>(const size_t size, const float epsilon,
+                                                             const bool update_slots, double *variable,
+                                                             double *accumulation, const double *learning_rate,
+                                                             const double *gradient, const uint32_t &device_id,
+                                                             cudaStream_t cuda_stream);
+
+template CUDA_LIB_EXPORT void ApplyAdagradV2<float, float>(const size_t size, const float epsilon,
+                                                           const bool update_slots, float *variable,
+                                                           float *accumulation, const float *learning_rate,
+                                                           const float *gradient, const uint32_t &device_id,
+                                                           cudaStream_t cuda_stream);
+
+template CUDA_LIB_EXPORT void ApplyAdagradV2<half, half>(const size_t size, const float epsilon,
+                                                         const bool update_slots, half *variable, half *accumulation,
+                                                         const half *learning_rate, const half *gradient,
+                                                         const uint32_t &device_id, cudaStream_t cuda_stream);
+
+template CUDA_LIB_EXPORT void ApplyAdagradV2<float, half>(const size_t size, const float epsilon,
+                                                          const bool update_slots, float *variable, float *accumulation,
+                                                          const half *learning_rate, const float *gradient,
+                                                          const uint32_t &device_id, cudaStream_t cuda_stream);
+
+template CUDA_LIB_EXPORT void ApplyAdagradV2<half, float>(const size_t size, const float epsilon,
+                                                          const bool update_slots, half *variable, half *accumulation,
+                                                          const float *learning_rate, const half *gradient,
+                                                          const uint32_t &device_id, cudaStream_t cuda_stream);
+
+template CUDA_LIB_EXPORT void ApplyAdagradV2<half, double>(const size_t size, const float epsilon,
+                                                           const bool update_slots, half *variable, half *accumulation,
+                                                           const double *learning_rate, const half *gradient,
+                                                           const uint32_t &device_id, cudaStream_t cuda_stream);
+
+template CUDA_LIB_EXPORT void ApplyAdagradV2<double, float>(const size_t size, const float epsilon,
+                                                            const bool update_slots, double *variable,
+                                                            double *accumulation, const float *learning_rate,
+                                                            const double *gradient, const uint32_t &device_id,
+                                                            cudaStream_t cuda_stream);
+
+template CUDA_LIB_EXPORT void ApplyAdagradV2<double, half>(const size_t size, const float epsilon,
+                                                           const bool update_slots, double *variable,
+                                                           double *accumulation, const half *learning_rate,
+                                                           const double *gradient, const uint32_t &device_id,
+                                                           cudaStream_t cuda_stream);
+
+template CUDA_LIB_EXPORT void ApplyAdagradV2<float, double>(const size_t size, const float epsilon,
+                                                            const bool update_slots, float *variable,
+                                                            float *accumulation, const double *learning_rate,
+                                                            const float *gradient, const uint32_t &device_id,
+                                                            cudaStream_t cuda_stream);
---
+++ mindspore/ccsrc/plugin/device/gpu/kernel/nn/adagrad_v2_gpu_kernel.h
@@ -0,0 +1,106 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MINDSPORE_CCSRC_PLUGIN_DEVICE_GPU_KERNEL_NN_ADAGRAD_V2_GPU_KERNEL_H_
+#define MINDSPORE_CCSRC_PLUGIN_DEVICE_GPU_KERNEL_NN_ADAGRAD_V2_GPU_KERNEL_H_
+
+#include <vector>
+#include <string>
+#include <functional>
+#include <map>
+#include <utility>
+#include <memory>
+#include <algorithm>
+#include <iostream>
+#include "mindspore/core/ops/apply_adagrad_v2.h"
+#include "kernel/common_utils.h"
+#include "include/curand.h"
+#include "abstract/utils.h"
+#include "plugin/device/gpu/kernel/gpu_kernel.h"
+#include "plugin/device/gpu/kernel/gpu_kernel_factory.h"
+#include "plugin/factory/ms_factory.h"
+#include "plugin/device/gpu/kernel/cuda_impl/cuda_ops/adagrad_v2_impl.cuh"
+// #include "plugin/device/gpu/kernel/cuda_impl/cuda_class/adagrad_v2_helper.h"
+
+namespace mindspore {
+namespace kernel {
+class AdagradV2GpuKernelMod : public NativeGpuKernelMod {
+ public:
+  AdagradV2GpuKernelMod() = default;
+  ~AdagradV2GpuKernelMod() override = default;
+
+  bool Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+            const std::vector<KernelTensorPtr> &outputs) override;
+
+  int Resize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+             const std::vector<KernelTensorPtr> &outputs, const std::map<uint32_t, tensor::TensorPtr> &) override;
+
+  bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
+              const std::vector<AddressPtr> &outputs, void *stream_ptr) override {
+    if (is_null_input_) {
+      return true;
+    }
+    stream_ptr_ = stream_ptr;
+    return kernel_func_(this, inputs, workspace, outputs);
+  }
+
+  std::vector<KernelAttr> GetOpSupport() override;
+
+  void ResetResource() noexcept {
+    is_null_input_ = false;
+    t_size_ = DEFAULT_SIZE_;
+    s_size_ = DEFAULT_SIZE_;
+    input_size_list_.clear();
+    output_size_list_.clear();
+    workspace_size_list_.clear();
+  }
+
+ private:
+  template <typename T, typename S>
+  bool LaunchKernel(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
+                    const std::vector<AddressPtr> &outputs);
+  using ApplyAdagradV2Func =
+    std::function<bool(AdagradV2GpuKernelMod *, const std::vector<kernel::AddressPtr> &,
+                       const std::vector<kernel::AddressPtr> &, const std::vector<kernel::AddressPtr> &)>;
+  void InOutputResize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+                      const std::vector<KernelTensorPtr> &outputs);
+
+ private:
+  constexpr static int64_t DEFAULT_SIZE_ = 4;
+
+  float epsilon_;
+  bool update_slots_;
+
+  int64_t variable_size_{0};
+  int64_t accumulation_size_{0};
+  int64_t learning_rate_size_{0};
+  int64_t gradient_size_{0};
+  bool is_null_input_{false};
+  std::string kernel_name_{"ApplyAdagradV2"};
+
+  int64_t t_size_{4};
+  int64_t s_size_{4};
+  int64_t input_elements_;
+  BaseOperatorPtr kernel_ptr_{nullptr};
+  std::vector<KernelTensorPtr> outputs_ = {};
+
+  ApplyAdagradV2Func kernel_func_{};
+  void *stream_ptr_{nullptr};
+  static std::vector<std::pair<KernelAttr, ApplyAdagradV2Func>> func_list_;
+};
+}  // namespace kernel
+}  // namespace mindspore
+#endif  // MINDSPORE_ADAGRAD_V2_GPU_KERNEL_H
