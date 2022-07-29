82b6495f4eb344717fd2b3a9f7df9d854600a646
gaopan12 <gaopan44@huawei.com>
2022-07-29 13:26:35 +0800

    fix linspace for nan output when num is 1

--- mindspore/ccsrc/plugin/device/gpu/kernel/cuda_impl/cuda_ops/linspace.cu
+++ mindspore/ccsrc/plugin/device/gpu/kernel/cuda_impl/cuda_ops/linspace.cu
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Huawei Technologies Co., Ltd
+ * Copyright 2022 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,15 +18,27 @@
 #include <iostream>
 
 template <typename T>
-__global__ void LinSpaceKernel(const T *start, const T *stop, const size_t value_count, T *output) {
+__global__ void LinSpaceCountNotEqualOneKernel(const T *start, const T *stop, const size_t value_count, T *output) {
   T add_value = ((*stop - *start) / (value_count - 1));
   for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < value_count; i += gridDim.x * blockDim.x) {
     output[i] = *start + (add_value * i);
   }
 }
 template <typename T>
+__global__ void LinSpaceCountEqualOneKernel(const T *start, const T *stop, const size_t value_count, T *output) {
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < value_count; i += gridDim.x * blockDim.x) {
+    output[i] = *start;
+  }
+}
+template <typename T>
 void calLinSpace(const T *start, const T *stop, const size_t value_count, T *output, cudaStream_t cuda_stream) {
-  LinSpaceKernel<<<GET_BLOCKS(value_count), GET_THREADS, 0, cuda_stream>>>(start, stop, value_count, output);
+  if (value_count != 1) {
+    LinSpaceCountNotEqualOneKernel<<<GET_BLOCKS(value_count), GET_THREADS, 0, cuda_stream>>>(start, stop, value_count,
+                                                                                             output);
+  } else {
+    LinSpaceCountEqualOneKernel<<<GET_BLOCKS(value_count), GET_THREADS, 0, cuda_stream>>>(start, stop, value_count,
+                                                                                          output);
+  }
 }
 template CUDA_LIB_EXPORT void calLinSpace<float>(const float *start, const float *stop, const size_t value_count,
                                                  float *output, cudaStream_t cuda_stream);
