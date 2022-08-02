aca8dc57130e6be56848aa6a5335727e8b048297
dayschan <chendeshi@huawei.com>
2022-08-02 16:03:28 +0800

    fix static check warnings

--- mindspore/ccsrc/plugin/device/cpu/kernel/akg/akg_cpu_kernel_build.cc
+++ mindspore/ccsrc/plugin/device/cpu/kernel/akg/akg_cpu_kernel_build.cc
@@ -15,15 +15,11 @@
  */
 
 #include "plugin/device/cpu/kernel/akg/akg_cpu_kernel_build.h"
-#include <Python.h>
-#include <vector>
 #include <memory>
 #include <string>
 #include "kernel/common_utils.h"
 #include "plugin/device/cpu/kernel/akg/akg_cpu_kernel_mod.h"
-#include "utils/ms_utils.h"
 #include "backend/common/session/anf_runtime_algorithm.h"
-#include "include/common/utils/anfalgo.h"
 
 namespace mindspore {
 namespace kernel {
