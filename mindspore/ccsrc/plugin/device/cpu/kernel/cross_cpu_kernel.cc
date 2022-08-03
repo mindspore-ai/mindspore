562f826244c3f16546b54876167e674621ca6e56
reku1997 <wangyucheng12@huawei.com>
2022-08-03 11:16:35 +0800

    clean code

--- mindspore/ccsrc/plugin/device/cpu/kernel/cross_cpu_kernel.cc
+++ mindspore/ccsrc/plugin/device/cpu/kernel/cross_cpu_kernel.cc
@@ -21,12 +21,6 @@ const size_t kDataSizeThreshold = 4 * 1024;
 const size_t kNumber0 = 0;
 const size_t kNumber1 = 1;
 const size_t kNumber3 = 3;
-
-#define CROSS_COMPUTE_CASE(DTYPE, TYPE)        \
-  case (DTYPE): {                              \
-    ret = LaunchKernel<TYPE>(inputs, outputs); \
-    break;                                     \
-  }
 }  // namespace
 
 namespace mindspore {
@@ -57,26 +51,36 @@ void CrossCpuKernelMod::InitKernel(const CNodePtr &kernel_node) {
 
 bool CrossCpuKernelMod::Launch(const std::vector<kernel::AddressPtr> &inputs, const std::vector<kernel::AddressPtr> &,
                                const std::vector<kernel::AddressPtr> &outputs) {
-  bool ret = true;
   switch (input1_dtype_) {
-    CROSS_COMPUTE_CASE(kNumberTypeInt8, int8_t)
-    CROSS_COMPUTE_CASE(kNumberTypeInt16, int16_t)
-    CROSS_COMPUTE_CASE(kNumberTypeInt32, int32_t)
-    CROSS_COMPUTE_CASE(kNumberTypeInt64, int64_t)
-    CROSS_COMPUTE_CASE(kNumberTypeUInt8, uint8_t)
-    CROSS_COMPUTE_CASE(kNumberTypeUInt16, uint16_t)
-    CROSS_COMPUTE_CASE(kNumberTypeUInt32, uint32_t)
-    CROSS_COMPUTE_CASE(kNumberTypeUInt64, uint64_t)
-    CROSS_COMPUTE_CASE(kNumberTypeFloat16, float16)
-    CROSS_COMPUTE_CASE(kNumberTypeFloat32, float)
-    CROSS_COMPUTE_CASE(kNumberTypeFloat64, double)
-    CROSS_COMPUTE_CASE(kNumberTypeComplex64, std::complex<float>)
-    CROSS_COMPUTE_CASE(kNumberTypeComplex128, std::complex<double>)
+    case kNumberTypeInt8:
+      return LaunchKernel<int8_t>(inputs, outputs);
+    case kNumberTypeInt16:
+      return LaunchKernel<int16_t>(inputs, outputs);
+    case kNumberTypeInt32:
+      return LaunchKernel<int32_t>(inputs, outputs);
+    case kNumberTypeInt64:
+      return LaunchKernel<int64_t>(inputs, outputs);
+    case kNumberTypeUInt8:
+      return LaunchKernel<uint8_t>(inputs, outputs);
+    case kNumberTypeUInt16:
+      return LaunchKernel<uint16_t>(inputs, outputs);
+    case kNumberTypeUInt32:
+      return LaunchKernel<uint32_t>(inputs, outputs);
+    case kNumberTypeUInt64:
+      return LaunchKernel<uint64_t>(inputs, outputs);
+    case kNumberTypeFloat16:
+      return LaunchKernel<float16>(inputs, outputs);
+    case kNumberTypeFloat32:
+      return LaunchKernel<float>(inputs, outputs);
+    case kNumberTypeFloat64:
+      return LaunchKernel<double>(inputs, outputs);
+    case kNumberTypeComplex64:
+      return LaunchKernel<std::complex<float>>(inputs, outputs);
+    case kNumberTypeComplex128:
+      return LaunchKernel<std::complex<double>>(inputs, outputs);
     default:
-      ret = false;
       MS_EXCEPTION(TypeError) << "Unsupported input data type: " << input1_dtype_;
   }
-  return ret;
 }
 
 template <typename T>
@@ -87,7 +91,7 @@ bool CrossCpuKernelMod::LaunchKernel(const std::vector<kernel::AddressPtr> &inpu
   for (size_t i = 0; i < input1_shape_.size(); i++) {
     tmp = tmp * input1_shape_[i];
   }
-  size_t input1_data_num = tmp;
+  size_t input1_data_num = LongToSize(tmp);
   auto input2_data_addr = reinterpret_cast<T *>(inputs[1]->addr);
   auto output_data_addr = reinterpret_cast<T *>(outputs[0]->addr);
   size_t total = input1_data_num / kNumber3;
@@ -95,25 +99,25 @@ bool CrossCpuKernelMod::LaunchKernel(const std::vector<kernel::AddressPtr> &inpu
   std::vector<size_t> a_stride(n);
   int64_t stride_tmp = 1;
   for (int64_t i = static_cast<int64_t>(n - 1); i >= 0; i--) {
-    a_stride[LongToSize(i)] = stride_tmp;
+    a_stride[LongToSize(i)] = LongToSize(stride_tmp);
     stride_tmp *= input1_shape_[LongToSize(i)];
   }
   size_t input1_data_stride = a_stride[LongToSize(dim_)];
   std::vector<size_t> b_stride(n);
   stride_tmp = 1;
   for (int64_t i = static_cast<int64_t>(n - 1); i >= 0; i--) {
-    b_stride[LongToSize(i)] = stride_tmp;
+    b_stride[LongToSize(i)] = LongToSize(stride_tmp);
     stride_tmp *= input2_shape_[LongToSize(i)];
   }
   size_t input2_data_stride = b_stride[LongToSize(dim_)];
   std::vector<size_t> r_stride(n);
   stride_tmp = 1;
   for (int64_t i = static_cast<int64_t>(n - 1); i >= 0; i--) {
-    r_stride[LongToSize(i)] = stride_tmp;
+    r_stride[LongToSize(i)] = LongToSize(stride_tmp);
     stride_tmp *= output_shape_[LongToSize(i)];
   }
   size_t output_data_stride = r_stride[LongToSize(dim_)];
-  const int64_t pos = 2;
+  const size_t pos = 2;
   auto cross_shard = [this, &a_stride, &b_stride, &r_stride, &output_data_addr, &input1_data_addr, &input2_data_addr,
                       &output_data_stride, &input1_data_stride, &input2_data_stride](size_t start, size_t end) {
     const size_t input1_data_dim = input1_shape_.size();
@@ -123,42 +127,44 @@ bool CrossCpuKernelMod::LaunchKernel(const std::vector<kernel::AddressPtr> &inpu
     int64_t input2_data_start = 0;
     int64_t output_data_start = 0;
     for (int64_t i = 0; i < static_cast<int64_t>(input1_data_dim); i++) {
-      if (i == static_cast<int64_t>(dim_)) continue;
+      if (i == static_cast<int64_t>(dim_)) {
+        continue;
+      }
       position_in_dims[LongToSize(i)] = index_in_curr_dim % input1_shape_[LongToSize(i)];
-      input1_data_start += (index_in_curr_dim % input1_shape_[LongToSize(i)]) * a_stride[LongToSize(i)];
-      input2_data_start += (index_in_curr_dim % input2_shape_[LongToSize(i)]) * b_stride[LongToSize(i)];
-      output_data_start += (index_in_curr_dim % output_shape_[LongToSize(i)]) * r_stride[LongToSize(i)];
+      input1_data_start += (index_in_curr_dim % input1_shape_[LongToSize(i)]) * SizeToLong(a_stride[LongToSize(i)]);
+      input2_data_start += (index_in_curr_dim % input2_shape_[LongToSize(i)]) * SizeToLong(b_stride[LongToSize(i)]);
+      output_data_start += (index_in_curr_dim % output_shape_[LongToSize(i)]) * SizeToLong(r_stride[LongToSize(i)]);
       index_in_curr_dim = index_in_curr_dim / input1_shape_[LongToSize(i)];
     }
     while (start < end) {
-      output_data_addr[output_data_start + kNumber0 * output_data_stride] =
-        static_cast<T>(input1_data_addr[input1_data_start + kNumber1 * input1_data_stride] *
-                         input2_data_addr[input2_data_start + pos * input2_data_stride] -
-                       input1_data_addr[input1_data_start + pos * input1_data_stride] *
-                         input2_data_addr[input2_data_start + kNumber1 * input2_data_stride]);
-      output_data_addr[output_data_start + kNumber1 * output_data_stride] =
-        static_cast<T>(input1_data_addr[input1_data_start + pos * input1_data_stride] *
-                         input2_data_addr[input2_data_start + kNumber0 * input2_data_stride] -
-                       input1_data_addr[input1_data_start + kNumber0 * input1_data_stride] *
-                         input2_data_addr[input2_data_start + pos * input2_data_stride]);
-      output_data_addr[output_data_start + pos * output_data_stride] =
-        static_cast<T>(input1_data_addr[input1_data_start + kNumber0 * input1_data_stride] *
-                         input2_data_addr[input2_data_start + kNumber1 * input2_data_stride] -
-                       input1_data_addr[input1_data_start + kNumber1 * input1_data_stride] *
-                         input2_data_addr[input2_data_start + kNumber0 * input2_data_stride]);
+      output_data_addr[output_data_start + SizeToLong(kNumber0 * output_data_stride)] =
+        static_cast<T>(input1_data_addr[input1_data_start + SizeToLong(kNumber1 * input1_data_stride)] *
+                         input2_data_addr[input2_data_start + SizeToLong(pos * input2_data_stride)] -
+                       input1_data_addr[input1_data_start + SizeToLong(pos * input1_data_stride)] *
+                         input2_data_addr[input2_data_start + SizeToLong(kNumber1 * input2_data_stride)]);
+      output_data_addr[output_data_start + SizeToLong(kNumber1 * output_data_stride)] =
+        static_cast<T>(input1_data_addr[input1_data_start + SizeToLong(pos * input1_data_stride)] *
+                         input2_data_addr[input2_data_start + SizeToLong(kNumber0 * input2_data_stride)] -
+                       input1_data_addr[input1_data_start + SizeToLong(kNumber0 * input1_data_stride)] *
+                         input2_data_addr[input2_data_start + SizeToLong(pos * input2_data_stride)]);
+      output_data_addr[output_data_start + SizeToLong(pos * output_data_stride)] =
+        static_cast<T>(input1_data_addr[input1_data_start + SizeToLong(kNumber0 * input1_data_stride)] *
+                         input2_data_addr[input2_data_start + SizeToLong(kNumber1 * input2_data_stride)] -
+                       input1_data_addr[input1_data_start + SizeToLong(kNumber1 * input1_data_stride)] *
+                         input2_data_addr[input2_data_start + SizeToLong(kNumber0 * input2_data_stride)]);
       start++;
       for (size_t i = 0; i < input1_data_dim; i++) {
         if (i == static_cast<size_t>(dim_)) {
           continue;
         }
         position_in_dims[i]++;
-        input1_data_start += a_stride[i];
-        input2_data_start += b_stride[i];
-        output_data_start += r_stride[i];
+        input1_data_start += SizeToLong(a_stride[i]);
+        input2_data_start += SizeToLong(b_stride[i]);
+        output_data_start += SizeToLong(r_stride[i]);
         if (position_in_dims[i] == input1_shape_[i] && i != (input1_shape_.size() - 1)) {
-          input1_data_start -= position_in_dims[i] * a_stride[i];
-          input2_data_start -= position_in_dims[i] * b_stride[i];
-          output_data_start -= position_in_dims[i] * r_stride[i];
+          input1_data_start -= position_in_dims[i] * SizeToLong(a_stride[i]);
+          input2_data_start -= position_in_dims[i] * SizeToLong(b_stride[i]);
+          output_data_start -= position_in_dims[i] * SizeToLong(r_stride[i]);
           position_in_dims[i] = 0;
         } else {
           break;
@@ -177,32 +183,20 @@ bool CrossCpuKernelMod::LaunchKernel(const std::vector<kernel::AddressPtr> &inpu
 std::vector<KernelAttr> CrossCpuKernelMod::GetOpSupport() {
   static std::vector<KernelAttr> support_list = {
     KernelAttr().AddInputAttr(kNumberTypeInt8).AddInputAttr(kNumberTypeInt8).AddOutputAttr(kNumberTypeInt8),
-
     KernelAttr().AddInputAttr(kNumberTypeInt16).AddInputAttr(kNumberTypeInt16).AddOutputAttr(kNumberTypeInt16),
-
     KernelAttr().AddInputAttr(kNumberTypeInt32).AddInputAttr(kNumberTypeInt32).AddOutputAttr(kNumberTypeInt32),
-
     KernelAttr().AddInputAttr(kNumberTypeInt64).AddInputAttr(kNumberTypeInt64).AddOutputAttr(kNumberTypeInt64),
-
     KernelAttr().AddInputAttr(kNumberTypeUInt8).AddInputAttr(kNumberTypeUInt8).AddOutputAttr(kNumberTypeUInt8),
-
     KernelAttr().AddInputAttr(kNumberTypeUInt16).AddInputAttr(kNumberTypeUInt16).AddOutputAttr(kNumberTypeUInt16),
-
     KernelAttr().AddInputAttr(kNumberTypeUInt32).AddInputAttr(kNumberTypeUInt32).AddOutputAttr(kNumberTypeUInt32),
-
     KernelAttr().AddInputAttr(kNumberTypeUInt64).AddInputAttr(kNumberTypeUInt64).AddOutputAttr(kNumberTypeUInt64),
-
     KernelAttr().AddInputAttr(kNumberTypeFloat16).AddInputAttr(kNumberTypeFloat16).AddOutputAttr(kNumberTypeFloat16),
-
     KernelAttr().AddInputAttr(kNumberTypeFloat32).AddInputAttr(kNumberTypeFloat32).AddOutputAttr(kNumberTypeFloat32),
-
     KernelAttr().AddInputAttr(kNumberTypeFloat64).AddInputAttr(kNumberTypeFloat64).AddOutputAttr(kNumberTypeFloat64),
-
     KernelAttr()
       .AddInputAttr(kNumberTypeComplex64)
       .AddInputAttr(kNumberTypeComplex64)
       .AddOutputAttr(kNumberTypeComplex64),
-
     KernelAttr()
       .AddInputAttr(kNumberTypeComplex128)
       .AddInputAttr(kNumberTypeComplex128)
