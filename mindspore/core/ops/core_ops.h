4b7333567f1560eb5868a666b160c6daaa7388ef
fyxz <980689557@qq.com>
2022-08-04 10:10:22 +0800

    [feat][assistant][I4DU1K] add new aicpu operator BroadcastTo

--- mindspore/core/ops/core_ops.h
+++ mindspore/core/ops/core_ops.h
@@ -201,6 +201,7 @@ constexpr auto kSparseTensorToCSRSparseMatrix = "SparseTensorToCSRSparseMatrix";
 constexpr auto kSparseSplit = "SparseSplit";
 constexpr auto kReverseV2 = "ReverseV2";
 constexpr auto kSparseSparseMinimum = "SparseSparseMinimum";
+constexpr auto kBroadcastTo = "BroadcastTo";
 
 // NN
 constexpr auto kApplyAddSign = "ApplyAddSign";
