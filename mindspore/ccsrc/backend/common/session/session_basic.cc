ef6f9a19f30852c93cfa9a35719cb28e965da898
zhupuxu <zhupuxu@huawei.com>
2022-08-08 11:34:26 +0800

    clean code
    Signed-off-by: zhupuxu <zhupuxu@huawei.com>

--- mindspore/ccsrc/backend/common/session/session_basic.cc
+++ mindspore/ccsrc/backend/common/session/session_basic.cc
@@ -3070,7 +3070,7 @@ void SessionBasic::DumpGraphs(const std::vector<KernelGraphPtr> &graphs) const {
 void SessionBasic::UnifyMindIR(const KernelGraphPtr &graph) { opt::CommonUnifyMindIR(graph); }
 
 #ifdef WITH_BACKEND
-void SessionBasic::InitPsWorker(const KernelGraphPtr &kernel_graph) {
+void SessionBasic::InitPsWorker(const KernelGraphPtr &kernel_graph) const {
   if (!ps::PSContext::instance()->is_worker()) {
     return;
   }
