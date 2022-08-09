33d8220d934527c3e604f276caba71e938f4d25e
liangzhibo <liangzhibo@huawei.com>
2022-08-09 08:47:39 +0800

    Enable tuple in tuple setitem

--- mindspore/python/mindspore/ops/composite/multitype_ops/setitem_impl.py
+++ mindspore/python/mindspore/ops/composite/multitype_ops/setitem_impl.py
@@ -246,6 +246,22 @@ def _dict_setitem_with_list(data, key, value):
     return F.dict_setitem(data, key, value)
 
 
+@setitem.register("Dictionary", "String", "Dictionary")
+def _dict_setitem_with_dict(data, key, value):
+    """
+    Assigns value to dictionary.
+
+    Inputs:
+        data (dict): Data of type dict.
+        key (str): Key of the data.
+        value (dict): Value given.
+
+    Outputs:
+        dict, type is as same as the element type of data.
+    """
+    return F.dict_setitem(data, key, value)
+
+
 @setitem.register("Dictionary", "String", "Tuple")
 def _dict_setitem_with_tuple(data, key, value):
     """
