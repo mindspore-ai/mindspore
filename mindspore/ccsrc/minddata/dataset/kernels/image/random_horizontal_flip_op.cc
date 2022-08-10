d60d08247e0cb0f1deae6079fcf5275114fdede1
luqilin <2831148979@qq.com>
2022-08-10 12:36:04 +0800

    [feat] [assistant] [I4S2FJ] Add new video operator RandomHorizontalFlip

--- mindspore/ccsrc/minddata/dataset/kernels/image/random_horizontal_flip_op.cc
+++ mindspore/ccsrc/minddata/dataset/kernels/image/random_horizontal_flip_op.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright 2021 Huawei Technologies Co., Ltd
+ * Copyright 2021-2022 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
  */
 #include "minddata/dataset/kernels/image/random_horizontal_flip_op.h"
 
+#include "minddata/dataset/kernels/data/data_utils.h"
 #include "minddata/dataset/kernels/image/image_utils.h"
 #include "minddata/dataset/util/status.h"
 
@@ -28,13 +29,34 @@ Status RandomHorizontalFlipOp::Compute(const TensorRow &input, TensorRow *output
   output->resize(output_count);
 
   for (const auto &image : input) {
-    RETURN_IF_NOT_OK(ValidateImageDtype("RandomHorizontalFlip", image->type()));
-    RETURN_IF_NOT_OK(ValidateImageRank("RandomHorizontalFlip", image->Rank()));
+    RETURN_IF_NOT_OK(ValidateImage(image, "RandomHorizontalFlip", {1, 2, 3, 4, 5, 6, 10, 11, 12}));
   }
 
   if (distribution_(rnd_)) {
-    for (size_t i = 0; i < input.size(); i++) {
-      RETURN_IF_NOT_OK(HorizontalFlip(input[i], &(*output)[i]));
+    for (dsize_t i = 0; i < output_count; ++i) {
+      auto input_shape = input[i]->shape();
+      dsize_t rank = input_shape.Rank();
+      if (rank <= kDefaultImageRank) {
+        // [H, W] or [H, W, C]
+        RETURN_IF_NOT_OK(HorizontalFlip(input[i], &(*output)[i]));
+      } else {
+        // reshape [..., H, W, C] to [N, H, W, C]
+        dsize_t num_batch = input[i]->Size() / (input_shape[-3] * input_shape[-2] * input_shape[-1]);
+        TensorShape new_shape({num_batch, input_shape[-3], input_shape[-2], input_shape[-1]});
+        RETURN_IF_NOT_OK(input[i]->Reshape(new_shape));
+
+        // split [N, H, W, C] to N [H, W, C], and flip N [H, W, C]
+        std::vector<std::shared_ptr<Tensor>> input_vector_hwc, output_vector_hwc;
+        RETURN_IF_NOT_OK(BatchTensorToTensorVector(input[i], &input_vector_hwc));
+        for (auto input_hwc : input_vector_hwc) {
+          std::shared_ptr<Tensor> flip;
+          RETURN_IF_NOT_OK(HorizontalFlip(input_hwc, &flip));
+          output_vector_hwc.push_back(flip);
+        }
+        // integrate N [H, W, C] to [N, H, W, C], and reshape [..., H, W, C]
+        RETURN_IF_NOT_OK(TensorVectorToBatchTensor(output_vector_hwc, &(*output)[i]));
+        RETURN_IF_NOT_OK((*output)[i]->Reshape(input_shape));
+      }
     }
     return Status::OK();
   }
