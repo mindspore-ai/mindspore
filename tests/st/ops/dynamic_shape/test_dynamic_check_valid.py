be198c69ec85b1274932d270cf64af022eb1f9a0
liqiliang <liqiliang1@huawei.com>
2022-08-15 19:19:22 +0800

    Add a NPU dynamic shape st for CheckValid.

--- tests/st/ops/dynamic_shape/test_dynamic_check_valid.py
+++ tests/st/ops/dynamic_shape/test_dynamic_check_valid.py
@@ -41,6 +41,8 @@ class CheckValidDynNet(nn.Cell):
 @pytest.mark.level0
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_ascend_training
 @pytest.mark.env_onecard
 @pytest.mark.parametrize('dtype', [np.float16])
 def test_dynamic_check_valid(dtype):
