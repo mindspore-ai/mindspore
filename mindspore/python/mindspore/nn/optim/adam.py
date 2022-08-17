326173f50af3332ebe8cda64f442cafe39d1d884
zhangyi <zhangyi267@huawei.com>
2022-08-17 16:53:26 +0800

    correct the errors on webpage0817

--- mindspore/python/mindspore/nn/optim/adam.py
+++ mindspore/python/mindspore/nn/optim/adam.py
@@ -369,7 +369,7 @@ class Adam(Optimizer):
 
         If parameters are not grouped, the `weight_decay` in optimizer will be applied on the network parameters without
         'beta' or 'gamma' in their names. Users can group parameters to change the strategy of decaying weight. When
-        parameters are grouped, each group can set `weight_decay`, if not, the `weight_decay` in optimizer will be
+        parameters are grouped, each group can set `weight_decay`. If not, the `weight_decay` in optimizer will be
         applied.
 
     Args:
