bacb1c81b0d05a2ecd05756c0655c43b420ded5d
yanghaoran <yanghaoran2@huawei.com>
2022-08-23 14:41:55 +0800

    update JSON for Modern C++ to 3.10.1

--- cmake/external_libs/json.cmake
+++ cmake/external_libs/json.cmake
@@ -3,24 +3,25 @@ if(MSVC)
     set(flatbuffers_CFLAGS "${CMAKE_CXX_FLAGS}")
     set(flatbuffers_LDFLAGS "${CMAKE_SHARED_LINKER_FLAGS}")
 else()
-    set(nlohmann_json373_CXXFLAGS "-D_FORTIFY_SOURCE=2 -O2")
-    set(nlohmann_json373_CFLAGS "-D_FORTIFY_SOURCE=2 -O2")
+    set(nlohmann_json3101_CXXFLAGS "-D_FORTIFY_SOURCE=2 -O2")
+    set(nlohmann_json3101_CFLAGS "-D_FORTIFY_SOURCE=2 -O2")
 endif()
 
 if(ENABLE_GITEE)
-    set(REQ_URL "https://gitee.com/mirrors/JSON-for-Modern-CPP/repository/archive/v3.7.3.zip")
-    set(MD5 "b758acca4f3e133bacf919e31ca302e3")
+    set(REQ_URL "https://gitee.com/mirrors/JSON-for-Modern-CPP/repository/archive/v3.10.1.zip")
+    set(MD5 "a402ee7412bd6c7fcb994946ff977c44")
     set(INCLUDE "./include")
 else()
-    set(REQ_URL "https://github.com/nlohmann/json/releases/download/v3.7.3/include.zip")
-    set(MD5 "fb96f95cdf609143e998db401ca4f324")
+
+    set(REQ_URL "https://github.com/nlohmann/json/releases/download/v3.10.1/include.zip")
+    set(MD5 "990b11a4fd9e1b05be02959a439fd402")
     set(INCLUDE "./include")
 endif()
 
-mindspore_add_pkg(nlohmann_json373
-        VER 3.7.3
+mindspore_add_pkg(nlohmann_json3101
+        VER 3.10.1
         HEAD_ONLY ${INCLUDE}
         URL ${REQ_URL}
         MD5 ${MD5})
-include_directories(${nlohmann_json373_INC})
-add_library(mindspore::json ALIAS nlohmann_json373)
+include_directories(${nlohmann_json3101_INC})
+add_library(mindspore::json ALIAS nlohmann_json3101)
