4d7715e03355b5a8d5fc0539d2a7610e16dd85a3
greatpanc <changpan_yin@163.com>
2022-08-25 10:52:23 +0800

    conv im2col refactor

---
+++ mindspore/lite/src/runtime/kernel/cpu/fp32/convolution_im2col_fp32.cc
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "src/runtime/kernel/cpu/fp32/convolution_im2col_fp32.h"
+#include "src/runtime/kernel/cpu/fp32/convolution_im2col_base_fp32.h"
+#if defined(ENABLE_AVX)
+#include "src/runtime/kernel/cpu/fp32/convolution_im2col_avx_fp32.h"
+#endif
+
+#if defined(ENABLE_SSE)
+#include "src/runtime/kernel/cpu/fp32/convolution_im2col_sse_fp32.h"
+#endif
+
+#if defined(ENABLE_ARM32)
+#include "src/runtime/kernel/cpu/fp32/convolution_im2col_arm32_fp32.h"
+#endif
+
+#if defined(ENABLE_ARM64)
+#include "src/runtime/kernel/cpu/fp32/convolution_im2col_arm64_fp32.h"
+#endif
+
+namespace mindspore::kernel {
+LiteKernel *CreateConvolutionIm2ColCPUKernel(OpParameter *parameter, const std::vector<lite::Tensor *> &inputs,
+                                             const std::vector<lite::Tensor *> &outputs, const lite::InnerContext *ctx,
+                                             float *origin_weight, float *origin_bias) {
+  LiteKernel *kernel = nullptr;
+#if defined(ENABLE_AVX)
+  if (kernel == nullptr) {
+    kernel = new (std::nothrow)
+      kernel::ConvolutionIm2ColAVXCPUKernel(parameter, inputs, outputs, ctx, origin_weight, origin_bias);
+  }
+#endif
+
+#if defined(ENABLE_SSE)
+  if (kernel == nullptr) {
+    kernel = new (std::nothrow)
+      kernel::ConvolutionIm2ColSSECPUKernel(parameter, inputs, outputs, ctx, origin_weight, origin_bias);
+  }
+#endif
+
+#if defined(ENABLE_ARM64)
+  kernel = new (std::nothrow)
+    kernel::ConvolutionIm2ColARM64CPUKernel(parameter, inputs, outputs, ctx, origin_weight, origin_bias);
+#elif defined(ENABLE_ARM32)
+  kernel = new (std::nothrow)
+    kernel::ConvolutionIm2ColARM32CPUKernel(parameter, inputs, outputs, ctx, origin_weight, origin_bias);
+#endif
+
+  if (kernel == nullptr) {
+    kernel = new (std::nothrow)
+      kernel::ConvolutionIm2ColBaseCPUKernel(parameter, inputs, outputs, ctx, origin_weight, origin_bias);
+  }
+  return kernel;
+}
+}  // namespace mindspore::kernel
