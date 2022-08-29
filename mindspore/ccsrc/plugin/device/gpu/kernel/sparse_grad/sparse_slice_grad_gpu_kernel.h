dd9e9458edd2c360a49db1cfe4718b4954199c17
chuht <chuhaotian2@huawei.com>
2022-08-29 12:29:59 +0800

    add sparce_slice_grad gpu kernel
    clean lint
    
    clean lint
    
    clean lint
    
    clean lint
    
    clean lint
    
    clean lint

---
+++ mindspore/ccsrc/plugin/device/gpu/kernel/sparse_grad/sparse_slice_grad_gpu_kernel.h
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MINDSPORE_CCSRC_PLUGIN_DEVICE_GPU_KERNEL_SPARSE_GRAD_SPARSE_SLICE_GRAD_GPU_KERNEL_H_
+#define MINDSPORE_CCSRC_PLUGIN_DEVICE_GPU_KERNEL_SPARSE_GRAD_SPARSE_SLICE_GRAD_GPU_KERNEL_H_
+
+#include <vector>
+#include <utility>
+#include <string>
+#include <memory>
+#include <map>
+#include <algorithm>
+#include <functional>
+#include "plugin/device/gpu/kernel/gpu_kernel.h"
+#include "plugin/device/gpu/kernel/gpu_kernel_factory.h"
+#include "mindspore/core/ops/grad/sparse_slice_grad.h"
+#include "plugin/device/gpu/kernel/cuda_impl/cuda_ops/cuda_common.h"
+#include "plugin/device/gpu/kernel/cuda_impl/cuda_class/cuda_class_common.h"
+#include "plugin/device/gpu/kernel/cuda_impl/cuda_ops/sparse_slice_grad_impl.cuh"
+namespace mindspore {
+namespace kernel {
+class SparseSliceGradGpuKernelMod : public NativeGpuKernelMod {
+ public:
+  SparseSliceGradGpuKernelMod() = default;
+  ~SparseSliceGradGpuKernelMod() override = default;
+
+  bool Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+            const std::vector<KernelTensorPtr> &outputs) override;
+
+  int Resize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+             const std::vector<KernelTensorPtr> &outputs,
+             const std::map<uint32_t, tensor::TensorPtr> &inputsOnHost) override;
+
+  bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
+              const std::vector<AddressPtr> &outputs, void *stream_ptr) override {
+    return kernel_func_(this, inputs, workspace, outputs, stream_ptr);
+  }
+
+ protected:
+  std::vector<KernelAttr> GetOpSupport() override;
+
+ private:
+  template <typename DataType, typename IndexType>
+  bool LaunchKernel(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
+                    const std::vector<AddressPtr> &outputs, void *stream_ptr);
+
+  using SparseSliceGradLaunchFunc =
+    std::function<bool(SparseSliceGradGpuKernelMod *, const std::vector<kernel::AddressPtr> &,
+                       const std::vector<kernel::AddressPtr> &, const std::vector<kernel::AddressPtr> &, void *)>;
+  static std::vector<std::pair<KernelAttr, SparseSliceGradLaunchFunc>> func_list_;
+  SparseSliceGradLaunchFunc kernel_func_;
+  size_t input_nnz_{1};
+  size_t output_nnz_{1};
+  size_t num_dim_{1};
+};
+}  // namespace kernel
+}  // namespace mindspore
+
+#endif  // MINDSPORE_CCSRC_PLUGIN_DEVICE_GPU_KERNEL_SPARSE_GRAD_SPARSE_SLICE_GRAD_GPU_KERNEL_H_
---
+++ tests/st/ops/gpu/test_sparse_slice_grad_op.py
@@ -0,0 +1,52 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""smoke tests for SparseSliceGrad"""
+
+import numpy as np
+import pytest
+from mindspore import Tensor, context
+from mindspore.ops.operations._grad_ops import SparseSliceGrad
+
+
+def generate_data(input_type="float32"):
+    """
+    generate data for sparse slice grad op test cases.
+    """
+    backprop_val_grad = Tensor(np.array([4, 2, 3]).astype(input_type))
+    indices = Tensor(np.array([[0, 0], [0, 2], [1, 2], [1, 3], [2, 3], [2, 4]]).astype(np.int64))
+    start = Tensor(np.array([0, 0]).astype(np.int64))
+    new_indices = Tensor(np.array([[1, 2], [1, 3], [2, 3]]).astype(np.int64))
+    data = backprop_val_grad, indices, start, new_indices
+    return data
+
+
+@pytest.mark.level0
+@pytest.mark.parametrize('input_type', ["int8", "uint8", "int16", "uint16",
+                                        "int32", "int64", "float16", "float32", "float64"])
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.env_onecard
+def test_sparse_tensor_dense_add(input_type):
+    """
+    Feature: Test sparse slice grad ops.
+    Description: Test 2D sparse slice grad ops.
+    Expectation: Success.
+    """
+    context.set_context(mode=context.GRAPH_MODE)
+    data = generate_data(input_type=input_type)
+    net = SparseSliceGrad()
+    out = net(data[0], data[1], data[2], data[3]).asnumpy()
+    expected = np.array([0, 0, 4, 2, 3, 0]).astype(input_type)
+    eps = 1e-6*np.array(np.ones_like(out))
+    assert np.all(expected - out < eps)
