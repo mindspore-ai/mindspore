2c51f141d6c40b8773269694aa55aa8230a92e97
bubtltk <2545940674@qq.com>
2022-08-30 23:27:13 +0800

    clean code 1.8

--- mindspore/ccsrc/plugin/device/cpu/kernel/grid_sampler_2d_grad_cpu_kernel.h
+++ mindspore/ccsrc/plugin/device/cpu/kernel/grid_sampler_2d_grad_cpu_kernel.h
@@ -190,7 +190,7 @@ struct Vec256 {
     int_same_size_t<T> buffer[size()];
     mask.store(buffer);
     for (int64_t i = 0; i < size(); i++) {
-      if (buffer[i] & static_cast<int_same_size_t<T>>(0x01)) {
+      if (buffer[i] & 0x01) {
         vec[i] = b[i];
       } else {
         vec[i] = a[i];
@@ -324,7 +324,7 @@ typename RETURN_TYPE inline MaskGather(const Vec256<T> &src, T const *base_addr,
   vindex.store(static_cast<void *>(index_arr));
   T buffer[kSize];
   for (int64_t i = 0; i < kSize; i++) {
-    if (mask_arr[i] & static_cast<int_same_size_t<T>>(0x01)) {
+    if (mask_arr[i] & 0x01) {
       buffer[i] = base_addr[static_cast<size_t>(index_arr[i] * static_cast<size_t>(scale) / sizeof(T))];
     } else {
       buffer[i] = src_arr[i];
@@ -685,7 +685,7 @@ template <typename T>
 static inline void MaskScatterAdd(const T *src, T *base_addr, const vec256::int_same_size_t<T> *offsets,
                                   const vec256::int_same_size_t<T> *mask, int64_t len) {
   for (int64_t i = 0; i < len; i++) {
-    if (mask[i] & static_cast<vec256::int_same_size_t<T>>(0x01)) {
+    if (mask[i] & 0x01) {
       base_addr[offsets[i]] += src[i];
     }
   }
