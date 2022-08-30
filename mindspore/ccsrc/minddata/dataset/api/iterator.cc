465ad87bb8ffbcfef78cd35062b7a9fb11f53a53
qiuzhongya <qiuzhongya@huawei.com>
2022-08-30 11:44:21 +0800

    support for windows msvc

--- mindspore/ccsrc/minddata/dataset/api/iterator.cc
+++ mindspore/ccsrc/minddata/dataset/api/iterator.cc
@@ -91,6 +91,10 @@ Status Iterator::BuildAndLaunchTree(const std::shared_ptr<Dataset> &ds, int32_t 
 
 PullIterator::PullIterator() : pull_consumer_(nullptr) {}
 
+#ifdef _MSC_VER
+PullIterator::~PullIterator() = default;
+#endif
+
 // Get the next row from the data pipeline.
 Status PullIterator::GetRows(int32_t num_rows, std::vector<MSTensorVec> *const row) {
   RETURN_UNEXPECTED_IF_NULL(row);
