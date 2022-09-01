1f4495077e84153563b232141c1653373e3c5f7e
huanghui <huanghui44@huawei.com>
2022-09-01 15:02:25 +0800

    print compile remind message when start and end

---
+++ mindspore/ccsrc/pipeline/jit/event_message_print.cc
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "pipeline/jit/event_message_print.h"
+#include "utils/log_adapter.h"
+#include "pipeline/jit/pipeline.h"
+
+namespace mindspore {
+namespace pipeline {
+void EventMessage::PrintCompileStartMsg(const std::string &phase, const std::string &obj_desc) {
+  if (IsPhaseLoadFromMindIR(phase)) {
+    return;
+  }
+  PrintEventMessage("Start compiling " + obj_desc + " and it will take a while. Please wait...");
+}
+
+void EventMessage::PrintCompileEndMsg(const std::string &phase, const std::string &obj_desc) {
+  if (IsPhaseLoadFromMindIR(phase)) {
+    return;
+  }
+  PrintEventMessage("End compiling " + obj_desc + ".");
+}
+
+void EventMessage::PrintEventMessage(const std::string &message) {
+  std::cout << "[EVENT] (PID" << getpid() << ") " << GetTimeString() << " " << message << std::endl;
+}
+}  // namespace pipeline
+}  // namespace mindspore
