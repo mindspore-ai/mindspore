3e3d899005f8737d56f5c883ded0096e2a1a1250
zhangzhaoju <zhangzhaoju@huawei.com>
2022-09-13 17:21:59 +0800

    Support Dynamic shape/rank for mirrorpad && maxpool

--- mindspore/core/ops/mirror_pad.cc
+++ mindspore/core/ops/mirror_pad.cc
@@ -27,95 +27,97 @@ namespace ops {
 namespace {
 constexpr int64_t kPaddingsSecondDimSize = 2;
 constexpr int64_t MAX_PADDINGS = 5;
-abstract::ShapePtr MirrorPadInferShape(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) {
-  MS_EXCEPTION_IF_NULL(primitive);
-  auto prim_name = primitive->name();
-  auto paddings = input_args[1]->BuildValue();
-  MS_EXCEPTION_IF_NULL(paddings);
-  auto paddings_arg = CheckAndConvertUtils::CheckTensorIntValue("paddings", paddings, prim_name);
-  std::vector<std::pair<int64_t, int64_t>> paddings_attr;
-  auto x_shape = CheckAndConvertUtils::ConvertShapePtrToShapeMap(input_args[0]->BuildShape())[kShape];
-  // ToSupport Dynamic rank
-  if (IsDynamicRank(x_shape)) {
-    return std::make_shared<abstract::Shape>(std::vector<int64_t>{-2});
-  }
-
-  auto paddings_shape = CheckAndConvertUtils::ConvertShapePtrToShapeMap(input_args[1]->BuildShape())[kShape];
-  auto mode = GetValue<std::string>(primitive->GetAttr("mode"));
-  if (paddings_shape.size() != kPaddingsSecondDimSize) {
-    MS_EXCEPTION(ValueError) << "For '" << prim_name << "', paddings must be equal to 2 dims, but got "
-                             << paddings_shape.size();
-  }
-  if (paddings_shape[1] != kPaddingsSecondDimSize) {
-    MS_EXCEPTION(ValueError) << "For '" << prim_name << "', paddings must be a matrix with 2 columns, but got "
-                             << paddings_shape[1];
-  }
-  if (static_cast<size_t>(paddings_shape[0]) != x_shape.size()) {
-    MS_EXCEPTION(ValueError) << "For '" << prim_name << "', paddings.shape[0] must equal to input's rank, but got "
-                             << paddings_shape[0];
-  }
-  for (size_t i = 0; i < paddings_arg.size(); i = i + kPaddingsSecondDimSize) {
-    paddings_attr.push_back(std::make_pair(paddings_arg[i], paddings_arg[i + 1]));
-  }
-  (void)CheckAndConvertUtils::CheckInteger("paddings_size", paddings_attr.size(), kEqual, x_shape.size(), prim_name);
-  auto input_x_shape_ptr = input_args[0]->BuildShape();
-  MS_EXCEPTION_IF_NULL(input_x_shape_ptr);
-  if (input_x_shape_ptr->IsDynamic()) {
-    return input_args[0]->BuildShape()->cast<abstract::ShapePtr>();
-  }
-  int64_t size = x_shape.size();
-  if (size < 0 || size > MAX_PADDINGS) {
-    MS_EXCEPTION(ValueError) << "For '" << prim_name
-                             << "', the dimension of input only supports less than or equal to 5 dims, but got " << size
-                             << " dims";
-  }
-  for (int64_t i = 0; i < size; i++) {
-    if (paddings_attr[i].first < 0 || paddings_attr[i].second < 0) {
-      MS_EXCEPTION(ValueError) << "For '" << prim_name << "', all elements of paddings must be >= 0.";
-    }
-    if (mode == "SYMMETRIC") {
-      if (paddings_attr[i].first > x_shape[i] || paddings_attr[i].second > x_shape[i])
-        MS_EXCEPTION(ValueError) << "For '" << prim_name
-                                 << "', paddings must be no greater "
-                                    "than the dimension size: ["
-                                 << paddings_attr[i].first << "], [" << paddings_attr[i].second << "] greater than ["
-                                 << x_shape[i] << "]";
-    } else if (mode == "REFLECT") {
-      if (paddings_attr[i].first >= x_shape[i] || paddings_attr[i].second >= x_shape[i])
-        MS_EXCEPTION(ValueError) << "For '" << prim_name
-                                 << "', paddings must be no greater "
-                                    "than the dimension size: ["
-                                 << paddings_attr[i].first << "], [" << paddings_attr[i].second << "] not less than ["
-                                 << x_shape[i] << "]";
-    }
-  }
-  std::vector<int64_t> out_shape;
-  for (size_t i = 0; i < x_shape.size(); i++) {
-    (void)out_shape.emplace_back(x_shape[i] + paddings_attr[i].first + paddings_attr[i].second);
-  }
-  return std::make_shared<abstract::Shape>(out_shape);
-}
-
-TypePtr MirrorPadInferType(const PrimitivePtr &prim, const std::vector<AbstractBasePtr> &input_args) {
-  for (const auto &item : input_args) {
-    MS_EXCEPTION_IF_NULL(item);
-  }
-  (void)CheckAndConvertUtils::CheckTensorTypeValid("paddings", input_args[1]->BuildType(), {kInt32, kInt64},
-                                                   prim->name());
-  return CheckAndConvertUtils::CheckTensorTypeValid(
-    "input_x", input_args[0]->BuildType(),
-    {kInt8, kInt16, kInt32, kInt64, kUInt8, kUInt16, kFloat16, kFloat32, kFloat64, kComplex64, kComplex128, kBool},
-    prim->name());
-}
 }  // namespace
 
+void MirrorPad::set_mode(const std::string &mode) { (void)AddAttr(kMode, api::MakeValue(mode)); }
+std::string MirrorPad::get_mode() const { return GetValue<std::string>(GetAttr(kMode)); }
+
 MIND_API_OPERATOR_IMPL(MirrorPad, BaseOperator);
-AbstractBasePtr MirrorPadInfer(const abstract::AnalysisEnginePtr &, const PrimitivePtr &primitive,
-                               const std::vector<AbstractBasePtr> &input_args) {
-  auto infer_type = MirrorPadInferType(primitive, input_args);
-  auto infer_shape = MirrorPadInferShape(primitive, input_args);
-  return abstract::MakeAbstract(infer_shape, infer_type);
-}
-REGISTER_PRIMITIVE_EVAL_IMPL(MirrorPad, prim::kPrimMirrorPad, MirrorPadInfer, nullptr, true);
+class MirrorPadInfer : public abstract::OpInferBase {
+ public:
+  BaseShapePtr InferShape(const PrimitivePtr &primitive,
+                          const std::vector<AbstractBasePtr> &input_args) const override {
+    MS_EXCEPTION_IF_NULL(primitive);
+    auto prim_name = primitive->name();
+    auto paddings = input_args[1]->BuildValue();
+    MS_EXCEPTION_IF_NULL(paddings);
+    auto paddings_arg = CheckAndConvertUtils::CheckTensorIntValue(kPaddings, paddings, prim_name);
+    std::vector<std::pair<int64_t, int64_t>> paddings_attr;
+    auto x_shape = CheckAndConvertUtils::ConvertShapePtrToShapeMap(input_args[0]->BuildShape())[kShape];
+    // ToSupport Dynamic rank
+    if (IsDynamicRank(x_shape)) {
+      return std::make_shared<abstract::Shape>(std::vector<int64_t>{-2});
+    }
+
+    auto paddings_shape = CheckAndConvertUtils::ConvertShapePtrToShapeMap(input_args[1]->BuildShape())[kShape];
+    auto mode = GetValue<std::string>(primitive->GetAttr(kMode));
+    if (paddings_shape.size() != kPaddingsSecondDimSize) {
+      MS_EXCEPTION(ValueError) << "For '" << prim_name << "', paddings must be equal to 2 dims, but got "
+                               << paddings_shape.size();
+    }
+    if (paddings_shape[1] != kPaddingsSecondDimSize) {
+      MS_EXCEPTION(ValueError) << "For '" << prim_name << "', paddings must be a matrix with 2 columns, but got "
+                               << paddings_shape[1];
+    }
+    if (static_cast<size_t>(paddings_shape[0]) != x_shape.size()) {
+      MS_EXCEPTION(ValueError) << "For '" << prim_name << "', paddings.shape[0] must equal to input's rank, but got "
+                               << paddings_shape[0];
+    }
+    for (size_t i = 0; i < paddings_arg.size(); i = i + kPaddingsSecondDimSize) {
+      paddings_attr.push_back(std::make_pair(paddings_arg[i], paddings_arg[i + 1]));
+    }
+    (void)CheckAndConvertUtils::CheckInteger(kPaddingsSize, paddings_attr.size(), kEqual, x_shape.size(), prim_name);
+    auto input_x_shape_ptr = input_args[0]->BuildShape();
+    MS_EXCEPTION_IF_NULL(input_x_shape_ptr);
+    if (input_x_shape_ptr->IsDynamic()) {
+      return input_args[0]->BuildShape()->cast<abstract::ShapePtr>();
+    }
+    int64_t size = x_shape.size();
+    if (size < 0 || size > MAX_PADDINGS) {
+      MS_EXCEPTION(ValueError) << "For '" << prim_name
+                               << "', the dimension of input only supports less than or equal to 5 dims, but got "
+                               << size << " dims";
+    }
+    for (int64_t i = 0; i < size; i++) {
+      if (paddings_attr[i].first < 0 || paddings_attr[i].second < 0) {
+        MS_EXCEPTION(ValueError) << "For '" << prim_name << "', all elements of paddings must be >= 0.";
+      }
+      if (mode == "SYMMETRIC") {
+        if (paddings_attr[i].first > x_shape[i] || paddings_attr[i].second > x_shape[i])
+          MS_EXCEPTION(ValueError) << "For '" << prim_name
+                                   << "', paddings must be no greater "
+                                      "than the dimension size: ["
+                                   << paddings_attr[i].first << "], [" << paddings_attr[i].second << "] greater than ["
+                                   << x_shape[i] << "]";
+      } else if (mode == "REFLECT") {
+        if (paddings_attr[i].first >= x_shape[i] || paddings_attr[i].second >= x_shape[i])
+          MS_EXCEPTION(ValueError) << "For '" << prim_name
+                                   << "', paddings must be no greater "
+                                      "than the dimension size: ["
+                                   << paddings_attr[i].first << "], [" << paddings_attr[i].second << "] not less than ["
+                                   << x_shape[i] << "]";
+      }
+    }
+    std::vector<int64_t> out_shape;
+    for (size_t i = 0; i < x_shape.size(); i++) {
+      (void)out_shape.emplace_back(x_shape[i] + paddings_attr[i].first + paddings_attr[i].second);
+    }
+    return std::make_shared<abstract::Shape>(out_shape);
+  }
+
+  TypePtr InferType(const PrimitivePtr &prim, const std::vector<AbstractBasePtr> &input_args) const override {
+    for (const auto &item : input_args) {
+      MS_EXCEPTION_IF_NULL(item);
+    }
+    (void)CheckAndConvertUtils::CheckTensorTypeValid("paddings", input_args[1]->BuildType(), {kInt32, kInt64},
+                                                     prim->name());
+    return CheckAndConvertUtils::CheckTensorTypeValid(
+      "input_x", input_args[0]->BuildType(),
+      {kInt8, kInt16, kInt32, kInt64, kUInt8, kUInt16, kFloat16, kFloat32, kFloat64, kComplex64, kComplex128, kBool},
+      prim->name());
+  }
+};
+
+REGISTER_PRIMITIVE_OP_INFER_IMPL(MirrorPad, prim::kPrimMirrorPad, MirrorPadInfer, false);
 }  // namespace ops
 }  // namespace mindspore
