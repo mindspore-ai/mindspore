42e0b87c7ed96839d81fc83487245702f5d0197c
fanjibin <fanjibin@huawei.com>
2022-09-17 10:25:47 +0800

    print support complex type

--- tests/st/ops/cpu/test_print_op.py
+++ tests/st/ops/cpu/test_print_op.py
@@ -42,8 +42,8 @@ class PrintFunc(nn.Cell):
 @pytest.mark.env_onecard
 @pytest.mark.platform_x86_cpu
 @pytest.mark.parametrize('mode', [context.GRAPH_MODE, context.PYNATIVE_MODE])
-@pytest.mark.parametrize("dtype", [np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16,
-                                   np.uint32, np.uint64, np.bool, np.float64, np.float32, np.float16])
+@pytest.mark.parametrize("dtype", [np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16, np.uint32, np.uint64,
+                                   np.bool, np.float64, np.float32, np.float16, np.complex64, np.complex128])
 def test_print_op_dtype(mode, dtype):
     """
     Feature: cpu Print ops.
