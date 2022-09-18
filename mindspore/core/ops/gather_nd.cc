6a28db2cb77f1c6eb29c30c23187106024f20d63
Zichun Ye <zichun.ye@huawei.com>
2022-09-18 21:53:33 +0800

    support dyn rank for  gather_nd/scatter_nd

--- mindspore/core/ops/gather_nd.cc
+++ mindspore/core/ops/gather_nd.cc
@@ -40,7 +40,7 @@ abstract::ShapePtr GatherNdInferShape(const PrimitivePtr &primitive, const std::
   for (size_t i = 0; i < indices_rank - 1; i++) {
     output_shape.push_back(indices_shape[i]);
   }
-  if (indices_rank - 1 >= 0 && indices_shape[indices_rank - 1] == UNKNOWN_DIM) {
+  if (indices_rank - 1 >= 0 && indices_shape[indices_rank - 1] < 0) {
     return std::make_shared<abstract::Shape>(std::vector<int64_t>{UNKNOWN_RANK});
   }
   for (size_t i = LongToSize(indices_shape[indices_rank - 1]); i < input_rank; ++i) {
