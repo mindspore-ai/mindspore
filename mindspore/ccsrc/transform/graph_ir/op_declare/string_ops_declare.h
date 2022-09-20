b6e5ed9d767c3897a3700eef1520e18f76cdb7d3
wujueying <wujueying1@huawei.com>
2022-09-20 20:51:54 +0800

    告警清理

--- mindspore/ccsrc/transform/graph_ir/op_declare/string_ops_declare.h
+++ mindspore/ccsrc/transform/graph_ir/op_declare/string_ops_declare.h
@@ -17,8 +17,6 @@
 #ifndef MINDSPORE_CCSRC_TRANSFORM_GRAPH_IR_OP_DECLARE_STRING_OPS_DECLARE_H_
 #define MINDSPORE_CCSRC_TRANSFORM_GRAPH_IR_OP_DECLARE_STRING_OPS_DECLARE_H_
 
-#include <string>
-#include <unordered_map>
 #include "transform/graph_ir/op_declare/op_declare_macro.h"
 #include "ops/string_ops.h"
 
