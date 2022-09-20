ff4ee30658e3e79b1f19f9fc86c5b5bd5d4c6dcb
luojianing <luojianing1@huawei.com>
2022-09-20 17:41:37 +0800

    fix params issues

--- mindspore/python/mindspore/ops/function/math_func.py
+++ mindspore/python/mindspore/ops/function/math_func.py
@@ -4788,10 +4788,10 @@ def renorm(input_x, p, dim, maxnorm):
     divided by the p-norm of the substensor and then multiplied by `maxnorm`.
 
     Args:
-        input_x: A Tensor, types: float32 or float16.
+        input_x (Tensor): A Tensor, types: float32 or float16.
         p (int): Power of norm calculation.
         dim (int): The dimension that expected to get the slice-tensor.
-        maxnorm (float): Max norm.
+        maxnorm (float32): Max norm.
 
     Returns:
         Tensor, has the same dtype and shape as input_x.
