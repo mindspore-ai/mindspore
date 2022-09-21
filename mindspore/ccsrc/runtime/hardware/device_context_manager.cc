36a61516d98bcc2d47bb22aaaf2eca817673a21e
zhoufeng <zhoufeng54@huawei.com>
2022-09-21 09:57:46 +0800

    optimize log of dlopen-ing plugins
    Signed-off-by: zhoufeng <zhoufeng54@huawei.com>

--- mindspore/ccsrc/runtime/hardware/device_context_manager.cc
+++ mindspore/ccsrc/runtime/hardware/device_context_manager.cc
@@ -43,7 +43,7 @@ void PluginLoader::LoadDynamicLib(const std::string &plugin_file, std::map<std::
   err_msg = GetDlErrorMsg();
 #endif
   if (handle == nullptr) {
-    MS_LOG(INFO) << "Load dynamic lib: " << so_name << " failed. " << err_msg;
+    MS_LOG(ERROR) << "Load dynamic library " << so_name << " failed, returns [" << err_msg << "].";
     return;
   }
   (*all_handles)[so_name] = handle;
@@ -143,11 +143,13 @@ void DeviceContextManager::LoadPlugin() {
   }
   if (plugin_path_.empty() && !plugin_loader::PluginLoader::GetPluginPath(&plugin_path_)) {
     MS_LOG(INFO) << "Plugin path is invalid, skip!";
+    load_init_ = true;
     return;
   }
   DIR *dir = opendir(plugin_path_.c_str());
   if (dir == nullptr) {
     MS_LOG(ERROR) << "Open plugin dir failed, plugin path:" << plugin_path_;
+    load_init_ = true;
     return;
   }
   struct dirent *entry;
