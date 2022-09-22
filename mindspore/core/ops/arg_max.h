5f6135f29078eb9a81f21d48b55269041447fde5
ClementineZTW <383297671@qq.com>
2022-09-22 11:05:05 +0800

    [feat][assistant][I4CRJC/I4CRJB] aicpu add new operator argmax/argmin

--- mindspore/core/ops/arg_max.h
+++ mindspore/core/ops/arg_max.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Huawei Technologies Co., Ltd
+ * Copyright 2022 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,6 +53,7 @@ class MIND_API Argmax : public BaseOperator {
 };
 abstract::AbstractBasePtr ArgMaxInfer(const abstract::AnalysisEnginePtr &, const PrimitivePtr &primitive,
                                       const std::vector<abstract::AbstractBasePtr> &input_args);
+using PrimArgMax = std::shared_ptr<Argmax>;
 }  // namespace ops
 }  // namespace mindspore
 
--- mindspore/core/ops/arg_min.h
+++ mindspore/core/ops/arg_min.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Huawei Technologies Co., Ltd
+ * Copyright 2022 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,6 +53,7 @@ class MIND_API ArgMin : public BaseOperator {
 };
 abstract::AbstractBasePtr ArgMinInfer(const abstract::AnalysisEnginePtr &, const PrimitivePtr &primitive,
                                       const std::vector<abstract::AbstractBasePtr> &input_args);
+using PrimArgMin = std::shared_ptr<ArgMin>;
 }  // namespace ops
 }  // namespace mindspore
 
