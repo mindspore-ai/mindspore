3ae897e253e47042a29b6a01cbedda11e3acb8e7
ligan <ligan15@huawei.com>
2022-09-27 17:21:54 +0800

    NotEqual

--- mindspore/core/ops/not_equal.cc
+++ mindspore/core/ops/not_equal.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020-2021 Huawei Technologies Co., Ltd
+ * Copyright 2020-2022 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@
 #include <vector>
 #include <algorithm>
 #include <memory>
+#include <complex>
 #include "ops/op_utils.h"
 #include "utils/check_convert_utils.h"
 #include "abstract/ops/primitive_infer_map.h"
@@ -28,6 +29,19 @@
 namespace mindspore {
 namespace ops {
 namespace {
+template <typename T>
+void NotEqualImpl(void *x1, void *x2, void *result, size_t size) {
+  MS_EXCEPTION_IF_NULL(x1);
+  MS_EXCEPTION_IF_NULL(x2);
+  MS_EXCEPTION_IF_NULL(result);
+  T *x1_data = static_cast<T *>(x1);
+  T *x2_data = static_cast<T *>(x2);
+  auto result_data = static_cast<bool *>(result);
+  for (size_t i = 0; i < size; ++i) {
+    result_data[i] = x1_data[i] != x2_data[i];
+  }
+}
+
 abstract::ShapePtr NotEqualInferShape(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) {
   MS_EXCEPTION_IF_NULL(primitive);
   auto prim_name = primitive->name();
@@ -44,6 +58,88 @@ TypePtr NotEqualInferType(const PrimitivePtr &prim, const std::vector<AbstractBa
   (void)CheckAndConvertUtils::CheckTensorTypeSame(types, valid_types, op_name);
   return std::make_shared<TensorType>(kBool);
 }
+
+ValuePtr NotEqualInferValue(const PrimitivePtr &prim, const std::vector<AbstractBasePtr> &input_args) {
+  auto result_type = NotEqualInferType(prim, input_args);
+  auto result_shape = NotEqualInferShape(prim, input_args)->cast<abstract::ShapePtr>();
+  constexpr size_t input_size = 2;
+  if (input_args.size() != input_size) {
+    MS_LOG(ERROR) << "input_args.size is not equal to 2";
+  }
+  auto x1 = input_args[0]->BuildValue();
+  auto x2 = input_args[1]->BuildValue();
+  if (x1 == nullptr || x2 == nullptr) {
+    return nullptr;
+  }
+  auto x1_tensor = x1->cast<tensor::TensorPtr>();
+  auto x2_tensor = x2->cast<tensor::TensorPtr>();
+  MS_EXCEPTION_IF_NULL(x1_tensor);
+  MS_EXCEPTION_IF_NULL(x2_tensor);
+  auto type_id = x1_tensor->data_type();
+  auto data_size = x1_tensor->DataSize();
+  auto result_tensor = std::make_shared<tensor::Tensor>(kNumberTypeBool, result_shape->shape());
+  switch (type_id) {
+    case kNumberTypeBool: {
+      NotEqualImpl<bool>(x1_tensor->data_c(), x2_tensor->data_c(), result_tensor->data_c(), data_size);
+      break;
+    }
+    case kNumberTypeInt: {
+      NotEqualImpl<int>(x1_tensor->data_c(), x2_tensor->data_c(), result_tensor->data_c(), data_size);
+      break;
+    }
+    case kNumberTypeInt8: {
+      NotEqualImpl<int8_t>(x1_tensor->data_c(), x2_tensor->data_c(), result_tensor->data_c(), data_size);
+      break;
+    }
+    case kNumberTypeInt16: {
+      NotEqualImpl<int16_t>(x1_tensor->data_c(), x2_tensor->data_c(), result_tensor->data_c(), data_size);
+      break;
+    }
+    case kNumberTypeInt32: {
+      NotEqualImpl<int32_t>(x1_tensor->data_c(), x2_tensor->data_c(), result_tensor->data_c(), data_size);
+      break;
+    }
+    case kNumberTypeInt64: {
+      NotEqualImpl<int64_t>(x1_tensor->data_c(), x2_tensor->data_c(), result_tensor->data_c(), data_size);
+      break;
+    }
+    case kNumberTypeUInt8: {
+      NotEqualImpl<uint8_t>(x1_tensor->data_c(), x2_tensor->data_c(), result_tensor->data_c(), data_size);
+      break;
+    }
+    case kNumberTypeFloat: {
+      NotEqualImpl<float>(x1_tensor->data_c(), x2_tensor->data_c(), result_tensor->data_c(), data_size);
+      break;
+    }
+    case kNumberTypeFloat16: {
+      NotEqualImpl<float16>(x1_tensor->data_c(), x2_tensor->data_c(), result_tensor->data_c(), data_size);
+      break;
+    }
+    case kNumberTypeFloat32: {
+      NotEqualImpl<float>(x1_tensor->data_c(), x2_tensor->data_c(), result_tensor->data_c(), data_size);
+      break;
+    }
+    case kNumberTypeFloat64: {
+      NotEqualImpl<double>(x1_tensor->data_c(), x2_tensor->data_c(), result_tensor->data_c(), data_size);
+      break;
+    }
+    case kNumberTypeComplex64: {
+      NotEqualImpl<std::complex<float>>(x1_tensor->data_c(), x2_tensor->data_c(), result_tensor->data_c(), data_size);
+      break;
+    }
+    case kNumberTypeComplex128: {
+      NotEqualImpl<std::complex<double>>(x1_tensor->data_c(), x2_tensor->data_c(), result_tensor->data_c(), data_size);
+      break;
+    }
+    default: {
+      MS_EXCEPTION(TypeError) << "For '" << prim->name()
+                              << "', the supported type is in the list: ['bool', 'int8', 'int16', 'int32', 'int64', "
+                                 "'complex64', 'complex128', 'uint8', 'float16', 'float32', 'float64'], but got "
+                              << result_type->ToString() << ".";
+    }
+  }
+  return result_tensor;
+}
 }  // namespace
 
 MIND_API_OPERATOR_IMPL(NotEqual, BaseOperator);
@@ -56,6 +152,6 @@ AbstractBasePtr NotEqualInfer(const abstract::AnalysisEnginePtr &, const Primiti
   auto infer_shape = NotEqualInferShape(primitive, input_args);
   return abstract::MakeAbstract(infer_shape, infer_type);
 }
-REGISTER_PRIMITIVE_EVAL_IMPL(NotEqual, prim::kPrimNotEqual, NotEqualInfer, nullptr, true);
+REGISTER_PRIMITIVE_EVAL_IMPL(NotEqual, prim::kPrimNotEqual, NotEqualInfer, NotEqualInferValue, true);
 }  // namespace ops
 }  // namespace mindspore
