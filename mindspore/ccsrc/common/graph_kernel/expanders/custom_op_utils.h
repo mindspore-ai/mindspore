3b87ff1bb23b4279bc0b7d9c01ceec7e4edc03f2
zengzitao <zengzitao@huawei.com>
2022-09-29 10:29:21 +0800

    lu ascend adapt

--- mindspore/ccsrc/common/graph_kernel/expanders/custom_op_utils.h
+++ mindspore/ccsrc/common/graph_kernel/expanders/custom_op_utils.h
@@ -24,6 +24,7 @@ constexpr int64_t kBlock = 16;
 
 const char kFuncType[] = "hybrid";
 const char kTrsmName[] = "trsm";
+const char kLUName[] = "lu_decomp";
 
 const std::map<std::string, std::string> kTrsmFuncStrMap = {
   {"trsmL_N_D",
@@ -54,13 +55,55 @@ const std::map<std::string, std::string> kTrsmFuncStrMap = {
    "                for k in vectorize(16):\n"
    "                    inverse_0[i, l * 16 + k] = a[i, j] * b[j, l * 16 + k]\n"
    "                    b[i, l * 16 + k] = b[i, l * 16 + k] - inverse_0[i, l * 16 + k]\n"
+   "    return b\n"},
+  {"trsmU_T",
+   "def trsmU_T(a, b):\n"
+   "    row = b.shape[0]\n"
+   "    col = b.shape[1]\n"
+   "    inverse_0 = allocate((col, ), b.dtype)\n"
+   "    tmp = allocate((col, ), b.dtype)\n"
+   "    for i in range(row):\n"
+   "        for j in range(col):\n"
+   "            tmp[j] = a[j, j]\n"
+   "            b[i, j] = b[i, j] / tmp[j]\n"
+   "            for k in vectorize(col):\n"
+   "                inverse_0[k] = b[i, j] * a[j, k]\n"
+   "            for k in vectorize(j + 1):\n"
+   "                inverse_0[k] = (0.0)\n"
+   "            for k in vectorize(col):\n"
+   "                b[i, k] = b[i, k] - inverse_0[k]\n"
    "    return b\n"}};
+
+const std::map<std::string, std::string> kLUFuncStrMap = {{"lu_decomp",
+                                                           "def lu_decomp(a):\n"
+                                                           "    out_0 = allocate(a.shape, a.dtype)\n"
+                                                           "    out_1 = allocate(a.shape, a.dtype)\n"
+                                                           "    for i in range(a.shape[0]):\n"
+                                                           "        for j in range(a.shape[1]):\n"
+                                                           "            if j > i:\n"
+                                                           "                a[j, i] = a[j, i] / a[i, i]\n"
+                                                           "        for k in range(a.shape[0]):\n"
+                                                           "            for l in vectorize(a.shape[1]):\n"
+                                                           "                out_0[k, l] = a[k, i]\n"
+                                                           "                out_1[k, l] = out_0[k, l] * a[i, l]\n"
+                                                           "                if k > i and l > i:\n"
+                                                           "                    a[k, l] = a[k, l] - out_1[k, l]\n"
+                                                           "    return a\n"}};
 
 const char kTrsmLAttrs[] =
   "{\"pragma_enable_reschedule\": false,"
   " \"enable_hoist_cond_write\": false,"
   " \"enable_approximate_read\": true,"
   " \"enable_post_poly_loop_partition\": false,"
+  " \"enable_polytops\": \"always\"}";
+
+const char kLUAttrs[] =
+  "{\"pragma_enable_reschedule\": false,"
+  " \"enable_hoist_cond_write\": false,"
+  " \"enable_double_buffer\": false,"
+  " \"enable_pre_poly_loop_partition\": false,"
+  " \"enable_post_poly_loop_partition\": false,"
+  " \"enable_to_three_address\": false,"
   " \"enable_polytops\": \"always\"}";
 }  // namespace mindspore::graphkernel::expanders
 #endif  // MINDSPORE_CCSRC_COMMON_GRAPH_KERNEL_EXPANDERS_CUS_UTILS_H_
