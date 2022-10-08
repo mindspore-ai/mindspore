ba0423cfc5e2a1f6c721798598a5ebbb2adc545a
zhengyuanhua <zhengyuanhua1@huawei.com>
2022-10-08 10:05:48 +0800

    adapte lite ascend smoke

--- mindspore/lite/test/st/scripts/ascend/run_ascend.sh
+++ mindspore/lite/test/st/scripts/ascend/run_ascend.sh
@@ -10,7 +10,11 @@ function PrePareLocal() {
   cp ./scripts/ascend/run_converter_ascend.sh ${benchmark_test_path} || exit 1
   cp ./scripts/ascend/run_benchmark_ascend.sh ${benchmark_test_path} || exit 1
   cp ./../${config_folder}/models_ascend.cfg ${benchmark_test_path} || exit 1
-  cp ${release_path}/centos_x86/ascend/*-linux-${arch}.tar.gz ${benchmark_test_path} || exit 1
+  if [[ ${backend} =~ "arm" ]]; then
+      cp ${release_path}/linux_aarch64/ascend/*-linux-${arch}.tar.gz ${benchmark_test_path} || exit 1
+  else
+      cp ${release_path}/centos_x86/ascend/*-linux-${arch}.tar.gz ${benchmark_test_path} || exit 1
+  fi
   echo "Copy file success"
 }
 
@@ -22,7 +26,11 @@ function PrePareRemote() {
   scp ./scripts/ascend/run_benchmark_ascend.sh ${user_name}@${device_ip}:${benchmark_test_path} || exit 1
   scp ./scripts/base_functions.sh ${user_name}@${device_ip}:${benchmark_test_path} || exit 1
   scp ./../${config_folder}/models_ascend.cfg ${user_name}@${device_ip}:${benchmark_test_path} || exit 1
-  scp ${release_path}/centos_x86/ascend/*-linux-${arch}.tar.gz ${user_name}@${device_ip}:${benchmark_test_path} || exit 1
+  if [[ ${backend} =~ "arm" ]]; then
+      scp ${release_path}/linux_aarch64/ascend/*-linux-${arch}.tar.gz ${user_name}@${device_ip}:${benchmark_test_path} || exit 1
+  else
+      scp ${release_path}/centos_x86/ascend/*-linux-${arch}.tar.gz ${user_name}@${device_ip}:${benchmark_test_path} || exit 1
+  fi
   echo "Copy file success"
 }
 
