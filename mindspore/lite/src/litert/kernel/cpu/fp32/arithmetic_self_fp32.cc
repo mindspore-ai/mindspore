e1ff88daf2d436d5cfeb1e77b2e57a38694d4cad
albert-yan <albert.liyan@huawei.com>
2022-10-10 14:23:23 +0800

    fuzz check

--- mindspore/lite/src/litert/kernel/cpu/fp32/arithmetic_self_fp32.cc
+++ mindspore/lite/src/litert/kernel/cpu/fp32/arithmetic_self_fp32.cc
@@ -82,6 +82,11 @@ int ArithmeticSelfCPUKernel::Prepare() {
   CHECK_NOT_EQUAL_RETURN(in_tensors_.size(), 1);
   CHECK_NOT_EQUAL_RETURN(out_tensors_.size(), 1);
 
+  if (out_tensors_[0]->category() == mindspore::lite::CONST_SCALAR ||
+      out_tensors_[0]->category() == mindspore::lite::CONST_TENSOR) {
+    MS_LOG(ERROR) << "out_tensors category is invalid.";
+    return RET_ERROR;
+  }
   if (!InferShapeDone()) {
     return RET_OK;
   }
