09ace3e8256f6b8cfbd6112f1fd4d5984b9e3005
albert-yan <albert.liyan@huawei.com>
2022-10-11 09:54:27 +0800

    fuzz check

--- mindspore/ccsrc/plugin/device/cpu/kernel/nnacl/infer/rank_infer.c
+++ mindspore/ccsrc/plugin/device/cpu/kernel/nnacl/infer/rank_infer.c
@@ -19,7 +19,7 @@
 
 int RankInferShape(const TensorC *const *inputs, size_t inputs_size, TensorC **outputs, size_t outputs_size,
                    OpParameter *parameter) {
-  int check_ret = CheckAugmentWithMinSize(inputs, inputs_size, outputs, outputs_size, parameter, 1, 1);
+  int check_ret = CheckAugmentNullSize(inputs, inputs_size, outputs, outputs_size, parameter, 1, 1);
   if (check_ret != NNACL_OK) {
     return check_ret;
   }
