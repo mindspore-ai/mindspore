5bf31b368af36d466064c510760a97fcc30a80fa
VectorSL <shiliang10@huawei.com>
2022-10-13 10:11:52 +0800

    self code check

--- mindspore/ccsrc/plugin/device/cpu/kernel/sparse_matrix_add_cpu_kernel.h
+++ mindspore/ccsrc/plugin/device/cpu/kernel/sparse_matrix_add_cpu_kernel.h
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_SPARSE_MATRIX_ADD_CPU_KERNEL_H_
-#define MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_SPARSE_MATRIX_ADD_CPU_KERNEL_H_
+#ifndef MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_SPARSE_MATRIX_ADD_CPU_KERNEL_H_
+#define MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_SPARSE_MATRIX_ADD_CPU_KERNEL_H_
 
 #include <vector>
 #include <map>
@@ -56,4 +56,4 @@ class SparseMatrixAddCpuKernelMod : public NativeCpuKernelMod, public MatchKerne
 }  // namespace kernel
 }  // namespace mindspore
 
-#endif  // MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_SPARSE_MATRIX_ADD_CPU_KERNEL_H_
+#endif  // MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_SPARSE_MATRIX_ADD_CPU_KERNEL_H_
