856b6768e408fbe9e1c927789bde2b5b24483063
VectorSL <shiliang10@huawei.com>
2022-10-13 11:29:51 +0800

    refactor dynamicbroadcastgradargs

--- mindspore/ccsrc/plugin/device/gpu/kernel/other/dynamic_broadcast_grad_args_gpu_kernel.cc
+++ mindspore/ccsrc/plugin/device/gpu/kernel/other/dynamic_broadcast_grad_args_gpu_kernel.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright 2021-2022 Huawei Technologies Co., Ltd
+ * Copyright 2022 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,37 +13,217 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <string>
+#include <utility>
+
+#include "mindspore/core/ops/dynamic_broadcast_gradient_args.h"
 #include "plugin/device/gpu/kernel/other/dynamic_broadcast_grad_args_gpu_kernel.h"
 
 namespace mindspore {
 namespace kernel {
-MS_REG_GPU_KERNEL_TWO(DynamicBroadcastGradientArgs,
-                      KernelAttr()
-                        .AddInputAttr(kNumberTypeInt64)
-                        .AddInputAttr(kNumberTypeInt64)
-                        .AddOutputAttr(kNumberTypeInt64)
-                        .AddOutputAttr(kNumberTypeInt64),
-                      DynamicBroadcastGradientArgsGpuKernelMod, int64_t, int64_t)
-MS_REG_GPU_KERNEL_TWO(DynamicBroadcastGradientArgs,
-                      KernelAttr()
-                        .AddInputAttr(kNumberTypeInt32)
-                        .AddInputAttr(kNumberTypeInt32)
-                        .AddOutputAttr(kNumberTypeInt64)
-                        .AddOutputAttr(kNumberTypeInt64),
-                      DynamicBroadcastGradientArgsGpuKernelMod, int32_t, int64_t)
-MS_REG_GPU_KERNEL_TWO(DynamicBroadcastGradientArgs,
-                      KernelAttr()
-                        .AddInputAttr(kNumberTypeUInt64)
-                        .AddInputAttr(kNumberTypeUInt64)
-                        .AddOutputAttr(kNumberTypeInt64)
-                        .AddOutputAttr(kNumberTypeInt64),
-                      DynamicBroadcastGradientArgsGpuKernelMod, uint64_t, int64_t)
-MS_REG_GPU_KERNEL_TWO(DynamicBroadcastGradientArgs,
-                      KernelAttr()
-                        .AddInputAttr(kNumberTypeUInt32)
-                        .AddInputAttr(kNumberTypeUInt32)
-                        .AddOutputAttr(kNumberTypeInt64)
-                        .AddOutputAttr(kNumberTypeInt64),
-                      DynamicBroadcastGradientArgsGpuKernelMod, uint32_t, int64_t)
+namespace {
+constexpr size_t kInputsNum = 2;
+constexpr size_t kOutputsNum = 2;
+constexpr char kKernelName[] = "DynamicBroadcastGradientArgs";
+using KernelRunFunc = DynamicBroadcastGradientArgsGpuKernelMod::KernelRunFunc;
+}  // namespace
+
+template <typename T>
+void AddGradReduceIdx(std::vector<std::vector<T>> *grad_reduce_idx, std::vector<bool> cur_one, bool none_one,
+                      const size_t max_rank, size_t j) {
+  MS_EXCEPTION_IF_NULL(grad_reduce_idx);
+  for (size_t i = 0; i < kInputsNum; i++) {
+    if (cur_one[i] && !none_one) {
+      (void)(*grad_reduce_idx)[i].emplace_back(SizeToLong(max_rank - 1 - j));
+    }
+  }
+}
+
+template <typename T>
+std::vector<std::vector<T>> GetGradIndex(const std::vector<std::vector<T>> &reverse_shapes, const size_t max_rank) {
+  std::vector<std::vector<T>> grad_reduce_index(kInputsNum);
+  std::vector<bool> pre_one(kInputsNum);
+  std::vector<bool> cur_one(kInputsNum);
+  for (size_t i = 0; i < kInputsNum; i++) {
+    pre_one[i] = false;
+    cur_one[i] = false;
+  }
+  bool set_one = false;
+  for (size_t j = 0; j < max_rank; j++) {
+    int out_dim = -1;
+    bool out_dim_set = false;
+    bool none_one = true;
+    for (size_t i = 0; i < kInputsNum; i++) {
+      if (reverse_shapes[i][j] == 1) {
+        cur_one[i] = true;
+        none_one = false;
+      } else {
+        cur_one[i] = false;
+        if (!out_dim_set || reverse_shapes[i][j] == static_cast<T>(out_dim)) {
+          out_dim = static_cast<int>(reverse_shapes[i][j]);
+          out_dim_set = true;
+        } else {
+          MS_LOG(EXCEPTION) << "Can not broadcast inputs[0] and inputs[1].";
+        }
+      }
+    }
+    if (!out_dim_set) {
+      for (size_t i = 0; i < kInputsNum; i++) {
+        (void)grad_reduce_index[i].emplace_back(max_rank - 1 - j);
+      }
+      continue;
+    } else if (std::equal(cur_one.begin(), cur_one.end(), pre_one.begin()) && set_one) {
+      AddGradReduceIdx(&grad_reduce_index, cur_one, none_one, max_rank, j);
+    } else {
+      AddGradReduceIdx(&grad_reduce_index, cur_one, none_one, max_rank, j);
+    }
+    set_one = true;
+    for (size_t i = 0; i < kInputsNum; i++) {
+      pre_one[i] = cur_one[i];
+    }
+  }
+  return grad_reduce_index;
+}
+
+template <typename T, typename S>
+size_t SetOuputValue(S *addr, const std::vector<T> &grad_reduce_idx, cudaStream_t cuda_stream) {
+  std::vector<S> output;
+  size_t index_num = grad_reduce_idx.size();
+  for (size_t i = 0; i < index_num; i++) {
+    output.push_back(static_cast<S>(grad_reduce_idx[index_num - 1 - i]));
+  }
+  size_t out_size = index_num;
+  if (index_num == 0) {
+    return out_size;
+  }
+  CHECK_CUDA_RET_WITH_EXCEPT_NOTRACE(
+    cudaMemcpyAsync(addr, &output[0], out_size * sizeof(S), cudaMemcpyHostToDevice, cuda_stream),
+    "DynamicBroadcastGradientArgs copy output failed");
+  return out_size;
+}
+
+template <typename T, typename S>
+bool DynamicBroadcastGradientArgsGpuKernelMod::LaunchKernel(const std::vector<kernel::AddressPtr> &inputs,
+                                                            const std::vector<AddressPtr> &,
+                                                            const std::vector<kernel::AddressPtr> &outputs) {
+  std::vector<std::vector<T>> reverse_shapes(kInputsNum);
+  std::vector<size_t> ranks = {input_size_list_[0] / sizeof(T), input_size_list_[1] / sizeof(T)};
+  std::vector<T> x0_value(input_size_list_[0] / sizeof(T), 0);
+  if (!is_null_input0_) {
+    auto s0_addr = GetDeviceAddress<T>(inputs, 0);
+    CHECK_CUDA_RET_WITH_EXCEPT_NOTRACE(
+      cudaMemcpyAsync(&x0_value[0], s0_addr, input_size_list_[0], cudaMemcpyDeviceToHost, cuda_stream_),
+      "DynamicBroadcastGradientArgs copy s0 value failed");
+    for (size_t j = 0; j < ranks[0]; j++) {
+      reverse_shapes[0].push_back(x0_value[ranks[0] - j - 1]);
+    }
+  } else {
+    ranks[0] = 0;
+  }
+  std::vector<T> x1_value(input_size_list_[1] / sizeof(T), 0);
+  if (!is_null_input1_) {
+    auto s1_addr = GetDeviceAddress<T>(inputs, 1);
+    CHECK_CUDA_RET_WITH_EXCEPT_NOTRACE(
+      cudaMemcpyAsync(&x1_value[0], s1_addr, input_size_list_[1], cudaMemcpyDeviceToHost, cuda_stream_),
+      "DynamicBroadcastGradientArgs copy s1 value failed");
+    for (size_t j = 0; j < ranks[1]; j++) {
+      reverse_shapes[1].push_back(x1_value[ranks[1] - j - 1]);
+    }
+  } else {
+    ranks[1] = 0;
+  }
+  auto r0_addr = GetDeviceAddress<S>(outputs, 0);
+  auto r1_addr = GetDeviceAddress<S>(outputs, 1);
+  CHECK_CUDA_RET_WITH_EXCEPT_NOTRACE(cudaStreamSynchronize(cuda_stream_),
+                                     "DynamicBroadcastGradientArgs cudaStreamSynchronized failed");
+  std::vector<std::vector<T>> grad_reduce_idx(kInputsNum);
+  size_t max_rank = ranks[0] > ranks[1] ? ranks[0] : ranks[1];
+  if (reverse_shapes[0].size() < max_rank) {
+    reverse_shapes[0].resize(max_rank, 1);
+  }
+  if (reverse_shapes[1].size() < max_rank) {
+    reverse_shapes[1].resize(max_rank, 1);
+  }
+
+  if (reverse_shapes[0] != reverse_shapes[1]) {
+    grad_reduce_idx = GetGradIndex(reverse_shapes, max_rank);
+  }
+  r0_size_ = SetOuputValue(r0_addr, grad_reduce_idx[0], cuda_stream_);
+  r1_size_ = SetOuputValue(r1_addr, grad_reduce_idx[1], cuda_stream_);
+
+  return true;
+}
+
+bool DynamicBroadcastGradientArgsGpuKernelMod::Init(const BaseOperatorPtr &base_operator,
+                                                    const std::vector<KernelTensorPtr> &inputs,
+                                                    const std::vector<KernelTensorPtr> &outputs) {
+  auto kernel_ptr = std::dynamic_pointer_cast<ops::DynamicBroadcastGradientArgs>(base_operator);
+  if (!kernel_ptr) {
+    MS_LOG(ERROR) << "cast DynamicBroadcastGradientArgs ops failed!";
+    return false;
+  }
+  kernel_name_ = kernel_ptr->name();
+
+  if (inputs.size() != kInputsNum || outputs.size() != kOutputsNum) {
+    MS_LOG(ERROR) << kernel_name_ << ": input and output size should be " << kInputsNum << " and " << kOutputsNum
+                  << ", but get " << inputs.size() << " and " << outputs.size();
+    return false;
+  }
+
+  if (!MatchKernelFunc(base_operator, inputs, outputs)) {
+    return false;
+  }
+  is_need_retrieve_output_shape_ = true;
+  return true;
+}
+
+int DynamicBroadcastGradientArgsGpuKernelMod::Resize(const BaseOperatorPtr &base_operator,
+                                                     const std::vector<KernelTensorPtr> &inputs,
+                                                     const std::vector<KernelTensorPtr> &outputs,
+                                                     const std::map<uint32_t, tensor::TensorPtr> &inputsOnHost) {
+  if (KernelMod::Resize(base_operator, inputs, outputs, inputsOnHost) == static_cast<int>(KRET_RESIZE_FAILED)) {
+    MS_LOG(WARNING) << kernel_name_ << " reinit failed.";
+    return static_cast<int>(KRET_RESIZE_FAILED);
+  }
+  // get input_shape
+  auto input_0_shape = inputs[0]->GetShapeVector();
+  auto input_1_shape = inputs[1]->GetShapeVector();
+  is_null_input0_ = CHECK_NULL_INPUT(input_0_shape);
+  is_null_input1_ = CHECK_NULL_INPUT(input_1_shape);
+  outputs_ = outputs;
+
+  return static_cast<int>(KRET_OK);
+}
+
+const std::vector<std::pair<KernelAttr, KernelRunFunc>> &DynamicBroadcastGradientArgsGpuKernelMod::GetFuncList() const {
+  static const std::vector<std::pair<KernelAttr, KernelRunFunc>> func_list = {
+    {KernelAttr()
+       .AddInputAttr(kNumberTypeInt32)
+       .AddInputAttr(kNumberTypeInt32)
+       .AddOutputAttr(kNumberTypeInt32)
+       .AddOutputAttr(kNumberTypeInt32),
+     &DynamicBroadcastGradientArgsGpuKernelMod::LaunchKernel<int32_t, int32_t>},
+    {KernelAttr()
+       .AddInputAttr(kNumberTypeInt32)
+       .AddInputAttr(kNumberTypeInt32)
+       .AddOutputAttr(kNumberTypeInt64)
+       .AddOutputAttr(kNumberTypeInt64),
+     &DynamicBroadcastGradientArgsGpuKernelMod::LaunchKernel<int32_t, int64_t>},
+    {KernelAttr()
+       .AddInputAttr(kNumberTypeInt64)
+       .AddInputAttr(kNumberTypeInt64)
+       .AddOutputAttr(kNumberTypeInt32)
+       .AddOutputAttr(kNumberTypeInt32),
+     &DynamicBroadcastGradientArgsGpuKernelMod::LaunchKernel<int64_t, int32_t>},
+    {KernelAttr()
+       .AddInputAttr(kNumberTypeInt64)
+       .AddInputAttr(kNumberTypeInt64)
+       .AddOutputAttr(kNumberTypeInt64)
+       .AddOutputAttr(kNumberTypeInt64),
+     &DynamicBroadcastGradientArgsGpuKernelMod::LaunchKernel<int64_t, int64_t>},
+  };
+  return func_list;
+}
+MS_KERNEL_FACTORY_REG(NativeGpuKernelMod, DynamicBroadcastGradientArgs, DynamicBroadcastGradientArgsGpuKernelMod);
 }  // namespace kernel
 }  // namespace mindspore
--- mindspore/ccsrc/plugin/device/gpu/kernel/other/dynamic_broadcast_grad_args_gpu_kernel.h
+++ mindspore/ccsrc/plugin/device/gpu/kernel/other/dynamic_broadcast_grad_args_gpu_kernel.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2021-2022 Huawei Technologies Co., Ltd
+ * Copyright 2022 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,194 +14,67 @@
  * limitations under the License.
  */
 
-#ifndef MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_GPU_OTHER_DYNAMIC_BRAODCAST_GRADIENT_ARGS_GPU_KERNEL_H_
-#define MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_GPU_OTHER_DYNAMIC_BRAODCAST_GRADIENT_ARGS_GPU_KERNEL_H_
+#ifndef MINDSPORE_CCSRC_PLUGIN_DEVICE_GPU_KERNEL_OTHER_DYN_BROADCAST_GRAD_ARGS_KERNEL_H_
+#define MINDSPORE_CCSRC_PLUGIN_DEVICE_GPU_KERNEL_OTHER_DYN_BROADCAST_GRAD_ARGS_KERNEL_H_
 
-#include <memory>
-#include <string>
 #include <vector>
-#include <functional>
+#include <map>
+#include <memory>
 #include <algorithm>
+#include <utility>
+
 #include "plugin/device/gpu/kernel/gpu_kernel.h"
 #include "plugin/device/gpu/kernel/gpu_kernel_factory.h"
-
+#include "kernel/common_utils.h"
 namespace mindspore {
 namespace kernel {
-constexpr size_t kInputNum = 2;
-template <typename T, typename S>
-class DynamicBroadcastGradientArgsGpuKernelMod : public DeprecatedNativeGpuKernelMod {
+class DynamicBroadcastGradientArgsGpuKernelMod : public NativeGpuKernelMod,
+                                                 public MatchKernelHelper<DynamicBroadcastGradientArgsGpuKernelMod> {
  public:
-  DynamicBroadcastGradientArgsGpuKernelMod() : r0_size_(0), r1_size_(0) { ResetResource(); }
-  ~DynamicBroadcastGradientArgsGpuKernelMod() = default;
+  DynamicBroadcastGradientArgsGpuKernelMod() : r0_size_(0), r1_size_(0) {}
+  ~DynamicBroadcastGradientArgsGpuKernelMod() override = default;
 
-  bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &,
+  bool Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+            const std::vector<KernelTensorPtr> &outputs) override;
+
+  int Resize(
+    const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+    const std::vector<KernelTensorPtr> &outputs,
+    const std::map<uint32_t, tensor::TensorPtr> &inputsOnHost = std::map<uint32_t, tensor::TensorPtr>()) override;
+
+  bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
               const std::vector<AddressPtr> &outputs, void *stream_ptr) override {
-    auto cuda_stream = reinterpret_cast<cudaStream_t>(stream_ptr);
-    auto s0_addr = GetDeviceAddress<T>(inputs, 0);
-    auto s1_addr = GetDeviceAddress<T>(inputs, 1);
-    auto r0_addr = GetDeviceAddress<S>(outputs, 0);
-    auto r1_addr = GetDeviceAddress<S>(outputs, 1);
-    std::vector<T> x0_value(input_size_list_[0] / sizeof(T), 0);
-    std::vector<T> x1_value(input_size_list_[1] / sizeof(T), 0);
-    CHECK_CUDA_RET_WITH_EXCEPT(
-      kernel_node_, cudaMemcpyAsync(&x0_value[0], s0_addr, input_size_list_[0], cudaMemcpyDeviceToHost, cuda_stream),
-      "DynamicBroadcastGradientArgs copy s0 value failed");
-    CHECK_CUDA_RET_WITH_EXCEPT(
-      kernel_node_, cudaMemcpyAsync(&x1_value[0], s1_addr, input_size_list_[1], cudaMemcpyDeviceToHost, cuda_stream),
-      "DynamicBroadcastGradientArgs copy s1 value failed");
-    CHECK_CUDA_RET_WITH_EXCEPT(kernel_node_, cudaStreamSynchronize(cuda_stream),
-                               "DynamicBroadcastGradientArgs cudaStreamSynchronized failed");
-    auto grad_reduce_idx = CalOut({x0_value, x1_value});
-    r0_size_ = SetOuputValue(r0_addr, grad_reduce_idx[0], x0_value.size(), cuda_stream);
-    r1_size_ = SetOuputValue(r1_addr, grad_reduce_idx[1], x1_value.size(), cuda_stream);
-
-    return true;
-  }
-  bool Init(const CNodePtr &kernel_node) override {
-    kernel_node_ = kernel_node;
-    auto input_num = common::AnfAlgo::GetInputTensorNum(kernel_node);
-    if (input_num != kInputNum) {
-      MS_LOG(EXCEPTION) << "DynamicBroadcastGradiendArgs needs " << kInputNum << " inputs, but get " << input_num;
-    }
-    auto s0_shape = AnfAlgo::GetInputDeviceShapeAdaptively(kernel_node, 0);
-    auto s1_shape = AnfAlgo::GetInputDeviceShapeAdaptively(kernel_node, 1);
-    auto r0_shape = Convert2SizeTClipNeg(AnfAlgo::GetOutputDeviceShapeAdaptively(kernel_node, 0));
-    auto r1_shape = Convert2SizeTClipNeg(AnfAlgo::GetOutputDeviceShapeAdaptively(kernel_node, 1));
-    if (s0_shape.size() != 1 || s1_shape.size() != 1) {
-      MS_LOG(EXCEPTION) << "Inputs must be [1-D], but get " << s0_shape.size() << "-D and " << s1_shape.size() << "-D.";
-    }
-
-    auto s0_size = sizeof(T) * SizeOf(s0_shape);
-    auto s1_size = sizeof(T) * SizeOf(s1_shape);
-
-    input_size_list_.push_back(s0_size);
-    input_size_list_.push_back(s1_size);
-    output_size_list_.push_back(static_cast<size_t>(r0_shape[0]) * sizeof(S));
-    output_size_list_.push_back(static_cast<size_t>(r1_shape[0]) * sizeof(S));
-    is_need_retrieve_output_shape_ = true;
-    return true;
-  }
-  void ResetResource() noexcept override {
-    input_size_list_.clear();
-    output_size_list_.clear();
-    workspace_size_list_.clear();
+    cuda_stream_ = reinterpret_cast<cudaStream_t>(stream_ptr);
+    return kernel_func_(this, inputs, workspace, outputs);
   }
 
- protected:
-  void SyncData() override {
+  const std::vector<std::pair<KernelAttr, KernelRunFunc>> &GetFuncList() const override;
+
+  std::vector<KernelAttr> GetOpSupport() override { return OpSupport(); }
+
+  std::vector<KernelTensorPtr> GetOutputs() override {
     ShapeVector r0_shape{SizeToLong(r0_size_)};
     ShapeVector r1_shape{SizeToLong(r1_size_)};
-    common::AnfAlgo::SetOutputInferTypeAndShape({TypeId::kNumberTypeInt64, TypeId::kNumberTypeInt64},
-                                                {r0_shape, r1_shape}, kernel_node_.lock().get());
-    MS_LOG(DEBUG) << "Run PostExecute for DynamicBroadcastGradientArgs, real r0 shape is " << r0_shape
-                  << ", r1 shape is " << r1_shape;
-  }
 
- protected:
-  void InitSizeLists() override{};
+    outputs_[0]->SetShapeVector(r0_shape);
+    outputs_[1]->SetShapeVector(r1_shape);
+
+    return outputs_;
+  }
 
  private:
-  std::vector<std::vector<T>> CalOut(const std::vector<std::vector<T>> &input_shapes) {
-    std::vector<std::vector<T>> grad_reduce_idx(kInputNum);
-    bool all_equal = true;
-    size_t max_rank = 0;
-    for (size_t i = 0; i < kInputNum; i++) {
-      if (input_shapes[i] != input_shapes[0]) {
-        all_equal = false;
-      }
-      if (input_shapes[i].size() > max_rank) {
-        max_rank = input_shapes[i].size();
-      }
-    }
-    if (all_equal) {
-      return grad_reduce_idx;
-    }
-    // Reverse shapes
-    std::vector<std::vector<T>> reverse_shapes(kInputNum);
-    for (size_t i = 0; i < kInputNum; i++) {
-      reverse_shapes[i] = input_shapes[i];
-      std::reverse(reverse_shapes[i].begin(), reverse_shapes[i].end());
-      if (reverse_shapes[i].size() < max_rank) {
-        reverse_shapes[i].resize(max_rank, 1);
-      }
-    }
-    grad_reduce_idx = GetGradIndex(reverse_shapes, max_rank);
-    return grad_reduce_idx;
-  }
+  template <typename T, typename S>
+  bool LaunchKernel(const std::vector<kernel::AddressPtr> &inputs, const std::vector<AddressPtr> &,
+                    const std::vector<kernel::AddressPtr> &outputs);
 
-  void AddGradReduceIdx(std::vector<std::vector<T>> *grad_reduce_idx, std::vector<bool> cur_one, bool none_one,
-                        const size_t max_rank, size_t j) {
-    MS_EXCEPTION_IF_NULL(grad_reduce_idx);
-    for (size_t i = 0; i < kInputNum; i++) {
-      if (cur_one[i] && !none_one) {
-        (void)(*grad_reduce_idx)[i].emplace_back(SizeToLong(max_rank - 1 - j));
-      }
-    }
-  }
-
-  std::vector<std::vector<T>> GetGradIndex(const std::vector<std::vector<T>> &revers_shapes, const size_t max_rank) {
-    std::vector<std::vector<T>> grad_reduce_index(kInputNum);
-    std::vector<bool> pre_one(kInputNum);
-    std::vector<bool> cur_one(kInputNum);
-    for (size_t i = 0; i < kInputNum; i++) {
-      pre_one[i] = false;
-      cur_one[i] = false;
-    }
-    bool set_one = false;
-    for (size_t j = 0; j < max_rank; j++) {
-      int out_dim = -1;
-      bool out_dim_set = false;
-      bool none_one = true;
-      for (size_t i = 0; i < kInputNum; i++) {
-        if (revers_shapes[i][j] == 1) {
-          cur_one[i] = true;
-          none_one = false;
-        } else {
-          cur_one[i] = false;
-          if (!out_dim_set || revers_shapes[i][j] == static_cast<T>(out_dim)) {
-            out_dim = static_cast<int>(revers_shapes[i][j]);
-            out_dim_set = true;
-          } else {
-            MS_LOG(EXCEPTION) << "Can not broadcast inputs[0] and inputs[1].";
-          }
-        }
-      }
-      if (!out_dim_set) {
-        for (size_t i = 0; i < kInputNum; i++) {
-          (void)grad_reduce_index[i].emplace_back(max_rank - 1 - j);
-        }
-        continue;
-      } else if (std::equal(cur_one.begin(), cur_one.end(), pre_one.begin()) && set_one) {
-        AddGradReduceIdx(&grad_reduce_index, cur_one, none_one, max_rank, j);
-      } else {
-        AddGradReduceIdx(&grad_reduce_index, cur_one, none_one, max_rank, j);
-      }
-      set_one = true;
-      for (size_t i = 0; i < kInputNum; i++) {
-        pre_one[i] = cur_one[i];
-      }
-    }
-    return grad_reduce_index;
-  }
-  size_t SetOuputValue(S *addr, const std::vector<T> grad_reduce_idx, size_t input_num, cudaStream_t stream) {
-    std::vector<S> output;
-    size_t index_num = grad_reduce_idx.size();
-    for (size_t i = 0; i < index_num; i++) {
-      output.push_back(static_cast<S>(grad_reduce_idx[index_num - 1 - i]));
-    }
-    size_t out_size = index_num;
-    if (index_num == 0) {
-      return out_size;
-    }
-    CHECK_CUDA_RET_WITH_EXCEPT(kernel_node_,
-                               cudaMemcpyAsync(addr, &output[0], out_size * sizeof(S), cudaMemcpyHostToDevice, stream),
-                               "DynamicBroadcastGradientArgs copy output failed");
-    return out_size;
-  }
   size_t r0_size_;
   size_t r1_size_;
+  std::vector<KernelTensorPtr> outputs_;
+  bool is_null_input0_{false};
+  bool is_null_input1_{false};
+  cudaStream_t cuda_stream_{nullptr};
 };
 }  // namespace kernel
 }  // namespace mindspore
 
-#endif  // MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_GPU_OTHER_DYNAMIC_BRAODCAST_GRADIENT_ARGS_GPU_KERNEL_H_
+#endif  // MINDSPORE_CCSRC_PLUGIN_DEVICE_GPU_KERNEL_OTHER_DYN_BROADCAST_GRAD_ARGS_KERNEL_H_
