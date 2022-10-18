b8f029b61bbd486461cbafbb6372034e5b526bfb
mengyuanli <limengyuan4@huawei.com>
2022-10-18 09:42:53 +0800

    make complex_abs cpu support dynamic shape

--- mindspore/ccsrc/plugin/device/cpu/kernel/complex_abs_cpu_kernel.cc
+++ mindspore/ccsrc/plugin/device/cpu/kernel/complex_abs_cpu_kernel.cc
@@ -17,41 +17,21 @@
 #include <complex>
 #include "plugin/device/cpu/hal/device/cpu_device_address.h"
 
-namespace {
-const size_t kOutputsNum = 1;
-const size_t kInputsNum = 1;
-
-#define ComplexAbs_COMPUTE_CASE(IN_DTYPE, IN_TYPE, OUT_DTYPE) \
-  case (IN_DTYPE): {                                          \
-    ret = LaunchKernel<IN_TYPE, OUT_DTYPE>(inputs, outputs);  \
-    break;                                                    \
-  }
-}  // namespace
-
 namespace mindspore {
 namespace kernel {
-void ComplexAbsCpuKernelMod::InitKernel(const CNodePtr &kernel_node) {
-  MS_EXCEPTION_IF_NULL(kernel_node);
-  kernel_name_ = common::AnfAlgo::GetCNodeName(kernel_node);
-  input_dtype_ = AnfAlgo::GetInputDeviceDataType(kernel_node, 0);
-}
-
-bool ComplexAbsCpuKernelMod::Launch(const std::vector<kernel::AddressPtr> &inputs,
-                                    const std::vector<kernel::AddressPtr> &,
-                                    const std::vector<kernel::AddressPtr> &outputs) {
-  CHECK_KERNEL_INPUTS_NUM(inputs.size(), kInputsNum, kernel_name_);
-  CHECK_KERNEL_OUTPUTS_NUM(outputs.size(), kOutputsNum, kernel_name_);
-  bool ret = true;
-  switch (input_dtype_) {
-    ComplexAbs_COMPUTE_CASE(kNumberTypeComplex64, std::complex<float>, float)
-      ComplexAbs_COMPUTE_CASE(kNumberTypeComplex128, std::complex<double>, double) default : ret = false;
-    MS_EXCEPTION(TypeError) << "For ComplexAbs: unsupported input data type: " << TypeIdToString(input_dtype_) << " .";
-  }
-  return ret;
+bool ComplexAbsCpuKernelMod::Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+                                  const std::vector<KernelTensorPtr> &outputs) {
+  constexpr size_t input_num = 1;
+  constexpr size_t output_num = 1;
+  CHECK_KERNEL_INPUTS_NUM(inputs.size(), input_num, kernel_name_);
+  CHECK_KERNEL_OUTPUTS_NUM(outputs.size(), output_num, kernel_name_);
+  kernel_name_ = base_operator->GetPrim()->name();
+  return MatchKernelFunc(base_operator, inputs, outputs);
 }
 
 template <typename T, typename T2>
 bool ComplexAbsCpuKernelMod::LaunchKernel(const std::vector<kernel::AddressPtr> &inputs,
+                                          const std::vector<kernel::AddressPtr> &,
                                           const std::vector<kernel::AddressPtr> &outputs) {
   auto input_addr = reinterpret_cast<T *>(inputs[0]->addr);
   auto output_addr = reinterpret_cast<T2 *>(outputs[0]->addr);
@@ -67,12 +47,15 @@ bool ComplexAbsCpuKernelMod::LaunchKernel(const std::vector<kernel::AddressPtr> 
   return true;
 }
 
-std::vector<KernelAttr> ComplexAbsCpuKernelMod::GetOpSupport() {
-  static std::vector<KernelAttr> support_list = {
-    KernelAttr().AddInputAttr(kNumberTypeComplex64).AddOutputAttr(kNumberTypeFloat32),
-    KernelAttr().AddInputAttr(kNumberTypeComplex128).AddOutputAttr(kNumberTypeFloat64)};
-
-  return support_list;
+const std::vector<std::pair<KernelAttr, ComplexAbsCpuKernelMod::KernelRunFunc>> &ComplexAbsCpuKernelMod::GetFuncList()
+  const {
+  static const std::vector<std::pair<KernelAttr, ComplexAbsCpuKernelMod::KernelRunFunc>> func_list = {
+    {KernelAttr().AddInputAttr(kNumberTypeComplex64).AddOutputAttr(kNumberTypeFloat32),
+     &ComplexAbsCpuKernelMod::LaunchKernel<std::complex<float>, float>},
+    {KernelAttr().AddInputAttr(kNumberTypeComplex128).AddOutputAttr(kNumberTypeFloat64),
+     &ComplexAbsCpuKernelMod::LaunchKernel<std::complex<double>, double>},
+  };
+  return func_list;
 }
 
 MS_KERNEL_FACTORY_REG(NativeCpuKernelMod, ComplexAbs, ComplexAbsCpuKernelMod);
--- mindspore/ccsrc/plugin/device/cpu/kernel/complex_abs_cpu_kernel.h
+++ mindspore/ccsrc/plugin/device/cpu/kernel/complex_abs_cpu_kernel.h
@@ -14,38 +14,44 @@
  * limitations under the License.
  */
 
-#ifndef MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_COMPLEXABS_CPU_KERNEL_H_
-#define MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_COMPLEXABS_CPU_KERNEL_H_
+#ifndef MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_COMPLEXABS_CPU_KERNEL_H_
+#define MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_COMPLEXABS_CPU_KERNEL_H_
 
 #include <vector>
 #include <memory>
 #include <map>
 #include <string>
+#include <utility>
 #include "plugin/device/cpu/kernel/cpu_kernel.h"
 #include "plugin/factory/ms_factory.h"
 
 namespace mindspore {
 namespace kernel {
-class ComplexAbsCpuKernelMod : public DeprecatedNativeCpuKernelMod {
+class ComplexAbsCpuKernelMod : public NativeCpuKernelMod, public MatchKernelHelper<ComplexAbsCpuKernelMod> {
  public:
   ComplexAbsCpuKernelMod() = default;
   ~ComplexAbsCpuKernelMod() override = default;
 
-  void InitKernel(const CNodePtr &kernel_node) override;
+  bool Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+            const std::vector<KernelTensorPtr> &outputs) override;
 
   bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
-              const std::vector<AddressPtr> &outputs) override;
+              const std::vector<AddressPtr> &outputs) override {
+    MS_EXCEPTION_IF_NULL(kernel_func_);
+    return kernel_func_(this, inputs, workspace, outputs);
+  }
+
+  const std::vector<std::pair<KernelAttr, KernelRunFunc>> &GetFuncList() const override;
 
  protected:
-  std::vector<KernelAttr> GetOpSupport() override;
+  std::vector<KernelAttr> GetOpSupport() override { return OpSupport(); }
 
  private:
-  template <typename T, typename t>
-  bool LaunchKernel(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &outputs);
-
-  TypeId input_dtype_;
+  template <typename T, typename T2>
+  bool LaunchKernel(const std::vector<AddressPtr> &inputs, const std::vector<kernel::AddressPtr> &,
+                    const std::vector<AddressPtr> &outputs);
 };
 }  // namespace kernel
 }  // namespace mindspore
 
-#endif  // MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_COMPLEXABS_CPU_KERNEL_H_
+#endif  // MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_COMPLEXABS_CPU_KERNEL_H_
