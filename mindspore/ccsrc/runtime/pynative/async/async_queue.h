08ea903ed92ff697dfe474917430d8e80e72808b
caifubi <caifubi1@huawei.com>
2022-10-20 11:10:33 +0800

    PyNative AsyncQueue

---
+++ mindspore/ccsrc/runtime/pynative/async/async_queue.h
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MINDSPORE_MINDSPORE_CCSRC_RUNTIME_PYNATIVE_ASYNC_ASYNC_QUEUE_H_
+#define MINDSPORE_MINDSPORE_CCSRC_RUNTIME_PYNATIVE_ASYNC_ASYNC_QUEUE_H_
+
+#include <queue>
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+#include "include/backend/visible.h"
+#include "runtime/pynative/async/task.h"
+
+namespace mindspore {
+namespace pynative {
+// Create a new thread to execute the tasks in the queue sequentially.
+class BACKEND_EXPORT AsyncQueue {
+ public:
+  AsyncQueue();
+  ~AsyncQueue();
+
+  // Add task to the end of the queue.
+  void Push(const std::shared_ptr<AsyncTask> &task);
+
+  // Wait for all async task finish executing.
+  void Wait();
+
+  // Check if the queue is empty.
+  bool Empty();
+
+  // When an exception occurs, the state needs to be reset.
+  void Reset();
+
+  // Thread join before the process exit.
+  void WorkerJoin();
+
+ private:
+  void WorkerLoop();
+
+  std::queue<std::shared_ptr<AsyncTask>> tasks_;
+  std::shared_ptr<std::thread> worker_;
+  std::mutex task_mutex_;
+  std::condition_variable task_cond_var_;
+};
+}  // namespace pynative
+}  // namespace mindspore
+
+#endif  // MINDSPORE_MINDSPORE_CCSRC_RUNTIME_PYNATIVE_ASYNC_ASYNC_QUEUE_H_
--- mindspore/ccsrc/runtime/pynative/op_executor.cc
+++ mindspore/ccsrc/runtime/pynative/op_executor.cc
@@ -15,9 +15,6 @@
  */
 
 #include "runtime/pynative/op_executor.h"
-#if !defined(_WIN32) && !defined(_WIN64) && !defined(__APPLE__)
-#include "include/common/utils/signal_util.h"
-#endif
 
 namespace mindspore::runtime {
 OpExecutor &OpExecutor::GetInstance() {
@@ -25,35 +22,20 @@ OpExecutor &OpExecutor::GetInstance() {
   return instance;
 }
 
-OpExecutor::OpExecutor() { worker_ = std::make_shared<std::thread>(&OpExecutor::WorkerLoop, this); }
+OpExecutor::OpExecutor() = default;
 
-OpExecutor::~OpExecutor() { WorkerJoin(); }
+OpExecutor::~OpExecutor() = default;
 
-void OpExecutor::Register(const std::function<void()> &callback) {
-  batch_build_callback_ = callback;
-  registered_ = true;
-}
+void OpExecutor::Register(const std::function<void()> &callback) { batch_build_callback_ = callback; }
 
 void OpExecutor::Reset() {
   ClearResources();
   batch_build_callback_ = nullptr;
-  registered_ = false;
-
-  // There is still one task in progress
-  try {
-    WaitForRun();
-  } catch (const std::exception &e) {
-    MS_LOG(ERROR) << "Wait failed, error message:" << e.what();
-  } catch (...) {
-    MS_LOG(ERROR) << "Wait failed";
-  }
+  async_queue_.Reset();
 }
 
 void OpExecutor::ClearResources() {
   MS_LOG(DEBUG) << "Start clear tasks";
-  std::lock_guard<std::mutex> lock(task_mutex_);
-  ClearRunOpTasks();
-
   // Set the build task failed, and no need to run op_run_tasks.
   for (auto &build_task : op_build_tasks_) {
     build_task->SetBuildReady(false);
@@ -73,9 +55,7 @@ void OpExecutor::WaitForBuild() {
 
 void OpExecutor::WaitForRun() {
   MS_LOG(DEBUG) << "Start";
-  std::unique_lock<std::mutex> lock(task_mutex_);
-  task_cond_var_.wait(lock, [this]() { return op_run_tasks_.empty(); });
-  MsException::Instance().CheckException();
+  async_queue_.Wait();
   MS_LOG(DEBUG) << "All task finish";
 }
 
@@ -84,20 +64,16 @@ void OpExecutor::Wait() {
   WaitForRun();
 }
 
-void OpExecutor::PushOpBuildTask(const std::shared_ptr<OpBuildTask> &op_build_task) {
-  std::lock_guard<std::mutex> lock(task_mutex_);
+void OpExecutor::PushOpBuildTask(const std::shared_ptr<pynative::BackendOpBuildTask> &op_build_task) {
   op_build_tasks_.push_back(op_build_task);
 }
 
-void OpExecutor::PushOpRunTask(const std::shared_ptr<OpTask> &op_run_task) {
-  std::lock_guard<std::mutex> lock(task_mutex_);
-  op_run_tasks_.push(op_run_task);
+void OpExecutor::PushOpRunTask(const std::shared_ptr<pynative::BackendOpRunTask> &op_run_task) {
+  async_queue_.Push(op_run_task);
   (void)actor_in_queue_.insert(op_run_task->context()->graph_id());
-  task_cond_var_.notify_all();
 }
 
 void OpExecutor::ClearOpBuildTasks() {
-  std::lock_guard<std::mutex> lock(task_mutex_);
   for (auto &task : op_build_tasks_) {
     task->SetBuildReady(true);
   }
@@ -105,101 +81,16 @@ void OpExecutor::ClearOpBuildTasks() {
   MS_LOG(DEBUG) << "Clear build task";
 }
 
-bool OpExecutor::BuildQueueEmpty() {
-  std::lock_guard<std::mutex> lock(task_mutex_);
-  return op_build_tasks_.empty();
-}
+bool OpExecutor::BuildQueueEmpty() { return op_build_tasks_.empty(); }
 
-bool OpExecutor::RunQueueEmpty() {
-  std::lock_guard<std::mutex> lock(task_mutex_);
-  return op_run_tasks_.empty();
-}
+bool OpExecutor::RunQueueEmpty() { return async_queue_.Empty(); }
 
-bool OpExecutor::BuildQueueFull() {
-  std::lock_guard<std::mutex> lock(task_mutex_);
-  return op_build_tasks_.size() > kMaxQueueSize;
-}
+bool OpExecutor::BuildQueueFull() { return op_build_tasks_.size() > kMaxQueueSize; }
 
 bool OpExecutor::ActorInQueue(GraphId graph_id) {
-  std::lock_guard<std::mutex> lock(task_mutex_);
   auto iter = actor_in_queue_.find(graph_id);
   return iter != actor_in_queue_.end();
 }
 
-void OpExecutor::ClearRunOpTasks() {
-  actor_in_queue_.clear();
-  std::queue<std::shared_ptr<OpTask>> empty;
-  // No need to worry about ExitOpTask.
-  // ClearRunOpTasks is executed before ~OpExecutor
-  std::swap(op_run_tasks_, empty);
-}
-
-void OpExecutor::WorkerLoop() {
-#if !defined(_WIN32) && !defined(_WIN64) && !defined(__APPLE__)
-  SignalGuard sig([](int, siginfo_t *, void *) {
-    int this_pid = getpid();
-    MS_LOG(WARNING) << "Process " << this_pid << " receive KeyboardInterrupt signal.";
-    (void)kill(this_pid, SIGTERM);
-  });
-#endif
-
-  while (true) {
-    std::shared_ptr<OpTask> task;
-    {
-      MS_LOG(DEBUG) << "Wait task in queue";
-      std::unique_lock<std::mutex> lock(task_mutex_);
-      task_cond_var_.wait(lock, [this]() { return !op_run_tasks_.empty(); });
-      task = op_run_tasks_.front();
-    }
-
-    MS_LOG(DEBUG) << "Get task";
-    MS_EXCEPTION_IF_NULL(task);
-    if (task->task_type() == kExitTask) {
-      MS_LOG(DEBUG) << "Thread exit";
-      return;
-    }
-    try {
-      task->Run();
-      std::unique_lock<std::mutex> lock(task_mutex_);
-      if (!op_run_tasks_.empty()) {
-        op_run_tasks_.pop();
-        (void)actor_in_queue_.erase(task->context()->graph_id());
-      }
-
-      if (op_run_tasks_.empty()) {
-        MS_LOG(DEBUG) << "Task queue empty";
-        task_cond_var_.notify_all();
-      }
-    } catch (const std::exception &e) {
-      MS_LOG(ERROR) << "Run lazy task failed, error message:" << e.what();
-      {
-        std::unique_lock<std::mutex> lock(task_mutex_);
-        ClearRunOpTasks();
-        MsException::Instance().SetException();
-        task_cond_var_.notify_all();
-      }
-    }
-  }
-}
-
-void OpExecutor::WorkerJoin() {
-  try {
-    // Avoid worker thread join itself which will cause deadlock
-    if (worker_->joinable() && worker_->get_id() != std::this_thread::get_id()) {
-      {
-        std::lock_guard<std::mutex> lock(task_mutex_);
-        auto task = std::make_shared<ExitOpTask>();
-        op_run_tasks_.push(task);
-        task_cond_var_.notify_all();
-        MS_LOG(DEBUG) << "Push exit task and notify all";
-      }
-      worker_->join();
-      MS_LOG(DEBUG) << "Worker join finish";
-    }
-  } catch (const std::exception &e) {
-    MS_LOG(ERROR) << "WorkerJoin failed: " << e.what();
-  } catch (...) {
-    MS_LOG(ERROR) << "WorkerJoin failed";
-  }
-}
+void OpExecutor::WorkerJoin() { async_queue_.WorkerJoin(); }
 }  // namespace mindspore::runtime
