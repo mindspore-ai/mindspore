bd1216e1bf4edeb744c6aa2cb70edc40a079f48c
yiyanzhi_akane <yiyanzhi@huawei.com>
2022-10-21 11:29:55 +0800

    new kernel mod: fractional_max_pool_grad_cpu_kernel

--- mindspore/ccsrc/plugin/device/cpu/kernel/fractional_max_pool_grad_cpu_kernel.h
+++ mindspore/ccsrc/plugin/device/cpu/kernel/fractional_max_pool_grad_cpu_kernel.h
@@ -17,6 +17,7 @@
 #ifndef MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_FRACTIONAL_MAX_POOL_GRAD_CPU_KERNEL_H_
 #define MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_FRACTIONAL_MAX_POOL_GRAD_CPU_KERNEL_H_
 
+#include <map>
 #include <unordered_map>
 #include <vector>
 #include <random>
@@ -27,15 +28,20 @@
 #include "Eigen/Core"
 #include "plugin/device/cpu/kernel/cpu_kernel.h"
 #include "plugin/factory/ms_factory.h"
+#include "mindspore/core/ops/grad/fractional_max_pool_grad.h"
 
 namespace mindspore {
 namespace kernel {
-class FractionalMaxPoolGradCpuKernelMod : public DeprecatedNativeCpuKernelMod {
+class FractionalMaxPoolGradCpuKernelMod : public NativeCpuKernelMod {
  public:
   FractionalMaxPoolGradCpuKernelMod() = default;
   ~FractionalMaxPoolGradCpuKernelMod() override = default;
 
-  void InitKernel(const CNodePtr &kernel_node) override;
+  bool Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+            const std::vector<KernelTensorPtr> &outputs) override;
+  int Resize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+             const std::vector<KernelTensorPtr> &outputs, const std::map<uint32_t, tensor::TensorPtr> &) override;
+
   bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &,
               const std::vector<AddressPtr> &outputs) override {
     return kernel_func_(this, inputs, outputs);
