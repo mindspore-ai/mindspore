f86509ac76f4cce7e9eced4a410e76ddbb88e588
hujiahui8 <hujiahui8@huawei.com>
2022-10-21 17:28:28 +0800

    kernelmod: logspace

--- mindspore/ccsrc/plugin/device/cpu/kernel/logspace_cpu_kernel.h
+++ mindspore/ccsrc/plugin/device/cpu/kernel/logspace_cpu_kernel.h
@@ -13,23 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_ZEROSLIKE_CPU_KERNEL_H_
-#define MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_ZEROSLIKE_CPU_KERNEL_H_
+#ifndef MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_LOGSPACE_CPU_KERNEL_H_
+#define MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_LOGSPACE_CPU_KERNEL_H_
 
 #include <complex>
 #include <vector>
 #include <utility>
+#include <map>
+
 #include "plugin/device/cpu/kernel/cpu_kernel.h"
 #include "plugin/factory/ms_factory.h"
 
 namespace mindspore {
 namespace kernel {
-class LogSpaceCpuKernelMod : public DeprecatedNativeCpuKernelMod {
+class LogSpaceCpuKernelMod : public NativeCpuKernelMod {
  public:
   LogSpaceCpuKernelMod() = default;
   ~LogSpaceCpuKernelMod() override = default;
 
-  void InitKernel(const CNodePtr &kernel_node) override;
+  bool Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+            const std::vector<KernelTensorPtr> &outputs) override;
+
+  int Resize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+             const std::vector<KernelTensorPtr> &outputs, const std::map<uint32_t, tensor::TensorPtr> &) override;
+
   bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
               const std::vector<AddressPtr> &outputs) override {
     return kernel_func_(this, inputs, workspace, outputs);
@@ -54,4 +61,4 @@ class LogSpaceCpuKernelMod : public DeprecatedNativeCpuKernelMod {
 }  // namespace kernel
 }  // namespace mindspore
 
-#endif  // MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_LOGSPACE_CPU_KERNEL_H_
+#endif  // MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_LOGSPACE_CPU_KERNEL_H_
