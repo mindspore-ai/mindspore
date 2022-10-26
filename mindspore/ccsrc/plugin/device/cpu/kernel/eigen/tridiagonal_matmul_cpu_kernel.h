41d8b652cadc0df6e18b5d92a3d7b7df6d6436bc
y00451588 <yangshuo22@huawei.com>
2022-10-26 19:48:55 +0800

    refactor TridiagonalMatmul operator kernelmod.

--- mindspore/ccsrc/plugin/device/cpu/kernel/eigen/tridiagonal_matmul_cpu_kernel.h
+++ mindspore/ccsrc/plugin/device/cpu/kernel/eigen/tridiagonal_matmul_cpu_kernel.h
@@ -15,6 +15,7 @@
  */
 #ifndef MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_TRIDIAGONAL_MATMUL_CPU_KERNEL_H_
 #define MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_TRIDIAGONAL_MATMUL_CPU_KERNEL_H_
+#include <map>
 #include <vector>
 #include "plugin/device/cpu/kernel/cpu_kernel.h"
 #include "plugin/factory/ms_factory.h"
@@ -22,11 +23,15 @@
 
 namespace mindspore {
 namespace kernel {
-class TridiagonalMatMulCpuKernelMod : public DeprecatedNativeCpuKernelMod {
+class TridiagonalMatMulCpuKernelMod : public NativeCpuKernelMod {
  public:
   TridiagonalMatMulCpuKernelMod() = default;
   ~TridiagonalMatMulCpuKernelMod() override = default;
-  void InitKernel(const CNodePtr &kernel_node) override;
+
+  bool Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+            const std::vector<KernelTensorPtr> &outputs) override;
+  int Resize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+             const std::vector<KernelTensorPtr> &outputs, const std::map<uint32_t, tensor::TensorPtr> &) override;
   bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
               const std::vector<AddressPtr> &outputs) override;
 
