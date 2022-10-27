9799b05617fd0b4e1f57694e656c4319f19ae064
mengyuanli <limengyuan4@huawei.com>
2022-10-27 17:29:53 +0800

    upper bound cpu op support dynamic shape

--- mindspore/ccsrc/plugin/device/cpu/kernel/upper_bound_cpu_kernel.h
+++ mindspore/ccsrc/plugin/device/cpu/kernel/upper_bound_cpu_kernel.h
@@ -13,22 +13,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_UPPER_BOUND_CPU_KERNEL_H_
-#define MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_UPPER_BOUND_CPU_KERNEL_H_
+#ifndef MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_UPPER_BOUND_CPU_KERNEL_H_
+#define MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_UPPER_BOUND_CPU_KERNEL_H_
 
 #include <vector>
 #include <utility>
+#include <map>
 
 #include "plugin/device/cpu/kernel/cpu_kernel.h"
 #include "plugin/factory/ms_factory.h"
 namespace mindspore {
 namespace kernel {
-class UpperBoundCpuKernelMod : public DeprecatedNativeCpuKernelMod {
+class UpperBoundCpuKernelMod : public NativeCpuKernelMod {
  public:
   UpperBoundCpuKernelMod() = default;
   ~UpperBoundCpuKernelMod() override = default;
 
-  void InitKernel(const CNodePtr &kernel_node) override;
+  bool Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+            const std::vector<KernelTensorPtr> &outputs) override;
+
+  int Resize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+             const std::vector<KernelTensorPtr> &outputs, const std::map<uint32_t, tensor::TensorPtr> &) override;
 
   bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
               const std::vector<AddressPtr> &outputs) override {
@@ -57,4 +62,4 @@ class UpperBoundCpuKernelMod : public DeprecatedNativeCpuKernelMod {
 }  // namespace kernel
 }  // namespace mindspore
 
-#endif  // MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_UPPER_BOUND_CPU_KERNEL_H_
+#endif  // MINDSPORE_CCSRC_PLUGIN_DEVICE_CPU_KERNEL_UPPER_BOUND_CPU_KERNEL_H_
