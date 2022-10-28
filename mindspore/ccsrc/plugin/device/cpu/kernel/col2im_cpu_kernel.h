08195f0f9812161d3aaa14f2a6a2f28821e4cb67
hw_hz <zhanghao491@huawei.com>
2022-10-28 15:37:10 +0800

    Coalesce CheckNumerics dyn

--- mindspore/ccsrc/plugin/device/cpu/kernel/col2im_cpu_kernel.h
+++ mindspore/ccsrc/plugin/device/cpu/kernel/col2im_cpu_kernel.h
@@ -18,19 +18,23 @@
 #define MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_COL2IM_CPU_KERNEL_H_
 
 #include <vector>
-
+#include <map>
 #include "plugin/device/cpu/kernel/cpu_kernel.h"
 #include "kernel/common_utils.h"
 #include "plugin/factory/ms_factory.h"
 
 namespace mindspore {
 namespace kernel {
-class Col2ImCpuKernelMod : public DeprecatedNativeCpuKernelMod {
+class Col2ImCpuKernelMod : public NativeCpuKernelMod {
  public:
   Col2ImCpuKernelMod() = default;
   ~Col2ImCpuKernelMod() override = default;
 
-  void InitKernel(const CNodePtr &kernel_node) override;
+  bool Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+            const std::vector<KernelTensorPtr> &outputs) override;
+
+  int Resize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+             const std::vector<KernelTensorPtr> &outputs, const std::map<uint32_t, tensor::TensorPtr> &) override;
 
   bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
               const std::vector<AddressPtr> &outputs) override;
