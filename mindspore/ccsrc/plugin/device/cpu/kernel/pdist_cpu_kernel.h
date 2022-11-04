c9f9b31c60788ce3008734784b2ed925749adb4c
mengyuanli <limengyuan4@huawei.com>
2022-11-04 15:56:10 +0800

    pdist support dynamic shape pdist_grad support dynamic shape

--- mindspore/ccsrc/plugin/device/cpu/kernel/pdist_cpu_kernel.h
+++ mindspore/ccsrc/plugin/device/cpu/kernel/pdist_cpu_kernel.h
@@ -20,6 +20,7 @@
 #include <vector>
 #include <memory>
 #include <map>
+#include <utility>
 #include <functional>
 #include "plugin/device/cpu/kernel/cpu_kernel.h"
 #include "plugin/factory/ms_factory.h"
@@ -34,9 +35,10 @@ class PdistCpuKernelMod : public NativeCpuKernelMod {
 
   bool Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
             const std::vector<KernelTensorPtr> &outputs) override;
-  int Resize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
-             const std::vector<KernelTensorPtr> &outputs,
-             const std::map<uint32_t, tensor::TensorPtr> &others = std::map<uint32_t, tensor::TensorPtr>()) override;
+  int Resize(
+    const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+    const std::vector<KernelTensorPtr> &outputs,
+    const std::map<uint32_t, tensor::TensorPtr> &inputsOnHost = std::map<uint32_t, tensor::TensorPtr>()) override;
   bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &,
               const std::vector<AddressPtr> &outputs) override;
 
--- mindspore/ccsrc/plugin/device/cpu/kernel/pdist_grad_cpu_kernel.h
+++ mindspore/ccsrc/plugin/device/cpu/kernel/pdist_grad_cpu_kernel.h
@@ -20,6 +20,7 @@
 #include <vector>
 #include <memory>
 #include <map>
+#include <utility>
 #include <functional>
 #include "mindspore/core/ops/grad/pdist_grad.h"
 #include "plugin/device/cpu/kernel/cpu_kernel.h"
@@ -35,9 +36,10 @@ class PdistGradCpuKernelMod : public NativeCpuKernelMod {
   bool Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
             const std::vector<KernelTensorPtr> &outputs) override;
 
-  int Resize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
-             const std::vector<KernelTensorPtr> &outputs,
-             const std::map<uint32_t, tensor::TensorPtr> &others = std::map<uint32_t, tensor::TensorPtr>()) override;
+  int Resize(
+    const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
+    const std::vector<KernelTensorPtr> &outputs,
+    const std::map<uint32_t, tensor::TensorPtr> &inputsOnHost = std::map<uint32_t, tensor::TensorPtr>()) override;
 
   bool Launch(const std::vector<AddressPtr> &inputs, const std::vector<AddressPtr> &workspace,
               const std::vector<AddressPtr> &outputs) override {
