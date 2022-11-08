45a31ac6125ec1f8ca764cc58ffab94d494d5305
jpc_chenjianping <jpc.chen@huawei.com>
2022-11-08 14:32:46 +0800

    fix bug|log level set but no effective

--- mindspore/lite/src/runtime/cxx_api/context.cc
+++ mindspore/lite/src/runtime/cxx_api/context.cc
@@ -48,8 +48,23 @@ constexpr auto KModelOptionAscendFusionSwitchCfgPath = "mindspore.option.ascend.
 constexpr auto kModelOptionAscendDynamicBatchSize = "mindspore.option.ascend.dynamic_batch_size";
 constexpr auto kModelOptionAscendDynamicImageSize = "mindspore.option.ascend.dynamic_image_size";
 constexpr auto kModelOptionAscendBufferOptimize = "mindspore.option.ascend.buffer_optimize";
+#ifdef USE_GLOG
+extern "C" {
+extern void mindspore_log_init();
+}
+#endif
 
-Context::Context() : data_(std::make_shared<Data>()) {}
+Context::Context() : data_(std::make_shared<Data>()) {
+#ifdef USE_GLOG
+#if defined(_WIN32) || defined(_WIN64) || defined(__APPLE__)
+#ifdef _MSC_VER
+  mindspore::mindspore_log_init();
+#endif
+#else
+  mindspore::mindspore_log_init();
+#endif
+#endif
+}
 
 template <class T, typename U = std::remove_cv_t<std::remove_reference_t<T>>>
 static const U &GetValue(const std::shared_ptr<DeviceInfoContext::Data> &data, const std::string &key) {
