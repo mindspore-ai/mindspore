9a4750c82c5e04dfd6297b43fc26b2a51ff8055b
yanghaoran <yanghaoran2@huawei.com>
2022-11-11 01:19:21 +0000

    回退 'Pull Request !44265 : PyNative Async Queue'

--- mindspore/ccsrc/runtime/pynative/op_executor.cc
+++ mindspore/ccsrc/runtime/pynative/op_executor.cc
@@ -15,6 +15,9 @@
  */
 
 #include "runtime/pynative/op_executor.h"
+#if !defined(_WIN32) && !defined(_WIN64) && !defined(__APPLE__)
+#include "include/common/utils/signal_util.h"
+#endif
 
 namespace mindspore::runtime {
 OpExecutor &OpExecutor::GetInstance() {
@@ -22,20 +25,35 @@ OpExecutor &OpExecutor::GetInstance() {
   return instance;
 }
 
-OpExecutor::OpExecutor() = default;
+OpExecutor::OpExecutor() { worker_ = std::make_shared<std::thread>(&OpExecutor::WorkerLoop, this); }
 
-OpExecutor::~OpExecutor() = default;
+OpExecutor::~OpExecutor() { WorkerJoin(); }
 
-void OpExecutor::Register(const std::function<void()> &callback) { batch_build_callback_ = callback; }
+void OpExecutor::Register(const std::function<void()> &callback) {
+  batch_build_callback_ = callback;
+  registered_ = true;
+}
 
 void OpExecutor::Reset() {
   ClearResources();
   batch_build_callback_ = nullptr;
-  async_queue_.Reset();
+  registered_ = false;
+
+  // There is still one task in progress
+  try {
+    WaitForRun();
+  } catch (const std::exception &e) {
+    MS_LOG(ERROR) << "Wait failed, error message:" << e.what();
+  } catch (...) {
+    MS_LOG(ERROR) << "Wait failed";
+  }
 }
 
 void OpExecutor::ClearResources() {
   MS_LOG(DEBUG) << "Start clear tasks";
+  std::lock_guard<std::mutex> lock(task_mutex_);
+  ClearRunOpTasks();
+
   // Set the build task failed, and no need to run op_run_tasks.
   for (auto &build_task : op_build_tasks_) {
     build_task->SetBuildReady(false);
@@ -55,7 +73,9 @@ void OpExecutor::WaitForBuild() {
 
 void OpExecutor::WaitForRun() {
   MS_LOG(DEBUG) << "Start";
-  async_queue_.Wait();
+  std::unique_lock<std::mutex> lock(task_mutex_);
+  task_cond_var_.wait(lock, [this]() { return op_run_tasks_.empty(); });
+  MsException::Instance().CheckException();
   MS_LOG(DEBUG) << "All task finish";
 }
 
@@ -64,16 +84,20 @@ void OpExecutor::Wait() {
   WaitForRun();
 }
 
-void OpExecutor::PushOpBuildTask(const std::shared_ptr<pynative::BackendOpBuildTask> &op_build_task) {
+void OpExecutor::PushOpBuildTask(const std::shared_ptr<OpBuildTask> &op_build_task) {
+  std::lock_guard<std::mutex> lock(task_mutex_);
   op_build_tasks_.push_back(op_build_task);
 }
 
-void OpExecutor::PushOpRunTask(const std::shared_ptr<pynative::BackendOpRunTask> &op_run_task) {
-  async_queue_.Push(op_run_task);
+void OpExecutor::PushOpRunTask(const std::shared_ptr<OpTask> &op_run_task) {
+  std::lock_guard<std::mutex> lock(task_mutex_);
+  op_run_tasks_.push(op_run_task);
   (void)actor_in_queue_.insert(op_run_task->context()->graph_id());
+  task_cond_var_.notify_all();
 }
 
 void OpExecutor::ClearOpBuildTasks() {
+  std::lock_guard<std::mutex> lock(task_mutex_);
   for (auto &task : op_build_tasks_) {
     task->SetBuildReady(true);
   }
@@ -81,16 +105,101 @@ void OpExecutor::ClearOpBuildTasks() {
   MS_LOG(DEBUG) << "Clear build task";
 }
 
-bool OpExecutor::BuildQueueEmpty() { return op_build_tasks_.empty(); }
+bool OpExecutor::BuildQueueEmpty() {
+  std::lock_guard<std::mutex> lock(task_mutex_);
+  return op_build_tasks_.empty();
+}
 
-bool OpExecutor::RunQueueEmpty() { return async_queue_.Empty(); }
+bool OpExecutor::RunQueueEmpty() {
+  std::lock_guard<std::mutex> lock(task_mutex_);
+  return op_run_tasks_.empty();
+}
 
-bool OpExecutor::BuildQueueFull() { return op_build_tasks_.size() > kMaxQueueSize; }
+bool OpExecutor::BuildQueueFull() {
+  std::lock_guard<std::mutex> lock(task_mutex_);
+  return op_build_tasks_.size() > kMaxQueueSize;
+}
 
 bool OpExecutor::ActorInQueue(GraphId graph_id) {
+  std::lock_guard<std::mutex> lock(task_mutex_);
   auto iter = actor_in_queue_.find(graph_id);
   return iter != actor_in_queue_.end();
 }
 
-void OpExecutor::WorkerJoin() { async_queue_.WorkerJoin(); }
+void OpExecutor::ClearRunOpTasks() {
+  actor_in_queue_.clear();
+  std::queue<std::shared_ptr<OpTask>> empty;
+  // No need to worry about ExitOpTask.
+  // ClearRunOpTasks is executed before ~OpExecutor
+  std::swap(op_run_tasks_, empty);
+}
+
+void OpExecutor::WorkerLoop() {
+#if !defined(_WIN32) && !defined(_WIN64) && !defined(__APPLE__)
+  SignalGuard sig([](int, siginfo_t *, void *) {
+    int this_pid = getpid();
+    MS_LOG(WARNING) << "Process " << this_pid << " receive KeyboardInterrupt signal.";
+    (void)kill(this_pid, SIGTERM);
+  });
+#endif
+
+  while (true) {
+    std::shared_ptr<OpTask> task;
+    {
+      MS_LOG(DEBUG) << "Wait task in queue";
+      std::unique_lock<std::mutex> lock(task_mutex_);
+      task_cond_var_.wait(lock, [this]() { return !op_run_tasks_.empty(); });
+      task = op_run_tasks_.front();
+    }
+
+    MS_LOG(DEBUG) << "Get task";
+    MS_EXCEPTION_IF_NULL(task);
+    if (task->task_type() == kExitTask) {
+      MS_LOG(DEBUG) << "Thread exit";
+      return;
+    }
+    try {
+      task->Run();
+      std::unique_lock<std::mutex> lock(task_mutex_);
+      if (!op_run_tasks_.empty()) {
+        op_run_tasks_.pop();
+        (void)actor_in_queue_.erase(task->context()->graph_id());
+      }
+
+      if (op_run_tasks_.empty()) {
+        MS_LOG(DEBUG) << "Task queue empty";
+        task_cond_var_.notify_all();
+      }
+    } catch (const std::exception &e) {
+      MS_LOG(ERROR) << "Run lazy task failed, error message:" << e.what();
+      {
+        std::unique_lock<std::mutex> lock(task_mutex_);
+        ClearRunOpTasks();
+        MsException::Instance().SetException();
+        task_cond_var_.notify_all();
+      }
+    }
+  }
+}
+
+void OpExecutor::WorkerJoin() {
+  try {
+    // Avoid worker thread join itself which will cause deadlock
+    if (worker_->joinable() && worker_->get_id() != std::this_thread::get_id()) {
+      {
+        std::lock_guard<std::mutex> lock(task_mutex_);
+        auto task = std::make_shared<ExitOpTask>();
+        op_run_tasks_.push(task);
+        task_cond_var_.notify_all();
+        MS_LOG(DEBUG) << "Push exit task and notify all";
+      }
+      worker_->join();
+      MS_LOG(DEBUG) << "Worker join finish";
+    }
+  } catch (const std::exception &e) {
+    MS_LOG(ERROR) << "WorkerJoin failed: " << e.what();
+  } catch (...) {
+    MS_LOG(ERROR) << "WorkerJoin failed";
+  }
+}
 }  // namespace mindspore::runtime
