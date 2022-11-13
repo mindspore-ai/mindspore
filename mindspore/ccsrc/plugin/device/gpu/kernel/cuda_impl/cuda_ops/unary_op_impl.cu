1085bb45e1b2a1ef812040b9524630a0a4978231
hj-ustb <hujing_hjhj@163.com>
2022-11-13 11:20:35 +0800

    [feat][assistant][ops][I5EWOO] add data type for sigmoidgrad

--- mindspore/ccsrc/plugin/device/gpu/kernel/cuda_impl/cuda_ops/unary_op_impl.cu
+++ mindspore/ccsrc/plugin/device/gpu/kernel/cuda_impl/cuda_ops/unary_op_impl.cu
@@ -1828,13 +1828,13 @@ template CUDA_LIB_EXPORT void Sigmoid<Complex<float>>(const Complex<float> *inpu
                                                       const size_t count, cudaStream_t cuda_stream);
 template CUDA_LIB_EXPORT void Square<Complex<float>>(const Complex<float> *input, Complex<float> *output,
                                                      const size_t count, cudaStream_t cuda_stream);
-
 template CUDA_LIB_EXPORT void Tanh<Complex<float>>(const Complex<float> *input, Complex<float> *output,
                                                    const size_t count, cudaStream_t cuda_stream);
 template CUDA_LIB_EXPORT void Logarithm<Complex<float>>(const Complex<float> *input, Complex<float> *output,
                                                         const size_t count, cudaStream_t cuda_stream);
 template CUDA_LIB_EXPORT void Negative<Complex<float>>(const Complex<float> *input, Complex<float> *output,
                                                        const size_t count, cudaStream_t cuda_stream);
+
 // complex128
 template CUDA_LIB_EXPORT void Exponential<Complex<double>>(const Complex<double> *input, Complex<double> *output,
                                                            const size_t count, cudaStream_t cuda_stream);
@@ -1880,6 +1880,7 @@ template CUDA_LIB_EXPORT void Logarithm<Complex<double>>(const Complex<double> *
                                                          const size_t count, cudaStream_t cuda_stream);
 template CUDA_LIB_EXPORT void Negative<Complex<double>>(const Complex<double> *input, Complex<double> *output,
                                                         const size_t count, cudaStream_t cuda_stream);
+
 // bool
 template CUDA_LIB_EXPORT void Real<bool>(const bool *input, bool *output, const size_t count, cudaStream_t cuda_stream);
 template CUDA_LIB_EXPORT void Imag<bool>(const bool *input, bool *output, const size_t count, cudaStream_t cuda_stream);
