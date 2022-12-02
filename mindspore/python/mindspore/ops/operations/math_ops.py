8564e343981e97357ca489fd5a5f7015b4ef8b0e
shaw_zhang <xiaohan.zhang@huawei.com>
2022-12-02 15:58:44 +0800

    Fix FFTWithSize typo

--- mindspore/python/mindspore/ops/operations/math_ops.py
+++ mindspore/python/mindspore/ops/operations/math_ops.py
@@ -7143,7 +7143,7 @@ class FFTWithSize(Primitive):
         ValueError: If norm is none of "backward", "forward" or "ortho".
 
     Supported Platforms:
-        ``GPU`` ``CPU``
+        ``CPU``
 
     Examples:
           >>> # case FFT: signal_ndim: 1, inverse: False, real: False.
