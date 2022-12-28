1989ecebcbbebc0abe5f46550cd85e062ee264b8
chenfei <chenfei52@huawei.com>
2022-12-28 14:51:40 +0800

    add gitee url of robin hood

--- cmake/external_libs/robin.cmake
+++ cmake/external_libs/robin.cmake
@@ -1,5 +1,10 @@
-set(REQ_URL "https://github.com/martinus/robin-hood-hashing/archive/3.11.5.zip")
-set(MD5 "35154dc71e47762d9b56b2114bc906ca")
+if(ENABLE_GITEE)
+    set(REQ_URL "https://gitee.com/mirrors/robin-hood-hashing/repository/archive/3.11.5.zip")
+    set(MD5 "b1f36d958f0bd75671b43ccf4685a5be")
+else()
+    set(REQ_URL "https://github.com/martinus/robin-hood-hashing/archive/3.11.5.zip")
+    set(MD5 "35154dc71e47762d9b56b2114bc906ca")
+endif()
 set(INCLUDE "./src")
 
 mindspore_add_pkg(robin_hood_hashing
