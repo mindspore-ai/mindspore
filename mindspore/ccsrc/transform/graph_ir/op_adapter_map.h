9643f7bde7cf649c048cec1f4cb471b94a4fe7b8
jiaorui <jiaorui3@huawei.com>
2023-01-11 11:02:34 +0800

    ms identity map to ge

--- mindspore/ccsrc/transform/graph_ir/op_adapter_map.h
+++ mindspore/ccsrc/transform/graph_ir/op_adapter_map.h
@@ -407,6 +407,7 @@ constexpr const char kNameTupleGetItem[] = "TupleGetItem";
 constexpr const char kNameLoad[] = "Load";
 constexpr const char kNameDepend[] = "Depend";
 constexpr const char kNameReturn[] = "Return";
+constexpr const char kNameIdentity[] = "Identity";
 constexpr const char kNameUpdateState[] = "UpdateState";
 constexpr const char kNameTransData[] = "TransData";
 constexpr const char kNameWhere[] = "Where";
