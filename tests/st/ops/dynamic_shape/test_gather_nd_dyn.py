24062d1b42114ecd855bef2b045ad8c1b47a5b72
yanghaoran <yanghaoran2@huawei.com>
2023-01-19 10:35:43 +0800

    takedown testcases to ensure gate stability

--- tests/st/ops/dynamic_shape/test_gather_nd_dyn.py
+++ tests/st/ops/dynamic_shape/test_gather_nd_dyn.py
@@ -62,7 +62,7 @@ def test_gather_nd_dyn_cpu():
     dyn_case()
 
 
-@pytest.mark.level0
+@pytest.mark.level1
 @pytest.mark.platform_arm_ascend_training
 @pytest.mark.platform_x86_ascend_training
 @pytest.mark.env_onecard
--- tests/st/ops/dynamic_shape/test_resize_bilinear_grad_dyn.py
+++ tests/st/ops/dynamic_shape/test_resize_bilinear_grad_dyn.py
@@ -52,7 +52,7 @@ def dyn_case():
     assert np.all(output.asnumpy() == expect)
 
 
-@pytest.mark.level0
+@pytest.mark.level1
 @pytest.mark.platform_arm_ascend_training
 @pytest.mark.platform_x86_ascend_training
 @pytest.mark.env_onecard
