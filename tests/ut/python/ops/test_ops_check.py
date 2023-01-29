fix broadcast check of batchmatmul
Signed-off-by: zhoufeng <zhoufeng54@huawei.com>

tests/ut/python/ops/test_ops_check.py
@@ -180,6 +180,21 @@ test_case_check_ops = [
     ('BatchMatMul', {
         'block': NetBatchMatMul(),
         'desc_inputs': [Tensor(np.ones(shape=[3, 1, 5])), Tensor(np.ones(shape=[3, 5, 4]))]}),
+    ('BatchMatMul_broadcast_1', {
+        'block': NetBatchMatMul(),
+        'desc_inputs': [Tensor(np.ones(shape=[3, 1, 5])), Tensor(np.ones(shape=[5, 4]))]}),
+    ('BatchMatMul_broadcast_2', {
+        'block': NetBatchMatMul(),
+        'desc_inputs': [Tensor(np.ones(shape=[3, 1, 5])), Tensor(np.ones(shape=[1, 5, 4]))]}),
+    ('BatchMatMul_broadcast_3', {
+        'block': NetBatchMatMul(),
+        'desc_inputs': [Tensor(np.ones(shape=[2, 1, 1, 5])), Tensor(np.ones(shape=[1, 2, 5, 4]))]}),
+    ('BatchMatMul_broadcast_4', {
+        'block': NetBatchMatMul(),
+        'desc_inputs': [Tensor(np.ones(shape=[2, 2, 1, 1, 5])), Tensor(np.ones(shape=[1, 2, 5, 4]))]}),
+    ('BatchMatMul_broadcast_5', {
+        'block': NetBatchMatMul(),
+        'desc_inputs': [Tensor(np.ones(shape=[3, 1, 5])), Tensor(np.ones(shape=[1, 3, 5, 4]))]}),
 ]
 
 test_case_lists = [test_case_check_ops]
@@ -250,9 +265,6 @@ raise_set = [
         'block': (P.BatchMatMul(), {'exception': ValueError}),
         'desc_inputs': [Tensor(np.ones(shape=[3, 1, 5])), Tensor(np.ones(shape=[3, 3, 4]))]}),
     ('BatchMatMul_4_Error', {
-        'block': (P.BatchMatMul(), {'exception': ValueError}),
-        'desc_inputs': [Tensor(np.ones(shape=[3, 1, 5])), Tensor(np.ones(shape=[1, 3, 5, 4]))]}),
-    ('BatchMatMul_5_Error', {
         'block': (P.BatchMatMul(), {'exception': ValueError}),
         'desc_inputs': [Tensor(np.ones(shape=[3, 1, 5])), Tensor(np.ones(shape=[2, 5, 4]))]}),
 ]