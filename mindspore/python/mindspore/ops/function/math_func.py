25ebfe6c5f50515d492f61a1dcb54e5ad75bca4e
shaojunsong <shaojunsong@huawei.com>
2023-01-30 17:21:33 +0800

    fix ops.inner

--- mindspore/python/mindspore/ops/function/math_func.py
+++ mindspore/python/mindspore/ops/function/math_func.py
@@ -7931,8 +7931,8 @@ def matmul(x1, x2):
 
 def inner(x, other):
     r"""
-    Computes the dot product of 1D tensors. For higher dimensions, the result will be the summation of the element-wise
-    production along their last dimension.
+    Computes the dot product of 1D tensors. For higher dimensions, the result will be the summation of the elemental
+    wise production along their last dimension.
 
     Note:
         If either `x` or `other` is a Tensor scalar, the result is equivalent to mindspore.mul(x, other).
@@ -7975,19 +7975,20 @@ def inner(x, other):
     """
     x_dim = x.ndim
     other_dim = other.ndim
+    x_dtype = x.dtype
 
     if x_dim == 0 or other_dim == 0:
-        return x * other
-
-    if x_dim == 1 or other_dim == 1:
-        return matmul(x, other)
+        output = x * other
+        if output.dtype != x.dtype:
+            return output.astype(x_dtype)
+        return output
 
     x_shape = x.shape
     other_shape = other.shape
     if x_shape[-1] != other_shape[-1]:
         raise ValueError(f"For 'inner', the last dimension of 'x' and 'other' must be the same, \
                          but got x.shape: {x_shape} and other.shape: {other_shape}.")
-    return matmul(x, other.swapaxes(-2, -1))
+    return C.tensor_dot(x, other, axes=(-1, -1))
 
 
 def bmm(input_x, mat2):
--- tests/st/ops/test_func_inner.py
+++ tests/st/ops/test_func_inner.py
@@ -94,3 +94,32 @@ def test_inner_1d(mode):
     out = net(x, y)
     expect_out = np.array(32, dtype=np.float32)
     assert np.allclose(out.asnumpy(), expect_out)
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.platform_arm_cpu
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_ascend_training
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('mode', [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
+def test_inner_multidims(mode):
+    """
+    Feature: ops.inner
+    Description: Verify the result of ops.inner
+    Expectation: success
+    """
+    ms.set_context(mode=mode)
+    net = Net()
+    x = ms.Tensor([[0.8173, 1.0874, 1.1784], [0.3279, 0.1234, 2.7894]], dtype=ms.float32)
+    y = ms.Tensor([[[-0.4682, -0.7159, 0.1506], [0.4034, -0.3657, 1.0387],
+                    [0.9892, -0.6684, 0.1774], [0.9482, 1.3261, 0.3917]],
+                   [[0.4537, 0.7493, 1.1724], [0.2291, 0.5749, -0.2267],
+                    [-0.7920, 0.3607, -0.3701], [1.3666, -0.5850, -1.7242]]], dtype=ms.float32)
+    out = net(x, y)
+    expect_out = np.array([[[-0.9836624, 1.1560408, 0.29070318, 2.6785443],
+                            [2.567154, 0.54524636, -0.6912023, -1.5510042]],
+                           [[0.17821884, 2.9844973, 0.7367177, 1.5671635],
+                            [3.5115247, -0.48629245, -1.2475433, -4.433564]]], dtype=np.float32)
+    assert np.allclose(out.asnumpy(), expect_out)
