e845349ed9ec18c868a693d52933b6813a8b7b7a
zhou_chao1993 <zhouchao46@huawei.com>
2023-01-31 09:47:58 +0800

    modify data type of depth_to_space

--- mindspore/ccsrc/plugin/device/cpu/kernel/nnacl/base/depth_to_space_base.c
+++ mindspore/ccsrc/plugin/device/cpu/kernel/nnacl/base/depth_to_space_base.c
@@ -23,17 +23,17 @@ void DepthToSpaceForNHWC(const void *input, void *output, const int *in_shape, c
   int32_t in_shape_dim1 = in_shape[1];
   size_t copy_size = (size_t)block_size * param->out_stride_dim2_ * param->data_type_size_;
   for (int i = 0; i < in_shape[0]; ++i) {
-    int in_offset_n = i * param->in_stride_dim0_;
-    int out_offset_n = i * param->out_stride_dim0_;
+    int64_t in_offset_n = i * param->in_stride_dim0_;
+    int64_t out_offset_n = i * param->out_stride_dim0_;
     for (int j = 0; j < in_shape_dim1; ++j) {
-      int in_offset_h = in_offset_n + j * param->in_stride_dim1_;
-      int out_offset_h = out_offset_n + j * block_size * param->out_stride_dim1_;
+      int64_t in_offset_h = in_offset_n + j * param->in_stride_dim1_;
+      int64_t out_offset_h = out_offset_n + j * block_size * param->out_stride_dim1_;
       for (int k = 0; k < in_shape_dim2; ++k) {
-        int in_offset_w = in_offset_h + k * param->in_stride_dim2_;
-        int out_offset_w = out_offset_h + k * block_size * param->out_stride_dim2_;
+        int64_t in_offset_w = in_offset_h + k * param->in_stride_dim2_;
+        int64_t out_offset_w = out_offset_h + k * block_size * param->out_stride_dim2_;
         for (int l = 0; l < block_size; ++l) {
-          int out_offset = (out_offset_w + l * param->out_stride_dim1_) * param->data_type_size_;
-          int in_offset = (in_offset_w + l * block_size * param->out_stride_dim2_) * param->data_type_size_;
+          int64_t out_offset = (out_offset_w + l * param->out_stride_dim1_) * param->data_type_size_;
+          int64_t in_offset = (in_offset_w + l * block_size * param->out_stride_dim2_) * param->data_type_size_;
           memcpy((int8_t *)output + out_offset, (int8_t *)input + in_offset, copy_size);
         }
       }
@@ -48,22 +48,22 @@ void DepthToSpaceCRDForNHWC(const void *input, void *output, const int *in_shape
   int32_t in_shape_dim1 = in_shape[1];
   size_t copy_size = param->data_type_size_;
   for (int i = 0; i < in_shape[0]; ++i) {
-    int in_offset_n = i * param->in_stride_dim0_;
-    int out_offset_n = i * param->out_stride_dim0_;
+    int64_t in_offset_n = i * param->in_stride_dim0_;
+    int64_t out_offset_n = i * param->out_stride_dim0_;
     for (int j = 0; j < in_shape_dim1; ++j) {
-      int in_offset_h = in_offset_n + j * param->in_stride_dim1_;
-      int out_offset_h = out_offset_n + j * block_size * param->out_stride_dim1_;
+      int64_t in_offset_h = in_offset_n + j * param->in_stride_dim1_;
+      int64_t out_offset_h = out_offset_n + j * block_size * param->out_stride_dim1_;
       for (int k = 0; k < in_shape_dim2; ++k) {
-        int in_offset_w = in_offset_h + k * param->in_stride_dim2_;
-        int out_offset_w = out_offset_h + k * block_size * param->out_stride_dim2_;
+        int64_t in_offset_w = in_offset_h + k * param->in_stride_dim2_;
+        int64_t out_offset_w = out_offset_h + k * block_size * param->out_stride_dim2_;
         for (int l = 0; l < in_shape_dim3; ++l) {
-          int offset = l % (block_size * block_size);
-          int out_offset_c =
+          int64_t offset = l % (block_size * block_size);
+          int64_t out_offset_c =
             out_offset_w +
             offset / block_size * block_size * in_shape_dim2 * in_shape_dim3 / (block_size * block_size) +
             offset % block_size * in_shape_dim3 / (block_size * block_size);
-          int out_offset = (out_offset_c + l / (block_size * block_size)) * param->data_type_size_;
-          int in_offset = (in_offset_w + l) * param->data_type_size_;
+          int64_t out_offset = (out_offset_c + l / (block_size * block_size)) * param->data_type_size_;
+          int64_t in_offset = (in_offset_w + l) * param->data_type_size_;
           memcpy((int8_t *)output + out_offset, (int8_t *)input + in_offset, copy_size);
         }
       }
