ffb9ebdd75ca5be7cf98bc5bcc60bc1c09971e95
emmmmtang <tangcong9@huawei.com>
2023-02-11 15:46:40 +0800

    fix openssl CVE

--- cmake/external_libs/openssl.cmake
+++ cmake/external_libs/openssl.cmake
@@ -28,7 +28,15 @@ if(BUILD_LITE)
                 CONFIGURE_COMMAND ./Configure android-arm64 -D__ANDROID_API__=29 no-zlib
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-3711.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-3712.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-4160.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-0778.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-1292.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-2068.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-2097.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4304.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4450.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0215.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0286.patch
                 )
     elseif(PLATFORM_ARM32 AND ANDROID_NDK_TOOLCHAIN_INCLUDED)
         set(ANDROID_NDK_ROOT $ENV{ANDROID_NDK})
@@ -44,7 +52,15 @@ if(BUILD_LITE)
                 CONFIGURE_COMMAND ./Configure android-arm -D__ANDROID_API__=19 no-zlib
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-3711.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-3712.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-4160.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-0778.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-1292.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-2068.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-2097.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4304.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4450.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0215.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0286.patch
                 )
     elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux" OR APPLE)
         mindspore_add_pkg(openssl
@@ -55,7 +71,15 @@ if(BUILD_LITE)
                 CONFIGURE_COMMAND ./config no-zlib
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-3711.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-3712.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-4160.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-0778.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-1292.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-2068.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-2097.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4304.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4450.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0215.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0286.patch
                 )
     else()
         MESSAGE(FATAL_ERROR "openssl does not support compilation for the current environment.")
@@ -74,7 +98,15 @@ else()
                 CONFIGURE_COMMAND ./config no-zlib no-shared
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-3711.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-3712.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2021-4160.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-0778.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-1292.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-2068.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-2097.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4304.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4450.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0215.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0286.patch
                 )
         include_directories(${openssl_INC})
         add_library(mindspore::ssl ALIAS openssl::ssl)
