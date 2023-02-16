37bb13823f897be1c7109b5a5df178057dbff714
zhou_chao1993 <zhouchao46@huawei.com>
2023-02-16 23:52:47 +0800

    modify ops reverseV2 doc

--- mindspore/python/mindspore/ops/operations/array_ops.py
+++ mindspore/python/mindspore/ops/operations/array_ops.py
@@ -2897,6 +2897,7 @@ class ReverseV2(Primitive):
     Raises:
         TypeError: If `axis` is neither list nor tuple.
         TypeError: If element of `axis` is not an int.
+        ValueError: There are multiple identical axes in `axis`.
 
     Supported Platforms:
         ``Ascend`` ``GPU`` ``CPU``
