b45df8238d3d2d0368ce66968a53db094ba5fbbd
hedongdong <hedongdong@huawei.com>
2023-02-23 18:42:43 +0800

    update testcase levels for index_fill and print

--- tests/st/ops/dynamic_shape/grad/test_index_fill.py
+++ tests/st/ops/dynamic_shape/grad/test_index_fill.py
@@ -32,7 +32,7 @@ class IndexFillNet(nn.Cell):
         return out
 
 
-@pytest.mark.level1
+@pytest.mark.level0
 @pytest.mark.env_onecard
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_x86_gpu_training
@@ -54,7 +54,7 @@ def test_dynamic_shape_index_fill():
         [Tensor(x_np), dim, Tensor(index_np), value], False)
 
 
-@pytest.mark.level1
+@pytest.mark.level0
 @pytest.mark.env_onecard
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_x86_gpu_training
