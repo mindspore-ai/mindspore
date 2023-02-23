4d68408583d2b242b4ede0f345beeebc46514c75
zhupuxu <zhupuxu@huawei.com>
2023-02-23 09:43:06 +0800

    delete testcase
    Signed-off-by: zhupuxu <zhupuxu@huawei.com>

--- tests/st/ops/ascend/test_aicpu_ops/test_deformable_offsets_grad.py
+++ tests/st/ops/ascend/test_aicpu_ops/test_deformable_offsets_grad.py
@@ -14,84 +14,12 @@
 # ============================================================================
 import pytest
 import numpy as np
-
+import mindspore.context as context
 from mindspore.ops.operations import _grad_ops as G
-from mindspore.ops import composite as C
 from mindspore import nn
 from mindspore import Tensor
-from mindspore import dtype
-from mindspore.ops.operations import nn_ops
 
-grad_all = C.GradOperation(get_all=True)
-
-
-class TestNetwork(nn.Cell):
-    def __init__(self):
-        super(TestNetwork, self).__init__()
-        stride = (1, 1, 1, 1)
-        pad = (0, 0, 0, 0)
-        ksize = (2, 2)
-        self.deformable_offsets_grad_op = G.DeformableOffsetsGrad(stride, pad, ksize)
-
-    def construct(self, dout, x, offsets):
-        output = self.deformable_offsets_grad_op(dout, x, offsets)
-        return output
-
-
-def test_grad_infer():
-    """
-    Feature: Aicpu operation.
-    Description: Test of Aicpu operation: DeformableOffsetsGrad
-    Expectation: No exception raised.
-    """
-    dout = Tensor(np.ones([1, 1, 2, 2]), dtype.float32)
-    x = Tensor(np.ones([1, 1, 2, 2]), dtype.float32)
-    offsets = Tensor(np.array([0.1] * 12).astype(np.float32).reshape([1, 12, 1, 1]))
-
-    net = TestNetwork()
-    grad = net(dout, x, offsets)
-    print("grad_x:", grad[0])
-    print("grad_offset:", grad[1])
-    return grad
-
-
-class ForwardNet(nn.Cell):
-    def __init__(self):
-        super(ForwardNet, self).__init__()
-        stride = (1, 1, 1, 1)
-        pad = (0, 0, 0, 0)
-        ksize = (2, 2)
-        self.deformable_offsets_grad_op = nn_ops.DeformableOffsets(stride, pad, ksize)
-
-    def construct(self, x, offsets):
-        output = self.deformable_offsets_grad_op(x, offsets)
-        return output
-
-
-class BackwardNet(nn.Cell):
-    def __init__(self, net):
-        super(BackwardNet, self).__init__()
-        self.net = net
-
-    def construct(self, *inputs):
-        out = self.net(*inputs)
-        return out, grad_all(self.net)(*inputs)
-
-
-def test_auto_diff():
-    """
-    Feature: Aicpu operation.
-    Description: Test of Aicpu operation: DeformableOffsetsGrad by auto diff.
-    Expectation: No exception raised.
-    """
-    x = Tensor(np.ones([1, 1, 2, 2]), dtype.float32)
-    offsets = Tensor(np.array([0.1] * 12).astype(np.float32).reshape([1, 12, 1, 1]))
-    forward_net = ForwardNet()
-    net = BackwardNet(forward_net)
-    grad = net(x, offsets)
-    print("grad_x:", grad[0])
-    print("grad_offset:", grad[1])
-    return grad
+context.set_context(mode=context.GRAPH_MODE, device_target="Ascend")
 
 
 class NetDeformableOffsetsGrad(nn.Cell):
