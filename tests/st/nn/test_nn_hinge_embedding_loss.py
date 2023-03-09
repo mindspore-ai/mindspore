d280c6ca1145b0a4994e3b180dfb3c1ab521c77a
yanghaoran <yanghaoran2@huawei.com>
2023-03-09 09:44:22 +0800

    takedownt testcases to keep CI stable

--- tests/st/nn/test_nn_hinge_embedding_loss.py
+++ tests/st/nn/test_nn_hinge_embedding_loss.py
@@ -16,7 +16,7 @@ class Net(nn.Cell):
         return loss
 
 
-@pytest.mark.level0
+@pytest.mark.level1
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_arm_cpu
 @pytest.mark.platform_x86_gpu_training
