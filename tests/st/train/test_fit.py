987403c24370d79fb3e33d48631a95db93b508ae
yanghaoran <yanghaoran2@huawei.com>
2023-03-10 10:09:53 +0800

    takedown randomly failing testcases

--- tests/st/train/test_fit.py
+++ tests/st/train/test_fit.py
@@ -89,7 +89,7 @@ def test_fit_train_dataset_non_sink_mode(mode):
     r.flush()
 
 
-@pytest.mark.level0
+@pytest.mark.level1
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_arm_cpu
 @pytest.mark.platform_x86_gpu_training
