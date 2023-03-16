79c2f4ea3cdc25caada0eff5efcd32b083135a77
yeyunpeng2020 <yeyunpeng1@huawei.com>
2023-03-16 17:37:48 +0800

    fix MarkOriginDataType bug

--- mindspore/lite/tools/converter/adapter/acl/src/acl_pass_impl.cc
+++ mindspore/lite/tools/converter/adapter/acl/src/acl_pass_impl.cc
@@ -173,6 +173,11 @@ STATUS AclPassImpl::CommonPass(const FuncGraphPtr &func_graph) {
     MS_LOG(INFO) << "Ms model no need to run const fold pass.";
     return lite::RET_OK;
   }
+  // Quantization dynamic model must set inputShape for calibration.
+  if (is_ascend_quant_ && IsDynamicInput()) {
+    MS_LOG(INFO) << "Dynamic input no need to run const fold pass.";
+    return lite::RET_OK;
+  }
   if (!lite::RunOptimizerPass(func_graph, {kConstFoldPass})) {
     MS_LOG(ERROR) << "Const fold pass failed.";
     return lite::RET_ERROR;
