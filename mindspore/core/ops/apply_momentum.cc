c2782468565f5b0d91a4dca495ad9fc45beda53a
yuchaojie <yuchaojie1@huawei.com>
2023-03-18 10:54:08 +0800

    code check clean

--- mindspore/core/ops/apply_momentum.cc
+++ mindspore/core/ops/apply_momentum.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020-2021 Huawei Technologies Co., Ltd
+ * Copyright 2020-2023 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,6 @@
 #include <utility>
 #include <map>
 #include <set>
-#include <type_traits>
 
 #include "utils/check_convert_utils.h"
 #include "abstract/abstract_value.h"
--- mindspore/core/ops/apply_power_sign_d.cc
+++ mindspore/core/ops/apply_power_sign_d.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright 2021 Huawei Technologies Co., Ltd
+ * Copyright 2021-2023 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,6 @@
 #include <map>
 #include <string>
 #include <utility>
-#include <type_traits>
 
 #include "utils/check_convert_utils.h"
 #include "abstract/abstract_value.h"
