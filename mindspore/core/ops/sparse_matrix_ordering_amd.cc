e610822279842f2c520f35c115324ae6d643d446
lby <liubuyu1@huawei.com>
2023-03-22 09:43:57 +0800

    code clean

--- mindspore/core/ops/sparse_matrix_ordering_amd.cc
+++ mindspore/core/ops/sparse_matrix_ordering_amd.cc
@@ -20,19 +20,7 @@
 #include <set>
 
 #include "abstract/ops/primitive_infer_map.h"
-#include "abstract/dshape.h"
-#include "abstract/ops/op_infer.h"
-#include "abstract/utils.h"
-#include "base/base.h"
-#include "ir/dtype/number.h"
-#include "ir/dtype/tensor_type.h"
-#include "ir/primitive.h"
-#include "mindapi/base/shape_vector.h"
-#include "ops/core_ops.h"
 #include "ops/op_name.h"
-#include "ops/primitive_c.h"
-#include "utils/check_convert_utils.h"
-#include "utils/log_adapter.h"
 #include "mindapi/src/helper.h"
 
 namespace mindspore {
