d2dd1e19ab5ab748f0e314fda35899a67ed403bf
ZPaC <zhoupeichen@huawei.com>
2023-03-22 11:22:00 +0800

    Add dependency for send recv to fix hang in ascend

--- mindspore/ccsrc/frontend/parallel/graph_util/graph_splitter.h
+++ mindspore/ccsrc/frontend/parallel/graph_util/graph_splitter.h
@@ -40,6 +40,7 @@
 namespace mindspore {
 namespace parallel {
 using distributed::cluster::ClusterContext;
+using AnfNodePtrSet = std::set<AnfNodePtr>;
 
 constexpr char kEnvNeedFusion[] = "fusion";
 
@@ -293,6 +294,18 @@ ValueNodePtr CreateUMonadNode();
  * @return {CNodePtr}: UpdateState node.
  */
 CNodePtr CreateUpdateStateNode(const FuncGraphPtr &func_graph, const AnfNodePtrList &update_state_inputs);
+
+// Filter out 'func_graph' nodes' dependency matrix to the specified target nodes set.
+std::map<AnfNodePtr, AnfNodePtrSet> FilterDependencyToTargetNode(const FuncGraphPtr &func_graph,
+                                                                 const AnfNodePtrSet &target_nodes);
+
+// After a new node is added, the depended set should be updated to keep the minimal dependencies.
+AnfNodePtrSet UpdateDependedSet(const AnfNodePtr &new_node, const AnfNodePtrSet &old_depended_set,
+                                const std::map<AnfNodePtr, AnfNodePtrSet> &node_dependency);
+
+// Connect hung nodes to output in case they are optimized out.
+void HandleHungNodes(const FuncGraphPtr &func_graph, const NodeLabels &node_labels, OperatorLabel process_label,
+                     const AnfNodePtrList &hung_nodes_list);
 
 // Base class for different execution modes. It builds distributed graphs, optimize execution performance, etc.
 class DistributedExecutionMode {
@@ -582,6 +595,9 @@ class GraphSplitter {
   InOutDegreeList GenerateInOutDegreeList(const std::vector<SplitGraphSegment> &segments,
                                           const InterProcessOpEdgesInfo &comm_edges);
 
+  //  Must add extra dependency edge for RpcSend and RpcRecv nodes in case they are optimized out or lose explicit
+  //  dependencies.
+  void AddDependencyBetweenEdges(const InterProcessOpEdgesInfo &comm_edges);
   // For the segments on this process, dependency edges should be created so that they won't be optimized out.
   void AddDependencyBetweenSegments(const InOutDegreeList &in_out_degree_list);
 
@@ -590,6 +606,11 @@ class GraphSplitter {
 
   // Replace nodes inputs with Recv nodes.
   void ReplaceOriginNodesWithRecv(const FusedInterProcessOpPairMap &fused_inter_process_op_pairs);
+
+  void AddSendRecvDependency(const InterProcessOpEdgesInfo &in_degree_comm_edges, const AnfNodePtrSet &send_src_nodes,
+                             const std::map<AnfNodePtr, AnfNodePtrSet> &src_nodes_to_send_nodes,
+                             const std::map<AnfNodePtr, AnfNodePtrSet> &node_dependency,
+                             std::map<AnfNodePtr, bool> *is_send_node_hung);
 
   // Add outputs edges for send nodes so that they won't be optimized out.
   void AddDependencyForSend(const FusedInterProcessOpPairMap &fused_inter_process_op_pairs);
