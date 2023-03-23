3167dd4247e92fbcc173b652c6ec82cebf2e2b4e
emmmmtang <tangcong9@huawei.com>
2023-03-23 16:22:59 +0800

    fix cve-2023-0464

--- cmake/external_libs/openssl.cmake
+++ cmake/external_libs/openssl.cmake
@@ -37,6 +37,7 @@ if(BUILD_LITE)
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4450.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0215.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0286.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0464.patch
                 )
     elseif(PLATFORM_ARM32 AND ANDROID_NDK_TOOLCHAIN_INCLUDED)
         set(openssl_USE_STATIC_LIBS OFF)
@@ -62,6 +63,7 @@ if(BUILD_LITE)
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4450.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0215.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0286.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0464.patch
                 )
     elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux" OR APPLE)
         set(openssl_CFLAGS -fvisibility=hidden)
@@ -82,6 +84,7 @@ if(BUILD_LITE)
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4450.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0215.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0286.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0464.patch
                 )
     else()
         MESSAGE(FATAL_ERROR "openssl does not support compilation for the current environment.")
@@ -109,6 +112,7 @@ else()
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2022-4450.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0215.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0286.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0464.patch
                 )
         include_directories(${openssl_INC})
         add_library(mindspore::ssl ALIAS openssl::ssl)
