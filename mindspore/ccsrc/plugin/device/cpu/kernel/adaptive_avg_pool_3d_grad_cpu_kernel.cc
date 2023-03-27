309f0ae615d2778d6cfecb93c4ccd48f77049ef7
fanjibin <fanjibin@huawei.com>
2023-03-27 22:34:56 +0800

    fix adaptiveavgpool3d

--- mindspore/ccsrc/plugin/device/cpu/kernel/adaptive_avg_pool_3d_grad_cpu_kernel.cc
+++ mindspore/ccsrc/plugin/device/cpu/kernel/adaptive_avg_pool_3d_grad_cpu_kernel.cc
@@ -164,7 +164,7 @@ bool AdaptiveAvgPool3DGradCPUKernelMod::LaunchKernel(const std::vector<kernel::A
         ParallelLaunchAutoSearch(shard_frame, sub_args.size_d, this, &parallel_search_info_);
       }
     };
-    ParallelLaunchAutoSearch(shard_template, orig_input_shape_data[0], this, &parallel_search_info_);
+    shard_template(0, orig_input_shape_data[0]);
   }
   for (int64_t i = 0; i < output_num; i++) {
     input_data_ptr_ret[i] = static_cast<SCALAR_T>(input_data_ptr[i]);
