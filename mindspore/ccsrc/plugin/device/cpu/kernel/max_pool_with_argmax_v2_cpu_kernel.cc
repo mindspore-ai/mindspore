77a8011dadf795f96780c0a6776056ba7431b7e1
moran <moran2@huawei.com>
2023-03-28 20:28:55 +0800

    fix CodeCheck

--- mindspore/ccsrc/plugin/device/cpu/kernel/max_pool_with_argmax_v2_cpu_kernel.cc
+++ mindspore/ccsrc/plugin/device/cpu/kernel/max_pool_with_argmax_v2_cpu_kernel.cc
@@ -72,7 +72,7 @@ int MaxPoolWithArgmaxV2CpuKernelMod::Resize(const BaseOperatorPtr &base_operator
   return KRET_OK;
 }
 
-std::vector<int64_t> MaxPoolWithArgmaxV2CpuKernelMod::GetValidAttr(const std::vector<int64_t> &src_attr) {
+std::vector<int64_t> MaxPoolWithArgmaxV2CpuKernelMod::GetValidAttr(const std::vector<int64_t> &src_attr) const {
   if (src_attr.size() == DIM_SIZE_1) {
     return {src_attr[kZero], src_attr[kZero]};
   } else if (src_attr.size() == DIM_SIZE_4) {
