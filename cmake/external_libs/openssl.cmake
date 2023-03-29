dfdf1513a9d732b604cc051969fcfabac05813ea
emmmmtang <tangcong9@huawei.com>
2023-03-29 15:26:12 +0800

    fix openssl CVE-2023-0465 0466

--- cmake/external_libs/openssl.cmake
+++ cmake/external_libs/openssl.cmake
@@ -38,6 +38,8 @@ if(BUILD_LITE)
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0215.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0286.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0464.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0465.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0466.patch
                 )
     elseif(PLATFORM_ARM32 AND ANDROID_NDK_TOOLCHAIN_INCLUDED)
         set(openssl_USE_STATIC_LIBS OFF)
@@ -64,6 +66,8 @@ if(BUILD_LITE)
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0215.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0286.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0464.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0465.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0466.patch
                 )
     elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux" OR APPLE)
         set(openssl_CFLAGS -fvisibility=hidden)
@@ -85,6 +89,8 @@ if(BUILD_LITE)
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0215.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0286.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0464.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0465.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0466.patch
                 )
     else()
         MESSAGE(FATAL_ERROR "openssl does not support compilation for the current environment.")
@@ -113,6 +119,8 @@ else()
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0215.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0286.patch
                 PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0464.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0465.patch
+                PATCHES ${OPENSSL_PATCH_ROOT}/CVE-2023-0466.patch
                 )
         include_directories(${openssl_INC})
         add_library(mindspore::ssl ALIAS openssl::ssl)
