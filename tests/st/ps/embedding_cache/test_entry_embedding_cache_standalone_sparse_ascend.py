d31e237322c0c3a7d41ed53ba7f03d788b0d5583
lizhenyu <lizhenyu13@huawei.com>
2023-03-30 21:37:33 +0800

    change embedding cache for ascend sparse mode to level0

--- tests/st/ps/embedding_cache/test_entry_embedding_cache_standalone_sparse_ascend.py
+++ tests/st/ps/embedding_cache/test_entry_embedding_cache_standalone_sparse_ascend.py
@@ -17,7 +17,7 @@ import os
 import pytest
 
 
-@pytest.mark.level1
+@pytest.mark.level0
 @pytest.mark.platform_x86_ascend_training
 @pytest.mark.platform_arm_ascend_training
 @pytest.mark.env_single
