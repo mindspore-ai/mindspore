a2f5d495d26463332d9adaf94eb513a03862c4d6
y00451588 <yangshuo22@huawei.com>
2023-04-03 12:44:08 +0800

    fix codecheck warnings

--- mindspore/core/ops/grad/lstm_grad.cc
+++ mindspore/core/ops/grad/lstm_grad.cc
@@ -41,6 +41,14 @@
 
 namespace mindspore {
 namespace ops {
+namespace {
+constexpr int64_t kInputsNumber = 11;
+constexpr int64_t kNumberOne = 1;
+constexpr int64_t kNumberTwo = 2;
+constexpr int64_t kNumberFour = 4;
+constexpr int64_t kShapeDims = 3;
+}  // namespace
+
 void LSTMGrad::set_input_size(const int64_t input_size) {
   (void)CheckAndConvertUtils::CheckInteger(kInput_size, input_size, kGreaterThan, 0, this->name());
   (void)AddAttr(kInput_size, api::MakeValue(input_size));
@@ -117,7 +125,7 @@ class LstmGradInfer : public abstract::OpInferBase {
                           const std::vector<AbstractBasePtr> &input_args) const override {
     MS_EXCEPTION_IF_NULL(primitive);
     auto prim_name = primitive->name();
-    (void)CheckAndConvertUtils::CheckInteger("input numbers", SizeToLong(input_args.size()), kEqual, kInputNum,
+    (void)CheckAndConvertUtils::CheckInteger("input numbers", SizeToLong(input_args.size()), kEqual, kInputsNumber,
                                              prim_name);
     auto y_shape = CheckAndConvertUtils::ConvertShapePtrToShapeMap(input_args[kInputIndex4]->BuildShape())[kShape];
     auto dy_shape = CheckAndConvertUtils::ConvertShapePtrToShapeMap(input_args[kInputIndex7]->BuildShape())[kShape];
@@ -125,13 +133,13 @@ class LstmGradInfer : public abstract::OpInferBase {
     auto dcy_shape = CheckAndConvertUtils::ConvertShapePtrToShapeMap(input_args[kInputIndex9]->BuildShape())[kShape];
     if (IsDynamicRank(y_shape) || IsDynamicRank(dy_shape) || IsDynamicRank(dhy_shape) || IsDynamicRank(dcy_shape)) {
       auto shape_ptr = std::make_shared<abstract::Shape>(std::vector<int64_t>{abstract::Shape::kShapeRankAny});
-      return std::make_shared<abstract::TupleShape>(std::vector<abstract::BaseShapePtr>(kNumber4, shape_ptr));
+      return std::make_shared<abstract::TupleShape>(std::vector<abstract::BaseShapePtr>(kNumberFour, shape_ptr));
     }
-    (void)CheckAndConvertUtils::CheckInteger("dhy_shape size", SizeToLong(dhy_shape.size()), kEqual, kShapeSize,
+    (void)CheckAndConvertUtils::CheckInteger("dhy_shape size", SizeToLong(dhy_shape.size()), kEqual, kShapeDims,
                                              prim_name);
-    (void)CheckAndConvertUtils::CheckInteger("dcy_shape size", SizeToLong(dcy_shape.size()), kEqual, kShapeSize,
+    (void)CheckAndConvertUtils::CheckInteger("dcy_shape size", SizeToLong(dcy_shape.size()), kEqual, kShapeDims,
                                              prim_name);
-    (void)CheckAndConvertUtils::CheckInteger("dy_shape size", SizeToLong(dy_shape.size()), kEqual, kShapeSize,
+    (void)CheckAndConvertUtils::CheckInteger("dy_shape size", SizeToLong(dy_shape.size()), kEqual, kShapeDims,
                                              prim_name);
     bool is_dy_shape_dcy = IsDynamic(dy_shape);
     bool is_dynamic_shape_dhy = IsDynamic(dhy_shape);
@@ -139,30 +147,31 @@ class LstmGradInfer : public abstract::OpInferBase {
 
     int64_t num_layers = GetValue<int64_t>(primitive->GetAttr(kNumLayers));
     bool bidirectional = GetValue<bool>(primitive->GetAttr(kBidirectional));
-    int64_t num_directions = kNumber1;
+    int64_t num_directions = kNumberOne;
     if (bidirectional) {
-      num_directions = kNumber2;
+      num_directions = kNumberTwo;
     }
     int64_t hidden_size = GetValue<int64_t>(primitive->GetAttr(kHidden_size));
     if (!is_dynamic_shape_dhy) {
-      (void)CheckAndConvertUtils::CheckInteger("h_shape[0]", dhy_shape[kDim0], kEqual, num_layers * num_directions,
+      (void)CheckAndConvertUtils::CheckInteger("h_shape[0]", dhy_shape[kIndex0], kEqual, num_layers * num_directions,
                                                prim_name);
-      (void)CheckAndConvertUtils::CheckInteger("h_shape[2]", dhy_shape[kDim2], kEqual, hidden_size, prim_name);
+      (void)CheckAndConvertUtils::CheckInteger("h_shape[2]", dhy_shape[kIndex2], kEqual, hidden_size, prim_name);
       if (!is_dynamic_shape_dcy) {
-        (void)CheckAndConvertUtils::Check("dhy and dcy shape", dhy_shape, kEqual, dcy_shape, prim_name);
+        CheckAndConvertUtils::Check("dhy and dcy shape", dhy_shape, kEqual, dcy_shape, prim_name);
       }
     }
     if (!is_dy_shape_dcy) {
-      if (dhy_shape[kDim1] != abstract::Shape::kShapeDimAny) {
-        (void)CheckAndConvertUtils::CheckInteger("dy_shape[1]", dy_shape[kDim1], kEqual, dhy_shape[kDim1], prim_name);
+      if (dhy_shape[kIndex1] != abstract::Shape::kShapeDimAny) {
+        (void)CheckAndConvertUtils::CheckInteger("dy_shape[1]", dy_shape[kIndex1], kEqual, dhy_shape[kIndex1],
+                                                 prim_name);
       }
-      (void)CheckAndConvertUtils::CheckInteger("dy_shape[2]", dy_shape[kDim2], kEqual, hidden_size * num_directions,
+      (void)CheckAndConvertUtils::CheckInteger("dy_shape[2]", dy_shape[kIndex2], kEqual, hidden_size * num_directions,
                                                prim_name);
     }
     int64_t input_size = GetValue<int64_t>(primitive->GetAttr(kInput_size));
     auto weight_size = GetWeightSize(primitive, num_layers, num_directions);
-    ShapeVector dx_shape = {y_shape[kDim0], y_shape[kDim1], input_size};
-    ShapeVector weight_shape = {weight_size, kNumber1, kNumber1};
+    ShapeVector dx_shape = {y_shape[kIndex0], y_shape[kIndex1], input_size};
+    ShapeVector weight_shape = {weight_size, kNumberOne, kNumberOne};
     std::vector<abstract::BaseShapePtr> output_shapes;
     output_shapes.push_back(std::make_shared<abstract::Shape>(dx_shape));
     output_shapes.push_back(std::make_shared<abstract::Shape>(dhy_shape));
@@ -171,7 +180,7 @@ class LstmGradInfer : public abstract::OpInferBase {
     return std::make_shared<abstract::TupleShape>(output_shapes);
   }
 
-  TypePtr InferType(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) const override {
+  TypePtr InferType(const PrimitivePtr &, const std::vector<AbstractBasePtr> &input_args) const override {
     auto hx_type_ptr = input_args[kInputIndex1]->BuildType();
     auto dy_type_ptr = input_args[kInputIndex7]->BuildType();
     std::vector<TypePtr> types = {dy_type_ptr, dy_type_ptr, dy_type_ptr, hx_type_ptr};
@@ -184,7 +193,7 @@ class LstmGradInfer : public abstract::OpInferBase {
     bool has_bias = GetValue<bool>(primitive->GetAttr(kHasBias));
     int64_t input_size = GetValue<int64_t>(primitive->GetAttr(kInput_size));
     int64_t hidden_size = GetValue<int64_t>(primitive->GetAttr(kHidden_size));
-    int64_t gate_size = hidden_size * kNumber4;
+    int64_t gate_size = hidden_size * kNumberFour;
     for (int i = 0; i < num_layers; ++i) {
       for (int j = 0; j < num_directions; ++j) {
         int64_t input_layer_size = input_size;
@@ -200,12 +209,6 @@ class LstmGradInfer : public abstract::OpInferBase {
     }
     return weight_size;
   }
-
-  const int kInputNum = 11;
-  const int64_t kNumber1 = 1;
-  const int64_t kNumber2 = 2;
-  const int64_t kNumber4 = 4;
-  const size_t kShapeSize = 3;
 };
 
 MIND_API_OPERATOR_IMPL(LSTMGrad, BaseOperator);
