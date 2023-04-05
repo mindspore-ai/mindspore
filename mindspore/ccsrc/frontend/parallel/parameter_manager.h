8e6fcdb262f9e8b25038b243a29c53508ca305c1
bichaoyang <bichaoyang@huawei.com>
2023-04-05 17:21:31 +0800

    fix performance issue of compile graph for 2.0rc1

--- mindspore/ccsrc/frontend/parallel/parameter_manager.h
+++ mindspore/ccsrc/frontend/parallel/parameter_manager.h
@@ -45,7 +45,7 @@ using RefKeyPair = std::pair<AnfNodePtr, std::vector<AnfNodePtr>>;
 using ParameterUsersInfo = std::pair<std::string, std::pair<AnfNodePtr, AnfNodeIndexSet>>;
 
 ParameterUsersInfo FindParameterUsers(const AnfNodePtr &node, bool (*IsCareNode)(const CNodePtr &),
-                                      const std::vector<AnfNodePtr> &all_nodes);
+                                      const std::vector<AnfNodePtr> &all_nodes, bool isFast);
 void CheckParameterSplit(const std::vector<AnfNodePtr> &all_nodes);
 void HandleSymbolicKeyInstance(const FuncGraphPtr &root, const std::vector<AnfNodePtr> &all_nodes);
 void HandleNoUsedParameter(const FuncGraphPtr &root);
