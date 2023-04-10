406364d4a2c9f487858773b0fd312dd1def8857c
huoxinyou <huoxinyou1@huawei.com>
2023-04-10 16:59:45 +0800

    fixed the _compile_utils bug

--- tests/st/ops/dynamic_shape/test_dynamic_setitem.py
+++ tests/st/ops/dynamic_shape/test_dynamic_setitem.py
@@ -167,7 +167,7 @@ def test_dynamic_setitem_int_tensor():
     fact.grad_impl()
 
 
-@pytest.mark.level1
+@pytest.mark.level0
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_x86_gpu_training
 @pytest.mark.platform_arm_ascend_training
@@ -248,7 +248,7 @@ def test_dynamic_setitem_tensor_tensor():
     fact.grad_impl()
 
 
-@pytest.mark.level1
+@pytest.mark.level0
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_x86_gpu_training
 @pytest.mark.platform_arm_ascend_training
@@ -387,7 +387,7 @@ def test_dynamic_setitem_ellipsis_tensor():
     fact.grad_impl()
 
 
-@pytest.mark.level1
+@pytest.mark.level0
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_x86_gpu_training
 @pytest.mark.platform_arm_ascend_training
@@ -431,7 +431,7 @@ def test_dynamic_setitem_bool_number():
     fact.grad_impl()
 
 
-@pytest.mark.level1
+@pytest.mark.level0
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_x86_gpu_training
 @pytest.mark.platform_arm_ascend_training
@@ -529,7 +529,7 @@ def test_dynamic_setitem_list_tensor():
     fact.grad_impl()
 
 
-@pytest.mark.level1
+@pytest.mark.level0
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_x86_gpu_training
 @pytest.mark.platform_arm_ascend_training
