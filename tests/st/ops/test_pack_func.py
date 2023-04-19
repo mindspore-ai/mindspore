753d3a8343502aa91480a107a86d89d2ad2fa5b7
huangchengnuo <huangchengnuo1@huawei.com>
2023-04-19 17:06:36 +0000

    The multi-output operator and Python infer are supported in PackFunc and PackTensor inherits StubTensor

--- tests/st/ops/test_pack_func.py
+++ tests/st/ops/test_pack_func.py
@@ -20,6 +20,7 @@ import mindspore as ms
 from mindspore import nn, Tensor
 from mindspore.ops._packfunc import pack
 from mindspore.ops import functional as F
+from mindspore.ops import operations as P
 
 
 @pytest.mark.level0
@@ -33,18 +34,52 @@ def test_pack_basic_cell():
     Expectation: success
     """
     class Net(nn.Cell):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.arg_max = P.ArgMaxWithValue()
+
         @pack
         def construct(self, x, y):
             if F.is_sequence_shape_unknown(x.shape):
                 z = x + y
                 return z * z
             z = x + y + 1
-            return z * z
+            _, b = self.arg_max(z)
+            return z * z, b
     ms.set_context(mode=ms.PYNATIVE_MODE)
     net = Net()
     x = Tensor([1, 2, 3, 4])
     y = Tensor([4, 5, 6, 7], ms.float64)
-    output = net(x, y)
+    output, max_ = net(x, y)
     expect = np.array([36., 64., 100., 144.])
+    expect_max = np.array([12])
     assert np.allclose(output.asnumpy(), expect)
+    assert np.allclose(max_.asnumpy(), expect_max)
     assert output.dtype == ms.float64
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.env_onecard
+def test_pack_python_infer_cell():
+    """
+    Feature: pack of cell
+    Description: Verify the result of pack
+    Expectation: success
+    """
+    class Net(nn.Cell):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.choleskytrsm = P.CholeskyTrsm()
+
+        @pack
+        def construct(self, x, y):
+            z = self.choleskytrsm(x) + y
+            return z * z
+    ms.set_context(mode=ms.PYNATIVE_MODE)
+    net = Net()
+    x = Tensor(np.array([[0.25, 0], [0, 0.25]]), ms.float32)
+    y = Tensor(np.array([[1, 2], [2, 1]]), ms.float32)
+    output = net(x, y)
+    expect = np.array([[9, 4], [4, 9]])
+    assert np.allclose(output.asnumpy(), expect)
