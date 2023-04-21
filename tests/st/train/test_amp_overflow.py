07c5a3168cb9711112b034ba8588c580882d9499
fandawei <fandawei2@huawei.com>
2023-04-21 11:12:32 +0800

    add tuple broaden for amp.all_finite

--- tests/st/train/test_amp_overflow.py
+++ tests/st/train/test_amp_overflow.py
@@ -34,7 +34,7 @@ class Net(nn.Cell):
         return output
 
 
-@pytest.mark.level1
+@pytest.mark.level0
 @pytest.mark.platform_arm_ascend_training
 @pytest.mark.platform_x86_ascend_training
 @pytest.mark.platform_x86_gpu_training
