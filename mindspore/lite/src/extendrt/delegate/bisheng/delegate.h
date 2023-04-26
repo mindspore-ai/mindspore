ce672041ce05f94fa020de24be85d2123731aa29
liu lili <liulili715@huawei.com>
2023-04-26 17:38:32 +0800

    mindspore lite: add bisheng delegate definition

---
+++ mindspore/lite/src/extendrt/delegate/bisheng/delegate.h
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2023 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MINDSPORE_LITE_SRC_EXTENDRT_DELEGATE_BISHENG_DELEGATE_H_
+#define MINDSPORE_LITE_SRC_EXTENDRT_DELEGATE_BISHENG_DELEGATE_H_
+
+#include <memory>
+
+#include "extendrt/delegate/type.h"
+
+namespace mindspore {
+class BishengDelegate : public ExtendDelegate {
+ public:
+  BishengDelegate() = default;
+  virtual ~BishengDelegate() = default;
+
+  void ReplaceNodes(const std::shared_ptr<FuncGraph> &graph) override;
+
+  bool IsDelegateNode(const std::shared_ptr<AnfNode> &node) override;
+
+  std::shared_ptr<kernel::Kernel> CreateKernel(const std::shared_ptr<AnfNode> &node) override;
+};
+}  // namespace mindspore
+
+#endif  // MINDSPORE_LITE_SRC_EXTENDRT_DELEGATE_BISHENG_DELEGATE_H_
---
+++ mindspore/lite/src/extendrt/delegate/type.h
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2023 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MINDSPORE_LITE_SRC_EXTENDRT_DELEGATE_TYPE_H_
+#define MINDSPORE_LITE_SRC_EXTENDRT_DELEGATE_TYPE_H_
+
+#include <memory>
+
+#include "include/api/delegate_api.h"
+#include "ir/func_graph.h"
+#include "include/api/kernel.h"
+
+namespace mindspore {
+class ExtendDelegate : public IDelegate<FuncGraph, AnfNode, kernel::Kernel> {
+ public:
+  ExtendDelegate() = default;
+  virtual ~ExtendDelegate() = default;
+
+  void ReplaceNodes(const std::shared_ptr<FuncGraph> &graph) override {
+    // not implemented
+  }
+
+  bool IsDelegateNode(const std::shared_ptr<AnfNode> &node) override {
+    // not implemented
+    return true;
+  }
+
+  std::shared_ptr<kernel::Kernel> CreateKernel(const std::shared_ptr<AnfNode> &node) override {
+    // not implemented
+    return nullptr;
+  }
+};
+}  // namespace mindspore
+
+#endif  // MINDSPORE_LITE_SRC_EXTENDRT_DELEGATE_TYPE_H_
