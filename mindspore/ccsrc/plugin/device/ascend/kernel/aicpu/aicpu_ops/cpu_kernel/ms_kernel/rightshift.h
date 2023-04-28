eda49dffeb7bcfcffd1dc09d88a670cb945c9e04
shen_jingxing <shenjingxing2@huawei.com>
2023-04-28 09:24:24 +0800

    add new aicpu operator supported and code clean

---
+++ mindspore/ccsrc/plugin/device/ascend/kernel/aicpu/aicpu_ops/cpu_kernel/ms_kernel/rightshift.h
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2022. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AICPU_KERNELS_NORMALIZED_RIGHTSHIFT_H
+#define AICPU_KERNELS_NORMALIZED_RIGHTSHIFT_H
+
+#include "cpu_ops_kernel.h"
+#include "utils/bcast.h"
+
+namespace aicpu {
+
+class RightShiftCpuKernel : public CpuKernel {
+ public:
+  RightShiftCpuKernel() = default;
+  ~RightShiftCpuKernel() override = default;
+
+  uint32_t Compute(CpuKernelContext &ctx) override;
+
+ private:
+  static uint32_t RightShiftParamCheck(const CpuKernelContext &ctx);
+
+  template <typename T>
+  void SpecialCompute(BcastShapeType type, int64_t start, int64_t end, const T *input1, const T *input2, T *output);
+
+  template <typename T>
+  uint32_t NoBcastCompute(const CpuKernelContext &ctx);
+
+  template <typename T>
+  uint32_t BcastCompute(const CpuKernelContext &ctx, const Bcast &bcast);
+
+  template <typename T>
+  uint32_t RightShiftCompute(const CpuKernelContext &ctx);
+};
+}  // namespace aicpu
+#endif
