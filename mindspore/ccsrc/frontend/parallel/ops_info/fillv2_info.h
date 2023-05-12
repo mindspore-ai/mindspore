84706a9f3c0a2bb2ef186cc5523553bfbc20c65f
liuluobin <liuluobin@huawei.com>
2023-05-12 10:50:19 +0800

    Implementation for FillV2Info

---
+++ mindspore/ccsrc/frontend/parallel/ops_info/fillv2_info.h
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2023 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MINDSPORE_CCSRC_FRONTEND_PARALLEL_OPS_INFO_FILLV2_INFO_H_
+#define MINDSPORE_CCSRC_FRONTEND_PARALLEL_OPS_INFO_FILLV2_INFO_H_
+
+#include <memory>
+#include <vector>
+#include <string>
+
+#include "frontend/parallel/ops_info/operator_info.h"
+#include "frontend/parallel/strategy.h"
+#include "frontend/parallel/tensor_layout/tensor_redistribution.h"
+
+namespace mindspore {
+namespace parallel {
+class FillV2Info : public OperatorInfo {
+ public:
+  FillV2Info(const std::string &name, const Shapes &input_shape, const Shapes &output_shape,
+             const PrimitiveAttrs &attrs)
+      : OperatorInfo(name, input_shape, output_shape, attrs, std::make_shared<FillV2Cost>()) {}
+  ~FillV2Info() = default;
+
+  std::vector<StrategyPtr> GenerateOpStrategies(int64_t stage_id) override;
+  Status SetCostUnderStrategy(const StrategyPtr &strategy) override { return SetCostUnderStrategyBase(strategy); }
+  void ReplaceNodeInputOrAttrs() override;
+
+ protected:
+  Status InferAttrs() override;
+  Status GetAttrs() override { return SUCCESS; };
+  Status CheckStrategy(const StrategyPtr &strategy) override;
+  Status InferDevMatrixShape() override;
+  Status InferTensorMap() override;
+  Status InferMirrorOps() override;
+  Status InferForwardCommunication() override { return SUCCESS; };
+
+ private:
+  void ResetInputsShape();
+  Shape GetShapeFromTensor(const tensor::TensorPtr &shape_tensor);
+};
+}  // namespace parallel
+}  // namespace mindspore
+
+#endif  // MINDSPORE_CCSRC_FRONTEND_PARALLEL_OPS_INFO_FILLV2_INFO_H_
--- mindspore/ccsrc/frontend/parallel/step_parallel_utils.cc
+++ mindspore/ccsrc/frontend/parallel/step_parallel_utils.cc
@@ -801,7 +801,7 @@ bool IsSplittableOperator(const std::string &op_name) {
      CHECK_VALID, INVERT, SCATTER_ADD, SCATTER_DIV, SCATTER_MUL, SCATTER_MAX, SCATTER_MIN, SCATTER_SUB, UNIQUE_WITH_PAD,
      POPULATION_COUNT, IDENTITY, BESSELI0, BESSELI1, BESSELJ0, BESSELJ1, CUM_MAX, CUM_MIN, HYPOT, IGAMMA, IGAMMAC,
      LEFT_SHIFT, RIGHT_SHIFT, NEXT_AFTER, ZETA, REVERSEV2, LGAMMA, TRUNC, BETAINC, GCD, CHOLESKY, MAXPOOL_3D,
-     AVGPOOL_3D};
+     AVGPOOL_3D, FILLV2};
   // clang-format on
 
   auto iter = splittable_op.find(op_name);
@@ -1034,8 +1034,8 @@ OperatorInfoPtr CreateOperatorInfo(const CNodePtr &cnode) {
   auto &inputs = cnode->inputs();
   std::vector<ValuePtr> input_value;
   for (size_t index = 1; index < inputs.size(); ++index) {
-    if (inputs[index]->isa<ValueNode>()) {
-      input_value.push_back(GetValueNode(inputs[index]));
+    if (inputs[index]->isa<ValueNode>() || inputs[index]->isa<tensor::Tensor>()) {
+      (void)input_value.emplace_back(GetValueNode(inputs[index]));
       continue;
     }
     (void)input_value.emplace_back(nullptr);
