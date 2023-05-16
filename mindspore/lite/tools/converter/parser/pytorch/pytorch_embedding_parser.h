c56442ae74731840103e30261358d03e97114af6
zhangyanhui <zhangyanhui17@huawei.com>
2023-05-16 16:48:34 +0800

    pytorch add some parser

--- mindspore/lite/tools/converter/parser/pytorch/pytorch_embedding_parser.h
+++ mindspore/lite/tools/converter/parser/pytorch/pytorch_embedding_parser.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2022 Huawei Technologies Co., Ltd
+ * Copyright 2023 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-#ifndef MINDSPORE_LITE_TOOLS_CONVERTER_PARSER_PYTORCH_PYTORCH_MATMUL_PARSER_H_
-#define MINDSPORE_LITE_TOOLS_CONVERTER_PARSER_PYTORCH_PYTORCH_MATMUL_PARSER_H_
+#ifndef MINDSPORE_LITE_TOOLS_CONVERTER_PARSER_PYTORCH_PYTORCH_EMBEDDING_PARSER_H_
+#define MINDSPORE_LITE_TOOLS_CONVERTER_PARSER_PYTORCH_PYTORCH_EMBEDDING_PARSER_H_
 
 #include <vector>
 #include "tools/converter/parser/pytorch/pytorch_node_parser.h"
@@ -23,21 +23,13 @@
 
 namespace mindspore {
 namespace lite {
-class PytorchAddmmParser : public PytorchNodeParser {
+class PytorchEmbeddingParser : public PytorchNodeParser {
  public:
-  PytorchAddmmParser() : PytorchNodeParser("Addmm") {}
-  ~PytorchAddmmParser() override = default;
-
-  PrimitiveCPtr Parse(const torch::jit::Node *torch_node, std::vector<size_t> *input_indices) override;
-};
-
-class PytorchLinearParser : public PytorchNodeParser {
- public:
-  PytorchLinearParser() : PytorchNodeParser("Linear") {}
-  ~PytorchLinearParser() override = default;
+  PytorchEmbeddingParser() : PytorchNodeParser("Embedding") {}
+  ~PytorchEmbeddingParser() override = default;
 
   PrimitiveCPtr Parse(const torch::jit::Node *torch_node, std::vector<size_t> *input_indices) override;
 };
 }  // namespace lite
 }  // namespace mindspore
-#endif  // MINDSPORE_LITE_TOOLS_CONVERTER_PARSER_PYTORCH_PYTORCH_MATMUL_PARSER_H_
+#endif  // MINDSPORE_LITE_TOOLS_CONVERTER_PARSER_PYTORCH_PYTORCH_EMBEDDING_PARSER_H_
--- mindspore/lite/tools/converter/parser/pytorch/pytorch_reshape_parser.cc
+++ mindspore/lite/tools/converter/parser/pytorch/pytorch_reshape_parser.cc
@@ -17,6 +17,8 @@
 #include "tools/converter/parser/pytorch/pytorch_reshape_parser.h"
 #include <memory>
 #include "ops/reshape.h"
+#include "ops/squeeze.h"
+#include "ops/unsqueeze.h"
 #include "nnacl/op_base.h"
 
 namespace mindspore {
@@ -38,6 +40,39 @@ PrimitiveCPtr PytorchReshapeParser::Parse(const torch::jit::Node *torch_node, st
 
   return prim->GetPrim();
 }
+
+PrimitiveCPtr PytorchSqueezeParser::Parse(const torch::jit::Node *torch_node, std::vector<size_t> *input_indices) {
+  MS_ASSERT(torch_node != nullptr && input_indices != nullptr);
+  auto prim = std::make_unique<ops::Squeeze>();
+  MS_CHECK_TRUE_RET(prim != nullptr, nullptr);
+
+  input_indices->push_back(0);
+  if (torch_node->inputs().size() > SECOND_INPUT) {
+    auto dim = PytorchNodeParser::GetValueFromConstNode<std::vector<int64_t>>(torch_node->input(SECOND_INPUT));
+    prim->set_axis(dim);
+  }
+
+  return prim->GetPrim();
+}
+
+PrimitiveCPtr PytorchUnSqueezeParser::Parse(const torch::jit::Node *torch_node, std::vector<size_t> *input_indices) {
+  MS_ASSERT(torch_node != nullptr && input_indices != nullptr);
+  auto prim = std::make_unique<ops::Unsqueeze>();
+  MS_CHECK_TRUE_RET(prim != nullptr, nullptr);
+
+  input_indices->push_back(0);
+  std::vector<int64_t> axis;
+  if (torch_node->inputs().size() > SECOND_INPUT) {
+    auto dim = PytorchNodeParser::GetValueFromConstNode<int64_t>(torch_node->input(SECOND_INPUT));
+    axis.emplace_back(dim);
+    prim->set_axis(axis);
+  }
+
+  return prim->GetPrim();
+}
+
 PytorchNodeRegistrar g_pytorchReshapeParser("reshape", new PytorchReshapeParser());
+PytorchNodeRegistrar g_pytorchSqueezeParser("squeeze", new PytorchSqueezeParser());
+PytorchNodeRegistrar g_pytorchUnSqueezeParser("unsqueeze", new PytorchUnSqueezeParser());
 }  // namespace lite
 }  // namespace mindspore
