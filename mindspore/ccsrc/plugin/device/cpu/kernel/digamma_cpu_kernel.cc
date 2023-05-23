ecbabbffa98d5654f62a605f224cd1a17e2d5569
panzhihui <panzhihui3@huawei.com>
2023-05-23 20:41:40 +0800

    fix polygamma when a = 0

--- mindspore/ccsrc/plugin/device/cpu/kernel/digamma_cpu_kernel.cc
+++ mindspore/ccsrc/plugin/device/cpu/kernel/digamma_cpu_kernel.cc
@@ -22,6 +22,7 @@
 #include <map>
 #include <string>
 
+#include "utils/digamma_helper.h"
 #include "plugin/device/cpu/hal/device/cpu_device_address.h"
 
 namespace mindspore {
@@ -33,8 +34,6 @@ constexpr size_t kInputsNum = 1;
 constexpr size_t kOutputsNum = 1;
 }  // namespace
 
-static inline double calc_digamma(double x);
-
 bool DigammaCpuKernelMod::Init(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
                                const std::vector<KernelTensorPtr> &outputs) {
   MS_EXCEPTION_IF_NULL(base_operator);
@@ -44,68 +43,6 @@ bool DigammaCpuKernelMod::Init(const BaseOperatorPtr &base_operator, const std::
     return false;
   }
   return true;
-}
-
-template <typename T>
-inline T ScalarDigamma(T x) {
-  return calc_digamma(x);
-}
-
-template <>
-inline Eigen::half ScalarDigamma(Eigen::half x) {
-  const Eigen::half val{static_cast<Eigen::half>(calc_digamma(static_cast<std::float_t>(x)))};
-  return val;
-}
-
-template <typename T>
-static inline T polevl(const T x, const T A[], size_t len) {
-  T result = 0;
-  for (size_t i = 0; i <= len; i++) {
-    result = result * x + A[i];
-  }
-  return result;
-}
-
-static inline double calc_digamma(double x) {
-  static constexpr double kPSI_10 = 2.25175258906672110764;
-  static constexpr double kPI = 3.141592653589793238462;
-  static constexpr int64_t kTEN = 10;
-  static constexpr double kHALF = 0.5;
-  static constexpr int64_t kSIX = 6;
-  if (static_cast<double>(x) == 0) {
-    return std::copysign(INFINITY, -x);
-  }
-
-  bool x_is_integer = x == trunc(x);
-  if (x < 0) {
-    if (x_is_integer) {
-      return std::numeric_limits<double>::quiet_NaN();
-    }
-    double q, r = 0;
-    r = std::modf(x, &q);
-    return calc_digamma(1 - x) - kPI / tan(kPI * r);
-  }
-
-  double result = 0;
-  while (x < kTEN) {
-    result -= 1 / x;
-    x += 1;
-  }
-  if (x == kTEN) {
-    return result + kPSI_10;
-  }
-
-  static const double A[] = {
-    8.33333333333333333333E-2, -2.10927960927960927961E-2, 7.57575757575757575758E-3, -4.16666666666666666667E-3,
-    3.96825396825396825397E-3, -8.33333333333333333333E-3, 8.33333333333333333333E-2,
-  };
-
-  double y = 0;
-  if (x < 1.0e17) {
-    double z = 1.0 / (x * x);
-    y = z * polevl(z, A, kSIX);
-  }
-  return result + log(x) - (kHALF / x) - y;
 }
 
 int DigammaCpuKernelMod::Resize(const BaseOperatorPtr &base_operator, const std::vector<KernelTensorPtr> &inputs,
@@ -143,7 +80,7 @@ bool DigammaCpuKernelMod::LaunchKernel(const std::vector<kernel::AddressPtr> &in
   auto output_y = reinterpret_cast<T *>(outputs[0]->addr);
 
   for (int64_t i = 0; i < input_tensor_size_; i++) {
-    *(output_y + i) = ScalarDigamma<T>(*(input_x + i));
+    *(output_y + i) = CalcDigamma<T>(*(input_x + i));
   }
   return true;
 }
