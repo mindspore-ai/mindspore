24fdc28ed4f063e671f39fa6d71428331f80bedc
lizhenyu <lizhenyu13@huawei.com>
2023-06-05 15:24:40 +0800

    CPU Hash Table support incrementally export

--- mindspore/ccsrc/plugin/device/cpu/hal/device/cpu_hash_table.cc
+++ mindspore/ccsrc/plugin/device/cpu/hal/device/cpu_hash_table.cc
@@ -13,14 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include "plugin/device/cpu/hal/device/cpu_hash_table.h"
+
 #include <memory>
 #include <vector>
+#include <algorithm>
 
 #include "utils/log_adapter.h"
 #include "utils/convert_utils_base.h"
 #include "utils/ms_context.h"
 #include "runtime/hardware/device_context_manager.h"
-#include "plugin/device/cpu/hal/device/cpu_hash_table.h"
 
 namespace mindspore {
 namespace device {
@@ -59,7 +61,7 @@ bool CPUHashTable<Key, Value>::Find(const Key *keys, size_t key_num, bool, Value
       size_t dst_size = value_size_;
 
       // Copy the value of the key from the hash table to the outputs.
-      auto value = values_[key];
+      auto value = values_[key].first;
       MS_EXCEPTION_IF_NULL(value);
       auto ret = memcpy_s(outputs + offset, dst_size, value, src_size);
       if (ret != EOK) {
@@ -75,28 +77,32 @@ bool CPUHashTable<Key, Value>::Find(const Key *keys, size_t key_num, bool, Value
 }
 
 template <typename Key, typename Value>
-bool CPUHashTable<Key, Value>::Insert(const Key *keys, size_t key_num, const Value *value, void *) {
+bool CPUHashTable<Key, Value>::Insert(const Key *keys, size_t key_num, const Value *values, void *) {
   std::unique_lock<std::shared_mutex> lock(mutex_);
+  MS_ERROR_IF_NULL(keys);
+  MS_ERROR_IF_NULL(values);
 
   for (size_t i = 0; i < key_num; ++i) {
     const auto &key = keys[i];
 
+    auto iter = values_.find(key);
     // The the key does not exist, a new value buffer should be allocated firstly.
-    if (values_.find(key) == values_.end()) {
+    if (iter == values_.end()) {
       auto value_addr = AllocateMemory(value_size_);
       MS_EXCEPTION_IF_NULL(value_addr);
-      values_[key] = reinterpret_cast<Value *>(value_addr);
+      iter = values_.emplace(key, std::make_pair(reinterpret_cast<Value *>(value_addr), Status::kModified)).first;
     }
 
     // Do the insertion copy.
     size_t offset = i * value_dim_;
     size_t src_size = value_size_;
     size_t dst_size = value_size_;
-    auto ret = memcpy_s(values_[key], dst_size, value + offset, src_size);
+    auto ret = memcpy_s(iter->second.first, dst_size, values + offset, src_size);
     if (ret != EOK) {
       MS_LOG(ERROR) << "memcpy_s error, errorno(" << ret << ")";
       return false;
     }
+    iter->second.second = Status::kModified;
   }
   is_dirty_ = true;
   return true;
@@ -109,9 +115,10 @@ bool CPUHashTable<Key, Value>::Erase(const Key *keys, size_t key_num, void *) {
   // Erase all the keys in the hash table.
   for (size_t i = 0; i < key_num; ++i) {
     const auto &key = keys[i];
-    if (values_.find(key) != values_.end()) {
-      auto value = values_[keys[i]];
-      (void)values_.erase(keys[i]);
+    auto iter = values_.find(key);
+    if (iter != values_.end()) {
+      auto value = iter->second.first;
+      (void)values_.erase(key);
 
       // Return the memory of value to the pool.
       MS_EXCEPTION_IF_NULL(value);
@@ -137,7 +144,7 @@ bool CPUHashTable<Key, Value>::GetKeysAndValues(Key *keys, Value *values, void *
   size_t index = 0;
   for (auto iter = values_.begin(); iter != values_.end(); iter++) {
     auto key = iter->first;
-    auto value = iter->second;
+    auto value = iter->second.first;
     // Copy the key.
     keys[index] = key;
 
@@ -161,20 +168,36 @@ bool CPUHashTable<Key, Value>::Import(const DataLenPair &) {
 }
 
 template <typename Key, typename Value>
-HashTableExportData CPUHashTable<Key, Value>::Export(bool) {
-  const size_t size = values_.size();
-  auto host_keys = std::make_shared<std::vector<char>>(size * sizeof(Key));
-  auto host_values = std::make_shared<std::vector<char>>(size * value_size_);
-  auto value_data = host_values->data();
-  auto host_statuses = std::make_shared<std::vector<char>>(size * sizeof(HashTableElementStatus));
+HashTableExportData CPUHashTable<Key, Value>::ExportIncrementally() {
+  // 1. Count export number of all modified elememts.
+  size_t update_elements_size = std::count_if(
+    values_.begin(), values_.end(), [](typename std::unordered_map<Key, ValueStatusPair>::const_reference item) {
+      return item.second.second == Status::kModified;
+    });
 
+  auto keys = std::make_shared<std::vector<char>>(update_elements_size * sizeof(Key));
+  auto keys_data = reinterpret_cast<Key *>(keys->data());
+  auto values = std::make_shared<std::vector<char>>(update_elements_size * value_size_);
+  auto value_data = reinterpret_cast<Value *>(values->data());
+  auto statuses = std::make_shared<std::vector<char>>(update_elements_size * sizeof(HashTableElementStatus));
+  auto statuses_data = reinterpret_cast<Status *>(statuses->data());
+
+  // 2. Export all modified elememts.
   size_t index = 0;
   for (auto iter = values_.begin(); iter != values_.end(); iter++) {
     auto key = iter->first;
-    auto value = iter->second;
-    // Export the key and value.
-    (*host_keys)[index] = key;
+    auto value = iter->second.first;
+    auto status = iter->second.second;
+    if (status != Status::kModified) {
+      continue;
+    }
 
+    // Export the key.
+    keys_data[index] = key;
+    // Export the status.
+    statuses_data[index] = status;
+
+    // Export the value.
     size_t offset = index * value_dim_;
     size_t src_size = value_size_;
     size_t dst_size = value_size_;
@@ -184,7 +207,52 @@ HashTableExportData CPUHashTable<Key, Value>::Export(bool) {
     }
     ++index;
   }
-  return {host_keys, host_values, host_statuses};
+  return {keys, values, statuses};
+}
+
+template <typename Key, typename Value>
+HashTableExportData CPUHashTable<Key, Value>::ExportFully() {
+  const size_t size = values_.size();
+  auto keys = std::make_shared<std::vector<char>>(size * sizeof(Key));
+  auto keys_data = reinterpret_cast<Key *>(keys->data());
+  auto values = std::make_shared<std::vector<char>>(size * value_size_);
+  auto value_data = reinterpret_cast<Value *>(values->data());
+  auto statuses = std::make_shared<std::vector<char>>(size * sizeof(HashTableElementStatus));
+  auto statuses_data = reinterpret_cast<Status *>(statuses->data());
+
+  size_t index = 0;
+  for (auto iter = values_.begin(); iter != values_.end(); iter++) {
+    auto key = iter->first;
+    auto value = iter->second.first;
+    auto status = iter->second.second;
+
+    // Export the key.
+    keys_data[index] = key;
+    // Export the status.
+    statuses_data[index] = status;
+
+    // Export the value.
+    size_t offset = index * value_dim_;
+    size_t src_size = value_size_;
+    size_t dst_size = value_size_;
+    auto ret = memcpy_s(value_data + offset, dst_size, value, src_size);
+    if (ret != EOK) {
+      MS_LOG(EXCEPTION) << "memcpy_s error, errorno(" << ret << ")";
+    }
+    ++index;
+  }
+  return {keys, values, statuses};
+}
+
+template <typename Key, typename Value>
+HashTableExportData CPUHashTable<Key, Value>::Export(bool incremental) {
+  // Update is_dirty_ to false because already get latest content after export.
+  is_dirty_ = false;
+
+  if (incremental) {
+    return ExportIncrementally();
+  }
+  return ExportFully();
 }
 
 template <typename Key, typename Value>
@@ -209,13 +277,12 @@ bool CPUHashTable<Key, Value>::Clear() {
   std::unique_lock<std::shared_mutex> lock(mutex_);
   // Return all the memory of values in hash table to the memory pool.
   for (auto iter = values_.begin(); iter != values_.end(); iter++) {
-    auto key = iter->first;
-    auto value = iter->second;
+    auto value = iter->second.first;
     if (value != nullptr) {
       FreeMemory(value);
     }
-    (void)values_.erase(key);
   }
+  (void)values_.clear();
   return true;
 }
 
