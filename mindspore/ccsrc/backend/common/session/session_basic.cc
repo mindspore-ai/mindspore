2159c3939d7d25ce4b4a257731a5704fca8d18fc
nomindcarry <caikairui@huawei.com>
2023-06-09 09:24:36 +0800

    fix acl unsupport format

--- mindspore/ccsrc/backend/common/session/session_basic.cc
+++ mindspore/ccsrc/backend/common/session/session_basic.cc
@@ -206,6 +206,12 @@ bool NeedDiscardTensorProperties(const std::string &op_device_target,
   return true;
 }
 
+bool AclSupportFormat(const std::string &format) {
+  static std::set<std::string> default_support = {kOpFormat_DEFAULT, kOpFormat_ND,    kOpFormat_NCHW,
+                                                  kOpFormat_NHWC,    kOpFormat_NDHWC, kOpFormat_NCDHW};
+  return default_support.find(format) != default_support.end();
+}
+
 ParameterPtr ConstructRunOpParameter(const std::shared_ptr<KernelGraph> &graph, const tensor::TensorPtr &input_tensor,
                                      const BackendOpRunInfoPtr &op_run_info, int64_t tensor_mask) {
   MS_EXCEPTION_IF_NULL(graph);
@@ -224,7 +230,11 @@ ParameterPtr ConstructRunOpParameter(const std::shared_ptr<KernelGraph> &graph, 
     TypeId param_init_data_type = common::AnfAlgo::IsParameterWeight(param) ? kTypeUnknown : input_tensor->data_type();
     kernel_build_info_builder->SetOutputsDeviceType(std::vector<TypeId>{param_init_data_type});
   } else {
-    kernel_build_info_builder->SetOutputsFormat(std::vector<std::string>{device_address->format()});
+    if (op_run_info->base_op_run_info.use_dynamic_shape_process && !AclSupportFormat(device_address->format())) {
+      kernel_build_info_builder->SetOutputsFormat(std::vector<std::string>{kOpFormat_DEFAULT});
+    } else {
+      kernel_build_info_builder->SetOutputsFormat(std::vector<std::string>{device_address->format()});
+    }
     kernel_build_info_builder->SetOutputsDeviceType(std::vector<TypeId>{device_address->type_id()});
     kernel_build_info_builder->SetOutputsReshapeType({input_tensor->padding_type()});
     AnfAlgo::SetOutputAddr(device_address, 0, param.get());
