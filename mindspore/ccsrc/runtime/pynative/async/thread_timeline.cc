b27bdcab5ce172adaee7d01fafacc149b77216ae
caifubi <caifubi1@huawei.com>
2023-06-11 20:29:01 +0800

    Add Timeline tool for PyNative pipeline

---
+++ mindspore/ccsrc/runtime/pynative/async/thread_timeline.cc
@@ -0,0 +1,148 @@
+/**
+ * Copyright 2023 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "runtime/pynative/async/thread_timeline.h"
+
+#include <map>
+#include <utility>
+#include "nlohmann/json.hpp"
+#include "include/common/debug/common.h"
+
+namespace mindspore {
+namespace pynative {
+namespace {
+enum class kTimelinePh { kTimelineBegin = 0, kTimelineEnd };
+constexpr auto kStrName = "name";
+constexpr auto kStrPh = "ph";
+constexpr auto kStrPid = "pid";
+constexpr auto kStrTid = "tid";
+constexpr auto kStrTs = "ts";
+std::string GetPhString(kTimelinePh ph) {
+  static const std::map<kTimelinePh, std::string> kPhMap = {{kTimelinePh::kTimelineBegin, "B"},
+                                                            {kTimelinePh::kTimelineEnd, "E"}};
+  return kPhMap.find(ph)->second;
+}
+
+std::string GetTypeString(kTimelineType type) {
+  static const std::map<kTimelineType, std::string> kTidMap = {{kTimelineType::kTypePyTask, "py_"},
+                                                               {kTimelineType::kTypeFpTask, "fp_"},
+                                                               {kTimelineType::kTypeBpTask, "bp_"},
+                                                               {kTimelineType::kTypeBackendTask, "backend_"},
+                                                               {kTimelineType::kTypeWaitValueTask, "WaitValue"},
+                                                               {kTimelineType::kTypeWaitAbsTask, "WaitAbs"},
+                                                               {kTimelineType::kTypeGraphTask, "Graph_"},
+                                                               {kTimelineType::kTypeDoCastTask, "DoCast_"},
+                                                               {kTimelineType::kTypeInferTask, "Infer_"},
+                                                               {kTimelineType::kTypeGetOutputTask, "GetOut_"},
+                                                               {kTimelineType::kTypeOpGradTask, "OpGrad_"}};
+  return kTidMap.find(type)->second;
+}
+
+// Each thread has independent data. So it's lock-free.
+thread_local std::vector<TimelineObj> t_timeline_data_;
+}  // namespace
+
+uint64_t ThreadTimeline::GetTime() {
+  auto now = std::chrono::steady_clock::now();
+  int64_t us = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count();
+  return static_cast<uint64_t>(us);
+}
+
+TimelineObj::TimelineObj(std::string name, std::thread::id tid, kTimelineType type, uint64_t ts_start, uint64_t ts_end)
+    : name_(std::move(name)), tid_(tid), type_(type), ts_start_(ts_start), ts_end_(ts_end) {}
+
+ThreadTimeline &ThreadTimeline::GetInstance() {
+  static ThreadTimeline instance;
+  return instance;
+}
+
+void ThreadTimeline::Init() {
+  TimelineGuard::enable();
+  start_ = ThreadTimeline::GetTime();
+}
+
+void ThreadTimeline::Record(const TimelineObj &obj) { t_timeline_data_.emplace_back(obj); }
+
+void ThreadTimeline::Combine() {
+  thread_local static bool combined = false;
+  if (combined) {
+    return;
+  }
+  std::unique_lock<std::mutex> lock(combine_mutex_);
+  for (auto &item : t_timeline_data_) {
+    timeline_data_map_[item.tid_].emplace_back(item);
+  }
+  combined = true;
+}
+
+std::string TidToString(const std::thread::id &tid) {
+  std::stringstream ss;
+  ss << tid;
+  return ss.str();
+}
+
+void ThreadTimeline::Parse() {
+  if (timeline_data_map_.empty()) {
+    return;
+  }
+  nlohmann::json out;
+  for (auto &timeline_data : timeline_data_map_) {
+    for (auto &item : timeline_data.second) {
+      nlohmann::json record_start;
+      record_start[kStrName] = GetTypeString(item.type_) + item.name_;
+      record_start[kStrPh] = GetPhString(kTimelinePh::kTimelineBegin);
+      record_start[kStrPid] = std::to_string(getpid());
+      record_start[kStrTid] = TidToString(item.tid_);
+      record_start[kStrTs] = item.ts_start_ - start_;
+
+      nlohmann::json record_end;
+      record_end[kStrName] = GetTypeString(item.type_) + item.name_;
+      record_end[kStrPh] = GetPhString(kTimelinePh::kTimelineEnd);
+      record_end[kStrPid] = std::to_string(getpid());
+      record_end[kStrTid] = TidToString(item.tid_);
+      record_end[kStrTs] = item.ts_end_ - start_;
+
+      out.emplace_back(record_start);
+      out.emplace_back(record_end);
+    }
+  }
+  DumpJson(out.dump());
+  timeline_data_map_.clear();
+}
+
+void ThreadTimeline::DumpJson(const std::string &json) const {
+  auto ts = ThreadTimeline::GetTime();
+  std::string filename = "async_timeline_" + std::to_string(ts) + ".json";
+  (void)Common::SaveStringToFile(filename, json);
+}
+
+TimelineGuard::TimelineGuard(std::string op_name, kTimelineType timeline_type) {
+  if (enable_) {
+    op_name_ = std::move(op_name);
+    timeline_type_ = timeline_type;
+    ts_start_ = ThreadTimeline::GetTime();
+  }
+}
+
+TimelineGuard::~TimelineGuard() {
+  if (enable_) {
+    auto &profile = ThreadTimeline::GetInstance();
+    auto ts_end = ThreadTimeline::GetTime();
+    profile.Record(TimelineObj(op_name_, std::this_thread::get_id(), timeline_type_, ts_start_, ts_end));
+  }
+}
+}  // namespace pynative
+}  // namespace mindspore
---
+++ mindspore/ccsrc/runtime/pynative/async/thread_timeline.h
@@ -0,0 +1,109 @@
+/**
+ * Copyright 2023 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MINDSPORE_MINDSPORE_CCSRC_RUNTIME_PYNATIVE_ASYNC_THREAD_TIMELINE_H_
+#define MINDSPORE_MINDSPORE_CCSRC_RUNTIME_PYNATIVE_ASYNC_THREAD_TIMELINE_H_
+
+#include <string>
+#include <thread>
+#include <vector>
+#include <unordered_map>
+#include <mutex>
+#include "utils/profile.h"
+#include "utils/ms_utils.h"
+#include "include/backend/visible.h"
+
+namespace mindspore {
+namespace pynative {
+enum kTimelineType {
+  kTypePyTask = 0,
+  kTypeFpTask,
+  kTypeBpTask,
+  kTypeBackendTask,
+  kTypeWaitValueTask,
+  kTypeWaitAbsTask,
+  kTypeGraphTask,
+  kTypeDoCastTask,
+  kTypeInferTask,
+  kTypeGetOutputTask,
+  kTypeOpGradTask,
+};
+
+struct BACKEND_EXPORT TimelineObj {
+  TimelineObj(std::string name, std::thread::id tid, kTimelineType type, uint64_t ts_start, uint64_t ts_end);
+  std::string name_;
+  std::thread::id tid_;
+  kTimelineType type_;
+  uint64_t ts_start_;
+  uint64_t ts_end_;
+};
+
+// Add a timeline tool for PyNative multi-stage pipeline.
+// The tool is thread-safe and lock-free when recording the time data.
+// Each thread has a thread_local data and need to merge the thread_local data
+// before the thread exits.
+class BACKEND_EXPORT ThreadTimeline {
+ public:
+  static ThreadTimeline &GetInstance();
+
+  void Init();
+
+  // Gets the current timestamp in microseconds.
+  // The unit of timestamps in the Timeline JSON file is required to be microseconds.
+  static uint64_t GetTime();
+
+  void Record(const TimelineObj &obj);
+
+  // Parse the Timeline JSON.
+  void Parse();
+
+  // Combine thread_local data before thread exit.
+  void Combine();
+
+ private:
+  ThreadTimeline() = default;
+  ~ThreadTimeline() = default;
+  DISABLE_COPY_AND_ASSIGN(ThreadTimeline);
+
+  void DumpJson(const std::string &json) const;
+
+  // The data after Combine thread_local data.
+  std::unordered_map<std::thread::id, std::vector<TimelineObj>> timeline_data_map_;
+  // Initialize the starting timestamp.
+  uint64_t start_{0};
+  std::mutex combine_mutex_;
+};
+
+// To add the Guard class using the C++ RAII mechanism.
+// Only need to add a line to the function that needs to be counted time-consuming.
+// The Guard class will record the start timestamp when constructing,
+// and the end timestamp will be recorded when Guard destroying.
+class BACKEND_EXPORT TimelineGuard {
+ public:
+  TimelineGuard(std::string op_name, kTimelineType timeline_type);
+  ~TimelineGuard();
+
+  static void enable() { enable_ = true; }
+
+ private:
+  std::string op_name_;
+  kTimelineType timeline_type_;
+  uint64_t ts_start_;
+  inline static bool enable_{false};
+};
+}  // namespace pynative
+}  // namespace mindspore
+#endif  // MINDSPORE_MINDSPORE_CCSRC_RUNTIME_PYNATIVE_ASYNC_THREAD_TIMELINE_H_
