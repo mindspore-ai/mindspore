09fe5b6f6fd28f1e6dd92ec2841e59007014b897
luojianing <luojianing1@huawei.com>
2023-06-13 02:26:52 +0000

    update mindspore/python/mindspore/nn/optim/momentum.py.
    Signed-off-by: luojianing <luojianing1@huawei.com>

--- mindspore/python/mindspore/nn/optim/momentum.py
+++ mindspore/python/mindspore/nn/optim/momentum.py
@@ -173,7 +173,7 @@ class Momentum(Optimizer):
         >>> from mindspore import nn
         >>>
         >>> # Define the network structure of LeNet5. Refer to
-        >>> # https://gitee.com/mindspore/docs/blob/r2.0/docs/mindspore/code/lenet.py
+        >>> # https://gitee.com/mindspore/docs/blob/r1.11/docs/mindspore/code/lenet.py
         >>> net = LeNet5()
         >>> #1) All parameters use the same learning rate and weight decay
         >>> optim = nn.Momentum(params=net.trainable_params(), learning_rate=0.1, momentum=0.9)
