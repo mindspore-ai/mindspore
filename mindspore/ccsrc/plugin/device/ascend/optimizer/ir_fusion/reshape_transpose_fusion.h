a11492766b74dcdeb3bbaedf4c11e400a766bb3c
yuchaojie <yuchaojie1@huawei.com>
2023-06-25 20:30:29 +0800

    add max shape size check for confusiontranspose fusion

--- mindspore/ccsrc/plugin/device/ascend/optimizer/ir_fusion/reshape_transpose_fusion.h
+++ mindspore/ccsrc/plugin/device/ascend/optimizer/ir_fusion/reshape_transpose_fusion.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Huawei Technologies Co., Ltd
+ * Copyright 2020-2023 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@
 namespace mindspore {
 namespace opt {
 bool CheckMatmulNeighborNodes(const FuncGraphPtr &func_graph, const AnfNodePtr &up_node, const AnfNodePtr &down_node);
+bool CheckShapeDimInfo(const ShapeVector &shape);
 
 class ReshapeTransposeFusion : public PatternProcessPassWithSwitch {
  public:
