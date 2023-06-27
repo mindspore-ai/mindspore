b04e7738cea8b30c0cd5e246b300254ab83adcd6
maning202007 <maning36@huawei.com>
2023-06-27 16:54:20 +0800

    Fix some bugs, such as: Use device_id instead of rank_id on GPU platform
    Fix some useless logs
    
    Fix the parameter check issue
    
    Only log one time when not enable profiler to avoid lower speed
    
    only collect memory when the module specify to profile memory

--- mindspore/python/mindspore/profiler/envprofiling.py
+++ mindspore/python/mindspore/profiler/envprofiling.py
@@ -122,6 +122,11 @@ def parse_pubilc_args(options):
             "The 'output_path' parameter of the environment variable MS_PROFILE_OPTIONS only supports absolute path, "
             f"it will be set to '{absolute_path}'.")
         options["output_path"] = absolute_path
+    if options.get("output_path") not in ["memory", "time", "all", None]:
+        logger.warning(
+            "The 'profile_framework' parameter of the environment variable MS_PROFILE_OPTIONS must be one of ['memory',"
+            " 'time', 'all', null], it will be set to 'all'.")
+        options['profile_framework'] = "all"
     return options
 
 
