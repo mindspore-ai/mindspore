9ee42bf759abde8b8407766311c53303970405b4
jpc_chenjianping <jpc.chen@huawei.com>
2023-07-04 15:56:52 +0800

    fix debug package fail|remove inline

--- mindspore/ccsrc/plugin/device/cpu/kernel/nnacl/base/cast_base.c
+++ mindspore/ccsrc/plugin/device/cpu/kernel/nnacl/base/cast_base.c
@@ -36,3 +36,117 @@ void Float32ToInt32(const float *input, int32_t *output, int number) {
     output[index] = (int32_t)input[index];
   }
 }
+
+void BoolToFloat32(const bool *input, float *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (float)input[i];
+  }
+}
+
+void Uint8ToFloat32(const uint8_t *input, float *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (float)input[i];
+  }
+}
+
+void Int32ToFloat32(const int32_t *input, float *output, int number);
+
+void Int64ToFloat32(const int64_t *input, float *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (float)input[i];
+  }
+}
+
+#ifdef ENABLE_FP16
+void Int64ToFp16(const int64_t *input, float16_t *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (float16_t)input[i];
+  }
+}
+
+void Int32ToFp16(const int32_t *input, float16_t *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (float16_t)input[i];
+  }
+}
+
+void BoolToFp16(const bool *input, float16_t *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (float16_t)input[i];
+  }
+}
+
+void Uint8ToFp16(const uint8_t *input, float16_t *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (float16_t)input[i];
+  }
+}
+
+void Float32ToFp16(const float *input, float16_t *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (float16_t)(input[i]);
+  }
+}
+
+void Fp16ToFloat32(const float16_t *input, float *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (float)(input[i]);
+  }
+}
+#else
+void Fp16ToFloat32(const uint16_t *input, float *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = ShortToFloat32(input[i]);
+  }
+}
+
+void Float32ToFp16(const float *input, uint16_t *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = Float32ToShort(input[i]);
+  }
+}
+#endif
+
+void Float32ToInt32(const float *input, int32_t *output, int number);
+
+void Float32ToInt64(const float *input, int64_t *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (int64_t)input[i];
+  }
+}
+
+void Int32ToInt64(const int32_t *input, int64_t *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (int64_t)input[i];
+  }
+}
+
+void Int64ToInt32(const int64_t *input, int32_t *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (int32_t)input[i];
+  }
+}
+
+void Float32ToInt16(const float *input, int16_t *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (int16_t)input[i];
+  }
+}
+
+void BoolToInt32(const bool *input, int32_t *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (int32_t)input[i];
+  }
+}
+
+void Float32ToBool(const float *input, bool *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (bool)input[i];
+  }
+}
+
+void Float32ToUint8(const float *input, uint8_t *output, int number) {
+  for (int i = 0; i < number; ++i) {
+    output[i] = (uint8_t)input[i];
+  }
+}
