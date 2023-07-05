e483f6a9ed0374942a00193f317bb7f1d0c549d9
zhujiaxing <zhujiaxing2@huawei.com>
2023-07-05 20:54:53 +0800

    Modify some docs of profile

--- mindspore/python/mindspore/profiler/profiling.py
+++ mindspore/python/mindspore/profiler/profiling.py
@@ -476,6 +476,8 @@ class Profiler:
             >>> from mindspore import Profiler
             >>> from mindspore import nn
             >>> from mindspore import Model
+            >>> from lenet import LeNet5
+            >>> from mnist import create_dataset
             >>> # Profiler init.
             >>> profiler = Profiler()
             >>> # Train Model or eval Model, taking LeNet5 as an example
@@ -578,6 +580,7 @@ class Profiler:
 
         Examples:
             >>> from mindspore.train import Callback
+            >>> from mindspore import Profiler
             >>> class StopAtStep(Callback):
             ...     def __init__(self, start_step, stop_step):
             ...         super(StopAtStep, self).__init__()
@@ -644,6 +647,7 @@ class Profiler:
 
         Examples:
             >>> from mindspore.train import Callback
+            >>> from mindspore import Profiler
             >>> class StopAtEpoch(Callback):
             ...     def __init__(self, start_epoch, stop_epoch):
             ...         super(StopAtEpoch, self).__init__()
