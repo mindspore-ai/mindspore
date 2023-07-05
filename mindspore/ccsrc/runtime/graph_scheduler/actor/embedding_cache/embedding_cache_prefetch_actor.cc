c072daaa17dc7ad3d68ce72bbad315aaa49e63b8
lizhenyu <lizhenyu13@huawei.com>
2023-07-05 10:55:10 +0800

    optimize local emebedding slice bound value for sparse mode

--- mindspore/ccsrc/runtime/graph_scheduler/actor/embedding_cache/embedding_cache_prefetch_actor.cc
+++ mindspore/ccsrc/runtime/graph_scheduler/actor/embedding_cache/embedding_cache_prefetch_actor.cc
@@ -162,13 +162,8 @@ void EmbeddingCachePrefetchActor::Initialize() {
   LinkRpcOperators();
 
   // Create the device embedding operation.
-  if (!distributed::EmbeddingCacheTableManager::GetInstance().is_sparse_format()) {
-    emb_ops_ = new DeviceDenseEmbeddingOperation(this, device_context_, local_embedding_slice_bounds_,
-                                                 local_device_cache_bounds_, &statistics_info_, stream_id_);
-  } else {
-    emb_ops_ = new DeviceSparseEmbeddingOperation(this, device_context_, local_embedding_slice_bounds_,
-                                                  local_device_cache_bounds_, &statistics_info_, stream_id_);
-  }
+  emb_ops_ = new DeviceDenseEmbeddingOperation(this, device_context_, local_embedding_slice_bounds_,
+                                               local_device_cache_bounds_, &statistics_info_, stream_id_);
   MS_EXCEPTION_IF_NULL(emb_ops_);
   if (!emb_ops_->Initialize()) {
     MS_LOG(ERROR) << "Failed to initialize the device embedding operation.";
