aa2b711a2ac6cf44547fdc27199a50a59d88ebfa
liu-yongqi-63 <liuyongqi5@huawei.com>
2023-07-12 09:36:16 +0800

    fix: add proper error handling when reset is called for pull-based iterators on 2.1.

--- mindspore/ccsrc/minddata/dataset/engine/consumers/tree_consumer.h
+++ mindspore/ccsrc/minddata/dataset/engine/consumers/tree_consumer.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020-2022 Huawei Technologies Co., Ltd
+ * Copyright 2020-2023 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,7 +62,7 @@ class TreeConsumer {
   /// \param step the step to reset the pipeline to.
   /// \param epoch_num the epoch to reset the pipeline to.
   /// \return Status error code
-  Status Reset(int64_t step, const int64_t epoch_num);
+  virtual Status Reset(int64_t step, const int64_t epoch_num);
 
   /// Function to stop the consumer.
   /// \return Status error code
