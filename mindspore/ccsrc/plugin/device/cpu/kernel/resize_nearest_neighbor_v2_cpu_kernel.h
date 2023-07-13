b002c0e5203039fd9c96caae5ed1c5fe62bc9fa6
hw_hz <zhanghao491@huawei.com>
2023-07-13 17:22:10 +0800

    clear_warning

--- mindspore/ccsrc/plugin/device/cpu/kernel/resize_nearest_neighbor_v2_cpu_kernel.h
+++ mindspore/ccsrc/plugin/device/cpu/kernel/resize_nearest_neighbor_v2_cpu_kernel.h
@@ -17,12 +17,9 @@
 #ifndef MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_RESIZE_NEAREST_NEIGHBOR_V2_CPU_KERNEL_H_
 #define MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_CPU_RESIZE_NEAREST_NEIGHBOR_V2_CPU_KERNEL_H_
 
-#include <algorithm>
 #include <map>
-#include <memory>
-#include <unordered_map>
-#include <utility>
 #include <vector>
+#include <utility>
 #include "plugin/device/cpu/kernel/cpu_kernel.h"
 #include "plugin/factory/ms_factory.h"
 
