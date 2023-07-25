259a8d853532b5bdbc4bd70479c1801f8a9ec188
zhoufeng <zhoufeng54@huawei.com>
2023-07-25 20:26:04 +0800

    fix batchmatmul attr name
    Signed-off-by: zhoufeng <zhoufeng54@huawei.com>

--- mindspore/ccsrc/transform/graph_ir/op_declare/matrix_calculation_ops_declare.cc
+++ mindspore/ccsrc/transform/graph_ir/op_declare/matrix_calculation_ops_declare.cc
@@ -173,8 +173,8 @@ OUTPUT_MAP(BatchMatMul) = {{0, OUTPUT_DESC(y)}};
 
 // BatchMatMul->BatchMatMulV2
 INPUT_MAP(BatchMatMulV2) = {{1, INPUT_DESC(x1)}, {2, INPUT_DESC(x2)}, {3, INPUT_DESC(bias)}};
-ATTR_MAP(BatchMatMulV2) = {{"transpose_x1", ATTR_DESC(adj_x1, AnyTraits<bool>())},
-                           {"transpose_x2", ATTR_DESC(adj_x2, AnyTraits<bool>())}};
+ATTR_MAP(BatchMatMulV2) = {{"transpose_a", ATTR_DESC(adj_x1, AnyTraits<bool>())},
+                           {"transpose_b", ATTR_DESC(adj_x2, AnyTraits<bool>())}};
 OUTPUT_MAP(BatchMatMulV2) = {{0, OUTPUT_DESC(y)}};
 REG_ADPT_DESC(BatchMatMul, kNameBatchMatMul, ADPT_DESC(BatchMatMul))
 REG_ADPT_DESC(BatchMatMulV2, kNameBatchMatMulV2, ADPT_DESC(BatchMatMulV2))
