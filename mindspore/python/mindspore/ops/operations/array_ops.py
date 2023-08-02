ce6fe02f809fc7e9381d3539caebc91d28ed5b6d
lianliguang <lianliguang@huawei.com>
2023-08-02 11:31:52 +0800

    rectify stridedslice doc

--- mindspore/python/mindspore/ops/operations/array_ops.py
+++ mindspore/python/mindspore/ops/operations/array_ops.py
@@ -3552,9 +3552,7 @@ class StridedSlice(PrimitiveWithInfer):
     Inputs:
         - **input_x** (Tensor) - The input Tensor to be extracted from.
         - **begin** (tuple[int]) - A tuple which represents the location where to start.
-          Only non-negative int is allowed.
         - **end** (tuple[int]) - A tuple or which represents the maximum location where to end.
-          Only non-negative int is allowed.
         - **strides** (tuple[int]) - A tuple which represents the strides is continuously added
           before reaching the maximum location. Only int is allowed, it can be negative
           which results in reversed slicing.
