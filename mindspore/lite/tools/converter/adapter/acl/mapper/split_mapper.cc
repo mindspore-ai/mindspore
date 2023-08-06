e739aa3b22d11fd97c090d7fd4ab423c9f1db04e
yefeng <yefeng24@huawei.com>
2023-08-06 10:34:43 +0800

    split mapper

--- mindspore/lite/tools/converter/adapter/acl/mapper/split_mapper.cc
+++ mindspore/lite/tools/converter/adapter/acl/mapper/split_mapper.cc
@@ -32,6 +32,13 @@ STATUS SplitMapper::Mapper(const CNodePtr &cnode) {
   auto split_num_val = prim->GetAttr(ops::kOutputNum);
   CHECK_NULL_RETURN(split_num_val);
   prim->AddAttr("num_split", split_num_val);
+  if (cnode->size() == opt::kInputSizeThree) {
+    auto dst_prim = std::make_shared<acl::SplitV>();
+    if (MoveAttrMap(cnode, dst_prim) != RET_OK) {
+      MS_LOG(ERROR) << "Split mapper failed.";
+      return RET_ERROR;
+    }
+  }
   int status = AddIntAttrToInput(func_graph, cnode, prim, ops::kAxis, false);
   if (status != RET_OK) {
     MS_LOG(ERROR) << "Add axis constant value to input failed.";
