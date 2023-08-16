628cdd17ea5f3bcab6e91865a111c0ccf6c8323d
xuyongfei <xuyongfei@huawei.com>
2023-08-16 10:00:55 +0800

    MSLite, add st for config parse error

--- mindspore/lite/test/st/python/test_inference_cloud_nocofig.py
+++ mindspore/lite/test/st/python/test_inference_cloud_nocofig.py
@@ -22,6 +22,8 @@ from functools import wraps
 import numpy as np
 import mindspore_lite as mslite
 
+error_happened = []
+
 
 def lite_test(func):
     @wraps(func)
@@ -30,11 +32,21 @@ def lite_test(func):
             print(f"[BEGIN] {str(func.__name__)}")
             func(*args, **kwargs)
             print(f"[SUCCESS] {str(func.__name__)}")
-        except Exception as e:
+        except Exception:  # pylint: disable=W0703
             traceback.print_exc()
             print(f"[FAILED] {str(func.__name__)}")
-            raise
+            global error_happened
+            error_happened.append(str(func.__name__))
+
     return wrap_test
+
+
+def handle_error():
+    if error_happened:
+        print(f"test_inference_cloud_nocofig.py: run testcases failed: {error_happened}")
+        sys.exit(1)
+    else:
+        print(f"test_inference_cloud_nocofig.py: run testcases success")
 
 
 # ============================ ascend inference ============================
@@ -65,12 +77,31 @@ def test_model_group_inference_ascend(mindir_dir):
         assert (outputs[0].get_data_to_numpy() == (np.ones((4, 4), np.float32) * 2)).all()
 
 
+@lite_test
+def test_model_invalid_dynamic_dims_error_ascend(mindir_dir):
+    context = mslite.Context()
+    context.target = ["ascend"]
+    context.ascend.device_id = 0
+    context.ascend.provider = "ge"
+    model0 = mslite.Model()
+
+    model_path0 = os.path.join(mindir_dir, "model_group_first.mindir")
+    config_dict = {"ascend_context": {"input_format": "NHWC", "input_shape": "input:[1,-1,-1,3]",
+                                      "dynamic_dims": "[19200,960],960"}}
+    try:
+        model0.build_from_file(model_path0, mslite.ModelType.MINDIR, context, "", config_dict)
+        assert False
+    except RuntimeError as ex:
+        assert "build_from_file failed" in str(ex)
+
+
 if __name__ == '__main__':
     print("test_inference_cloud_nocofig.py: begin run testcases.")
     model_dir = sys.argv[1]
     backend = sys.argv[2]
     if backend == "Ascend":
         test_model_group_inference_ascend(model_dir)
+        test_model_invalid_dynamic_dims_error_ascend(model_dir)
     else:
         print(f'test_inference_cloud_nocofig.py: skip backend {backend}!')
-    print("test_inference_cloud_nocofig.py: run testcases success.")
+    handle_error()
