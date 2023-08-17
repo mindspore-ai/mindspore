8e8cc0910de587b9513d9f42b2dc9461649980b7
ZPaC <zhoupeichen@huawei.com>
2023-08-17 19:24:32 +0800

    Fix static 1.11

--- mindspore/ccsrc/distributed/cluster/cluster_context.cc
+++ mindspore/ccsrc/distributed/cluster/cluster_context.cc
@@ -266,7 +266,7 @@ void ClusterContext::PostProcessForCGN() {
     // 2. Set this node's client ip address in this cluster.
     client_ip_in_cluster_ = cgn->client_ip();
     MS_LOG(INFO) << "Client ip address in this cluster of this compute graph node is " << client_ip_in_cluster_;
-    common::SetEnv(kEnvWorkerClientIp, client_ip_in_cluster_.c_str());
+    (void)common::SetEnv(kEnvWorkerClientIp, client_ip_in_cluster_.c_str());
 
     // 3. Set port range of this node.
     std::string port_range_pb = cgn->GetMetadata(kNodePortRange);
