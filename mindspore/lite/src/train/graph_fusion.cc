4d14ee9bef8e3bbbeb07fec376a5fb422d1957ef
zhangyanhui <zhangyanhui17@huawei.com>
2023-08-17 16:59:07 +0800

    add matmul-matmul-add fusion in fb graph

--- mindspore/lite/src/train/graph_fusion.cc
+++ mindspore/lite/src/train/graph_fusion.cc
@@ -23,6 +23,7 @@
 #include "src/train/optimizer/fusion/gru_fusion_pass.h"
 #include "src/train/optimizer/fusion/matmul_activation_fusion_pass.h"
 #include "src/train/optimizer/fusion/reshape_gather_reshape_fusion_pass.h"
+#include "src/train/optimizer/fusion/matmul_matmul_add_fusion_pass.h"
 #include "tools/converter/legacy_optimizer/graph/isolated_node_remove_pass.h"
 #include "tools/converter/legacy_optimizer/graph/subgraph_node_pass.h"
 
@@ -53,6 +54,7 @@ STATUS GraphFusion::Run(schema::MetaGraphT *graph) {
   fusion_optimizer.AddPass(new (std::nothrow) ReshapeGatherReshapeFusionPass());
   fusion_optimizer.AddPass(new (std::nothrow) MatMulBiasAddFusionPass());
   fusion_optimizer.AddPass(new (std::nothrow) MatMulActivationFusionPass());
+  fusion_optimizer.AddPass(new (std::nothrow) MatMulMatMulAddFusionPass());
   fusion_optimizer.AddPass(new (std::nothrow) IsolatedNodeRemovePass());
   fusion_optimizer.AddPass(new (std::nothrow) SubgraphNodePass(old_nodes));
   auto status = fusion_optimizer.Run(graph);
