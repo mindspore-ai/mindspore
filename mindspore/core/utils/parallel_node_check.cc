f45659af6cd3531eb0bf8d85ac507990c75ee751
liuluobin <liuluobin@huawei.com>
2023-08-19 16:04:12 +0800

    Remove FillV2 from ParallelBlackList

--- mindspore/core/utils/parallel_node_check.cc
+++ mindspore/core/utils/parallel_node_check.cc
@@ -30,7 +30,7 @@ static const std::set<std::string> PARALLEL_BLACK_LIST_ = {mindspore::kTupleGetI
   "array_getitem", "tuple_setitem", "Depend", "list_setitem", "array_setitem", "dict_getitem",
   "list_append", "list_map", "list_reduce", "tuple_reversed", "tile_shape", "tuple_div", "tuple_to_array",
   "make_dict", "make_slice", "string_eq", "VirtualLoss", "Return", "env_getitem", "TensorShape", "ScalarToTensor",
-  "partial", "env_setitem", "env_getitem", "env_add", "Shape", "ScalarSub", "ScalarAdd", "FillV2",
+  "partial", "env_setitem", "env_getitem", "env_add", "Shape", "ScalarSub", "ScalarAdd",
   "dot", "im2col", "col2im", "im2col_v1", "state_setitem", "ScalarSummary", "ScalarFloordiv", "ScalarCast",
   "ImageSummary", "TensorSummary", "Debug", "HistogramSummary", "col2im_v1", "resolve", "BroadcastGradientArgs",
   "InvertPermutation", "DropoutGenMask", "StatelessDropOutGenMask", "embed", "create_instance", "RefToEmbed",
