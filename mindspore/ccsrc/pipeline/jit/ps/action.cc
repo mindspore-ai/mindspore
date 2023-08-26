075219769b505546b4fcf1535fc46003b8e26c8a
ligan <ligan15@huawei.com>
2023-08-26 15:03:17 +0800

    precompile_only

--- mindspore/ccsrc/pipeline/jit/ps/action.cc
+++ mindspore/ccsrc/pipeline/jit/ps/action.cc
@@ -1653,6 +1653,11 @@ std::vector<ActionItem> VmPipeline(const ResourcePtr &resource) {
     }
 #endif
   }
+  auto is_precompile_only = MsContext::GetInstance()->get_param<bool>(MS_CTX_PRECOMPILE_ONLY);
+  if (is_precompile_only) {
+    MS_LOG(INFO) << "PrecompileOnly, stop run graph";
+    return actions;
+  }
   auto ms_context = MsContext::GetInstance();
   MS_EXCEPTION_IF_NULL(ms_context);
 #ifndef WITH_BACKEND
--- tests/st/dyn_shape_dev/test_ops_celu.py
+++ tests/st/dyn_shape_dev/test_ops_celu.py
@@ -16,15 +16,12 @@ import pytest
 import numpy as np
 import mindspore.nn as nn
 from mindspore import Tensor, context
-from mindspore.gen_ops_def import CeLU
-
-context.set_context(mode=context.GRAPH_MODE, device_target='CPU')
-
+from mindspore.ops import operations as P
 
 class CeluTEST(nn.Cell):
     def __init__(self, alpha):
         super(CeluTEST, self).__init__()
-        self.celu = CeLU(alpha)
+        self.celu = P.CeLU(alpha)
 
     def construct(self, x):
         return self.celu(x)
@@ -32,23 +29,35 @@ class CeluTEST(nn.Cell):
 
 @pytest.mark.level1
 @pytest.mark.env_onecard
-@pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_x86_cpu_training
 @pytest.mark.parametrize("data_type", [np.float32, np.float16])
-def test_celu_op(data_type):
+def test_celu_op_cpu(data_type):
     """
     Feature: Celu cpu kernel
     Description: test the celu alpha = 1.0.
     Expectation: match to np benchmark.
     """
-    error = 1e-3
     celu = CeluTEST(1.)
     x = Tensor(np.array([-2.0, -1.0, 1.0, 2.0]).astype(data_type))
-    expect = np.array([-0.8646, -0.6321, 1., 2.]).astype(data_type)
-    context.set_context(mode=context.GRAPH_MODE)
+    context.set_context(mode=context.GRAPH_MODE, device_target='CPU', precompile_only=True)
     output = celu(x)
     print(output)
-    np.testing.assert_allclose(output.asnumpy(), expect, rtol=error)
-    # context.set_context(mode=context.PYNATIVE_MODE)
-    # output = celu(x)
-    # print(output)
-    # np.testing.assert_allclose(output.asnumpy(), expect, rtol=error)
+    assert output is None
+
+
+@pytest.mark.level1
+@pytest.mark.env_onecard
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.parametrize("data_type", [np.float32, np.float16])
+def test_celu_op_gpu(data_type):
+    """
+    Feature: Celu gpu kernel
+    Description: test the celu alpha = 1.0.
+    Expectation: match to np benchmark.
+    """
+    celu = CeluTEST(1.)
+    x = Tensor(np.array([-2.0, -1.0, 1.0, 2.0]).astype(data_type))
+    context.set_context(mode=context.GRAPH_MODE, device_target='GPU', precompile_only=True)
+    output = celu(x)
+    print(output)
+    assert output is None
