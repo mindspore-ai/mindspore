de4a5f6613ed9bf6c27a2c5329fbce11a5e426cb
yangruoqi713 <yangruoqi@huawei.com>
2023-08-30 14:38:09 +0800

    [OPS][Dynamic] move python generate code command from shell to cmake

--- cmake/gencode.cmake
+++ cmake/gencode.cmake
@@ -1,28 +1,20 @@
-set(C_GEN_OPS ${CMAKE_BINARY_DIR}/gen_ops_def.cc CACHE STRING "FILE:gen_ops_def.cc")
-set(yaml_file "${CMAKE_SOURCE_DIR}/mindspore/python/mindspore/ops_generate/ops.yaml")
-# generate operation definition code, include python/mindspore/ops_generate/gen_ops_def.py
-# and core/ops/ops_generate/gen_ops_def.cc
-add_custom_command(
-                OUTPUT "${CMAKE_SOURCE_DIR}/mindspore/python/mindspore/ops_generate/gen_ops_def.py"
-                ${C_GEN_OPS}
-                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-                COMMAND "${PYTHON_EXECUTABLE}"
-                "${CMAKE_SOURCE_DIR}/mindspore/python/mindspore/ops_generate/gen_ops.py" ${CMAKE_SOURCE_DIR}
-                DEPENDS ${yaml_file} "${CMAKE_SOURCE_DIR}/mindspore/python/mindspore/ops_generate/gen_ops.py"
-                COMMENT "Generate operator python/c++ definitions" VERBATIM)
+if("${PYTHON_EXECUTABLE}" STREQUAL "")
+    set(PYTHON_EXECUTABLE "python3")
+else()
+    set(PYTHON_EXECUTABLE "${PYTHON_EXECUTABLE}")
+endif()
 
-set(enum_yaml_file "${CMAKE_SOURCE_DIR}/mindspore/python/mindspore/ops_generate/enum.yaml")
-add_custom_command(
-        OUTPUT "${CMAKE_SOURCE_DIR}/mindspore/python/mindspore/ops_generate/gen_enum_def.py"
-        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-        COMMAND "${PYTHON_EXECUTABLE}"
-        "${CMAKE_SOURCE_DIR}/mindspore/python/mindspore/ops_generate/gen_enum.py" ${CMAKE_SOURCE_DIR}
-        DEPENDS ${enum_yaml_file}
-        COMMENT "Generate enum python definitions" VERBATIM)
+# generate operation definition code, include python/mindspore/ops/auto_generate/gen_ops_def.py
+# and core/ops/ops_generate/gen_ops_def.cc
+execute_process(COMMAND "${PYTHON_EXECUTABLE}"
+        "${CMAKE_SOURCE_DIR}/mindspore/python/mindspore/ops_generate/gen_ops.py")
+execute_process(COMMAND "${PYTHON_EXECUTABLE}"
+        "${CMAKE_SOURCE_DIR}/mindspore/python/mindspore/ops_generate/gen_enum.py")
 
 add_custom_target(generated_code DEPENDS
-        "${CMAKE_SOURCE_DIR}/mindspore/python/mindspore/ops_generate/gen_enum_def.py"
-        "${CMAKE_SOURCE_DIR}/mindspore/python/mindspore/ops_generate/gen_ops_def.py"
-        ${C_GEN_OPS})
-set_source_files_properties(${C_GEN_OPS} PROPERTIES GENERATED TRUE)
-add_library(c_gen_ops OBJECT ${C_GEN_OPS})
+        "${CMAKE_SOURCE_DIR}/mindspore/python/mindspore/ops/auto_generate/gen_enum_def.py"
+        "${CMAKE_SOURCE_DIR}/mindspore/python/mindspore/ops/auto_generate/gen_ops_def.py"
+        "${CMAKE_SOURCE_DIR}/mindspore/core/ops/gen_ops_def.cc"
+        "${CMAKE_SOURCE_DIR}/mindspore/core/ops/gen_lite_ops.h"
+        "${CMAKE_SOURCE_DIR}/mindspore/core/ops/gen_ops_name.h"
+        "${CMAKE_SOURCE_DIR}/mindspore/core/ops/gen_ops_primitive.h")
