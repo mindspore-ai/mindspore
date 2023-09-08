aeded127f11c5f37cb8779d92162e58a1e48ca77
Xin Du <duxin45@huawei.com>
2023-09-08 16:50:58 +0800

    fix bugs in remove_format_transform_pair

--- mindspore/ccsrc/plugin/device/gpu/optimizer/remove_format_transform_pair.cc
+++ mindspore/ccsrc/plugin/device/gpu/optimizer/remove_format_transform_pair.cc
@@ -42,28 +42,49 @@ const AnfNodePtr RemoveFormatTransformPair::Process(const FuncGraphPtr &graph, c
   MS_EXCEPTION_IF_NULL(graph);
   MS_EXCEPTION_IF_NULL(node);
   MS_LOG(DEBUG) << "Process node:" << node->fullname_with_scope();
-  auto input_node = common::AnfAlgo::GetInputNode(utils::cast<CNodePtr>(node), 0);
-  MS_EXCEPTION_IF_NULL(input_node);
-  if (common::AnfAlgo::GetCNodeName(node) != prim::kPrimTranspose->name() ||
-      common::AnfAlgo::GetCNodeName(input_node) != prim::kPrimTranspose->name()) {
+  auto transpose2 = utils::cast<CNodePtr>(node);
+  auto transpose1 = utils::cast<CNodePtr>(common::AnfAlgo::GetInputNode(transpose2, 0));
+  auto prim_name = prim::kPrimTranspose->name();
+  if (common::AnfAlgo::GetCNodeName(transpose1) != prim_name ||
+      common::AnfAlgo::GetCNodeName(transpose2) != prim_name) {
     MS_LOG(EXCEPTION) << "The  pattern is not transpose pair, "
-                      << "node:" << common::AnfAlgo::GetCNodeName(node)
-                      << " node input:" << common::AnfAlgo::GetCNodeName(input_node);
+                      << "node:" << common::AnfAlgo::GetCNodeName(transpose2)
+                      << " node input:" << common::AnfAlgo::GetCNodeName(transpose1);
   }
   // If transpose operator used by more than one other operators, it cant not be deleted directly.
-  if (IsUsedByOthers(graph, input_node)) {
-    MS_LOG(DEBUG) << "The transpose node [" << input_node->fullname_with_scope()
+  if (IsUsedByOthers(graph, transpose1)) {
+    MS_LOG(DEBUG) << "The transpose node [" << transpose1->fullname_with_scope()
                   << "] is used by more than one other operators.";
     return nullptr;
   }
-  auto transpose1_input_shape = AnfAlgo::GetInputDeviceShape(input_node, 0);
-  auto transpose2_output_shape = AnfAlgo::GetOutputDeviceShape(node, 0);
-  if (transpose2_output_shape == transpose1_input_shape) {
-    auto transpose1_input_node = common::AnfAlgo::GetInputNode(utils::cast<CNodePtr>(input_node), 0);
-    MS_EXCEPTION_IF_NULL(transpose1_input_node);
-    return transpose1_input_node;
+
+  auto transpose1_input_format = AnfAlgo::GetInputFormat(transpose1, 0);
+  auto transpose2_output_format = AnfAlgo::GetOutputFormat(transpose2, 0);
+  if (transpose1_input_format != transpose2_output_format) {
+    MS_LOG(DEBUG) << "The input format of the first transpose is different from the output format of the second "
+                     "transpose, can't remove this transpose pair.";
+    return nullptr;
   }
-  return nullptr;
+  auto perm1 = common::AnfAlgo::GetInputNode(transpose1, 1);
+  auto perm2 = common::AnfAlgo::GetInputNode(transpose2, 1);
+  auto perm1_value = perm1->cast<ValueNodePtr>()->value();
+  auto perm2_value = perm2->cast<ValueNodePtr>()->value();
+  auto perm1_vec = CheckAndConvertUtils::CheckTensorIntValue("permutation1", perm1_value, prim_name);
+  auto perm2_vec = CheckAndConvertUtils::CheckTensorIntValue("permutation2", perm2_value, prim_name);
+  auto dim = perm1_vec.size();
+  for (size_t i = 0; i < dim; i++) {
+    MS_EXCEPTION_IF_CHECK_FAIL(i < perm2_vec.size(), "perm is out of bound");
+    auto index = perm2_vec[i] < 0 ? perm2_vec[i] + dim : perm2_vec[i];
+    MS_EXCEPTION_IF_CHECK_FAIL(index < dim, "perm is out of bound");
+    auto axis = perm1_vec[index] < 0 ? perm1_vec[index] + dim : perm1_vec[index];
+    if (static_cast<size_t>(axis) != i) {
+      MS_LOG(DEBUG) << "Permutation changes, can't remove this transpose pair";
+      return nullptr;
+    }
+  }
+  auto transpose1_input_node = common::AnfAlgo::GetInputNode(utils::cast<CNodePtr>(transpose1), 0);
+  MS_EXCEPTION_IF_NULL(transpose1_input_node);
+  return transpose1_input_node;
 }
 }  // namespace opt
 }  // namespace mindspore
