8e0e2b5241a5cf0246f83737479914a5dd4ba58e
yide12 <wangyide10@huawei.com>
2023-09-08 12:58:55 +0800

    add_one_hot_bce_loss_st

---
+++ tests/st/ops/test_func_binary_cross_entropy_with_logits.py
@@ -0,0 +1,48 @@
+# Copyright 2023 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+import numpy as np
+import pytest
+
+import mindspore as ms
+import mindspore.nn as nn
+
+
+class Net(nn.Cell):
+    def construct(self, x, y):
+        output = ms.ops.binary_cross_entropy_with_logits(x, y)
+        return output
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.platform_arm_cpu
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_ascend_training
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('mode', [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
+def test_ops_binary_cross_entropy_with_logits(mode):
+    """
+    Feature: ops.binary_cross_entropy_with_logits
+    Description: Verify the result of binary_cross_entropy_with_logits
+    Expectation: success
+    """
+    ms.set_context(mode=mode)
+    x = ms.Tensor(np.array([[-0.8, 1.2, 0.7], [-0.1, -0.4, 0.7]]), ms.float32)
+    y = ms.Tensor(np.array([[0.3, 0.8, 1.2], [-0.6, 0.1, 2.2]]), ms.float32)
+    net = Net()
+    output = net(x, y)
+    expect_output = 0.34636116
+    assert np.allclose(output.asnumpy(), expect_output)
