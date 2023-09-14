178b69a8f1d3ba156356897d732e77181de2ea5c
yangruoqi713 <yangruoqi@huawei.com>
2023-09-14 11:46:07 +0800

    [OPS][Dynamic] add deprecate label for yaml

--- mindspore/python/mindspore/ops/auto_generate/__init__.py
+++ mindspore/python/mindspore/ops/auto_generate/__init__.py
@@ -19,12 +19,13 @@ Primitive operator classes and operator functional.
 A collection of operators to build neural networks or to compute functions.
 """
 
-from . import gen_ops_def, gen_arg_handler, gen_labels
+from . import gen_ops_def, gen_arg_handler, gen_labels, gen_enum_def
 from ..operations.manually_defined import ops_def
 
 from .gen_ops_def import *
 from .gen_arg_handler import *
 from .gen_labels import *
+from .gen_enum_def import *
 from ..operations.manually_defined.ops_def import *
 
 __all__ = []
---
+++ mindspore/python/mindspore/ops_generate/gen_utils.py
@@ -0,0 +1,144 @@
+# Copyright 2023-2025 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""
+Generate operator utils function
+"""
+import os
+import glob
+import hashlib
+import yaml
+
+py_licence_str = f"""# Copyright 2023 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""
+
+cc_license_str = f"""/**
+ * Copyright 2023 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */"""
+
+
+def get_type_str(type_str):
+    """
+    Get the unified type str for operator arg dtype.
+    """
+    # add more type here
+    type_kind_dict = {
+        'int': 'int',
+        'tuple[int]': 'tuple',
+        'list[int]': 'list',
+        'float': 'float',
+        'tuple[float]': 'tuple',
+        'list[float]': 'list',
+        'bool': 'bool',
+        'tuple[bool]': 'tuple',
+        'list[bool]': 'list',
+        'number': 'Number',
+        'tensor': 'Tensor',
+        'tuple[tensor]': 'tuple',
+        'list[tensor]': 'list',
+    }
+    if type_str in type_kind_dict:
+        return type_kind_dict[type_str]
+    raise TypeError(f"""Unsupported type {type_str} for args.""")
+
+
+def get_file_md5(file_path):
+    """
+    Get the md5 value for file.
+    """
+    if not os.path.exists(file_path):
+        return ""
+    if os.path.isdir(file_path):
+        return ""
+    with open(file_path, 'rb') as f:
+        data = f.read()
+    md5_value = hashlib.md5(data).hexdigest()
+    return md5_value
+
+
+def check_change_and_replace_file(last_file_path, tmp_file_path):
+    """
+    Compare tmp_file with the md5 value of the last generated file.
+    If the md5 value is the same, retain the last generated file.
+    Otherwise, update the last generated file to tmp_file.
+    """
+    last_md5 = get_file_md5(last_file_path)
+    tmp_md5 = get_file_md5(tmp_file_path)
+
+    if last_md5 == tmp_md5:
+        os.system(f'rm {tmp_file_path}')
+    else:
+        os.system(f'mv {tmp_file_path} {last_file_path}')
+
+
+def merge_files_to_one_file(file_paths, merged_file_path):
+    """
+    Merge multiple files into one file.
+    """
+    merged_content = ''
+    file_paths.sort()
+    for file_path in file_paths:
+        with open(file_path, 'r') as file:
+            merged_content += file.read()
+            merged_content += '\n'
+    one_file = open(merged_file_path, 'w')
+    one_file.write(merged_content)
+    one_file.close()
+
+
+def merge_files(origin_dir, merged_file_path, file_format):
+    """
+    Merge multiple files into one file.
+    origin_dir: indicates the origin file directory.
+    merged_file_path: indicates the merged file path.
+    file_format: indicates the format of regular matching.
+    Files whose names meet the regular matching in 'origin_dir' directory will be merged into one file.
+    """
+    op_yaml_file_names = glob.glob(os.path.join(origin_dir, file_format))
+    merge_files_to_one_file(op_yaml_file_names, merged_file_path)
+
+
+def safe_load_yaml(yaml_file_path):
+    """
+    Load yaml dictionary from file.
+    """
+    yaml_str = dict()
+    with open(yaml_file_path, 'r') as yaml_file:
+        yaml_str.update(yaml.safe_load(yaml_file))
+    return yaml_str
