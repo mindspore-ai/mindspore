954729691ca7302d3f3e9d3b27c4f6a4eb968b6c
huoxinyou <huoxinyou1@huawei.com>
2023-09-15 10:17:53 +0800

    update akg

--- mindspore/ccsrc/plugin/device/cpu/kernel/tensor_copy_slices_cpu_kernel.cc
+++ mindspore/ccsrc/plugin/device/cpu/kernel/tensor_copy_slices_cpu_kernel.cc
@@ -105,8 +105,11 @@ bool TensorCopySlicesCpuKernelMod::Launch(const std::vector<kernel::AddressPtr> 
   auto output_addr = reinterpret_cast<uint8_t *>(outputs[0]->addr);
   if (!get_value_before_launch_) {
     auto begin_ptr = GetDeviceAddress<int64_t>(inputs, kIndex2);
+    MS_EXCEPTION_IF_NULL(begin_ptr);
     auto end_ptr = GetDeviceAddress<int64_t>(inputs, kIndex3);
+    MS_EXCEPTION_IF_NULL(end_ptr);
     auto strides_ptr = GetDeviceAddress<int64_t>(inputs, kIndex4);
+    MS_EXCEPTION_IF_NULL(strides_ptr);
     std::vector<int64_t> begin{begin_ptr, begin_ptr + begin_shape_[0]};
     std::vector<int64_t> end{end_ptr, end_ptr + end_shape_[0]};
     std::vector<int64_t> stride{strides_ptr, strides_ptr + stride_shape_[0]};
