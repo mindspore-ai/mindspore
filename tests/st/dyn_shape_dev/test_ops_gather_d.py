fc99f7e83c1cd57ae16f477d770a5521b99f5589
ling <lingqiaomin@huawei.com>
2023-09-21 10:35:29 +0800

    gather_d vmap st

--- tests/st/dyn_shape_dev/test_ops_gather_d.py
+++ tests/st/dyn_shape_dev/test_ops_gather_d.py
@@ -70,6 +70,8 @@ def test_gather_d_backward():
 
 @pytest.mark.level0
 @pytest.mark.env_onecard
+@pytest.mark.platform_x86_cpu
+@pytest.mark.platform_x86_gpu_training
 @pytest.mark.platform_arm_ascend_training
 def test_gather_d_vmap():
     """
