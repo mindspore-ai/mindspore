86069dbac6ee5de005c60a18acc5b49096529251
ling <lingqiaomin@huawei.com>
2023-09-22 09:47:55 +0800

    floor st

--- tests/st/dyn_shape_dev/test_ops_floor.py
+++ tests/st/dyn_shape_dev/test_ops_floor.py
@@ -33,6 +33,7 @@ def floor_backward_func(x):
 @pytest.mark.env_onecard
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
 def test_floor_forward():
     """
     Feature: Ops.
@@ -49,6 +50,7 @@ def test_floor_forward():
 @pytest.mark.env_onecard
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
 def test_floor_backward():
     """
     Feature: Auto grad.
@@ -65,6 +67,7 @@ def test_floor_backward():
 @pytest.mark.env_onecard
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
 def test_floor_vmap():
     """
     Feature: test vmap function.
