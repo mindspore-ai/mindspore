8ed9d62dcc1c031bd6fb1076bfe74a9eccbb3643
yangruoqi713 <yangruoqi@huawei.com>
2023-09-22 14:27:57 +0800

    [OPS][Dynamic] remove fail testcase

--- tests/st/dyn_shape_dev/test_round.py
+++ tests/st/dyn_shape_dev/test_round.py
@@ -33,7 +33,6 @@ def round_backward_func(x):
 @pytest.mark.env_onecard
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_x86_gpu_training
-@pytest.mark.platform_arm_ascend_training
 def test_round():
     """
     Feature: Ops.
@@ -55,7 +54,6 @@ def test_round():
 @pytest.mark.env_onecard
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_x86_gpu_training
-@pytest.mark.platform_arm_ascend_training
 def test_round_vmap():
     """
     Feature: test vmap function.
