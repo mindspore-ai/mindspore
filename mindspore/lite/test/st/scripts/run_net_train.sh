a17d4819a4874d10e6c1151f9a33f3b951f076db
zhou_chao1993 <zhouchao46@huawei.com>
2023-09-22 11:58:42 +0800

    deleter converter_lite nofusion param

--- mindspore/lite/test/st/scripts/run_net_train.sh
+++ mindspore/lite/test/st/scripts/run_net_train.sh
@@ -157,7 +157,7 @@ function parse_line() {
     suffix_print=""
     check_convert=0
     do_api=false
-    no_opt="--NoFusion=false"
+    no_opt="--optimize=general"
     expression=0
     model_name=${line_array[0]}_train
     while [[ $i < ${#line_array[@]} ]] ; do
@@ -205,7 +205,7 @@ function parse_line() {
           "expression")
             model_name="${line_array[0]}_expr"
             do_api=true
-            no_opt="--NoFusion=true"
+            no_opt="--optimize=none"
             expression=1
             ;;
           *)
