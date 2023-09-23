e8c3371d29666ac963e325fabe095a4e20b6ad4a
ustc_hb <hubin79@huawei.com>
2023-09-23 15:06:51 +0800

    change json file mod

--- mindspore/ccsrc/plugin/device/ascend/kernel/aicpu/aicpu_ops/customize/utils/aicpu_parser_ini.py
+++ mindspore/ccsrc/plugin/device/ascend/kernel/aicpu/aicpu_ops/customize/utils/aicpu_parser_ini.py
@@ -321,8 +321,7 @@ def write_json_file(aicpu_ops_info, json_file_path):
         os.remove(json_file_real_path)
     with os.fdopen(os.open(json_file_real_path, flags, modes), "w") as json_file:
         # Only the owner and group have rights
-        os.chmod(json_file_real_path, stat.S_IWGRP + stat.S_IWUSR + stat.S_IRGRP
-                 + stat.S_IRUSR)
+        os.chmod(json_file_real_path, stat.S_IWUSR + stat.S_IRUSR)
         json.dump(aicpu_ops_info, json_file, sort_keys=True,
                   indent=4, separators=(',', ':'))
     print("Compile aicpu op info cfg successfully.")
