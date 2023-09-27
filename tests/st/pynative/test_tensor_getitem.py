46d005b9bb7964e9399191dad52284ca188fc4d1
shaojunsong <shaojunsong@huawei.com>
2023-09-27 16:27:42 +0800

    fix tensor.item() testcase

--- tests/st/pynative/test_tensor_getitem.py
+++ tests/st/pynative/test_tensor_getitem.py
@@ -242,7 +242,7 @@ class TensorItemByNone(Cell):
         return ret
 
 
-@pytest.mark.level1
+@pytest.mark.level0
 @pytest.mark.platform_arm_ascend_training
 @pytest.mark.platform_x86_ascend_training
 @pytest.mark.platform_x86_gpu_training
@@ -255,7 +255,7 @@ def test_item_by_none():
     input_3d_ms = Tensor(input_3d_np, mstype.float32)
 
     output_ms = net(input_1d_ms)
-    assert np.all(output_ms.asnumpy() == input_1d_np.item())
+    assert np.all(output_ms == input_1d_np.item())
 
     with pytest.raises(ValueError):
         net(input_3d_ms)
