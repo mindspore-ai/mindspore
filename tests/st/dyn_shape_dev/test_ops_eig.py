405e8120f0a233797852f289b00831343b9c2876
ling <lingqiaomin@huawei.com>
2023-09-27 17:59:25 +0800

    eig ascend st

--- tests/st/dyn_shape_dev/test_ops_eig.py
+++ tests/st/dyn_shape_dev/test_ops_eig.py
@@ -27,6 +27,8 @@ def eig_forward_func(input_x, compute_v):
 @pytest.mark.level0
 @pytest.mark.env_onecard
 @pytest.mark.platform_x86_cpu
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
 def test_eig_forward():
     """
     Feature: Ops.
