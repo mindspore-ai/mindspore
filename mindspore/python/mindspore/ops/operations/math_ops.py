367ebe5cb77fb753c3bf11db9211fc813b8e60bb
mengyuanli <limengyuan4@huawei.com>
2023-09-28 09:30:35 +0800

    fix bug of error log

--- mindspore/python/mindspore/ops/operations/math_ops.py
+++ mindspore/python/mindspore/ops/operations/math_ops.py
@@ -90,10 +90,10 @@ class _MathBinaryOp(_BinaryOp):
         """Staticmethod of infer dtype for _MathBinaryOp."""
         args_type = {"x": x_dtype, "y": y_dtype}
         complex_types = [mstype.TensorType(mstype.complex64), mstype.TensorType(mstype.complex128)]
-        if (not isinstance(x_dtype, type(mstype.tensor_type))) or \
-           (not isinstance(y_dtype, type(mstype.tensor_type))):
-            raise TypeError('Only Tensor type support Complex')
         if x_dtype in complex_types or y_dtype in complex_types:
+            if (not isinstance(x_dtype, type(mstype.tensor_type))) or \
+               (not isinstance(y_dtype, type(mstype.tensor_type))):
+                raise TypeError('Only Tensor type support Complex')
             type_infer_dict = {
                 (mstype.complex64, mstype.complex64): mstype.TensorType(mstype.complex64),
                 (mstype.complex64, mstype.float32): mstype.TensorType(mstype.complex64),
