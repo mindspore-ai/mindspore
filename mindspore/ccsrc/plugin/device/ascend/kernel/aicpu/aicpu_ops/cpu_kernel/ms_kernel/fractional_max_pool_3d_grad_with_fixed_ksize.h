d09676611e7cb32539d455695d10f6d2faf53114
zengxianglong <zengxianglong1@huawei.com>
2023-09-28 13:41:41 +0800

    fix fractional_max_pool_3d_grad_with_fixed_ksize precision problem

--- mindspore/ccsrc/plugin/device/ascend/kernel/aicpu/aicpu_ops/cpu_kernel/ms_kernel/fractional_max_pool_3d_grad_with_fixed_ksize.h
+++ mindspore/ccsrc/plugin/device/ascend/kernel/aicpu/aicpu_ops/cpu_kernel/ms_kernel/fractional_max_pool_3d_grad_with_fixed_ksize.h
@@ -17,41 +17,23 @@
 #ifndef AICPU_KERNELS_NORMALIZED_FRACTIONAL_MAX_POOL3D_GRAD_WITH_FIXED_KSIZE_H_
 #define AICPU_KERNELS_NORMALIZED_FRACTIONAL_MAX_POOL3D_GRAD_WITH_FIXED_KSIZE_H_
 
+#include <vector>
 #include "cpu_kernel/inc/cpu_ops_kernel.h"
 #include "cpu_kernel/inc/cpu_types.h"
 
 namespace aicpu {
-struct TmpVar {
-  int64_t c_dim = 0;
-  int64_t d_dim = 0;
-  int64_t h_dim = 0;
-  int64_t w_dim = 0;
-  int64_t inputN = 0;
-  int64_t inputC = 0;
-  int64_t inputT = 0;
-  int64_t inputH = 0;
-  int64_t inputW = 0;
-  int64_t outputT = 0;
-  int64_t outputH = 0;
-  int64_t outputW = 0;
-};
-
 class FractionalMaxPool3DGradWithFixedKsizeCpuKernel : public CpuKernel {
  public:
   FractionalMaxPool3DGradWithFixedKsizeCpuKernel() = default;
   ~FractionalMaxPool3DGradWithFixedKsizeCpuKernel() override = default;
 
  protected:
-  uint32_t GetInputAndCheck(const CpuKernelContext &ctx);
+  uint32_t GetInputAndCheck(CpuKernelContext &ctx);
   uint32_t Compute(CpuKernelContext &ctx) override;
   template <typename backprop_t, typename argmax_t>
-  uint32_t Process4DCase(const CpuKernelContext &ctx, TmpVar *tmp_var);
-  template <typename backprop_t, typename argmax_t>
-  uint32_t Process5DCase(const CpuKernelContext &ctx, TmpVar *tmp_var);
-  template <typename backprop_t, typename argmax_t>
-  uint32_t FractionalMaxPool3DGradWithFixedKsizeOutCpuTemplate(const CpuKernelContext &ctx);
+  uint32_t FractionalMaxPool3DGradWithFixedKsizeOutCpuTemplate(CpuKernelContext &ctx);
   template <typename backprop_t>
-  uint32_t DoComputeWithArgmaxType(const CpuKernelContext &ctx, DataType argmax_type);
+  uint32_t DoComputeWithArgmaxType(CpuKernelContext &ctx, DataType argmax_type);
   std::vector<int64_t> input_shape;
   std::vector<int64_t> out_backprop_shape;
   std::vector<int64_t> argmax_shape;
