f1892a62a2b1b3155de2695e9c1e32650fac224d
yuchaojie <yuchaojie1@huawei.com>
2023-10-09 15:52:09 +0800

    move SetAclOpPrecisionMode to GeKernelExecutor::Initialize

--- mindspore/ccsrc/transform/acl_ir/acl_utils.h
+++ mindspore/ccsrc/transform/acl_ir/acl_utils.h
@@ -194,8 +194,6 @@ class AclRunner {
 
   void SetPrecisionMode(const AclPrecisionMode mode);
 
-  void SetOpPrecisionMode();
-
   void ResizeOpInputs(size_t size) {
     acl_param_.input_desc.clear();
     acl_param_.input_desc.resize(size, nullptr);
