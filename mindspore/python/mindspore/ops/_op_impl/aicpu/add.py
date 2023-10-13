ddd194920408b2a88db3e2f0a21640bd5ea976c1
panzhihui <panzhihui3@huawei.com>
2023-10-13 19:42:31 +0800

    Fix add aicpu registration

--- mindspore/python/mindspore/ops/_op_impl/aicpu/add.py
+++ mindspore/python/mindspore/ops/_op_impl/aicpu/add.py
@@ -29,9 +29,9 @@ add_op_info = AiCPURegOp("Add") \
     .dtype_format(DataType.I32_Default, DataType.I32_Default, DataType.I32_Default) \
     .dtype_format(DataType.I64_Default, DataType.I64_Default, DataType.I64_Default) \
     .dtype_format(DataType.U8_Default, DataType.U8_Default, DataType.U8_Default) \
-    .dtype_format(DataType.U16_Default, DataType.I16_Default, DataType.I16_Default) \
-    .dtype_format(DataType.U32_Default, DataType.I32_Default, DataType.I32_Default) \
-    .dtype_format(DataType.U64_Default, DataType.I64_Default, DataType.I64_Default) \
+    .dtype_format(DataType.U16_Default, DataType.U16_Default, DataType.U16_Default) \
+    .dtype_format(DataType.U32_Default, DataType.U32_Default, DataType.U32_Default) \
+    .dtype_format(DataType.U64_Default, DataType.U64_Default, DataType.U64_Default) \
     .dtype_format(DataType.C64_Default, DataType.C64_Default, DataType.C64_Default) \
     .dtype_format(DataType.C128_Default, DataType.C128_Default, DataType.C128_Default) \
     .get_op_info()
