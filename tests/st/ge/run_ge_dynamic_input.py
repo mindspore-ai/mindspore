486317b4499ed2135b5f112be7937a610f90aa8c
hbhu_bin <hubin79@huawei.com>
2023-10-17 15:07:10 +0800

    fix-ge-st

---
+++ tests/st/ge/run_ge_dynamic_input.py
@@ -0,0 +1,44 @@
+# Copyright 2023 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+import mindspore
+from mindspore import context
+from mindspore import Tensor, nn
+from mindspore.ops import operations as P
+import numpy as np
+import ge_infer_env  # pylint: disable=unused-import
+
+context.set_context(mode=context.GRAPH_MODE, device_target="Ascend")
+
+
+def test_ge_dynamic_input():
+    """
+    Description: Test GE dynamic input.
+    Description: Support dynamic inputs.
+    Expectation: Run without errors.
+    """
+
+    class Net(nn.Cell):
+        def __init__(self):
+            super().__init__()
+            self.addn = P.AddN()
+
+        def construct(self, *x):
+            return self.addn(x)
+
+    net = Net()
+    x1 = Tensor(np.array([4, 1, 3]), mindspore.int32)
+    x2 = Tensor(np.array([2, 1, 5]), mindspore.int32)
+    x3 = Tensor(np.array([9, 0, 2]), mindspore.int32)
+    net(x1, x2, x3, x3, x2, x1)
--- tests/st/ge/test_ge_call.py
+++ tests/st/ge/test_ge_call.py
@@ -12,21 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-import os
-import sys
 import pytest
-
-
-def run_testcase(file_name, case_name=""):
-    log_file = file_name + "_" + case_name + '.log'
-    if case_name == "":
-        ret = os.system(f'{sys.executable} {file_name}.py &> {log_file}')
-    else:
-        ret = os.system(f"{sys.executable} -c 'import {file_name};{file_name}.{case_name}()' &> {log_file}")
-    os.system(f'grep -E "CRITICAL|ERROR|Error" {log_file} -C 3')
-    os.system(f'rm {log_file} -rf')
-    assert ret == 0
-
+from ge_test_utils import run_testcase
 
 @pytest.mark.level0
 @pytest.mark.platform_arm_ascend_training
