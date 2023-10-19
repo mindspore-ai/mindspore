ff4a6e39f4c8346caa0d78a09b1dd0df1d6d593e
ling <lingqiaomin@huawei.com>
2023-10-19 15:03:05 +0800

    PromptKVCache & DecoderKVCache

---
+++ mindspore/core/ops/decoder_k_v_cache.cc
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2023 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ops/decoder_k_v_cache.h"
+#include "abstract/abstract_value.h"
+#include "abstract/dshape.h"
+#include "abstract/ops/op_infer.h"
+#include "abstract/ops/primitive_infer_map.h"
+#include "abstract/utils.h"
+#include "base/base.h"
+#include "ir/anf.h"
+#include "ir/dtype/number.h"
+#include "ir/primitive.h"
+#include "mindapi/base/type_id.h"
+#include "mindapi/src/helper.h"
+#include "mindspore/core/ops/nn_ops.h"
+#include "ops/op_name.h"
+#include "ops/primitive_c.h"
+#include "utils/check_convert_utils.h"
+#include "utils/convert_utils_base.h"
+#include "utils/log_adapter.h"
+#include "ops/lite_ops.h"
+
+namespace mindspore {
+namespace ops {
+MIND_API_OPERATOR_IMPL(DecoderKVCache, BaseOperator);
+class DecoderKVCacheInfer : public abstract::OpInferBase {
+ public:
+  BaseShapePtr InferShape(const PrimitivePtr &primitive,
+                          const std::vector<AbstractBasePtr> &input_args) const override {
+    return input_args.front()->BuildShape()->Clone();
+  }
+  TypePtr InferType(const PrimitivePtr &prim, const std::vector<AbstractBasePtr> &input_args) const override {
+    return input_args.front()->BuildType()->Clone();
+  }
+};
+REGISTER_PRIMITIVE_OP_INFER_IMPL(DecoderKVCache, prim::kPrimDecoderKVCache, DecoderKVCacheInfer, false);
+}  // namespace ops
+}  // namespace mindspore
