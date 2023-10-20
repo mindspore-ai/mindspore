6f62e54333a6a6429eba489a98796c44e171b831
qiuyue_laura <liaoqiuyue1@huawei.com>
2023-10-20 13:56:49 +0800

    [huawei] add st test for exp
    [huawei] modify some format

---
+++ tests/st/ops/ascend/test_exp_op.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pytest
+
+import mindspore.context as context
+import mindspore.nn as nn
+from mindspore import ops, Tensor
+from mindspore.common import dtype as mstype
+
+
+class NetExp(nn.Cell):
+    def __init__(self):
+        super(NetExp, self).__init__()
+        self.exp = ops.exp
+
+    def construct(self, x):
+        return self.exp(x)
+
+
+def test_exp_tensor_api(ms_type):
+    """
+    Feature: Test exp forward tensor api given data type.
+    Description: Test exp tensor api.
+    Expectation: The result match to the expect value.
+    """
+    x_ms = Tensor([0.2, 0.74, 0.04], ms_type)
+    exp = NetExp()
+    output = exp(x_ms)
+    output = output.float().asnumpy() if ms_type == mstype.bfloat16 else output.asnumpy()
+
+    expected = np.array([1.2216413, 2.0923362, 1.0408515]).astype(np.float32)
+    assert np.allclose(output, expected, rtol=0.004, atol=0.004)
+
+
+@pytest.mark.level0
+@pytest.mark.platform_arm_ascend910b_training
+@pytest.mark.env_onecard
+def test_exp_tensor_bf16_api():
+    """
+    Feature: Test exp forward tensor api.
+    Description: Test exp for bfloat16.
+    Expectation: the result match with the expected result.
+    :return:
+    """
+    context.set_context(mode=context.GRAPH_MODE, device_target="Ascend")
+    test_exp_tensor_api(mstype.bfloat16)
+    context.set_context(mode=context.PYNATIVE_MODE, device_target="Ascend")
+    test_exp_tensor_api(mstype.bfloat16)
