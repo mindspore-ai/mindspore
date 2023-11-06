53b0e13d8a8d6f01f700913c37b4a5d62afb2fa2
jiaxueyu <jiaxueyu@huawei.com>
2023-11-06 20:40:42 +0800

    remove shape infervalue to ops_frontend_func_impl

--- mindspore/core/ops/ops_func_impl/shape.cc
+++ mindspore/core/ops/ops_func_impl/shape.cc
@@ -41,39 +41,5 @@ TypePtr ShapeFuncImpl::InferType(const PrimitivePtr &primitive, const std::vecto
   return std::make_shared<Tuple>(TypePtrList(shape.size(), kInt64));
 }
 
-class ShapeFrontendFuncImpl : public OpFrontendFuncImpl {
- public:
-  ValuePtr InferValue(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) const override {
-    auto in_shape_ptr = input_args[0]->GetShape();
-    const auto &inshape = in_shape_ptr->GetShapeVector();
-    if (IsDynamic(inshape)) {
-      // If the input of shape is dynamic shape/rank tensor, value can not be directly built.
-      // Run infer of shape.
-      return nullptr;
-    }
-    return MakeValue(inshape);
-  }
-  AbstractBasePtr InferAbstract(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) const {
-    auto input_shape_ptr = input_args[kIndex0]->GetShape();
-    const auto &in_shape = input_shape_ptr->GetShapeVector();
-    AbstractBasePtrList abs_list;
-    // Set shape value to kValueAny when dynamic shape/rank.
-    (void)std::transform(in_shape.begin(), in_shape.end(), std::back_inserter(abs_list),
-                         [](int64_t item) -> std::shared_ptr<abstract::AbstractScalar> {
-                           auto ret = std::make_shared<abstract::AbstractScalar>(item);
-                           if (item == abstract::Shape::kShapeRankAny || item == abstract::Shape::kShapeDimAny) {
-                             ret->set_value(kValueAny);
-                           }
-                           return ret;
-                         });
-    auto abs = std::make_shared<abstract::AbstractTuple>(abs_list);
-    if (IsDynamicRank(in_shape)) {
-      abs->CheckAndConvertToDynamicLenSequence();
-    }
-    return abs;
-  }
-};
-REGISTER_PRIMITIVE_FUNCTION_FRONTEND_FUNC_IMPL("Shape", ShapeFrontendFuncImpl);
-
 }  // namespace ops
 }  // namespace mindspore
