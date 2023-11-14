efab110bc2ee01fd1dfaab3cd1278b0631ff88ef
z00838343 <zhengyi74@huawei.com>
2023-11-14 14:44:23 +0800

    add came parallel

--- mindspore/ccsrc/frontend/parallel/parameter_manager.h
+++ mindspore/ccsrc/frontend/parallel/parameter_manager.h
@@ -17,6 +17,7 @@
 #ifndef MINDSPORE_CCSRC_FRONTEND_PARALLEL_PARAMETER_MANAGER_H_
 #define MINDSPORE_CCSRC_FRONTEND_PARALLEL_PARAMETER_MANAGER_H_
 
+#include <set>
 #include <vector>
 #include <string>
 #include <utility>
@@ -25,6 +26,7 @@
 #include "base/base.h"
 #include "frontend/parallel/device_manager.h"
 #include "frontend/parallel/step_parallel_utils.h"
+#include "frontend/parallel/came_parallel_handler.h"
 #include "pipeline/jit/ps/resource.h"
 #include "pybind11/pybind11.h"
 
@@ -32,10 +34,6 @@ namespace py = pybind11;
 
 namespace mindspore {
 namespace parallel {
-constexpr char EXP_AVG[] = "exp_avg";
-constexpr char EXP_AVG_SQ_ROW[] = "exp_avg_sq_row_";
-constexpr char EXP_AVG_SQ_COL[] = "exp_avg_sq_col_";
-constexpr char EXP_AVG_SQ[] = "exp_avg_sq_";
 constexpr char OBJ[] = "obj";
 constexpr char CLONED_OBJ[] = "cloned_obj";
 constexpr char SLICE_PARAMETER_FN_PATH[] = "mindspore.parallel._utils";
@@ -54,7 +52,8 @@ void HandleSymbolicKeyInstance(const FuncGraphPtr &root, const std::vector<AnfNo
 void HandleNoUsedParameter(const FuncGraphPtr &root);
 void HandleFullySplitParameters(const FuncGraphPtr &root);
 void SetClonedTensorShapeForOptimizer(const FuncGraphPtr &root);
-void HandleAdaFactorOpt(const FuncGraphPtr &root);
+void HandleCameAndAdaFactorOpt(const FuncGraphPtr &root, const std::vector<AnfNodePtr> &all_nodes,
+                               const FuncGraphManagerPtr &manager);
 void AutoParallelPostProcess(const FuncGraphPtr &root);
 void SliceTensorObj(const ParameterPtr &parameter, const TensorLayoutPtr &tensor_layout, size_t rank_id = 0);
 // Init the parameters for graph which not specified by shard under PyNative mode.
