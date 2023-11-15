5469f542dd0a42a67a2148ad7755491b5e5fe045
lby <liubuyu1@huawei.com>
2023-11-15 16:31:32 +0800

    fix renorm precision problem on 910B

--- mindspore/ccsrc/plugin/device/ascend/optimizer/mindir/renorm_split.cc
+++ mindspore/ccsrc/plugin/device/ascend/optimizer/mindir/renorm_split.cc
@@ -1,5 +1,5 @@
 /**
- * Copyright 2022 Huawei Technologies Co., Ltd
+ * Copyright 2022-2023 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,11 @@
  * limitations under the License.
  */
 
-#include "plugin/device/ascend/optimizer/ir_fission/renorm_split.h"
+#include "plugin/device/ascend/optimizer/mindir/renorm_split.h"
 #include <algorithm>
 #include <memory>
 #include <vector>
+#include "include/common/utils/anfalgo.h"
 #include "ops/nn_op_name.h"
 #include "ops/array_ops.h"
 #include "ops/math_ops.h"
