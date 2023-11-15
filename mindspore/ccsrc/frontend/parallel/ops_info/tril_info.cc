3161c0a19dc1ca26993054c823c1ff01bf9d8a01
xiao_yao1994 <adam.xiaoyao@huawei.com>
2023-11-15 19:43:40 +0800

    add tril op distribute impl

---
+++ mindspore/ccsrc/frontend/parallel/ops_info/tril_info.cc
@@ -0,0 +1,83 @@
+/**
+ * Copyright 2023 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <utility>
+#include <algorithm>
+
+#include "frontend/parallel/dynamic_creator.h"
+#include "frontend/parallel/ops_info/tril_info.h"
+
+namespace mindspore {
+namespace parallel {
+Status TrilInfo::CheckStrategy(const StrategyPtr &strategy) {
+  if (CheckStrategyValue(strategy, inputs_shape_) != SUCCESS) {
+    return FAILED;
+  }
+
+  auto input_num = strategy->GetInputNumber();
+  if (input_num != 1) {
+    MS_LOG(ERROR) << name_ << ": The strategy size must be equal to 1"
+                  << ", but got strategie size is " << input_num;
+    return FAILED;
+  }
+  // The strategy for each input tensor must be equal
+  Strategies strategies = strategy->GetInputDim();
+  auto stra_value = strategies.at(0);
+  constexpr size_t smallest_stra_len = 2;
+  if (stra_value.size() < smallest_stra_len) {
+    MS_LOG(ERROR) << name_ << ": The strategy value size must be greater than 2"
+                  << ", but got strategie value size is " << stra_value.size();
+    return FAILED;
+  }
+  auto last_stra = *(stra_value.rbegin());
+  if (last_stra != 1) {
+    MS_LOG(ERROR) << name_ << ": The last strategy value must be equal to 1, but got " << last_stra;
+    return FAILED;
+  }
+  auto penultimate_stra = *(stra_value.rbegin() + 1);
+  if (penultimate_stra != 1) {
+    MS_LOG(ERROR) << name_ << ": The penultimate strategy value must be equal to 1, but got " << penultimate_stra;
+    return FAILED;
+  }
+  return SUCCESS;
+}
+
+std::vector<StrategyPtr> TrilInfo::GenerateOpStrategies(int64_t stage_id) {
+  if ((inputs_shape_.size() != 1)) {
+    MS_LOG(EXCEPTION) << name_ << " : Inputs shape size is wrong.";
+  }
+
+  Shape input0_split(inputs_shape_.at(0).size(), 1);
+  *(input0_split.rbegin()) = 0;
+  *(input0_split.rbegin() + 1) = 0;
+  Shapes splittable_inputs = {input0_split};
+
+  std::vector<StrategyPtr> sp_vector;
+  if (GenerateStrategiesForIndependentInputs(stage_id, inputs_shape_, splittable_inputs, &sp_vector) != SUCCESS) {
+    MS_LOG(EXCEPTION) << name_ << " : Generate strategies for independent inputs failed.";
+  }
+  return sp_vector;
+}
+
+void TrilInfo::ReComputeBatchSplitFlagList() {
+  // The last two dim can not be spilt
+  constexpr size_t last_two_dim = 2;
+  split_flag_list_[0] = inputs_shape_[0].size() > last_two_dim ? true : false;
+}
+
+REGISTER(TrilInfo);
+}  // namespace parallel
+}  // namespace mindspore
