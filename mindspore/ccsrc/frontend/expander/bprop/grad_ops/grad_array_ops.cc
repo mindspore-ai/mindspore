e0d90d4e0f5ddae4ef7f70ac664573d18d061d65
chenfei <chenfei52@huawei.com>
2023-11-23 21:33:47 +0800

    fix yolov7 bug

--- mindspore/ccsrc/frontend/expander/bprop/grad_ops/grad_array_ops.cc
+++ mindspore/ccsrc/frontend/expander/bprop/grad_ops/grad_array_ops.cc
@@ -869,6 +869,7 @@ REG_BPROP_BUILDER("ResizeNearestNeighbor").SetUnusedInputs({i0, i4}).SetBody(BOD
     shape = ib->EmitValue(MakeValue(new_shape));
   } else {
     shape = ib->ShapeCalc(g_resize_nearest_neighbor, {x})[0];
+    shape = ib->TensorToTuple(shape);
   }
   auto dx = ib->Emit("ResizeNearestNeighborGrad", {dout, shape, align_corners, half_pixel_centers}, {});
   return {dx, ib->OutZeros(ib->GetInput(kIndex1)), ib->OutZeros(align_corners), ib->OutZeros(half_pixel_centers)};
