f79d470873b6c5b207c515c1b1e6915598b98b11
VectorSL <864733542@qq.com>
2023-11-23 20:20:08 +0800

    update aclnn matmul for transpose

--- mindspore/ccsrc/plugin/device/ascend/kernel/opapi/aclnn/batch_matmul_aclnn_kernel.cc
+++ mindspore/ccsrc/plugin/device/ascend/kernel/opapi/aclnn/batch_matmul_aclnn_kernel.cc
@@ -13,32 +13,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "plugin/device/ascend/kernel/opapi/aclnn/matmul_aclnn_kernel.h"
+#include "plugin/device/ascend/kernel/opapi/aclnn/batch_matmul_aclnn_kernel.h"
 #include <vector>
+#include <algorithm>
 #include "ir/tensor.h"
 #include "runtime/stream.h"
-#include "transform/acl_ir/acl_helper.h"
-#include "abstract/ops/primitive_infer_map.h"
+#include "runtime/device/kernel_runtime.h"
 
 namespace mindspore {
 namespace kernel {
-void MMAclnnKernelMod::GetWorkSpaceInfo(const std::vector<KernelTensor *> &inputs,
-                                        const std::vector<KernelTensor *> &outputs) {
+void BMMAclnnKernelMod::GetWorkSpaceInfo(const std::vector<KernelTensor *> &inputs,
+                                         const std::vector<KernelTensor *> &outputs) {
   const auto &attr_list = primitive()->attrs();
-  bool trans_a = false;
-  bool trans_b = false;
-  if (attr_list.at("transpose_a") && attr_list.at("transpose_b")) {
-    trans_a = GetValue<bool>(attr_list.at("transpose_a"));
-    trans_b = GetValue<bool>(attr_list.at("transpose_b"));
+  if (attr_list.at("transpose_a") || attr_list.at("transpose_b")) {
+    MS_LOG(EXCEPTION) << "Please check attr transpose_a or transpose_b in BatchMatMul";
   }
+  auto trans_a = GetValue<bool>(attr_list.at("transpose_a"));
+  auto trans_b = GetValue<bool>(attr_list.at("transpose_b"));
   input_a_ = std::pair<KernelTensor *, bool>(inputs[kIndex0], trans_a);
   input_b_ = std::pair<KernelTensor *, bool>(inputs[kIndex0], trans_b);
+
   auto return_value = GEN_EXECUTOR(op_type_, input_a_, input_b_, outputs[kIndex0], OpApiUtil::GetCubeMathType());
   UpdateWorkspace(return_value);
 }
 
-bool MMAclnnKernelMod::Launch(const std::vector<KernelTensor *> &inputs, const std::vector<KernelTensor *> &workspace,
-                              const std::vector<KernelTensor *> &outputs, void *stream_ptr) {
+bool BMMAclnnKernelMod::Launch(const std::vector<KernelTensor *> &inputs, const std::vector<KernelTensor *> &workspace,
+                               const std::vector<KernelTensor *> &outputs, void *stream_ptr) {
   MS_EXCEPTION_IF_NULL(stream_ptr);
   ParseGenExecutor(GEN_EXECUTOR(op_type_, input_a_, input_b_, outputs[kIndex0], OpApiUtil::GetCubeMathType()));
   RunOp(stream_ptr, workspace);
