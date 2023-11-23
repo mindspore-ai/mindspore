5b87e825491bb53e91919246f162188708e1a2cd
wang_shaocong <wangshaocong1@huawei.com>
2023-11-23 19:17:40 +0800

    [MSLITE] code clean

--- mindspore/lite/src/litert/delegate/nnapi/op/conv_nnapi.cc
+++ mindspore/lite/src/litert/delegate/nnapi/op/conv_nnapi.cc
@@ -17,7 +17,6 @@
 #include "src/litert/delegate/nnapi/op/conv_nnapi.h"
 #include <algorithm>
 #include <vector>
-#include <unordered_map>
 #include "src/litert/delegate/nnapi/nnapi_utils.h"
 #include "src/common/utils.h"
 #include "nnacl/op_base.h"
