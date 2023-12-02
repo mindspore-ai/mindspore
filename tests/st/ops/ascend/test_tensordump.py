0cbc7d09a4cbc797d76ddc81b47dd498a53ae3a2
fandawei <fandawei2@huawei.com>
2023-12-02 10:56:21 +0800

    TensorDump support save temp Tensor & fix pynative PermissionError

--- tests/st/ops/ascend/test_tensordump.py
+++ tests/st/ops/ascend/test_tensordump.py
@@ -52,11 +52,13 @@ def test_net(dtype, mode):
             self.dump = ops.TensorDump()
             self.path_x1 = str(path / "x1")
             self.path_x2 = str(path / "x2")
+            self.path_x3 = str(path / "x3")
             self.path_out = str(path / "out")
 
-        def construct(self, x1, x2):
+        def construct(self, x1, x2, x3):
             self.dump(self.path_x1, x1)
             self.dump(self.path_x2, x2)
+            self.dump(self.path_x3, x3)
             out = x1 + x2
             self.dump(self.path_out, out)
             return out
@@ -66,14 +68,16 @@ def test_net(dtype, mode):
     path = Path(temp_dir.name)
     x1 = ops.randint(0, 10, (3, 5)).astype(dtype)
     x2 = ops.randint(0, 10, (3, 5)).astype(dtype)
+    x3 = Tensor((), dtype=dtype)
     net = Net(path)
-    output = net(Tensor(x1), Tensor(x2))
+    output = net(x1, x2, x3)
     out = output.asnumpy()
     time.sleep(0.1)
     name2file = find_npy_files(path)
 
     assert np.allclose(x1.asnumpy(), np.load(name2file["x1"]))
     assert np.allclose(x2.asnumpy(), np.load(name2file["x2"]))
+    assert np.allclose(x3.asnumpy(), np.load(name2file["x3"]))
     assert np.allclose(out, np.load(name2file["out"]))
 
 
@@ -95,11 +99,13 @@ def test_net_bool(mode):
             self.dump = ops.TensorDump()
             self.path_x1 = str(path / "x1")
             self.path_x2 = str(path / "x2")
+            self.path_x3 = str(path / "x3")
             self.path_out = str(path / "out")
 
-        def construct(self, x1, x2):
+        def construct(self, x1, x2, x3):
             self.dump(self.path_x1, x1)
             self.dump(self.path_x2, x2)
+            self.dump(self.path_x3, x3)
             out = ops.logical_and(x1, x2)
             self.dump(self.path_out, out)
             return out
@@ -109,11 +115,13 @@ def test_net_bool(mode):
     path = Path(temp_dir.name)
     x1 = Tensor(np.array([True, False, True]), ms.bool_)
     x2 = Tensor(np.array([True, True, False]), ms.bool_)
+    x3 = Tensor((), ms.bool_)
     net = Net(path)
-    output = net(Tensor(x1), Tensor(x2))
+    output = net(x1, x2, x3)
     out = output.asnumpy()
     time.sleep(0.1)
     name2file = find_npy_files(path)
     assert np.allclose(x1.asnumpy(), np.load(name2file["x1"]))
     assert np.allclose(x2.asnumpy(), np.load(name2file["x2"]))
+    assert np.allclose(x3.asnumpy(), np.load(name2file["x3"]))
     assert np.allclose(out, np.load(name2file["out"]))
