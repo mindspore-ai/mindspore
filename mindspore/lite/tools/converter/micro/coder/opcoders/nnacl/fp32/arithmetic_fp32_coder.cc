af0aa61081e37be2442e1dd1cd08f901f3c7c52d
zhangyanhui <zhangyanhui17@huawei.com>
2023-12-04 15:21:48 +0800

    clean code for r2.2.10

--- mindspore/lite/tools/converter/micro/coder/opcoders/nnacl/fp32/arithmetic_fp32_coder.cc
+++ mindspore/lite/tools/converter/micro/coder/opcoders/nnacl/fp32/arithmetic_fp32_coder.cc
@@ -393,9 +393,9 @@ int ArithmeticFP32Coder::BatchScalarCalc(int task_id, CoderContext *const contex
 }
 
 int ArithmeticFP32Coder::BiasCalc(int task_id, CoderContext *const context, NNaclFp32Serializer *const code) {
-  MS_CHECK_TRUE_RET(
-    static_cast<int>(arithmetic_parameter_->ndim_) - 1 >= 0 && static_cast<int>(arithmetic_parameter_->ndim_) - 1 < 10,
-    RET_ERROR);
+  MS_CHECK_TRUE_RET(static_cast<int>(arithmetic_parameter_->ndim_) - 1 >= 0 &&
+                      static_cast<int>(arithmetic_parameter_->ndim_) - 1 < C10NUM,
+                    RET_ERROR);
   if (support_parallel_) {
     thread_num_ = 1;
   }
