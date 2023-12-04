fe0f1771ebef693534659fe7cfec89bea9b92ea8
Margaret_wangrui <wangrui178@huawei.com>
2023-12-04 15:32:02 +0800

    Adapte test case.

--- tests/st/fallback/test_graph_fallback_side_effect.py
+++ tests/st/fallback/test_graph_fallback_side_effect.py
@@ -133,7 +133,7 @@ def test_fallback_side_effect_dict_2():
     assert out[1] == {'a': 1, 'b': 4, 'c': 3, 'd': 22}
 
 
-@pytest.mark.level1
+@pytest.mark.level0
 @pytest.mark.platform_x86_gpu_training
 @pytest.mark.env_onecard
 def test_fallback_side_effect_nested_net():
@@ -164,7 +164,7 @@ def test_fallback_side_effect_nested_net():
         @ms.jit
         def construct(self, x, y):
             out = InnerNet().inner.act(InnerNet().renew_para(x, y) + x, y)
-            out = out + InnerNet().renew_para(out, y)
+            out = out + (InnerNet().renew_para(out, y) * 1)
             return out
 
     x = ms.Tensor(2, dtype=ms.float64)
@@ -258,7 +258,6 @@ def test_fallback_side_effect_assign_1():
     assert out[1] == 20
 
 
-@pytest.mark.skip("the PyExecute is in the other graph and the parameter not get user data")
 @pytest.mark.level0
 @pytest.mark.platform_x86_gpu_training
 @pytest.mark.env_onecard
