ad0d382f9161a8e89cb7c0bd5c2bc8e069f01e02
xiaoxiongzhu <zhuxiaoxiong@huawei.com>
2023-12-05 12:25:58 +0800

    [feature] clean code for java

--- mindspore/lite/java/src/main/java/com/mindspore/Model.java
+++ mindspore/lite/java/src/main/java/com/mindspore/Model.java
@@ -247,9 +247,9 @@ public class Model {
      * @return input tensor.
      */
     public MSTensor getInputByTensorName(String tensorName) {
-        List<MSTensor> inputTensors = this.getInputs();
-        for (int i = 0; i < inputTensors.size(); i++) {
-            MSTensor tensor = inputTensors.get(i);
+        List<MSTensor> inputs = this.getInputs();
+        for (int i = 0; i < inputs.size(); i++) {
+            MSTensor tensor = inputs.get(i);
             if (tensor.tensorName().equals(tensorName)) {
                 return tensor;
             }
