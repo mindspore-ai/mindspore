91516aed286d9617c4df50fd8c339edc4d1ce742
lizhenyu <lizhenyu13@huawei.com>
2023-12-11 11:37:52 +0800

    BindDevice in device synchronizer

--- mindspore/ccsrc/plugin/device/gpu/hal/device/gpu_device_synchronizer.cc
+++ mindspore/ccsrc/plugin/device/gpu/hal/device/gpu_device_synchronizer.cc
@@ -22,8 +22,9 @@
 namespace mindspore {
 namespace device {
 namespace gpu {
-bool GPUDeviceSynchronizer::SyncDeviceToHost(void *host_ptr, void *device_ptr, size_t size, mindspore::Format format,
-                                             const ShapeVector &shape, size_t stream_id,
+bool GPUDeviceSynchronizer::SyncDeviceToHost(void *host_ptr, void *device_ptr, size_t size,
+                                             const std::string &device_name, uint32_t device_id,
+                                             mindspore::Format format, const ShapeVector &shape, size_t stream_id,
                                              const UserDataPtr &user_data) const {
   MS_EXCEPTION_IF_NULL(host_ptr);
   MS_EXCEPTION_IF_NULL(device_ptr);
@@ -32,6 +33,13 @@ bool GPUDeviceSynchronizer::SyncDeviceToHost(void *host_ptr, void *device_ptr, s
     stream = GPUDeviceManager::GetInstance().default_stream();
   }
   MS_ERROR_IF_NULL(stream);
+
+  auto gpu_device_context = DeviceContextManager::GetInstance().GetOrCreateDeviceContext({device_name, device_id});
+  MS_EXCEPTION_IF_NULL(gpu_device_context);
+  if (!gpu_device_context->device_res_manager_->BindDeviceToCurrentThread(false)) {
+    MS_LOG(WARNING) << "Bind device to current thread failed.";
+  }
+
   CHECK_RET_WITH_RETURN_ERROR(CudaDriver::CopyDeviceMemToHostAsync(host_ptr, device_ptr, size, stream),
                               "CopyHostMemToDeviceAsync failed");
 
@@ -40,8 +48,9 @@ bool GPUDeviceSynchronizer::SyncDeviceToHost(void *host_ptr, void *device_ptr, s
   return true;
 }
 
-bool GPUDeviceSynchronizer::SyncHostToDevice(void *device_ptr, void *host_ptr, size_t size, mindspore::Format format,
-                                             const ShapeVector &shape, size_t stream_id,
+bool GPUDeviceSynchronizer::SyncHostToDevice(void *device_ptr, void *host_ptr, size_t size,
+                                             const std::string &device_name, uint32_t device_id,
+                                             mindspore::Format format, const ShapeVector &shape, size_t stream_id,
                                              const UserDataPtr &user_data) const {
   MS_EXCEPTION_IF_NULL(device_ptr);
   MS_EXCEPTION_IF_NULL(host_ptr);
@@ -50,6 +59,13 @@ bool GPUDeviceSynchronizer::SyncHostToDevice(void *device_ptr, void *host_ptr, s
     stream = GPUDeviceManager::GetInstance().default_stream();
   }
   MS_ERROR_IF_NULL(stream);
+
+  auto gpu_device_context = DeviceContextManager::GetInstance().GetOrCreateDeviceContext({device_name, device_id});
+  MS_EXCEPTION_IF_NULL(gpu_device_context);
+  if (!gpu_device_context->device_res_manager_->BindDeviceToCurrentThread(false)) {
+    MS_LOG(WARNING) << "Bind device to current thread failed.";
+  }
+
   CHECK_RET_WITH_RETURN_ERROR(CudaDriver::CopyHostMemToDeviceAsync(device_ptr, host_ptr, size, stream),
                               "CopyHostMemToDeviceAsync failed");
 
