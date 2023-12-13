70191937ea9512657e811d1e11b85850fd0d90b3
Zhuoranbupt <929623751@qq.com>
2023-12-13 14:59:17 +0800

    Add example case in export docs

--- mindspore/python/mindspore/train/serialization.py
+++ mindspore/python/mindspore/train/serialization.py
@@ -1553,7 +1553,9 @@ def export(net, *inputs, file_name, file_format, **kwargs):
 
             - custom_func (function): Functions for custom defined export policies. This function will be used to
               customize the model during network export. Currently only support for files with mindir format. The
-              function only accepts one input representing the proto object of the mindir file. Default: ``None`` .
+              function only accepts one input representing the proto object of the mindir file. When modifying a model,
+              it is necessary to ensure the correctness of the `custom_func` , otherwise it may lead to model loading
+              failure or functional errors. Default: ``None`` .
 
     Examples:
         >>> import mindspore as ms
@@ -1565,6 +1567,16 @@ def export(net, *inputs, file_name, file_format, **kwargs):
         >>> net = LeNet5()
         >>> input_tensor = Tensor(np.ones([1, 1, 32, 32]).astype(np.float32))
         >>> ms.export(net, input_tensor, file_name='lenet', file_format='MINDIR')
+        >>>
+        >>> # Export model in MindIR format and modified the model info using custom_func
+        >>> # The custom_func only support one input representing the Proto object of the model
+        >>> # And custom_func does not support return value
+        >>> def _custom_func(mindir_model):
+        ...     mindir_model.producer_name = "test11111"
+        ...     mindir_model.producer_version = "11.0"
+        ...     mindir_model.user_info["version"] = "11.0"
+        >>> ms.export(net, input_tensor, file_name="lenet", file_format='MINDIR', custom_func=_custom_func)
+
 
     Tutorial Examples:
         - `Saving and Loading the Model - Saving and Loading MindIR
