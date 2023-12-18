a3c8b4fad8c29ae06c4b3dcd59a015bba0ac342f
yujianfeng <yujianfeng5@huawei.com>
2023-12-18 16:32:53 +0800

    add mind api
    [pijit] bypass tensor operation ignored by graph

---
+++ mindspore/ccsrc/pipeline/jit/pi/graph_build/func_graph_builder.h
@@ -0,0 +1,112 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MINDSPORE_CCSRC_PIPELINE_JIT_PI_GRAPH_BUILD_FUNC_GRAPH_BUILDER_H_
+#define MINDSPORE_CCSRC_PIPELINE_JIT_PI_GRAPH_BUILD_FUNC_GRAPH_BUILDER_H_
+
+#include <vector>
+#include <memory>
+#include <string>
+#include "ir/value.h"
+#include "ops/sequence_ops.h"
+#include "pipeline/jit/ps/parse/parse_base.h"
+
+namespace mindspore {
+class FuncGraphBuilder {
+ public:
+  FuncGraphBuilder() : graph_(std::make_shared<FuncGraph>()) {
+    (void)output_nodes_.emplace_back(NewValueNode(prim::kPrimMakeTuple));
+  }
+  virtual ~FuncGraphBuilder() { converted_py_obj_.clear(); }
+
+  /// \brief Add an input parameter to the graph.
+  ///
+  /// \param[in] obj The input python object.
+  ///
+  /// \return If the input is a tensor, return a fake tensor python object, else return the origin python object.
+  py::object AddInput(const py::object &obj);
+
+  /// \brief Add a cnode to the graph.
+  ///
+  /// \param[in] callable_obj The callable python object.
+  /// \param[in] inputs_obj The input python objects.
+  ///
+  /// \return The python object of the infer result.
+  py::object AddNode(const py::object &callable_obj, const std::vector<py::object> &inputs_obj);
+
+  /// \brief Add a cnode to the graph.
+  ///
+  /// \param[in] callable_value The callable value.
+  /// \param[in] inputs_obj The input python objects.
+  ///
+  /// \return The python object of the infer result.
+  py::object AddNode(const ValuePtr &callable_value, const std::vector<py::object> &inputs_obj);
+
+  /// \brief Add an output node to the graph.
+  ///
+  /// \param[in] output_obj The output python object.
+  ///
+  /// \return Return true if the output object can be used as the output of the graph.
+  bool AddOutput(const py::object &output_obj);
+
+  /// \brief Get the callable python primitive or function.
+  ///
+  /// \param[in] obj The method of a python object.
+  ///
+  /// \return Return the corresponding primitive of function of the func.
+  static Any ConvertMethod(const py::object &obj);
+
+  /// \brief Check if the python object can be converted to a cnode directly.
+  ///
+  /// \param[in] obj A python object.
+  ///
+  /// \return Return true if the python object can be converted to a cnode directly.
+  static bool CheckCallable(const py::object &obj);
+
+  /// \brief Set the final outputs and get the graph.
+  ///
+  /// \return The graph constructed.
+  FuncGraphPtr graph();
+
+  static ValuePtr ConvertPyObjToValue(const py::object &obj);
+
+  static AbstractBasePtr EvalValue(const ValuePtr &value, const AbstractBasePtrList &inputs_abs_list);
+
+  static py::object GetStandardMethod(const std::string &func_name);
+
+ private:
+  static py::object ConvertToPyObj(const AbstractBasePtr &abs);
+
+  static bool CheckCallable(const ValuePtr &value, const AbstractBasePtr &abs);
+
+  static bool CheckGraphOutput(const AbstractBasePtr &abs);
+
+  py::object AddFgCallNode(const FuncGraphPtr &fg, const std::vector<py::object> &inputs_obj);
+
+  bool GetInputNodesAndAbstracts(const ValuePtr &callable_value, const std::vector<py::object> &inputs_obj,
+                                 std::vector<AnfNodePtr> *input_node_list,
+                                 std::vector<AbstractBasePtr> *input_abs_list);
+
+  static AbstractBasePtr DoInferAndCheck(const ValuePtr &callable_value,
+                                         const std::vector<AbstractBasePtr> &input_abs_list);
+
+  FuncGraphPtr graph_{nullptr};
+  bool has_set_output_{false};
+  HashMap<PyObject *, AnfNodePtr> converted_py_obj_;
+  std::vector<AnfNodePtr> output_nodes_;
+};
+}  // namespace mindspore
+#endif  // MINDSPORE_CCSRC_PIPELINE_JIT_PI_GRAPH_BUILD_FUNC_GRAPH_BUILDER_H_
---
+++ tests/ut/cpp/python_input/gtest_input/pipeline/pi/func_graph_builder.py
@@ -0,0 +1,81 @@
+# Copyright 2024 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+from mindspore.ops import Primitive
+from mindspore.ops.operations import _scalar_ops
+
+make_tuple = Primitive("MakeTuple")
+
+
+class FnDict:
+    def __init__(self):
+        self.fn_dict = {}
+
+    def __call__(self, fn):
+        self.fn_dict[fn.__name__] = fn
+
+    def __getitem__(self, name):
+        return self.fn_dict.get(name)
+
+
+# pylint: disable=unused-variable
+def test_add_inputs_and_outputs(tag):
+    """
+    Feature: Build graph in pi_jit.
+    Description: Use the func_graph_builder api to add inputs and add outputs.
+    Expectation: The expected graph is constructed.
+    """
+    fns = FnDict()
+
+    @fns
+    def graph(x, y):
+        return make_tuple(y)
+
+    return fns[tag]
+
+
+def test_add_node(tag):
+    """
+    Feature: Build graph in pi_jit.
+    Description: Use the func_graph_builder api to add cnode.
+    Expectation: The expected graph is constructed.
+    """
+    fns = FnDict()
+    scalar_add = _scalar_ops.ScalarAdd()
+
+    @fns
+    def graph(x, y):
+        return make_tuple(scalar_add(x, y))
+
+    return fns[tag]
+
+
+def test_add_fg_call_node(tag):
+    """
+    Feature: Build graph in pi_jit.
+    Description: Use the func_graph_builder api to add func_graph called node.
+    Expectation: The expected graph is constructed.
+    """
+    fns = FnDict()
+    scalar_add = _scalar_ops.ScalarAdd()
+
+    def graph1(x, y):
+        return make_tuple(scalar_add(x, y))
+
+    @fns
+    def graph(x, y):
+        return make_tuple(graph1(x, y))
+
+    return fns[tag]
