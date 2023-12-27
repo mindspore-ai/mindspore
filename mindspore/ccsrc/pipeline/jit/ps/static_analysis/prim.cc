39e28605375ebe8ba3c847e504babdc90600b2c2
ling <lingqiaomin@huawei.com>
2023-12-27 15:16:07 +0800

    Revert "checkvalidation before infer value"
    This reverts commit 191bd5a7641554d351dedb498fa62a4b749be35a.

--- mindspore/ccsrc/pipeline/jit/ps/static_analysis/prim.cc
+++ mindspore/ccsrc/pipeline/jit/ps/static_analysis/prim.cc
@@ -1277,8 +1277,9 @@ void PrimitiveFunctionEvaluator::CheckArgsSizeAndType(const AbstractBasePtrList 
   }
 }
 
-AbstractBasePtr PrimitiveFunctionEvaluator::OpsInfer(const AbstractBasePtrList &args) {
+AbstractBasePtr PrimitiveFunctionEvaluator::CheckAndInfer(const AbstractBasePtrList &args) {
   if (op_def_ != nullptr) {
+    (void)op_def_->func_impl_.CheckValidation(prim_func_, args);
     if (frontend_func_impl_ != nullptr) {
       auto infer_result = frontend_func_impl_->InferAbstract(prim_func_, args);
       if (infer_result != nullptr) {
@@ -1307,7 +1308,6 @@ EvalResultPtr PrimitiveFunctionEvaluator::EvalPrim(const AnalysisEnginePtr &engi
 
   AbstractBasePtr abs_base = nullptr;
   prim_func_->BeginRecordAddAttr();
-  (void)op_def_->func_impl_.CheckValidation(prim_func_, args);
   if (need_infer_value && frontend_func_impl_ != nullptr) {
     auto value = frontend_func_impl_->InferValue(prim_func_, args);
     if (value != nullptr && !value->ContainsValueAny()) {
@@ -1317,7 +1317,7 @@ EvalResultPtr PrimitiveFunctionEvaluator::EvalPrim(const AnalysisEnginePtr &engi
       return std::make_shared<EvalResult>(abs_base, std::make_shared<AttrValueMap>(added_attrs));
     }
   }
-  abs_base = OpsInfer(args);
+  abs_base = CheckAndInfer(args);
   MS_EXCEPTION_IF_NULL(abs_base);
   prim_func_->EndRecordAddAttr();
   const auto &added_attrs = prim_func_->evaluate_added_attrs();
