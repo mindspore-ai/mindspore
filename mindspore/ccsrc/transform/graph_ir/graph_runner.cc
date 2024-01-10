79f0d51f890a19854cd350ff2e42a2041f850d79
fary86 <fary.fanrui@huawei.com>
2024-01-10 17:58:06 +0800

    fix mslite coredump

--- mindspore/ccsrc/transform/graph_ir/graph_runner.cc
+++ mindspore/ccsrc/transform/graph_ir/graph_runner.cc
@@ -34,10 +34,21 @@
 #endif
 #include "utils/ms_context.h"
 #include "include/common/utils/scoped_long_running.h"
+#include "acl/acl_base.h"
 
 #ifndef ENABLE_LITE_ACL
 namespace py = pybind11;
 #endif
+namespace {
+bool IsInferencePlatform() {
+  const char *soc_name_c = aclrtGetSocName();
+  if (soc_name_c == nullptr) {
+    return false;
+  }
+  std::string soc_name(soc_name_c);
+  return (soc_name.find("310") != std::string::npos) || (soc_name.find("710") != std::string::npos);
+}
+}  // namespace
 namespace mindspore {
 namespace transform {
 std::shared_ptr<::ge::Session> GraphRunner::NewSession(const SessionOptions &sess_options) {
@@ -45,7 +56,7 @@ std::shared_ptr<::ge::Session> GraphRunner::NewSession(const SessionOptions &ses
   std::shared_ptr<::ge::Session> ret;
   auto ms_context = MsContext::GetInstance();
   MS_EXCEPTION_IF_NULL(ms_context);
-  if (ms_context->backend_policy() == "ge" || ms_context->backend_policy() == "ms") {
+  if (ms_context->backend_policy() == "ge" || (ms_context->backend_policy() == "ms" && !IsInferencePlatform())) {
     ret = std::make_shared<::ge::Session>(sess_options);
     if (ret == nullptr) {
       MS_LOG(EXCEPTION) << "Create GE session failed!";
@@ -125,7 +136,7 @@ Status GraphRunner::AddGraph(const std::string &name) {
 #if (defined ENABLE_D) || (defined ENABLE_LITE_ACL)
   auto ms_context = MsContext::GetInstance();
   MS_EXCEPTION_IF_NULL(ms_context);
-  if (ms_context->backend_policy() != "ge" && ms_context->backend_policy() != "ms") {
+  if (ms_context->backend_policy() != "ge" && !(ms_context->backend_policy() == "ms" && !IsInferencePlatform())) {
     return Status::SUCCESS;
   }
   std::set<string> saved_graph = graph_manager_.GetSavedGraphs();
@@ -374,7 +385,7 @@ Status GraphRunner::RunGraphWithStreamAsync(const RunOptions &options, void *str
   struct timeval start_time, end_time;
   (void)gettimeofday(&start_time, nullptr);
 
-  if (ms_context->backend_policy() == "ge" || ms_context->backend_policy() == "ms") {
+  if (ms_context->backend_policy() == "ge" || (ms_context->backend_policy() == "ms" && !IsInferencePlatform())) {
     if (sess_ == nullptr) {
       MS_LOG(ERROR) << "The GE session is null, can't run the graph!";
       return Status::FAILED;
