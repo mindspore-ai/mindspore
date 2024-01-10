2d71741a8c041d2d8131cfadda4e327c737c27e1
Margaret_wangrui <wangrui178@huawei.com>
2024-01-10 09:02:12 +0800

    Handle the slicing of the AdapterTensor.

--- mindspore/ccsrc/pipeline/jit/ps/parse/parse_base.h
+++ mindspore/ccsrc/pipeline/jit/ps/parse/parse_base.h
@@ -115,6 +115,7 @@ const char PYTHON_PARSE_GET_OPERATION_SYMBOL[] = "get_operation_symbol";
 const char PYTHON_PARSE_GET_OPERATION_NAMESPACE_SYMBOL[] = "get_operation_namespace_symbol";
 const char PYTHON_PARSE_IS_CLASS_TENSOR_TYPE[] = "is_tensor_class_type";
 const char PYTHON_PARSE_GET_NAMESPACE_SYMBOL[] = "get_namespace_symbol";
+const char PYTHON_PARSE_IS_BUILTIN_FUNCTION_NAME[] = "is_builtin_function_name";
 const char PYTHON_PARSE_GET_LOCATION[] = "get_location";
 const char PYTHON_PARSE_EXPAND_EXPR_STATEMENT[] = "expand_expr_statement";
 const char PYTHON_PARSE_GENERATE_SCOPE[] = "generate_scope";
