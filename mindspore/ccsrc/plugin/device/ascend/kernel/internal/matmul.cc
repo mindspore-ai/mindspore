24a38bbb98326e2bb6d44f165e4b6f0f40798cb0
yiyanzhi <yiyanzhi@huawei.com>
2024-01-17 21:06:54 +0800

    matmul custom adapter

--- mindspore/ccsrc/plugin/device/ascend/kernel/internal/matmul.cc
+++ mindspore/ccsrc/plugin/device/ascend/kernel/internal/matmul.cc
@@ -23,6 +23,15 @@ internal::OpParamPtr InternalMatMul::CreateOpParam(const std::vector<KernelTenso
   internal::MatMulParam matmul_param;
   param_ptr->opId = internal::OpId::MatMul;
   // setup matmul param from inputs
+  bool transpose_a = primitive_->HasAttr("transpose_a") ? GetValue<bool>(primitive_->GetAttr("transpose_a")) : false;
+  bool transpose_b = primitive_->HasAttr("transpose_b") ? GetValue<bool>(primitive_->GetAttr("transpose_b")) : false;
+  auto shape_a = inputs[0]->GetShapeVector();
+  auto shape_b = inputs[1]->GetShapeVector();
+  int m = (!transpose_a) ? shape_a[0] : shape_a[1];
+  int k = (!transpose_a) ? shape_a[1] : shape_a[0];
+  int n = (!transpose_b) ? shape_b[1] : shape_b[0];
+
+  matmul_param = {transpose_a, transpose_b, {m, k, n}};
   param_ptr->specificParam = matmul_param;
   return param_ptr;
 }
