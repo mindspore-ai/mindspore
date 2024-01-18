eeb55ec277d56afa0d44df5af70d59abf82960cd
hejianheng001 <hejianheng1@huawei.com>
2024-01-18 16:46:02 +0800

    Summary: change ci testcases to single case
    Signed-of-by: hejianheng
    
    Email: hejianheng1@huawei.com

--- tests/st/pi_jit/calculation/test_mul.py
+++ tests/st/pi_jit/calculation/test_mul.py
@@ -14,38 +14,14 @@ def mul(a, b):
 def jit_mul(a, b):
     return a * b
 
-
-standard_test_cases = [
-    ([11], [10]),
-    ([10.0], [11.0]),
-    ([20], [11.0]),
-    ([2.0], [Tensor(np.ones((2, 3)).astype(np.float32))]),
-    ([Tensor(ops.fill(np.float32, (2, 3), 8))], [2.0]),
-    ([Tensor(ops.fill(np.float32, (2, 3), 8))],
-     [Tensor(np.ones((2, 3)).astype(np.float32))]),
-    ([(1.0, 2.0, 3.0)], [Tensor(np.ones((2, 3)).astype(np.float32))]),
-    ([Tensor(np.ones((2, 3)).astype(np.float32))], [(1.0, 2.0, 3.0)]),
-    ([[1.0, 2.0, 3.0]], [Tensor(np.ones((2, 3)).astype(np.float32))]),
-    ([Tensor(np.ones((2, 3)).astype(np.float32))], [[1.0, 2.0, 3.0]]),
-    ([(1.0, 2.0, 3.0)], [3]),
-    ([2], [(1.0, 2.0, 3.0)]),
-    ([[1.0, 2.0, 3.0]], [3]),
-    ([2], [[1.0, 2.0, 3.0]]),
-]
-
-special_test_cases = [
-    (["Hello"], [3]),
-    ([3], ["Hello"])
-]
-
-
 @pytest.mark.level0
 @pytest.mark.platform_x86_cpu
 @pytest.mark.env_onecard
 @pytest.mark.parametrize('func', [mul])
 @pytest.mark.parametrize('ms_func', [jit_mul])
-@pytest.mark.parametrize('a, b', standard_test_cases)
-def test_standard_mul_operations(func, ms_func, a, b):
+@pytest.mark.parametrize('a', [[11]])
+@pytest.mark.parametrize('b', [[10]])
+def test_standard_mul_list(func, ms_func, a, b):
     """
     Feature: ALL TO ALL (Except special cases)
     Description: test cases for mul in PYNATIVE mode
@@ -57,14 +33,206 @@ def test_standard_mul_operations(func, ms_func, a, b):
     ms_res = ms_func(a[0], b[0])
     match_array(res, ms_res, error=0, err_msg=str(ms_res))
 
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [mul])
+@pytest.mark.parametrize('ms_func', [jit_mul])
+@pytest.mark.parametrize('a', [[10.0]])
+@pytest.mark.parametrize('b', [[11.0]])
+def test_standard_mul_list1(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL (Except special cases)
+    Description: test cases for mul in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a[0], b[0])
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a[0], b[0])
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
 
 @pytest.mark.level0
 @pytest.mark.platform_x86_cpu
 @pytest.mark.env_onecard
 @pytest.mark.parametrize('func', [mul])
 @pytest.mark.parametrize('ms_func', [jit_mul])
-@pytest.mark.parametrize('a, b', special_test_cases)
+@pytest.mark.parametrize('a', [[20]])
+@pytest.mark.parametrize('b', [[11.0]])
+def test_standard_mul_list2(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL (Except special cases)
+    Description: test cases for mul in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a[0], b[0])
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a[0], b[0])
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [mul])
+@pytest.mark.parametrize('ms_func', [jit_mul])
+@pytest.mark.parametrize('a', [[2.0]])
+@pytest.mark.parametrize('b', [[Tensor(np.ones((2, 3)).astype(np.float32))]])
+def test_standard_mul_list3(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL (Except special cases)
+    Description: test cases for mul in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a[0], b[0])
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a[0], b[0])
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.skip(reason="GetDevicePtr() error")
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [mul])
+@pytest.mark.parametrize('ms_func', [jit_mul])
+@pytest.mark.parametrize('a', [[Tensor(ops.fill(np.float32, (2, 3), 8))]])
+@pytest.mark.parametrize('b', [[2.0]])
+def test_standard_mul_Tensor1(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL (Except special cases)
+    Description: test cases for mul in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a[0], b[0])
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a[0], b[0])
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [mul])
+@pytest.mark.parametrize('ms_func', [jit_mul])
+@pytest.mark.parametrize('a', [[Tensor(ops.fill(np.float32, (2, 3), 8))]])
+@pytest.mark.parametrize('b', [[Tensor(np.ones((2, 3)).astype(np.float32))]])
+def test_standard_mul_Tensor2(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL (Except special cases)
+    Description: test cases for mul in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a[0], b[0])
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a[0], b[0])
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [mul])
+@pytest.mark.parametrize('ms_func', [jit_mul])
+@pytest.mark.parametrize('a', [[(1.0, 2.0, 3.0)]])
+@pytest.mark.parametrize('b', [[Tensor(np.ones((2, 3)).astype(np.float32))]])
+def test_standard_mul_tuple1(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL (Except special cases)
+    Description: test cases for mul in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a[0], b[0])
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a[0], b[0])
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.skip(reason="GetDevicePtr() error")
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [mul])
+@pytest.mark.parametrize('ms_func', [jit_mul])
+@pytest.mark.parametrize('a', [[Tensor(np.ones((2, 3)).astype(np.float32))]])
+@pytest.mark.parametrize('b', [[(1.0, 2.0, 3.0)]])
+def test_standard_mul_tuple2(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL (Except special cases)
+    Description: test cases for mul in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a[0], b[0])
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a[0], b[0])
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [mul])
+@pytest.mark.parametrize('ms_func', [jit_mul])
+@pytest.mark.parametrize('a', [[(1.0, 2.0, 3.0)]])
+@pytest.mark.parametrize('b', [[3]])
+def test_standard_mul_tuple_list(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL (Except special cases)
+    Description: test cases for mul in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a[0], b[0])
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a[0], b[0])
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [mul])
+@pytest.mark.parametrize('ms_func', [jit_mul])
+@pytest.mark.parametrize('a', [[2]])
+@pytest.mark.parametrize('b', [[(1.0, 2.0, 3.0)]])
+def test_standard_mul_list_tuple(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL (Except special cases)
+    Description: test cases for mul in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a[0], b[0])
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a[0], b[0])
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [mul])
+@pytest.mark.parametrize('ms_func', [jit_mul])
+@pytest.mark.parametrize('a', [["Hello"]])
+@pytest.mark.parametrize('b', [[3]])
 def test_special_mul_operations(func, ms_func, a, b):
+    """
+    Feature: Special Cases
+    Description: test cases for mul in PYNATIVE mode
+    Expectation: an exception is raised
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a[0], b[0])
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = a[0] * b[0]
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [mul])
+@pytest.mark.parametrize('ms_func', [jit_mul])
+@pytest.mark.parametrize('a', [[3]])
+@pytest.mark.parametrize('b', [["Hello"]])
+def test_special_mul_operations2(func, ms_func, a, b):
     """
     Feature: Special Cases
     Description: test cases for mul in PYNATIVE mode
--- tests/st/pi_jit/calculation/test_sub.py
+++ tests/st/pi_jit/calculation/test_sub.py
@@ -14,28 +14,186 @@ def jit_sub(a, b):
     return a - b
 
 
-combinations = [
-    (11, 10),
-    (10.0, 11.0),
-    (20, 11.0),
-    (2.0, Tensor(np.ones((2, 3)).astype(np.float32))),
-    (Tensor(ops.fill(np.float32, (2, 3), 8)), 2.0),
-    (Tensor(ops.fill(np.float32, (2, 3), 8)),
-     Tensor(np.ones((2, 3)).astype(np.float32))),
-    ((1.0, 2.0, 3.0), Tensor(np.ones((2, 3)).astype(np.float32))),
-    (Tensor(np.ones((2, 3)).astype(np.float32)), (1.0, 2.0, 3.0)),
-    ([1.0, 2.0, 3.0], Tensor(np.ones((2, 3)).astype(np.float32))),
-    (Tensor(np.ones((2, 3)).astype(np.float32)), [1.0, 2.0, 3.0])
-]
-
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [sub])
+@pytest.mark.parametrize('ms_func', [jit_sub])
+@pytest.mark.parametrize('a', [11])
+@pytest.mark.parametrize('b', [10])
+def test_subtraction_int(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL
+    Description: test cases for sub in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a, b)
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a, b)
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
 
 @pytest.mark.level0
 @pytest.mark.platform_x86_cpu
 @pytest.mark.env_onecard
 @pytest.mark.parametrize('func', [sub])
 @pytest.mark.parametrize('ms_func', [jit_sub])
-@pytest.mark.parametrize('a, b', combinations)
-def test_subtraction_operations(func, ms_func, a, b):
+@pytest.mark.parametrize('a', [10.0])
+@pytest.mark.parametrize('b', [11.0])
+def test_subtraction_float(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL
+    Description: test cases for sub in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a, b)
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a, b)
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [sub])
+@pytest.mark.parametrize('ms_func', [jit_sub])
+@pytest.mark.parametrize('a', [20])
+@pytest.mark.parametrize('b', [11.0])
+def test_subtraction_int_float(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL
+    Description: test cases for sub in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a, b)
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a, b)
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [sub])
+@pytest.mark.parametrize('ms_func', [jit_sub])
+@pytest.mark.parametrize('a', [2.0])
+@pytest.mark.parametrize('b', [Tensor(np.ones((2, 3)).astype(np.float32))])
+def test_subtraction_float_tensor(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL
+    Description: test cases for sub in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a, b)
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a, b)
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [sub])
+@pytest.mark.parametrize('ms_func', [jit_sub])
+@pytest.mark.parametrize('a', [Tensor(ops.fill(np.float32, (2, 3), 8))])
+@pytest.mark.parametrize('b', [2.0])
+def test_subtraction_tensor_float(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL
+    Description: test cases for sub in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a, b)
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a, b)
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [sub])
+@pytest.mark.parametrize('ms_func', [jit_sub])
+@pytest.mark.parametrize('a', [Tensor(ops.fill(np.float32, (2, 3), 8))])
+@pytest.mark.parametrize('b', [Tensor(np.ones((2, 3)).astype(np.float32))])
+def test_subtraction_tensor_tensor(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL
+    Description: test cases for sub in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a, b)
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a, b)
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [sub])
+@pytest.mark.parametrize('ms_func', [jit_sub])
+@pytest.mark.parametrize('a', [(1.0, 2.0, 3.0)])
+@pytest.mark.parametrize('b', [Tensor(np.ones((2, 3)).astype(np.float32))])
+def test_subtraction_tuple_tensor(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL
+    Description: test cases for sub in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a, b)
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a, b)
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [sub])
+@pytest.mark.parametrize('ms_func', [jit_sub])
+@pytest.mark.parametrize('a', [Tensor(np.ones((2, 3)).astype(np.float32))])
+@pytest.mark.parametrize('b', [(1.0, 2.0, 3.0)])
+def test_subtraction_tensor_tuple(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL
+    Description: test cases for sub in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a, b)
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a, b)
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.skip(reason="GetDevicePtr() error")
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [sub])
+@pytest.mark.parametrize('ms_func', [jit_sub])
+@pytest.mark.parametrize('a', [[1.0, 2.0, 3.0]])
+@pytest.mark.parametrize('b', [Tensor(np.ones((2, 3)).astype(np.float32))])
+def test_subtraction_list_tensor(func, ms_func, a, b):
+    """
+    Feature: ALL TO ALL
+    Description: test cases for sub in PYNATIVE mode
+    Expectation: the result match
+    """
+    context.set_context(mode=context.PYNATIVE_MODE)
+    res = func(a, b)
+    context.set_context(mode=context.GRAPH_MODE)
+    ms_res = ms_func(a, b)
+    match_array(res, ms_res, error=0, err_msg=str(ms_res))
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('func', [sub])
+@pytest.mark.parametrize('ms_func', [jit_sub])
+@pytest.mark.parametrize('a', [Tensor(np.ones((2, 3)).astype(np.float32))])
+@pytest.mark.parametrize('b', [[1.0, 2.0, 3.0]])
+def test_subtraction_tensor_list(func, ms_func, a, b):
     """
     Feature: ALL TO ALL
     Description: test cases for sub in PYNATIVE mode
