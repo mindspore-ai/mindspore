ddc98e8a6edfa86f9fc0e689d09f0cdbc52b7dc4
hanhuifeng2020 <hanhuifeng1@huawei.com>
2024-01-19 17:41:37 +0800

    fix a conflict bug

--- mindspore/ccsrc/pybind_api/utils/op_enum_py.cc
+++ mindspore/ccsrc/pybind_api/utils/op_enum_py.cc
@@ -20,6 +20,35 @@
 #include "mindapi/base/types.h"
 
 namespace mindspore::ops {
+
+namespace {
+// The pybind11 registers the enum globally. Therefore, if different modules register the same enum type,
+// conflicts occur.
+// To avoid conflicts with the mindspore_lite, the FormatEnum added for use in the pybind.
+enum FormatEnum : int64_t {
+  DEFAULT_FORMAT = Format::DEFAULT_FORMAT,
+  NCHW = Format::NCHW,
+  NHWC = Format::NHWC,
+  NHWC4 = Format::NHWC4,
+  HWKC = Format::HWKC,
+  HWCK = Format::HWCK,
+  KCHW = Format::KCHW,
+  CKHW = Format::CKHW,
+  KHWC = Format::KHWC,
+  CHWK = Format::CHWK,
+  HW = Format::HW,
+  HW4 = Format::HW4,
+  NC = Format::NC,
+  NC4 = Format::NC4,
+  NC4HW4 = Format::NC4HW4,
+  NCDHW = Format::NCDHW,
+  NWC = Format::NWC,
+  NCW = Format::NCW,
+  NDHWC = Format::NDHWC,
+  NC8HW8 = Format::NC8HW8
+};
+}  // namespace
+
 void RegOpEnum(py::module *m) {
   auto m_sub = m->def_submodule("op_enum", "submodule for op enum");
   (void)m_sub.def("str_to_enum", &StringToEnumImpl, "string to enum value");
@@ -49,27 +78,27 @@ void RegOpEnum(py::module *m) {
     .value("DT_TYPE", OP_DTYPE::DT_TYPE)
     .value("DT_END", OP_DTYPE::DT_END);
   // There are currently some deficiencies in format, which will be filled in later.
-  (void)py::enum_<Format>(*m, "FormatEnum", py::arithmetic())
-    .value("DEFAULT_FORMAT", Format::DEFAULT_FORMAT)
-    .value("NCHW", Format::NCHW)
-    .value("NHWC", Format::NHWC)
-    .value("NHWC4", Format::NHWC4)
-    .value("HWKC", Format::HWKC)
-    .value("HWCK", Format::HWCK)
-    .value("KCHW", Format::KCHW)
-    .value("CKHW", Format::CKHW)
-    .value("KHWC", Format::KHWC)
-    .value("CHWK", Format::CHWK)
-    .value("HW", Format::HW)
-    .value("HW4", Format::HW4)
-    .value("NC", Format::NC)
-    .value("NC4", Format::NC4)
-    .value("NC4HW4", Format::NC4HW4)
-    .value("NCDHW", Format::NCDHW)
-    .value("NWC", Format::NWC)
-    .value("NCW", Format::NCW)
-    .value("NDHWC", Format::NDHWC)
-    .value("NC8HW8", Format::NC8HW8);
+  (void)py::enum_<FormatEnum>(*m, "FormatEnum", py::arithmetic())
+    .value("DEFAULT_FORMAT", FormatEnum::DEFAULT_FORMAT)
+    .value("NCHW", FormatEnum::NCHW)
+    .value("NHWC", FormatEnum::NHWC)
+    .value("NHWC4", FormatEnum::NHWC4)
+    .value("HWKC", FormatEnum::HWKC)
+    .value("HWCK", FormatEnum::HWCK)
+    .value("KCHW", FormatEnum::KCHW)
+    .value("CKHW", FormatEnum::CKHW)
+    .value("KHWC", FormatEnum::KHWC)
+    .value("CHWK", FormatEnum::CHWK)
+    .value("HW", FormatEnum::HW)
+    .value("HW4", FormatEnum::HW4)
+    .value("NC", FormatEnum::NC)
+    .value("NC4", FormatEnum::NC4)
+    .value("NC4HW4", FormatEnum::NC4HW4)
+    .value("NCDHW", FormatEnum::NCDHW)
+    .value("NWC", FormatEnum::NWC)
+    .value("NCW", FormatEnum::NCW)
+    .value("NDHWC", FormatEnum::NDHWC)
+    .value("NC8HW8", FormatEnum::NC8HW8);
   (void)py::enum_<Reduction>(*m, "ReductionEnum", py::arithmetic())
     .value("SUM", Reduction::REDUCTION_SUM)
     .value("MEAN", Reduction::MEAN)
