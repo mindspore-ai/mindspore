a1bb435eed5ca20f51f9e50e9af72077cdf3fd72
r1chardf1d0 <xiaruijie@huawei.com>
2024-02-01 11:27:45 +0800

    fix subgraph no output

--- mindspore/ccsrc/pipeline/jit/pi/graph_capture/graph_build.cc
+++ mindspore/ccsrc/pipeline/jit/pi/graph_capture/graph_build.cc
@@ -886,9 +886,7 @@ bool MindGraphBuilder::DoIsOp(const Instr &instr) {
   return true;
 }
 
-bool GraphBuilder::DoIsOp(const Instr &instr) {
-  return DoBinary(instr);
-}
+bool GraphBuilder::DoIsOp(const Instr &instr) { return DoBinary(instr); }
 
 bool GraphBuilder::DoBinary(const Instr &instr) {
   int opcode = instr.op();
@@ -1829,17 +1827,20 @@ StopTraceReason MindGraphBuilder::BuildSubGraph(CallNode *call_node, int depth, 
   if (sub_ret != nullptr) {
     if (CheckConstPyObject(sub_ret->GetVobj()->GetPyObject().ptr())) {
       call_node->SetVobj(sub_ret->GetVobj());
-    } else if (sg->FGBuilder()->graph() == nullptr) {
-      MS_LOG(ERROR) << "subgraph trace null";
-      return StopTraceReason::kTrace_Fail;
     } else {
-      auto res = FGBuilder()->AddNode(sg->FGBuilder()->graph(), args);
-      if (res.ptr()) {
-        MS_LOG(INFO) << "add fg node suc: ";
-        call_node->SetVobj(AObject::Convert(res));
+      sg->FGAddOutput();
+      if (sg->FGBuilder()->graph() == nullptr) {
+        MS_LOG(ERROR) << "subgraph trace null";
+        return StopTraceReason::kTrace_Fail;
       } else {
-        MS_LOG(ERROR) << "add fg node fail";
-        stat = InlineReason::kInlineInfer_Fail;
+        auto res = FGBuilder()->AddNode(sg->FGBuilder()->graph(), args);
+        if (res.ptr()) {
+          MS_LOG(INFO) << "add fg node suc: ";
+          call_node->SetVobj(AbstractFuncGraphOut::MakeAObject(res));
+        } else {
+          MS_LOG(ERROR) << "add fg node fail";
+          stat = InlineReason::kInlineInfer_Fail;
+        }
       }
     }
     stat = is_make_func || ApplyInlinePolicy(subgraph->GetGraph()) ? stat : InlineReason::kInlinePolicyDisabled;
