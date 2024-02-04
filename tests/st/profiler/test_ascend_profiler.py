220c8a8f44f82a6f8600cf8b5d4adf96f2616cd9
yanghaoran <yanghaoran2@huawei.com>
2024-02-04 15:02:10 +0800

    takedown randomly failing testcases

--- tests/st/profiler/test_ascend_profiler.py
+++ tests/st/profiler/test_ascend_profiler.py
@@ -111,7 +111,7 @@ def test_ascend_profiling():
         assert len(glob.glob(f"{tmpdir}/profiler*/*PROF*/device_*/data/l2_cache.data*")) >= 2
 
 
-@pytest.mark.level0
+@pytest.mark.level1
 @pytest.mark.platform_arm_ascend_training
 @pytest.mark.platform_x86_ascend_training
 @pytest.mark.env_onecard
