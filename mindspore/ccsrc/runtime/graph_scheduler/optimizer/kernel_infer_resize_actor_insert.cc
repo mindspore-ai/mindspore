d1ab1fa44c1d6f672861735897092b2463bb59c8
VectorSL <864733542@qq.com>
2024-02-05 20:57:37 +0800

    temp fix actor refcount

--- mindspore/ccsrc/runtime/graph_scheduler/optimizer/kernel_infer_resize_actor_insert.cc
+++ mindspore/ccsrc/runtime/graph_scheduler/optimizer/kernel_infer_resize_actor_insert.cc
@@ -146,6 +146,12 @@ void AddDataArrows(const ActorSet *actor_set, const KernelActor *kernel_actor, K
     } else {
       SchedulerHelper::AddDataArrow(from_actor, kernel_infer_actor, data_arrow->from_output_index_,
                                     data_arrow->to_input_index_);
+      if (from_actor->type() == KernelTransformType::kCopyActor) {
+        auto *copy_actor = dynamic_cast<CopyActor *>(from_actor);
+        MS_EXCEPTION_IF_NULL(copy_actor);
+        UpdateRefCount(copy_actor->output[0].get(), false);
+        UpdateRefCount(copy_actor->output[0].get(), false);
+      }
       SchedulerHelper::AddDataArrow(from_actor, kernel_resize_actor, data_arrow->from_output_index_,
                                     data_arrow->to_input_index_);
     }
