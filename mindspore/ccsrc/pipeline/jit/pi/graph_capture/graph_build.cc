284f75d1470dc1cb66c99647acc4732111dcd67c
r1chardf1d0 <xiaruijie@huawei.com>
2024-02-06 11:49:03 +0800

    fix subgraph break

--- mindspore/ccsrc/pipeline/jit/pi/graph_capture/graph_build.cc
+++ mindspore/ccsrc/pipeline/jit/pi/graph_capture/graph_build.cc
@@ -2085,7 +2085,7 @@ StopTraceReason MindGraphBuilder::BuildSubGraph(CallNode *call_node, int depth, 
   }
 
   MS_LOG(INFO) << "new subgraph->TraceRun:" << py::str(func);
-  subgraph->TraceRun(args);
+  auto reason = subgraph->TraceRun(args);
   MS_LOG(INFO) << "new subgraph->TraceRun end:" << py::str(func);
 
   call_node->SetSubGraph(subgraph->GetGraph());
@@ -2140,7 +2140,7 @@ StopTraceReason MindGraphBuilder::BuildSubGraph(CallNode *call_node, int depth, 
 
   // if stat == InlineReason::kInline, guard free variable
   call_node->SetInlineReason(stat);
-  return StopTraceReason::kNonStopTrace;
+  return reason;
 }
 
 // build sub-graph
