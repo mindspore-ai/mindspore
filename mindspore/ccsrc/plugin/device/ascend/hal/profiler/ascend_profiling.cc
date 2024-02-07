20c6a0ecd30b34a546744cdc45117e128835c5c6
jijiarong <jijiarong@huawei.com>
2024-02-07 19:00:53 +0800

    dynamic profiler

--- mindspore/ccsrc/plugin/device/ascend/hal/profiler/ascend_profiling.cc
+++ mindspore/ccsrc/plugin/device/ascend/hal/profiler/ascend_profiling.cc
@@ -198,10 +198,6 @@ void AscendProfiler::Stop() {
   if (aclRet != ACL_SUCCESS) {
     MS_LOG(EXCEPTION) << "Failed to call aclprofStop function.";
   }
-  aclRet = aclprofDestroyConfig(acl_config_);
-  if (aclRet != ACL_SUCCESS) {
-    MS_LOG(EXCEPTION) << "Failed to call aclprofDestroyConfig function.";
-  }
 
   MemoryProfiling::GetInstance().StopMemoryProfiling();
 
@@ -209,8 +205,12 @@ void AscendProfiler::Stop() {
 }
 
 void AscendProfiler::Finalize() {
+  aclError aclRet = aclprofDestroyConfig(acl_config_);
+  if (aclRet != ACL_SUCCESS) {
+    MS_LOG(EXCEPTION) << "Failed to call aclprofDestoryConfig function.";
+  }
   MS_LOG(INFO) << "Begin to finalize profiling";
-  aclError aclRet = aclprofFinalize();
+  aclRet = aclprofFinalize();
   if (aclRet != ACL_SUCCESS) {
     MS_LOG(EXCEPTION) << "Failed to call aclprofDestroyConfig function.";
   }
