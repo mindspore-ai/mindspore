c8f883adfd30012a4e39b2dc7608d8265817ea52
panzhihui <panzhihui3@huawei.com>
2024-03-01 08:27:13 +0000

    Update tests/st/tensor/test_logcumsumexp.py add ascend tests

--- tests/st/tensor/test_logcumsumexp.py
+++ tests/st/tensor/test_logcumsumexp.py
@@ -25,6 +25,8 @@ class Net(nn.Cell):
 
 
 @pytest.mark.level0
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_ascend_training
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_arm_cpu
 @pytest.mark.platform_x86_gpu_training
