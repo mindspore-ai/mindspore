579fb494a34e58550798a084e6e0e8bad629695f
wen_xiang_zou_admin <zouwenxiang1@huawei.com>
2024-03-02 11:01:48 +0800

    fix add/sub infer value

--- mindspore/core/ops/ops_frontend_func_impl/sub.cc
+++ mindspore/core/ops/ops_frontend_func_impl/sub.cc
@@ -75,7 +75,8 @@ class SubFrontendFuncImpl : public OpFrontendFuncImpl {
 
     auto data_size = x1_tensor->DataSize();
     auto dtype = x1_tensor->data_type();
-    auto result_tensor = std::make_shared<tensor::Tensor>(dtype, x1_shape);
+    auto result_tensor = x1_shape.size() != 0 ? std::make_shared<tensor::Tensor>(dtype, x1_shape)
+                                              : std::make_shared<tensor::Tensor>(dtype, x2_shape);
     MS_EXCEPTION_IF_NULL(result_tensor);
     auto iter = sub_impl_list.find(dtype);
     if (iter == sub_impl_list.end()) {
