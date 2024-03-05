a9590fe949c74310f182f95fad35c311913c91ca
caifubi <caifubi1@huawei.com>
2024-03-05 19:37:03 +0800

    Set tensor lazy callback once

--- mindspore/ccsrc/pipeline/pynative/forward/forward.cc
+++ mindspore/ccsrc/pipeline/pynative/forward/forward.cc
@@ -85,7 +85,6 @@ ValuePtr CopyTensorValueWithNewId(const ValuePtr &v) {
     auto new_tensor = std::make_shared<tensor::Tensor>(tensor->data_type(), tensor->shape(), tensor->data_ptr());
     new_tensor->set_device_address(tensor->device_address());
     new_tensor->set_sync_status(tensor->sync_status());
-    new_tensor->set_lazy_callback([]() { runtime::OpExecutor::GetInstance().WaitAll(); });
     return new_tensor;
   } else if (v->isa<ValueTuple>()) {
     const auto &v_tup = v->cast<ValueTuplePtr>();
@@ -214,7 +213,6 @@ void CreateOutputTensor(const AbstractBasePtr &abstract, std::vector<tensor::Ten
                         std::vector<DeviceAddressPromisePtr> *device_sync_promises) {
   auto create_tensor = [&outputs, &device_sync_promises](const TypePtr &type, const ShapeVector &shape_vector) {
     auto output_tensor = std::make_shared<tensor::Tensor>(type->type_id(), shape_vector);
-    output_tensor->set_lazy_callback([]() { runtime::OpExecutor::GetInstance().WaitAll(); });
     (void)outputs->emplace_back(output_tensor);
     MS_LOG(DEBUG) << "Create output tensor " << output_tensor->ToString();
 
@@ -1068,7 +1066,6 @@ void ForwardExecutor::CreateViewOutputTensor(const FrontendOpRunInfoPtr &op_run_
   MS_EXCEPTION_IF_NULL(input_tensor);
   MS_EXCEPTION_IF_NULL(storage_info);
   auto output_tensor = std::make_shared<tensor::Tensor>(input_tensor->data_type(), storage_info->shape);
-  output_tensor->set_lazy_callback([]() { runtime::OpExecutor::GetInstance().WaitAll(); });
   output_tensor->set_contiguous_callback([this](const DeviceSyncPtr &device_address) -> DeviceSyncPtr {
     return TensorContiguousCallback(device_address, device_address->GetTensorStorageInfo());
   });
