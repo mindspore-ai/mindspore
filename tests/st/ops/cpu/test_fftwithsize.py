f4d4f8aef5e0dbb371dfa7f46e128afab3bbba4b
c00611613 <chengqiang10@huawei.com>
2024-03-08 14:50:32 +0800

    [深圳湾I86SLK]新增FFTWithSize三维场景下的梯度计算和相关测试用例，并与numpy对齐

--- tests/st/ops/cpu/test_fftwithsize.py
+++ tests/st/ops/cpu/test_fftwithsize.py
@@ -16,15 +16,36 @@
 import numpy as np
 import pytest
 
-import mindspore
+import mindspore as ms
 import mindspore.context as context
-from mindspore import Tensor
-from mindspore.nn import Cell
-import mindspore.ops as F
+from mindspore import Tensor, ops
+from tests.st.utils import test_utils
+
+
+@test_utils.run_with_cell
+def fft_forward_func(x, signal_ndim, inverse, real, norm='backward', onesided=True, signal_sizes=()):
+    return ops.FFTWithSize(signal_ndim, inverse, real, norm, onesided, signal_sizes)(x)
+
+
+@test_utils.run_with_cell
+def rfft_and_irfft_forward_func(x, signal_ndim, inverse, real, norm='backward', onesided=True, signal_sizes=()):
+    x = ops.FFTWithSize(signal_ndim, inverse, real, norm, onesided, signal_sizes)(x)
+    return ops.FFTWithSize(signal_ndim, not inverse, real, norm, onesided, signal_sizes)(x)
+
+
+@test_utils.run_with_cell
+def fft_backward_func(x, signal_ndim, inverse, real, norm='backward', onesided=True, signal_sizes=()):
+    return ops.grad(fft_forward_func, (0,))(x, signal_ndim, inverse, real, norm, onesided, signal_sizes)
+
+
+@test_utils.run_with_cell
+def rfft_and_irfft_backward_func(x, signal_ndim, inverse, real, norm='backward', onesided=True, signal_sizes=()):
+    return ops.grad(rfft_and_irfft_forward_func, (0,))(x, signal_ndim, inverse, real, norm, onesided, signal_sizes)
 
 
 @pytest.mark.level0
 @pytest.mark.platform_x86_cpu
+@pytest.mark.platform_arm_cpu
 @pytest.mark.env_onecard
 @pytest.mark.parametrize('dtype, eps', [(np.complex64, 1e-6), (np.complex128, 1e-6)])
 def test_fftwithsize_fft_ifft(dtype, eps):
@@ -38,17 +59,18 @@ def test_fftwithsize_fft_ifft(dtype, eps):
     error = np.ones(shape=[4]) * eps
     context.set_context(mode=context.GRAPH_MODE, device_target="CPU")
 
-    output = F.fft(x)
+    output = fft_forward_func(x, 1, False, False)
     diff = np.abs(output.asnumpy() - expect)
     assert np.all(diff < error)
 
-    output_ifft = F.ifft(output)
+    output_ifft = fft_forward_func(output, 1, True, False)
     diff_ifft = np.abs(output_ifft.asnumpy() - x.asnumpy())
     assert np.all(diff_ifft < error)
 
 
 @pytest.mark.level0
 @pytest.mark.platform_x86_cpu
+@pytest.mark.platform_arm_cpu
 @pytest.mark.env_onecard
 @pytest.mark.parametrize('dtype, eps', [(np.complex64, 1e-6), (np.complex128, 1e-6)])
 def test_fftwithsize_fft2_ifft2(dtype, eps):
@@ -62,43 +84,108 @@ def test_fftwithsize_fft2_ifft2(dtype, eps):
     error = np.ones(shape=[2, 2]) * eps
     context.set_context(mode=context.GRAPH_MODE, device_target="CPU")
 
-    output = F.fft2(x)
+    output = fft_forward_func(x, 2, False, False)
     diff = np.abs(output.asnumpy() - expect)
     assert np.all(diff < error)
 
-    output_ifft2 = F.ifft2(output)
+    output_ifft2 = fft_forward_func(output, 2, True, False)
     diff_ifft2 = np.abs(output_ifft2.asnumpy() - x.asnumpy())
     assert np.all(diff_ifft2 < error)
 
 
 @pytest.mark.level0
 @pytest.mark.platform_x86_cpu
+@pytest.mark.platform_arm_cpu
 @pytest.mark.env_onecard
-@pytest.mark.parametrize('dtype, eps', [(np.complex64, 1e-6), (np.complex128, 1e-6)])
-def test_fftwithsize_fftn_ifftn(dtype, eps):
+@pytest.mark.parametrize('mode', [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
+def test_fft_with_size_rfft3_forward(mode):
     """
-    Feature: fftn & ifftn function
-    Description: test cases for fftn & ifftn
+    Feature: rfft3 forward function
+    Description: test cases for rfft
     Expectation: the result matches pytorch
     """
-    x = Tensor(np.array([[[1.6243454 +0.j, -0.6117564 +0.j, -0.5281718 +0.j],
-                          [-1.0729686 +0.j, 0.86540765+0.j, -2.3015387 +0.j]]]).astype(dtype))
-    expect = np.array([[[-2.02468245+0.j, 1.83940642-2.6702696j, 1.83940642+2.6702696j],
-                        [2.99351685+0.j, 2.54921257+2.81504238j, 2.54921257-2.81504238j]]]).astype(dtype)
-    error = np.ones(shape=[1, 2, 3]) * eps
-    context.set_context(mode=context.GRAPH_MODE, device_target="CPU")
-
-    output = F.fftn(x)
-    diff = np.abs(output.asnumpy() - expect)
-    assert np.all(diff < error)
-
-    output_ifftn = F.ifftn(output)
-    diff_ifftn = np.abs(output_ifftn.asnumpy() - x.asnumpy())
-    assert np.all(diff_ifftn < error)
+    ms.context.set_context(mode=mode)
+    x = np.arange(1 * 2 * 3 * 4, dtype=np.float64).reshape(1, 2, 3, 4)
+    ms_x = ms.Tensor(x)
+    output = fft_forward_func(ms_x, 3, False, True)
+    expect = np.fft.rfftn(x, s=(2, 3, 4))
+    assert np.allclose(output.asnumpy(), expect)
 
 
 @pytest.mark.level0
 @pytest.mark.platform_x86_cpu
+@pytest.mark.platform_arm_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('mode', [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
+def test_fft_with_size_irfft3_forward(mode):
+    """
+    Feature: irfft3 forward function
+    Description: test cases for irfft3
+    Expectation: the result matches pytorch
+    """
+    ms.context.set_context(mode=mode)
+    x = np.arange(1 * 2 * 3 * 3, dtype=np.complex128).reshape(1, 2, 3, 3)
+    ms_x = ms.Tensor(x)
+    output = fft_forward_func(ms_x, 3, True, True)
+    expect = np.fft.irfftn(x, s=(2, 3, 4))
+    assert np.allclose(output.asnumpy(), expect)
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.platform_arm_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('mode', [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
+def test_fft_with_size_rfft3_backward(mode):
+    """
+    Feature: rfft3 backward function
+    Description: test cases for rfft3
+    Expectation: the result matches pytorch
+    """
+    ms.context.set_context(mode=mode)
+    dim1 = 1
+    dim2 = 2
+    dim3 = 3
+    dim4 = 4
+    offset_size = dim1 * dim2 * dim3 * dim4
+    x = np.arange(offset_size, dtype=np.float64).reshape(dim1, dim2, dim3, dim4)
+    ms_x = ms.Tensor(x)
+    output = fft_backward_func(ms_x, 3, False, True)
+    dout = np.ones((dim1, dim2, dim3, dim4 // 2 + 1), dtype=np.complex128)
+    concat_array = np.zeros((dim1, dim2, dim3, dim4 - dim4 // 2 - 1))
+    concat_array = concat_array.astype(np.complex128)
+    dout = np.concatenate((dout, concat_array), axis=-1)
+    expect = np.fft.ifftn(dout, s=(dim2, dim3, dim4)) * offset_size
+    assert np.allclose(output.asnumpy(), expect.real)
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.platform_arm_cpu
+@pytest.mark.env_onecard
+@pytest.mark.parametrize('mode', [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
+def test_fft_with_size_rfft3_and_irfft3_backward(mode):
+    """
+    Feature: rfft3_and_irfft3 function
+    Description: test cases for rfft3_and_irfft3
+    Expectation: the result matches pytorch
+    """
+    ms.context.set_context(mode=mode)
+    dim1 = 1
+    dim2 = 2
+    dim3 = 3
+    dim4 = 4
+    offset_size = dim1 * dim2 * dim3 * dim4
+    x = np.arange(offset_size, dtype=np.float64).reshape(dim1, dim2, dim3, dim4)
+    ms_x = ms.Tensor(x)
+    output = rfft_and_irfft_backward_func(ms_x, 3, False, True)
+    expect = np.ones((dim1, dim2, dim3, dim4))
+    assert np.allclose(output.asnumpy(), expect)
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.platform_arm_cpu
 @pytest.mark.env_onecard
 def test_fftwithsize_exception():
     """
@@ -109,14 +196,6 @@ def test_fftwithsize_exception():
     signal_ndim = 2
     inverse = True
     real = True
-    class Net(Cell):
-        def __init__(self):
-            super().__init__()
-            self.layer = F.FFTWithSize(signal_ndim, inverse, real, norm='backward', onesided=True, signal_sizes=())
-
-        def construct(self, x):
-            return self.layer(x)
-    x = Tensor(np.random.uniform(-10, 10, size=[2, 1])).astype(mindspore.complex64)
-    op_net = Net()
+    x = Tensor(np.random.uniform(-10, 10, size=[2, 1])).astype(ms.complex64)
     with pytest.raises(ValueError, match="For 'FFTWithSize', the last dimension of the input cannot be 1"):
-        op_net(x)
+        fft_forward_func(x, signal_ndim, inverse, real)
