e79a2f8ae84279187545ab1010089ba26139dda1
fary86 <fary.fanrui@huawei.com>
2024-03-11 15:28:36 +0800

    Adapt mindspore to support print big tensor

--- mindspore/ccsrc/plugin/device/ascend/hal/device/mbuf_receive_manager.h
+++ mindspore/ccsrc/plugin/device/ascend/hal/device/mbuf_receive_manager.h
@@ -19,16 +19,18 @@
 
 #include <atomic>
 #include <condition_variable>
+#include <cstdint>
 #include <functional>
 #include <future>
 #include <map>
 #include <memory>
 #include <mutex>
-#include <queue>
+#include <sstream>
 #include <string>
 #include <thread>
 #include <utility>
 #include <vector>
+#include <variant>
 #include "ir/tensor.h"
 #include "transform/symbol/acl_tdt_symbol.h"
 #include "transform/symbol/symbol_utils.h"
@@ -39,7 +41,9 @@
 
 namespace mindspore::device::ascend {
 
-using MbufFuncType = std::function<void(acltdtDataset *)>;
+class ScopeAclTdtDataset;
+
+using MbufFuncType = std::function<void(ScopeAclTdtDataset &)>;
 
 const std::map<aclDataType, TypeId> kAclDataTypeMap = {
   {ACL_INT8, TypeId::kNumberTypeInt8},       {ACL_UINT8, TypeId::kNumberTypeUInt8},
@@ -49,11 +53,32 @@ const std::map<aclDataType, TypeId> kAclDataTypeMap = {
   {ACL_FLOAT16, TypeId::kNumberTypeFloat16}, {ACL_FLOAT, TypeId::kNumberTypeFloat32},
   {ACL_DOUBLE, TypeId::kNumberTypeFloat64},  {ACL_BOOL, TypeId::kNumberTypeBool}};
 
-mindspore::tensor::TensorPtr acltdtDataItemToTensorPtr(acltdtDataItem *item);
+struct SlicedTensor {
+  SlicedTensor(size_t slice_num, aclDataType type, const ShapeVector &shape)
+      : slice_id_(0), slice_num_(slice_num), data_type_(type), tensor_shape_(shape) {}
+  SlicedTensor(const SlicedTensor &) = delete;
+  SlicedTensor &operator=(const SlicedTensor &) = delete;
+  ~SlicedTensor() = default;
+
+  // the id of current slice of tensor
+  size_t slice_id_{0};
+  // the number of total slices of tensor
+  size_t slice_num_{0};
+  // tensor's data type and shape
+  aclDataType data_type_;
+  ShapeVector tensor_shape_;
+  // buffer for storing contents of sliced tensor
+  std::ostringstream buffer_;
+};
+
+using DataItem = std::variant<std::string, mindspore::tensor::TensorPtr>;
 
 class ScopeAclTdtDataset {
  public:
-  ScopeAclTdtDataset() { acl_dataset_ = CALL_ASCEND_API(acltdtCreateDataset); }
+  ScopeAclTdtDataset() {
+    acl_dataset_ = CALL_ASCEND_API(acltdtCreateDataset);
+    Reset();
+  }
   acltdtDataset *Get() const { return acl_dataset_; }
   ~ScopeAclTdtDataset() {
     if (acl_dataset_ != nullptr && CALL_ASCEND_API(acltdtDestroyDataset, acl_dataset_) != ACL_SUCCESS) {
@@ -63,8 +88,51 @@ class ScopeAclTdtDataset {
     }
   }
 
+  void Reset() {
+    sliced_tensor_ = nullptr;
+    sliced_string_ = nullptr;
+    dataset_name_ = "";
+    tensor_type_ = ACL_TENSOR_DATA_UNDEFINED;
+    data_items_.clear();
+  }
+
+  const std::vector<DataItem> &GetDataItems() const { return data_items_; }
+
+  const std::string &GetDatasetName() const { return dataset_name_; }
+
+  // process full tensor(i.e. the content of tensor is in only one acltdtDataItem)
+  // return true when success, otherwise false
+  bool ProcessFullTensor(acltdtDataItem *item);
+
+  // process sliced tensor(i.e. the content of tensor spans multiple acltdtDataItems)
+  // return true when success, otherwise false
+  bool ProcessSliceTensor(acltdtDataItem *item);
+
+  // call this function when received last piece of slice tensor, return true when success, otherwise false
+  bool FinishSliceTensor();
+
+  // return true when encounter the end of OutfeedEnqueueOpV2's output, otherwise false
+  bool ProcessDataset(acltdtDataset *acl_dataset);
+
+  // set and check consistency of tensor types of data items, return true when success, otherwise false
+  bool CheckAndSetTensorType(acltdtTensorType tensor_type);
+
  private:
-  acltdtDataset *acl_dataset_;
+  // acl tdt dataset for receiving data, created once, used many times
+  acltdtDataset *acl_dataset_{nullptr};
+
+  // structure for connecting tensor slices to a full tensor
+  std::shared_ptr<SlicedTensor> sliced_tensor_{nullptr};
+  // structure for connecting string slices to a full string
+  std::shared_ptr<std::ostringstream> sliced_string_{nullptr};
+
+  // ONLY the FIRST dataset containing the dataset name when the outputs of OutfeedEnqueueOpV2 span multiple datasets
+  std::string dataset_name_;
+  // NOTE: the data items of output of one OutfeedEnqueueOpV2 must be all with type ACL_TENSOR_DATA_TENSOR, or all with
+  // type ACL_TENSOR_DATA_SLICE_TENSOR(ACL_TENSOR_DATA_END_TENSOR is also indicating type ACL_TENSOR_DATA_SLICE_TENSOR)
+  acltdtTensorType tensor_type_{ACL_TENSOR_DATA_UNDEFINED};
+  // vector for buffering outputs of OutfeedEnqueueOpV2 at a time
+  std::vector<DataItem> data_items_;
 };
 
 class MbufDataHandler {
@@ -90,7 +158,7 @@ class MbufDataHandler {
   acltdtChannelHandle *acl_handle_;
 
   void HandleData();
-  bool ReceiveAndProcessData(const ScopeAclTdtDataset &scope_acl_dataset);
+  bool ReceiveAndProcessData(ScopeAclTdtDataset *dataset);
   bool QueryChannelSize(size_t *queue_size);
 };
 
@@ -105,6 +173,17 @@ class MbufDataHandlerManager {
   MbufDataHandlerManager &operator=(const MbufDataHandlerManager &) = delete;
 
   void AddHandler(std::unique_ptr<MbufDataHandler> handler) { handles_.push_back(std::move(handler)); }
+
+  void DestoryPrintHandler() {
+    for (auto iter = handles_.begin(); iter != handles_.end(); iter++) {
+      if ((*iter)->GetChannelName() == kChannelNameNpuLog) {
+        (*iter)->StopReceive();
+        handles_.erase(iter);
+        break;
+      }
+    }
+  }
+
   void DestoryHandler() {
     for (auto &handle : handles_) {
       handle->StopReceive();
@@ -119,6 +198,5 @@ class MbufDataHandlerManager {
   MbufDataHandlerManager() = default;
   std::vector<std::unique_ptr<MbufDataHandler>> handles_;
 };
-
 }  // namespace mindspore::device::ascend
 #endif  // MINDSPORE_CCSRC_PLUGIN_DEVICE_ASCEND_HAL_DEVICE_TENSORDUMP_UTILS_H_
