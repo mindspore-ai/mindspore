c6c397c2c36f40d632e73656528a3e33c8b3ecf4
fandawei <fandawei2@huawei.com>
2024-03-16 15:09:07 +0800

    fix summary st failure to run continuously and split the use cases

--- tests/st/summary/test_summary_collector.py
+++ tests/st/summary/test_summary_collector.py
@@ -13,26 +13,24 @@
 # limitations under the License.
 # ============================================================================
 """test SummaryCollector."""
+import json
 import os
 import re
-import shutil
 import tempfile
-import json
 from collections import Counter
-import numpy as np
 
+import numpy as np
 import pytest
 from mindspore.common import set_seed
-from mindspore import nn, Tensor, context
 from mindspore.common.initializer import Normal
-from mindspore.train import Loss
 from mindspore.nn.optim import Momentum
 from mindspore.ops import operations as P
-from mindspore.train import Model
-from mindspore import SummaryCollector, SummaryLandscape, SummaryRecord
+from mindspore.train import Loss, Model
+
+from mindspore import SummaryCollector, SummaryLandscape, SummaryRecord, Tensor, context, nn
+from tests.security_utils import security_off_wrap
 from tests.st.summary.dataset import create_mnist_dataset
 from tests.summary_utils import SummaryReader
-from tests.security_utils import security_off_wrap
 
 
 def callback_fn():
@@ -101,105 +99,42 @@ class LeNet5(nn.Cell):
         return x
 
 
+def run_network(dataset_sink_mode=False, num_samples=2, dir_suffix="summary", **kwargs):
+    """run network."""
+    lenet = LeNet5()
+    loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction="mean")
+    optim = Momentum(lenet.trainable_params(), learning_rate=0.1, momentum=0.9)
+    model = Model(lenet, loss_fn=loss, optimizer=optim, metrics={'loss': Loss()})
+    summary_dir = tempfile.TemporaryDirectory(suffix=dir_suffix)
+    summary_collector = SummaryCollector(summary_dir=summary_dir.name, collect_freq=2, **kwargs)
+
+    ds_train = create_mnist_dataset("train", num_samples=num_samples)
+    model.train(1, ds_train, callbacks=[summary_collector], dataset_sink_mode=dataset_sink_mode)
+
+    ds_eval = create_mnist_dataset("test")
+    model.eval(ds_eval, dataset_sink_mode=dataset_sink_mode, callbacks=[summary_collector])
+    return summary_dir
+
+
+def train_network(epoch=3, dataset_sink_mode=False, num_samples=2, dir_suffix="summary", **kwargs):
+    """run network."""
+    lenet = LeNet5()
+    loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction="mean")
+    optim = Momentum(lenet.trainable_params(), learning_rate=0.01, momentum=0.9)
+    model = Model(lenet, loss_fn=loss, optimizer=optim)
+    summary_dir = tempfile.TemporaryDirectory(suffix=dir_suffix)
+    summary_collector = SummaryCollector(summary_dir=summary_dir.name, collect_freq=2, **kwargs)
+
+    ds_train = create_mnist_dataset("train", num_samples=num_samples)
+    model.train(epoch, ds_train, callbacks=[summary_collector], dataset_sink_mode=dataset_sink_mode)
+    return summary_dir
+
+
 class TestSummary:
     """Test summary collector the basic function."""
-    base_summary_dir = ''
-
-    @classmethod
-    def setup_class(cls):
-        """Run before test this class."""
-        device_id = int(os.getenv('DEVICE_ID')) if os.getenv('DEVICE_ID') else 0
-        context.set_context(mode=context.GRAPH_MODE, device_id=device_id)
-        cls.base_summary_dir = tempfile.mkdtemp(suffix='summary')
-
-    @classmethod
-    def teardown_class(cls):
-        """Run after test this class."""
-        if os.path.exists(cls.base_summary_dir):
-            shutil.rmtree(cls.base_summary_dir)
-
-    def _run_network(self, dataset_sink_mode=False, num_samples=2, **kwargs):
-        """run network."""
-        lenet = LeNet5()
-        loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction="mean")
-        optim = Momentum(lenet.trainable_params(), learning_rate=0.1, momentum=0.9)
-        model = Model(lenet, loss_fn=loss, optimizer=optim, metrics={'loss': Loss()})
-        summary_dir = tempfile.mkdtemp(dir=self.base_summary_dir)
-        summary_collector = SummaryCollector(summary_dir=summary_dir, collect_freq=2, **kwargs)
-
-        ds_train = create_mnist_dataset("train", num_samples=num_samples)
-        model.train(1, ds_train, callbacks=[summary_collector], dataset_sink_mode=dataset_sink_mode)
-
-        ds_eval = create_mnist_dataset("test")
-        model.eval(ds_eval, dataset_sink_mode=dataset_sink_mode, callbacks=[summary_collector])
-        return summary_dir
-
-    @pytest.mark.level0
-    @pytest.mark.platform_x86_ascend_training
-    @pytest.mark.platform_arm_ascend_training
-    @pytest.mark.platform_x86_gpu_training
-    @pytest.mark.env_onecard
-    @security_off_wrap
-    def test_summary_with_sink_mode_false(self):
-        """Test summary with sink mode false, and num samples is 64."""
-        summary_dir = self._run_network(num_samples=10)
-
-        tag_list = self._list_summary_tags(summary_dir)
-
-        expected_tag_set = {'conv1.weight/auto', 'conv2.weight/auto', 'fc1.weight/auto', 'fc1.bias/auto',
-                            'fc2.weight/auto', 'input_data/auto', 'loss/auto',
-                            'histogram', 'image', 'scalar', 'tensor'}
-        assert set(expected_tag_set) == set(tag_list)
-
-        # num samples is 10, batch size is 2, so step is 5, collect freq is 2,
-        # SummaryCollector will collect the first step and 2th, 4th step
-        tag_count = 3
-        for value in Counter(tag_list).values():
-            assert value == tag_count
-
-    @pytest.mark.level0
-    @pytest.mark.platform_x86_ascend_training
-    @pytest.mark.platform_arm_ascend_training
-    @pytest.mark.platform_x86_gpu_training
-    @pytest.mark.env_onecard
-    @security_off_wrap
-    def test_summary_with_sink_mode_true(self):
-        """Test summary with sink mode true, and num samples is 64."""
-        summary_dir = self._run_network(dataset_sink_mode=True, num_samples=10)
-
-        tag_list = self._list_summary_tags(summary_dir)
-
-        # There will not record input data when dataset sink mode is True
-        expected_tags = {'conv1.weight/auto', 'conv2.weight/auto', 'fc1.weight/auto', 'fc1.bias/auto',
-                         'fc2.weight/auto', 'loss/auto', 'histogram', 'image', 'scalar', 'tensor'}
-        assert set(expected_tags) == set(tag_list)
-
-        tag_count = 1
-        for value in Counter(tag_list).values():
-            assert value == tag_count
-
-    @pytest.mark.level0
-    @pytest.mark.platform_x86_ascend_training
-    @pytest.mark.platform_arm_ascend_training
-    @pytest.mark.env_onecard
-    @security_off_wrap
-    def test_summarycollector_user_defind(self):
-        """Test SummaryCollector with user-defined."""
-        summary_dir = self._run_network(dataset_sink_mode=True, num_samples=2,
-                                        custom_lineage_data={'test': 'self test'},
-                                        export_options={'tensor_format': 'npy'})
-
-        tag_list = self._list_summary_tags(summary_dir)
-        file_list = self._list_tensor_files(summary_dir)
-        # There will not record input data when dataset sink mode is True
-        expected_tags = {'conv1.weight/auto', 'conv2.weight/auto', 'fc1.weight/auto', 'fc1.bias/auto',
-                         'fc2.weight/auto', 'loss/auto', 'histogram', 'image', 'scalar', 'tensor'}
-        assert set(expected_tags) == set(tag_list)
-        expected_files = {'tensor_1.npy'}
-        assert set(expected_files) == set(file_list)
 
     @staticmethod
-    def _list_summary_tags(summary_dir):
+    def list_summary_tags(summary_dir):
         """list summary tags."""
         summary_file_path = ''
         for file in os.listdir(summary_dir):
@@ -220,7 +155,7 @@ class TestSummary:
         return tags
 
     @staticmethod
-    def _list_tensor_files(summary_dir):
+    def list_tensor_files(summary_dir):
         """list tensor tags."""
         export_file_path = ''
         for file in os.listdir(summary_dir):
@@ -237,21 +172,8 @@ class TestSummary:
 
         return tensors
 
-    def _train_network(self, epoch=3, dataset_sink_mode=False, num_samples=2, **kwargs):
-        """run network."""
-        lenet = LeNet5()
-        loss = nn.SoftmaxCrossEntropyWithLogits(sparse=True, reduction="mean")
-        optim = Momentum(lenet.trainable_params(), learning_rate=0.01, momentum=0.9)
-        model = Model(lenet, loss_fn=loss, optimizer=optim)
-        summary_dir = tempfile.mkdtemp(dir=self.base_summary_dir)
-        summary_collector = SummaryCollector(summary_dir=summary_dir, collect_freq=2, **kwargs)
-
-        ds_train = create_mnist_dataset("train", num_samples=num_samples)
-        model.train(epoch, ds_train, callbacks=[summary_collector], dataset_sink_mode=dataset_sink_mode)
-        return summary_dir
-
     @staticmethod
-    def _list_summary_collect_landscape_tags(summary_dir):
+    def list_summary_collect_landscape_tags(summary_dir):
         """list summary landscape tags."""
         summary_dir_path = ''
         for file in os.listdir(summary_dir):
@@ -277,7 +199,7 @@ class TestSummary:
         return tags
 
     @staticmethod
-    def _list_landscape_tags(summary_dir):
+    def list_landscape_tags(summary_dir):
         """list landscape tags."""
         expected_tags = {'landscape_[1, 3]', 'landscape_[3]'}
         summary_list = []
@@ -304,57 +226,142 @@ class TestSummary:
                             break
         return tags
 
-    @pytest.mark.level0
-    @pytest.mark.platform_x86_ascend_training
-    @pytest.mark.platform_arm_ascend_training
-    @pytest.mark.platform_x86_gpu_training
-    @pytest.mark.env_onecard
-    @security_off_wrap
-    def test_summary_collector_landscape(self):
-        """
-        Feature: Summary collector with landscape.
-        Description: Test summary collector with landscape.
-        Expectation: Landscape data collected with expected value.
-        """
-        set_seed(1)
-        interval_1 = [1, 2, 3]
-        num_samples = 6
-        summary_dir = self._train_network(epoch=3, num_samples=num_samples,
-                                          collect_specified_data={'collect_landscape':
-                                                                  {'landscape_size': 4,
-                                                                   'unit': 'epoch',
-                                                                   'create_landscape': {'train': True,
-                                                                                        'result': True},
-                                                                   'num_samples': num_samples,
-                                                                   'intervals': [interval_1]}})
 
-        tag_list = self._list_summary_collect_landscape_tags(summary_dir)
-        expected_tags = {'epoch_group', 'model_params_file_map', 'step_per_epoch', 'unit', 'num_samples',
-                         'landscape_size', 'create_landscape'}
-        assert set(expected_tags) == set(tag_list)
-        device_id = int(os.getenv('DEVICE_ID')) if os.getenv('DEVICE_ID') else 0
-        summary_landscape = SummaryLandscape(summary_dir)
-        summary_landscape.gen_landscapes_with_multi_process(callback_fn, device_ids=[device_id])
-        tag_list_landscape = self._list_landscape_tags(summary_dir)
-        assert np.allclose(tag_list_landscape[0], 2.28, atol=0.03)
-        assert np.allclose(tag_list_landscape[1], 2.28, atol=0.03)
+@pytest.mark.level0
+@pytest.mark.platform_x86_ascend_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.env_onecard
+@security_off_wrap
+def test_summary_with_sink_mode_false():
+    """
+    Feature: Test summary with sink mode false, and num samples is 64.
+    Description: Test summary with sink mode false, and num samples is 64.
+    Expectation: Passed.
+    """
+    context.set_context(mode=context.GRAPH_MODE)
+    summary_dir = run_network(num_samples=10, dir_suffix="test_summary_with_sink_mode_false")
 
-    @pytest.mark.level0
-    @pytest.mark.platform_x86_ascend_training
-    @pytest.mark.platform_arm_ascend_training
-    @pytest.mark.platform_x86_gpu_training
-    @pytest.mark.env_onecard
-    @security_off_wrap
-    def test_summary_of_more_than_one_instance(self):
-        """
-        Feature: Test the multi instances of SummaryRecord in a script.
-        Description: Multi instances of SummaryRecord in a script.
-        Expectation: Throw RuntimeError.
-        """
-        with pytest.raises(RuntimeError) as errinfo:
-            summary_dir1 = tempfile.mkdtemp(dir=self.base_summary_dir)
-            summary_record1 = SummaryRecord(log_dir=summary_dir1)
-            summary_dir2 = tempfile.mkdtemp(dir=self.base_summary_dir)
-            _ = SummaryRecord(log_dir=summary_dir2)
-        assert "only one instance is supported in a training process" in str(errinfo.value)
-        summary_record1.close()
+    tag_list = TestSummary.list_summary_tags(summary_dir.name)
+
+    expected_tag_set = {'conv1.weight/auto', 'conv2.weight/auto', 'fc1.weight/auto', 'fc1.bias/auto',
+                        'fc2.weight/auto', 'input_data/auto', 'loss/auto',
+                        'histogram', 'image', 'scalar', 'tensor'}
+    assert set(expected_tag_set) == set(tag_list)
+
+    # num samples is 10, batch size is 2, so step is 5, collect freq is 2,
+    # SummaryCollector will collect the first step and 2th, 4th step
+    tag_count = 3
+    for value in Counter(tag_list).values(): # pylint: disable=E1121
+        assert value == tag_count
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_ascend_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.env_onecard
+@security_off_wrap
+def test_summary_with_sink_mode_true():
+    """
+    Feature: Test summary with sink mode true, and num samples is 64.
+    Description: Test summary with sink mode true, and num samples is 64.
+    Expectation: Passed.
+    """
+    context.set_context(mode=context.GRAPH_MODE)
+    summary_dir = run_network(dataset_sink_mode=True, num_samples=10, dir_suffix="test_summary_with_sink_mode_true")
+
+    tag_list = TestSummary.list_summary_tags(summary_dir.name)
+
+    # There will not record input data when dataset sink mode is True
+    expected_tags = {'conv1.weight/auto', 'conv2.weight/auto', 'fc1.weight/auto', 'fc1.bias/auto',
+                     'fc2.weight/auto', 'loss/auto', 'histogram', 'image', 'scalar', 'tensor'}
+    assert set(expected_tags) == set(tag_list)
+
+    tag_count = 1
+    for value in Counter(tag_list).values(): # pylint: disable=E1121
+        assert value == tag_count
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_ascend_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.env_onecard
+@security_off_wrap
+def test_summarycollector_user_defind():
+    """
+    Feature: Test SummaryCollector with user-defined.
+    Description: Test SummaryCollector with user-defined.
+    Expectation: Passed.
+    """
+    context.set_context(mode=context.GRAPH_MODE)
+    summary_dir = run_network(dataset_sink_mode=True, num_samples=2, dir_suffix="test_summarycollector_user_defind",
+                              custom_lineage_data={'test': 'self test'},
+                              export_options={'tensor_format': 'npy'})
+
+    tag_list = TestSummary.list_summary_tags(summary_dir.name)
+    file_list = TestSummary.list_tensor_files(summary_dir.name)
+    # There will not record input data when dataset sink mode is True
+    expected_tags = {'conv1.weight/auto', 'conv2.weight/auto', 'fc1.weight/auto', 'fc1.bias/auto',
+                     'fc2.weight/auto', 'loss/auto', 'histogram', 'image', 'scalar', 'tensor'}
+    assert set(expected_tags) == set(tag_list)
+    expected_files = {'tensor_1.npy'}
+    assert set(expected_files) == set(file_list)
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_ascend_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.env_onecard
+@security_off_wrap
+def test_summary_collector_landscape():
+    """
+    Feature: Summary collector with landscape.
+    Description: Test summary collector with landscape.
+    Expectation: Landscape data collected with expected value.
+    """
+    context.set_context(mode=context.GRAPH_MODE)
+    set_seed(1)
+    interval_1 = [1, 2, 3]
+    num_samples = 6
+    summary_dir = train_network(epoch=3, num_samples=num_samples, dir_suffix="test_summary_collector_landscape",
+                                collect_specified_data={'collect_landscape':
+                                                        {'landscape_size': 4,
+                                                         'unit': 'epoch',
+                                                         'create_landscape': {'train': True, 'result': True},
+                                                         'num_samples': num_samples,
+                                                         'intervals': [interval_1]}})
+
+    tag_list = TestSummary.list_summary_collect_landscape_tags(summary_dir.name)
+    expected_tags = {'epoch_group', 'model_params_file_map', 'step_per_epoch', 'unit', 'num_samples',
+                     'landscape_size', 'create_landscape'}
+    assert set(expected_tags) == set(tag_list)
+    device_id = int(os.getenv('DEVICE_ID')) if os.getenv('DEVICE_ID') else 0
+    summary_landscape = SummaryLandscape(summary_dir.name)
+    summary_landscape.gen_landscapes_with_multi_process(callback_fn, device_ids=[device_id])
+    tag_list_landscape = TestSummary.list_landscape_tags(summary_dir.name)
+    assert np.allclose(tag_list_landscape[0], 2.28, atol=0.03)
+    assert np.allclose(tag_list_landscape[1], 2.28, atol=0.03)
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_ascend_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_gpu_training
+@pytest.mark.env_onecard
+@security_off_wrap
+def test_summary_of_more_than_one_instance():
+    """
+    Feature: Test the multi instances of SummaryRecord in a script.
+    Description: Multi instances of SummaryRecord in a script.
+    Expectation: Throw RuntimeError.
+    """
+    context.set_context(mode=context.GRAPH_MODE)
+    with pytest.raises(RuntimeError) as errinfo:
+        summary_dir1 = tempfile.TemporaryDirectory(suffix="test_summary_of_more_than_one_instance")
+        summary_record1 = SummaryRecord(log_dir=summary_dir1.name)
+        summary_dir2 = tempfile.TemporaryDirectory(suffix="test_summary_of_more_than_one_instance")
+        _ = SummaryRecord(log_dir=summary_dir2.name)
+    assert "only one instance is supported in a training process" in str(errinfo.value)
+    summary_record1.close()
