49e07cfea7cc10ad24763a72cd4f6c7215918fad
daiyuxin0511 <455472400@qq.com>
2024-03-22 10:57:37 +0800

    add UniformCandidateSampler adapter

--- mindspore/ccsrc/transform/graph_ir/op_adapter_map.h
+++ mindspore/ccsrc/transform/graph_ir/op_adapter_map.h
@@ -530,6 +530,7 @@ constexpr const char kNameCumulativeLogsumexp[] = "CumulativeLogsumexp";
 constexpr const char kNameMatmulReduceScatter[] = "MatmulReduceScatter";
 constexpr const char kNameAllGatherMatmul[] = "AllGatherMatmul";
 constexpr const char kSilentCheck[] = "SilentCheck";
+constexpr const char kNameUniformCandidateSampler[] = "UniformCandidateSampler";
 
 class OpAdapterDesc;
 
