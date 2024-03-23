e4041484512cc51b489fb896aa2cff973ab16b8b
yuchaojie <yuchaojie1@huawei.com>
2024-03-23 09:49:37 +0800

    clean code

--- mindspore/python/mindspore/ops/operations/_grad_ops.py
+++ mindspore/python/mindspore/ops/operations/_grad_ops.py
@@ -1,4 +1,4 @@
-# Copyright 2020-2022 Huawei Technologies Co., Ltd
+# Copyright 2020-2024 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -3162,6 +3162,7 @@ class FlashAttentionScoreGrad(Primitive):
                                         'attn_mask', 'softmax_max', 'softmax_sum', 'softmax_out', 'attention_in',
                                         'prefix'],
                                 outputs=['dq', 'dk', 'dv', 'dpse'])
+
 
 class RmsNormGrad(Primitive):
     r"""
