63141fba7c3648afafe78c9943c5a63ad1b3d01f
yanghaoran <yanghaoran2@huawei.com>
2024-03-25 09:42:47 +0800

    takedown randomly failing testcases

--- tests/st/optimizer_ex/test_asgd_cmp.py
+++ tests/st/optimizer_ex/test_asgd_cmp.py
@@ -218,7 +218,7 @@ def test_asgd_lr_dynamic(mode):
     fact.result_cmp()
 
 
-@pytest.mark.level0
+@pytest.mark.level1
 @pytest.mark.platform_x86_gpu_training
 @pytest.mark.platform_arm_ascend_training
 @pytest.mark.platform_x86_ascend_training
