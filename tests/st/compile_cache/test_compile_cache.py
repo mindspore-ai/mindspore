a5415669df63e3f25f99c56610444727c7634479
xiao_yao1994 <adam.xiaoyao@huawei.com>
2024-03-27 22:13:41 +0800

    fix bug for I9BoZV and I9B30I

--- tests/st/compile_cache/test_compile_cache.py
+++ tests/st/compile_cache/test_compile_cache.py
@@ -192,6 +192,10 @@ def run_twice_with_different_networks(file_name_first, file_name_second, cache_p
         data_first = f_first.read()
     assert "Check the consistency of dependency files hash failed. Execute all the compilation actions." in data_first
 
+    for i in range(8):
+        ge_cache = cache_path + "/rank_" + str(i) + "/ge_cache"
+        shutil.rmtree(ge_cache)
+
     # Second run with compile cache
     cmd_second = f"GLOG_v=2 python " + file_name_second + " '" + cache_path + "' > " + log_file_name_second + " 2>&1"
     subprocess.check_output(cmd_second, shell=True)
