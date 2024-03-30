3b63493436699af6d56b45b3cb22ef2570e11205
zhang-minli <zhangminli8@huawei.com>
2024-03-30 15:05:46 +0800

    add matml_ffn parallel split

--- mindspore/ccsrc/frontend/parallel/ops_info/matmul_ffn_info.cc
+++ mindspore/ccsrc/frontend/parallel/ops_info/matmul_ffn_info.cc
@@ -14,30 +14,22 @@
  * limitations under the License.
  */
 
-#include "frontend/parallel/ops_info/matmul_qkv_info.h"
+#include "frontend/parallel/ops_info/matmul_ffn_info.h"
 #include "frontend/parallel/dynamic_creator.h"
 
 namespace mindspore {
 namespace parallel {
-// MatMulQkv has 3 inputs and 3 outputs
-// x:         (batch * seq_len (inc is 1), query_hidden_size)
-// q:         (query_hidden_size, query_hidden_size)
-// k:         (key_hidden_size, query_hidden_size)
-// v:         (value_hidden_size, query_hidden_size)
+// MatMulQkv has 3 inputs and 2 outputs
+// x:         (batch * seq_len (inc is 1), hidden_size)
+// weight_1:         (weight_1_hidden_size, hidden_size)
+// weight_2:         (weight_2_hidden_size, hidden_size)
 // ------------------------------
-// output_q:  (batch * seq_len (inc is 1), query_hidden_size)
-// output_k:  (batch * seq_len (inc is 1), key_hidden_size)
-// output_v:  (batch * seq_len (inc is 1), value_hidden_size)
+// output_1:  (batch * seq_len (inc is 1), weight_1_hidden_size)
+// output_2:  (batch * seq_len (inc is 1), weight_2_hidden_size)
 
-// split strategy
-// batch is not able to split
-// seq_len is not able to split
-// query_hidden_size is able to split
-// key_hidden_size is able to split
-// value_hidden_size is able to split
-constexpr size_t kMatMulQkvOutputSize = 3;
+constexpr size_t kMatMulFfnOutputSize = 2;
 
-Status MatmulQkvInfo::CheckStrategy(const StrategyPtr &strategy) {
+Status MatmulFfnInfo::CheckStrategy(const StrategyPtr &strategy) {
   if (CheckStrategyValue(strategy, inputs_shape_) != SUCCESS) {
     return FAILED;
   }
@@ -47,41 +39,36 @@ Status MatmulQkvInfo::CheckStrategy(const StrategyPtr &strategy) {
   return SUCCESS;
 }
 
-Status MatmulQkvInfo::InferDevMatrixShape() {
+Status MatmulFfnInfo::InferDevMatrixShape() {
   auto input_strategies = strategy()->GetInputDim();
-  auto x = input_strategies.at(0);  // (batch * seq_len, q_hidden_size)
-  auto q = input_strategies.at(1);
+  auto x = input_strategies.at(0);  // (batch * seq_len, hidden_size)
+  auto weight_1 = input_strategies.at(1);
   // dp   mp
   // 1    0
-  dev_matrix_shape_ = {x.at(0), q.at(0)};
+  dev_matrix_shape_ = {x.at(0), weight_1.at(0)};
 
   return SUCCESS;
 }
 
-Status MatmulQkvInfo::InferTensorMap() {
+Status MatmulFfnInfo::InferTensorMap() {
   Shape x_tensor_map{1, -1};
-  Shape q_tensor_map{0, -1};
-  Shape k_tensor_map{0, -1};
-  Shape v_tensor_map{0, -1};
-
+  Shape weight_1_tensor_map{0, -1};
+  Shape weight_2_tensor_map{0, -1};
   inputs_tensor_map_.emplace_back(x_tensor_map);
-  inputs_tensor_map_.emplace_back(q_tensor_map);
-  inputs_tensor_map_.emplace_back(k_tensor_map);
-  inputs_tensor_map_.emplace_back(v_tensor_map);
+  inputs_tensor_map_.emplace_back(weight_1_tensor_map);
+  inputs_tensor_map_.emplace_back(weight_2_tensor_map);
 
   Shape output_q_tensor_map{1, 0};
   Shape output_k_tensor_map{1, 0};
-  Shape output_v_tensor_map{1, 0};
   outputs_tensor_map_.emplace_back(output_q_tensor_map);
   outputs_tensor_map_.emplace_back(output_k_tensor_map);
-  outputs_tensor_map_.emplace_back(output_v_tensor_map);
 
   return SUCCESS;
 }
 
-Status MatmulQkvInfo::InferAsLossDivisor() {
-  if (outputs_tensor_map_.size() != kMatMulQkvOutputSize) {
-    MS_LOG(ERROR) << name_ << ": The size of outputs tensor map must be 3, but got " << outputs_tensor_map_.size();
+Status MatmulFfnInfo::InferAsLossDivisor() {
+  if (outputs_tensor_map_.size() != kMatMulFfnOutputSize) {
+    MS_LOG(ERROR) << name_ << ": The size of outputs tensor map must be 2, but got " << outputs_tensor_map_.size();
     return FAILED;
   }
   as_loss_divisor_ = ComputeRepeatDeviceNumByTensorMap(dev_matrix_shape_, outputs_tensor_map_[0]);
@@ -90,6 +77,6 @@ Status MatmulQkvInfo::InferAsLossDivisor() {
                << ", as_loss_divisor_ is " << as_loss_divisor_;
   return SUCCESS;
 }
-REGISTER(MatmulQkvInfo);
+REGISTER(MatmulFfnInfo);
 }  // namespace parallel
 }  // namespace mindspore
