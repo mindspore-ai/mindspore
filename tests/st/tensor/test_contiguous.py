90cb9790f23600e43ef13b4f235427a10d405c77
fengyihang <fengyihang1@huawei.com>
2024-04-01 11:18:55 +0800

    add st mark r2.3

--- tests/st/tensor/test_contiguous.py
+++ tests/st/tensor/test_contiguous.py
@@ -27,6 +27,8 @@ from mindspore import dtype as mstype
 @pytest.mark.platform_x86_cpu
 @pytest.mark.platform_arm_cpu
 @pytest.mark.platform_x86_gpu_training
+@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_x86_ascend_training
 @pytest.mark.env_onecard
 def test_contiguous_pynative():
     """
