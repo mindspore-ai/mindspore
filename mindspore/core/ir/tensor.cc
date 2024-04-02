b1d01c66924b384dcac261ba1c51dffc386d963f
huangbingjian <huangbingjian1@huawei.com>
2024-04-02 15:12:15 +0800

    Optimize error message.

--- mindspore/core/ir/tensor.cc
+++ mindspore/core/ir/tensor.cc
@@ -653,7 +653,7 @@ TensorDataPtr MakeTensorData(TypeId data_type, Args &&... args) {
     default:
       break;
   }
-  MS_LOG(ERROR) << "Cannot construct Tensor because of unsupported data type: " << data_type << ".";
+  MS_LOG(ERROR) << "Cannot construct Tensor because of unsupported data type: " << TypeIdToString(data_type) << ".";
   return nullptr;
 }
 
