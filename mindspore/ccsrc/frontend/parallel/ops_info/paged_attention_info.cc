a727e4e9f78abef51eb97424724bd55d58dfcf63
mengyuanli <limengyuan4@huawei.com>
2024-04-03 09:31:38 +0800

    change ReshapeAndCache's k and v form (b*s, n, d) format to (b, s, h) format. change PA's q form (b*s, n, d) format to (b, s, h) format.

--- mindspore/ccsrc/frontend/parallel/ops_info/paged_attention_info.cc
+++ mindspore/ccsrc/frontend/parallel/ops_info/paged_attention_info.cc
@@ -20,18 +20,19 @@
 namespace mindspore {
 namespace parallel {
 // PagedAttention has 5 inputs
-// query:         (batch * seq_len (inc is 1), num_head, head_dim)
+// query:         (batch, seq_len, hidden_size)
 // key_cache:     (block_size, num_blocks, num_head, head_dim)
 // value_cache:   (block_size, num_blocks, num_head, head_dim)
 // block_tables:  (batch, max_num_block_per_batch)
 // context_lens:  (batch * seq_len)
 // ------------------------------
-// output:        (batch * seq_len (inc is 1), num_head, head_dim)
+// output:        (batch, seq_len, hidden_size)
 
 // split strategy
 // num_blocks is not able to split
 // block_size is not able to split
 // batch is able to split
+// hidden_size is able to split
 // num_head is able to split
 // head_dim is able to split
 // max_num_block_per_batch is not able to split
@@ -42,7 +43,7 @@ Status PagedAttentionInfo::CheckStrategy(const StrategyPtr &strategy) {
   }
 
   auto input_strategies = strategy->GetInputDim();
-  auto strategy_query = input_strategies.at(0);         // (dp, mp, 1)
+  auto strategy_query = input_strategies.at(0);         // (dp, 1, mp)
   auto strategy_key_cache = input_strategies.at(1);     // (1, 1, mp, 1)
   auto strategy_value_cache = input_strategies.at(2);   // (1, 1, mp, 1)
   auto strategy_block_tables = input_strategies.at(3);  // (dp, 1)
@@ -69,15 +70,17 @@ Status PagedAttentionInfo::CheckStrategy(const StrategyPtr &strategy) {
     return FAILED;
   }
 
-  if ((strategy_key_cache.at(2) != strategy_value_cache.at(2)) || (strategy_query.at(1) != strategy_key_cache.at(2))) {
-    MS_LOG(ERROR) << name_ << ": Invalid strategy: The num_head must be shard at the same time, but got"
-                  << " query's strategy: " << strategy_query << ", key_cache's strategy: " << strategy_key_cache
-                  << ", value_cache's strategy: " << strategy_value_cache;
+  if ((strategy_key_cache.at(2) != strategy_value_cache.at(2)) || (strategy_query.at(2) != strategy_key_cache.at(2))) {
+    MS_LOG(ERROR)
+      << name_
+      << ": Invalid strategy: The cache num_head and update hidden_size must be shard at the same time, but got"
+      << " query's strategy: " << strategy_query << ", key_cache's strategy: " << strategy_key_cache
+      << ", value_cache's strategy: " << strategy_value_cache;
     return FAILED;
   }
 
-  if ((strategy_key_cache.at(3) != strategy_value_cache.at(3)) || (strategy_query.at(2) != strategy_key_cache.at(3))) {
-    MS_LOG(ERROR) << name_ << ": Invalid strategy: The head_dim must be shard at the same time, but got"
+  if ((strategy_key_cache.at(3) != strategy_value_cache.at(3)) || (strategy_key_cache.at(3) != 1)) {
+    MS_LOG(ERROR) << name_ << ": Invalid strategy: The head_dim can't be shard, but got"
                   << " query's strategy: " << strategy_query << ", key_cache's strategy: " << strategy_key_cache
                   << ", value_cache's strategy: " << strategy_value_cache;
     return FAILED;
@@ -88,23 +91,23 @@ Status PagedAttentionInfo::CheckStrategy(const StrategyPtr &strategy) {
 
 Status PagedAttentionInfo::InferDevMatrixShape() {
   auto input_strategies = strategy()->GetInputDim();
-  auto query = input_strategies.at(0);         // (batch * seq_len (inc is 1), num_head, head_dim)
-  auto cache = input_strategies.at(1);         // (block_size, num_blocks, num_head, head_dim)
+  auto query = input_strategies.at(0);         // (batch, seq_len, hidden_size)
+  auto cache = input_strategies.at(1);         // (block_size, num_blocks, hidden_size)
   auto block_tables = input_strategies.at(3);  // (batch, max_num_block_per_batch)
-  auto context_lens = input_strategies.at(4);  // (batch * seq_len (inc is 1))
+  auto context_lens = input_strategies.at(4);  // (context_lens)
 
-  // query_batch   context_lens   block_batch   num_head   head_dim
-  // 4             3              2             1          0
-  dev_matrix_shape_ = {query.at(0), context_lens.at(0), block_tables.at(0), cache.at(2), cache.at(3)};
+  // batch   block_size   num_blocks   seq_len   hidden_size
+  //  4          3             2          1          0
+  dev_matrix_shape_ = {query.at(0), cache.at(0), cache.at(1), query.at(1), cache.at(2)};
 
   return SUCCESS;
 }
 
 Status PagedAttentionInfo::InferTensorMap() {
   Shape query_tensor_map{4, 1, 0};
-  Shape cache_tensor_map{-1, -1, 1, 0};
-  Shape block_tensor_map{2, -1};
-  Shape context_tensor_map{3};
+  Shape cache_tensor_map{-1, -1, 0, -1};
+  Shape block_tensor_map{4, -1};
+  Shape context_tensor_map{-1};
   inputs_tensor_map_.emplace_back(query_tensor_map);
   inputs_tensor_map_.emplace_back(cache_tensor_map);
   inputs_tensor_map_.emplace_back(cache_tensor_map);
