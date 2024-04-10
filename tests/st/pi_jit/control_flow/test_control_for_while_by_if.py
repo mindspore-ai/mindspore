88d77d7e8a25af9b4185ec42965f9e24566a7c28
hejianheng001 <hejianheng1@huawei.com>
2024-04-10 11:25:07 +0800

    Summary: Add control testcases
    Signed-of-by: hejianheng001
    
    Email: hejianheng1@huawei.com

---
+++ tests/st/pi_jit/control_flow/test_control_for_while_by_if.py
@@ -0,0 +1,102 @@
+import numpy as np
+from mindspore.nn import Cell
+from mindspore.common import dtype as ms
+from mindspore import Tensor
+from mindspore.ops import operations as P
+from mindspore import context, jit
+from mindspore.common.parameter import Parameter
+from ..share.utils import match_array
+import pytest
+
+
+class CtrlWhilebyIfBR(Cell):
+    def __init__(self, t):
+        super().__init__()
+        self.add = P.Add()
+        self.mul = P.Mul()
+        self.para = Parameter(t, name="a")
+
+    def construct(self, x, y):
+        out = self.add(y, y)
+        while x > -4:
+            x -= 1
+            if x < 0:
+                out = self.mul(out, out)
+                break
+            out = self.add(out, y)
+            if x < -1:
+                return out
+        if x > -4:
+            out = self.add(out, self.para)
+        return out
+
+
+@pytest.mark.level5
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_control_flow_while_by_if_break_return():
+    """
+    Feature: PIJit
+    Description: create a net, with while by if, break return in while
+    Expectation: No exception.
+    """
+    input_np = np.random.randn(3, 2).astype(np.float32)
+    x = Tensor([5], ms.int32)
+    t = Tensor(input_np, ms.int32)
+    y = Tensor(input_np, ms.int32)
+    context.set_context(mode=context.GRAPH_MODE)
+    jit(fn=CtrlWhilebyIfBR.construct, mode="PSJit")
+    ps_net = CtrlWhilebyIfBR(t)
+    ps_out = ps_net(x, y)
+    context.set_context(mode=context.PYNATIVE_MODE)
+    jit(fn=CtrlWhilebyIfBR.construct, mode="PIJit")
+    pi_net = CtrlWhilebyIfBR(t)
+    pi_out = pi_net(x, y)
+    match_array(ps_out, pi_out)
+
+
+class CtrlWhilebyIfCR(Cell):
+    def __init__(self, t):
+        super().__init__()
+        self.add = P.Add()
+        self.mul = P.Mul()
+        self.assign = P.Assign()
+        self.para = Parameter(Tensor(t, ms.float32), name="a")
+
+    def construct(self, x, y):
+        out = self.mul(y, y)
+        while x > 5:
+            self.para -= 1
+            x += 1
+            if x > 3:
+                self.assign(self.para, x)
+                continue
+            out = self.add(out, y)
+        if x != 3:
+            return out
+        out = self.mul(out, y)
+        return out
+
+
+@pytest.mark.level5
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_control_flow_while_by_if_continue_return():
+    """
+    Feature: PIJit
+    Description: create a net, with while by if, continue in while, return in if
+    Expectation: No exception.
+    """
+    input_np = np.random.randn(3, 2).astype(np.float32)
+    x = Tensor([2], ms.int32)
+    t = Tensor([8], ms.int32)
+    y = Tensor(input_np, ms.int32)
+    context.set_context(mode=context.GRAPH_MODE)
+    jit(fn=CtrlWhilebyIfCR.construct, mode="PSJit")
+    ps_net = CtrlWhilebyIfCR(t)
+    ps_out = ps_net(x, y)
+    context.set_context(mode=context.PYNATIVE_MODE)
+    jit(fn=CtrlWhilebyIfCR.construct, mode="PIJit")
+    pi_net = CtrlWhilebyIfCR(t)
+    pi_out = pi_net(x, y)
+    match_array(ps_out, pi_out)
---
+++ tests/st/pi_jit/control_flow/test_control_while_in_for_return.py
@@ -0,0 +1,269 @@
+from mindspore.nn import Cell
+import mindspore.ops.operations as P
+from mindspore.common.parameter import Parameter
+from .ctrl_factory import CtrlFactory
+import pytest
+
+
+class CtrlWhileInForReturnX(Cell):
+    def __init__(self):
+        super().__init__()
+        self.add = P.Add()
+
+    def construct(self, x, t):
+        out = t
+        for _ in range(4):
+            out = self.add(out, t)
+            x += 1
+            while x > 4:
+                x -= 1
+                out = self.add(out, t)
+            if x < 2:
+                return out
+        return out
+
+
+@pytest.mark.level1
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_control_flow_while_in_for_return_in_for_x():
+    '''
+    TEST_SUMMARY:
+    Description:
+        1. create a net, with while in for, return in for
+        2. change x
+    Expectation:
+        1. the network run ok
+        2. the network forward and backward result is the same as psjit
+    '''
+    x = 6
+    t = [1, 2, 3]
+    fact = CtrlFactory(x, t)
+    ps_net = CtrlWhileInForReturnX()
+    pi_net = CtrlWhileInForReturnX()
+    fact.compare(ps_net, pi_net)
+
+
+class CtrlWhileInForReturn(Cell):
+    def __init__(self):
+        super().__init__()
+        self.add = P.Add()
+
+    def construct(self, x):
+        out = x
+        for i in range(10):
+            out = self.add(out, x)
+            if i > 5:
+                return out
+            while x > 3:
+                out = self.add(out, x)
+                x -= 1
+        return out
+
+
+@pytest.mark.level1
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_control_flow_while_in_for_return_in_for():
+    '''
+    TEST_SUMMARY:
+    Description:
+        1. create a net, with while in for, return in for
+        2. not change x
+    Expectation:
+        1. the network run ok
+        2. the network forward and backward result is the same as psjit
+    '''
+    x = 9
+    fact = CtrlFactory(x)
+    ps_net = CtrlWhileInForReturn()
+    pi_net = CtrlWhileInForReturn()
+    fact.compare(ps_net, pi_net)
+
+
+class CtrlWhileInForReturnOne(Cell):
+    def __init__(self, tensor):
+        super().__init__()
+        self.param = Parameter(tensor, name="p")
+
+    def construct(self, x):
+        for _ in range(3):
+            self.param += 2
+            while x < 5:
+                self.param += 1
+                x += 1
+            if x > 1:
+                return x
+        return x
+
+
+@pytest.mark.level1
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_control_flow_for_in_while_param_return_in_for():
+    '''
+    TEST_SUMMARY:
+    Description:
+        1. create a net, with while in for, return in for
+        2. change parameter
+    Expectation:
+        1. the network run ok
+        2. the network forward and backward result is the same as psjit
+    '''
+    x = -2
+    t = 2
+    fact = CtrlFactory(x)
+    ps_net = CtrlWhileInForReturnOne(t)
+    pi_net = CtrlWhileInForReturnOne(t)
+    fact.compare(ps_net, pi_net)
+
+
+class CtrlWhileInForReturnAdd(Cell):
+    def __init__(self):
+        super().__init__()
+        self.add = P.Add()
+
+    def construct(self, x):
+        out = x
+        for _ in range(3):
+            out = self.add(out, x)
+            while x < 5:
+                out = self.add(out, x)
+                x += 1
+            if x > 1:
+                return out
+        return out
+
+
+@pytest.mark.level1
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_control_flow_while_in_for_return_in_while_no_param():
+    '''
+    TEST_SUMMARY:
+    Description:
+        1. create a net, with while in for, return in for
+        2. no parameter
+    Expectation:
+        1. the network run ok
+        2. the network forward and backward result is the same as psjit
+    '''
+    x = -2
+    fact = CtrlFactory(x)
+    ps_net = CtrlWhileInForReturnAdd()
+    pi_net = CtrlWhileInForReturnAdd()
+    fact.compare(ps_net, pi_net)
+
+
+class CtrlWhileReturnInForX(Cell):
+    def __init__(self):
+        super().__init__()
+        self.add = P.Add()
+
+    def construct(self, x):
+        out = x
+        for i in range(3):
+            x -= i
+            while x > 1:
+                out = self.add(out, x)
+                x -= 1
+                if x < 0:
+                    return out
+            out = self.add(out, x)
+        return out
+
+
+@pytest.mark.level1
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_control_flow_while_in_for_return_in_while_x():
+    '''
+    TEST_SUMMARY:
+    Description:
+        1. create a net, with while in for, return in while
+        2. change x
+    Expectation:
+        1. the network run ok
+        2. the network forward and backward result is the same as psjit
+    '''
+    x = 3
+    fact = CtrlFactory(x)
+    ps_net = CtrlWhileReturnInForX()
+    pi_net = CtrlWhileReturnInForX()
+    fact.compare(ps_net, pi_net)
+
+
+class CtrlWhileReturnInFor(Cell):
+    def __init__(self):
+        super().__init__()
+        self.add = P.Add()
+
+    def construct(self, x):
+        out = x
+        tmp = x
+        for _ in range(5):
+            out = self.add(out, x)
+            while x > 1:
+                x -= 1
+                out = self.add(out, x)
+                if x > 2:
+                    return out
+            x = tmp
+        return out
+
+
+@pytest.mark.level0
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_control_flow_while_in_for_return_in_for_nochange():
+    '''
+    TEST_SUMMARY:
+    Description:
+        1. create a net, with while in for, return in while
+        2. not change x
+    Expectation:
+        1. the network run ok
+        2. the network forward and backward result is the same as psjit
+    '''
+    x = 3
+    fact = CtrlFactory(x)
+    ps_net = CtrlWhileReturnInFor()
+    pi_net = CtrlWhileReturnInFor()
+    fact.compare(ps_net, pi_net)
+
+
+class CtrlWhileReturnInForN(Cell):
+    def __init__(self):
+        super().__init__()
+        self.add = P.Add()
+
+    def construct(self, x):
+        out = x
+        for _ in range(3):
+            out = self.add(out, x)
+            while x < 5:
+                out = self.add(out, x)
+                if x > 1:
+                    return x
+                x += 1
+        return x
+
+
+@pytest.mark.level1
+@pytest.mark.platform_x86_cpu
+@pytest.mark.env_onecard
+def test_control_flow_while_in_for_return_in_while_no():
+    '''
+    TEST_SUMMARY:
+    Description:
+        1. create a net, with while in for, return in while
+        2. no parameter
+    Expectation:
+        1. the network run ok
+        2. the network forward and backward result is the same as psjit
+    '''
+    x = -3
+    fact = CtrlFactory(x)
+    ps_net = CtrlWhileReturnInForN()
+    pi_net = CtrlWhileReturnInForN()
+    fact.compare(ps_net, pi_net)
