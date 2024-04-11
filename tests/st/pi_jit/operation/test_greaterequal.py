88dda22f10f07f35a73b9e11bd55c4794cb1bb7d
hejianheng001 <hejianheng1@huawei.com>
2024-04-11 16:50:44 +0800

    Summary: remove gpu training
    Signed-of-by: hejianheng001
    
    Email: hejianheng1@huawei.com

--- tests/st/pi_jit/operation/test_greaterequal.py
+++ tests/st/pi_jit/operation/test_greaterequal.py
@@ -14,7 +14,6 @@ def greater_equal_backward_func(x, y):
 
 @pytest.mark.level0
 @pytest.mark.platform_x86_cpu
-@pytest.mark.platform_x86_gpu_training
 @pytest.mark.env_onecard
 def test_greater_equal_forward():
     """
@@ -32,7 +31,6 @@ def test_greater_equal_forward():
 
 @pytest.mark.level0
 @pytest.mark.platform_x86_cpu
-@pytest.mark.platform_x86_gpu_training
 @pytest.mark.env_onecard
 def test_greater_equal_backward():
     """
--- tests/st/pi_jit/operation/test_inplacesub.py
+++ tests/st/pi_jit/operation/test_inplacesub.py
@@ -78,7 +78,6 @@ def test_dynamic_shape_p_inplacesub_input_2d_float32():
 
 @pytest.mark.level1
 @pytest.mark.platform_x86_cpu
-@pytest.mark.platform_x86_gpu_training
 @pytest.mark.env_onecard
 def test_dynamic_shape_p_inplacesub_input_3d_float64():
     """
