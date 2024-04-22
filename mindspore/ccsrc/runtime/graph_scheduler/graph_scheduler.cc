b0414de67b0a495531a08daa337a20eabaae2af5
reku1997 <wangyucheng12@huawei.com>
2024-04-22 14:54:46 +0800

    fix runtime skip condition

--- mindspore/ccsrc/runtime/graph_scheduler/graph_scheduler.cc
+++ mindspore/ccsrc/runtime/graph_scheduler/graph_scheduler.cc
@@ -2093,7 +2093,8 @@ void GraphScheduler::LinkControlArrowByAutoMonad(
   if (checked_nodes != nullptr) {
     auto context_ptr = MsContext::GetInstance();
     MS_EXCEPTION_IF_NULL(context_ptr);
-    if (context_ptr->get_param<int>(MS_CTX_MEMORY_OPTIMIZE_LEVEL) != kOptimizeO0) {
+    if (context_ptr->get_param<int>(MS_CTX_MEMORY_OPTIMIZE_LEVEL) != kOptimizeO0 &&
+        context_ptr->CellReuseLevel() == CellReuseLevel::kLazyInline && context_ptr->IsKByKExecutorMode()) {
       return;
     }
     if (checked_nodes->find(input_cnode) != checked_nodes->end()) {
