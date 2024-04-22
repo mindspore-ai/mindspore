b3afff3ad7e5dd17e42ddeb7cd93bd7fdc0b3661
liuchongming <liuchongming1@huawei.com>
2024-04-22 10:34:31 +0800

    Fix send and recv in dynamic shape.

--- mindspore/ccsrc/plugin/device/ascend/kernel/hccl/hccl_kernel.h
+++ mindspore/ccsrc/plugin/device/ascend/kernel/hccl/hccl_kernel.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019-2022 Huawei Technologies Co., Ltd
+ * Copyright 2019-2024 Huawei Technologies Co., Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -71,7 +71,6 @@ class HcclKernel : public KernelMod {
   bool is_graph_mode_{false};
 };
 
-extern int64_t op_tag;
 using HcclKernelCreater = std::function<std::shared_ptr<HcclKernel>()>;
 
 class HcclKernelFactory {
