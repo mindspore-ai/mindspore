e85011a37b7ffa70c82fd69bf9b8cd7fc5606db3
chenfei <chenfei52@huawei.com>
2024-04-26 11:54:47 +0800

    add const tensor check of input tuple arg
    add tests

--- tests/ut/python/mutable/test_const_arg_tensor.py
+++ tests/ut/python/mutable/test_const_arg_tensor.py
@@ -13,9 +13,13 @@
 # limitations under the License.
 # ============================================================================
 """test const tensor for network arg"""
+import os
+import subprocess
 import numpy as np
+import mindspore as ms
 from mindspore.ops.composite import GradOperation
 from mindspore.common import mutable
+from mindspore import ops
 from mindspore.ops import operations as P
 import mindspore.nn as nn
 import mindspore.common.dtype as mstype
@@ -241,3 +245,189 @@ def test_check_mutable_value():
         x.set_const_arg(1)
     except TypeError as e:
         assert str(e) == "For 'set_const_arg', the type of 'const_arg' should be 'bool', but got type 'int'."
+
+
+def test_run_same_value_const_tensor_run_twice():
+    """
+    Feature: Check const tensor in tuple.
+    Description: Check warning raising when const tensor arg in tuple.
+    Expectation: No warning raised when const arg cause recompiling.
+    """
+
+    @ms.jit
+    def net_func_tuple_input(tuple_input):
+        return ops.add(tuple_input[0], tuple_input[1])
+
+    const_tensor1 = ms.Tensor([1])
+    const_tensor2 = ms.Tensor([1])
+    net_func_tuple_input((const_tensor1, const_tensor2))
+    net_func_tuple_input((const_tensor1, const_tensor2))
+
+
+def test_run_different_value_const_tensor_run_twice():
+    """
+    Feature: Check const tensor in tuple.
+    Description: Check warning raising when const tensor arg in tuple.
+    Expectation: Warning raised when const arg cause recompiling.
+    """
+
+    @ms.jit
+    def net_func_tuple_input(tuple_input):
+        return ops.add(tuple_input[0], tuple_input[1])
+
+    const_tensor1 = ms.Tensor([1])
+    const_tensor2 = ms.Tensor([2])
+    net_func_tuple_input((const_tensor1, const_tensor1))
+    net_func_tuple_input((const_tensor2, const_tensor2))
+
+
+def test_run_cell_different_value_const_tensor_run_twice():
+    """
+    Feature: Check const tensor in tuple.
+    Description: Check warning raising when const tensor arg in tuple.
+    Expectation: Warning raised when const arg cause recompiling.
+    """
+
+    class Net(nn.Cell):
+        def construct(self, tuple_input):
+            return ops.add(tuple_input[0], tuple_input[1])
+
+    const_tensor1 = ms.Tensor([1])
+    const_tensor2 = ms.Tensor([2])
+    context.set_context(mode=context.GRAPH_MODE)
+    net_func_tuple_input = Net()
+    net_func_tuple_input((const_tensor1, const_tensor1))
+    net_func_tuple_input((const_tensor2, const_tensor2))
+
+
+def test_run_different_value_mutable_tensor_run_twice():
+    """
+    Feature: Check const tensor in tuple.
+    Description: Check warning raising when const tensor arg in tuple.
+    Expectation: No warning raised.
+    """
+
+    @ms.jit
+    def net_func_tuple_input(tuple_input):
+        return ops.add(tuple_input[0], tuple_input[1])
+
+    const_tensor1 = ms.Tensor([1])
+    const_tensor2 = ms.Tensor([2])
+    net_func_tuple_input(mutable((const_tensor1, const_tensor1)))
+    net_func_tuple_input(mutable((const_tensor2, const_tensor2)))
+
+
+def test_run_different_value_const_tensor_run_twice2():
+    """
+    Feature: Check const tensor in tuple.
+    Description: Check warning raising when const tensor arg in tuple-tuple.
+    Expectation: Warning raised when const arg cause recompiling.
+    """
+
+    @ms.jit
+    def net_func_tuple_tuple_input(tuple_tuple_input):
+        return ops.add(tuple_tuple_input[0][0], tuple_tuple_input[0][1])
+
+    const_tensor1 = ms.Tensor([1])
+    const_tensor2 = ms.Tensor([2])
+    net_func_tuple_tuple_input(((const_tensor1, const_tensor1),))
+    net_func_tuple_tuple_input(((const_tensor2, const_tensor2),))
+
+
+def test_create_cell_instance_twice():
+    """
+    Feature: Check create same cell instance twice in function.
+    Description: Check warning raising when same cell create twice.
+    Expectation: Warning raised when cell create twice.
+    """
+
+    class Net(nn.Cell):
+        def construct(self, input_arg):
+            return ops.add(input_arg, input_arg)
+
+    def exec_cell_obj(arg):
+        return Net()(arg)
+
+    const_tensor1 = ms.Tensor([1])
+    context.set_context(mode=context.GRAPH_MODE)
+    exec_cell_obj(const_tensor1)
+    exec_cell_obj(const_tensor1)
+
+def test_create_cell_instance_twice_no_warning():
+    """
+    Feature: Check create same cell instance twice.
+    Description: Check warning raising when same cell create twice.
+    Expectation: No warning raised when cell create twice.
+    """
+
+    class Net(nn.Cell):
+        def construct(self, input_arg):
+            return ops.add(input_arg, input_arg)
+
+
+    const_tensor1 = ms.Tensor([1])
+    context.set_context(mode=context.GRAPH_MODE)
+    net1 = Net()
+    net2 = Net()
+    out1 = net1(const_tensor1)
+    out2 = net2(const_tensor1)
+    print("out1:", out1)
+    print("out2:", out2)
+
+def test_check_const_tensor_in_tuple_warning():
+    """
+    Feature: Check const tensor in tuple.
+    Description: Check warning raising when const tensor arg in tuple.
+    Expectation: Warning raised when const arg cause recompiling.
+    """
+
+    def run_test_case_and_save_log(file_name, case_name, filter_str, is_in):
+        _cur_dir = os.path.dirname(os.path.realpath(__file__))
+        file_name = os.path.join(_cur_dir, file_name)
+        assert os.path.exists(file_name)
+
+        log_file_name = case_name + "_tmp_log"
+        log_file_name = os.path.join(_cur_dir, log_file_name)
+        if os.path.exists(log_file_name):
+            os.remove(log_file_name)
+        assert not os.path.exists(log_file_name)
+        cmd_first = f"GLOG_v=2 pytest -s " + file_name + "::" + case_name + " > " + log_file_name + " 2>&1"
+        subprocess.check_output(cmd_first, shell=True)
+        assert os.path.exists(log_file_name)
+        with open(log_file_name, "r") as f_first:
+            data_first = f_first.read()
+        if is_in:
+            assert filter_str in data_first
+        else:
+            assert filter_str not in data_first
+
+        # Clean files
+        os.remove(log_file_name)
+
+    # Same value no warning log.
+    run_test_case_and_save_log("test_const_arg_tensor.py", "test_run_same_value_const_tensor_run_twice",
+                               "Constant value tensor are detected in tuple or list", False)
+
+    # Different value of tuple print warning log.
+    run_test_case_and_save_log("test_const_arg_tensor.py", "test_run_different_value_const_tensor_run_twice",
+                               "Constant value tensor are detected in tuple or list", True)
+
+    # Different value of tuple print warning log with cell.
+    run_test_case_and_save_log("test_const_arg_tensor.py", "test_run_cell_different_value_const_tensor_run_twice",
+                               "Constant value tensor are detected in tuple or list", True)
+
+    # Different mutable value of tuple no warning log.
+    run_test_case_and_save_log("test_const_arg_tensor.py", "test_run_different_value_mutable_tensor_run_twice",
+                               "Constant value tensor are detected in tuple or list", False)
+
+    # Different value of tuple-tuple print warning log.
+    run_test_case_and_save_log("test_const_arg_tensor.py", "test_run_different_value_const_tensor_run_twice2",
+                               "Constant value tensor are detected in tuple or list", True)
+
+    # Create instance twice print warning log
+    run_test_case_and_save_log("test_const_arg_tensor.py", "test_create_cell_instance_twice",
+                               "only once to avoid recompiling", True)
+
+    # Create instance twice no warning log
+    run_test_case_and_save_log("test_const_arg_tensor.py", "test_create_cell_instance_twice_no_warning",
+                               "only once to avoid recompiling", False)
