8fdf072b5778fe5f97e09363cabf287079dbb950
AllenXiao_O <xiaoshihan@huawei.com>
2024-04-30 18:30:47 +0800

    add fa alltoallv parallel

--- mindspore/ccsrc/plugin/device/ascend/optimizer/ge_backend_optimization.cc
+++ mindspore/ccsrc/plugin/device/ascend/optimizer/ge_backend_optimization.cc
@@ -63,6 +63,7 @@
 #include "plugin/device/ascend/optimizer/enhancer/eliminate_maketuple_getitem.h"
 #include "plugin/device/ascend/optimizer/ge/pad_v3_paddings.h"
 #include "plugin/device/ascend/optimizer/ge/broadcast_for_select.h"
+#include "plugin/device/ascend/optimizer/ge/fa_alltoallv_parallel.h"
 #include "plugin/device/ascend/optimizer/ir_fusion/shape_reshape_fusion.h"
 
 namespace mindspore {
@@ -84,6 +85,7 @@ void GEBackendOptimization(const KernelGraphPtr &kernel_graph) {
   opt_ge_pm->AddPass(std::make_shared<opt::GEConvertConstInputToTensorInput>());
   opt_ge_pm->AddPass(std::make_shared<opt::RemoveTensorToScalarOrTupleOps>());
   opt_ge_pm->AddPass(std::make_shared<opt::AllToAllvForGE>());
+  opt_ge_pm->AddPass(std::make_shared<opt::FaAlltoAllvParallel>());
   opt_ge_pm->AddPass(std::make_shared<opt::InsertLoadForAllGather>());
   opt_ge_pm->AddPass(std::make_shared<opt::InsertTensorMoveForHcclOpGe>());
   opt_ge_pm->AddPass(std::make_shared<opt::InsertDependForAllGatherGe>());
