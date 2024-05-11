a880183167270a4a537ddd7075977606ce7a3096
wen_xiang_zou_admin <zouwenxiang1@huawei.com>
2024-05-11 16:08:33 +0800

    aclnn weight quant batch matmul + int4

---
+++ mindspore/ccsrc/plugin/device/ascend/kernel/pyboost/customize/weight_quant_batch_matmul.h
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MINDSPORE_MINDSPORE_CCSRC_PLUGIN_DEVICE_ASCEND_KERNEL_PYBOOST_CUSTOMIZE_WEIGHT_QUANT_V2_H_
+#define MINDSPORE_MINDSPORE_CCSRC_PLUGIN_DEVICE_ASCEND_KERNEL_PYBOOST_CUSTOMIZE_WEIGHT_QUANT_V2_H_
+
+#include <vector>
+#include <memory>
+#include "ir/tensor.h"
+#include "ir/value.h"
+#include "runtime/hardware/device_context_manager.h"
+#include "kernel/pyboost/op_runner.h"
+
+namespace mindspore {
+namespace kernel {
+namespace pyboost {
+tensor::BaseTensorPtr WeightQuantBatchMatmulV2AscendCustomize(
+  const std::shared_ptr<OpRunner> &op, const BaseTensorPtr &x_tensor, const BaseTensorPtr &weight_tensor,
+  const BaseTensorPtr &antiquant_scale_tensor, const std::optional<BaseTensorPtr> &antiquant_offset_tensor,
+  const std::optional<BaseTensorPtr> &quant_scale_tensor, const std::optional<BaseTensorPtr> &quant_offset_tensor,
+  const std::optional<BaseTensorPtr> &bias_tensor, const BoolImmPtr &transpose_x, const BoolImmPtr &transpose_weight,
+  const Int64ImmPtr &antiquant_group_size);
+}  // namespace pyboost
+}  // namespace kernel
+}  // namespace mindspore
+#endif  // MINDSPORE_MINDSPORE_CCSRC_PLUGIN_DEVICE_ASCEND_KERNEL_PYBOOST_CUSTOMIZE_WEIGHT_QUANT_V2_H_
