89e02e64779ed1fc2da3c5c6cfcec56888714e8d
zhengzuohe <zhengzuohe@huawei.com>
2024-05-13 13:49:37 +0800

    FA attr to inputs

--- mindspore/ccsrc/plugin/device/ascend/kernel/internal/flash_attention_score.cc
+++ mindspore/ccsrc/plugin/device/ascend/kernel/internal/flash_attention_score.cc
@@ -30,21 +30,23 @@ internal::OpParamPtr InternalFlashAttentionScore::CreateOpParam(const std::vecto
   auto q_shape = inputs[kIndex0]->GetShapeVector();
   auto kv_shape = inputs[kIndex1]->GetShapeVector();
 
-  int64_t head_num = primitive_->HasAttr("head_num") ? GetValue<int64_t>(primitive_->GetAttr("head_num")) : 0;
+  int64_t head_num = primitive_->HasAttr("head_num") ? GetValue<int64_t>(primitive_->GetAttr("head_num"))
+                                                     : inputs[kIndex10]->GetValueWithCheck<int64_t>();
   int64_t head_dim = q_shape[kDim2] / head_num;
 
-  constexpr int64_t kDefaultPreTokens = 2147483647;
-  int64_t pre_tokens =
-    primitive_->HasAttr("pre_tokens") ? GetValue<int64_t>(primitive_->GetAttr("pre_tokens")) : kDefaultPreTokens;
-  int64_t next_tokens = primitive_->HasAttr("next_tokens") ? GetValue<int64_t>(primitive_->GetAttr("next_tokens")) : 0;
+  int64_t pre_tokens = primitive_->HasAttr("pre_tokens") ? GetValue<int64_t>(primitive_->GetAttr("pre_tokens"))
+                                                         : inputs[kIndex13]->GetValueWithCheck<int64_t>();
+  int64_t next_tokens = primitive_->HasAttr("next_tokens") ? GetValue<int64_t>(primitive_->GetAttr("next_tokens"))
+                                                           : inputs[kIndex14]->GetValueWithCheck<int64_t>();
 
   param_ptr->head_num = head_num;
-  param_ptr->inner_precise =
-    primitive_->HasAttr("inner_precise") ? GetValue<int64_t>(primitive_->GetAttr("inner_precise")) : 0;
+  param_ptr->inner_precise = primitive_->HasAttr("inner_precise")
+                               ? GetValue<int64_t>(primitive_->GetAttr("inner_precise"))
+                               : inputs[kIndex15]->GetValueWithCheck<int64_t>();
   param_ptr->pre_tokens = pre_tokens;
   param_ptr->next_tokens = next_tokens;
-  param_ptr->sparse_mode =
-    primitive_->HasAttr("sparse_mode") ? GetValue<int64_t>(primitive_->GetAttr("sparse_mode")) : 0;
+  param_ptr->sparse_mode = primitive_->HasAttr("sparse_mode") ? GetValue<int64_t>(primitive_->GetAttr("sparse_mode"))
+                                                              : inputs[kIndex17]->GetValueWithCheck<int64_t>();
 
   param_ptr->mask_dtype_ = InternalKernelUtils::ToInternalDType(inputs[kIndex6]->dtype_id());
   param_ptr->mask_dims_ = internal::VecToSVec<int64_t>(inputs[kIndex6]->GetShapeVector());
@@ -54,7 +56,8 @@ internal::OpParamPtr InternalFlashAttentionScore::CreateOpParam(const std::vecto
   op_param.headSize = head_num;
   op_param.preTokens = pre_tokens;
   op_param.nextTokens = next_tokens;
-  op_param.tor = primitive_->HasAttr("scale_value") ? GetValue<float>(primitive_->GetAttr("scale_value")) : 0;
+  op_param.tor = primitive_->HasAttr("scale_value") ? GetValue<float>(primitive_->GetAttr("scale_value"))
+                                                    : inputs[kIndex12]->GetValueWithCheck<float>();
   op_param.kvHead = kv_shape[kDim2] / head_dim;
 
   for (int64_t i = 0; i < kv_shape[kDim0]; i++) {
