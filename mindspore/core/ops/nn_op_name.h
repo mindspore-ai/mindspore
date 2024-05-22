c7b64d2b24ab6ef64644d50fcf41b6e680eca310
yiyanzhi <yiyanzhi@huawei.com>
2024-05-23 01:19:34 +0800

    kbk concat param with multi matmuls

--- mindspore/core/ops/nn_op_name.h
+++ mindspore/core/ops/nn_op_name.h
@@ -156,6 +156,7 @@ constexpr auto kMatmulQkvOpName = "MatmulQkv";
 constexpr auto kMatmulFfnOpName = "MatmulFfn";
 constexpr auto kWeightQuantMatmulQkvOpName = "WeightQuantMatmulQkv";
 constexpr auto kWeightQuantMatmulFfnOpName = "WeightQuantMatmulFfn";
+constexpr auto kMultiWeightMatmulV2OpName = "MultiWeightMatmulV2";
 }  // namespace mindspore
 
 #endif  // MINDSPORE_CORE_BASE_NN_OP_NAME_H_
