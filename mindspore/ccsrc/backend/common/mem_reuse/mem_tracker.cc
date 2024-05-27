0927226e7afa85bc5907501b86e5adc14b9c9e2d
caifubi <caifubi1@huawei.com>
2024-05-27 09:32:43 +0800

    fix cherry-pick bug

--- mindspore/ccsrc/backend/common/mem_reuse/mem_tracker.cc
+++ mindspore/ccsrc/backend/common/mem_reuse/mem_tracker.cc
@@ -140,7 +140,8 @@ void MemoryTrackerEnabled::AddCompileTimeMemInfo(const std::string &task_name, s
 
 void MemoryTrackerEnabled::BindDevicePtr(const DeviceAddress *device_address, DeviceMemPtr device_ptr,
                                          const std::string &file_name, size_t line_num) {
-  if (kernel_tensor == nullptr) {
+  MS_EXCEPTION_IF_NULL(device_address);
+  if (device_address->kernel_tensor() == nullptr) {
     return;
   }
   std::lock_guard lock(mutex_);
