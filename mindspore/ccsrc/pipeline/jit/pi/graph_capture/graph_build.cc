423be4b5d2cc8404aeabe40bef021ee907f0dc6e
chaiyouheng <chaiyouheng@huawei.com>
2024-05-29 14:44:45 +0800

    refact trace run for item access

--- mindspore/ccsrc/pipeline/jit/pi/graph_capture/graph_build.cc
+++ mindspore/ccsrc/pipeline/jit/pi/graph_capture/graph_build.cc
@@ -2800,11 +2800,10 @@ bool GraphBuilder::TraceRunForIterSequence(int jump_bci, bool is_range_type) {
 
   py::object index_object = py::int_(index);
   ValueNode *index_node = NewValueNode(AObject::Convert(index_object), LOAD_CONST, -1, {});
-  ValueNode *item_node = TupleDictGetItem(seq_node, index_node);
-  if (item_node == nullptr) {
-    item_node = NewValueNode(AObject::Convert(item), BINARY_SUBSCR, 0, {seq_node, index_node});
-    graph_->GetTracedNodes().push_back(item_node);
-  }
+  push(seq_node);
+  push(index_node);
+  DoItemAccess({BINARY_SUBSCR, 0});
+  ValueNode *item_node = pop();
   Py_DECREF(item);
 
   index++;
