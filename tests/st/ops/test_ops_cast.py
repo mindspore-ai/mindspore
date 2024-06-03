982db16ff73270567bb0140386165d39ad681a29
wujueying <wujueying1@huawei.com>
2024-06-03 19:03:25 +0800

    add testcase: cast,ne,conv2d

--- tests/st/ops/test_ops_cast.py
+++ tests/st/ops/test_ops_cast.py
@@ -46,38 +46,44 @@ def cast_vmap_func(x, dtype):
 @pytest.mark.platform_x86_gpu_training
 @pytest.mark.platform_arm_ascend_training
 @pytest.mark.parametrize("context_mode", [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
-def test_ops_cast_forward(context_mode):
+def test_ops_cast_normal(context_mode):
     """
     Feature: pyboost function.
     Description: test function cast forward.
     Expectation: expect correct result.
     """
     ms.context.set_context(mode=context_mode)
-    x = generate_random_input((2, 3, 4, 5), np.float32)
-    dtype = mstype.float16
+    x = generate_random_input((1280, 1280), np.float16)
+    dtype = mstype.float32
     output = cast_forward_func(ms.Tensor(x), dtype)
-    assert output.asnumpy().dtype == 'float16'
-    assert output.asnumpy().shape == (2, 3, 4, 5)
+    assert output.asnumpy().dtype == 'float32'
+    assert output.asnumpy().shape == (1280, 1280)
+
+    x = generate_random_input((1280,), np.float32)
+    dtype = mstype.float16
+    output = cast_backward_func(ms.Tensor(x), dtype)
+    assert output.asnumpy().dtype == 'float32'
+    assert output.asnumpy().shape == (1280,)
 
 
 @pytest.mark.level0
 @pytest.mark.env_onecard
-@pytest.mark.platform_x86_cpu
-@pytest.mark.platform_x86_gpu_training
-@pytest.mark.platform_arm_ascend_training
+@pytest.mark.platform_arm_ascend910b_training
 @pytest.mark.parametrize("context_mode", [ms.GRAPH_MODE, ms.PYNATIVE_MODE])
-def test_ops_cast_backward(context_mode):
+def test_ops_cast_bf16(context_mode):
     """
     Feature: pyboost function.
     Description: test function cast backward.
     Expectation: expect correct result.
     """
     ms.context.set_context(mode=context_mode)
-    x = generate_random_input((2, 3, 4), np.float32)
-    dtype = mstype.float16
+    x1 = generate_random_input((3, 32, 1), np.float32)
+    x2 = generate_random_input((3, 32, 1), np.float32)
+    x = tuple((x1, x2))
+    dtype = mstype.bfloat16
     output = cast_backward_func(ms.Tensor(x), dtype)
     assert output.asnumpy().dtype == 'float32'
-    assert output.asnumpy().shape == (2, 3, 4)
+    assert output.asnumpy().shape == (2, 3, 32, 1)
 
 
 @pytest.mark.level1
@@ -93,11 +99,11 @@ def test_ops_cast_vmap(context_mode):
     Expectation: expect correct result.
     """
     ms.context.set_context(mode=context_mode)
-    x = generate_random_input((2, 3, 4, 5), np.float32)
+    x = generate_random_input((640, 640), np.float32)
     dtype = mstype.float16
     output = cast_vmap_func(ms.Tensor(x), dtype)
     assert output.asnumpy().dtype == 'float16'
-    assert output.asnumpy().shape == (2, 3, 4, 5)
+    assert output.asnumpy().shape == (640, 640)
 
 
 @pytest.mark.level1
@@ -140,18 +146,18 @@ def test_ops_cast_forward_dynamic_rank(context_mode):
     Expectation: expect correct result.
     """
     ms.context.set_context(mode=context_mode)
-    x_dyn = ms.Tensor(shape=None, dtype=ms.float32)
+    x_dyn = ms.Tensor(shape=None, dtype=ms.float64)
     test_cell = test_utils.to_cell_obj(cast_forward_func)
-    dtype = mstype.float16
+    dtype = mstype.float32
     test_cell.set_inputs(x_dyn, dtype)
-    x1 = generate_random_input((2, 3, 4, 5), np.float32)
+    x1 = generate_random_input((1280, 1280, 3, 3), np.float64)
     output = test_cell(ms.Tensor(x1), dtype)
-    assert output.asnumpy().dtype == 'float16'
-    assert output.asnumpy().shape == (2, 3, 4, 5)
-    x2 = generate_random_input((3, 4, 5, 6), np.float32)
+    assert output.asnumpy().dtype == 'float32'
+    assert output.asnumpy().shape == (1280, 1280, 3, 3)
+    x2 = generate_random_input((320, 320), np.float64)
     output = test_cell(ms.Tensor(x2), dtype)
-    assert output.asnumpy().dtype == 'float16'
-    assert output.asnumpy().shape == (3, 4, 5, 6)
+    assert output.asnumpy().dtype == 'float32'
+    assert output.asnumpy().shape == (320, 320)
 
 @pytest.mark.level1
 @pytest.mark.env_onecard
@@ -197,11 +203,11 @@ def test_ops_cast_backward_dynamic_rank(context_mode):
     dtype = mstype.float16
     test_cell = test_utils.to_cell_obj(cast_backward_func)
     test_cell.set_inputs(x_dyn, dtype)
-    x1 = generate_random_input((2, 3, 4, 5), np.float32)
+    x1 = generate_random_input((1280, 1280, 3, 3), np.float32)
     output = test_cell(ms.Tensor(x1), dtype)
     assert output.asnumpy().dtype == 'float32'
-    assert output.asnumpy().shape == (2, 3, 4, 5)
-    x2 = generate_random_input((3, 4, 5, 6), np.float32)
+    assert output.asnumpy().shape == (1280, 1280, 3, 3)
+    x2 = generate_random_input((1280, 1280), np.float32)
     output = test_cell(ms.Tensor(x2), dtype)
     assert output.asnumpy().dtype == 'float32'
-    assert output.asnumpy().shape == (3, 4, 5, 6)
+    assert output.asnumpy().shape == (1280, 1280)
