e3f237d1f799b5cd027b546556f47e1578d8f798
jxlang910 <jinxiulang@huawei.com>
2024-06-03 10:53:23 +0800

    fix tts model converter error

--- mindspore/lite/tools/converter/adapter/acl/mapper/scatter_nd_update_mapper.cc
+++ mindspore/lite/tools/converter/adapter/acl/mapper/scatter_nd_update_mapper.cc
@@ -23,6 +23,7 @@
 #include "ops/tensor_copy.h"
 #include "src/common/log_util.h"
 #include "mindspore/core/ops/op_name.h"
+#include "ops/array_ops.h"
 
 namespace mindspore {
 namespace lite {
@@ -54,6 +55,27 @@ STATUS ScatterNdUpdateMapper::Mapper(const CNodePtr &cnode) {
   if (dst_prim == nullptr) {
     MS_LOG(ERROR) << "make ScatterNdUpdate failed.";
     return RET_ERROR;
+  }
+  TypeId type_id;
+  if (cnode->inputs().size() < kNumCnodeInputIndex + 1) {
+    MS_LOG(ERROR) << "The inputs num of " << cnode->fullname_with_scope() << " is smaller than "
+                  << (kNumCnodeInputIndex + 1) << ", please check it!";
+    return RET_ERROR;
+  }
+  auto scale_input = cnode->inputs()[kNumCnodeInputIndex];
+  if (opt::GetDataTypeFromAnfNode(scale_input, &type_id) != RET_OK) {
+    MS_LOG(ERROR) << "GetDataTypeFromAnfNode failed!";
+    return RET_ERROR;
+  }
+  if (type_id == kNumberTypeBool && cnode->input(kNumCnodeInputIndex)->abstract() != nullptr) {
+    auto cast_int32_node = NewCNode(
+      cnode, prim::kPrimCast, {cnode->input(kNumCnodeInputIndex), NewValueNode(TypeIdToType(kNumberTypeInt32))},
+      cnode->input(kNumCnodeInputIndex)->abstract()->Clone(), cnode->fullname_with_scope() + "_cast_int32");
+    if (cast_int32_node == nullptr) {
+      MS_LOG(ERROR) << "Make CNode failed!";
+      return RET_ERROR;
+    }
+    cnode->set_input(kNumCnodeInputIndex, cast_int32_node);
   }
   dst_prim->SetAttrs(src_prim->attrs());
   value_node->set_value(dst_prim);
