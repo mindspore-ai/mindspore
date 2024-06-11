64fecf42b559065a6a6469e10176860389e930ae
wang_ziqi <wangziqi4@huawei.com>
2024-06-11 09:46:59 +0800

    Remove ci yaml check
    ------
    
    Signed-off-by: wang_ziqi <wangziqi4@huawei.com>

--- mindspore/python/mindspore/ops_generate/gen_ops.py
+++ mindspore/python/mindspore/ops_generate/gen_ops.py
@@ -1030,7 +1030,6 @@ def main():
     # merge ops yaml
     ops_yaml_path = os.path.join(work_path, 'mindspore/python/mindspore/ops_generate/ops.yaml')
     doc_yaml_path = os.path.join(work_path, 'mindspore/python/mindspore/ops_generate/ops_doc.yaml')
-    st_yaml_path = os.path.join(work_path, 'tests/st/ops/dynamic_shape/test_op_utils.yaml')
 
     ops_yaml_dir_path = os.path.join(work_path, 'mindspore/core/ops/ops_def/')
     infer_ops_yaml_dir_path = os.path.join(work_path, 'mindspore/core/ops/ops_def/infer/')
@@ -1038,8 +1037,6 @@ def main():
     merge_files(ops_yaml_dir_path, ops_yaml_path, '*op.yaml')
     merge_files_append(infer_ops_yaml_dir_path, ops_yaml_path, '*op.yaml')
     merge_files(doc_yaml_dir_path, doc_yaml_path, '*doc.yaml')
-    merge_files(ops_yaml_dir_path, st_yaml_path, '*op.yaml')
-    merge_files_append(infer_ops_yaml_dir_path, st_yaml_path, '*op.yaml')
 
     # make auto_generate dir
     cc_path = os.path.join(work_path, 'mindspore/core/ops/auto_generate/')
