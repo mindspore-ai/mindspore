62987762a44faf5ba085d57874588cff33313b77
ckey_Dou <ckey.chengbin@huawei.com>
2024-06-13 18:03:00 +0800

    acme api for internal kernels

---
+++ mindspore/ccsrc/plugin/device/ascend/kernel/internal/acme/acme_helper.h
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2024 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_INTERNAL_KERNEL_ACME_ACME_HELPER_H_
+#define MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_INTERNAL_KERNEL_ACME_ACME_HELPER_H_
+
+#include <memory>
+#include "acme/include/acme.h"
+#include "include/api/format.h"
+#include "ir/dtype/type_id.h"
+#include "mindapi/base/shape_vector.h"
+
+namespace mindspore {
+namespace kernel {
+inline acme::ShapeInfo TransAcmeShape(const ShapeVector &shape) { return shape; }
+
+acme::DataType TransAcmeDataType(TypeId ms_type);
+
+acme::TensorFormat TransAcmeFormat(Format format);
+
+inline acme::ArgDescPtr MakeDefaultArgDesc(acme::TensorFormat format, acme::DataType dtype) {
+  acme::ShapeInfo shape{1};
+  return std::make_shared<acme::ArgDesc>(shape, dtype, format);
+}
+}  // namespace kernel
+}  // namespace mindspore
+#endif  // MINDSPORE_CCSRC_BACKEND_KERNEL_COMPILER_INTERNAL_KERNEL_ACME_ACME_HELPER_H_
