c07ea4da341f3240018798eeeca6fd5d6a8b4379
yide12 <wangyide10@huawei.com>
2024-06-17 15:26:51 +0800

    aiturbo_master

---
+++ tests/st/export_and_load/test_aiturbo.py
@@ -0,0 +1,50 @@
+# Copyright 2024 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+import os
+import pytest
+import numpy as np
+
+from mindspore.train import ModelCheckpoint, CheckpointConfig
+
+
+@pytest.mark.level0
+@pytest.mark.platform_arm_ascend910b_training
+@pytest.mark.platform_x86_ascend910b_training
+@pytest.mark.env_onecard
+def test_aiturbo_save_error():
+    '''
+    Feature: aiturbo raise import error
+    Description: save checkpoint without aiturbo
+    Expectation: success
+    '''
+    os.environ["AITURBO"] = "1"
+    with pytest.raises(ImportError):
+        ModelCheckpoint()
+    if "AITURBO" in os.environ:
+        del os.environ["AITURBO"]
+
+
+@pytest.mark.level0
+@pytest.mark.platform_arm_ascend910b_training
+@pytest.mark.platform_x86_ascend910b_training
+@pytest.mark.env_onecard
+def test_aiturbo_CheckpointConfig():
+    '''
+    Feature: CheckpointConfig init
+    Description: CheckpointConfig init with enable_redundance
+    Expectation: success
+    '''
+    config = CheckpointConfig(save_checkpoint_seconds=100, keep_checkpoint_per_n_minutes=5, enable_redundance=True)
+    assert np.allclose(config.enable_redundance, True)
