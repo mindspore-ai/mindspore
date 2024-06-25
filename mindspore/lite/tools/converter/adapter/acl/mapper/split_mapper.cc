40c8121a0ab0fcc281a9adf3f351e93513bd7a07
ZhangZGC <zhangzhugucheng1@huawei.com>
2024-06-25 14:08:22 +0800

    fix split mapper bug

--- mindspore/lite/tools/converter/adapter/acl/mapper/split_mapper.cc
+++ mindspore/lite/tools/converter/adapter/acl/mapper/split_mapper.cc
@@ -71,7 +71,7 @@ STATUS SplitMapper::Mapper(const CNodePtr &cnode) {
       dst_prim->AddAttr("size_splits", size_splits_value);
     }
     if (size_splits_value == nullptr) {
-      int status = AddIntAttrToInput(func_graph, cnode, dst_prim, ops::kAxis, false);
+      int status = AddIntAttrToInput(func_graph, cnode, src_prim, ops::kAxis, false);
       if (status != RET_OK) {
         MS_LOG(ERROR) << "Add axis constant value to input failed.";
         return RET_ERROR;
