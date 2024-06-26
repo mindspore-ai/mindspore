1ad494c4532646b0376e2d39bebefb2430bef1c5
liuruotao <liuruotao@huawei.com>
2024-06-26 10:21:09 +0800

    takedown random failure testcases

--- tests/st/dyn_shape_dev/test_print_string.py
+++ tests/st/dyn_shape_dev/test_print_string.py
@@ -214,7 +214,7 @@ def test_side_effect_bprop_one_input():
 
 
 @security_off_wrap
-@pytest.mark.level0
+@pytest.mark.level1
 @pytest.mark.platform_arm_ascend_training
 @pytest.mark.platform_x86_ascend_training
 @pytest.mark.env_onecard
