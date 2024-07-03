f069e31adba3dacf17b527065452084b0ad0af23
tianxiaodong <tianxiaodong3@huawei.com>
2024-07-03 16:40:36 +0800

    support reshape fusion

--- mindspore/ccsrc/plugin/device/ascend/optimizer/backend_common_unify_mindir.cc
+++ mindspore/ccsrc/plugin/device/ascend/optimizer/backend_common_unify_mindir.cc
@@ -142,6 +142,7 @@ void GetBackendCommonUnifyMindIRPassManager(PassManagerPtr *unify_mindir_pm) {
   }
   (*unify_mindir_pm)->AddPass(std::make_shared<opt::CentralizationMindIR>());
 #ifdef ENABLE_INTERNAL_KERNELS
+  (*unify_mindir_pm)->AddPass(std::make_shared<opt::InferenceMatmulSplitFusion>());
   (*unify_mindir_pm)->AddPass(std::make_shared<opt::AddLayernormFusion>());
   (*unify_mindir_pm)->AddPass(std::make_shared<opt::ShapeReshapeFusion>());
   (*unify_mindir_pm)->AddPass(std::make_shared<opt::ShapeReshapeFusion2>());
@@ -149,7 +150,6 @@ void GetBackendCommonUnifyMindIRPassManager(PassManagerPtr *unify_mindir_pm) {
   (*unify_mindir_pm)->AddPass(std::make_shared<opt::AddCastRmsNormCastFusion>());
   (*unify_mindir_pm)->AddPass(std::make_shared<opt::MatMulAllReduceFusion>());
   (*unify_mindir_pm)->AddPass(std::make_shared<opt::InferenceSwiGLUFusion>());
-  (*unify_mindir_pm)->AddPass(std::make_shared<opt::InferenceMatmulSplitFusion>());
 #endif  // ENABLE_INTERNAL_KERNELS
 }
 
