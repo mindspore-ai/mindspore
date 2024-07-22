bf8ce458b586dd62dc041efdf45080ce77d29119
z00512154 <zhoulili20@huawei.com>
2024-07-22 19:41:06 +0800

    change es st level1 to level0

--- tests/st/es/run_msrun.sh
+++ tests/st/es/run_msrun.sh
@@ -2,12 +2,11 @@
 rm -rf ./*.log
 CURDIR=$(pwd)
 
-HOST_IP_STR=$(ifconfig -a | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' | tr -d "addr:")
-HOST_IPS=("$HOST_IP_STR")
-HOST_IP=${HOST_IPS[-1]}
-
-DEVICE_IP=$(cat /etc/hccn.conf | grep -E 'address_0')
-DEVICE_IP=${DEVICE_IP#*=}
+HOST_IP=$(hostname -I | awk '{print $1}')
+rank_table_file="/home/workspace/mindspore_config/hccl/rank_table_8p.json"
+DEVICE_IP=$(cat $rank_table_file | grep -E "device_ip" | head -1 | awk '{print $2}' | awk -F "[\"\"]" '{print $2}')
+echo "host ip is: $HOST_IP"
+echo "device_ip is: $DEVICE_IP"
 
 RANK_TABLE_FILE=${CURDIR}/rank_table_file.json
 ESCLUSTER_CONFIG_PATH=${CURDIR}/escluster_config.json
@@ -21,11 +20,17 @@ export RANK_TABLE_FILE
 export ESCLUSTER_CONFIG_PATH
 export MS_DISABLE_REF_MODE=1
 export JOB_ID=10087
+export ASCEND_DEVICE_ID=0
+export DEVICE_NUM=1
+export DEVICE_ID=0
+export RANK_ID=0
+export RANK_SIZE=1
+export MS_DEV_JIT_SYNTAX_LEVEL=0
 
-msrun --worker_num=1 --rank_table_file=$RANK_TABLE_FILE --local_worker_num=1 python ${CURDIR}/test_es_external_api.py > es.log 2>&1 &
+python ${CURDIR}/test_es_external_api.py > worker_0.log
 
-status=$(echo $?)
-if [ "${status}" != "0" ]; then
+result=$?
+if [ $result != 0 ]; then
     exit 1
 fi
 exit 0
