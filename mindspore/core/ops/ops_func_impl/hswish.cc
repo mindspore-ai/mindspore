a01c75b5fe98bdec0ab2cd6801796e53fa488a73
shilishan <shilishan@huawei.com>
2024-07-22 11:13:37 +0800

    del infer dtype clone of hswish and hsigmoid

--- mindspore/core/ops/ops_func_impl/hswish.cc
+++ mindspore/core/ops/ops_func_impl/hswish.cc
@@ -29,7 +29,7 @@ BaseShapePtr HSwishFuncImpl::InferShape(const PrimitivePtr &primitive,
 TypePtr HSwishFuncImpl::InferType(const PrimitivePtr &primitive, const std::vector<AbstractBasePtr> &input_args) const {
   MS_EXCEPTION_IF_NULL(input_args[0]);
   MS_EXCEPTION_IF_NULL(input_args[0]->GetType());
-  return input_args[0]->GetType()->Clone();
+  return input_args[0]->GetType();
 }
 
 TypePtrList HSwishFuncImpl::InferType(const PrimitivePtr &primitive, const ValuePtrList &input_values) const {
