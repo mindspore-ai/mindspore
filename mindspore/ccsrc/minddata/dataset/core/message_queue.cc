6d4d95ed35f88f2afc3cfe94e03ac056f4b87cad
Jony Guo <guozhijian@huawei.com>
2024-07-25 19:34:06 +0800

    feature: uniform err msg for independent dataset

--- mindspore/ccsrc/minddata/dataset/core/message_queue.cc
+++ mindspore/ccsrc/minddata/dataset/core/message_queue.cc
@@ -31,9 +31,18 @@ MessageQueue::MessageQueue(key_t key, int msg_queue_id)
       key_(key),
       msg_queue_id_(msg_queue_id),
       release_flag_(true),
-      state_(State::kInit) {}
+      state_(State::kInit) {
+  auto ret = memset_s(err_msg_, kWorkerErrorMsgSize, 0, kWorkerErrorMsgSize);
+  if (ret != EOK) {
+    MS_LOG(EXCEPTION) << "memset_s failed. err code: " << std::to_string(ret);
+  }
+}
 
-MessageQueue::~MessageQueue() {
+MessageQueue::~MessageQueue() { ReleaseQueue(); }
+
+void MessageQueue::SetReleaseFlag(bool flag) { release_flag_ = flag; }
+
+void MessageQueue::ReleaseQueue() {
   if (release_flag_ && msg_queue_id_ != -1 && msgget(key_, kMsgQueuePermission) == msg_queue_id_) {
     if (msgctl(msg_queue_id_, IPC_RMID, 0) == -1) {
       MS_LOG(ERROR) << "Delete msg queue id: " << msg_queue_id_ << " failed.";
@@ -43,8 +52,6 @@ MessageQueue::~MessageQueue() {
     msg_queue_id_ = -1;
   }
 }
-
-void MessageQueue::SetReleaseFlag(bool flag) { release_flag_ = flag; }
 
 Status MessageQueue::GetOrCreateMessageQueueID() {
   msg_queue_id_ = msgget(key_, kMsgQueuePermission);
@@ -99,6 +106,203 @@ Status MessageQueue::MsgRcv(int64_t mtype) {
 
 int MessageQueue::MsgRcv(int64_t mtype, int msgflg) {
   return msgrcv(msg_queue_id_, this, sizeof(MessageQueue), mtype, msgflg);
+}
+
+// two error status case:
+//
+// case 1 - contains c layer info with cpp file and line number
+//
+// E    RuntimeError: Exception thrown from user defined Python function in dataset.
+// E
+// E    ------------------------------------------------------------------
+// E    - Python Call Stack:
+// E    ------------------------------------------------------------------
+// E    map operation: [PyFunc] failed. The corresponding data file is: ../train-0000-of-0001.data. Error description:
+// E    ValueError: Traceback (most recent call last):
+// E      File "/home/user/mindspore/dataset/transforms/py_transforms_util.py", line 63, in compose
+// E        args = transform(*args)
+// E      File "/home/user/mindspore/dataset/transforms/transforms.py", line 85, in __call__
+// E        return self._execute_py(*input_tensor_list)
+// E      File "/home/user/mindspore/dataset/transforms/transforms.py", line 946, in _execute_py
+// E        return util.random_choice(img, self.transforms)
+// E      File "/home/user/mindspore/dataset/transforms/py_transforms_util.py", line 171, in random_choice
+// E        return random.choice(transforms)(img)
+// E      File "/home/user/mindspore/dataset/vision/transforms.py", line 97, in __call__
+// E        return super().__call__(*input_tensor_list)
+// E      File "/home/user/mindspore/dataset/transforms/transforms.py", line 85, in __call__
+// E        return self._execute_py(*input_tensor_list)
+// E      File "/home/user/mindspore/dataset/vision/transforms.py", line 4279, in _execute_py
+// E        self.fill_value, Border.to_python_type(self.padding_mode))
+// E      File "/home/user/mindspore/dataset/vision/py_transforms_util.py", line 490, in random_crop
+// E        top, left, height, width = _input_to_factor(img, size)
+// E      File "/home/user/mindspore/dataset/vision/py_transforms_util.py", line 472, in _input_to_factor
+// E        raise ValueError("Crop size {} is larger than input image size {}.".format(size, (img_height, img_width)))
+// E    ValueError: Crop size (5000, 5000) is larger than input image size (2268, 4032).
+// E
+// E    ------------------------------------------------------------------
+// E    - Dataset Pipeline Error Message:
+// E    ------------------------------------------------------------------
+// E    [ERROR] Execute user Python code failed, check 'Python Call Stack' above.
+// E
+// E    ------------------------------------------------------------------
+// E    - C++ Call Stack: (For framework developers)
+// E    ------------------------------------------------------------------
+// E    mindspore/ccsrc/minddata/dataset/engine/datasetops/map_op/map_job.h(57).
+//
+// ../../../build/package/mindspore/dataset/engine/iterators.py:260: RuntimeError
+//
+// case 2 - just python stack info
+//
+// E    RuntimeError: Exception thrown from user defined Python function in dataset.
+// E
+// E    ------------------------------------------------------------------
+// E    - Python Call Stack:
+// E    ------------------------------------------------------------------
+// E    ZeroDivisionError: Traceback (most recent call last):
+// E      File "/home/user/mindspore/dataset/transforms/py_transforms_util.py", line 199, in __call__
+// E        result = self.transform(*args)
+// E      File "/home/user/mindspore/tests/ut/python/dataset/test_formatted_exception.py", line 125, in batch_func
+// E        fake_data = 1/zero
+// E    ZeroDivisionError: division by zero
+// E
+// E    ------------------------------------------------------------------
+// E    - Dataset Pipeline Error Message:
+// E    ------------------------------------------------------------------
+// E    [ERROR] Execute user Python code failed, check 'Python Call Stack' above.
+//
+// ../../../build/package/mindspore/dataset/engine/iterators.py:260: RuntimeError
+
+Status MessageQueue::SerializeStatus(const Status &status) {
+  // StatusCode : 4bytes
+  // line_of_code: 4bytes
+  // file_name : 4bytes + data
+  // err_description : 4bytes + data
+  auto ret = memset_s(err_msg_, kWorkerErrorMsgSize, 0, kWorkerErrorMsgSize);
+  CHECK_FAIL_RETURN_UNEXPECTED(ret == EOK, "memset_s failed. err code: " + std::to_string(ret));
+
+  MS_LOG(INFO) << "Begin serialize status: " << status.ToString();
+
+  // StatusCode
+  int32_t offset = 0;
+  auto status_code = status.StatusCode();
+  auto ret_code = memcpy_s(err_msg_ + offset, kFourBytes, &status_code, kFourBytes);
+  CHECK_FAIL_RETURN_UNEXPECTED(ret_code == EOK,
+                               "memcpy_s the status code of Status failed. err code: " + std::to_string(ret_code));
+  offset += kFourBytes;
+
+  // line_of_code
+  int line_of_code = status.GetLineOfCode();
+  ret_code = memcpy_s(err_msg_ + offset, kFourBytes, &line_of_code, kFourBytes);
+  CHECK_FAIL_RETURN_UNEXPECTED(ret_code == EOK,
+                               "memcpy_s the line number of Status failed. err code: " + std::to_string(ret_code));
+  offset += kFourBytes;
+
+  if (line_of_code != -1) {
+    // file_name
+    const auto vec_file_name = status.GetFileName();
+    std::string file_name(vec_file_name);
+    if (offset + kFourBytes + vec_file_name.size() >= kWorkerErrorMsgSize) {
+      file_name = file_name.substr(0, kWorkerErrorMsgSize - kFourBytes - offset);
+    }
+    int file_name_len = file_name.size();
+    ret_code = memcpy_s(err_msg_ + offset, kFourBytes, &file_name_len, kFourBytes);
+    CHECK_FAIL_RETURN_UNEXPECTED(
+      ret_code == EOK, "memcpy_s the file name length of Status failed. err code: " + std::to_string(ret_code));
+    offset += kFourBytes;
+
+    ret_code = memcpy_s(err_msg_ + offset, file_name_len, file_name.data(), file_name_len);
+    CHECK_FAIL_RETURN_UNEXPECTED(ret_code == EOK,
+                                 "memcpy_s the file name of Status failed. err code: " + std::to_string(ret_code));
+    offset += file_name_len;
+
+    if (vec_file_name.size() != file_name_len) {
+      err_msg_[kWorkerErrorMsgSize - 1] = '\0';
+      return Status::OK();
+    }
+  }
+
+  // err_description
+  auto vec_err_description = status.GetErrDescription();
+  std::string err_description(vec_err_description);
+  if (offset + kFourBytes + vec_err_description.size() >= kWorkerErrorMsgSize) {
+    err_description = err_description.substr(0, kWorkerErrorMsgSize - kFourBytes - offset);
+  }
+  int err_description_len = err_description.size();
+  ret_code = memcpy_s(err_msg_ + offset, kFourBytes, &err_description_len, kFourBytes);
+  CHECK_FAIL_RETURN_UNEXPECTED(
+    ret_code == EOK, "memcpy_s the err description len of Status failed. err code: " + std::to_string(ret_code));
+  offset += kFourBytes;
+
+  ret_code = memcpy_s(err_msg_ + offset, err_description_len, err_description.data(), err_description_len);
+  CHECK_FAIL_RETURN_UNEXPECTED(ret_code == EOK,
+                               "memcpy_s the err description of Status failed. err code: " + std::to_string(ret_code));
+
+  err_msg_[kWorkerErrorMsgSize - 1] = '\0';
+
+  MS_LOG(INFO) << "End serialize status.";
+  return Status::OK();
+}
+
+Status MessageQueue::DeserializeStatus() {
+  StatusCode status_code = StatusCode::kSuccess;
+  int line_of_code = -1;
+  std::string file_name = "";
+  std::string err_description = "";
+
+  MS_LOG(INFO) << "Begin deserialize status.";
+
+  // status_code
+  int32_t offset = 0;
+  auto ret_code = memcpy_s(&status_code, kFourBytes, err_msg_ + offset, kFourBytes);
+  CHECK_FAIL_RETURN_UNEXPECTED(ret_code == EOK,
+                               "memcpy_s the status code of Status failed. err code: " + std::to_string(ret_code));
+  offset += kFourBytes;
+
+  // line_of_code
+  ret_code = memcpy_s(&line_of_code, kFourBytes, err_msg_ + offset, kFourBytes);
+  CHECK_FAIL_RETURN_UNEXPECTED(ret_code == EOK,
+                               "memcpy_s the line number of Status failed. err code: " + std::to_string(ret_code));
+  offset += kFourBytes;
+
+  if (line_of_code != -1) {
+    // file_name
+    int file_name_len = 0;
+    ret_code = memcpy_s(&file_name_len, kFourBytes, err_msg_ + offset, kFourBytes);
+    CHECK_FAIL_RETURN_UNEXPECTED(ret_code == EOK,
+                                 "memcpy_s the file name len of Status failed. err code: " + std::to_string(ret_code));
+    offset += kFourBytes;
+
+    file_name.resize(file_name_len + 1, '\0');
+    ret_code = memcpy_s(file_name.data(), file_name_len, err_msg_ + offset, file_name_len);
+    CHECK_FAIL_RETURN_UNEXPECTED(ret_code == EOK,
+                                 "memcpy_s the file name of Status failed. err code: " + std::to_string(ret_code));
+    offset += file_name_len;
+
+    if (offset >= kWorkerErrorMsgSize) {
+      return Status(status_code, line_of_code, file_name.c_str());
+    }
+  }
+
+  // err_description
+  int err_description_len = 0;
+  ret_code = memcpy_s(&err_description_len, kFourBytes, err_msg_ + offset, kFourBytes);
+  CHECK_FAIL_RETURN_UNEXPECTED(
+    ret_code == EOK, "memcpy_s the err description len of Status failed. err code: " + std::to_string(ret_code));
+  offset += kFourBytes;
+
+  err_description.resize(err_description_len + 1, '\0');
+  ret_code = memcpy_s(err_description.data(), err_description_len, err_msg_ + offset, err_description_len);
+  CHECK_FAIL_RETURN_UNEXPECTED(ret_code == EOK,
+                               "memcpy_s the err description of Status failed. err code: " + std::to_string(ret_code));
+
+  Status ret;
+  if (line_of_code != -1) {
+    ret = Status(status_code, line_of_code, file_name.c_str(), err_description);
+  } else {
+    ret = Status(status_code, err_description);
+  }
+  MS_LOG(INFO) << "End deserialize status: " << ret;
+  return ret;
 }
 #endif
 }  // namespace dataset
